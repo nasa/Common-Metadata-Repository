2d232730ee80d7f3f68f13a74b0788bd29f24e06
Jason Gilman <jason@element84.com>
2013-12-02 07:49:14 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cmr-es-spatial-plugin
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/cmr_es_spatial_plugin/core_test.clj
@@ -0,0 +1,7 @@
+(ns cmr-es-spatial-plugin.core-test
+  (:require [clojure.test :refer :all]
+            [cmr-es-spatial-plugin.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
