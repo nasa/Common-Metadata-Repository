9953b289f07d5d914ce583aa5224aedad88a15c3
Jason Gilman <jason@element84.com>
2013-12-06 10:30:45 -0500

    Revert "Test commit"
    This reverts commit 6264473b36712e3e70bb1c30ec40ea684ce3e190.

--- project.clj
+++ project.clj
@@ -3,9 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-
-  ;; test commit
-
-
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.taoensso/timbre "2.6.3"]])
