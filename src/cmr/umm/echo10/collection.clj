c6363f9e5bab12101924b6d3b10b3c404f392923
Jason Gilman <jason@element84.com>
2014-03-12 14:49:23 -0400

    CMR-121: Implemented parse collection.

--- src/cmr/umm/echo10/collection.clj
+++ src/cmr/umm/echo10/collection.clj
@@ -2,12 +2,50 @@
   "Contains functions for parsing and generating the ECHO10 dialect."
   (:require [clojure.data.xml :as x]
             [clojure.java.io :as io]
+            [cmr.umm.collection :as c]
             [cmr.umm.xml-schema-validator :as v]))
 
+(defn- content-at-path
+  "This is a helper that will pull the XML content from the xml-struct at the given path.
+  Note this implementation is not likely to be particularly fast. We should investigate that as part
+  of another future issue."
+  [xml-struct path]
+  (if (sequential? path)
+    (reduce content-at-path xml-struct path)
+    (cond
+      (sequential? xml-struct) (->> xml-struct (filter #(= path (:tag %))) first :content)
+      (map? xml-struct) (when (= path (:tag xml-struct)) (:content xml-struct))
+      :else (throw (Exception.
+                     (format
+                       "Unexpected xml-struct at path. path: [%s] xml-struct: [%s]"
+                       path xml-struct))))))
+
+(defn- string-at-path
+  "Extracts a string from the given path in the XML structure."
+  [xml-struct path]
+  (-> (content-at-path xml-struct path) first str))
+
+(defn- xml-elem->Product
+  "Returns a UMM Product from a parsed Collection XML structure"
+  [xml-struct]
+  (let [collection-content (content-at-path xml-struct [:Collection])]
+    (c/map->Product {:short-name (string-at-path collection-content [:ShortName])
+                     :long-name (string-at-path collection-content [:LongName])
+                     :version-id (string-at-path collection-content [:VersionId])})))
+
+(defn- xml-elem->Collection
+  "Returns a UMM Product from a parsed Collection XML structure"
+  [xml-struct]
+  (let [collection-content (content-at-path xml-struct [:Collection])
+        product (xml-elem->Product xml-struct)]
+    (c/map->UmmCollection {:entry-id (str (:short-name product) "_" (:version-id product))
+                           :entry-title (string-at-path collection-content [:DataSetId])
+                           :product product})))
 
 (defn parse-collection
   "Parses ECHO10 XML into a UMM Collection record."
-  [xml])
+  [xml]
+  (xml-elem->Collection (x/parse-str xml)))
 
 (defn generate-collection
   "Generates ECHO10 XML from a UMM Collection record."
@@ -32,6 +70,5 @@
   "Validates the XML against the ECHO10 schema."
   [xml]
   (v/validate-xml (io/resource "schema/echo10/Collection.xsd") xml))
-
 
 
--- test/cmr/umm/test/echo10/collection.clj
+++ test/cmr/umm/test/echo10/collection.clj
@@ -8,16 +8,21 @@
             [cmr.umm.test.generators :as umm-gen]
 
             ;; my code
-            [cmr.umm.echo10.collection :as c]))
+            [cmr.umm.echo10.collection :as c]
+            [cmr.umm.collection :as umm-c]))
 
-(defspec generate-collection-is-valid-xml-test 10
+(defspec generate-collection-is-valid-xml-test 100
   (for-all [collection umm-gen/collections]
     (let [xml (c/generate-collection collection)]
       (and
         (> (count xml) 0)
-        (= 0 (count (c/validate-xml xml)))
-        ;;TODO verify fields once parsing is implemented
-        ))))
+        (= 0 (count (c/validate-xml xml)))))))
+
+(defspec generate-and-parse-collection-test 100
+  (for-all [collection umm-gen/collections]
+    (let [xml (c/generate-collection collection)
+          parsed (c/parse-collection xml)]
+      (= parsed collection))))
 
 (def valid-collection-xml
   "<Collection>
@@ -32,6 +37,17 @@
   <Visible>true</Visible>
 </Collection>")
 
+(deftest parse-collection-test
+  (let [expected (umm-c/map->UmmCollection
+                   {:entry-id "MINIMAL_1"
+                    :entry-title "A minimal valid collection V 1"
+                    :product (umm-c/map->Product
+                               {:short-name "MINIMAL"
+                                :long-name "A minimal valid collection"
+                                :version-id "1"})})
+        actual (c/parse-collection valid-collection-xml)]
+    (is (= expected actual))))
+
 (deftest validate-xml
   (testing "valid xml"
     (is (= 0 (count (c/validate-xml valid-collection-xml)))))
@@ -41,10 +57,3 @@
             "Line 5 - cvc-datatype-valid.1.2.1: 'XXXX-12-31T19:00:00-05:00' is not a valid value for 'dateTime'."
             "Line 5 - cvc-type.3.1.3: The value 'XXXX-12-31T19:00:00-05:00' of element 'LastUpdate' is not valid."]
            (c/validate-xml (s/replace valid-collection-xml "1999" "XXXX"))))))
-
-(comment
-
-(c/validate-xml (c/generate-collection (first (gen/sample-seq umm-gen/collections))))
-
-
-)
