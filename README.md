d521deafe5c5eb33973039301f60d8e113a23bd6
Jason Gilman <jason@element84.com>
2014-03-17 06:40:55 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# cmr-system-trace
+
+A Clojure library designed to ... well, that part is up to you.
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2014 NASA
+
---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject cmr-system-trace "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.5.1"]])
