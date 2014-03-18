3179b473305ace3e09b13a4bbb4d6e1093c58848
Jason Gilman <jason@element84.com>
2014-03-18 16:17:53 -0400

    Moved XML helper functions to cmr-common-lib.

--- src/cmr/umm/echo10/collection.clj
+++ src/cmr/umm/echo10/collection.clj
@@ -2,44 +2,27 @@
   "Contains functions for parsing and generating the ECHO10 dialect."
   (:require [clojure.data.xml :as x]
             [clojure.java.io :as io]
+            [cmr.common.xml :as cx]
             [cmr.umm.collection :as c]
             [cmr.umm.xml-schema-validator :as v]))
 
-(defn- content-at-path
-  "This is a helper that will pull the XML content from the xml-struct at the given path.
-  Note this implementation is not likely to be particularly fast. We should investigate that as part
-  of another future issue."
-  [xml-struct path]
-  (if (sequential? path)
-    (reduce content-at-path xml-struct path)
-    (cond
-      (sequential? xml-struct) (->> xml-struct (filter #(= path (:tag %))) first :content)
-      (map? xml-struct) (when (= path (:tag xml-struct)) (:content xml-struct))
-      :else (throw (Exception.
-                     (format
-                       "Unexpected xml-struct at path. path: [%s] xml-struct: [%s]"
-                       path xml-struct))))))
 
-(defn- string-at-path
-  "Extracts a string from the given path in the XML structure."
-  [xml-struct path]
-  (-> (content-at-path xml-struct path) first str))
 
 (defn- xml-elem->Product
   "Returns a UMM Product from a parsed Collection XML structure"
   [xml-struct]
-  (let [collection-content (content-at-path xml-struct [:Collection])]
-    (c/map->Product {:short-name (string-at-path collection-content [:ShortName])
-                     :long-name (string-at-path collection-content [:LongName])
-                     :version-id (string-at-path collection-content [:VersionId])})))
+  (let [collection-content (cx/content-at-path xml-struct [:Collection])]
+    (c/map->Product {:short-name (cx/string-at-path collection-content [:ShortName])
+                     :long-name (cx/string-at-path collection-content [:LongName])
+                     :version-id (cx/string-at-path collection-content [:VersionId])})))
 
 (defn- xml-elem->Collection
   "Returns a UMM Product from a parsed Collection XML structure"
   [xml-struct]
-  (let [collection-content (content-at-path xml-struct [:Collection])
+  (let [collection-content (cx/content-at-path xml-struct [:Collection])
         product (xml-elem->Product xml-struct)]
     (c/map->UmmCollection {:entry-id (str (:short-name product) "_" (:version-id product))
-                           :entry-title (string-at-path collection-content [:DataSetId])
+                           :entry-title (cx/string-at-path collection-content [:DataSetId])
                            :product product})))
 
 (defn parse-collection
