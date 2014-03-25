9a7b42d355ba688d9bf42e30c0931ec3897e6d85
Jason Gilman <jason@element84.com>
2014-03-25 13:36:06 -0400

    Initial commit.

---
+++ src/cmr/index_set/api/routes.clj
@@ -0,0 +1,30 @@
+(ns cmr.index-set.api.routes
+  "Defines the HTTP URL routes for the application."
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]
+            [compojure.core :refer :all]
+            [ring.middleware.json :as ring-json]
+            [cheshire.core :as json]
+            [cmr.common.log :refer (debug info warn error)]
+            [cmr.common.api.errors :as errors]
+            [cmr.system-trace.http :as http-trace]))
+
+(defn- build-routes [system]
+  (routes
+    (context "/foo" []
+      (GET "/" {params :params headers :headers context :request-context}
+        {:status 200
+         :headers {"Content-Type" "text/plain"}
+         :body "foo"}))
+    (route/not-found "Not Found")))
+
+(defn make-api [system]
+  (-> (build-routes system)
+      (http-trace/build-request-context-handler system)
+      errors/exception-handler
+      handler/site
+      ring-json/wrap-json-body
+      ring-json/wrap-json-response))
+
+
+
