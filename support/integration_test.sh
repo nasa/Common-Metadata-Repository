c5cf43ca657f00bb7792258990553795a0e8e818
Jason Gilman <jason@element84.com>
2014-03-26 08:35:26 -0400

    Revert "Temporary changes to get ci to work."
    This reverts commit a2e2ac2d607a66ab35b9992caa5ac9fbfd3f7046.

--- support/integration_test.sh
+++ support/integration_test.sh
@@ -4,27 +4,27 @@
 
 echo "Installing dependent libraries"
 
-# ## Install CMR common
-# cd ../cmr-common-lib
-# lein install
+## Install CMR common
+cd ../cmr-common-lib
+lein install
 
-# ## Install System trace lib
-# cd ../cmr-system-trace-lib
-# lein install
+## Install System trace lib
+cd ../cmr-system-trace-lib
+lein install
 
-# ## Build metadata db uber jar
-# cd ../cmr-metadata-db-app
-# lein uberjar
+## Build metadata db uber jar
+cd ../cmr-metadata-db-app
+lein uberjar
 
-# ## Capture the uberjar filename
-# UBERJAR=`find target/*standalone.jar`
-# nohup java -jar $UBERJAR &
-# MDB_PID=$!
+## Capture the uberjar filename
+UBERJAR=`find target/*standalone.jar`
+nohup java -jar $UBERJAR &
+MDB_PID=$!
 
-# echo "Metadata DB started with process id: " $MDB_PID
+echo "Metadata DB started with process id: " $MDB_PID
 
-# lein with-profile +integration-test test-out
+lein with-profile +integration-test test-out
 
-# echo "Kill metadata db"
-# kill $MDB_PID
+echo "Kill metadata db"
+kill $MDB_PID
 
