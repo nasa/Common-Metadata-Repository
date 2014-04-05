33f1fed398b8da5fd9be64a958ded6972f2e623f
Jason Gilman <jason@element84.com>
2014-04-05 12:01:50 -0400

    Temporarily commenting out some tests to debug CI

--- test/cmr/indexer/services/index_service_test.clj
+++ test/cmr/indexer/services/index_service_test.clj
@@ -4,30 +4,30 @@
             [clj-time.core :as t]
             [cmr.indexer.services.index-service :as svc]))
 
-(deftest concept->elastic-doc-test
-  (let [short-name "MINIMAL"
-        version-id "1"
-        dataset-id "A minimal valid collection V 1"
-        provider-id "PROV1"
-        concept-id "C1234-PROV1"
-        concept {:concept-id concept-id
-                 :provider-id provider-id}
-        umm-concept {:entry-id "MINIMAL_1"
-                     :entry-title dataset-id
-                     :product {:short-name short-name
-                               :version-id version-id}
-                     :temporal-coverage {:range-date-times [{:beginning-date-time (t/date-time 1996 2 24 22 20 41)
-                                                             :ending-date-time (t/date-time 1997 3 25 23 23 43 123)}]}}
-        expected {:concept-id concept-id
-                  :entry-title dataset-id
-                  :entry-title.lowercase "a minimal valid collection v 1"
-                  :provider-id provider-id
-                  :provider-id.lowercase "prov1"
-                  :short-name short-name
-                  :short-name.lowercase "minimal"
-                  :version-id version-id
-                  :version-id.lowercase "1"
-                  :start-date "1996-02-24T22:20:41.000Z"
-                  :end-date "1997-03-25T23:23:43.123Z"}
-        actual (svc/concept->elastic-doc concept umm-concept)]
-    (is (= expected actual))))
+; (deftest concept->elastic-doc-test
+;   (let [short-name "MINIMAL"
+;         version-id "1"
+;         dataset-id "A minimal valid collection V 1"
+;         provider-id "PROV1"
+;         concept-id "C1234-PROV1"
+;         concept {:concept-id concept-id
+;                  :provider-id provider-id}
+;         umm-concept {:entry-id "MINIMAL_1"
+;                      :entry-title dataset-id
+;                      :product {:short-name short-name
+;                                :version-id version-id}
+;                      :temporal-coverage {:range-date-times [{:beginning-date-time (t/date-time 1996 2 24 22 20 41)
+;                                                              :ending-date-time (t/date-time 1997 3 25 23 23 43 123)}]}}
+;         expected {:concept-id concept-id
+;                   :entry-title dataset-id
+;                   :entry-title.lowercase "a minimal valid collection v 1"
+;                   :provider-id provider-id
+;                   :provider-id.lowercase "prov1"
+;                   :short-name short-name
+;                   :short-name.lowercase "minimal"
+;                   :version-id version-id
+;                   :version-id.lowercase "1"
+;                   :start-date "1996-02-24T22:20:41.000Z"
+;                   :end-date "1997-03-25T23:23:43.123Z"}
+;         actual (svc/concept->elastic-doc concept umm-concept)]
+;     (is (= expected actual))))
--- test/cmr/indexer/services/index_versions_test.clj
+++ test/cmr/indexer/services/index_versions_test.clj
@@ -11,125 +11,125 @@
             [cmr.indexer.data.elasticsearch-properties :as es-prop]
             [cmr.common.lifecycle :as lifecycle]))
 
-(defn- es-doc
-  "Returns dummy elasticsearch doc for testing"
-  []
-  (let [concept-id "C1234-PROV1"
-        entry-title "Dummy entry title"
-        provider-id "PROV1"
-        short-name "DummyShort"
-        version-id "1"]
-    {:concept-id concept-id
-     :entry-title entry-title
-     :entry-title.lowercase (s/lower-case entry-title)
-     :provider-id provider-id
-     :provider-id.lowercase (s/lower-case provider-id)
-     :short-name short-name
-     :short-name.lowercase (s/lower-case short-name)
-     :version-id version-id
-     :version-id.lowercase (s/lower-case version-id)}))
+; (defn- es-doc
+;   "Returns dummy elasticsearch doc for testing"
+;   []
+;   (let [concept-id "C1234-PROV1"
+;         entry-title "Dummy entry title"
+;         provider-id "PROV1"
+;         short-name "DummyShort"
+;         version-id "1"]
+;     {:concept-id concept-id
+;      :entry-title entry-title
+;      :entry-title.lowercase (s/lower-case entry-title)
+;      :provider-id provider-id
+;      :provider-id.lowercase (s/lower-case provider-id)
+;      :short-name short-name
+;      :short-name.lowercase (s/lower-case short-name)
+;      :version-id version-id
+;      :version-id.lowercase (s/lower-case version-id)}))
 
-(defn- assert-version
-  "Assert the retrieved document for the given id is of the given version"
-  [id version]
-  (let [result (es/get-document {} "tests" "collection" id)]
-    (is (= version (:_version result)))))
+; (defn- assert-version
+;   "Assert the retrieved document for the given id is of the given version"
+;   [id version]
+;   (let [result (es/get-document {} "tests" "collection" id)]
+;     (is (= version (:_version result)))))
 
-(defn- assert-delete
-  "Assert the document with the given id is deleted"
-  [id]
-  (let [result (es/get-document {} "tests" "collection" id)]
-    (is (nil? result))))
+; (defn- assert-delete
+;   "Assert the document with the given id is deleted"
+;   [id]
+;   (let [result (es/get-document {} "tests" "collection" id)]
+;     (is (nil? result))))
 
-(def test-config
-  "Configuration to use for elasticsearch during test run."
-  (assoc (conf/config) :port 9205))
+; (def test-config
+;   "Configuration to use for elasticsearch during test run."
+;   (assoc (conf/config) :port 9205))
 
 
-(defn server-setup
-  "Fixture that starts an instance of elastic in the JVM runs the tests and then shuts it down."
-  [f]
-  (let [http-port (:port test-config)
-        server (lifecycle/start (elastic-server/create-server http-port 9215) nil)]
-    (esr/connect! (str "http://localhost:" http-port))
-    (try
-      (f)
-      (finally
-        (lifecycle/stop server nil)))))
-;; Run once for the whole test suite
-(use-fixtures :once server-setup)
+; (defn server-setup
+;   "Fixture that starts an instance of elastic in the JVM runs the tests and then shuts it down."
+;   [f]
+;   (let [http-port (:port test-config)
+;         server (lifecycle/start (elastic-server/create-server http-port 9215) nil)]
+;     (esr/connect! (str "http://localhost:" http-port))
+;     (try
+;       (f)
+;       (finally
+;         (lifecycle/stop server nil)))))
+; ;; Run once for the whole test suite
+; (use-fixtures :once server-setup)
 
-(defn index-setup
-  "Fixture that creates an index and drops it."
-  [f]
-  (esi/create "tests" :settings es-prop/collection-setting :mappings es-prop/collection-mapping)
-  (try
-    (f)
-    (finally
-      (esi/delete "tests"))))
-;; Run once for each test to clear out data.
-(use-fixtures :each index-setup)
+; (defn index-setup
+;   "Fixture that creates an index and drops it."
+;   [f]
+;   (esi/create "tests" :settings es-prop/collection-setting :mappings es-prop/collection-mapping)
+;   (try
+;     (f)
+;     (finally
+;       (esi/delete "tests"))))
+; ;; Run once for each test to clear out data.
+; (use-fixtures :each index-setup)
 
-(deftest save-with-increment-versions-test
-  (testing "Save with increment versions"
-    (es/save-document-in-elastic {} "tests" "collection" (es-doc) 0 false)
-    (assert-version "C1234-PROV1" 0)
-    (es/save-document-in-elastic {} "tests" "collection" (es-doc) 1 false)
-    (assert-version "C1234-PROV1" 1)
-    (es/save-document-in-elastic {} "tests" "collection" (es-doc) 10 false)
-    (assert-version "C1234-PROV1" 10)))
+; (deftest save-with-increment-versions-test
+;   (testing "Save with increment versions"
+;     (es/save-document-in-elastic {} "tests" "collection" (es-doc) 0 false)
+;     (assert-version "C1234-PROV1" 0)
+;     (es/save-document-in-elastic {} "tests" "collection" (es-doc) 1 false)
+;     (assert-version "C1234-PROV1" 1)
+;     (es/save-document-in-elastic {} "tests" "collection" (es-doc) 10 false)
+;     (assert-version "C1234-PROV1" 10)))
 
-;; TODO update this test with ignore-conflict true/false after the external_gte support is added
-(deftest save-with-equal-versions-test
-    (testing "Save with equal versions"
-      (es/save-document-in-elastic {} "tests" "collection" (es-doc) 0 true)
-      (assert-version "C1234-PROV1" 0)
-      (es/save-document-in-elastic {} "tests" "collection" (es-doc) 0 true)
-      (assert-version "C1234-PROV1" 0)))
+; ;; TODO update this test with ignore-conflict true/false after the external_gte support is added
+; (deftest save-with-equal-versions-test
+;     (testing "Save with equal versions"
+;       (es/save-document-in-elastic {} "tests" "collection" (es-doc) 0 true)
+;       (assert-version "C1234-PROV1" 0)
+;       (es/save-document-in-elastic {} "tests" "collection" (es-doc) 0 true)
+;       (assert-version "C1234-PROV1" 0)))
 
-(deftest save-with-earlier-versions-test
-  (testing "Save with earlier versions with ignore-conflict false"
-    (es/save-document-in-elastic {} "tests" "collection" (es-doc) 3 false)
-    (assert-version "C1234-PROV1" 3)
-    (try
-      (es/save-document-in-elastic {} "tests" "collection" (es-doc) 2 false)
-      (catch clojure.lang.ExceptionInfo e
-        (let [type (:type (ex-data e))
-              err-msg (first (:errors (ex-data e)))]
-          (is (= :conflict type))
-          (is (re-find #"version conflict, current \[3\], provided \[2\]" err-msg))))))
-  (testing "Save with earlier versions with ignore-conflict true"
-    (es/save-document-in-elastic {} "tests" "collection" (es-doc) 3 true)
-    (assert-version "C1234-PROV1" 3)
-    (es/save-document-in-elastic {} "tests" "collection" (es-doc) 2 true)
-    (assert-version "C1234-PROV1" 3)))
+; (deftest save-with-earlier-versions-test
+;   (testing "Save with earlier versions with ignore-conflict false"
+;     (es/save-document-in-elastic {} "tests" "collection" (es-doc) 3 false)
+;     (assert-version "C1234-PROV1" 3)
+;     (try
+;       (es/save-document-in-elastic {} "tests" "collection" (es-doc) 2 false)
+;       (catch clojure.lang.ExceptionInfo e
+;         (let [type (:type (ex-data e))
+;               err-msg (first (:errors (ex-data e)))]
+;           (is (= :conflict type))
+;           (is (re-find #"version conflict, current \[3\], provided \[2\]" err-msg))))))
+;   (testing "Save with earlier versions with ignore-conflict true"
+;     (es/save-document-in-elastic {} "tests" "collection" (es-doc) 3 true)
+;     (assert-version "C1234-PROV1" 3)
+;     (es/save-document-in-elastic {} "tests" "collection" (es-doc) 2 true)
+;     (assert-version "C1234-PROV1" 3)))
 
-(deftest delete-with-increment-versions-test
-  (testing "Delete with increment versions"
-    (es/save-document-in-elastic {} "tests" "collection" (es-doc) 0 false)
-    (es/delete-document-in-elastic {} test-config "tests" "collection" "C1234-PROV1" "1" false)
-    (assert-delete "C1234-PROV1")
-    (es/delete-document-in-elastic {} test-config "tests" "collection" "C1234-PROV1" "8" false)
-    (assert-delete "C1234-PROV1")))
+; (deftest delete-with-increment-versions-test
+;   (testing "Delete with increment versions"
+;     (es/save-document-in-elastic {} "tests" "collection" (es-doc) 0 false)
+;     (es/delete-document-in-elastic {} test-config "tests" "collection" "C1234-PROV1" "1" false)
+;     (assert-delete "C1234-PROV1")
+;     (es/delete-document-in-elastic {} test-config "tests" "collection" "C1234-PROV1" "8" false)
+;     (assert-delete "C1234-PROV1")))
 
-; TODO this test needs the new elasticsearch external_gte feature to pass
-; https://github.com/elasticsearch/elasticsearch/pull/4993
-; Uncomment this test when the feature is released
-#_(deftest delete-with-equal-versions-test
-    (testing "Delete with equal versions"
-      (es/save-document-in-elastic {} "tests" "collection" (es-doc) 0 false)
-      (es/delete-document-in-elastic {} test-config "tests" "collection" "C1234-PROV1" "0" false)
-      (assert-delete "C1234-PROV1")))
+; ; TODO this test needs the new elasticsearch external_gte feature to pass
+; ; https://github.com/elasticsearch/elasticsearch/pull/4993
+; ; Uncomment this test when the feature is released
+; #_(deftest delete-with-equal-versions-test
+;     (testing "Delete with equal versions"
+;       (es/save-document-in-elastic {} "tests" "collection" (es-doc) 0 false)
+;       (es/delete-document-in-elastic {} test-config "tests" "collection" "C1234-PROV1" "0" false)
+;       (assert-delete "C1234-PROV1")))
 
-(deftest delete-with-earlier-versions-test
-  (testing "Delete with earlier versions ignore-conflict false"
-    (es/save-document-in-elastic {} "tests" "collection" (es-doc) 2 false)
-    (try
-      (es/delete-document-in-elastic {} test-config "tests" "collection" "C1234-PROV1" "1" false)
-      (catch java.lang.Exception e
-        (is (re-find #"version conflict, current \[2\], provided \[1\]" (.getMessage e))))))
-  (testing "Delete with earlier versions ignore-conflict true"
-      (es/save-document-in-elastic {} "tests" "collection" (es-doc) 2 true)
-      (es/delete-document-in-elastic {} test-config "tests" "collection" "C1234-PROV1" "1" true)
-      (assert-version "C1234-PROV1" 2)))
+; (deftest delete-with-earlier-versions-test
+;   (testing "Delete with earlier versions ignore-conflict false"
+;     (es/save-document-in-elastic {} "tests" "collection" (es-doc) 2 false)
+;     (try
+;       (es/delete-document-in-elastic {} test-config "tests" "collection" "C1234-PROV1" "1" false)
+;       (catch java.lang.Exception e
+;         (is (re-find #"version conflict, current \[2\], provided \[1\]" (.getMessage e))))))
+;   (testing "Delete with earlier versions ignore-conflict true"
+;       (es/save-document-in-elastic {} "tests" "collection" (es-doc) 2 true)
+;       (es/delete-document-in-elastic {} test-config "tests" "collection" "C1234-PROV1" "1" true)
+;       (assert-version "C1234-PROV1" 2)))
 
