a356adf51706b79e5019bcac9f6efe55278cba1b
Jason Gilman <jason@element84.com>
2014-04-05 11:51:16 -0400

    Putting the elastic utils lib in the correct maven group.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cmr-elastic-utils-lib "0.1.0-SNAPSHOT"
+(defproject nasa-cmr/cmr-elastic-utils-lib "0.1.0-SNAPSHOT"
   :description "A library containing utilities for dealing with Elasticsearch."
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
