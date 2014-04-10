bd714009e011a3c7a92ce84000f58518720d33d2
Jason Gilman <jason@element84.com>
2014-04-10 13:10:21 -0400

    Removing cmr.metadata-db.database namespace.

--- src/cmr/metadata_db/database.clj
+++
@@ -1,9 +0,0 @@
-(ns cmr.metadata-db.database
-  "Defines a protocol for basic operations on a database.")
-
-(defprotocol Database
-  "Provides functions for basic database operations."
-  
-  (reset
-    [db]
-    "Delete everything from the database.  USE WITH CAUTION."))
