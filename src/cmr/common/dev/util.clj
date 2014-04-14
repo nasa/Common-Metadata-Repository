9b520192e8e853ad58b82535e7ac7c09a42707bc
James Norton <james@element84.com>
2014-04-14 08:06:29 -0400

    Added workaround for problem with REPL not reloading code at times.

--- src/cmr/common/dev/util.clj
+++ src/cmr/common/dev/util.clj
@@ -1,15 +1,33 @@
 (ns cmr.common.dev.util
   "This contains utility functions for development"
-  (:require [clojure.java.shell :as sh]))
+  (:require [clojure.java.shell :as sh]
+            [clojure.java.io :as io]))
+
+(defn touch-file
+  [file]
+  (future
+    (try
+      (clojure.java.shell/sh "touch" file)
+      (catch Throwable e
+        (println "Error touch" file)
+        (.printStackTrace e))))
+  nil)
+  
 
 (defn touch-user-clj
   "Touches dev/user.clj to help avoid cases where file changes are not caught by
   clojure.tools.namespace refresh."
   []
-  (future
-    (try
-      (clojure.java.shell/sh "touch" "dev/user.clj")
-      (catch Throwable e
-        (println "Error touch user.clj")
-        (.printStackTrace e))))
-  nil)
+  (touch-file "dev/user.clj"))
+
+(defn touch-files-in-dir
+  "Touches all top level files in the folder."
+  [dir]
+  (let [d (io/file dir)
+        files (seq (.listFiles d))]
+    (dorun (map #(-> % str touch-file) (filter #(not (.isDirectory %)) files)))))
+
+(comment 
+(touch-files-in-dir ".")
+
+)
