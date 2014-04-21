d262a3e2660edd4710f7e240db790a50a38d5ec3
Jason Gilman <jason@element84.com>
2014-04-21 07:59:07 -0400

    Added some code comments about changes I'd like to make and updated the way the date-time generator is used.

--- src/cmr/umm/test/generators/collection/temporal.clj
+++ src/cmr/umm/test/generators/collection/temporal.clj
@@ -2,49 +2,55 @@
   "Provides clojure.test.check generators for use in testing other projects."
   (:require [clojure.test.check.generators :as gen]
             [clj-time.core :as t]
-            [cmr.common.test.test-check-ext :as ext-gen :refer [optional]]
+            [cmr.common.test.test-check-ext :as ext-gen]
             [cmr.umm.collection :as c]
             [cmr.umm.temporal-coverage :as tc]))
 
 ;; temporal attributes
 (def time-types
-  (optional (ext-gen/string-alpha-numeric 1 80)))
+  ;; FIXME - I'd prefer optional to be used where a field is used. A field is optional. The value
+  ;; should always be generated.
+  (ext-gen/optional (ext-gen/string-alpha-numeric 1 80)))
 
 (def date-types
-  (optional (ext-gen/string-alpha-numeric 1 80)))
+  (ext-gen/optional (ext-gen/string-alpha-numeric 1 80)))
 
 (def temporal-range-types
-  (optional (ext-gen/string-alpha-numeric 1 80)))
+  (ext-gen/optional (ext-gen/string-alpha-numeric 1 80)))
 
 (def precision-of-seconds
-  (optional gen/s-pos-int))
+  (ext-gen/optional gen/s-pos-int))
 
 (def ends-at-present-flag
-  (optional gen/boolean))
+  (ext-gen/optional gen/boolean))
 
 (def range-date-times
-  (let [begin (first (gen/sample (ext-gen/date-time) 1))
+  ;; FIXME - We should not be using sampling inside of a generator.
+  ;; the bind function is the way to accomplish this.
+  (let [begin (first (gen/sample ext-gen/date-time 1))
         end (t/plus begin (t/days 1))]
-    (ext-gen/model-gen c/->RangeDateTime (gen/return begin) (optional (gen/return end)))))
+    (ext-gen/model-gen c/->RangeDateTime (gen/return begin) (ext-gen/optional (gen/return end)))))
 
 ;; periodic-date-time attributes
 (def names
   (ext-gen/string-alpha-numeric 1 30))
 
 (def duration-units
-  (gen/one-of [(gen/return "DAY") (gen/return "MONTH") (gen/return "YEAR")]))
+  (gen/elements ["DAY" "MONTH" "YEAR"]))
+
+;; FIXME both duration values and period-cycle-duration values aren't ideal here
+;; They only choose a valid that makes sense as a month. Their value should change based on
+;; the duration unit being used.
 
 (def duration-values
   (gen/choose 1 12))
-
-(def period-cycle-duration-units
-  (gen/one-of [(gen/return "DAY") (gen/return "MONTH") (gen/return "YEAR")]))
 
 (def period-cycle-duration-values
   (gen/choose 1 12))
 
 (def periodic-date-times
-  (let [begin (first (gen/sample (ext-gen/date-time) 1))
+  ;; FIXME - We should not be using sampling inside of a generator.
+  (let [begin (first (gen/sample ext-gen/date-time 1))
         end (t/plus begin (t/days 1))]
     (ext-gen/model-gen
       c/map->PeriodicDateTime
@@ -53,7 +59,7 @@
                     :end-date (gen/return end)
                     :duration-unit duration-units
                     :duration-value duration-values
-                    :period-cycle-duration-unit period-cycle-duration-units
+                    :period-cycle-duration-unit duration-units
                     :period-cycle-duration-value period-cycle-duration-values))))
 
 (def temporal-coverages-ranges
@@ -74,7 +80,7 @@
                   :temporal-range-type temporal-range-types
                   :precision-of-seconds precision-of-seconds
                   :ends-at-present-flag ends-at-present-flag
-                  :single-date-times (gen/vector (ext-gen/date-time) 1 3))))
+                  :single-date-times (gen/vector ext-gen/date-time 1 3))))
 
 (def temporal-coverages-periodics
   (ext-gen/model-gen
