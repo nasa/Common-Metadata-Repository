2fcb44626774f8b1f3d9d51779698009d5d91834
Jason Gilman <jason@element84.com>
2014-04-21 12:18:45 -0400

    Deprecated time parser namespace.

--- src/cmr/common/date_time_parser.clj
+++ src/cmr/common/date_time_parser.clj
@@ -2,8 +2,18 @@
   "Contains helper to parse datetime string to clj-time"
   (:require [clj-time.format :as f]))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; DEPRECATED
+;; Each project should do it's own time parsing using clj-time.format
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Define the regex for datetime parsing. It will match something like "1986-10-14T04:03:27.456-05:00Z"
 (def datetime-regex #"^(\d\d\d\d-(\d)?\d-(\d)?\dT(\d)?\d:(\d)?\d:(\d)?\d(\.\d+)?)(([+-]\d\d:\d\d)|Z)?$")
+
+(def formatter
+  "The formatter used to parse date time strings"
+  (f/formatters :date-hour-minute-second-fraction))
 
 (defn string->datetime
   "Convert datetime string to Joda DateTime"
@@ -11,5 +21,5 @@
   (if-let [matches (re-find datetime-regex value)]
     (let [match (get matches 1)
           conformed-value (if (re-find #"\." match) match (str match ".000"))]
-      (f/parse (f/formatters :date-hour-minute-second-fraction) conformed-value))
+      (f/parse formatter conformed-value))
     (throw (Exception. (format "Invalid DateTime string: %s" value)))))
