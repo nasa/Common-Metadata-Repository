5b56a715cfe657e2bc3a3418aef57b3edadb4326
James Norton <james@element84.com>
2014-04-21 08:38:58 -0400

    Added comments to messages namespace.

--- src/cmr/common/services/messages.clj
+++ src/cmr/common/services/messages.clj
@@ -1,6 +1,10 @@
 (ns cmr.common.services.messages
+  "This namespace provides functions to generate messages used for error reporting, logging, etc.
+   Messages used in more than one project should be placed here."
   (:require [clojure.string :as str]
             [cmr.common.services.errors :as errors]))
 
 (defn data-error [error-type msg-fn & args]
+  "Utility method that uses throw-service-error to generate a response with a specific status code
+  and error message."
   (errors/throw-service-error error-type (apply msg-fn args)))
