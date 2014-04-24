da0e1a36ba998ba0ae75b05e781bf7006d9405d5
Yonggang Liu <ygliuvt@gmail.com>
2014-04-24 17:53:04 -0400

    Added indexing support for granule temporal search.

--- src/cmr/indexer/services/granule.clj
+++ src/cmr/indexer/services/granule.clj
@@ -1,8 +1,10 @@
 (ns cmr.indexer.services.granule
   "Contains functions to parse and convert granule concept"
   (:require [clojure.string :as s]
+            [clj-time.format :as f]
             [cmr.indexer.services.index-service :as idx]
-            [cmr.umm.echo10.granule :as granule]))
+            [cmr.umm.echo10.granule :as granule]
+            [cmr.indexer.services.temporal :as temporal]))
 
 (defmethod idx/parse-concept :granule
   [concept]
@@ -12,10 +14,14 @@
   [concept umm-concept]
   (let [{:keys [concept-id extra-fields provider-id]} concept
         {:keys [parent-collection-id]} extra-fields
-        {:keys [granule-ur]} umm-concept]
+        {:keys [granule-ur temporal-coverage]} umm-concept
+        start-date (temporal/start-date :granule temporal-coverage)
+        end-date (temporal/end-date :granule temporal-coverage)]
     {:concept-id concept-id
      :collection-concept-id parent-collection-id
      :provider-id provider-id
      :provider-id.lowercase (s/lower-case provider-id)
      :granule-ur granule-ur
-     :granule-ur.lowercase (s/lower-case granule-ur)}))
+     :granule-ur.lowercase (s/lower-case granule-ur)
+     :start-date (when start-date (f/unparse (f/formatters :date-time) start-date))
+     :end-date (when end-date (f/unparse (f/formatters :date-time) end-date))}))
