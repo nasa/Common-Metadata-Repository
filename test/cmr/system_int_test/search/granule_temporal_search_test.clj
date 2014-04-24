a477f7e96464d9ad06c0cab7b1df956ad5293d18
Yonggang Liu <ygliuvt@gmail.com>
2014-04-24 17:50:26 -0400

    CMR-405: Added temporal data setup for collection/granule. Added tests for granule temporal search.

---
+++ test/cmr/system_int_test/search/granule_temporal_search_test.clj
@@ -0,0 +1,95 @@
+(ns cmr.system-int-test.search.granule-temporal-search-test
+  "Integration test for CMR granule temporal search"
+  (:require [clojure.test :refer :all]
+            [cmr.system-int-test.utils.ingest-util :as ingest]
+            [cmr.system-int-test.utils.search-util :as search]
+            [cmr.system-int-test.utils.index-util :as index]
+            [cmr.system-int-test.data2.collection :as dc]
+            [cmr.system-int-test.data2.granule :as dg]
+            [cmr.system-int-test.data2.core :as d]))
+
+(use-fixtures :each (ingest/reset-fixture "CMR_PROV1"))
+
+(deftest search-by-temporal
+  (let [coll1 (d/ingest "CMR_PROV1" (dc/collection {}))
+        gran1 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule1"
+                                                       :beginning-date-time "2010-01-01T12:00:00Z"
+                                                       :ending-date-time "2010-01-11T12:00:00Z"}))
+        gran2 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule2"
+                                                       :beginning-date-time "2010-01-31T12:00:00Z"
+                                                       :ending-date-time "2010-12-12T12:00:00Z"}))
+        gran3 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule3"
+                                                       :beginning-date-time "2010-12-03T12:00:00Z"
+                                                       :ending-date-time "2010-12-20T12:00:00Z"}))
+        gran4 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule4"
+                                                       :beginning-date-time "2010-12-12T12:00:00Z"
+                                                       :ending-date-time "2011-01-03T12:00:00Z"}))
+        gran5 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule5"
+                                                       :beginning-date-time "2011-02-01T12:00:00Z"
+                                                       :ending-date-time "2011-03-01T12:00:00Z"}))
+        gran6 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule6"
+                                                       :beginning-date-time "2010-01-30T12:00:00Z"}))
+        gran7 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule7"
+                                                       :beginning-date-time "2010-12-12T12:00:00Z"}))
+        gran8 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule8"
+                                                       :beginning-date-time "2011-12-13T12:00:00Z"}))
+        gran9 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule9"}))]
+    (index/flush-elastic-index)
+
+    (testing "search by temporal_start."
+      (let [references (search/find-refs :granule
+                                         {"temporal[]" "2010-12-12T12:00:00Z,"})]
+        (is (= 7 (count references)))
+        (is (= #{"Granule2" "Granule3" "Granule4" "Granule5"
+                 "Granule6" "Granule7" "Granule8"}
+               (set (map :name references))))))
+    (testing "search by temporal_end."
+      (let [references (search/find-refs :granule
+                                         {"temporal[]" ",2010-12-12T12:00:00Z"})]
+        (is (= 6 (count references)))
+        (is (= #{"Granule1" "Granule2" "Granule3" "Granule4"
+                 "Granule6" "Granule7"}
+               (set (map :name references))))))
+    (testing "search by temporal_range."
+      (let [references (search/find-refs :granule
+                                         {"temporal[]" "2010-01-01T10:00:00Z,2010-01-10T12:00:00Z"})]
+        (is (= 1 (count references)))
+        (is (= #{"Granule1"}
+               (set (map :name references))))))
+    (testing "search by multiple temporal_range."
+      (let [references (search/find-refs :granule
+                                         {"temporal[]" ["2010-01-01T10:00:00Z,2010-01-10T12:00:00Z" "2010-12-22T10:00:00Z,2010-12-30T12:00:00Z"]})]
+        (is (= 4 (count references)))
+        (is (=  #{"Granule1" "Granule4" "Granule6" "Granule7"}
+               (set (map :name references))))))
+    (testing "search by multiple temporal_range, options :or."
+      (let [references (search/find-refs :granule
+                                         {"temporal[]" ["2010-01-01T10:00:00Z,2010-01-10T12:00:00Z" "2010-12-22T10:00:00Z,2010-12-30T12:00:00Z"]
+                                          "options[temporal][or]" ""})]
+        (is (= 4 (count references)))
+        (is (= #{"Granule1" "Granule4" "Granule6" "Granule7"}
+               (set (map :name references))))))
+    (testing "search by multiple temporal_range, options :and."
+      (let [references (search/find-refs :granule
+                                         {"temporal[]" ["2010-01-01T10:00:00Z,2010-01-10T12:00:00Z" "2010-12-22T10:00:00Z,2010-12-30T12:00:00Z"]
+                                          "options[temporal][and]" ""})]
+        (is (= 0 (count references)))))))
+
+;; Just some symbolic invalid temporal testing, more complete test coverage is in unit tests
+(deftest search-temporal-error-scenarios
+  (testing "search by invalid temporal format."
+    (try
+      (search/find-refs :granule {"temporal[]" "2010-12-12T12:00:00,"})
+      (catch clojure.lang.ExceptionInfo e
+        (let [status (get-in (ex-data e) [:object :status])
+              body (get-in (ex-data e) [:object :body])]
+          (is (= 422 status))
+          (is (re-find #"temporal datetime is invalid:.*" body))))))
+  (testing "search by invalid temporal start-date after end-date."
+    (try
+      (search/find-refs :granule {"temporal[]" "2011-01-01T10:00:00Z,2010-01-10T12:00:00Z"})
+      (catch clojure.lang.ExceptionInfo e
+        (let [status (get-in (ex-data e) [:object :status])
+              body (get-in (ex-data e) [:object :body])]
+          (is (= 422 status))
+          (is (re-find #"start_date \[2011-01-01T10:00:00Z\] must be before end_date \[2010-01-10T12:00:00Z\]" body)))))))
