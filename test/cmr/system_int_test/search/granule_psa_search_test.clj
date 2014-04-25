92fda715b4b594d4550809294b5f90fb11e6f208
Jason Gilman <jason@element84.com>
2014-04-25 14:25:46 -0400

    CMR-309: Added float attribute tests.

--- test/cmr/system_int_test/search/granule_psa_search_test.clj
+++ test/cmr/system_int_test/search/granule_psa_search_test.clj
@@ -21,6 +21,7 @@
        (= {:status 422 :errors [error]}
           (search/get-search-failure-data (search/find-refs :granule {"attribute[]" v})))
        ",alpha,a" (am/invalid-type-msg "")
+       "foo,alpha,a" (am/invalid-type-msg "foo")
        ",alpha,a,b" (am/invalid-type-msg "")
        "string,,a" (am/invalid-name-msg "")
        "string,,a,b" (am/invalid-name-msg "")
@@ -28,10 +29,12 @@
        "string,alpha" (am/invalid-num-parts-msg)
        "string,alpha,," (am/one-of-min-max-msg)
        "string,alpha,b,a" (am/max-must-be-greater-than-min-msg "b" "a")
-       "string,alpha,b,b" (am/max-must-be-greater-than-min-msg "b" "b"))
+       "string,alpha,b,b" (am/max-must-be-greater-than-min-msg "b" "b")
+       "float,alpha,a" (am/invalid-value-msg :float "a")
+       "float,alpha,a,0" (am/invalid-value-msg :float "a")
+       "float,alpha,0,b" (am/invalid-value-msg :float "b"))
 
-
-  (is (= {:status 422 :errors [(am/attributes-must-be-sequence-msg)]}
+    (is (= {:status 422 :errors [(am/attributes-must-be-sequence-msg)]}
          (search/get-search-failure-data
            (search/find-refs :granule {"attribute" "string,alpha,a"})))))
 
@@ -40,7 +43,6 @@
   (let [psa1 (dc/psa "alpha" :string)
         psa2 (dc/psa "bravo" :string)
         psa3 (dc/psa "charlie" :string)
-
         coll1 (d/ingest "PROV1" (dc/collection {:product-specific-attributes [psa1 psa2]}))
         gran1 (d/ingest "PROV1" (dg/granule coll1 {:product-specific-attributes [(dg/psa "alpha" ["ab" "bc"])
                                                                                  (dg/psa "bravo" ["cd" "bf"])]}))
@@ -74,6 +76,45 @@
 
            ;; only max range provided
            "string,bravo,,bc" [gran2 gran3]))))
+
+(deftest float-psas-search-test
+  (let [psa1 (dc/psa "alpha" :float)
+        psa2 (dc/psa "bravo" :float)
+        psa3 (dc/psa "charlie" :float)
+        coll1 (d/ingest "PROV1" (dc/collection {:product-specific-attributes [psa1 psa2]}))
+        gran1 (d/ingest "PROV1" (dg/granule coll1 {:product-specific-attributes [(dg/psa "alpha" [10.5 123])
+                                                                                 (dg/psa "bravo" [-12])]}))
+        gran2 (d/ingest "PROV1" (dg/granule coll1 {:product-specific-attributes [(dg/psa "bravo" [10.5 123])]}))
+
+        coll2 (d/ingest "PROV1" (dc/collection {:product-specific-attributes [psa1 psa2 psa3]}))
+        gran3 (d/ingest "PROV1" (dg/granule coll2 {:product-specific-attributes [(dg/psa "alpha" [14])
+                                                                                 (dg/psa "bravo" [13.7 123])]}))
+        gran4 (d/ingest "PROV1" (dg/granule coll2 {:product-specific-attributes [(dg/psa "charlie" [14])]}))]
+    (index/flush-elastic-index)
+
+    (testing "search by value"
+      (are [v items]
+           (d/refs-match? items (search/find-refs :granule {"attribute[]" v}))
+           "float,bravo,123" [gran2, gran3]
+           "float,alpha,10" []
+           "float,bravo,-12" [gran1]))
+
+    (testing "search by range"
+      (are [v items]
+           (d/refs-match? items (search/find-refs :granule {"attribute[]" v}))
+
+           ;; inside range
+           "float,alpha,10.2,11" [gran1]
+           ;; beginning edge of range
+           "float,alpha,10.5,10.6" [gran1]
+           ;; ending edge of range
+           "float,alpha,10,10.5" [gran1]
+
+           ;; only min range provided
+           "float,bravo,120," [gran2 gran3]
+
+           ;; only max range provided
+           "float,bravo,,13.6" [gran2]))))
 
 
 
