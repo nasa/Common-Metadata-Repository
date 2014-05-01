4aea04aaa1e37615d5d115de107a0952c918b675
James Norton <james@element84.com>
2014-05-01 10:32:13 -0400

    CMR-348: Added file to generate elastic docs from orbit calcualted spatial domains.

---
+++ src/cmr/indexer/services/concepts/orbit_calculated_spatial_domain.clj
@@ -0,0 +1,30 @@
+(ns cmr.indexer.services.concepts.orbit-calculated-spatial-domain
+  "Contains functions for converting orbit calcuated spatial domains into a elastic documents"
+  (:require [clojure.string :as s]
+            [cmr.common.services.errors :as errors]))
+
+(defn ocsd->elastic-doc
+  "Converts a OCSD into the portion going in an elastic document"
+  [ocsd]
+  (try
+  (let [{:keys [orbital-model-name
+                orbit-number
+                start-orbit-number
+                stop-orbit-number
+                equator-crossing-longitude
+                equator-crossing-date-time]} ocsd]
+    {:orbital-model-name orbital-model-name
+     :orbital-model-name.lowercase (when orbital-model-name (s/lower-case orbital-model-name))
+     :orbit-number orbit-number
+     :start-orbit-number start-orbit-number
+     :stop-orbit-number stop-orbit-number
+     :equator-crossing-longitude equator-crossing-date-time
+     :equator-crossing-date-time equator-crossing-date-time})
+  (catch Throwable e
+    (println e)
+    (.printStackTrace e))))
+
+(defn ocsds->elastic-docs
+  "Converts the ocsds into a list of elastic documents"
+  [granule]
+  (map ocsd->elastic-doc granule))
