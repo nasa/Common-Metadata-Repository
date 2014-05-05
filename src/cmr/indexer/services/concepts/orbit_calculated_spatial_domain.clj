d849d63cfa9d6bd9bc0fe719a23886ec2a0e6de1
James Norton <james@element84.com>
2014-05-04 23:02:09 -0400

    CMR-348: Fixed umm->elastic-doc for orbit calculated spatial domains.

--- src/cmr/indexer/services/concepts/orbit_calculated_spatial_domain.clj
+++ src/cmr/indexer/services/concepts/orbit_calculated_spatial_domain.clj
@@ -1,6 +1,7 @@
 (ns cmr.indexer.services.concepts.orbit-calculated-spatial-domain
   "Contains functions for converting orbit calcuated spatial domains into a elastic documents"
   (:require [clojure.string :as s]
+            [cmr.indexer.services.concepts.attribute :as attr]
             [cmr.common.services.errors :as errors]))
 
 (defn ocsd->elastic-doc
@@ -11,14 +12,15 @@
                 start-orbit-number
                 stop-orbit-number
                 equator-crossing-longitude
-                equator-crossing-date-time]} ocsd]
+                equator-crossing-date-time]} ocsd
+        ecdt (attr/value->elastic-value :datetime equator-crossing-date-time)]
     {:orbit-number orbit-number
      :start-orbit-number start-orbit-number
      :stop-orbit-number stop-orbit-number
-     :equator-crossing-longitude equator-crossing-date-time
-     :equator-crossing-date-time equator-crossing-date-time}))
+     :equator-crossing-longitude equator-crossing-longitude
+     :equator-crossing-date-time ecdt}))
 
 (defn ocsds->elastic-docs
   "Converts the ocsds into a list of elastic documents"
   [granule]
-  (map ocsd->elastic-doc granule))
+  (map ocsd->elastic-doc (:orbit-calculated-spatial-domains granule)))
