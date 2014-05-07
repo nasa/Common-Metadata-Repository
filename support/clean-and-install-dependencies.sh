02122704d31c7c1dd5571769fea00dc9df87c021
Jason Gilman <jason@element84.com>
2014-05-07 11:41:34 -0400

    Added calls to clean things to remove the leftover built jars that break the REPL.

--- support/clean-and-install-dependencies.sh
+++ support/clean-and-install-dependencies.sh
@@ -4,28 +4,28 @@
 # Must be run from within the cmr-dev-system folder
 
 cd ../cmr-common-lib
-lein do clean, install
+lein do clean, install, clean
 cd ../cmr-umm-lib
-lein do clean, install
+lein do clean, install, clean
 cd ../cmr-system-trace-lib
-lein do clean, install
+lein do clean, install, clean
 cd ../cmr-elastic-utils-lib
-lein do clean, install
+lein do clean, install, clean
 cd ../cmr-transmit-lib
-lein do clean, install
+lein do clean, install, clean
 cd ../cmr-ingest-app
-lein do clean, install
+lein do clean, install, clean
 cd ../cmr-search-app
-lein do clean, install
+lein do clean, install, clean
 cd ../cmr-indexer-app
-lein do clean, install
+lein do clean, install, clean
 cd ../cmr-index-set-app
 cp resources/config/elasticsearch_config.json.template \
 resources/config/elasticsearch_config.json
-lein do clean, install
+lein do clean, install, clean
 cd ../cmr-metadata-db-app
-lein do clean, install
+lein do clean, install, clean
 cd ../cmr-system-int-test
-lein do clean, install
+lein do clean, install, clean
 
 cd ../cmr-dev-system
--- support/stop.sh
+++ support/stop.sh
@@ -4,3 +4,4 @@
 # True is appended to ignore an error response from curl. Calling stop synchronously shutsdown the jvm so no response could be sent.
 
 curl -i -XPOST http://localhost:2999/stop; true
+lein clean
