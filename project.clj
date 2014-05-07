154808dd392fcdbe1de0d011d17681ab7a6f264d
Jason Gilman <jason@element84.com>
2014-05-07 07:27:33 -0400

    Upgrading libraries.

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
 (defproject nasa-cmr/cmr-indexer-app "0.1.0-SNAPSHOT"
   :description "This is the indexer application for the CMR. It is responsible for indexing modified data into Elasticsearch."
   :url "***REMOVED***projects/CMR/repos/cmr-indexer-app/browse"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [nasa-cmr/cmr-common-lib "0.1.1-SNAPSHOT"]
                  [nasa-cmr/cmr-umm-lib "0.1.0-SNAPSHOT"]
                  [nasa-cmr/cmr-transmit-lib "0.1.0-SNAPSHOT"]
                  [nasa-cmr/cmr-system-trace-lib "0.1.0-SNAPSHOT"]
                  [compojure "1.1.6"]
                  [ring/ring-core "1.2.2"]
-                 [ring/ring-json "0.3.0"]
-                 [org.clojure/tools.reader "0.8.3"]
+                 [ring/ring-json "0.3.1"]
+                 [org.clojure/tools.reader "0.8.4"]
                  [org.clojure/tools.cli "0.3.1"]
                  [clojurewerkz/elastisch "2.0.0-beta1" :exclusions [commons-codec]]
                  [org.clojure/data.xml "0.0.7"]
