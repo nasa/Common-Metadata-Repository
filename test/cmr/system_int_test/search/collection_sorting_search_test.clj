b0c4779e840f282f745010e20114135c7a5209ea
Jason Gilman <jason@element84.com>
2014-05-07 20:07:10 -0400

    CMR-226: Added test for searching with multiple sort keys.

--- test/cmr/system_int_test/search/collection_sorting_search_test.clj
+++ test/cmr/system_int_test/search/collection_sorting_search_test.clj
@@ -34,7 +34,7 @@
         c5 (make-coll "PROV2" "et98" 9 19)
         c6 (make-coll "PROV2" "et91" 15 25)
         c7 (make-coll "PROV2" "et79" 20 29)
-        c8 (make-coll "PROV2" "ET75" 25 36)
+        c8 (make-coll "PROV2" "ET94" 25 36)
 
         c9 (make-coll "PROV1" "et95" nil nil)
         c10 (make-coll "PROV2" "et85" nil nil)
@@ -42,22 +42,6 @@
         c12 (make-coll "PROV2" "et86" nil 22)
         all-colls [c1 c2 c3 c4 c5 c6 c7 c8 c9 c10 c11 c12]]
     (index/flush-elastic-index)
-
-    (testing "temporal start date"
-      (are [sort-key items] (d/refs-match-order?
-                              items
-                              (search/find-refs :collection {:page-size 20
-                                                             :sort-key sort-key}))
-           "start_date" [c5 c1 c11 c2 c6 c3 c7 c4 c8 c9 c10 c12]
-           "-start_date" [c8 c4 c7 c3 c6 c2 c11 c1 c5 c9 c10 c12]))
-
-    (testing "temporal end date"
-      (are [sort-key items] (d/refs-match-order?
-                              items
-                              (search/find-refs :collection {:page-size 20
-                                                             :sort-key sort-key}))
-           "end_date" [c5 c1 c12 c2 c6 c7 c3 c4 c8 c9 c10 c11]
-           "-end_date" [c8 c4 c3 c7 c6 c2 c12 c1 c5 c9 c10 c11]))
 
     (testing "default sorting"
       (is (d/refs-match-order?
@@ -79,5 +63,42 @@
                         (reverse (sort-by (comp str/lower-case :entry-title) all-colls))
                         (search/find-refs :collection {:page-size 20 :sort-key sort-key}))
            "-entry_title"
-           "-dataset_id"))))
+           "-dataset_id"))
 
+     (testing "temporal start date"
+      (are [sort-key items] (d/refs-match-order?
+                              items
+                              (search/find-refs :collection {:page-size 20
+                                                             :sort-key sort-key}))
+           "start_date" [c5 c1 c11 c2 c6 c3 c7 c4 c8 c9 c10 c12]
+           "-start_date" [c8 c4 c7 c3 c6 c2 c11 c1 c5 c9 c10 c12]))
+
+    (testing "temporal end date"
+      (are [sort-key items] (d/refs-match-order?
+                              items
+                              (search/find-refs :collection {:page-size 20
+                                                             :sort-key sort-key}))
+           "end_date" [c5 c1 c12 c2 c6 c7 c3 c4 c8 c9 c10 c11]
+           "-end_date" [c8 c4 c3 c7 c6 c2 c12 c1 c5 c9 c10 c11]))))
+
+(deftest multiple-sort-key-test
+  (let [c1 (make-coll "PROV1" "et10" 10 nil)
+        c2 (make-coll "PROV1" "et20" 10 nil)
+        c3 (make-coll "PROV1" "et30" 10 nil)
+        c4 (make-coll "PROV1" "et40" 10 nil)
+
+        c5 (make-coll "PROV2" "et10" 20 nil)
+        c6 (make-coll "PROV2" "et20" 20 nil)
+        c7 (make-coll "PROV2" "et30" 20 nil)
+        c8 (make-coll "PROV2" "et40" 20 nil)]
+    (index/flush-elastic-index)
+
+    (are [sort-key items] (d/refs-match-order?
+                            items
+                            (search/find-refs :collection {:page-size 20
+                                                           :sort-key sort-key}))
+         ["entry_title" "start_date"] [c1 c5 c2 c6 c3 c7 c4 c8]
+         ["entry_title" "-start_date"] [c5 c1 c6 c2 c7 c3 c8 c4]
+         ["start_date" "entry_title"] [c1 c2 c3 c4 c5 c6 c7 c8]
+         ["start_date" "-entry_title"] [c4 c3 c2 c1 c8 c7 c6 c5]
+         ["-start_date" "entry_title"] [c5 c6 c7 c8 c1 c2 c3 c4])))
