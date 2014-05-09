ef6002a128eae59ad062e7c796186ab7006e77ba
Yonggang Liu <ygliuvt@gmail.com>
2014-05-09 14:04:00 -0400

    Added tests for inheritanced fields for granule search.

--- test/cmr/system_int_test/search/granule_platform_instrument_sensor_search_test.clj
+++ test/cmr/system_int_test/search/granule_platform_instrument_sensor_search_test.clj
@@ -11,7 +11,8 @@
 (use-fixtures :each (ingest/reset-fixture "CMR_PROV1"))
 
 (deftest search-by-platform-short-names
-  (let [p1 (dc/platform "platform_Sn A")
+  (let [p0 (dc/platform "platform_Inherit")
+        p1 (dc/platform "platform_Sn A")
         p2 (dc/platform "platform_Sn a")
         p3 (dc/platform "platform_SnA")
         p4 (dc/platform "platform_Snx")
@@ -19,23 +20,28 @@
         pr2 (dg/platform-ref "platform_Sn a")
         pr3 (dg/platform-ref "platform_SnA")
         pr4 (dg/platform-ref "platform_Snx")
-        coll (d/ingest "CMR_PROV1" (dc/collection {:platforms [p1 p2 p3 p4]}))
-        gran1 (d/ingest "CMR_PROV1" (dg/granule coll {:platform-refs [pr1]}))
-        gran2 (d/ingest "CMR_PROV1" (dg/granule coll {:platform-refs [pr1 pr2]}))
-        gran3 (d/ingest "CMR_PROV1" (dg/granule coll {:platform-refs [pr2]}))
-        gran4 (d/ingest "CMR_PROV1" (dg/granule coll {:platform-refs [pr3]}))
-        gran5 (d/ingest "CMR_PROV1" (dg/granule coll {:platform-refs [pr4]}))
-        gran6 (d/ingest "CMR_PROV1" (dg/granule coll {}))]
+        coll1 (d/ingest "CMR_PROV1" (dc/collection {:platforms [p1 p2 p3 p4]}))
+        coll2 (d/ingest "CMR_PROV1" (dc/collection {:platforms [p0]}))
+        gran1 (d/ingest "CMR_PROV1" (dg/granule coll1 {:platform-refs [pr1]}))
+        gran2 (d/ingest "CMR_PROV1" (dg/granule coll1 {:platform-refs [pr1 pr2]}))
+        gran3 (d/ingest "CMR_PROV1" (dg/granule coll1 {:platform-refs [pr2]}))
+        gran4 (d/ingest "CMR_PROV1" (dg/granule coll1 {:platform-refs [pr3]}))
+        gran5 (d/ingest "CMR_PROV1" (dg/granule coll1 {:platform-refs [pr4]}))
+        gran6 (d/ingest "CMR_PROV1" (dg/granule coll2 {}))]
 
     (index/flush-elastic-index)
 
     (testing "search by platform, single value"
       (are [platform-sn items] (d/refs-match? items (search/find-refs :granule {:platform platform-sn}))
            "platform_Sn A" [gran1 gran2]
+           "platform_Sn a" [gran2 gran3]
            "BLAH" []))
     (testing "search by platform, multiple values"
       (is (d/refs-match? [gran1 gran2 gran4]
                          (search/find-refs :granule {"platform[]" ["platform_SnA" "platform_Sn A"]}))))
+    (testing "search by platform, inheritance"
+      (is (d/refs-match? [gran6]
+                         (search/find-refs :granule {"platform[]" ["platform_Inherit"]}))))
     (testing "search by platform, ignore case true"
       (is (d/refs-match? [gran1 gran2 gran3]
                          (search/find-refs :granule {"platform[]" ["platform_Sn A"]
@@ -62,7 +68,9 @@
                                                         "options[platform][and]" "true"}))))))
 
 (deftest search-by-instrument-short-names
-  (let [i1 (dg/instrument-ref "instrument_Sn A")
+  (let [i0 (dc/instrument "instrument_Inherit")
+        p0 (dc/platform "collection_platform" i0)
+        i1 (dg/instrument-ref "instrument_Sn A")
         i2 (dg/instrument-ref "instrument_Sn a")
         i3 (dg/instrument-ref "instrument_SnA")
         i4 (dg/instrument-ref "instrument_Snx")
@@ -71,24 +79,29 @@
         pr3 (dg/platform-ref "platform_3" i3)
         pr4 (dg/platform-ref "platform_4" i4)
         pr5 (dg/platform-ref "platform_5" i1 i2)
-        coll (d/ingest "CMR_PROV1" (dc/collection {}))
-        gran1 (d/ingest "CMR_PROV1" (dg/granule coll {:platform-refs [pr1]}))
-        gran2 (d/ingest "CMR_PROV1" (dg/granule coll {:platform-refs [pr1 pr2]}))
-        gran3 (d/ingest "CMR_PROV1" (dg/granule coll {:platform-refs [pr2]}))
-        gran4 (d/ingest "CMR_PROV1" (dg/granule coll {:platform-refs [pr3]}))
-        gran5 (d/ingest "CMR_PROV1" (dg/granule coll {:platform-refs [pr4]}))
-        gran6 (d/ingest "CMR_PROV1" (dg/granule coll {:platform-refs [pr5]}))
-        gran7 (d/ingest "CMR_PROV1" (dg/granule coll {}))]
+        coll1 (d/ingest "CMR_PROV1" (dc/collection {}))
+        coll2 (d/ingest "CMR_PROV1" (dc/collection {:platforms [p0]}))
+        gran1 (d/ingest "CMR_PROV1" (dg/granule coll1 {:platform-refs [pr1]}))
+        gran2 (d/ingest "CMR_PROV1" (dg/granule coll1 {:platform-refs [pr1 pr2]}))
+        gran3 (d/ingest "CMR_PROV1" (dg/granule coll1 {:platform-refs [pr2]}))
+        gran4 (d/ingest "CMR_PROV1" (dg/granule coll1 {:platform-refs [pr3]}))
+        gran5 (d/ingest "CMR_PROV1" (dg/granule coll1 {:platform-refs [pr4]}))
+        gran6 (d/ingest "CMR_PROV1" (dg/granule coll1 {:platform-refs [pr5]}))
+        gran7 (d/ingest "CMR_PROV1" (dg/granule coll2 {}))]
 
     (index/flush-elastic-index)
 
     (testing "search by instrument, single value"
       (are [instrument-sn items] (d/refs-match? items (search/find-refs :granule {:instrument instrument-sn}))
            "instrument_Sn A" [gran1 gran2 gran6]
+           "instrument_SnA" [gran4]
            "BLAH" []))
     (testing "search by instrument, multiple values"
       (is (d/refs-match? [gran1 gran2 gran4 gran6]
                          (search/find-refs :granule {"instrument[]" ["instrument_SnA" "instrument_Sn A"]}))))
+    (testing "search by instrument, inheritance"
+      (is (d/refs-match? [gran7]
+                         (search/find-refs :granule {"instrument[]" ["instrument_Inherit"]}))))
     (testing "search by instrument, ignore case true"
       (is (d/refs-match? [gran1 gran2 gran3 gran6]
                          (search/find-refs :granule {"instrument[]" ["instrument_Sn A"]
@@ -115,7 +128,10 @@
                                                         "options[instrument][and]" "true"}))))))
 
 (deftest search-by-sensor-short-names
-  (let [s1 (dg/sensor-ref "sensor_Sn A")
+  (let [s0 (dc/sensor "sensor_Inherit")
+        i0 (dc/instrument "collection_instrument" s0)
+        p0 (dc/platform "collection_platform" i0)
+        s1 (dg/sensor-ref "sensor_Sn A")
         s2 (dg/sensor-ref "sensor_Sn a")
         s3 (dg/sensor-ref "sensor_SnA")
         s4 (dg/sensor-ref "sensor_Snx")
@@ -130,15 +146,16 @@
         p4 (dg/platform-ref "platform_4" i4)
         p5 (dg/platform-ref "platform_5" i5)
         p6 (dg/platform-ref "platform_6" i1 i2)
-        coll (d/ingest "CMR_PROV1" (dc/collection {}))
-        gran1 (d/ingest "CMR_PROV1" (dg/granule coll {:platform-refs [p1]}))
-        gran2 (d/ingest "CMR_PROV1" (dg/granule coll {:platform-refs [p1 p2]}))
-        gran3 (d/ingest "CMR_PROV1" (dg/granule coll {:platform-refs [p2]}))
-        gran4 (d/ingest "CMR_PROV1" (dg/granule coll {:platform-refs [p3]}))
-        gran5 (d/ingest "CMR_PROV1" (dg/granule coll {:platform-refs [p4]}))
-        gran6 (d/ingest "CMR_PROV1" (dg/granule coll {:platform-refs [p5]}))
-        gran7 (d/ingest "CMR_PROV1" (dg/granule coll {:platform-refs [p6]}))
-        gran8 (d/ingest "CMR_PROV1" (dg/granule coll {}))]
+        coll1 (d/ingest "CMR_PROV1" (dc/collection {}))
+        coll2 (d/ingest "CMR_PROV1" (dc/collection {:platforms [p0]}))
+        gran1 (d/ingest "CMR_PROV1" (dg/granule coll1 {:platform-refs [p1]}))
+        gran2 (d/ingest "CMR_PROV1" (dg/granule coll1 {:platform-refs [p1 p2]}))
+        gran3 (d/ingest "CMR_PROV1" (dg/granule coll1 {:platform-refs [p2]}))
+        gran4 (d/ingest "CMR_PROV1" (dg/granule coll1 {:platform-refs [p3]}))
+        gran5 (d/ingest "CMR_PROV1" (dg/granule coll1 {:platform-refs [p4]}))
+        gran6 (d/ingest "CMR_PROV1" (dg/granule coll1 {:platform-refs [p5]}))
+        gran7 (d/ingest "CMR_PROV1" (dg/granule coll1 {:platform-refs [p6]}))
+        gran8 (d/ingest "CMR_PROV1" (dg/granule coll2 {}))]
 
     (index/flush-elastic-index)
 
@@ -149,6 +166,9 @@
     (testing "search by sensor, multiple values"
       (is (d/refs-match? [gran1 gran2 gran4 gran6 gran7]
                          (search/find-refs :granule {"sensor[]" ["sensor_SnA" "sensor_Sn A"]}))))
+    (testing "search by sensor, inheritance"
+      (is (d/refs-match? [gran8]
+                         (search/find-refs :granule {"sensor[]" ["sensor_Inherit"]}))))
     (testing "search by sensor, ignore case true"
       (is (d/refs-match? [gran1 gran2 gran3 gran6 gran7]
                          (search/find-refs :granule {"sensor[]" ["sensor_Sn A"]
