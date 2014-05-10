092e4e5a026265cb9fb7f9f1b0c226e9d1ebf19e
James Norton <james@element84.com>
2014-05-09 22:52:28 -0400

    CMR-351: Added test for equator-date-crossing parameter.

---
+++ test/cmr/system_int_test/search/granule_equator_crossing_date_search_test.clj
@@ -0,0 +1,48 @@
+(ns cmr.system-int-test.search.granule-equator-crossing-date-search-test
+  "Integration test for CMR granule temporal search"
+  (:require [clojure.test :refer :all]
+            [cmr.system-int-test.utils.ingest-util :as ingest]
+            [cmr.system-int-test.utils.search-util :as search]
+            [cmr.system-int-test.utils.index-util :as index]
+            [cmr.system-int-test.data2.collection :as dc]
+            [cmr.system-int-test.data2.granule :as dg]
+            [cmr.system-int-test.data2.core :as d]
+            [cmr.common.services.messages :as cm]))
+
+(use-fixtures :each (ingest/reset-fixture "PROV1"))
+
+(deftest invalid-equator-crossing-dates
+  (are [v]
+       (= {:status 422 :errors [(cm/invalid-date-range-msg v)]}
+          (search/find-refs :granule {:equator-crossing-date  v}))
+       "a,2011-02-01T12:00:00Z"
+       "foo,alpha,a"
+       ",alpha,a,b"))
+
+
+(deftest granule-equator-crossing-date
+  (let [coll (d/ingest "PROV1" (dc/collection {}))
+        make-gran (fn [crossing-date]
+                    (d/ingest "PROV1"
+                              (dg/granule coll {:orbit-calculated-spatial-domains [{:equator-crossing-date-time crossing-date}]})))
+        g1 (make-gran "2011-02-01T12:00:00Z")
+        g2 (make-gran "2011-02-02T12:00:00Z")
+        g3 (make-gran "2011-02-02T12:00:01Z")
+        g4 (make-gran "2011-02-02T12:00:10Z")
+        g5 (make-gran "2011-02-02T12:01:00Z")
+        g6 (make-gran "2011-02-03T12:10:00Z")
+        g7 (make-gran "2011-02-07T12:01:00Z")]
+    (index/flush-elastic-index)
+    (are [date-range items]
+         (d/refs-match? items (search/find-refs :granule {:equator-crossing-date date-range}))
+         "2011-02-01T12:00:00Z,2011-02-02T12:00:01Z" [g1 g2 g3]
+         "2011-02-03T12:00:00Z," [g6 g7]
+         ",2011-02-02T12:00:10Z" [g1 g2 g3 g4])
+    ;; catalog-rest style
+    (are [start-date end-date items]
+         (d/refs-match? items (search/find-refs :granule {:equator-crossing-start-date start-date
+                                                          :equator-crossing-end-date end-date}))
+         "2011-02-01T12:00:00Z" "2011-02-02T12:00:01Z" [g1 g2 g3]
+         "2011-02-03T12:00:00Z" nil [g6 g7]
+         nil "2011-02-02T12:00:10Z" [g1 g2 g3 g4])))
+
