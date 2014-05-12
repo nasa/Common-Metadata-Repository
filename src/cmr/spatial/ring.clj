689d1536db2b29008103a488aec2f28df4402980
Jason Gilman <jason@element84.com>
2014-05-12 07:16:30 -0400

    Ported ring namespace from prototype.

---
+++ src/cmr/spatial/ring.clj
@@ -0,0 +1,240 @@
+(ns cmr.spatial.ring
+  (:require [cmr.spatial.point :as p]
+            [cmr.spatial.math :refer :all]
+            [cmr.common.util :as util]
+            [primitive-math]
+            [cmr.spatial.mbr :as mbr]
+            [cmr.spatial.conversion :as c]
+            [cmr.spatial.arc :as a])
+  (:import cmr.spatial.arc.Arc))
+(primitive-math/use-primitive-operators)
+
+(defrecord Ring
+  [
+   ;; The points that make up the ring. Points must be in counterclockwise order. The last point
+   ;; must match the first point.
+   points
+
+   ;; The arcs of the ring
+   arcs
+
+   ;; This attribute contains the rotation direction for the ring. Rotation direction will be one of
+   ;; :clockwise, :counter_clockwise, or :none to indicate the point order direction.
+   ;; * :clockwise indicates the points are listed in a clockwise order around a center point.
+   ;; * :counter_clockwise indicates the points are listed in a counter clockwise order around a center point.
+   ;; * :none indicates the point order is around the earth like a belt.
+   ;; Depending on the order it could contain the south or north pole.
+   course-rotation-direction
+
+   ;; true if ring contains north pole
+   contains-north-pole
+
+   ;; true if ring contains south pole
+   contains-south-pole
+
+   ;; the minimum bounding rectangle
+   mbr
+
+   ;; Two points that are not within the ring. These are used to test if a point is inside or
+   ;; outside a ring. We generate multiple external points so that we have a backup if one external
+   ;; point is antipodal to a point we're checking is inside a ring.
+   external-points
+
+   ])
+
+(defn covers-point?
+  "Determines if a ring covers the given point. The algorithm works by counting the number of times
+  an arc between the point and a known external point crosses the ring. An even count means the point
+  is external. An odd count means the point is inside the ring."
+  [ring point]
+  ;; The pre check is necessary for rings which might contain both north and south poles
+  {:pre [(> (count (:external-points ring)) 0)]}
+  ;; Only do real intersection if the ring covers the point.
+  (when (mbr/covers-point? (:mbr ring) point)
+    (if (some (set (:points ring)) point)
+      true ; The point is actually one of the rings points
+      ;; otherwise we'll do the real intersection algorithm
+      (let [antipodal-point (p/antipodal point)
+            ;; Find an external point to use. We can't use an external point that is antipodal to the given point.
+            external-point (first (filter #(not= % antipodal-point) (:external-points ring)))
+            ;; Create the test arc
+            crossing-arc (a/arc point external-point)
+            ;; Find all the points the arc passes through
+            intersections (mapcat #(a/intersections % crossing-arc) (:arcs ring))
+            ;; Round the points. If the crossing arc passes through a point on the ring the
+            ;; intersection algorithm will result in two very, very close points. By rounding to
+            ;; within an acceptable range they'll be seen as the same point.
+            intersections (set (map (partial p/round-point 5) intersections))]
+        (or (odd? (count intersections))
+            ;; if the point itself is one of the intersections then the ring covers it
+            (intersections point))))))
+
+(defn intersects-ring?
+  "Returns true if the rings intersect each other."
+  [r1 r2]
+  (or
+    ;; Do any of the arcs intersect?
+    ;; TODO this should use the multiple arc intersection algorithm to avoid O(N^2) intersections
+    (some (fn [[a1 a2]]
+               (seq (a/intersections a1 a2)))
+          (for [a1 (:arcs r1) a2 (:arcs r2)] [a1 a2]))
+
+    ;; Are any of the points in ring 2 inside ring 1?
+    (some #(covers-point? r1 %) (:points r2))
+
+    ;; Are any of the points in ring 1 inside ring 2?
+    (some #(covers-point? r2 %) (:points r1))))
+
+
+(comment
+  (let [ords [ -78.4111074120776 -41.55810108186105 -78.40288285484534 -41.41987483268612
+              -78.591143372866 -41.41345428632509 -78.59968818214728 -41.55167244952579
+              -78.4111074120776 -41.55810108186105]
+        r1 (ords->ring -55.3,30 -55.3,27, -43,27, -43,30, -55.3,30)]
+    (criterium.core/with-progress-reporting
+      (criterium.core/bench
+        (intersects-ring? r1 (apply ords->ring ords)))))
+
+
+  (for [a1 (range 3) a2 (range 3)] [a1 (str a2)])
+
+)
+
+(defn- rotation-direction
+  "A helper function that determines the final rotation direction based on a set of angles in
+  degrees. It works by summing the differences between each angle. A net negative means clockwise,
+  net positive is counter clockwise, and approximatly zero means that there was no net turn in either
+  direction.
+  Returns one of three keywords, :none, :counter-clockwise, or :clockwise, to indicate net direction
+  of rotation"
+  [angles]
+  (let [angle-delta (fn [^double a1 ^double a2]
+                      (let [a2 (if (< a2 a1)
+                                 ;; Shift angle 2 so it is always greater than angle 1. This allows
+                                 ;; us to get the real radial distance between angle 2 and angle 1
+                                 (+ 360.0 a2)
+                                 a2)
+                            ;; Then when we subtract angle 1 from angle 2 we're asking "How far do
+                            ;; we have to turn to the  left to get to angle 2 from angle 1?"
+                            left-turn-amount (- a2 a1)]
+                        ;; Determine which is smaller: turning to the left or turning to the right
+                        (cond
+                          ;; In this case we can't determine whether turning to the left or the
+                          ;; right is smaller. We handle this by returning 0. Summing the angle
+                          ;; deltas in this case will == 180 or -180
+                          (== 180.0 left-turn-amount) 0
+                          ;; Turning to the right is less than turning to the left in this case.
+                          ;; Returns a negative number between 0 and -180.0
+                          (> left-turn-amount 180.0) (- left-turn-amount 360.0)
+                          :else left-turn-amount)))
+
+        ;; Calculates the amount of change between each angle.
+        ;; Positive numbers are turns to the left (counter-clockwise).
+        ;; Negative numbers are turns to the right (clockwise)
+        deltas (util/map-n angle-delta 2 1 angles)
+
+        ;; Summing the amounts of turn will give us a net turn. If it's positive then there
+        ;; is a net turn to the right. If it's negative then there's a net turn to the left.
+        ^double net (loop [m 0.0 deltas deltas]
+                      (if (empty? deltas)
+                        m
+                        (recur (+ m ^double (first deltas))
+                               (rest deltas))))]
+    (cond
+      (< (abs net) 0.01) :none
+      (> net 0.0) :counter-clockwise
+      :else :clockwise)))
+
+(defn- arcs->course-rotation-direction
+  "Calculates the rotation direction of the arcs of a ring. Will be one of :clockwise,
+  :counter_clockwise, or :none.
+
+  It works by calculating the number of degrees of turning that the ring does. It gets the initial
+  and ending course from each arc. It determines how many degrees each turn is. Turns to the left,
+  counter clockwise, are positive. Turns to the right, clockwise, are negative. This adds all of the
+  differences together to get the net bearing change while traveling around the ring. A normal
+  counter clockwise ring will be approximately 360 degrees of turn. A clockwise ring will be -360.
+  A ring around a pole will be approximately 0 net degrees turn. If a ring crosses or has a point on
+  a single pole then the sum will be -180 or 180. If a ring crosses both poles then the sum will be
+  0."
+  [arcs]
+  ;; Gets a list of the arc initial and ending courses to show all the angles that are travelled on
+  ;; throughout the ring.
+  (let [courses (loop [courses (transient []) arcs arcs]
+                  (if (empty? arcs)
+                    (persistent! courses)
+                    (let [^Arc a (first arcs)]
+                      (recur (-> courses
+                                 (conj! (.initial_course a))
+                                 (conj! (.ending_course a)))
+                             (rest arcs)))))
+        ;; Add the first turn angle on again to complete the turn
+        courses (conj courses (first courses))]
+    (rotation-direction courses)))
+
+cons
+
+(defn ring [points]
+  (let [arcs (a/points->arcs points)
+        course-rotation-direction (arcs->course-rotation-direction arcs)
+        ;; The net rotation direction of the longitudes of the ring around the earth if looking
+        ;; down on the north pole
+        lon-rotation-direction (->> points (map :lon) rotation-direction)
+
+        contains-north-pole (or (some p/is-north-pole? points)
+                                (some a/crosses-north-pole? arcs)
+                                (= :clockwise course-rotation-direction)
+                                (and (= :none course-rotation-direction)
+                                     (= :counter-clockwise lon-rotation-direction)))
+
+        contains-south-pole (or (some p/is-south-pole? points)
+                                (some a/crosses-south-pole? arcs)
+                                (= :clockwise course-rotation-direction)
+                                (and (= :none course-rotation-direction)
+                                     (= :clockwise lon-rotation-direction)))
+
+        ;; find the minimum bounding rectangle for the ring
+        br (->> arcs (mapcat a/mbrs) (reduce mbr/union))
+        br (if contains-north-pole
+             (mbr/mbr -180.0 90.0 180.0 (:south br))
+             br)
+        br (if contains-south-pole
+             (mbr/mbr -180.0 (:north br) 180.0 -90.0)
+             br)
+
+        ;; Finds two points that are not within the ring.
+        external-points (cond
+                          (and contains-north-pole contains-south-pole)
+                          ;; Cannot determine external points of a ring which contains both north and south poles
+                          []
+
+                          contains-north-pole
+                          ;; We can use the south pole and some other point outside the bounding
+                          ;; rectangle. Find a point between the southern end of the bounding
+                          ;; rectangle and -90. Simplified version of algorithm to find middle
+                          ;; between -90 and south: -90 + (south - -90)/2
+                          (let [middle-lat (- (/ ^double (:south br) 2.0) 45.0)]
+                            [(p/point 0.0 -90.0) (p/point 0 middle-lat)])
+
+                          contains-south-pole
+                          ;; We can use the north pole and some other point outside the bounding
+                          ;; rectangle. Find a point between the northern end of the bounding
+                          ;; rectangle and 90. Simplified version of algorithm to find middle
+                          ;; between 90 and north: (90 - north)/2 + north
+                          (let [middle-lat (+ (/ ^double (:north br) 2.0) 45.0)]
+                            [(p/point 0.0 90.0) (p/point 0.0 middle-lat)])
+
+                          :else
+                          ;; We know neither pole is in the ring so we'll use them
+                          [(p/point 0 -90) (p/point 0 90)])]
+    (->Ring points arcs course-rotation-direction contains-north-pole contains-south-pole
+            br external-points)))
+
+(defn ords->ring
+  "Takes all arguments as coordinates for points, lon1, lat1, lon2, lat2, and creates a ring."
+  [& ords]
+  (ring (apply p/ords->points ords)))
+
+(defn ring->ords [ring]
+  (p/points->ords (:points ring)))
+
---
+++ test/cmr/spatial/test/ring.clj
@@ -0,0 +1,186 @@
+(ns cmr.spatial.test.ring
+  (:require [clojure.test :refer :all]
+            [clojure.edn :as edn]
+            ; [clojure.test.check.clojure-test :refer [defspec]]
+            ;; Temporarily included to use the fixed defspec. Remove once issue is fixed.
+            [cmr.common.test.test-check-ext :refer [defspec]]
+
+            [clojure.test.check.properties :refer [for-all]]
+            [clojure.test.check.generators :as gen]
+
+            ;; my code
+            [cmr.spatial.math :refer :all]
+            [cmr.spatial.point :as p]
+            [cmr.spatial.mbr :as m]
+            [cmr.spatial.ring :as r]
+            [cmr.spatial.test.generators :as sgen]))
+
+;; TODO Need to finish this spec
+#_(defspec ring-covers-point-spec num-tests
+
+    ;; properties to verify
+    ; The MBR covers all the ring points
+    ; The ring covers all the points of the ring.
+    ; The ring does not cover the external points.
+    ; Midpoint of each arc of the ring is covered
+    ; does or does not cover poles depending on the contains variable
+
+    ;; other points inside the ring?
+    ;; One way would be to take cross section arcs from one side of the mbr to another.
+    ;; Find intersections of the arc and the ring.
+    ;; All cross section arcs should intersect the ring 0 or an even number of times.
+    ;; If an arc intersects at all then the midpoint of the intersections should be inside the ring.
+
+  )
+
+
+(declare ring-examples)
+
+(deftest ring-examples-test
+  (doseq [[ring-name
+           {:keys [ords bounds internal-points external-points north-pole south-pole]}]
+          ring-examples
+          :let [north-pole (if (nil? north-pole) false north-pole)
+                south-pole (if (nil? south-pole) false south-pole)]]
+    (testing (str "Ring Example " ring-name)
+      (let [ring (apply r/ords->ring ords)]
+        (is (= bounds (:mbr ring)))
+
+        (is (= north-pole (:contains-north-pole ring)))
+        (is (= south-pole (:contains-south-pole ring)))
+
+        (doseq [point (apply p/ords->points internal-points)]
+            (is (r/covers-point? ring point) (str "Ring should cover point " point)))
+
+        (doseq [point (apply p/ords->points external-points)]
+            (is (not (r/covers-point? ring point)) (str "Ring should not cover point " point)))))))
+
+
+(def ring-examples
+  {
+   :touching-eq {:ords [0,0 4,0 6,5 2,5 0,0]
+                 :bounds (m/mbr 0 5.0030322578503075 6 0)
+                 :internal-points [3,0.001 0.36,0.19 2.23,4.84 2,2]
+                 :external-points [4.4,0.34 2.03,-0.27 0.37,1.83 3.55,5.05]}
+
+   :convex {:ords [0,0 4,0 4,1 2,1 2,4 4,4 4,5 0,5 0,0]
+            :bounds (m/mbr 0 5.0030322578503075 4 0)
+            :internal-points [2.98,0.43 3.01,4.47]
+            :external-points [4,2 4,3 3,2]}
+
+   :around-np {:ords [8.09,84.55 175,80 -67.93,87.41 8.09,84.55]
+               :north-pole true
+               :bounds (m/mbr -180 90 180 80)
+               :internal-points [173.94,84.33 -178.33,84.36 0,87.47]
+               :external-points [0,80 92.58,88.97 174.58,78.85]}
+
+   :around-sp {:ords [0,-85 -99.02,-85.42 179.01,-85.86 75,-85 0,-85]
+               :south-pole true
+               :bounds (m/mbr -180 -85 180 -90)
+               :internal-points [-97.67,-86.36 162.06,-87.37]
+               :external-points [-116.73,-86.24 19.62,-85.61]}
+
+   :point-on-np {:ords [0,0 90,0 0,90 0,0]
+                 :north-pole true
+                 :bounds (m/mbr -180 90 180 0)
+                 :internal-points [0,90 45,85 1,85 89.9,85 45,1 1,1 90,1]
+                 :external-points [0,-90 -1,0 45,-85]}
+
+   :point-on-np2 {:ords [-90,85 0,85 90,85 0,90 -90,85]
+                  :north-pole true
+                  :bounds (m/mbr -180 90 180 85)
+                  :internal-points [0,87 89,87 -89,87]
+                  :external-points [0,-90 180,0 0,0 -180,89 -135,89 135,89 180,89 89,-87 -89,-87]}
+
+   :point-on-np-across-am {:ords [90,85 180,85 -90,85 0,90 90,85]
+                           :north-pole true
+                           :bounds (m/mbr -180 90 180 85)
+                           :internal-points [-180,89 -135,89 135,89 180,89]
+                           :external-points [0,-90 180,0 0,0 0,87 89,87 -89,87 89,-87 -89,-87]}
+
+   :point-near-np {:ords [0,0 89.9,0 0,89.9 0,0]
+                   :bounds (m/mbr 0 89.9 89.9 0)
+                   :internal-points [0,89.9 45,85 1,85 86.95,85.01 45,1 1,1 89.85,1]
+                   :external-points [0,90 0,-90 -1,0 45,-85]}
+
+   :arc-across-np {:ords [-90,75, 0,60, 90,-74, -90,75]
+                   :north-pole true
+                   :bounds (m/mbr -180 90 180 -74)
+
+                   :internal-points [0,90 -87,76 88,84 87,75 87,-72]
+                   :external-points [0,-90 0,0 91,-72]}
+
+   :point-on-sp {:ords [-175,0 175,0 0,-90 -175,0]
+                 :south-pole true
+                 :bounds (m/mbr -180 0 180 -90)
+                 :internal-points [180,0 -180,0 180,-1 175,-10 -175,-10 175,-89 -175,-89]
+                 :external-points [0,0 0,90 174,-10 -174,-10 174,-89 -174,-89]}
+
+   :point-on-sp2 {:ords [-90,-85 0,-90 90,-85 0,-85 -90,-85]
+                  :south-pole true
+                  :bounds (m/mbr -180 -85 180 -90)
+                  :internal-points [0,-87 89,-87 -89,-87]
+                  :external-points [0,90 180,0 0,0 -180,-89 -135,-89 135,-89 180,-89 89,87 -89,87]}
+
+   :point-near-sp {:ords [-175,0 175,0 180,-89 -175,0]
+                   :bounds (m/mbr 175 0 -175 -89)
+                   :internal-points [180,0 -180,0 180,-1 176,-10 -176,-10 179.99,-88.9 -179.99,-88.9]
+                   :external-points [0,0 0,90 174,-10 -174,-10 174,-89 -174,-89]}
+
+   :arc-across-sp {:ords [-90,75 90,-76 0,60 -90,75]
+                   :south-pole true
+                   :bounds (m/mbr -180 76.33917886681955 180 -90)
+                   :internal-points [0,0 -81,75 0,-89 -89,-89 90,-76.1]
+                   :external-points [0,90 180,0 -180,0 81,75 180,-89 90,-75.9]}
+
+   :giant-zig-zag {:ords [-43.37,-45.69 27.42,12.19 -53.07,-52.64 44.94,30.89 -46.34,-44.43
+                          26.09,12.69 -43.37,-45.69]
+                   :bounds (m/mbr -53.07 30.89 44.94 -52.64)
+                   :internal-points [8.9,-5.62 10.44,-6.52 -40.49,-42.55 13.05,-7.91]
+                   :external-points [0,-90 0,0 9.57,-6.31 11.57,-7.19 -40.61,-43.93 -43.64,-45.96]}
+
+   :on-eq {:ords [0,0 90,0 180,0 -90,0 0,0]
+           :north-pole true
+           :bounds (m/mbr -180 90 180 0)
+           :internal-points [0,1]
+           :external-points [0,-1]}
+
+   :wrapping-around-np {:ords [-69.11,89.76 -60.39,89.66 54.98,89.53 127.49,89.34 -128.78,89.04
+                               -66.7,89.02 52.17,88.82 127.05,88.52 -132.94,88.15 -62.27,87.98
+                               38.97,87.99 115.94,88.08 120.61,88.27 47.01,88.36 -66.91,88.47
+                               -131.53,88.54 128.99,88.98 55.21,89.16 -70.93,89.33 -123.07,89.37
+                               136.34,89.67 58.88,89.75 -69.11,89.76]
+                        :bounds (m/mbr -180 89.89262938556303 180 87.98)
+                        :internal-points [-26.46,89.83 -18.28,88.81 127.85,88.73]
+                        :external-points [-115.16,89.47 -95.54,88.95 -11.63,88.6 0,0 -180,0 90,0 -90,0]}
+
+   :across-am {:ords [178.31,1.01 178.16,-3.85 -177.21,-4.19 -176.79,1.32 178.31,1.01]
+               :bounds (m/mbr 178.16 1.32 -176.79 -4.19)
+               :internal-points [-180,0 180,0 -178.46,-3.2 -180,-3.2]
+               :external-points [180,2 -180,2 -90,0 90,0 0,0 179.48,-4.41]}
+
+   :ends-on-am1 {:ords [172,0 174,-5 -180,-5 180,0.75 172,0]
+                 :bounds (m/mbr 172 0.75 180 -5.006826875103258)
+                 :internal-points [179.48,-4.41 180,-4 -180,-4]
+                 :external-points [180,-7 180,7]}
+
+   :ends-on-am2 {:ords [172,0 174,-5 -180,-5 -180,0.75 172,0]
+                 :bounds (m/mbr 172 0.75 180 -5.006826875103258)
+                 :internal-points [179.48,-4.41 180,-4 -180,-4]
+                 :external-points [180,-7 180,7]}
+
+   :ends-on-am3 {:ords [172,0 174,-5 180,-5 180,0.75 172,0]
+                 :bounds (m/mbr 172 0.75 180 -5.006826875103258)
+                 :internal-points [179.48,-4.41 180,-4 -180,-4]
+                 :external-points [180,-7 180,7]}
+
+   :starts-on-am1 {:ords [-176.17,0.46 180,0.75 -180,-5 -177.38,-3.92 -176.17,0.46]
+                   :bounds (m/mbr -180 0.75 -176.17 -5)}
+
+   :starts-on-am2 {:ords [-176.17,0.46, -180,0.75, -180,-5, -177.38,-3.92, -176.17,0.46]
+                   :bounds (m/mbr -180 0.75 -176.17 -5)}
+
+   :starts-on-am3 {:ords [-176.17,0.46 180,0.75 180,-5 -177.38,-3.92 -176.17,0.46]
+                   :bounds (m/mbr -180 0.75 -176.17 -5)}})
+
+
