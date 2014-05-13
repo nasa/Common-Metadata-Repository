36f057a9fd7573ec938ee1ebf682d929418a7d37
James Norton <james@element84.com>
2014-05-13 10:12:47 -0400

    CMR-493: Added code to simplify high level queries by converting them into their constituent components before the query->elastic phases to support query optimization.

---
+++ src/cmr/search/data/complex_to_simple.clj
@@ -0,0 +1,31 @@
+(ns cmr.search.data.complex-to-simple
+  (:require [cmr.search.models.query :as qm]))
+
+(defprotocol ComplexQueryToSimple
+  "Defines a function to convert a complex / high level query/condition into simpler ones that
+  can be optimized more easily."
+  (simplify-query
+    [query]
+    "Converts a high-level query condition into a simpler form."))
+
+
+(extend-protocol ComplexQueryToSimple
+  cmr.search.models.query.Query
+  (simplify-query
+    [query]
+    (println "SIMPLIFY")
+    (update-in query [:condition] #(simplify-query %)))
+
+  cmr.search.models.query.ConditionGroup
+  (simplify-query
+    [condition]
+    (update-in condition [:conditions] (fn [cond]
+                                         (map #(simplify-query %) cond))))
+
+
+  ;; catch all
+  java.lang.Object
+  (simplify-query
+    [this]
+    ;; do nothing
+    this))
