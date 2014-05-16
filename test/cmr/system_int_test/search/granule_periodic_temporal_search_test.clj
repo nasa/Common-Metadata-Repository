87a0e25de783503f2195e1f77e1e9c3b81513b72
Prem Gazula <prem.g8@gmail.com>
2014-05-16 12:59:44 -0400

    Made changes to periodic temporal tests per feedback

--- test/cmr/system_int_test/search/granule_periodic_temporal_search_test.clj
+++ test/cmr/system_int_test/search/granule_periodic_temporal_search_test.clj
@@ -13,59 +13,59 @@
 (deftest search-by-periodic-temporal
   (let [coll1 (d/ingest "CMR_PROV1" (dc/collection {}))
         gran1 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule1"
-                                                    :beginning-date-time "2000-01-01T12:00:00Z"
-                                                    :ending-date-time "2000-02-14T12:00:00Z"}))
+                                                       :beginning-date-time "2000-01-01T12:00:00Z"
+                                                       :ending-date-time "2000-02-14T12:00:00Z"}))
         gran2 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule2"
-                                                    :beginning-date-time "2000-02-14T12:00:00Z"
-                                                    :ending-date-time "2000-02-15T12:00:00Z"}))
+                                                       :beginning-date-time "2000-02-14T12:00:00Z"
+                                                       :ending-date-time "2000-02-15T12:00:00Z"}))
         gran3 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule3"
-                                                    :beginning-date-time "2000-03-15T12:00:00Z"
-                                                    :ending-date-time "2000-04-15T12:00:00Z"}))
+                                                       :beginning-date-time "2000-03-15T12:00:00Z"
+                                                       :ending-date-time "2000-04-15T12:00:00Z"}))
         gran4 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule4"
-                                                    :beginning-date-time "2000-04-01T12:00:00Z"
-                                                    :ending-date-time "2000-04-15T12:00:00Z"}))
+                                                       :beginning-date-time "2000-04-01T12:00:00Z"
+                                                       :ending-date-time "2000-04-15T12:00:00Z"}))
         gran5 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule5"
-                                                    :beginning-date-time "2001-01-01T12:00:00Z"
-                                                    :ending-date-time "2001-01-31T12:00:00Z"}))
+                                                       :beginning-date-time "2001-01-01T12:00:00Z"
+                                                       :ending-date-time "2001-01-31T12:00:00Z"}))
         gran6 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule6"
-                                                    :beginning-date-time "2001-01-01T12:00:00Z"
-                                                    :ending-date-time "2001-02-14T12:00:00Z"}))
+                                                       :beginning-date-time "2001-01-01T12:00:00Z"
+                                                       :ending-date-time "2001-02-14T12:00:00Z"}))
         gran7 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule7"
-                                                    :beginning-date-time "2001-03-15T12:00:00Z"
-                                                    :ending-date-time "2001-04-15T12:00:00Z"}))
+                                                       :beginning-date-time "2001-03-15T12:00:00Z"
+                                                       :ending-date-time "2001-04-15T12:00:00Z"}))
         gran8 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule8"
-                                                    :beginning-date-time "2001-04-01T12:00:00Z"
-                                                    :ending-date-time "2001-04-15T12:00:00Z"}))
+                                                       :beginning-date-time "2001-04-01T12:00:00Z"
+                                                       :ending-date-time "2001-04-15T12:00:00Z"}))
         gran9 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule9"
-                                                    :beginning-date-time "2002-01-01T12:00:00Z"
-                                                    :ending-date-time "2002-01-31T12:00:00Z"}))
+                                                       :beginning-date-time "2002-01-01T12:00:00Z"
+                                                       :ending-date-time "2002-01-31T12:00:00Z"}))
         gran10 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule10"
-                                                     :beginning-date-time "2002-01-01T12:00:00Z"
-                                                     :ending-date-time "2002-02-14T12:00:00Z"}))
+                                                        :beginning-date-time "2002-01-01T12:00:00Z"
+                                                        :ending-date-time "2002-02-14T12:00:00Z"}))
         gran11 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule11"
-                                                     :beginning-date-time "2002-03-14T12:00:00Z"
-                                                     :ending-date-time "2002-04-15T12:00:00Z"}))
+                                                        :beginning-date-time "2002-03-14T12:00:00Z"
+                                                        :ending-date-time "2002-04-15T12:00:00Z"}))
         gran12 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule12"
-                                                     :beginning-date-time "2002-03-15T12:00:00Z"
-                                                     :ending-date-time "2002-04-15T12:00:00Z"}))
+                                                        :beginning-date-time "2002-03-15T12:00:00Z"
+                                                        :ending-date-time "2002-04-15T12:00:00Z"}))
         gran13 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule13"
-                                                     :beginning-date-time "2002-04-01T12:00:00Z"
-                                                     :ending-date-time "2002-04-15T12:00:00Z"}))
+                                                        :beginning-date-time "2002-04-01T12:00:00Z"
+                                                        :ending-date-time "2002-04-15T12:00:00Z"}))
         gran14 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule14"
-                                                     :beginning-date-time "1999-02-15T12:00:00Z"
-                                                     :ending-date-time "1999-03-15T12:00:00Z"}))
+                                                        :beginning-date-time "1999-02-15T12:00:00Z"
+                                                        :ending-date-time "1999-03-15T12:00:00Z"}))
         gran15 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule15"
-                                                     :beginning-date-time "2003-02-15T12:00:00Z"
-                                                     :ending-date-time "2003-03-15T12:00:00Z"}))
+                                                        :beginning-date-time "2003-02-15T12:00:00Z"
+                                                        :ending-date-time "2003-03-15T12:00:00Z"}))
         gran16 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule16"
-                                                     :beginning-date-time "1999-02-15T12:00:00Z"}))
+                                                        :beginning-date-time "1999-02-15T12:00:00Z"}))
         gran17 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule17"
-                                                     :beginning-date-time "2001-02-15T12:00:00Z"}))
+                                                        :beginning-date-time "2001-02-15T12:00:00Z"}))
         gran18 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule18"
-                                                     :beginning-date-time "2002-03-15T12:00:00Z"}))
+                                                        :beginning-date-time "2002-03-15T12:00:00Z"}))
         gran19 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule19"
-                                                     :beginning-date-time "2001-11-15T12:00:00Z"
-                                                     :ending-date-time "2001-12-15T12:00:00Z"}))
+                                                        :beginning-date-time "2001-11-15T12:00:00Z"
+                                                        :ending-date-time "2001-12-15T12:00:00Z"}))
         gran20 (d/ingest "CMR_PROV1" (dg/granule coll1 {:granule-ur "Granule20"}))]
     (index/flush-elastic-index)
 
@@ -74,18 +74,33 @@
                                          {"temporal[]" "2000-02-15T00:00:00Z, 2002-03-15T00:00:00Z, 32, 90"
                                           :page_size 100})]
         (is (d/refs-match? [gran2 gran3 gran6 gran7 gran10 gran11 gran16 gran17] references))))
+    (testing "search by both start-day and end-day - testing singular temporal."
+      (let [extent "2000-02-15T00:00:00Z, 2002-03-15T00:00:00Z, 32, 90"
+            references (search/find-refs :granule {"temporal[]" extent :page_size 100})
+            references1 (search/find-refs :granule {"temporal" extent :page_size 100})]
+        (is (= references references1))))
     (testing "search by end-day."
       (let [references (search/find-refs :granule
                                          {"temporal[]" "2000-02-15T00:00:00Z, 2002-03-15T00:00:00Z, , 90"
                                           :page_size 100})]
         (is (d/refs-match?
               [gran2 gran3 gran5 gran6 gran7 gran9 gran10 gran11 gran16 gran17] references))))
+    (testing "search by end-day - testing singular temporal."
+      (let [extent "2000-02-15T00:00:00Z, 2002-03-15T00:00:00Z, , 90"
+            references (search/find-refs :granule {"temporal[]" extent :page_size 100})
+            references1 (search/find-refs :granule {"temporal" extent :page_size 100})]
+        (is (= references references1))))
     (testing "search by start-day."
       (let [references (search/find-refs :granule
                                          {"temporal[]" "2000-02-15T00:00:00Z, 2002-03-15T00:00:00Z, 32,"
                                           :page_size 100})]
         (is (d/refs-match?
               [gran2 gran3 gran4 gran6 gran7 gran8 gran10 gran11 gran16 gran17 gran19] references))))
+    (testing "search by start-day - testing singular temporal."
+      (let [extent "2000-02-15T00:00:00Z, 2002-03-15T00:00:00Z, 32,"
+            references (search/find-refs :granule {"temporal[]" extent :page_size 100})
+            references1 (search/find-refs :granule {"temporal" extent :page_size 100})]
+        (is (= references references1))))
     (testing "search by start-day without end_date."
       (let [references (search/find-refs :granule
                                          {"temporal[]" ["2000-02-15T00:00:00Z, , 32"]
@@ -93,18 +108,35 @@
         (is (d/refs-match?
               [gran2 gran3 gran4 gran6 gran7 gran8 gran10 gran11 gran12 gran13 gran15 gran16 gran17 gran18 gran19]
               references))))
+    (testing "search by start-day without end_date - testing singular temporal."
+      (let [extent "2000-02-15T00:00:00Z, , 32"
+            references (search/find-refs :granule {"temporal[]" [extent] :page_size 100})
+            references1 (search/find-refs :granule {"temporal" extent :page_size 100})]
+        (is (= references references1))))
     (testing "search by start-day/end-day with date crossing year boundary."
       (let [references (search/find-refs :granule
                                          {"temporal[]" ["2000-04-03T00:00:00Z, 2002-01-02T00:00:00Z, 93, 2"]
                                           :page_size 100})]
         (is (d/refs-match?
               [gran3 gran4 gran5 gran6 gran7 gran8 gran9 gran10 gran16 gran17 gran19] references))))
+    (testing "search by start-day/end-day with date crossing year boundary - testing singular temporal."
+      (let [extent "2000-04-03T00:00:00Z, 2002-01-02T00:00:00Z, 93, 2"
+            references (search/find-refs :granule {"temporal[]" [extent] :page_size 100})
+            references1 (search/find-refs :granule {"temporal" extent :page_size 100})]
+        (is (= references references1))))
     (testing "search by multiple temporal."
       (let [references (search/find-refs :granule
                                          {"temporal[]" ["1998-01-15T00:00:00Z, 1999-03-15T00:00:00Z, 60, 90"
                                                         "2000-02-15T00:00:00Z, 2001-03-15T00:00:00Z, 40, 50"]
                                           :page_size 100})]
-        (is (d/refs-match? [gran2 gran6 gran14 gran16 gran17] references))))))
+        (is (d/refs-match? [gran2 gran6 gran14 gran16 gran17] references))))
+    (testing "search by multiple temporal - testing singular temporal."
+      (let [extent1 "1998-01-15T00:00:00Z, 1999-03-15T00:00:00Z, 60, 90"
+            extent2 "2000-02-15T00:00:00Z, 2001-03-15T00:00:00Z, 40, 50"
+            references (search/find-refs :granule {"temporal[]" [extent1 extent2] :page_size 100})
+            references1 (search/find-refs :granule {"temporal" [extent1 extent2] :page_size 100})]
+        (is (= references references1))))))
+
 
 ;; Just some symbolic invalid temporal testing, more complete test coverage is in unit tests
 (deftest search-temporal-error-scenarios
