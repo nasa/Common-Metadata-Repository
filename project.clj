8ec4914e395d0c9af2382bc6b9623c08725d3917
Jason Gilman <jason@element84.com>
2014-05-22 13:08:09 -0400

    Adding dependencies for oracle stuff.

--- project.clj
+++ project.clj
@@ -1,9 +1,19 @@
 (defproject nasa-cmr/cmr-oracle-lib "0.1.0-SNAPSHOT"
   :description "Contains utilities for connecting to and manipulating data in Oracle."
   :url "***REMOVED***projects/CMR/repos/cmr-oracle-lib/browse"
+
+  ;; Need the maven repo for Oracle jars that aren't available in public maven repos.
+  :repositories [["releases" "http://devrepo1.dev.echo.nasa.gov/data/dist/projects/echo/mavenrepo/"]]
+
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [nasa-cmr/cmr-common-lib "0.1.1-SNAPSHOT"]]
+                 [nasa-cmr/cmr-common-lib "0.1.1-SNAPSHOT"]
+                 [org.clojure/java.jdbc "0.3.3"]
+                 [com.oracle/ojdbc6 "11.2.0.3"]
+                 [com.mchange/c3p0 "0.9.2.1"]
+                 [sqlingvo "0.5.17"]]
+
   :plugins [[lein-test-out "0.3.1"]]
+
   :profiles
   {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]
                         [org.clojars.gjahad/debug-repl "0.3.3"]]
