3c70aabfa5b385532951e3bde76480ffaa7a8dc3
James Norton <james@element84.com>
2014-05-22 12:58:52 -0400

    Added connection namespace.

---
+++ src/cmr/oracle/connection.clj
@@ -0,0 +1,54 @@
+(ns cmr.oracle.connection
+ "Contains a record definition that implements the ConcpetStore and Lifecycle protocols
+  backed by an Oracle database."
+  (:require [cmr.common.lifecycle :as lifecycle]
+            [cmr.common.log :refer (debug info warn error)])
+  (:import com.mchange.v2.c3p0.ComboPooledDataSource))
+
+;;; Constants
+
+(def db-username (get (System/getenv) "MDB_DB_USERNAME" "METADATA_DB"))
+(def db-password (get (System/getenv) "MDB_DB_PASSWORD" "METADATA_DB"))
+(def db-host (get (System/getenv) "MDB_DB_HOST" "localhost"))
+(def db-port (get (System/getenv) "MDB_DB_PORT" "1521"))
+(def db-sid (get (System/getenv) "MDB_DB_SID" "orcl"))
+
+(def db-spec
+  {:classname "oracle.jdbc.driver.OracleDriver"
+   :subprotocol "oracle"
+   :subname (format "thin:@%s:%s:%s" db-host db-port db-sid)
+   :user db-username
+   :password db-password})
+
+
+(defrecord OracleStore [db]
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  lifecycle/Lifecycle
+
+  (start [this system]
+         this)
+
+  (stop [this system]
+        this))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn pool
+  [spec]
+  (let [cpds (doto (ComboPooledDataSource.)
+               (.setDriverClass (:classname spec))
+               (.setJdbcUrl (str "jdbc:" (:subprotocol spec) ":" (:subname spec)))
+               (.setUser (:user spec))
+               (.setPassword (:password spec))
+               ;; expire excess connections after 30 minutes of inactivity:
+               (.setMaxIdleTimeExcessConnections (* 30 60))
+               ;; expire connections after 3 hours of inactivity:
+               (.setMaxIdleTime (* 3 60 60)))]
+    {:datasource cpds}))
+
+(defn create-db
+  "Creates and returns the database connection pool."
+  [db-spec]
+  (map->OracleStore (pool db-spec)))
