cc0a3346235fd2c54920f297d9570f50f2be67d3
James Norton <james@element84.com>
2014-05-22 12:51:20 -0400

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cmr-oracle-lib
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/cmr_oracle_lib/core_test.clj
@@ -0,0 +1,7 @@
+(ns cmr-oracle-lib.core-test
+  (:require [clojure.test :refer :all]
+            [cmr-oracle-lib.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
