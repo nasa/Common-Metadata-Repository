b44a8da78b1ea3b03f494323c0863737e22a1baf
Jason Gilman <jason@element84.com>
2014-05-30 07:30:42 -0400

    Removing library that was listed twice in clean and install dependencies script.

--- support/clean-and-install-dependencies.sh
+++ support/clean-and-install-dependencies.sh
@@ -9,6 +9,8 @@ lein do clean, install, clean
 
 cd ../cmr-vdd-spatial-viz
 lein do clean, install, clean
+# Run as a separate step since it fails in CI but works locally. It's not necessary for CI.
+lein compile-coffeescript
 
 cd ../cmr-spatial-lib
 lein do clean, install, clean
@@ -30,11 +32,6 @@ lein do clean, install, clean
 
 cd ../cmr-es-spatial-plugin
 lein do clean, install, clean
-
-cd ../cmr-vdd-spatial-viz
-lein do clean, install, clean
-# Run as a separate step since it fails in CI but works locally. It's not necessary for CI.
-lein compile-coffeescript
 
 cd ../cmr-ingest-app
 lein do clean, install, clean
