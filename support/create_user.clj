63fb88806ec84d6b1ded36e5ed721a6a3b037d59
James Norton <james@element84.com>
2014-06-03 13:25:07 -0400

    Added call to grant select priviliges during user creation.

--- support/create_user.clj
+++ support/create_user.clj
@@ -4,8 +4,14 @@
 (deps '[[nasa-cmr/cmr-oracle-lib "0.1.0-SNAPSHOT"]]
       :repositories {"releases" "http://devrepo1.dev.echo.nasa.gov/data/dist/projects/echo/mavenrepo/"})
 
-(require '[cmr.oracle.user :as o])
+(require '[cmr.oracle.user :as o]
+         '[cmr.common.config :as config])
 
 ;; TODO change the password for different systems
 
-(o/create-user (o/sys-dba-conn) "METADATA_DB" "METADATA_DB")
+(let [db (o/sys-dba-conn)
+      tablespace ((config/config-value-fn :catalog-rest-tablespace "DEV_52_CATALOG_REST"))
+      from-user ((config/config-value-fn :catalog-rest-user "DEV_52_CATALOG_REST"))
+      to_user ((config/config-value-fn :metadata-db-user "METADATA_DB"))]
+	(o/create-user db "METADATA_DB" "METADATA_DB")
+	(o/grant-select-priviliges db tablespace from-user to_user))
