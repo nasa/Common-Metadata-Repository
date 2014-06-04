353ffe18e38cec144d794447e930331b0fa2a6e5
Jason Gilman <jason@element84.com>
2014-06-04 16:17:55 -0400

    Updating the name of a config env variable.

--- support/build-and-run.sh
+++ support/build-and-run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a script for building and running the dev system for continuous integration.
 # It will build and run it in the background.
-# CMR_SEPARATE_COLL_INDEX is the environment variable used by indexer to set up granule indexes
+# CMR_COLLS_WITH_SEPARATE_INDEXES is the environment variable used by indexer to set up granule indexes
 # for large collections. It is set to facilitate granule_search_index_name_test in system-int-test.
 
 lein clean
 lein uberjar
-CMR_SEPARATE_COLL_INDEX='C1-SEP_PROV1,C2-SEP_PROV1' nohup java -XX:MaxPermSize=256m -jar target/cmr-dev-system-0.1.0-SNAPSHOT-standalone.jar&
+CMR_COLLS_WITH_SEPARATE_INDEXES='C1-SEP_PROV1,C2-SEP_PROV1' nohup java -XX:MaxPermSize=256m -jar target/cmr-dev-system-0.1.0-SNAPSHOT-standalone.jar&
