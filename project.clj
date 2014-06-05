5a3ad852890e2fc4a7d7358bad9e90ba508c9de4
Jason Gilman <jason@element84.com>
2014-06-05 11:52:48 -0400

    Created elastic configuration namespace.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [nasa-cmr/cmr-common-lib "0.1.1-SNAPSHOT"]
                  [org.elasticsearch/elasticsearch "1.1.1" :exclusions [commons-codec]]
-                 [clojurewerkz/elastisch "2.0.0-rc1"]
+                 [clojurewerkz/elastisch "2.0.0-rc1" :exclusions [commons-codec]]
 
                  ;; Log4j needed to configure logging in elasticsearch.
                  ;; Version set to match elastic search numbers. Look in elasticsearch pom.xml
