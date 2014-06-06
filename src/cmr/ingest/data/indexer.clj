75f7b8436872788a0211766d83802d1153242c77
Jason Gilman <jason@element84.com>
2014-06-06 08:52:32 -0400

    CMR-149: Updated to use persistent http connections.

--- src/cmr/ingest/data/indexer.clj
+++ src/cmr/ingest/data/indexer.clj
@@ -6,23 +6,21 @@
             [cmr.common.services.errors :as errors]
             [cmr.system-trace.core :refer [deftracefn]]
             [cmr.system-trace.http :as ch]
-            [cmr.transmit.config :as transmit-config]))
-
-(defn- context->indexer-url
-  [context]
-  (let [{:keys [host port]} (transmit-config/context->app-connection context :indexer)]
-    (format "http://%s:%s" host port)))
+            [cmr.transmit.config :as transmit-config]
+            [cmr.transmit.connection :as transmit-conn]))
 
 (deftracefn index-concept
   "Forward newly created concept for indexer app consumption."
   [context concept-id revision-id]
-  (let [indexer-url (context->indexer-url context)
+  (let [conn (transmit-config/context->app-connection context :indexer)
+        indexer-url (transmit-conn/root-url conn)
         concept-attribs {:concept-id concept-id, :revision-id revision-id}
         response (client/post indexer-url {:body (cheshire/generate-string concept-attribs)
                                            :content-type :json
                                            :throw-exceptions false
                                            :accept :json
-                                           :headers (ch/context->http-headers context)})
+                                           :headers (ch/context->http-headers context)
+                                           :connection-manager (transmit-conn/conn-mgr conn)})
         status (:status response)]
     (when-not (= 201 status)
       (errors/internal-error! (str "Operation to index a concept failed. Indexer app response status code: "  status  " " response)))))
@@ -30,11 +28,13 @@
 (deftracefn delete-concept-from-index
   "Delete a concept with given revision-id from index."
   [context concept-id revision-id]
-  (let [indexer-url (context->indexer-url context)
+  (let [conn (transmit-config/context->app-connection context :indexer)
+        indexer-url (transmit-conn/root-url conn)
         response (client/delete (format "%s/%s/%s" indexer-url concept-id revision-id)
                                 {:accept :json
                                  :throw-exceptions false
-                                 :headers (ch/context->http-headers context)})
+                                 :headers (ch/context->http-headers context)
+                                 :connection-manager (transmit-conn/conn-mgr conn)})
         status (:status response)]
     (when-not (some #{200, 201} [status])
       (errors/internal-error! (str "Delete concept operation failed. Indexer app response status code: "  status " " response)))))
