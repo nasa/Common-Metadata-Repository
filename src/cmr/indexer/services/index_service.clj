c48ce5c54f06c5c81c2426b8f864cc487fe2cd2f
James Norton <james@element84.com>
2014-06-06 10:57:52 -0400

    CMR-512: Added bulk indexing.

--- src/cmr/indexer/services/index_service.clj
+++ src/cmr/indexer/services/index_service.clj
@@ -5,12 +5,15 @@
             [cmr.common.log :as log :refer (debug info warn error)]
             [cmr.common.concepts :as cs]
             [cmr.transmit.metadata-db :as meta-db]
+            [clojurewerkz.elastisch.rest.bulk :as bulk]
             [cmr.indexer.data.elasticsearch :as es]
             [cmr.umm.echo10.collection :as collection]
             [cmr.umm.echo10.granule :as granule]
             [cheshire.core :as cheshire]
             [cmr.indexer.data.index-set :as idx-set]
             [cmr.common.cache :as cache]
+            [cmr.common.services.errors :as errors]
+            [cmr.indexer.services.messages :as msg]
             [cmr.system-trace.core :refer [deftracefn]]))
 
 (defmulti parse-concept
@@ -22,6 +25,56 @@
   "Returns elastic json that can be used to insert into Elasticsearch for the given concept"
   (fn [context concept umm-concept]
     (cs/concept-id->type (:concept-id concept))))
+
+
+(def concepts
+    [{:revision-id 1, :deleted false, :format "ECHO10", :provider-id "FIX_PROV1", :native-id "RANGED-112", :concept-id "G1000000000-FIX_PROV1", :metadata "<Granule>\n  <GranuleUR>RANGED-112</GranuleUR>\n  <InsertTime>1999-12-31T19:00:00-05:00</InsertTime>\n  <LastUpdate>1999-12-31T19:00:00-05:00</LastUpdate>\n  <Collection>\n    <DataSetId>Ranged dataset example ID</DataSetId>\n  </Collection>\n  <DataGranule>\n    <SizeMBDataGranule>0.1</SizeMBDataGranule>\n    <ProducerGranuleId>granule ranger plots 112</ProducerGranuleId>\n    <DayNightFlag>DAY</DayNightFlag>\n    <ProductionDateTime>2011-11-08T14:30:00-05:00</ProductionDateTime>\n  </DataGranule>\n  <Orderable>true</Orderable>\n</Granule>", :revision-date "2014-06-04T15:31:57.029Z", :extra-fields {:delete-time nil, :parent-collection-id "C1000000073-FIX_PROV1"}, :concept-type :granule}
+     {:revision-id 1, :deleted false, :format "ECHO10", :provider-id "FIX_PROV1", :native-id "RANGED-113", :concept-id "G1000000001-FIX_PROV1", :metadata "<Granule>\n  <GranuleUR>RANGED-113</GranuleUR>\n  <InsertTime>1999-12-31T19:00:00-05:00</InsertTime>\n  <LastUpdate>1999-12-31T19:00:00-05:00</LastUpdate>\n  <Collection>\n    <DataSetId>Ranged dataset example ID</DataSetId>\n  </Collection>\n  <DataGranule>\n    <SizeMBDataGranule>0.1</SizeMBDataGranule>\n    <ProducerGranuleId>granule ranger plots 113</ProducerGranuleId>\n    <DayNightFlag>DAY</DayNightFlag>\n    <ProductionDateTime>2011-11-08T14:30:00-05:00</ProductionDateTime>\n  </DataGranule>\n  <Orderable>true</Orderable>\n</Granule>", :revision-date "2014-06-04T15:31:57.029Z", :extra-fields {:delete-time nil, :parent-collection-id "C1000000073-FIX_PROV1"}, :concept-type :granule}])
+
+(defn testy [context]
+  (validate-batch context concepts))
+
+
+
+(comment
+
+
+  (println user/system)
+  (let [indexer-context (get-in user/system [:apps :indexer])
+
+        _ (println context)
+        concepts (first (cmr.metadata-db.data.concepts/find-concepts-in-batches (get-in user/system [:apps :metadata-db :db]) {:concept-type :granule :parent-collection-id "C1000000073-FIX_PROV1" :provider-id "FIX_PROV1"} 10))
+        _ (validate-batch context concepts)])
+
+
+  )
+
+(deftracefn bulk-index
+  "Index many concepts at once using the elastic bulk api. The concepts to be indexed are passed
+  directly to this function - it does not retrieve them from metadata db. The bulk API is
+  invoked repeatedly if necessary - processing batch-size concepts each time."
+  [context concepts batch-size]
+  (doseq [[batch-index batch] (map-indexed vector (partition-all batch-size concepts))]
+    (let [all-index-names (set (map (fn [concept]
+                                    (let [{:keys [concept-id revision-id]} concept]
+                                      (idx-set/get-concept-index-name context
+                                                                      concept-id
+                                                                      revision-id concept)))
+                                  concepts))
+          all-types (set (map cs/concept->type concepts))
+          _ (assert (= 1 (count all-index-names) (msg/inconsistent-index-names-msg)))
+          _ (assert (= 1 (count all-types) (msg/inconsistent-types-msg)))
+          concept-index (first all-index-names)
+          type (first all-types)
+          bulk-operations (bulk/bulk-index batch)
+          response (bulk/bulk-with-index-and-type bulk-operations concept-index type)
+          oks (->> response
+                     :items
+                     (map #(get-in % [:index :ok])))]
+        (when-not (every? identity oks)
+          (errors/internal-error! (msg/bulk-indexing-error-msg batch-index response))))
+      ))
+
 
 (deftracefn index-concept
   "Index the given concept and revision-id"
