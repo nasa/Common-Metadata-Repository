2fa2116c970f820d5f3664da49c381cbab3fc681
Yonggang Liu <ygliuvt@gmail.com>
2014-06-16 10:13:52 -0400

    Renamed elasticsearch namespace to connect.

--- src/cmr/index_set/data/elasticsearch.clj
+++ src/cmr/index_set/data/elasticsearch.clj
@@ -9,7 +9,7 @@
             [cmr.index-set.services.messages :as m]
             [cheshire.core :as cheshire]
             [cmr.index-set.config.elasticsearch-config :as es-config]
-            [cmr.elastic-utils.elasticsearch :as es]
+            [cmr.elastic-utils.connect :as es]
             [cmr.system-trace.core :refer [deftracefn]]))
 
 (defn create-index
