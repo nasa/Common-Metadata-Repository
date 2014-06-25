9fd8dfc89966d37cde124f935ddc563dd58e925c
James Norton <james@element84.com>
2014-06-25 16:25:08 -0400

    CMR-564: Removed unneeded file.

--- src/cmr/search/data/transformer.clj
+++
@@ -1,9 +0,0 @@
-(ns cmr.search.data.transformer
-  "Provdes functions for retrieving formatted concepts."
-  (:require [cmr.transmit.transformer :as t]))
-
-(defn get-formatted-concepts
-  "Get concepts given by the concept-id, revision-id tuples in the
-  given format."
-  [context concept-tuples format]
-  (let [concepts "A"]))
