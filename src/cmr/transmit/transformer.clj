a6f2ff473309e2ce6f4cf6d8987d44717cd6cd33
James Norton <james@element84.com>
2014-06-25 14:27:23 -0400

    Changed code to set format correctly.

--- src/cmr/transmit/transformer.clj
+++ src/cmr/transmit/transformer.clj
@@ -3,21 +3,23 @@
   (:require [clj-http.client :as client]
             [cmr.common.services.errors :as errors]
             [cmr.transmit.config :as config]
-            [cheshire.core :as cheshire]
+            [cheshire.core :as json]
             [cmr.system-trace.http :as ch]
             [cmr.system-trace.core :refer [deftracefn]]
-            [cmr.transmit.connection :as conn]))
+            [cmr.transmit.connection :as conn]
+            [cmr.common.mime-types :as mt]))
 
 (deftracefn get-formatted-concept-revisions
   "Ask the transformer app for the concepts given by the concept-id, revision-id tuples in the
   given format."
   [context concept-tuples format]
   (let [conn (config/context->app-connection context :transformer)
-        tuples-json-str (cheshire/generate-string concept-tuples)
+        mime-type (format mt/format->mime-type)
+        tuples-json-str (json/encode concept-tuples)
         request-url (str (conn/root-url conn) "/concepts")
         response (client/post request-url {:body tuples-json-str
                                            :content-type :json
-                                           :accept format
+                                           :accept mime-type
                                            :throw-exceptions false
                                            :headers (ch/context->http-headers context)
                                            :connection-manager (conn/conn-mgr conn)})
@@ -28,7 +30,7 @@
         (errors/throw-service-error :not-found err-msg))
 
       200
-      (cheshire/decode (:body response) true)
+      (json/decode (:body response) true)
 
       ;; default
       (errors/internal-error! (str "Get concept revisions failed. Transformer app response status code: "
