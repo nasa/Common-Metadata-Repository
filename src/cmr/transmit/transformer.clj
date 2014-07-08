c4d9746ec74f9647c018d4f733e8b931309f8b7a
James Norton <james@element84.com>
2014-07-08 13:36:50 -0400

    CMR-586: Added call to get-latest-concepts.

--- src/cmr/transmit/transformer.clj
+++ src/cmr/transmit/transformer.clj
@@ -14,10 +14,39 @@
   given format."
   [context concept-tuples format]
   (let [conn (config/context->app-connection context :transformer)
-        mime-type (format mt/format->mime-type)
+        mime-type (mt/format->mime-type format)
         tuples-json-str (json/encode concept-tuples)
         request-url (str (conn/root-url conn) "/concepts")
         response (client/post request-url {:body tuples-json-str
+                                           :content-type :json
+                                           :accept mime-type
+                                           :throw-exceptions false
+                                           :headers (ch/context->http-headers context)
+                                           :connection-manager (conn/conn-mgr conn)})
+        status (:status response)]
+    (case status
+      404
+      (let [err-msg "Unable to find all concepts."]
+        (errors/throw-service-error :not-found err-msg))
+
+      200
+      (json/decode (:body response) true)
+
+      ;; default
+      (errors/internal-error! (str "Get concept revisions failed. Transformer app response status code: "
+                                   status
+                                   " "
+                                   response)))))
+
+(deftracefn get-latest-formatted-concepts
+  "Ask the transformer app for the latest version of the concepts given by the concept-ids in the
+  given format."
+  [context concept-ids format]
+  (let [conn (config/context->app-connection context :transformer)
+        mime-type (mt/format->mime-type format)
+        ids-json-str (json/encode concept-ids)
+        request-url (str (conn/root-url conn) "/latest-concepts")
+        response (client/post request-url {:body ids-json-str
                                            :content-type :json
                                            :accept mime-type
                                            :throw-exceptions false
