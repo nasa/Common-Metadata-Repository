93e7733e60ca6f246892371f05bd48382689175f
James Norton <james@element84.com>
2014-07-08 10:02:04 -0400

    CMR-586: Updated transformer tests to use new transformer APIs

--- test/cmr/system_int_test/transformer/granule.clj
+++ test/cmr/system_int_test/transformer/granule.clj
@@ -22,7 +22,7 @@
     (testing "transform granules to echo10"
       (are [concepts]
            (= (t/expected-response concepts :echo10)
-              (:response (t/transform-concepts concepts :echo10)))
+              (map :metadata (:response (t/transform-concepts concepts :echo10))))
            [g1-1]
            [g1-1 g2-1]
            [g1-1 g2-2]))
@@ -39,9 +39,9 @@
         g2-1 (d/ingest "PROV1" g2)
         g2-2 (d/ingest "PROV1" g2)]
     (testing "transform latest revision of granules to echo10"
-      (are [sent expected]
+      (are [sent-ids expected]
            (= (t/expected-response expected :echo10)
-              (:response (t/transform-latest-concepts sent :echo10)))
+              (map :metadata (:response (t/transform-latest-concepts sent-ids :echo10))))
            [(:concept-id g1-1)] [g1-1]
            [(:concept-id g1-1) (:concept-id g2-1)] [g1-1 g2-2]))
     (testing "transform latest with missing concept-id returns 404"
