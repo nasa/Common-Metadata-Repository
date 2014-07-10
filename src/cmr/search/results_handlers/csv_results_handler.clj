fdb16d678706d94a462d4580862e3f8f7538faff
Yonggang Liu <ygliuvt@gmail.com>
2014-07-10 18:40:45 -0400

    Added more fields to atom links; Updated search to use the new atom-links field in granule index.

--- src/cmr/search/results_handlers/csv_results_handler.clj
+++ src/cmr/search/results_handlers/csv_results_handler.clj
@@ -3,6 +3,7 @@
   (:require [cmr.search.data.elastic-results-to-query-results :as elastic-results]
             [cmr.search.data.elastic-search-index :as elastic-search-index]
             [cmr.search.services.query-service :as qs]
+            [clojure.data.json :as json]
             [clojure.data.csv :as csv]
             [clojure.string :as str])
   (:import
@@ -17,7 +18,7 @@
    "producer-gran-id"
    "start-date"
    "end-date"
-   "downloadable-urls"
+   "atom-links"
    "cloud-cover"
    "day-night"
    "size"])
@@ -28,15 +29,19 @@
          [producer-gran-id] :producer-gran-id
          [start-date] :start-date
          [end-date] :end-date
-         [downloadable-urls] :downloadable-urls
+         [atom-links] :atom-links
          [cloud-cover] :cloud-cover
          [day-night] :day-night
          [size] :size} (:fields elastic-result)
         start-date (when start-date (str/replace (str start-date) #"\+0000" "Z"))
-        end-date (when end-date (str/replace (str end-date) #"\+0000" "Z"))]
-    [granule-ur producer-gran-id start-date end-date downloadable-urls
-               (str cloud-cover) day-night (str size)]))
-
+        end-date (when end-date (str/replace (str end-date) #"\+0000" "Z"))
+        atom-links (if atom-links (json/read-str atom-links) [])
+        downloadable-urls (seq (map #(get % "href")
+                                    (filter #(= (get % "link-type") "data") atom-links)))
+        browse-urls (seq (map #(get % "href")
+                              (filter #(= (get % "link-type") "browse") atom-links)))]
+    [granule-ur producer-gran-id start-date end-date (str/join "," downloadable-urls)
+     (str/join "," browse-urls) (str cloud-cover) day-night (str size)]))
 
 (defmethod qs/search-results->response :csv
   [context query results]
