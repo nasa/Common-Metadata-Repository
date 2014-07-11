13dccfb046326d2c05d270e864f6e153db23e842
Jason Gilman <jason@element84.com>
2014-07-11 09:47:02 -0400

    Adding GZIP Compression to web server responses.

--- src/cmr/common/api/web_server.clj
+++ src/cmr/common/api/web_server.clj
@@ -3,7 +3,8 @@
   (:require [cmr.common.lifecycle :as lifecycle]
             [ring.adapter.jetty :as jetty]
             [cmr.common.log :refer (debug info warn error)])
-  (:import org.eclipse.jetty.server.Server))
+  (:import org.eclipse.jetty.server.Server
+           org.eclipse.jetty.server.handler.GzipHandler))
 
 (def MIN_THREADS
   "The minimum number of threads for Jetty to use to process requests. The was originally set to the
@@ -19,6 +20,13 @@
   making sure that the transmit library uses the same amount."
   50)
 
+(def MIN_GZIP_SIZE
+  "The size that will be used to determine if responses should be GZIP'd. See the following Stack
+  Overflow question:
+  http://webmasters.stackexchange.com/questions/31750/what-is-recommended-minimum-object-size-for-gzip-performance-benefits
+  Akamai recommend 860 bytes. We're transmitting UTF-8 which should be about a byte a character."
+  860)
+
 (defrecord WebServer
   [
    ;; The port Jetty will be running on
@@ -29,7 +37,7 @@
 
    ;;The actual Jetty instance
    server
-  ]
+   ]
 
   lifecycle/Lifecycle
 
@@ -37,10 +45,21 @@
     [this system]
     (let [{:keys [port routes-fn]} this
           routes (routes-fn system)
-          server (jetty/run-jetty routes {:port port
+          ^Server server (jetty/run-jetty routes {:port port
                                           :join? false
                                           :min-threads MIN_THREADS
-                                          :max-threads MAX_THREADS})]
+                                          :max-threads MAX_THREADS})
+          ;; Create a GZIP handler to handle compression of responses
+          new-handler (doto (GzipHandler.)
+                        (.setHandler (.getHandler server))
+                        (.setMinGzipSize MIN_GZIP_SIZE))]
+
+      ;; Replace the existing handler with the gzip handler
+      (doto server
+        (.stop)
+        (.setHandler new-handler)
+        (.start))
+
       (info "Jetty started on port" port)
       (assoc this :server server)))
 
