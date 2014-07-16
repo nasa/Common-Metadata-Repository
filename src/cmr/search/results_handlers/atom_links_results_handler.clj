a000ff40411ed14e4d2413c696ebf68ee75afea0
Yonggang Liu <ygliuvt@gmail.com>
2014-07-16 15:53:11 -0400

    Refactored code.

--- src/cmr/search/results_handlers/atom_links_results_handler.clj
+++ src/cmr/search/results_handlers/atom_links_results_handler.clj
@@ -6,6 +6,8 @@
   (:require [cmr.search.data.elastic-results-to-query-results :as elastic-results]
             [cmr.search.data.elastic-search-index :as elastic-search-index]
             [cmr.search.services.query-service :as qs]
+            [cmr.search.services.query-execution :as qe]
+            [cmr.search.models.query :as q]
             [clojure.data.xml :as x]
             [cheshire.core :as json]
             [clojure.string :as str]
@@ -23,3 +25,12 @@
         atom-links (map #(json/decode % true) atom-links)]
     [concept-id atom-links]))
 
+(defn find-collection-atom-links
+  "Returns a mapping of collection-concept-ids and its atom links for the given collection-concept-ids"
+  [context collection-concept-ids]
+  (let [collection-links-query (q/query {:concept-type :collection
+                                         :condition (q/string-conditions :concept-id collection-concept-ids true)
+                                         :page-size :unlimited
+                                         :result-format :atom-links})
+        result (qe/execute-query context collection-links-query)]
+    (into {} (:items result))))
--- src/cmr/search/results_handlers/atom_results_handler.clj
+++ src/cmr/search/results_handlers/atom_results_handler.clj
@@ -2,15 +2,14 @@
   "Handles the ATOM results format and related functions"
   (:require [cmr.search.data.elastic-results-to-query-results :as elastic-results]
             [cmr.search.data.elastic-search-index :as elastic-search-index]
-            [cmr.search.services.query-execution :as qe]
             [cmr.search.services.query-service :as qs]
-            [cmr.search.models.query :as q]
             [clojure.data.xml :as x]
             [cheshire.core :as json]
             [clojure.string :as str]
             [clj-time.core :as time]
             [cmr.spatial.serialize :as srl]
-            [cmr.search.results-handlers.atom-spatial-results-handler :as atom-spatial]))
+            [cmr.search.results-handlers.atom-spatial-results-handler :as atom-spatial]
+            [cmr.search.results-handlers.atom-links-results-handler :as atom-links]))
 
 (defmethod elastic-search-index/concept-type+result-format->fields [:granule :atom]
   [concept-type result-format]
@@ -158,21 +157,11 @@
                         (concat atom-links))]
     (assoc reference :atom-links atom-links)))
 
-(defn- find-collection-atom-links
-  "Returns a mapping of collection-concept-ids and its atom links for the given collection-concept-ids"
-  [context collection-concept-ids]
-  (let [collection-links-query (q/query {:concept-type :collection
-                                         :condition (q/string-conditions :concept-id collection-concept-ids true)
-                                         :page-size :unlimited
-                                         :result-format :atom-links})
-        result (qe/execute-query context collection-links-query)]
-    (into {} (:items result))))
-
 (defn- append-collection-links
   "Returns the references after appending collection non-downloadable links into the atom-links"
   [context refs]
   (let [collection-concept-ids (distinct (map :collection-concept-id refs))
-        collection-links-map (find-collection-atom-links context collection-concept-ids)]
+        collection-links-map (atom-links/find-collection-atom-links context collection-concept-ids)]
     (map (partial append-links collection-links-map) refs)))
 
 (defmethod qs/search-results->response :atom
