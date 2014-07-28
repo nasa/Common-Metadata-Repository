d41615a2949175a0884c6bc5562f31de3bbf33ae
Yonggang Liu <ygliuvt@gmail.com>
2014-07-28 10:51:59 -0400

    Addressed review comments.

--- src/cmr/system_int_test/data2/provider_holdings.clj
+++ src/cmr/system_int_test/data2/provider_holdings.clj
@@ -4,6 +4,11 @@
             [cmr.common.xml :as cx]
             [cheshire.core :as json]))
 
+(defmulti parse-provider-holdings
+  "Returns the parsed provider holdings based on the given format and result string"
+  (fn [format result]
+  format))
+
 (defn- xml-elem->provider-holding
   "Returns the provider holding entry by parsing the given xml struct"
   [xml-elem]
@@ -12,22 +17,15 @@
    :granule-count (cx/long-at-path xml-elem [:granule-count])
    :provider-id (cx/string-at-path xml-elem [:provider-id])})
 
-(defn- parse-xml-provider-holdings
-  "Returns the parsed provider holdings for the given xml string"
-  [xml]
+(defmethod parse-provider-holdings :xml
+  [format xml]
   (let [xml-struct (x/parse-str xml)]
     (map xml-elem->provider-holding
          (cx/elements-at-path xml-struct [:provider-holding]))))
 
-(defn- parse-json-provider-holdings
-  "Returns the parsed provider holdings for the given json string"
-  [json-str]
+(defmethod parse-provider-holdings :json
+  [format json-str]
   (json/decode json-str true))
 
-(defn parse-provider-holdings
-  "Returns the parsed provider holdings based on the given format and result string"
-  [format result]
-  (if (= :xml format)
-    (parse-xml-provider-holdings result)
-    (parse-json-provider-holdings result)))
+
 
