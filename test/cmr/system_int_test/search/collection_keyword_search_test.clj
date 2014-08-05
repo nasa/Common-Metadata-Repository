68cb779342850cadc7958155b3260d9744c9a656
James Norton <james@element84.com>
2014-08-05 11:57:23 -0400

    CMR-203: Updated tests.

--- test/cmr/system_int_test/search/collection_keyword_search_test.clj
+++ test/cmr/system_int_test/search/collection_keyword_search_test.clj
@@ -14,6 +14,8 @@
         psa2 (dc/psa "bravo" :string "bf")
         psa3 (dc/psa "charlie" :string "foo")
         psa4 (dc/psa "case" :string "up")
+        p1 (dc/platform "platform_Sn B")
+        p2 (dc/platform "platform_SnA")
         sk1 (dc/science-keyword {:category "Cat1"
                                  :topic "Topic1"
                                  :term "Term1"
@@ -28,29 +30,30 @@
                                  :variable-level-2 "Level2-2"
                                  :variable-level-3 "Level2-3"
                                  :detailed-variable "UNIVERSAL"})
+        tdcs1 (dc/two-d "XYZ")
         coll1 (d/ingest "CMR_PROV1" (dc/collection {}))
-        coll2 (d/ingest "CMR_PROV1" (dc/collection {:entry-id "ABC!XYZ"}))
+        coll2 (d/ingest "CMR_PROV1" (dc/collection {:entry-title "ABC!XYZ"}))
         coll3 (d/ingest "CMR_PROV1" (dc/collection {:collection-data-type "Foo"}))
         coll4 (d/ingest "CMR_PROV2" (dc/collection {:collection-data-type "Bar"}))
-        coll5 (d/ingest "CMR_PROV2" (dc/collection {:short-name "Short name"}))
-        coll6 (d/ingest "CMR_PROV2" (dc/collection {:archive-center "Some$Place"}))
-        coll7 (d/ingest "CMR_PROV2" (dc/collection {:version-id "ABC123"}))
+        coll5 (d/ingest "CMR_PROV2" (dc/collection {:entry-title "ABC" :short-name "Space!Laser"}))
+        coll6 (d/ingest "CMR_PROV2" (dc/collection {:organizations [(dc/org :archive-center "Some&Place")]}))
+        coll7 (d/ingest "CMR_PROV2" (dc/collection {:version-id "Laser"}))
         coll8 (d/ingest "CMR_PROV2" (dc/collection {:processing-level-id "PDQ123"}))
         coll9 (d/ingest "CMR_PROV2" (dc/collection {:science-keywords [sk1 sk2]}))
         coll10 (d/ingest "CMR_PROV2" (dc/collection {:spatial-keywords ["in out"]}))
-        coll11 (d/ingest "CMR_PROV2" (dc/collection {:platform-sn "Space Laser"}))
+        coll11 (d/ingest "CMR_PROV2" (dc/collection {:platforms [p1 p2]}))
         coll12 (d/ingest "CMR_PROV2" (dc/collection {:product-specific-attributes [psa1 psa2 psa3 psa4]}))
-        coll13 (d/ingest "CMR_PROV2" (dc/collection {:two-d-coord-name "XY"}))]
+        coll13 (d/ingest "CMR_PROV2" (dc/collection {:two-d-coordinate-systems [tdcs1]}))]
 
     (index/refresh-elastic-index)
 
     (testing "search by keywords."
       (are [keyword-str items] (d/refs-match? items (search/find-refs :collection {:keyword keyword-str}))
-           "ABC" [coll2]
+           "ABC" [coll2 coll5]
            "XYZ" [coll2 coll13]
            "place" [coll6]
-           "Laser" [coll11]
-           "ABC place Huricane" [coll2 coll6 coll9]
+           "Laser" [coll5 coll7]
+           "ABC place Hurricane" [coll2 coll5 coll6 coll9]
            "BLAH" []))
     ; (testing "search by spatial keywords using wildcard *."
     ;   (is (d/refs-match? [coll3 coll4 coll5 coll7]
