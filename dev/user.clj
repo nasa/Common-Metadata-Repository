fbc0c63b0bbd42ee00c76a13738b24f1176a4a2b
Jason Gilman <jason@element84.com>
2014-08-06 10:31:06 -0400

    Fixed error in atom results handler found in workload.

--- dev/user.clj
+++ dev/user.clj
@@ -7,22 +7,26 @@
             [cmr.search.api.routes :as routes]
             [cmr.common.dev.repeat-last-request :as repeat-last-request :refer (repeat-last-request)]
             [cmr.common.dev.util :as d]
-            [cmr.transmit.config :as transmit-config])
+            [cmr.transmit.config :as transmit-config]
+            [cmr.common.config :as cfg])
   (:use [clojure.test :only [run-all-tests]]
         [clojure.repl]
         [alex-and-georges.debug-repl]))
 
 (def system nil)
 
+(defn create-system
+  []
+  (let [web-server (web/create-web-server (transmit-config/search-port)
+                                          (repeat-last-request/wrap-api routes/make-api))]
+    (assoc (system/create-system) :web web-server)))
+
 (defn start
   "Starts the current development system."
   []
-  (let [web-server (web/create-web-server (transmit-config/search-port)
-                                          (repeat-last-request/wrap-api routes/make-api))
-        s (assoc (system/create-system) :web web-server)]
-    (alter-var-root #'system
-                    (constantly
-                      (system/start s))))
+  (alter-var-root #'system
+                  (constantly
+                    (system/start (create-system))))
   (d/touch-user-clj))
 
 (defn stop
@@ -36,5 +40,26 @@
   (stop)
   ; Refreshes all of the code and then restarts the system
   (refresh :after 'user/start))
+
+
+(defn tunnel-system
+  "Allows tunneling the search to a indexer and elasticsearch running on a different system"
+  []
+  (cfg/set-config-value! :index-set-port 4005)
+  ;; Stop the current system
+  (stop)
+
+  (let [updated-sys (-> (create-system)
+                        (assoc-in [:search-index :config :port] 9211)
+                        system/start)]
+    (alter-var-root #'system (constantly updated-sys))))
+
+(comment
+  ;; workload can be tunnelled by running these
+  ;; ssh -L4005:localhost:3005 cmr-wl-app1.dev.echo.nasa.gov
+  ;; ssh -L9211:localhost:9200 cmr-wl-elastic1.dev.echo.nasa.gov
+  (tunnel-system)
+
+)
 
 (info "Custom user.clj loaded.")
