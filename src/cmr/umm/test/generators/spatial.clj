9eb103b423ff92eac0e7ed272f0a0eefbb65c8e3
Jason Gilman <jason@element84.com>
2014-08-07 09:44:45 -0400

    Fixing failing tests due to spatial issue.

--- src/cmr/umm/test/generators/spatial.clj
+++ src/cmr/umm/test/generators/spatial.clj
@@ -3,6 +3,7 @@
             [clojure.test.check.generators :as gen]
             [cmr.common.test.test-check-ext :as ext-gen :refer [optional]]
             [cmr.spatial.polygon :as poly]
+            [cmr.spatial.line-string :as l]
             [cmr.umm.spatial :as umm-s]))
 
 (def generic-rings
@@ -14,10 +15,14 @@
 
 (def polygons
   "Generates polygons that are not valid but could be used for testing where validity is not important"
-  (gen/fmap (partial apply poly/polygon)
-            (gen/tuple (gen/vector generic-rings 1 4))))
+  (gen/fmap poly/polygon
+            (gen/vector generic-rings 1 4)))
+
+(def lines
+  (gen/fmap l/line-string
+            (gen/bind (gen/choose 2 6) sgen/non-antipodal-points)))
 
 (def geometries
   "A generator returning individual points, bounding rectangles, lines, and polygons.
   The spatial areas generated will not necessarily be valid."
-  (gen/one-of [sgen/points sgen/mbrs sgen/lines polygons]))
+  (gen/one-of [sgen/points sgen/mbrs lines polygons]))
