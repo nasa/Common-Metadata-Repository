355a948ca50fa540918e5e231beab9ebcc013c78
Jason Gilman <jason@element84.com>
2014-08-08 16:21:11 -0400

    Moved XML Schema validation code into cmr-common-lib.

--- src/cmr/common/xml.clj
+++ src/cmr/common/xml.clj
@@ -2,7 +2,13 @@
   "Contains XML helpers for extracting data from XML structs created using clojure.data.xml.
   See the test file for examples."
   (:require [cmr.common.date-time-parser :as p]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.java.io :as io])
+  (:import javax.xml.validation.SchemaFactory
+           javax.xml.XMLConstants
+           javax.xml.transform.stream.StreamSource
+           org.xml.sax.ext.DefaultHandler2
+           java.io.StringReader))
 
 (defn remove-xml-processing-instructions
   "Removes xml processing instructions from XML so it can be embedded in another XML document"
@@ -104,3 +110,39 @@
   [xml-struct path]
   (first (datetimes-at-path xml-struct path)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; XML Schema Validation
+
+
+(defn- sax-parse-exception->str
+  "Converts a SaxParseException to a String"
+  [e]
+  (format "Line %d - %s" (.getLineNumber e) (.getMessage e)))
+
+(defn- create-error-handler
+  "Creates an instance of DefaultHandler2 that will save all schema errors in the errors-atom."
+  [errors-atom]
+  (proxy [DefaultHandler2] []
+    (error
+      [e]
+      (swap! errors-atom conj (sax-parse-exception->str e)))
+    (fatalError
+      [e]
+      (swap! errors-atom conj (sax-parse-exception->str e)))))
+
+(defn validate-xml
+  "Validates the XML against the schema in the given resource. schema-resource should be a classpath
+  resource as returned by clojure.java.io/resource.
+  Returns a list of errors in the XML schema."
+  [schema-resource xml]
+  (let [factory (SchemaFactory/newInstance XMLConstants/W3C_XML_SCHEMA_NS_URI)
+        schema (.newSchema factory schema-resource)
+        validator (.newValidator schema)
+        errors-atom (atom [])]
+    (.setErrorHandler validator (create-error-handler errors-atom))
+    (try
+      (.validate validator (StreamSource. (StringReader. xml)))
+      (catch org.xml.sax.SAXParseException e
+        ;; An exception can be thrown if it is completely invalid XML.
+        (reset! errors-atom [(sax-parse-exception->str e)])))
+    (seq @errors-atom)))
