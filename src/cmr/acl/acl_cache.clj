f42e5d0bcae9a4c49cfa9f49e34a673504cce763
Jason Gilman <jason@element84.com>
2014-08-18 13:56:31 -0400

    Moved some acl provider id retrieval to the transmit library.

--- src/cmr/acl/acl_cache.clj
+++ src/cmr/acl/acl_cache.clj
@@ -6,7 +6,6 @@
             [clj-time.core :as t]
             [cmr.common.jobs :refer [defjob]]
             [cmr.transmit.echo.acls :as echo-acls]
-            [cmr.transmit.echo.providers :as echo-providers]
             [cmr.common.log :as log :refer (debug info warn error)]
             [clojure.walk :as w]))
 
@@ -24,16 +23,6 @@
   [context]
   (get-in context [:system :acl-cache]))
 
-(defn- set-acl-provider-id
-  "Sets the provider-id in the acl to replace the provider guid"
-  [provider-guid-id-map acl]
-  (update-in acl
-             [:catalog-item-identity]
-             (fn [cii]
-               (-> cii
-                   (assoc :provider-id (provider-guid-id-map (:provider-guid cii)))
-                   (dissoc :provider-guid)))))
-
 (defn reset
   "Resets the cache back to it's initial state"
   [context]
@@ -46,11 +35,9 @@
   to keep the cache fresh. This will throw an exception if there is a problem fetching ACLs. The
   caller is responsible for catching and logging the exception."
   [context]
-  (let [acl-cache-atom (context->acl-cache context)
-        provider-guid-id-map (echo-providers/get-provider-guid-id-map context)
-        acls (mapv (partial set-acl-provider-id provider-guid-id-map)
-                   (echo-acls/get-acls-by-type context "CATALOG_ITEM"))]
-    (reset! acl-cache-atom {:acls acls :last-updated (t/now)})))
+  (let [acl-cache-atom (context->acl-cache context)]
+    (reset! acl-cache-atom {:acls (echo-acls/get-acls-by-type context "CATALOG_ITEM")
+                            :last-updated (t/now)})))
 
 (defn get-acls
   "Gets the current cached acls."
