a4efc9282b6b4df02424a2b2a67546bdd784d945
Jason Gilman <jason@element84.com>
2014-08-28 09:16:37 -0400

    Changed version of cmr-dev-system.

--- project.clj
+++ project.clj
@@ -36,7 +36,7 @@
 
 ;; The version number here is for the sprint number. It will be incremented each sprint. The second
 ;; number is for which delivery of the version was given to ECHO for use.
-(defproject nasa-cmr/cmr-dev-system "0.11.1"
+(defproject nasa-cmr/cmr-dev-system "0.11.2"
   :description "Dev System combines together the separate microservices of the CMR into a single
                application to make it simpler to develop."
   :url "***REMOVED***projects/CMR/repos/cmr-dev-system/browse"
