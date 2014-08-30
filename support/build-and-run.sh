c4fe182f23fbe603ed3c2d787f245e8b105bae7c
Jason Gilman <jason@element84.com>
2014-08-30 08:20:50 -0400

    Incremented version. Removed TODOs

--- support/build-and-run.sh
+++ support/build-and-run.sh
@@ -7,4 +7,4 @@
 lein clean
 rm -rf es_data
 LEIN_SNAPSHOTS_IN_RELEASE=true lein uberjar
-CMR_COLLS_WITH_SEPARATE_INDEXES='C1-SEP_PROV1,C2-SEP_PROV1' nohup java -XX:MaxPermSize=256m -jar target/cmr-dev-system-0.11.2-standalone.jar&
+CMR_COLLS_WITH_SEPARATE_INDEXES='C1-SEP_PROV1,C2-SEP_PROV1' nohup java -XX:MaxPermSize=256m -jar target/cmr-dev-system-0.11.3-standalone.jar&
