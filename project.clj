d9de5cecaa44a27fd0e058cd8e07c528aa9474c6
Jason Gilman <jason@element84.com>
2014-09-03 10:56:19 -0400

    Fixing issue with starting development repl.

--- project.clj
+++ project.clj
@@ -2,7 +2,6 @@
   :description "Contains utilities for retreiving and working with ACLs."
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [nasa-cmr/cmr-common-lib "0.1.1-SNAPSHOT"]
                  [nasa-cmr/cmr-transmit-lib "0.1.0-SNAPSHOT"]]
 
   :plugins [[lein-test-out "0.3.1"]]
