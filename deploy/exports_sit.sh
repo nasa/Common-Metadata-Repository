7e0bb2c950f6a9599ec0a2d7a350658a097451d0
Jason Gilman <jason@element84.com>
2014-09-03 12:32:48 -0400

    Adding deployment scripts.

---
+++ deploy/exports_sit.sh
@@ -0,0 +1,4 @@
+export cap_application=bootstrap
+export cap_user=clj-bootstrap
+export cap_deploy_to=/data/clojure/bootstrap
+export cap_servers=cmr-sit-app1.dev.echo.nasa.gov,cmr-sit-app2.dev.echo.nasa.gov
---
+++ deploy/exports_wl.sh
@@ -0,0 +1,4 @@
+export cap_application=bootstrap
+export cap_user=clj-bootstrap
+export cap_deploy_to=/data/clojure/bootstrap
+export cap_servers=cmr-wl-app1.dev.echo.nasa.gov,cmr-wl-app2.dev.echo.nasa.gov,cmr-wl-app3.dev.echo.nasa.gov
