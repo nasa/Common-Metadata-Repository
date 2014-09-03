a55dad57c738482356ded4f86101fb87d168d13e
Jason Gilman <jason@element84.com>
2014-09-03 10:56:42 -0400

    Fixing issue with starting development repl.

--- project.clj
+++ project.clj
@@ -3,12 +3,11 @@
                for indexing and searching for concepts."
   :url "***REMOVED***projects/CMR/repos/cmr-index-set-app/browse"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [nasa-cmr/cmr-common-lib "0.1.1-SNAPSHOT"]
                  [nasa-cmr/cmr-umm-lib "0.1.0-SNAPSHOT"]
                  [nasa-cmr/cmr-system-trace-lib "0.1.0-SNAPSHOT"]
                  [nasa-cmr/cmr-elastic-utils-lib "0.1.0-SNAPSHOT"]
                  [compojure "1.1.6"]
-                 [ring/ring-core "1.2.2"]
+                 [ring/ring-core "1.2.2" :exclusions [clj-time]]
                  [ring/ring-json "0.3.1"]
                  [org.clojure/tools.reader "0.8.4"]
                  [org.clojure/tools.cli "0.3.1"]]
