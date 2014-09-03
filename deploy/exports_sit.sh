8d910e960c10c6614e74b2b5a48067ece5cc44de
Jason Gilman <jason@element84.com>
2014-09-03 12:33:06 -0400

    Adding deployment scripts.

---
+++ deploy/exports_sit.sh
@@ -0,0 +1,4 @@
+export cap_application=ingest
+export cap_user=clj-ingest
+export cap_deploy_to=/data/clojure/ingest
+export cap_servers=cmr-sit-app1.dev.echo.nasa.gov,cmr-sit-app2.dev.echo.nasa.gov
