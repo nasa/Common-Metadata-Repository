edd08d5227587c1e810e9921f6314752c0d0445d
Jason Gilman <jason@element84.com>
2014-09-03 12:46:45 -0400

    Adding deployments

---
+++ deploy/exports_sit.sh
@@ -0,0 +1,4 @@
+export cap_application=metadata-db
+export cap_user=clj-metadata-db
+export cap_deploy_to=/data/clojure/metadata-db
+export cap_servers=cmr-sit-app1.dev.echo.nasa.gov,cmr-sit-app2.dev.echo.nasa.gov
