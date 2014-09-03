54b508364511aa3e8036f30e3f52aa1f98ddbdf1
Jason Gilman <jason@element84.com>
2014-09-03 12:29:06 -0400

    Adding deployments

---
+++ deploy/exports_sit.sh
@@ -0,0 +1,4 @@
+export cap_application=index-set
+export cap_user=clj-index-set
+export cap_deploy_to=/data/clojure/index-set
+export cap_servers=cmr-sit-app1.dev.echo.nasa.gov,cmr-sit-app2.dev.echo.nasa.gov
