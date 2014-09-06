3463d9ec75f3b5e40e3da8b6f80dd873a9bf73fe
Jason Gilman <jason@element84.com>
2014-09-06 13:21:23 -0400

    Updating clojure.data.xml dependency to 0.0.8.

--- project.clj
+++ project.clj
@@ -2,16 +2,11 @@
   :description "Provides common utility code for CMR projects."
   :url "***REMOVED***projects/CMR/repos/cmr-common-lib/browse"
 
-  ;; Add sonatype repository to pull in the 0.0.8-SNAPSHOT of org.clojure/data.xml
-  ;; We need this snapshot version of data.xml to address this issue: http://dev.clojure.org/jira/browse/DXML-14
-  ;; Once data.xml 0.0.8 is officially released, we should remove this repository
-  :repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]]
-
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.taoensso/timbre "3.2.0"]
                  [ring/ring-jetty-adapter "1.2.2"]
                  [org.clojure/test.check "0.5.8"]
-                 [org.clojure/data.xml "0.0.8-SNAPSHOT"]
+                 [org.clojure/data.xml "0.0.8"]
                  [camel-snake-kebab "0.1.5"]
                  [org.clojure/core.cache "0.6.3"]
                  [org.clojure/data.codec "0.1.0"]
