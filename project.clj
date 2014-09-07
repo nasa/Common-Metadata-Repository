a1fa8001944602822e335856ad3a6471669b146f
Jason Gilman <jason@element84.com>
2014-09-07 13:33:01 -0400

    Upgrading some library versions.

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [nasa-cmr/cmr-oracle-lib "0.1.0-SNAPSHOT"]
                  [nasa-cmr/cmr-system-trace-lib "0.1.0-SNAPSHOT"]
-                 [compojure "1.1.6"]
-                 [ring/ring-core "1.2.2" :exclusions [clj-time]]
+                 [compojure "1.1.9"]
+                 [ring/ring-core "1.3.1" :exclusions [clj-time]]
                  [ring/ring-json "0.3.1"]
                  [org.clojure/tools.reader "0.8.4"]
                  [org.clojure/tools.cli "0.3.1"]
@@ -23,7 +23,7 @@
   :repl-options {:init-ns user}
 
   :profiles
-  {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]
+  {:dev {:dependencies [[org.clojure/tools.namespace "0.2.5"]
                         [org.clojars.gjahad/debug-repl "0.3.3"]
                         [cheshire "5.3.1"]
                         [clj-http "0.9.1"]]
