047e8f181648cf6e53d8222655eab9b3dc7505c3
Jason Gilman <jason@element84.com>
2014-09-07 14:17:17 -0400

    Updating for camel snake kebab changes.

--- project.clj
+++ project.clj
@@ -7,7 +7,6 @@
                  [nasa-cmr/cmr-acl-lib "0.1.0-SNAPSHOT"]
                  [nasa-cmr/cmr-system-trace-lib "0.1.0-SNAPSHOT"]
                  [compojure "1.1.9"]
-                 [clj-time "0.7.0"]
                  [ring/ring-core "1.3.1" :exclusions [clj-time]]
                  [ring/ring-json "0.3.1"]
                  [org.clojure/tools.reader "0.8.4"]
