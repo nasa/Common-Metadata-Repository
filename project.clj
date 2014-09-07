5bffa82f8f0d025ce75ca490f88cc3175f456d52
Jason Gilman <jason@element84.com>
2014-09-07 14:53:56 -0400

    Upgrading library versions.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :url "***REMOVED***projects/CMR/repos/cmr-transmit-lib/browse"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-http "0.9.1"]
+                 [clj-http "1.0.0"]
                  [nasa-cmr/cmr-common-lib "0.1.1-SNAPSHOT"]
                  [nasa-cmr/cmr-system-trace-lib "0.1.0-SNAPSHOT"]]
 
