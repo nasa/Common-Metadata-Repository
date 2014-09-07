07583aa1368d095249ad08c682d7f6cdfb38bf69
Jason Gilman <jason@element84.com>
2014-09-07 13:31:46 -0400

    Upgrading some library versions.

--- project.clj
+++ project.clj
@@ -7,8 +7,8 @@
                  [nasa-cmr/cmr-system-trace-lib "0.1.0-SNAPSHOT"]
                  [nasa-cmr/cmr-umm-lib "0.1.0-SNAPSHOT"]
                  [nasa-cmr/cmr-transmit-lib "0.1.0-SNAPSHOT"]
-                 [compojure "1.1.6"]
-                 [ring/ring-core "1.2.2" :exclusions [clj-time]]
+                 [compojure "1.1.9"]
+                 [ring/ring-core "1.3.1" :exclusions [clj-time]]
                  [ring/ring-json "0.3.1"]
                  [clj-http "0.9.0"]
                  [org.clojure/tools.reader "0.8.4"]
@@ -27,7 +27,7 @@
   :repl-options {:init-ns user}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.5"]
-                        [org.clojure/tools.namespace "0.2.4"]
+                        [org.clojure/tools.namespace "0.2.5"]
                         [org.clojars.gjahad/debug-repl "0.3.3"]]
          :source-paths ["src" "dev" "test"]}
    :uberjar {:main cmr.ingest.runner
