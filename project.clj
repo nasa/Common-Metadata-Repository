a97b97da1e4661914fdd462657519559940565cc
Jason Gilman <jason@element84.com>
2014-09-07 14:52:02 -0400

    Upgrading library versions.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Bootstrap is a CMR application that can bootstrap the CMR with data from Catalog REST."
   :url "***REMOVED***projects/CMR/repos/cmr-bootstrap-app/browse"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-http "0.9.1"]
+                 [clj-http "1.0.0"]
                  [nasa-cmr/cmr-oracle-lib "0.1.0-SNAPSHOT"]
                  [nasa-cmr/cmr-system-trace-lib "0.1.0-SNAPSHOT"]
                  [nasa-cmr/cmr-metadata-db-app "0.1.0-SNAPSHOT"]
@@ -10,10 +10,10 @@
                  [compojure "1.1.9"]
                  [ring/ring-core "1.3.1" :exclusions [clj-time]]
                  [ring/ring-json "0.3.1"]
-                 [org.clojure/tools.reader "0.8.4"]
+                 [org.clojure/tools.reader "0.8.8"]
                  [org.clojure/tools.cli "0.3.1"]
                  [sqlingvo "0.5.17"]
-                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
                  [nasa-cmr/cmr-transmit-lib "0.1.0-SNAPSHOT"]]
   :plugins [[lein-test-out "0.3.1"]]
   :repl-options {:init-ns user}
