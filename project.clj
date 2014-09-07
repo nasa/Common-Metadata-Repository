813cda993415fb011b19d6e397d6344835fb2293
Jason Gilman <jason@element84.com>
2014-09-07 13:30:12 -0400

    Upgrading some library versions.

--- project.clj
+++ project.clj
@@ -14,12 +14,12 @@
                  [nasa-cmr/cmr-search-app "0.1.0-SNAPSHOT"]
 
                  ; include ring-core to support encoding of params
-                 [ring/ring-core "1.2.2" :exclusions [clj-time]]
+                 [ring/ring-core "1.3.1" :exclusions [clj-time]]
                  [cheshire "5.3.1"]]
   :plugins [[lein-test-out "0.3.1"]]
 
   :profiles
-  {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]
+  {:dev {:dependencies [[org.clojure/tools.namespace "0.2.5"]
                         [org.clojars.gjahad/debug-repl "0.3.3"]
                         [nasa-cmr/cmr-vdd-spatial-viz "0.1.0-SNAPSHOT"]]
          :source-paths ["src" "dev"]}})
