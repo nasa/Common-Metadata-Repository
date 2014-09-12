d0aac566ed63360b7882f7f902cb82776a06d792
James Norton <james@element84.com>
2014-09-12 10:19:16 -0400

    CMR-387: Cleaned up test.

--- test/cmr/system_int_test/search/granule_orbit_search_test.clj
+++ test/cmr/system_int_test/search/granule_orbit_search_test.clj
@@ -13,15 +13,7 @@
             [cmr.spatial.mbr :as m]
             [cmr.spatial.ring-relations :as rr]
             [cmr.spatial.derived :as derived]
-            [cmr.spatial.codec :as codec]
-            [clojure.string :as str]
-            [cmr.spatial.dev.viz-helper :as viz-helper]
-            [cmr.spatial.serialize :as srl]
-            [cmr.common.dev.util :as dev-util]
-            [cmr.spatial.lr-binary-search :as lbs]
-            [cmr.umm.spatial :as umm-s]
-            [cmr.umm.collection :as c]
-            [cmr.umm.echo10.collection :as ec]))
+            [cmr.spatial.codec :as codec]))
 
 (use-fixtures :each (ingest/reset-fixture {"provguid1" "PROV1"}))
 
@@ -69,24 +61,25 @@
     (index/refresh-elastic-index)
 
     (testing "bounding rectangle searches"
-        (are [wnes items]
-             (let [found (search/find-refs :granule {:bounding-box (codec/url-encode (apply m/mbr wnes))
-                                                     :page-size 50})
-                   matches? (d/refs-match? items found)]
-               (when-not matches?
-                 (println "Expected:" (->> items (map :granule-ur) sort pr-str))
-                 (println "Actual:" (->> found :refs (map :name) sort pr-str)))
-               matches?)
+      (are [wnes items params]
+           (let [found (search/find-refs
+                         :granule
+                         (merge {:bounding-box
+                                 (codec/url-encode (apply m/mbr wnes))
+                                 :page-size 50} params))
+                 matches? (d/refs-match? items found)]
+             (when-not matches?
+               (println "Expected:" (->> items (map :granule-ur) sort pr-str))
+               (println "Actual:" (->> found :refs (map :name) sort pr-str)))
+             matches?)
 
-             ;; Search for orbits crossing a rectangle over the equator and anti-meridian
-             [145 45 -145 -45] [g2 g7]))
-             ;; Search for orbits crossing a rectangle over the equator and meridian
-             [-45 45 45 -45] [g1 g3 g8]
-             ;; Search for orbits crossing a rectangle in the western hemisphere near the north pole
-             [-90 89 -45 85] [g5]
-             ;; Search for orbits crossing a rectangle in the southern hemisphere crossing the anti-meridian
-             [145 -45 -145 -85] [g2 g3 g4 g7]
-
-             ;[-145 45 145 -45] [g2]))
-
-    ))
+           ;; Search for orbits crossing a rectangle over the equator and anti-meridian
+           [145 45 -145 -45] [g2 g7] nil
+           ;; Search for orbits crossing a rectangle over the equator and meridian
+           [-45 45 45 -45] [g1 g3 g8] nil
+           ;; Search for orbits crossing a rectangle in the western hemisphere near the north pole
+           [-90 89 -45 85] [g5] nil
+           ;; Search for orbits crossing a rectangle in the southern hemisphere crossing the anti-meridian
+           [145 -45 -145 -85] [g2 g3 g4 g7] nil
+           ;; Search while specifying parent collection
+           [145 45 -145 -45] [g2] {:concept-id (:concept-id coll1)}))))
