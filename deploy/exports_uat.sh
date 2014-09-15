b751fadc17f2a3fd026af1603da94f6f72b3e295
Jason Gilman <jason@element84.com>
2014-09-15 13:47:49 -0400

    Adding uat deployment properties.

---
+++ deploy/exports_uat.sh
@@ -0,0 +1,4 @@
+export cap_application=bootstrap
+export cap_user=clj-bootstrap
+export cap_deploy_to=/data/clojure/bootstrap
+export cap_servers=cmr-uat-app1.dev.echo.nasa.gov,cmr-uat-app2.dev.echo.nasa.gov
