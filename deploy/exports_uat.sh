9c07cfc6df0fa92f8f88e9c5a8ff444e19009919
Jason Gilman <jason@element84.com>
2014-09-15 13:48:22 -0400

    Adding uat deployment properties.

---
+++ deploy/exports_uat.sh
@@ -0,0 +1,4 @@
+export cap_application=ingest
+export cap_user=clj-ingest
+export cap_deploy_to=/data/clojure/ingest
+export cap_servers=cmr-uat-app1.dev.echo.nasa.gov,cmr-uat-app2.dev.echo.nasa.gov
