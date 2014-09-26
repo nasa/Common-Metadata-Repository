f9d8eacfc326cecf2854e9417fec61df3c70cd8a
Jason Gilman <jason@element84.com>
2014-09-26 07:20:36 -0400

    CMR-678: Added helpef function for enforcing that a user has ingest managment granted.

--- src/cmr/acl/core.clj
+++ src/cmr/acl/core.clj
@@ -2,9 +2,35 @@
   "Contains code for retrieving and manipulating ACLs."
   (:require [cmr.common.services.errors :as errors]
             [cmr.acl.acl-cache :as acl-cache]
+            [cmr.transmit.echo.acls :as echo-acls]
+            [cmr.transmit.echo.tokens :as echo-tokens]
             [cmr.acl.collection-matchers :as cm]))
 
+(defn context->sids
+  "Returns the security identifiers (group guids and :guest or :registered) of the user identified
+  by the token in the context."
+  [context]
+  (let [{:keys [token]} context]
+    (if token
+      (echo-tokens/get-current-sids context token)
+      [:guest])))
 
+(defn- ace-matches-sid?
+  "Returns true if the ACE is applicable to the SID."
+  [sid ace]
+  (or
+    (= sid (:user-type ace))
+    (= sid (:group-guid ace))))
+
+(defn acl-matches-sids-and-permission?
+  "Returns true if the acl is applicable to any of the sids."
+  [sids permission acl]
+  (some (fn [sid]
+          (some (fn [ace]
+                  (and (ace-matches-sid? sid ace)
+                       (some #(= % permission) (:permissions ace))))
+                (:aces acl)))
+        sids))
 
 (defn get-coll-permitted-group-ids
   "Returns the groups ids (group guids, 'guest', 'registered') that have permission to read
@@ -22,17 +48,21 @@
        (map #(or (:group-guid %) (some-> % :user-type name)))
        distinct))
 
+(defn verify-ingest-management-permission
+  "Verifies the current user has been granted INGEST_MANAGEMENT_PERMISSION in ECHO ACLs"
+  ([context]
+   (verify-ingest-management-permission context :update))
+  ([context permission-type]
+   (when-not  (->> (echo-acls/get-acls-by-type context "SYSTEM_OBJECT")
+                   ;; Find acls on INGEST_MANAGEMENT
+                   (filter (comp (partial = "INGEST_MANAGEMENT_ACL") :target :system-object-identity))
+                   ;; Find acls for this user and permission type
+                   (filter (partial acl-matches-sids-and-permission?
+                                    (context->sids context)
+                                    permission-type))
+                   seq)
+     (errors/throw-service-error
+       :unauthorized
+       "You do not have permission to perform that action."))))
 
-(comment
 
-  (require '[cmr.umm.collection :as umm-c])
-  (def coll (umm-c/map->UmmCollection {:entry-title  "GHRSST Level 4 USA NAVOCEANO K10_SST Global SST:1"}))
-
-  (acl-cache/get-acls {:system user/system})
-
-  (get-coll-permitted-group-ids {:system user/system} "PODAAC" coll)
-
-
-
-
-  )
