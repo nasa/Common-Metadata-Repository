8f0336548029f1025a1b785f89b69881a12c483d
Jason Gilman <jason@element84.com>
2014-09-28 19:41:23 -0400

    Fix concurrency issue when processing XSLT. Saxon XSLT transformers are not thread safe.

--- src/cmr/search/services/xslt.clj
+++ src/cmr/search/services/xslt.clj
@@ -6,28 +6,36 @@
            java.io.StringReader
            java.io.StringWriter
            javax.xml.transform.stream.StreamSource
-           javax.xml.transform.stream.StreamResult))
+           javax.xml.transform.stream.StreamResult
+           javax.xml.transform.Templates))
 
 (def xsl-transformer-cache-name
-  :xsl-transformers)
+  "This is the name of the cache to use for XSLT transformer templates. Templates are thread
+  safe but transformer instances are not.
+  http://www.onjava.com/pub/a/onjava/excerpt/java_xslt_ch5/?page=9"
+  :xsl-transformer-templates)
 
 (defn context->xsl-transformer-cache
   [context]
   (get-in context [:system :caches xsl-transformer-cache-name]))
 
-(defn- xsl->transformer
-  "Returns the xsl transformer for the given xsl file"
+(defn- xsl->transformer-template
+  "Returns the xsl transformer template for the given xsl file"
   [xsl]
   (let [xsl-resource (new StreamSource (io/file xsl))
         factory (TransformerFactory/newInstance)]
-    (.newTransformer factory xsl-resource)))
+    (.newTemplates factory xsl-resource)))
 
 (defn transform
   "Transforms the given xml by appling the given xsl"
   [context xml xsl]
-  (let [transformer (cache/cache-lookup
-                      (context->xsl-transformer-cache context) xsl #(xsl->transformer xsl))
+  (let [^Templates template (cache/cache-lookup
+                              (context->xsl-transformer-cache context)
+                              xsl
+                              #(xsl->transformer-template xsl))
+        transformer (.newTransformer template)
         source (new StreamSource (new StringReader xml))
         result (new StreamResult (new StringWriter))]
     (.transform transformer source result)
     (.toString (.getWriter result))))
+
