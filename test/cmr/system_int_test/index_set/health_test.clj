5be29178f905a6c359a902204730e9799755891c
Yonggang Liu <ygliuvt@gmail.com>
2014-10-01 11:28:19 -0400

    Updated test.

--- test/cmr/system_int_test/index_set/health_test.clj
+++ test/cmr/system_int_test/index_set/health_test.clj
@@ -10,23 +10,9 @@
   (testing "good health"
     (let [response (client/get (url/index-set-health-url)
                                {:accept :json
-                                :query-params {:token "mock-echo-system-token"}
                                 :connection-manager (url/conn-mgr)})
           health (json/decode (:body response) true)
           {:keys [elastic_search echo-rest]} health]
       (is (= 200 (:status response)))
       (is (some #{elastic_search} ["green" "yellow"]))
-      (is (= "ok" echo-rest))))
-
-  (testing "bad health"
-    (let [response (client/get (url/index-set-health-url)
-                               {:accept :json
-                                :throw-exceptions false
-                                :connection-manager (url/conn-mgr)})
-          health (json/decode (:body response) true)
-          {:keys [elastic_search echo-rest]} health]
-      (is (= 503 (:status response)))
-      (is (some #{elastic_search} ["green" "yellow"]))
-      (is (= "401 UNAUTHORIZED" echo-rest)))))
-
-
+      (is (= "ok" echo-rest)))))
