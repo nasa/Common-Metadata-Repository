fe1ec6a28bb625a1016dca62007feb442db60c76
Yonggang Liu <ygliuvt@gmail.com>
2014-10-01 12:49:26 -0400

    Addressed review comments.

--- test/cmr/system_int_test/index_set/health_test.clj
+++ test/cmr/system_int_test/index_set/health_test.clj
@@ -11,8 +11,8 @@
     (let [response (client/get (url/index-set-health-url)
                                {:accept :json
                                 :connection-manager (url/conn-mgr)})
-          health (json/decode (:body response) true)
-          {:keys [elastic_search echo-rest]} health]
-      (is (= 200 (:status response)))
-      (is (some #{elastic_search} ["green" "yellow"]))
-      (is (= "ok" echo-rest)))))
+          status-code (:status response)
+          health-detail (json/decode (:body response) true)]
+
+      (is (= [200 {:elastic_search {:ok? true}, :echo {:ok? true}}]
+             [status-code health-detail])))))
