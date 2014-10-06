56e4b8656692a4de06888f8c3b91ec56d9e1df87
Yonggang Liu <ygliuvt@gmail.com>
2014-10-06 09:54:58 -0400

    Refactored code.

--- test/cmr/system_int_test/health_test.clj
+++ test/cmr/system_int_test/health_test.clj
@@ -6,105 +6,74 @@
             [cmr.system-int-test.utils.url-helper :as url]
             [cmr.system-int-test.utils.test-environment :as test-env]))
 
+(defn- get-app-health
+  "Returns the status code and health detail of the app with the given health endpoint url"
+  [app-health-url]
+  (let [response (client/get app-health-url
+                             {:accept :json
+                              :throw-exceptions false
+                              :connection-manager (url/conn-mgr)})
+        status-code (:status response)
+        health-detail (json/decode (:body response) true)]
+    [status-code health-detail]))
+
+(def good-metadata-db-health
+  {:ok? true
+   :dependencies {:oracle {:ok? true}, :echo {:ok? true}}})
+
+(def good-index-set-db-health
+  {:ok? true
+   :dependencies {:elastic_search {:ok? true}, :echo {:ok? true}}})
+
 (deftest index-set-health-test
   (testing "index set good health"
-    (let [response (client/get (url/index-set-health-url)
-                               {:accept :json
-                                :throw-exceptions false
-                                :connection-manager (url/conn-mgr)})
-          status-code (:status response)
-          health-detail (json/decode (:body response) true)]
-
-      (is (= [200 {:elastic_search {:ok? true}, :echo {:ok? true}}]
-             [status-code health-detail])))))
+    (is (= [200 {:elastic_search {:ok? true} :echo {:ok? true}}]
+           (get-app-health (url/index-set-health-url))))))
 
 (deftest metadata-db-health-test
   (testing "metadata db good health"
     (test-env/only-with-real-database
-      (let [response (client/get (url/mdb-health-url)
-                                 {:accept :json
-                                  :throw-exceptions false
-                                  :connection-manager (url/conn-mgr)})
-            status-code (:status response)
-            health-detail (json/decode (:body response) true)]
-
-        (is (= [200 {:oracle {:ok? true}, :echo {:ok? true}}]
-               [status-code health-detail]))))))
+      (is (= [200 {:oracle {:ok? true} :echo {:ok? true}}]
+             (get-app-health (url/mdb-health-url)))))))
 
 (deftest indexer-health-test
   (testing "indexer good health"
     (test-env/only-with-real-database
-      (let [response (client/get (url/indexer-health-url)
-                                 {:accept :json
-                                  :throw-exceptions false
-                                  :connection-manager (url/conn-mgr)})
-            status-code (:status response)
-            health-detail (json/decode (:body response) true)]
-
-        (is (= [200 {:elastic_search {:ok? true} :echo {:ok? true}
-                     :metadata-db {:ok? true
-                                   :dependencies {:oracle {:ok? true}, :echo {:ok? true}}}
-                     :index-set {:ok? true
-                                 :dependencies {:elastic_search {:ok? true}, :echo {:ok? true}}}}]
-               [status-code health-detail]))))))
+      (is (= [200 {:elastic_search {:ok? true}
+                   :echo {:ok? true}
+                   :metadata-db good-metadata-db-health
+                   :index-set good-index-set-db-health}]
+             (get-app-health (url/indexer-health-url)))))))
 
 (deftest ingest-health-test
   (testing "ingest good health"
     (test-env/only-with-real-database
-      (let [response (client/get (url/ingest-health-url)
-                                 {:accept :json
-                                  :throw-exceptions false
-                                  :connection-manager (url/conn-mgr)})
-            status-code (:status response)
-            health-detail (json/decode (:body response) true)]
-
-        (is (= [200 {:oracle {:ok? true} :echo {:ok? true}
-                     :metadata-db {:ok? true
-                                   :dependencies {:oracle {:ok? true}, :echo {:ok? true}}}
-                     :indexer
-                     {:ok? true
-                      :dependencies
-                      {:elastic_search {:ok? true} :echo {:ok? true}
-                       :metadata-db {:ok? true :dependencies {:oracle {:ok? true}, :echo {:ok? true}}}
-                       :index-set
-                       {:ok? true :dependencies {:elastic_search {:ok? true}, :echo {:ok? true}}}}}}]
-               [status-code health-detail]))))))
+      (is (= [200 {:oracle {:ok? true}
+                   :echo {:ok? true}
+                   :metadata-db good-metadata-db-health
+                   :indexer {:ok? true
+                             :dependencies {:elastic_search {:ok? true}
+                                            :echo {:ok? true}
+                                            :metadata-db good-metadata-db-health
+                                            :index-set good-index-set-db-health}}}]
+             (get-app-health (url/ingest-health-url)))))))
 
 (deftest search-health-test
   (testing "search good health"
     (test-env/only-with-real-database
-      (let [response (client/get (url/search-health-url)
-                                 {:accept :json
-                                  :throw-exceptions false
-                                  :connection-manager (url/conn-mgr)})
-            status-code (:status response)
-            health-detail (json/decode (:body response) true)]
-
-        (is (= [200 {:echo {:ok? true}
-                     :metadata-db {:ok? true, :dependencies {:oracle {:ok? true}, :echo {:ok? true}}}
-                     :index-set {:ok? true
-                                 :dependencies {:elastic_search {:ok? true}, :echo {:ok? true}}}}]
-               [status-code health-detail]))))))
+      (is (= [200 {:echo {:ok? true}
+                   :internal-metadata-db good-metadata-db-health
+                   :index-set good-index-set-db-health}]
+             (get-app-health (url/search-health-url)))))))
 
 (deftest bootstrap-health-test
   (testing "bootstrap good health"
     (test-env/only-with-real-database
-      (let [response (client/get (url/bootstrap-health-url)
-                                 {:accept :json
-                                  :throw-exceptions false
-                                  :connection-manager (url/conn-mgr)})
-            status-code (:status response)
-            health-detail (json/decode (:body response) true)]
-
-        (is (= [200
-                {:metadata-db {:ok? true, :dependencies {:oracle {:ok? true}, :echo {:ok? true}}}
-                 :internal-meta-db {:ok? true, :dependencies {:oracle {:ok? true}, :echo {:ok? true}}}
-                 :indexer
-                 {:ok? true
-                  :dependencies
-                  {:elastic_search {:ok? true} :echo {:ok? true}
-                   :metadata-db {:ok? true
-                                 :dependencies {:oracle {:ok? true}, :echo {:ok? true}}}
-                   :index-set {:ok? true
-                               :dependencies {:elastic_search {:ok? true}, :echo {:ok? true}}}}}}]
-               [status-code health-detail]))))))
+      (is (= [200 {:metadata-db good-metadata-db-health
+                   :internal-metadata-db good-metadata-db-health
+                   :indexer {:ok? true
+                             :dependencies {:elastic_search {:ok? true}
+                                            :echo {:ok? true}
+                                            :metadata-db good-metadata-db-health
+                                            :index-set good-index-set-db-health}}}]
+             (get-app-health (url/bootstrap-health-url)))))))
