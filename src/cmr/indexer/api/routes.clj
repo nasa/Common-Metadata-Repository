907e5ca876aee32c4f62f585d90ab3f3620a18d2
James Norton <james@element84.com>
2014-10-08 13:41:33 -0400

    CMR-183: Updated more caches to use cmr.common.cache.

--- src/cmr/indexer/api/routes.clj
+++ src/cmr/indexer/api/routes.clj
@@ -11,6 +11,7 @@
             [cheshire.core :as json]
             [cmr.common.log :as log :refer (debug info warn error)]
             [cmr.common.api.errors :as errors]
+            [cmr.common.cache :as cache]
             [cmr.acl.core :as acl]
 
             ;; These must be required here to make multimethod implementations available.
@@ -55,12 +56,14 @@
 
     ;; Querying cache
     (context "/caches" []
+      ;; Get the list of caches
       (GET "/" {:keys [params request-context headers]}
         (let [context (acl/add-authentication-to-context request-context params headers)
-              caches (keys (get-in context [:system :caches]))]
+              caches (map name (keys (get-in context [:system :caches])))]
           (acl/verify-ingest-management-permission context :read)
           {:status 200
-           :body caches}))
+           :body (json/generate-string caches)}))
+      ;; Get the keys for the given cache
       (GET "/:cache-name" {{:keys [cache-name] :as params} :params
                            request-context :request-context
                            headers :headers}
@@ -68,14 +71,14 @@
               cache (get-in context [:system :caches (keyword cache-name)])]
           (acl/verify-ingest-management-permission context :read)
           (when cache
-            (let [result (-> cache
+            (let [result (->> cache
                              :atom
                              deref
                              keys
-                             pr-str)]
+                             (map name))]
               {:status 200
-               :body result}))))
-
+               :body (json/generate-string result)}))))
+      ;; Get the value for the given key for the given cache
       (GET "/:cache-name/:cache-key" {{:keys [cache-name cache-key] :as params} :params
                                       request-context :request-context
                                       headers :headers}
@@ -94,7 +97,7 @@
     (POST "/clear-cache" {:keys [request-context params headers]}
       (let [context (acl/add-authentication-to-context request-context params headers)]
         (acl/verify-ingest-management-permission context :update)
-        (index-svc/clear-cache context))
+        (cache/reset-caches context))
       {:status 200})
 
     (POST "/reindex-provider-collections"
