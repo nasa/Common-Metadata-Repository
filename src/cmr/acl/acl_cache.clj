e37dffc31a1e6987d53eb3ebc7f0ca9af4222096
James Norton <james@element84.com>
2014-10-08 13:44:31 -0400

    CMR-183: Updated call to cmr.common.cache and removed debug output.

--- src/cmr/acl/acl_cache.clj
+++ src/cmr/acl/acl_cache.clj
@@ -28,10 +28,11 @@
   to keep the cache fresh. This will throw an exception if there is a problem fetching ACLs. The
   caller is responsible for catching and logging the exception."
   [context]
-  (let [acl-cache (context->acl-cache context)]
+  (let [acl-cache (context->acl-cache context)
+        updated-acls (echo-acls/get-acls-by-type context "CATALOG_ITEM")]
     (cache/update-cache
       acl-cache
-      (fn [_] {:acls (echo-acls/get-acls-by-type context "CATALOG_ITEM")}))))
+      #(assoc % :acls updated-acls))))
 
 (defn get-acls
   "Gets the current cached acls."
