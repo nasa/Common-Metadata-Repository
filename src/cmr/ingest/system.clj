09667b62e066fce73291eaff1b204c9934db51b0
Chris Durbin <christopher.d.durbin@nasa.gov>
2014-10-09 11:02:52 -0400

    CMR-610: Configure applications to use VIPs and handle a root context other than /

--- src/cmr/ingest/system.clj
+++ src/cmr/ingest/system.clj
@@ -12,7 +12,8 @@
             [cmr.ingest.config :as config]
             [cmr.ingest.services.jobs :as ingest-jobs]
             [cmr.common.jobs :as jobs]
-            [cmr.oracle.connection :as oracle]))
+            [cmr.oracle.connection :as oracle]
+            [cmr.common.config :as cfg]))
 
 (def
   ^{:doc "Defines the order to start the components."
@@ -23,6 +24,8 @@
   "Required for jobs"
   (atom nil))
 
+(def relative-root-url (cfg/config-value-fn :ingest-relative-root-url ""))
+
 (defn create-system
   "Returns a new instance of the whole application."
   ([]
@@ -32,7 +35,8 @@
               :web (web/create-web-server (transmit-config/ingest-port) routes/make-api)
               :db (oracle/create-db (config/db-spec connection-pool-name))
               :zipkin (context/zipkin-config "Ingest" false)
-              :scheduler (jobs/create-clustered-scheduler `system-holder ingest-jobs/jobs)}]
+              :scheduler (jobs/create-clustered-scheduler `system-holder ingest-jobs/jobs)
+              :relative-root-url (relative-root-url)}]
      (transmit-config/system-with-connections sys [:metadata-db :indexer :echo-rest]))))
 
    (defn start
@@ -60,3 +64,4 @@
                                   (reverse component-order))]
        (info "System stopped")
        stopped-system))
+
