062476725b2fdd3dd875b39c476e676745a1c6c6
James Norton <james@element84.com>
2014-10-15 00:33:02 -0400

    CMR-1022: Added collection platform characteristics and sensor technique to umm.

---
+++ umm-lib/src/cmr/umm/echo10/collection/characteristic.clj
@@ -0,0 +1,35 @@
+(ns cmr.umm.echo10.collection.characteristic
+  (:require [clojure.data.xml :as x]
+            [cmr.common.xml :as cx]
+            [cmr.umm.collection :as c]
+            [cmr.umm.echo10.collection.sensor :as sensor]))
+
+(defn xml-elem->Characteristic
+  [char-elem]
+  (let [name (cx/string-at-path char-elem [:Name])
+        description (cx/string-at-path char-elem [:Description])
+        data-type (cx/string-at-path char-elem [:DataType])
+        unit (cx/string-at-path char-elem [:Unit])
+        value (cx/string-at-path char-elem [:Value])]
+    (c/->Characteristic name description data-type unit value)))
+
+(defn xml-elem->Characteristics
+  [platform-element]
+  (seq (map xml-elem->Characteristic
+            (cx/elements-at-path
+              platform-element
+              [:Characteristics :Characteristic]))))
+
+(defn generate-characteristics
+  [characteristics]
+  (when-not (empty? characteristics)
+    (x/element
+      :Characteristics {}
+      (for [characteristic characteristics]
+        (let [{:keys [name description data-type unit value]} characteristic]
+          (x/element :Characteristic {}
+                     (x/element :Name {} name)
+                     (x/element :Description {} description)
+                     (x/element :DataType {} data-type)
+                     (x/element :Unit {} unit)
+                     (x/element :Value {} value)))))))
