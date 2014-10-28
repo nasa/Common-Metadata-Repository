86c75f3722d9334306db36f19e1276aa51db7568
Jason Gilman <jason@element84.com>
2014-10-28 15:58:36 -0400

    CMR-1055: Adding additional logging.

--- dev-system/src/cmr/dev_system/control.clj
+++ dev-system/src/cmr/dev_system/control.clj
@@ -72,12 +72,14 @@
       (debug "dev system /reset")
       (doseq [[service-name reset-fn] service-reset-fns]
         (reset-fn (app-context system service-name)))
+      (debug "dev system /reset complete")
       {:status 200})
 
     (POST "/clear-cache" []
       (debug "dev system /clear-cache")
       (doseq [[service-name clear-cache-fn] service-clear-cache-fns]
         (clear-cache-fn (app-context system service-name)))
+      (debug "dev system /clear-cache complete")
       {:status 200})
 
     (POST "/stop" []
