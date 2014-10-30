486caee7c857444e49bd2c65e3df3fbf3f63e935
Prem Gazula <prem.g8@gmail.com>
2014-10-30 10:17:32 -0400

    CMR-599: removed result_format.clj validator per feedback.

--- search-app/src/cmr/search/validators/validation.clj
+++ search-app/src/cmr/search/validators/validation.clj
@@ -2,10 +2,26 @@
   "Defines protocols and functions to validate conditions"
   (:require [cmr.search.models.query :as qm]
             [cmr.spatial.validation :as spatial-validation]
-            [cmr.search.validators.result-format :as rfmt]
+            [clojure.set]
+            [cmr.common.mime-types :as mt]
 
             ;; Must be required to be available.
             [cmr.spatial.geodetic-ring-validations]))
+
+;; Records search result supported formats by concept.
+;; TODO - its hard to discover supported search result formats are maintained here.
+;; Find a better way.
+(def concept-type->supported-result-formats
+  {:collection #{:xml, :json, :echo10, :dif, :atom, :iso19115, :csv, :kml}
+   :granule #{:xml, :json, :echo10, :atom, :iso19115, :csv, :kml}})
+
+(defn validate-result-format
+  "Validate requested search result format."
+  [concept-type result-format]
+  (let [mime-type (mt/format->mime-type result-format)
+        valid-mime-types (set (map #(mt/format->mime-type %)
+                                   (concept-type->supported-result-formats concept-type)))]
+    (mt/validate-request-mime-type mime-type valid-mime-types)))
 
 (defprotocol Validator
   "Defines the protocol for validating query conditions.
@@ -18,7 +34,7 @@
   cmr.search.models.query.Query
   (validate
     [{:keys [concept-type result-format condition]}]
-    (rfmt/validate concept-type result-format)
+    (validate-result-format concept-type result-format)
     (validate condition))
 
   cmr.search.models.query.ConditionGroup
