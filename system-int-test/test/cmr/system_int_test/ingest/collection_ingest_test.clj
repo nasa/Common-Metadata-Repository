9d2e99e7e61b348b257abf4f7120b3ccc230c00d
Yonggang Liu <ygliuvt@gmail.com>
2014-11-12 11:23:00 -0500

    CMR-1078: Addressed review comments.

--- system-int-test/test/cmr/system_int_test/ingest/collection_ingest_test.clj
+++ system-int-test/test/cmr/system_int_test/ingest/collection_ingest_test.clj
@@ -49,17 +49,32 @@
       (is (apply = (map :concept-id created-concepts)))
       (is (= (range 1 (inc n)) (map :revision-id created-concepts))))))
 
+(defn- verify-format-ingested-and-searchable
+  [identifier coll-format]
+  (let [coll (d/ingest "PROV1"
+                       (dc/collection {:entry-id "S1"
+                                       :short-name "S1"
+                                       :version-id "V1"
+                                       :entry-title "ET1"
+                                       :long-name "L4"
+                                       :summary (name coll-format)})
+                       coll-format)
+        {:keys [concept-id revision-id]} coll]
+
+    (when (seq @identifier)
+      (is (= {:concept-id concept-id :revision-id (dec revision-id)}
+             @identifier)))
+
+    (index/refresh-elastic-index)
+    (is (= 1 (:hits (search/find-refs :collection {:keyword (name coll-format)}))))
+
+    (swap! identifier assoc :concept-id concept-id :revision-id revision-id)))
+
 (deftest update-collection-with-different-formats-test
   (testing "update collection in different formats ..."
-    (let [collection-formats [:echo10 :dif :iso19115 :iso-smap]
-          colls (map (partial d/ingest "PROV1" (dc/collection {:entry-id "S1"
-                                                               :short-name "S1"
-                                                               :version-id "V1"
-                                                               :entry-title "ET1"
-                                                               :long-name "L4"}))
-                     (shuffle collection-formats))]
-      (is (apply = (map :concept-id colls)))
-      (is (= (range 1 (inc (count collection-formats))) (map :revision-id colls))))))
+    (let [identifier (atom {})]
+      (doseq [coll-format [:echo10 :dif :iso19115 :iso-smap]]
+        (verify-format-ingested-and-searchable identifier coll-format)))))
 
 ;; Verify ingest behaves properly if empty body is presented in the request.
 (deftest empty-collection-ingest-test
