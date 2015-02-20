4c38a37dbe3fd5b616a425bf30b6fb6c1cf1b154
Chris Durbin <christopher.d.durbin@nasa.gov>
2015-02-20 13:34:27 -0500

    CMR-1195: Refactor code

--- metadata-db-app/src/cmr/metadata_db/services/concept_constraints.clj
+++ metadata-db-app/src/cmr/metadata_db/services/concept_constraints.clj
@@ -1,50 +1,56 @@
 (ns cmr.metadata-db.services.concept-constraints
   "Functions for enforcing constraint checks just after a concept has been saved."
-  (:require [cmr.metadata-db.services.messages :as msg]
-            [cmr.common.concepts :as cc]
-            [clojure.set :as set]
-            [cmr.common.services.errors :as errors]
-            [cmr.common.date-time-parser :as p]
-            [cmr.common.util :as util]
-            [cmr.metadata-db.services.concept-service :as cs]))
+  (:require [cmr.common.services.errors :as errors]
+            [cmr.metadata-db.services.concept-service :as cs]
+            [clojure.string :as str]))
 
-(defn- max-revision-id
+(defn- get-max-revision-id
   "Get max revisison ID from a vector of concept maps."
   [concepts]
   (apply max (map :revision-id concepts)))
 
-(defn- remove-old-revision-ids
+(defn- keep-max-revision-id
   "Remove all but the most recent revision id from a vector of concept maps."
   [concepts]
-  (filter #(= (max-revision-id concepts) (:revision-id %)) concepts))
+  (let [max-revision-id (get-max-revision-id concepts)]
+    (filter #(= max-revision-id (:revision-id %)) concepts)))
 
 (comment
   (def concepts-found
-    [{:concept-id "C1" :revision-id 1 :entry-title "E1" :deleted 0}
-     {:concept-id "C1" :revision-id 2 :entry-title "E1" :deleted 0}
-     {:concept-id "C2" :revision-id 1 :entry-title "E1" :deleted 0}
-     {:concept-id "C2" :revision-id 2 :entry-title "E1" :deleted 1}
-     {:concept-id "C3" :revision-id 1 :entry-title "E1" :deleted 0}])
+    [{:concept-id "C1" :revision-id 1 :entry-title "E1" :deleted false}
+     {:concept-id "C1" :revision-id 2 :entry-title "E1" :deleted false}
+     {:concept-id "C2" :revision-id 1 :entry-title "E1" :deleted false}
+     {:concept-id "C2" :revision-id 2 :entry-title "E1" :deleted true}
+     {:concept-id "C3" :revision-id 1 :entry-title "E1" :deleted false}])
+
+  (keep-latest-non-deleted-concepts concepts-found)
   )
 
-(defn remove-old-revisions-and-tombstones
-  "Helper function used to filter out all but the latest revision ID for a concept ID.  Filters out
-  tombstones as well.  Takes a vector of concepts."
+(defn keep-latest-non-deleted-concepts
+  "Helper function used to filter out all but the latest revision ID for a concept ID. Filters out
+  tombstones as well. Takes a vector of concepts."
   [concepts]
   (->> concepts
        (group-by :concept-id)
        (map val)
-       (map remove-old-revision-ids)
-       flatten
-       (filter #(= 0 (:deleted %)))))
+       (mapcat keep-max-revision-id)
+       (filter (complement :deleted))))
 
 (defn entry-title-unique-constraint
-  "Verifies that there is only one valid collection with the "
+  "Verifies that there is only one valid collection with the entry-title which matches the
+  entry-title of the provided concept."
   [db concept]
-  (let [concepts (cs/find-concepts db {:concept-type :collection
-                                       :provider-id (:provider-id concept)
-                                       :entry-title (:entry-title concept)})
-        num-concepts (remove-old-revisions-and-tombstones concepts)]
-    (if (= 1 num-concepts)
-      nil
-      (str "Entry-title must be unique.  Found " num-concepts "concepts with the same entry-title"))))
+  (let [concepts (->>
+                   (cs/find-concepts db {:concept-type :collection
+                                         :provider-id (:provider-id concept)
+                                         :entry-title (:entry-title concept)})
+
+                   keep-latest-non-deleted-concepts)
+        num-concepts (count concepts)]
+    (when (not= 1 num-concepts)
+      (if (> 1 num-concepts)
+        (format "Entry-title must be unique. Found [%d] concepts with the same entry-title. The concept-ids are [%s]."
+                num-concepts
+                (str/join ", " (map :concept-id concepts)))
+        ;; May want to throw a service error here instead of return the string message
+        "Unexpected error. Concept appears to not have saved correctly."))))
--- metadata-db-app/test/cmr/metadata_db/test/services/concept_constraints.clj
+++ metadata-db-app/test/cmr/metadata_db/test/services/concept_constraints.clj
@@ -5,40 +5,40 @@
 
 (def invalid-concepts
   [{:provider-id "PROV1" :metadata "xml here" :format "echo10" :concept-type :collection
-    :concept-id "C1" :revision-id 1 :entry-title "E1" :deleted 0}
+    :concept-id "C1" :revision-id 1 :entry-title "E1" :deleted false}
    {:provider-id "PROV1" :metadata "xml here" :format "echo10" :concept-type :collection
-    :concept-id "C1" :revision-id 2 :entry-title "E1" :deleted 0}
+    :concept-id "C1" :revision-id 2 :entry-title "E1" :deleted false}
    {:provider-id "PROV1" :metadata "xml here" :format "echo10" :concept-type :collection
-    :concept-id "C2" :revision-id 1 :entry-title "E1" :deleted 0}
+    :concept-id "C2" :revision-id 1 :entry-title "E1" :deleted false}
    {:provider-id "PROV1" :metadata "xml here" :format "echo10" :concept-type :collection
-    :concept-id "C2" :revision-id 2 :entry-title "E1" :deleted 1}
+    :concept-id "C2" :revision-id 2 :entry-title "E1" :deleted true}
    {:provider-id "PROV1" :metadata "xml here" :format "echo10" :concept-type :collection
-    :concept-id "C3" :revision-id 1 :entry-title "E1" :deleted 0}
+    :concept-id "C3" :revision-id 1 :entry-title "E1" :deleted false}
    {:provider-id "PROV1" :metadata "xml here" :format "echo10" :concept-type :collection
-    :concept-id "C4" :revision-id 1 :entry-title "E1" :deleted 0}
+    :concept-id "C4" :revision-id 1 :entry-title "E1" :deleted false}
    {:provider-id "PROV1" :metadata "xml here" :format "echo10" :concept-type :collection
-    :concept-id "C4" :revision-id 2 :entry-title "E1" :deleted 1}
+    :concept-id "C4" :revision-id 2 :entry-title "E1" :deleted true}
    {:provider-id "PROV1" :metadata "xml here" :format "echo10" :concept-type :collection
-    :concept-id "C4" :revision-id 20 :entry-title "E1" :deleted 0}])
+    :concept-id "C4" :revision-id 20 :entry-title "E1" :deleted false}])
 
 (def valid-concepts
   [{:provider-id "PROV1" :metadata "xml here" :format "echo10" :concept-type :collection
-    :concept-id "C1" :revision-id 1 :entry-title "E1" :deleted 0}
+    :concept-id "C1" :revision-id 1 :entry-title "E1" :deleted false}
    {:provider-id "PROV1" :metadata "xml here" :format "echo10" :concept-type :collection
-    :concept-id "C1" :revision-id 2 :entry-title "E1" :deleted 1}
+    :concept-id "C1" :revision-id 2 :entry-title "E1" :deleted true}
    {:provider-id "PROV1" :metadata "xml here" :format "echo10" :concept-type :collection
-    :concept-id "C2" :revision-id 1 :entry-title "E1" :deleted 0}
+    :concept-id "C2" :revision-id 1 :entry-title "E1" :deleted false}
    {:provider-id "PROV1" :metadata "xml here" :format "echo10" :concept-type :collection
-    :concept-id "C2" :revision-id 2 :entry-title "E1" :deleted 1}
+    :concept-id "C2" :revision-id 2 :entry-title "E1" :deleted true}
    {:provider-id "PROV1" :metadata "xml here" :format "echo10" :concept-type :collection
-    :concept-id "C3" :revision-id 1 :entry-title "E1" :deleted 0}
+    :concept-id "C3" :revision-id 1 :entry-title "E1" :deleted false}
    ;; Use this in integration test to make sure entry-title is successfully filtered on
    ; {:provider-id "PROV1" :metadata "xml here" :format "echo10" :concept-type :collection
    ; :concept-id "C4" :revision-id 1 :entry-title "E2" :deleted 0}
    ])
 
-(deftest verify-remove-old-revisions-and-tombstones
+(deftest verify-keep-latest-non-deleted-concepts
   ;; Make sure that filtering out old revisions and tombstones works as expected.
-  (testing "Verifying remove-old-revisions-and-tombstones returns the correct concepts"
-    (is (= 3 (count (cc/remove-old-revisions-and-tombstones invalid-concepts))))
-    (is (= 1 (count (cc/remove-old-revisions-and-tombstones valid-concepts))))))
+  (testing "Verifying keep-latest-non-deleted-concepts returns the correct concepts"
+    (is (= 3 (count (cc/keep-latest-non-deleted-concepts invalid-concepts))))
+    (is (= 1 (count (cc/keep-latest-non-deleted-concepts valid-concepts))))))
