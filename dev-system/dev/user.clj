92d1d64ba1b1f62b71d3b2a1311968679a33c4e2
Jason Gilman <jason@element84.com>
2015-02-21 19:22:05 -0500

    Fixing problems with running dev system with ECHO environment variable settings.

--- dev-system/dev/user.clj
+++ dev-system/dev/user.clj
@@ -9,11 +9,31 @@
             [cmr.common.config :as config]
             [earth.driver :as earth-viz]
             [common-viz.util :as common-viz]
-            [vdd-core.core :as vdd])
+            [vdd-core.core :as vdd]
+            [cmr.indexer.config :as indexer-config]
+            [cmr.transmit.config :as transmit-config]
+            [cmr.elastic-utils.config :as elastic-config])
   (:use [clojure.test :only [run-all-tests]]
         [clojure.repl]
         [alex-and-georges.debug-repl]
         [cmr.common.dev.capture-reveal]))
+
+(def external-elastic-port 9210)
+
+(def external-echo-port 10000)
+
+(defn external-echo-system-token
+  "Returns the ECHO system token based on the value for ECHO_SYSTEM_READ_TOKEN in the ECHO
+  configuration file.  The WORKSPACE_HOME environment variable must be set in order to find the
+  file.  Returns nil if it cannot extract the value."
+  []
+  (try
+    (->> (slurp (str (System/getenv "WORKSPACE_HOME") "/deployment/primary/config.properties"))
+         (re-find #"\n@ECHO_SYSTEM_READ_TOKEN@=(.*)\n")
+         peek)
+    (catch Exception e
+      (warn "Unable to extract the ECHO system read token from configuration.")
+      nil)))
 
 (def system nil)
 
@@ -33,13 +53,32 @@
    ; :message-queue :external
    })
 
+(defn set-config-by-system-type
+  "Overrides environment variables to setup communication parameters."
+  [system-type]
+
+  ;; Set the default job start delay to avoid jobs kicking off with tests etc.
+  (jobs/set-default-job-start-delay! (* 3 3600))
+
+  (when (= :external (:echo system-type))
+    (transmit-config/set-echo-rest-port! external-echo-port)
+    (transmit-config/set-echo-system-token! (external-echo-system-token))
+    (transmit-config/set-echo-rest-context! "/echo-rest"))
+
+  (if (= :in-memory (:elastic system-type))
+    (elastic-config/set-elastic-port! system/in-memory-elastic-port)
+    (elastic-config/set-elastic-port! external-elastic-port))
+
+  (if (= :in-memory (:message-queue system-type))
+    (indexer-config/set-indexing-communication-method! "http")
+    (indexer-config/set-indexing-communication-method! "queue")))
+
 (defn start
   "Starts the current development system."
   []
   (config/reset-config-values)
 
-  ;; Set the default job start delay to avoid jobs kicking off with tests etc.
-  (jobs/set-default-job-start-delay! (* 3 3600))
+  (set-config-by-system-type system-type)
 
   (let [s (system/create-system system-type)]
     (alter-var-root #'system
--- dev-system/src/cmr/dev_system/runner.clj
+++ dev-system/src/cmr/dev_system/runner.clj
@@ -3,12 +3,14 @@
   (:require [cmr.dev-system.system :as system]
             [clojure.tools.cli :refer [cli]]
             [clojure.string :as string]
+            [cmr.common.config :as config]
             [cmr.common.log :refer (debug info warn error)])
   (:gen-class))
 
 (defn -main
   "Starts the App."
   [& args]
+  (config/print-all-configs-docs)
   (let [system (system/create-system {:elastic :in-memory
                                       :echo :in-memory
                                       :db :in-memory
