d6e9a53dc96db8150655daf464fe3eb6e881f3c9
Chris Durbin <christopher.d.durbin@nasa.gov>
2015-03-13 14:56:56 -0400

    CMR-1296: Add a single script to be called by the CI server to manage building and testing the CMR applications.

---
+++ dev-system/support/build-and-test-ci.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This script is used to manage building and testing the CMR applications within the continuous
+# integration (CI) environment. The intent is to never need to modify the configuration of the CI
+# server. Instead the CI server will simply call this script. The script should be run from the cmr
+# root directory, ie, ./dev-system/support/build-and-test-ci.sh
+
+lein modules do clean, install
+cd search-app
+lein with-profile docs generate-docs
+cd ../dev-system
+./support/build-and-run.sh
+cd ..
+CMR_ELASTIC_PORT=9206 lein modules test-out
+curl -XPOST http://localhost:2999/stop
