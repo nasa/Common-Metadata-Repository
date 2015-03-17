bd4d63efb9ef63229459705691fefa02eced5988
Upendra Dadi <upendra.dadi@nasa.gov>
2015-03-17 09:29:05 -0400

    CMR-1188: Addressed the code review comments. Did some refactoring. Added a test to check if clojure tiles match with ruby tiles

--- spatial-lib/src/cmr/spatial/tile.clj
+++ spatial-lib/src/cmr/spatial/tile.clj
@@ -1,7 +1,23 @@
 (ns cmr.spatial.tile
   "This contains functions to work with tiles defined in various 2D coordinate 
-  system grids such as Modis Integerized Sinusoidal Grid."
+  system grids such as Modis Integerized Sinusoidal Grid.
+  
+  The tile geometry is computed as necessary. Currently only computation of MODIS Sinusoidal tiles
+  is implemneted. Computation of tile coordinates for a Modis tile follows the steps below. 
+  (Please See: https://wiki.earthdata.nasa.gov/display/CMR/Computation+of+MODIS+Tile+Geometry) 
+  1) Map the tile to an equivalent tile in the upper-right quadrant(UR).
+  2) Find if the mapped tile is a fictional tile or a real tile or a degenerate tile. Ignore 
+     fictional and degenerate tiles. Degenerate tiles have only one vertex which is a real point. 
+     All the remaining points of the tile are fictional points.
+  3) Find the coordinates of the four vertices of the rectangle corresponding to the mapped tile.
+  4) Densify each of the four segments of the tile mapped.
+  5) Remove fictional points on each of the edges if there are any.
+  6) Add the edge-point if the edge has fictional points on one side and real points on another.
+  7) Map the densified points back to its original quadrant.
+  8) Convert the points from planar coordinates to geodetic coordinates. Convert from radians to 
+     degrees."
   (:require [cmr.spatial.math :refer :all]
+            [primitive-math]
             [cmr.spatial.geodetic-ring :as gr]
             [cmr.spatial.ring-relations :as rr]
             [clojure.java.io :as io]
@@ -10,21 +26,26 @@
             [cmr.spatial.point :as p]
             [cmr.spatial.line-segment :as s]))
 
-(def ^:const ^:private ^:int H
-  "The number of tiles along east-west in Modis Sinusoidal Grid"
+  (primitive-math/use-primitive-operators)
+  
+(def ^:const ^:private ^:int NUM_HORIZONTAL_TILES
+  "The number of tiles along east-west in Modis Sinusoidal Grid."
   36)
 
-(def ^:const ^:private ^:int V
-  "The number of tiles along north-south in Modis Sinusoidal Grid" 
+(def ^:const ^:private ^:int NUM_VERTICAL_TILES
+  "The number of tiles along north-south in Modis Sinusoidal Grid." 
   18)
 
-(def ^:const ^:private ^:float SIZE
-  "Size of each of the two sides of a tile"
-  (/ TAU H))
+(def ^:const ^:private ^:double TILE_SIZE
+  "Size of each of the four edges of a tile projected off a unit sphere. The 
+  need for radius of earth in computations, which is cancelled out anyway, is 
+  avoided by using a unit sphere. The units of this parameter is radians."
+  (/ TAU (double NUM_HORIZONTAL_TILES)))
 
-(def ^:const ^:private ^:float DENSIFICATION_DISTANCE
-  ""
-  (+ (/ SIZE 20) 0.00000001))
+(def ^:const ^:private ^:int NUM_DENSIFICATION_SEGMENTS
+  "The number of segments into which each edge of a tile is partitioned in 
+  the projected coordinate system during densification"
+  20)
   			
 ;;Modis Sinusoidal Tile
 (defrecord ModisSinTile
@@ -37,31 +58,33 @@
    geometry])
 
 (defn intersects?
-  "Returns true if geometry of the tile intersects with the given geometry. geometry could
-   be of any geometric type including point, line, polygon, mbr and ring"
+  "Returns true if geometry of the tile intersects with the given geometry. Geometry could
+   be of any type including point, line, polygon, mbr and ring"
   [tile geometry]
   (rel/intersects-ring? geometry (:geometry tile)))
 
 (defn- find-tile-quadrant
-  "The quadrant in which a tile with the given coordinates falls."
+  "The quadrant in which a tile with the given tile coordinates falls."
   [h v]
-  (case (vector (< h (/ H 2)) (< v (/ V 2)))
+  (case [(< h (/ NUM_HORIZONTAL_TILES 2)) (< v (/ NUM_VERTICAL_TILES 2))]
     [false true] :ur
     [true true] :ul
     [true false] :ll
     [false false] :lr))
 
-(defn- map-tile-to-ur-quadrant
-  "Map the given tile to the 'equivalent' tile in the upper right quadrant."
+(defn- tile->ur-quadrant-tile
+  "Map the given tile to the 'equivalent' tile in the upper right quadrant. The symmetry 
+  of the tiles along the equator and central meridian is used to reduce the number of 
+  conditional checks during computation of coordinates along tile edges."
   [h v]
   (case (find-tile-quadrant h v)
     :ur [h v]
-    :ul [(- H h 1) v]
-    :ll [(- H h 1) (- V v 1)]
-    :lr [h (- V v 1)]))
+    :ul [(- NUM_HORIZONTAL_TILES h 1) v]
+    :ll [(- NUM_HORIZONTAL_TILES h 1) (- NUM_VERTICAL_TILES v 1)]
+    :lr [h (- NUM_VERTICAL_TILES v 1)]))
 
-(defn- map-tile-point-backwards
-  "Maps a point on the edge of a tile in the upper-right quadrant back to its original quadrant."
+(defn- ur-quadrant-point->point
+  "Maps a point in the upper-right quadrant back to its original quadrant."
   [[x y] quadrant]
   (case quadrant
     :ur [x y]
@@ -69,154 +92,163 @@
     :ll [(- x) (- y)]
     :lr [x (- y)]))
 
-(defn- find-tile-vertices
-  "Find the coordinates of the four vertices of a tile"
+(defn- tile->corner-vertices
+  "Find the coordinates of the four vertices of a tile. The origin of the coordinate system 
+  used is at the centre of the grid, i.e. intersection of equator and meridian and NOT the 
+  top left corner. The function returns a vector of tuples, each tuple being a 
+  pair of coordinates for a vertex. The vertices are put in anti-clockwise order with 
+  lower-right vertex of the tile as the first vertex."
   [h v]
-  (let [x_min (* (- h (/ H 2)) SIZE)
-        y_min (* (- (/ V 2) (+ v 1)) SIZE)
-        x_max (+ x_min SIZE)
-        y_max (+ y_min SIZE)]
-    [[x_max y_min] [x_max y_max] [x_min y_max] [x_min y_min]]))
+  (let [x-min (* (double (- h (/ NUM_HORIZONTAL_TILES 2))) TILE_SIZE)
+        y-min (* (double (- (/ NUM_VERTICAL_TILES 2) (+ v 1))) TILE_SIZE)
+        x-max (+ x-min TILE_SIZE)
+        y-max (+ y-min TILE_SIZE)]
+    [[x-max y-min] [x-max y-max] [x-min y-max] [x-min y-min]]))
 
-(defn- max-x
-  "Determine the largest x coordinate for a given y 
-  coordinate so that [x  y] is not a fictional point"
+(defn- max-x-for-y
+  "Determine the largest x coordinate for a given y coordinate so that 
+  [max-x-for-y  y] is an edge-point"
   [y]
-  (* (/ TAU 2) (cos y)))
+  (* (/ TAU 2.) (cos y)))
 
-(defn- max-y
+(defn- bound
+  "Return value if absolute value of value is less than max-value otherwise return max-value
+  if value is greater than 0 and -max-value if value is less than 0."
+  [value max-value]
+  (if (< (abs value) max-value) 
+         value
+         (if (< value 0.) (- max-value) max-value)))
+
+(defn- max-y-for-x
   "Determine the largest y coordinate for a given x 
-  coordinate so that [x  y] is not a fictional point"
+  coordinate so that [x  max-y-for-x] is not a fictional point"
   [x]
-  (acos (/ x (/ TAU 2))))
+  (acos (bound (/ x (/ TAU 2.)) 1.)))
 
 (defn- fictional-point?
   "Determine if a point in the Sinusoidal grid maps to a real point on the earth"
   [x y]
-  (or (> x (max-x y)) (> y (max-y x))))
+  (or (> x (max-x-for-y y)) (> y (max-y-for-x x))))
 
-(defn- degenerate_tile?
+(defn- degenerate-tile?
   "Determine if a tile is an edge case, i.e. only one vertex of the tile is not a 
   fictional point and rest of the points in the tile are fictional points."
   [y1 x4]
-  (= x4 (max-x y1)))
+  (= x4 (max-x-for-y y1)))
 
 (defmulti add-edge-point
   "Method to find 'edge-point' given a line segment joining two points exactly one of 
   which is a fictional point (in Sinusoidal projection). Edge points lie on 
-  anti-meridian and they divide fictional points from real points on a tile edge."
-  (fn [[x1 y1] [x2 y2] densified_segment] 
+  anti-meridian and they divide fictional points from real points on a tile edge.
+  The function arguments are coordinates of the two points and a vector of points holding the
+  densified segement between the two points from which all fictional points are removed."
+  (fn [[x1 y1] [x2 y2] densified-segment] 
       (cond
          (= y1 y2) (if (< x1 x2) :left-right :right-left)
-         (= x1 x2) (if (< y1 y2) :bottom-top :top-bottom))))
+         (= x1 x2) (if (< y1 y2) :bottom-top :top-bottom)
+         :else (throw (Exception. "Either x or y-coordinates of the given points must be equal")))))
 
 (defmethod add-edge-point :left-right
-  [p1 [x2 y2] densified_segment]
-  (conj densified_segment [(max-x y2) y2]))
+  [p1 [x2 y2] densified-segment]
+  (concat densified-segment [[(max-x-for-y y2) y2]]))
 
 (defmethod add-edge-point :bottom-top
-  [[x1 y1] p2 densified_segment]
-  (let [max_y1 (max-y x1)]
+  [[x1 y1] p2 densified-segment]
+  (let [max-y1 (max-y-for-x x1)]
     ;;The if condition avoids duplicate points which occur when only real point on the line segment 
     ;;is an end-point. Same is true for the next function.
-    (if (= y1 max_y1) densified_segment (conj densified_segment [x1 max_y1]))))
+    (if (approx= y1 max-y1 DELTA) 
+      densified-segment 
+      (concat densified-segment [[x1 max-y1]]))))
 
 (defmethod add-edge-point :right-left
-  [p1 [x2 y2] densified_segment]
-  (let [max_x2 (max-x y2)]
-    (if (= x2 max_x2) densified_segment (into [[max_x2 y2]] densified_segment))))
+  [p1 [x2 y2] densified-segment]
+  (let [max-x2 (max-x-for-y y2)]
+    (if (approx= x2 max-x2 DELTA)
+      densified-segment 
+      (concat [[max-x2 y2]] densified-segment))))
 
 (defmethod add-edge-point :top-bottom
-  [[x1 y1] p2 densified_segment]
-  (into [[x1 (max-y x1)]] densified_segment))
+  [[x1 y1] p2 densified-segment]
+  (concat [[x1 (max-y-for-x x1)]] densified-segment))
 
 (defn- coord
-  "Find (x or y) coordinate of a point at a distance i/n of df from coordinate c.
-  This is used to add (n - 1) new points along the line segment between two 
-  points with a distance of df between them"
-  [c i df n]
-  (float (+ c (* df (/ i n)))))
+  "Find (x or y) coordinate of a point at a distance i/num-points of dist from the coordinate 
+  ord along the axis of ord. This is used to add (num-points - 1) new points along the line segment 
+  between two points."
+  [ord i dist num-points]
+  (+ ord (* dist (/ (double i) (double num-points)))))
 
 (defn- densify-segment
-  "Densify a line segment joining two input points"
+  "Densify a line segment joining two input points."
   [[x1 y1] [x2 y2]]
-  (let [df_x (- x2 x1)
-        df_y (- y2 y1)
-        df_dist (sqrt (+ (* df_x df_x) (* df_y df_y)))
-        npts (inc (int (/ df_dist DENSIFICATION_DISTANCE)))
-        inter_points (map #(vector (coord x1 % df_x npts) (coord y1 % df_y npts)) (range npts))]
-    (into (vec inter_points) [[x2 y2]])))
+  (let [df-x (- x2 x1)
+        df-y (- y2 y1)
+        num-segments NUM_DENSIFICATION_SEGMENTS]
+    (map #(vector (coord x1 % df-x num-segments) (coord y1 % df-y num-segments)) 
+         (range (inc num-segments)))))
 
 (defn- densify-tile-edge
-  "Densify a tile edge, remove fictional points and add edge points"
+  "Densify a tile edge, remove fictional points and add edge point if necessary"
   [[p1 p2]]
-  (let [densified_segment (->> (densify-segment p1 p2)
+  (let [densified-segment (->> (densify-segment p1 p2)
                                drop-last
-                               (remove #(apply fictional-point? %))
-                               vec)]
+                               (remove (partial apply fictional-point?)))]
     (if (not= (apply fictional-point? p1) (apply fictional-point? p2))
-      (add-edge-point p1 p2 densified_segment) densified_segment)))
+      (add-edge-point p1 p2 densified-segment) densified-segment)))
 
 (defn- densify-tile
   "Densify the bounding rectangle corresponding to a tile by adding new points and 
   removing fictional points. The input vertices are assumed to be in anti-clockwise order."
   [vertices]
-  (reduce into [] (map densify-tile-edge (partition 2 1 [(first vertices)] vertices)))) 
+  (mapcat densify-tile-edge (partition 2 1 [(first vertices)] vertices)))
   
 (defn- planar->geodetic
   "Maps a point in the Sinusoidal projection to a corresponding geodetic coordinate"
   [x y]
-  (let [authalic_lat y
-        cos_lat (cos y)
-        authalic_lon (if (= cos_lat 0) 0 (/ x cos_lat))]
-    (map degrees [authalic_lon authalic_lat])))
+  (let [authalic-lat y
+        cos-lat (cos y)
+        authalic-lon (if (= cos-lat 0) 0 (/ x cos-lat))]
+    [(bound (degrees authalic-lon) 180.) (bound (degrees authalic-lat) 90.)]))
 
-(defn- compute-geometry
+(defn- tile->geometry
   "Computes the geometry of a Modis Sinusoidal Tile as a vector of geodetic coordinates 
   representing the ring corresponding to the boundary of the tile."
   [h v]
   (let [quadrant (find-tile-quadrant h v)
-        [h_ur v_ur] (map-tile-to-ur-quadrant h v)
-        [p1 p2 p3 p4] (find-tile-vertices h_ur v_ur)
+        [h-ur v-ur] (tile->ur-quadrant-tile h v)
+        [p1 p2 p3 p4] (tile->corner-vertices h-ur v-ur)
         [x1 y1] p1
         [x4 y4] p4]
-    (if-not (or (fictional-point? x4 y4) (degenerate_tile? y1 x4))
+    (if-not (or (fictional-point? x4 y4) (degenerate-tile? y1 x4))
       (->> (densify-tile [p1 p2 p3 p4])
-           (#(conj % (first %)))
-           (map #(map-tile-point-backwards % quadrant))
-           (map #(apply planar->geodetic %))
+           (#(concat % [(first %)]))
+           (map #(ur-quadrant-point->point % quadrant))
+           (map (partial apply planar->geodetic))
            (#(if (or (= quadrant :ul) (= quadrant :lr)) (reverse %) %))
            flatten))))
 
-;; Computation of a Modis tile coordinates follows the steps below. (Please See: 
-;; https://wiki.earthdata.nasa.gov/display/CMR/Computation+of+MODIS+Tile+Geometry)
-;; 1) Find if the tile is a fictional tile or a real tile or a degenerate tile. Ignore fictional 
-;;    and degenerate tiles. Degenerate tiles have only one vertex which is a real point. All the 
-;;    remaining points of the tile are fictional points.
-;; 2) Map the tile to an equivalent tile in the upper-right quadrant(UR).
-;; 3) Find the coordinates of the four vertices of the rectangle corresponding to the tile.
-;; 4) Densify the each of the four segments of the tile.
-;; 5) Remove fictional points on each of the edges if there are any.
-;; 6) Add the edge-points if the edge has fictional points on one side and real points on another.
-;; 7) Map the densified points back to its original quadrant.
-;; 8) Convert the points from planar coordinates to geodetic coordinates. 
-;;    Convert from radians to degrees.
-
 (def modis-sin-tiles 
   "A vector consisting of all ModisSinTile records"
+  ;; We use delay here to postpone the compilation of code inside delay until runtime. This was 
+  ;; needed since we were seeing an unexpected compilation error during the CI build:
+  ;; No implementation of method: :calculate-derived of protocol: 
+  ;; #'cmr.spatial.derived/DerivedCalculator found for class: cmr.spatial.geodetic_ring.GeodeticRing
+  ;; (Please see: ***REMOVED*** for more details)
+  ;; Investigation of the issue did not result in a fix for the issue till now. 
+  ;; TODO: Identify the root cause of the issue and get rid of delay.
   (delay (vec (keep identity 
-                    (for [h (range H)
-                          v (range V)]
-                      (let [ords (map #(round 4 %) (compute-geometry h v))]
-                        (if (not(empty? ords))
-                          (->ModisSinTile 
+                    (for [h (range NUM_HORIZONTAL_TILES)
+                          v (range NUM_VERTICAL_TILES)]
+                      (when-let [ords (tile->geometry h v)]
+                        (->ModisSinTile 
                             (vector h v)
-                            (d/calculate-derived (apply rr/ords->ring :geodetic ords))))))))))
+                            (d/calculate-derived (apply rr/ords->ring :geodetic ords)))))))))
 
 (defn geometry->tiles
   "Gets tiles which intersect the given geometry as a sequence of tuples, each tuple 
-  being a vector holding two entries in the format [h v]. geometry could be of 
-  any geometric type including point, line, polygon, mbr and ring"
+  being a vector holding two entries in the format [h v]. geometry could be of any type 
+  including point, line, polygon, mbr and ring"
   [geometry]
   (let [geometry (d/calculate-derived geometry)]
     (keep  #(when (intersects? % geometry) (:coordinates %)) @modis-sin-tiles)))
