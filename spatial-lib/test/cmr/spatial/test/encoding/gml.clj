8eaae918eeab78a835cefa17b7734ad15bfa1aaf
John Cromartie <john@element84.com>
2015-04-02 11:43:56 -0400

    CMR-1380 Updates gml test to use :as instead of :refer :all

--- spatial-lib/test/cmr/spatial/test/encoding/gml.clj
+++ spatial-lib/test/cmr/spatial/test/encoding/gml.clj
@@ -6,7 +6,7 @@
             [clojure.test.check.clojure-test :refer :all]
             [clojure.test.check.properties :refer :all]
             [cmr.common.xml :as cx]
-            [cmr.spatial.encoding.gml :refer :all]
+            [cmr.spatial.encoding.gml :as gml]
             [cmr.spatial.line-string :as line]
             [cmr.spatial.point :as p]
             [cmr.spatial.polygon :as poly]
@@ -44,41 +44,41 @@
 
 (deftest test-parse-lat-lon-string
   (testing "one point"
-    (is (= (parse-lat-lon-string "9 10")
+    (is (= (gml/parse-lat-lon-string "9 10")
            [(p/point 10 9 true)])))
   (testing "multiple points"
-    (is (= (parse-lat-lon-string "2 1.03 -4 3")
+    (is (= (gml/parse-lat-lon-string "2 1.03 -4 3")
            [(p/point 1.03 2 true) (p/point 3 -4 true)]))))
 
 (deftest test-decode-point
   (testing "decoding points from GML"
     (is (= (p/point -110.45 45.256)
-           (decode (cx/element-at-path (x/parse-str gml-xml) [:Point]))))))
+           (gml/decode (cx/element-at-path (x/parse-str gml-xml) [:Point]))))))
 
 (deftest test-decode-line-string
   (testing "decoding GML line strings"
     (is (= (line/ords->line-string :cartesian -110.45 45.256, -109.48 46.46, -109.86 43.84, -109.2 45.8)
-           (decode (cx/element-at-path (x/parse-str gml-xml) [:LineString]))))))
+           (gml/decode (cx/element-at-path (x/parse-str gml-xml) [:LineString]))))))
 
 (deftest test-decode-polygon
   (testing "decoding GML polygons"
     (is (= (poly/polygon [(rr/ords->ring :cartesian -110.45 45.256, -109.48 46.46, -109.86 43.84, -110.45 45.256)])
-           (decode (cx/element-at-path (x/parse-str gml-xml) [:Polygon]))))))
+           (gml/decode (cx/element-at-path (x/parse-str gml-xml) [:Polygon]))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Property-Based Tests
 
 (defspec check-gml-point-round-trip 100
   (for-all [p spatial-gen/points]
-    (let [element (-> (encode p) emit-gml-str x/parse-str)]
-      (= p (decode element)))))
+    (let [element (-> (gml/encode p) emit-gml-str x/parse-str)]
+      (= p (gml/decode element)))))
 
 (defspec check-gml-line-string-round-trip 100
   (for-all [l spatial-gen/cartesian-lines]
-    (let [element (-> (encode l) emit-gml-str x/parse-str)]
-      (= l (decode element)))))
+    (let [element (-> (gml/encode l) emit-gml-str x/parse-str)]
+      (= l (gml/decode element)))))
 
 (defspec check-gml-polygon-round-trip 100
   (for-all [polygon spatial-gen/cartesian-polygons-with-holes]
-    (let [element (-> (encode polygon) emit-gml-str x/parse-str)]
-      (= (:points polygon) (:points (decode element))))))
+    (let [element (-> (gml/encode polygon) emit-gml-str x/parse-str)]
+      (= (:points polygon) (:points (gml/decode element))))))
