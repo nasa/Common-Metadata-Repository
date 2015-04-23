8527f84dff742b7a77caf376a21f2a7da5170677
Jason Gilman <jason@element84.com>
2015-04-23 13:57:35 -0400

    CMR-1375: Adding up and down to comment

--- metadata-db-app/src/migrations/012_add_sync_delete_work_indexes.clj
+++ metadata-db-app/src/migrations/012_add_sync_delete_work_indexes.clj
@@ -4,12 +4,12 @@
 
 (defn up
   []
-  (println "migrations.012-add-sync-delete-work-indexes")
+  (println "migrations.012-add-sync-delete-work-indexes up")
   (h/sql "CREATE INDEX sdw_cid_rid ON sync_delete_work(concept_id, revision_id)")
   (h/sql "CREATE INDEX sdw_deleted ON sync_delete_work(deleted)"))
 
 (defn down
   []
-  (println "migrations.012-add-sync-delete-work-indexes")
+  (println "migrations.012-add-sync-delete-work-indexes down")
   (h/sql "DROP INDEX sdw_cid_rid")
   (h/sql "DROP INDEX sdw_deleted"))
