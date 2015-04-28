a98c83a0d7afc33c24d04a13f1f4253f1d699780
Chris Durbin <christopher.d.durbin@nasa.gov>
2015-04-28 11:20:09 -0400

    CMR-1239: Reorder migrations

--- metadata-db-app/src/migrations/013_add_granule_ur_to_granule_tables.clj
+++ metadata-db-app/src/migrations/013_add_granule_ur_to_granule_tables.clj
@@ -1,11 +1,11 @@
-(ns migrations.014-add-granule-ur-to-granule-tables
+(ns migrations.013-add-granule-ur-to-granule-tables
   "Adds granule_ur column to granule tables"
   (:require [config.mdb-migrate-helper :as h]))
 
 (defn up
   "Migrates the database up to version 14."
   []
-  (println "migrations.014-add-granule-ur-to-granule-tables up...")
+  (println "migrations.013-add-granule-ur-to-granule-tables up...")
 
   ;; Create the new column for each granule table first - this will be fast. After the column has
   ;; been created, ongoing ingests should succeed even while the rest of this migration runs.
@@ -23,6 +23,6 @@
 (defn down
   "Migrates the database down from version 14."
   []
-  (println "migrations.014-add-granule-ur-to-granule-tables down.")
+  (println "migrations.013-add-granule-ur-to-granule-tables down.")
   (doseq [t (h/get-granule-tablenames)]
     (h/sql (format "alter table %s drop column granule_ur" t))))
