f039652d59285f12cfd524679eaf42a7f3e8a850
Chris Durbin <christopher.d.durbin@nasa.gov>
2015-04-29 10:57:42 -0400

    CMR-1380: Do not perform the database migrations in parallel.

--- metadata-db-app/src/migrations/013_add_granule_ur_to_granule_tables.clj
+++ metadata-db-app/src/migrations/013_add_granule_ur_to_granule_tables.clj
@@ -3,7 +3,7 @@
   (:require [config.mdb-migrate-helper :as h]))
 
 (defn up
-  "Migrates the database up to version 14."
+  "Migrates the database up to version 13."
   []
   (println "migrations.013-add-granule-ur-to-granule-tables up...")
 
@@ -12,16 +12,14 @@
   (doseq [t (h/get-granule-tablenames)]
     (h/sql (format "alter table %s add granule_ur VARCHAR(250)" t)))
 
-  ;; Populate the granule_ur column. Perform the updates in parallel to reduce the time of the
-  ;; overall migration
-  (doall (pmap (fn [t]
-                 (h/sql (format "update %s set granule_ur = native_id" t))
-                 (h/sql (format "alter table %s modify granule_ur NOT NULL" t))
-                 (h/sql (format "CREATE INDEX idx_%s_ur ON %s(granule_ur)" t t)))
-               (h/get-granule-tablenames))))
+  ;; Populate the granule_ur column and create an index simultaneous with ongoing ingest.
+  (doseq [t (h/get-granule-tablenames)]
+    (h/sql (format "update %s set granule_ur = native_id" t))
+    (h/sql (format "alter table %s modify granule_ur NOT NULL" t))
+    (h/sql (format "CREATE INDEX idx_%s_ur ON %s(granule_ur)" t t))))
 
 (defn down
-  "Migrates the database down from version 14."
+  "Migrates the database down from version 13."
   []
   (println "migrations.013-add-granule-ur-to-granule-tables down.")
   (doseq [t (h/get-granule-tablenames)]
