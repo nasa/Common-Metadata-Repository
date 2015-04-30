30e92cd7cf89eb30bbdbe09a1ab5407475d07cf5
Jason Gilman <jason@element84.com>
2015-04-30 10:28:30 -0400

    Adding a test that the combination of consistent cache and single thread lookup cache still functions as a consistent cache.

--- common-app-lib/test/cmr/common_app/test/cache/consistent_cache.clj
+++ common-app-lib/test/cmr/common_app/test/cache/consistent_cache.clj
@@ -5,7 +5,9 @@
             [cmr.common.cache.in-memory-cache :as mem-cache]
             [cmr.common.cache.cache-spec :as cache-spec]
             [cmr.common-app.cache.consistent-cache-spec :as consistent-cache-spec]
-            [cmr.common.cache :as c]))
+            [cmr.common.cache :as c]
+            [cmr.common.cache.single-thread-lookup-cache :as slc]
+            [cmr.common.lifecycle :as l]))
 
 ;; Test the consistent cache with just in memory caches
 (deftest consistent-cache-functions-as-cache-test
@@ -18,3 +20,21 @@
     (consistent-cache-spec/assert-consistent-cache
       (cc/create-consistent-cache (mem-cache/create-in-memory-cache) hash-cache)
       (cc/create-consistent-cache (mem-cache/create-in-memory-cache) hash-cache))))
+
+(deftest consistent-cache-with-single-thread-cache-functions-as-consistent-cache-test
+  (let [hash-cache (mem-cache/create-in-memory-cache)
+        slc-cache1 (l/start (slc/create-single-thread-lookup-cache
+                               (cc/create-consistent-cache
+                                 (mem-cache/create-in-memory-cache)
+                                 hash-cache))
+                            nil)
+        slc-cache2 (l/start (slc/create-single-thread-lookup-cache
+                               (cc/create-consistent-cache
+                                 (mem-cache/create-in-memory-cache)
+                                 hash-cache))
+                            nil)]
+    (try
+      (consistent-cache-spec/assert-consistent-cache slc-cache1 slc-cache2)
+      (finally
+        (l/stop slc-cache1 nil)
+        (l/stop slc-cache2 nil)))))
