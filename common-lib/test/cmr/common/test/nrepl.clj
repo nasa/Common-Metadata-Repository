a6062f09a5dd9d30dc8ad6e0cbe7a71fe1e95d3d
John Cromartie <john@element84.com>
2015-05-20 07:37:10 -0400

    CMR-1436 Adds and updates nREPL tests.

--- common-lib/test/cmr/common/test/nrepl.clj
+++ common-lib/test/cmr/common/test/nrepl.clj
@@ -6,12 +6,16 @@
             [cmr.common.nrepl :as nrepl-component]))
 
 (deftest test-nrepl-component
-  (let [component (lifecycle/start (nrepl-component/create-nrepl 0) nil)
-        port (:port component)
-        response (with-open [conn (nrepl/connect :port port)]
+  (let [repl (lifecycle/start (nrepl-component/create-nrepl 0) nil)
+        response (with-open [conn (nrepl/connect :port (:port repl))]
                    (-> (nrepl/client conn 1000)
                        (nrepl/message {:op :eval :code "(+ 21 21)"})
                        nrepl/response-values
                        first))]
     (is (= 42 response))
-    (lifecycle/stop component nil)))
+    (lifecycle/stop repl nil)))
+
+(deftest test-nrepl-port-in-use
+  (let [repl (lifecycle/start (nrepl-component/create-nrepl 0) nil)]
+    (is (thrown? java.net.BindException (lifecycle/start (nrepl-component/create-nrepl (:port repl)) nil)))
+    (lifecycle/stop repl nil)))
