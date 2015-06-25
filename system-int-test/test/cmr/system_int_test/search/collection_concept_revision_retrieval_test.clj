841bba4b37de0833dd83a80aeae1639501f59392
James Norton <james@element84.com>
2015-06-25 15:45:24 -0400

    CMR-1412: Removed unused requires

--- system-int-test/test/cmr/system_int_test/search/collection_concept_revision_retrieval_test.clj
+++ system-int-test/test/cmr/system_int_test/search/collection_concept_revision_retrieval_test.clj
@@ -1,15 +1,11 @@
 (ns cmr.system-int-test.search.collection-concept-revision-retrieval-test
   "Integration test for collection concept map retrieval with params"
   (:require [clojure.test :refer :all]
-            [cmr.umm.echo10.collection :as c]
             [cmr.system-int-test.utils.ingest-util :as ingest]
             [cmr.system-int-test.utils.search-util :as search]
             [cmr.system-int-test.utils.index-util :as index]
             [cmr.system-int-test.data2.collection :as dc]
-            [cmr.mock-echo.client.echo-util :as e]
-            [cmr.system-int-test.system :as s]
             [cmr.system-int-test.data2.core :as d]
-            [cmr.system-int-test.utils.dev-system-util :as dev-sys-util]
             [cmr.common.util :refer [are2]]
             [cmr.transmit.config :as transmit-config]))
 
