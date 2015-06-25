744061b13d3418bb77652415b5767d18bb13c451
Jason Gilman <jason@element84.com>
2015-06-25 08:55:59 -0400

    CMR-1715: Removing ingest event aliases.

--- indexer-app/src/cmr/indexer/services/ingest_event_handler.clj
+++ indexer-app/src/cmr/indexer/services/ingest_event_handler.clj
@@ -9,22 +9,10 @@
             [cmr.message-queue.services.queue :as queue]
             [cmr.common.log :refer (debug info warn error)]))
 
-(def ingest-event-aliases
-  "The actions sent in the ingest events can change from time to time. This has aliases from past
-  events to the latest events. This allows multiple versions of the CMR to be deployed and running
-  at the same time."
-  {:index-concept :concept-update
-   :delete-concept :concept-delete})
-
-(defn translate-action-alias
-  "Returns the latest name for an action"
-  [action]
-  (get ingest-event-aliases action action))
-
 (defmulti handle-ingest-event
   "Handle the various actions that can be requested via the indexing queue"
   (fn [context msg]
-    (translate-action-alias (keyword (:action msg)))))
+    (keyword (:action msg))))
 
 (defmethod handle-ingest-event :default
   [_ _]
