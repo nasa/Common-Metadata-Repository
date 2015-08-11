a76c5078efbf039618a95054b63ae4e95e9a3649
Upendra Dadi <upendra.dadi@nasa.gov>
2015-08-11 16:16:52 -0400

    CMR-1589: Modified migrations to consolidate the two migrations into one. Changed size of user_id column to 50.

--- metadata-db-app/src/migrations/018_add_user_id_collection_tables.clj
+++ metadata-db-app/src/migrations/018_add_user_id_collection_tables.clj
@@ -7,13 +7,14 @@
   "Migrates the database up to version 18."
   []
   (println "migrations.018-add-user-id-collection-tables up...")
+  (h/sql "alter table small_prov_collections add user_id VARCHAR(50)")
   (doseq [t (h/get-collection-tablenames)]
-    (h/sql (format "alter table %s add user_id VARCHAR(200)" t))
-    (h/sql (format "CREATE INDEX %s_uid_i ON %s(user_id)" t t))))
+    (h/sql (format "alter table %s add user_id VARCHAR(50)" t))))
 
 (defn down
   "Migrates the database down from version 18."
   []
   (println "migrations.018-add-user-id-collection-tables down...")
+  (h/sql "alter table small_prov_collections drop column user_id")
   (doseq [t (h/get-collection-tablenames)]
     (h/sql (format "alter table %s drop column user_id" t))))
