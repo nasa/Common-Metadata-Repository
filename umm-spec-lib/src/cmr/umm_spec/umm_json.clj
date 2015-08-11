ce8141ed890ed22331c04ec7cdf844499aa06961
Jason Gilman <jason@element84.com>
2015-08-11 10:40:56 -0400

    CMR-1580: Moving code to convert joda time to JSON to common location.

--- umm-spec-lib/src/cmr/umm_spec/umm_json.clj
+++ umm-spec-lib/src/cmr/umm_spec/umm_json.clj
@@ -1,19 +1,15 @@
 (ns cmr.umm-spec.umm-json
   "Contains functions for converting a UMM into JSON and back out of JSON."
   (:require [cheshire.core :as json]
-            [cheshire.generate :as json-gen]
             [cmr.umm-spec.json-schema :as js]
             [cmr.umm-spec.record-generator :as record-gen]
-            [cmr.common.date-time-parser :as dtp]))
+            [cmr.common.date-time-parser :as dtp]
+
+            ;; To get ability to convert joda time to json
+            [cmr.common.joda-time]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; UMM To JSON
-
-;; Adds the ability for Joda DateTimes to be converted to JSON.
-(json-gen/add-encoder
-  org.joda.time.DateTime
-  (fn [c jsonGenerator]
-    (.writeString jsonGenerator (str c))))
 
 (defprotocol ToJsonAble
   "A function for converting data into data that can easily be converted to JSON"
