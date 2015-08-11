e629426672dd5e3a7dafb91454324e6a7a4ada89
Chris Durbin <christopher.d.durbin@nasa.gov>
2015-08-11 08:23:39 -0400

    CMR-1830: Refactor and fix edge cases for parsing KMS CSV files

--- transmit-lib/src/cmr/transmit/kms.clj
+++ transmit-lib/src/cmr/transmit/kms.clj
@@ -7,46 +7,60 @@
             [cmr.transmit.connection :as conn]
             [cmr.common.log :as log :refer (debug info warn error)]))
 
-(def leaf-node-field
-  "A map of the concept-scheme to the field within that concept-scheme that identifies an entry
-  as a leaf node."
+(def leaf-node-field-name
+  "A map of the concept-scheme to the field name within that concept-scheme that identifies an
+  entry as a leaf node."
   {:providers :Short_Name})
 
 (comment
-  (zipmap [:1 :2 :3] (str/split "\"abc\",\"def\"" #","))
-  (str/split "\"abc\",\"def\"" #",")
-  (map keyword (str/split (str/replace "\"B1\",\"B2\",\"short-name\""
-                                       "\""
-                                       "")
-                          #",")))
+
+  )
+
+(defn- parse-single-csv-line
+  "Parses a single CSV line into an array of values. An example line:
+
+  \"U.S. STATE/REGIONAL/LOCAL AGENCIES\",\"WYOMING\",\"\",\"\",\"WY/TC/DEM\",\"Department of
+  Emergency Management,Teton County, Wyoming\",\"http://www.tetonwyo.org/em/\",
+  \"d51f97c7-387a-4794-b445-bb1daa486cde\""
+  [csv-line field-separator]
+  (-> csv-line
+      (str/replace (re-pattern "^\"") "")
+      (str/replace (re-pattern "\"$") "")
+      (str/split (re-pattern field-separator))))
 
 (defn- parse-entries-from-csv
   "Parses the CSV returned by the GCMD KMS. It is expected that the CSV will be returned in a
   specific format with the first line providing metadata information, the second line providing
   a breakdown of the subfields within the hierarchy, and from the third line on are the actual
-  values."
-  [concept-scheme csv]
-  (let [all-lines (str/split-lines csv)
-        sub-field-names (map keyword (-> (second all-lines)
-                                         (str/replace "\"" "")
-                                         (str/split #",")))
-        keyword-value-strings (rest (rest all-lines))
-        keyword-values (map (fn [value-as-str] (zipmap sub-field-names
-                                                       (-> value-as-str
-                                                           (str/replace "\"" "")
-                                                           (str/split #","))))
-                            keyword-value-strings)
-        ]
-    (cmr.common.dev.capture-reveal/capture keyword-value-strings)
-    (cmr.common.dev.capture-reveal/capture keyword-values)
+  values.
 
-    (debug (str "keyword-values are " (first keyword-values)))
+  Returns an array of maps containing the subfield names as keys for each of the values."
+  [concept-scheme csv-content]
+  (let [all-lines (str/split-lines csv-content)
+        ;; Line 2 contains the names of the subfield names
+        subfield-names (map keyword (parse-single-csv-line (second all-lines) ","))
+        keyword-values (map #(zipmap subfield-names (parse-single-csv-line % "\",\""))
+                            ;; Lines 3 to the end are the values
+                            (rest (rest all-lines)))]
+
     ;; Filter out any values that are not leaf nodes
-    (remove #(str/blank? ((leaf-node-field concept-scheme) %)) keyword-values)))
+    (remove #(str/blank? ((leaf-node-field-name concept-scheme) %)) keyword-values)))
 
 (comment
-  (get-by-concept-scheme {:system (cmr.search.system/create-system)} "providers")
-  (get-provider-hierarchy {:system (cmr.search.system/create-system)})
+  (take 20 (get-provider-hierarchy {:system (cmr.search.system/create-system)}))
+
+  ;; Need to test with "U.S. STATE/REGIONAL/LOCAL AGENCIES","WYOMING","","","WY/TC/DEM","Department of Emergency Management,Teton County, Wyoming","http://www.tetonwyo.org/em/","d51f97c7-387a-4794-b445-bb1daa486cde"
+
+  (zipmap [:1 :2 :3] (str/split "\"abc\",\"def\"" #","))
+  (str/split "\"abc\",\"def\"" #",")
+  (map keyword (str/split (str/replace "\"B1\",\"B2\",\"short-name\""
+                                       "\""
+                                       "")
+                          #","))
+
+  (re-pattern "\"?.*\"?")
+  (def all-lines (str/split-lines (slurp (clojure.java.io/resource "provider_kms.csv"))))
+    (parse-single-csv-line (second all-lines))
   )
 
 (defn- get-by-concept-scheme
@@ -69,8 +83,6 @@
        (format
          "Completed KMS Request to %s in [%d] ms" url (- (System/currentTimeMillis) start)))
      response)))
-
-
 
 ;; Public API
 
