136e6815c1e8f7b95fcbc09b0b91525ce812734f
Jason Gilman <jason@element84.com>
2015-08-12 12:11:51 -0400

    CMR-1580: Documented translation API. Added tests of input and output supported formats.

--- ingest-app/src/cmr/ingest/api/translation.clj
+++ ingest-app/src/cmr/ingest/api/translation.clj
@@ -1,9 +1,12 @@
 (ns cmr.ingest.api.translation
+  "Defines an API for translating metadata between formats."
   (:require [compojure.core :refer :all]
             [cmr.common.mime-types :as mt]
-            [cmr.umm-spec.core :as umm-spec]))
+            [cmr.umm-spec.core :as umm-spec]
+            [cmr.common.services.errors :as errors]))
 
 (def supported-formats
+  "The list of formats that are supported both for input and output of translation."
   #{mt/echo10
     mt/umm-json
     mt/iso19115
@@ -12,18 +15,16 @@
     mt/iso-smap})
 
 (defn translate
+  "Fulfills the translate request using the body, content type header, and accept header. Returns
+  a ring response with translated metadata."
   [context concept-type headers body-input]
   (let [body (slurp body-input)
-        ;; TODO test that output format is provided
-        output-mime-type (mt/accept-mime-type headers supported-formats)
+        output-mime-type (mt/extract-header-mime-type supported-formats headers "accept" true)
         output-format (mt/mime-type->format output-mime-type)
-        ;; TODO test that content type is provided
-        input-mime-type (mt/content-type-mime-type headers supported-formats)
+        input-mime-type (mt/extract-header-mime-type supported-formats headers "content-type" true)
         input-format (mt/mime-type->format input-mime-type)
         umm (umm-spec/parse-metadata concept-type input-format body)
         output-str (umm-spec/generate-metadata concept-type output-format umm)]
-    ;; TODO validate formats and add integration tests
-
     ;; TODO validate input data against XML schema or JSON schema
     ;; I'll do this as another pull request
     {:status 200
@@ -34,10 +35,6 @@
   (context "/translate" []
     (POST "/collection" {:keys [body headers request-context]}
       (translate request-context :collection headers body))
-
-    ;; TODO granule translation
-
+    ;; Granule translation is not supported yet. This will be done when granules are added to the UMM spec.
     ))
 
-;; TODO think about integration tests and failure cases
-;; - metadata does not match
