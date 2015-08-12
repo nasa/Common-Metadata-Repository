55c4be659127876496b77f10d646ddde45d32e07
Jason Gilman <jason@element84.com>
2015-08-12 15:35:16 -0400

    Correcting TODO in metadata db test.

--- metadata-db-app/int_test/cmr/metadata_db/int_test/concepts/search_test.clj
+++ metadata-db-app/int_test/cmr/metadata_db/int_test/concepts/search_test.clj
@@ -170,7 +170,7 @@
             "small provider - short-name, version-id"
             [coll4-3] {:short-name "s4" :version-id "v5" :latest true}
 
-            ;; TODO - this should change when the "find latest" logic is updated in CMR-1416.
+            ;; TODO - this should change when the "find latest" logic is updated in CMR-1831
             "small provider - match multiple - version-id"
             [coll3 coll4-1] {:version-id "v3" :latest true}
 
