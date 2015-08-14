31105c029f103d5f4878adbb1944da46b202ff0c
John Cromartie <john@element84.com>
2015-08-14 13:01:13 -0400

    CMR-1867 Refactors generate-and-parse test namespace.
    The tests now use the parse/generate functions in core. The example
    record is updated to reflect the latest UMM schema in regards to the
    TemporalExtent. :require is simplified.

--- umm-spec-lib/test/cmr/umm_spec/test/generate_and_parse.clj
+++ umm-spec-lib/test/cmr/umm_spec/test/generate_and_parse.clj
@@ -2,101 +2,94 @@
   "Tests roundtrip XML generation from a Clojure record and parsing it. Ensures that the same data
   is returned."
   (:require [clojure.test :refer :all]
-            [cmr.umm-spec.umm-to-xml-mappings.xml-generator :as xg]
-            [cmr.umm-spec.umm-to-xml-mappings.echo10 :as xm-echo10]
-            [cmr.umm-spec.xml-to-umm-mappings.echo10 :as um-echo10]
-            [cmr.umm-spec.umm-to-xml-mappings.iso19115-2 :as xm-iso2]
-            [cmr.umm-spec.xml-to-umm-mappings.iso19115-2 :as um-iso2]
-            [cmr.umm-spec.umm-to-xml-mappings.dif9 :as xm-dif9]
-            [cmr.umm-spec.xml-to-umm-mappings.dif9 :as um-dif9]
-            [cmr.umm-spec.umm-to-xml-mappings.dif10 :as xm-dif10]
-            [cmr.umm-spec.xml-to-umm-mappings.dif10 :as um-dif10]
-            [cmr.umm-spec.umm-to-xml-mappings.iso-smap :as xm-smap]
-            [cmr.umm-spec.xml-to-umm-mappings.iso-smap :as um-smap]
-            [cmr.umm-spec.xml-to-umm-mappings.parser :as xp]
             [cmr.umm-spec.test.expected-conversion :as expected-conversion]
+            [cmr.umm-spec.core :as core]
             [cmr.umm-spec.models.collection :as umm-c]
             [cmr.umm-spec.models.common :as umm-cmn]
-            [cmr.umm-spec.json-schema :as js]
-            [cmr.umm-spec.core :as c]
             [clj-time.core :as t]
             [cmr.common.util :refer [are2]]))
-
-(def example-record-echo10-supported
-  "This contains an example record will all the fields supported by ECHO10. It supported
-  more initially for demonstration purposes."
-  (umm-c/map->UMM-C
-    {
-     :EntryId (umm-cmn/map->EntryIdType {:Id "short_V1"})
-     :EntryTitle "The entry title V5"
-     :Abstract "A very abstract collection"
-     :TemporalExtent [(umm-cmn/map->TemporalExtentType
-                        {:TemporalRangeType "temp range"
-                         :PrecisionOfSeconds 3
-                         :EndsAtPresentFlag false
-                         :RangeDateTime (mapv umm-cmn/map->RangeDateTimeType
-                                              [{:BeginningDateTime (t/date-time 2000)
-                                                :EndingDateTime (t/date-time 2001)}
-                                               {:BeginningDateTime (t/date-time 2002)
-                                                :EndingDateTime (t/date-time 2003)}])
-                         :SingleDateTime [(t/date-time 2003) (t/date-time 2004)]
-                         :PeriodicDateTime (mapv umm-cmn/map->PeriodicDateTimeType
-                                                 [{:Name "period1"
-                                                   :StartDate (t/date-time 2000)
-                                                   :EndDate (t/date-time 2001)
-                                                   :DurationUnit "YEAR"
-                                                   :DurationValue 4
-                                                   :PeriodCycleDurationUnit "DAY"
-                                                   :PeriodCycleDurationValue 3}
-                                                  {:Name "period2"
-                                                   :StartDate (t/date-time 2000)
-                                                   :EndDate (t/date-time 2001)
-                                                   :DurationUnit "YEAR"
-                                                   :DurationValue 4
-                                                   :PeriodCycleDurationUnit "DAY"
-                                                   :PeriodCycleDurationValue 3}])})]}))
 
 (def example-record
   "This contains an example record with fields supported by all formats"
   (umm-c/map->UMM-C
-    {:EntryTitle "The entry title V5"
-     :EntryId (umm-cmn/map->EntryIdType {:Id "short_V1"})
-     :Abstract "Abstract description"}))
+   {:EntryTitle "The entry title V5"
+    :EntryId (umm-cmn/map->EntryIdType {:Id "short_V1"})
+    :Abstract "Abstract description"
+    :TemporalExtent [(umm-cmn/map->TemporalExtentType
+                      {:TemporalRangeType "temp range"
+                       :PrecisionOfSeconds 3
+                       :EndsAtPresentFlag false
+                       :RangeDateTime (mapv umm-cmn/map->RangeDateTimeType
+                                            [{:BeginningDateTime (t/date-time 2000)
+                                              :EndingDateTime (t/date-time 2001)}
+                                             {:BeginningDateTime (t/date-time 2002)
+                                              :EndingDateTime (t/date-time 2003)}])})
+                     (umm-cmn/map->TemporalExtentType
+                      {:TemporalRangeType "temp range"
+                       :PrecisionOfSeconds 3
+                       :EndsAtPresentFlag false
+                       :SingleDateTime [(t/date-time 2003) (t/date-time 2004)]})
+                     (umm-cmn/map->TemporalExtentType
+                      {:TemporalRangeType "temp range"
+                       :PrecisionOfSeconds 3
+                       :EndsAtPresentFlag false
+                       :PeriodicDateTime (mapv umm-cmn/map->PeriodicDateTimeType
+                                               [{:Name "period1"
+                                                 :StartDate (t/date-time 2000)
+                                                 :EndDate (t/date-time 2001)
+                                                 :DurationUnit "YEAR"
+                                                 :DurationValue 4
+                                                 :PeriodCycleDurationUnit "DAY"
+                                                 :PeriodCycleDurationValue 3}
+                                                {:Name "period2"
+                                                 :StartDate (t/date-time 2000)
+                                                 :EndDate (t/date-time 2001)
+                                                 :DurationUnit "YEAR"
+                                                 :DurationValue 4
+                                                 :PeriodCycleDurationUnit "DAY"
+                                                 :PeriodCycleDurationValue 3}])})]}))
 
+(defn xml-round-trip
+  "Returns record after being converted to XML and back to UMM through
+  the given to-xml and to-umm mappings."
+  [record format]
+  (core/parse-metadata :collection format (core/generate-metadata :collection format record)))
+
+(deftest dif9-round-trip
+  (is (= (expected-conversion/convert example-record :dif)
+         (xml-round-trip example-record :dif))))
 
 (deftest roundtrip-gen-parse
-  (are2 [metadata-format to-xml to-umm]
+  (are2 [metadata-format]
     (= (expected-conversion/convert example-record metadata-format)
-       (xp/parse-xml to-umm (xg/generate-xml to-xml example-record)))
-    
+       (xml-round-trip example-record metadata-format))
     "echo10"
-    :echo10 xm-echo10/umm-c-to-echo10-xml um-echo10/echo10-xml-to-umm-c
+    :echo10
 
     "dif9"
-    :dif xm-dif9/umm-c-to-dif9-xml um-dif9/dif9-xml-to-umm-c
+    :dif
 
     "dif10"
-    :dif10 xm-dif10/umm-c-to-dif10-xml um-dif10/dif10-xml-to-umm-c
+    :dif10
 
     "iso-smap"
-    :iso-smap xm-smap/umm-c-to-iso-smap-xml um-smap/iso-smap-xml-to-umm-c
+    :iso-smap
 
     "ISO19115-2"
-    :iso19115 xm-iso2/umm-c-to-iso19115-2-xml um-iso2/iso19115-2-xml-to-umm-c)
-
-  ;; This is here because echo10 supported additional fields
-  (testing "echo10 supported fields"
-    (let [xml (xg/generate-xml xm-echo10/umm-c-to-echo10-xml example-record-echo10-supported)
-          parsed (xp/parse-xml um-echo10/echo10-xml-to-umm-c xml)
-          expected (expected-conversion/convert example-record-echo10-supported :echo10)]
-      (is (= expected parsed)))))
+    :iso19115))
 
 (deftest generate-valid-xml
   (testing "valid XML is generated for each format"
-    (are [fmt generator]
-        (empty? (c/validate-xml :collection fmt (xg/generate-xml generator example-record)))
-      :echo10   xm-echo10/umm-c-to-echo10-xml
-      :dif      xm-dif9/umm-c-to-dif9-xml
-      :dif10    xm-dif10/umm-c-to-dif10-xml
-      :iso-smap xm-smap/umm-c-to-iso-smap-xml
-      :iso19115 xm-iso2/umm-c-to-iso19115-2-xml)))
+    (are [fmt]
+        (empty? (core/validate-xml :collection fmt (core/generate-metadata :collection fmt example-record)))
+      :echo10
+      :dif
+      :dif10
+      :iso-smap
+      :iso19115)))
+
+(comment
+  (->> example-record
+       (core/generate-metadata :collection :dif)
+       (core/parse-metadata :collection :dif))
+  )
