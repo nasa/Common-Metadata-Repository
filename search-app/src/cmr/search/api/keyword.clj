a14ed855f7a808a2c1937d42922ba3743edade17
Chris Durbin <christopher.d.durbin@nasa.gov>
2015-09-04 08:23:32 -0400

    CMR-1980: Start testing returning keywords hierarchically from keyword endpoint

--- search-app/src/cmr/search/api/keyword.clj
+++ search-app/src/cmr/search/api/keyword.clj
@@ -6,6 +6,7 @@
             [cmr.common-app.services.kms-fetcher :as kf]
             [cmr.transmit.kms :as kms]
             [cmr.common.mime-types :as mt]
+            [cmr.common.util :as util]
             [cmr.common.services.errors :as errors]
             [cmr.search.services.query-execution.facets-results-feature :as frf]))
 
@@ -25,6 +26,72 @@
 ;     (let [all-values-for-subfield (map subfield keywords)]
 ;       (for [value all-values-for-subfield]
 ;         (parse-hierarchical-keywords keyword-scheme (rest keyword-hierarchy) keywords)))))
+
+
+;; Sample output
+
+(comment
+  ["0dd83b2a-e83f-4a0c-a1ff-2fbdbbcce62d"
+   {:uuid "0dd83b2a-e83f-4a0c-a1ff-2fbdbbcce62d",
+    :variable-level-1 "MOBILE GEOGRAPHIC INFORMATION SYSTEMS",
+    :term "GEOGRAPHIC INFORMATION SYSTEMS",
+    :topic "DATA ANALYSIS AND VISUALIZATION",
+    :category "EARTH SCIENCE SERVICES"}]
+
+  {:category [{:value "EARTH SCIENCE SERVICES"
+               :uuid "894f9116-ae3c-40b6-981d-5113de961710"
+               :subfields ["topic"]
+               :topic [{:value "DATA ANALYSIS AND VISUALIZATION"
+                        :uuid "41adc080-c182-4753-9666-435f8b1c913f"
+                        :subfields ["term"]
+                        :term [{:value "GEOGRAPHIC INFORMATION SYSTEMS"
+                                :uuid "794e3c3b-791f-44de-9ff3-358d8ed74733"
+                                :subfields ["variable-level-1"]
+                                :variable-level-1 [{:value "MOBILE GEOGRAPHIC INFORMATION SYSTEMS"
+                                                    :uuid "0dd83b2a-e83f-4a0c-a1ff-2fbdbbcce62d"
+                                                    }]}]}]}]}
+
+  (def keyword-hierarchy
+    [:category :topic :term :variable-level-1 :variable-level-2 :variable-level-3])
+
+  (def keywords
+    [{:uuid "0dd83b2a-e83f-4a0c-a1ff-2fbdbbcce62d",
+      :variable-level-1 "MOBILE GEOGRAPHIC INFORMATION SYSTEMS",
+      :term "GEOGRAPHIC INFORMATION SYSTEMS",
+      :topic "DATA ANALYSIS AND VISUALIZATION",
+      :category "EARTH SCIENCE SERVICES"}
+     {:uuid "894f9116-ae3c-40b6-981d-5113de961710",
+      :category "EARTH SCIENCE SERVICES"}
+     {:uuid "41adc080-c182-4753-9666-435f8b1c913f",
+      :topic "DATA ANALYSIS AND VISUALIZATION",
+      :category "EARTH SCIENCE SERVICES"}
+     {:uuid "e9f67a66-e9fc-435c-b720-ae32a2c3d8f5",
+      :category "EARTH SCIENCE"}])
+
+  {:category (map (fn [k-word] (util/remove-nil-keys {:value (:category k-word)
+                                                      :uuid (when-not (:topic k-word)
+                                                              (:uuid k-word))})) keywords)}
+
+  (let [all-categories (keep (fn [k-word] (when-not (:topic k-word)
+                                            {:value (:category k-word)
+                                             :uuid  (:uuid k-word)})) keywords)
+        all-topics (for [category all-categories
+                         :let [topics (keep (fn [k-word]
+                                              (when (and (nil? (:term k-word))
+                                                         (:topic k-word)
+                                                         (= (:value category) (:category k-word)))
+                                                {:value (:topic k-word)
+                                                 :uuid (:uuid k-word)})) keywords)]]
+                     (if (seq topics)
+                       (assoc category
+                              :subfields ["topic"]
+                              :topic topics)
+                       category))]
+    {:category all-topics})
+
+
+  (parse-hierarchical-keywords :science-keywords keyword-hierarchy keywords)
+ )
 
 (def keyword-api-routes
   (context "/keywords" []
