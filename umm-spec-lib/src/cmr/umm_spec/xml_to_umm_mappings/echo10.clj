9a01dd2a6a5d52c7a12e55167548f834a1ddb610
John Cromartie <john@element84.com>
2015-09-10 16:03:04 -0400

    Renaming new-echo10 namespaces to take the place of old echo10 namespaces.

--- umm-spec-lib/src/cmr/umm_spec/xml_to_umm_mappings/echo10.clj
+++ umm-spec-lib/src/cmr/umm_spec/xml_to_umm_mappings/echo10.clj
@@ -1,80 +1,71 @@
-(ns cmr.umm-spec.xml-to-umm-mappings.echo10
+(ns cmr.umm-spec.xml-to-umm-mappings.new-echo10
   "Defines mappings from ECHO10 XML into UMM records"
-  (:require [cmr.common.xml :as cx]
-            [cmr.umm-spec.xml-to-umm-mappings.dsl :refer :all]
-            [cmr.umm-spec.xml-to-umm-mappings.add-parse-type :as apt]
-            [cmr.umm-spec.json-schema :as js]))
+  (:require [cmr.umm-spec.json-schema :as js]
+            [cmr.umm-spec.simple-xpath :refer [select text]]
+            [cmr.umm-spec.xml.parse :refer :all]))
 
-(def temporal-mappings
-  (for-each "/Collection/Temporal"
-    (object
-      {:TemporalRangeType (xpath "TemporalRangeType")
-       :PrecisionOfSeconds (xpath "PrecisionOfSeconds")
-       :EndsAtPresentFlag (xpath "EndsAtPresentFlag")
-       :RangeDateTimes (for-each "RangeDateTime"
-                         (matching-object :BeginningDateTime :EndingDateTime))
-       :SingleDateTimes (select "SingleDateTime")
-       :PeriodicDateTimes (for-each "PeriodicDateTime"
-                            (matching-object :Name :StartDate :EndDate :DurationUnit :DurationValue
-                                             :PeriodCycleDurationUnit :PeriodCycleDurationValue))})))
+(defn parse-temporal
+  "Returns seq of UMM temporal extents from an ECHO10 XML document."
+  [doc]
+  (for [temporal (select doc "/Collection/Temporal")]
+    {:TemporalRangeType (value-of temporal "TemporalRangeType")
+     :PrecisionOfSeconds (value-of temporal "PrecisionOfSeconds")
+     :EndsAtPresentFlag (value-of temporal "EndsAtPresentFlag")
+     :RangeDateTimes (for [rdt (select temporal "RangeDateTime")]
+                       (fields-from rdt :BeginningDateTime :EndingDateTime))
+     :SingleDateTimes (values-at temporal "SingleDateTime")
+     :PeriodicDateTimes (for [pdt (select temporal "PeriodicDateTime")]
+                          (fields-from pdt :Name :StartDate :EndDate :DurationUnit :DurationValue
+                                       :PeriodCycleDurationUnit :PeriodCycleDurationValue))}))
 
-(def characteristic-mapping
-  (matching-object :Name
-                   :Description
-                   :DataType
-                   :Unit
-                   :Value))
+(defn parse-characteristic
+  "Returns a UMM characteristic record from an ECHO10 Characteristic element."
+  [element]
+  (fields-from element :Name :Description :DataType :Unit :Value))
 
-(def sensor-mapping
-  (object {:ShortName (xpath "ShortName")
-           :LongName (xpath "LongName")
-           :Technique (xpath "Technique")
-           :Characteristics (for-each "Characteristics/Characteristic"
-                              characteristic-mapping)}))
+(defn parse-characteristics
+  "Returns a seq of UMM characteristic records from the element's child Characteristics."
+  [el]
+  (map parse-characteristic (select el "Characteristics/Characteristic")))
 
-(def instrument-mapping
-  (object {:ShortName (xpath "ShortName")
-           :LongName (xpath "LongName")
-           :Technique (xpath "Technique")
-           :NumberOfSensors (xpath "NumberOfSensors")
-           :OperationalModes (select "OperationModes/OperationMode")
-           :Characteristics (for-each "Characteristics/Characteristic"
-                              characteristic-mapping)
-           :Sensors (for-each "Sensors/Sensor"
-                      sensor-mapping)}))
+(defn parse-sensor
+  "Returns a UMM Sensor record from an ECHO10 Sensor element."
+  [sensor-element]
+  (assoc (fields-from sensor-element :ShortName :LongName :Technique)
+         :Characteristics (parse-characteristics sensor-element)))
 
-(def echo10-xml-to-umm-c
-  (apt/add-parsing-types
-    js/umm-c-schema
-    (object
-      {:EntryTitle (xpath "/Collection/DataSetId")
-       :EntryId (xpath "/Collection/ShortName")
-       :Version (xpath "/Collection/VersionId")
-       :Abstract (xpath "/Collection/Description")
-       :CollectionDataType (xpath "/Collection/CollectionDataType")
-       :Purpose (xpath "/Collection/SuggestedUsage")
-       :CollectionProgress (xpath "/Collection/CollectionState")
-       :AccessConstraints (object
-                            {:Description (xpath "/Collection/RestrictionComment")
-                             :Value (xpath "/Collection/RestrictionFlag")})
-       :Distributions (for-each "/Collection"
-                                (object
-                                  {:DistributionFormat (xpath "DataFormat")
-                                   :Fees (xpath "Price")}))
-       :SpatialKeywords (select "/Collection/SpatialKeywords/Keyword")
-       :TemporalKeywords (select "/Collection/TemporalKeywords/Keyword")
-       :TemporalExtents temporal-mappings
-       :Platforms (for-each "/Collection/Platforms/Platform"
-                            (object {:ShortName (xpath "ShortName")
-                                     :LongName (xpath "LongName")
-                                     :Type (xpath "Type")
-                                     :Characteristics (for-each "Characteristics/Characteristic"
-                                                                characteristic-mapping)
-                                     :Instruments (for-each "Instruments/Instrument"
-                                                            instrument-mapping)}))
-       :ProcessingLevel (object
-                          {:Id (xpath "/Collection/ProcessingLevelId")
-                           :ProcessingLevelDescription (xpath "/Collection/ProcessingLevelDescription")})
-       :AdditionalAttributes (for-each "/Collection/AdditionalAttributes/AdditionalAttribute"
-                                       (matching-object :Name :Description :DataType :ParameterRangeBegin
-                                                        :ParameterRangeEnd :Value))})))
+(defn parse-instrument
+  "Returns a UMM Instrument record from an ECHO10 Instrument element."
+  [inst]
+  (assoc (fields-from inst :ShortName :LongName :Technique :NumberOfSensors)
+         :OperationalModes (values-at inst "OperationModes/OperationMode")
+         :Characteristics (parse-characteristics inst)
+         :Sensors (map parse-sensor (select inst "Sensors/Sensor"))))
+
+(defn parse-echo10-xml
+  "Returns UMM-C collection structure from ECHO10 collection XML document."
+  [doc]
+  (js/coerce
+   {:EntryTitle (value-of doc "/Collection/DataSetId")
+    :EntryId    (value-of doc "/Collection/ShortName")
+    :Version    (value-of doc "/Collection/VersionId")
+    :Abstract   (value-of doc "/Collection/Description")
+    :CollectionDataType (value-of doc "/Collection/CollectionDataType")
+    :Purpose    (value-of doc "/Collection/SuggestedUsage")
+    :CollectionProgress (value-of doc "/Collection/CollectionState")
+    :AccessConstraints {:Description (value-of doc "/Collection/RestrictionComment")
+                        :Value (value-of doc "/Collection/RestrictionFlag")}
+    :Distributions [{:DistributionFormat (value-of doc "/Collection/DataFormat")
+                     :Fees (value-of doc "/Collection/Price")}]
+    :TemporalKeywords (values-at doc "/Collection/TemporalKeywords/Keyword")
+    :SpatialKeywords  (values-at doc "/Collection/SpatialKeywords/Keyword")
+    :TemporalExtents  (parse-temporal doc)
+    :Platforms (for [plat (select doc "/Collection/Platforms/Platform")]
+                 (assoc (fields-from plat :ShortName :LongName :Type)
+                        :Characteristics (parse-characteristics plat)
+                        :Instruments (map parse-instrument (select plat "Instruments/Instrument"))))
+    :ProcessingLevel {:Id (value-of doc "/Collection/ProcessingLevelId")
+                      :ProcessingLevelDescription (value-of doc "/Collection/ProcessingLevelDescription")}
+    :AdditionalAttributes (for [aa (select doc "/Collection/AdditionalAttributes/AdditionalAttribute")]
+                            (fields-from aa :Name :Description :DataType :ParameterRangeBegin
+                                         :ParameterRangeEnd :Value))}))
