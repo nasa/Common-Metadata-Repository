b8296cdf7cb72752f2e949c64779efe3d4f205c9
Jason Gilman <jason@element84.com>
2015-09-15 07:07:39 -0400

    CMR-1995: Removing project-sn and switching remaining uses to project-sn2.

--- search-app/src/cmr/search/services/query_execution/facets_results_feature.clj
+++ search-app/src/cmr/search/services/query_execution/facets_results_feature.clj
@@ -54,8 +54,7 @@
   "This is the aggregations map that will be passed to elasticsearch to request faceted results
   from a collection search."
   {:archive-center (terms-facet :archive-center)
-   ;; TODO Change to use project-sn2 field with CMR-1995
-   :project (terms-facet :project-sn)
+   :project (terms-facet :project-sn2)
    :platform (terms-facet :platform-sn)
    :instrument (terms-facet :instrument-sn)
    :sensor (terms-facet :sensor-sn)
@@ -73,8 +72,7 @@
   "This is the aggregations map that will be passed to elasticsearch to request faceted results
   from a collection search."
   {:archive-centers (nested-facet :archive-centers)
-   ;; TODO Change to use project-sn2 field with CMR-1995
-   :project (terms-facet :project-sn)
+   :project (terms-facet :project-sn2)
    :platforms (nested-facet :platforms)
    :instruments (nested-facet :instruments)
    :sensor (terms-facet :sensor-sn)
