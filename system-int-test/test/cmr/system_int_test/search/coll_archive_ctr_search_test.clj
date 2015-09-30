4f6b4353d832317e00905e5e89964a82fbe79fa5
Chris Durbin <christopher.d.durbin@nasa.gov>
2015-09-30 13:18:56 -0400

    CMR-2038: Add support for searching by data centers using a JSON Query.

--- system-int-test/test/cmr/system_int_test/search/coll_archive_ctr_search_test.clj
+++ system-int-test/test/cmr/system_int_test/search/coll_archive_ctr_search_test.clj
@@ -1,4 +1,4 @@
-(ns ^{:doc "Search CMR Collections by ArchiveCenter"}
+(ns ^{:doc "Search CMR Collections by Data Centers."}
   cmr.system-int-test.search.coll-archive-ctr-search-test
   (:require [clojure.test :refer :all]
             [cmr.system-int-test.utils.ingest-util :as ingest]
@@ -22,10 +22,17 @@
         coll7 (d/ingest "PROV2" (dc/collection {:organizations [(dc/org :archive-center "Sedac AC")
                                                                 (dc/org :processing-center "Sedac")]}))
         ;; KMS collections, but with different case for the short-names
-        coll8 (d/ingest "PROV1" (dc/collection
-                                  {:organizations [(dc/org :archive-center "Ucar/ncar/eoL/ceoPdm")]}))
-        coll9 (d/ingest "PROV1" (dc/collection
-                                  {:organizations [(dc/org :archive-center "Doi/uSGs/Cmg/wHSc")]}))]
+        kms-coll1 (d/ingest "PROV1"
+                            (dc/collection
+                              {:organizations [(dc/org :archive-center "Ucar/ncar/eoL/ceoPdm")]}))
+        kms-coll2 (d/ingest "PROV1"
+                            (dc/collection
+                              {:organizations [(dc/org :archive-center "Doi/uSGs/Cmg/wHSc")]}))
+        ;; DIF collections support distribution-center data centers
+        dif-coll1 (d/ingest "PROV1"
+                            (dc/collection-dif
+                              {:organizations [(dc/org :distribution-center "Dist center")]})
+                            {:format :dif})]
 
     (index/wait-until-indexed)
 
@@ -80,7 +87,7 @@
            [] {:archive_center {:short_name "BLAH"}}
            [coll4 coll5 coll6 coll7] {:or [{:archive_center {:short_name "SEDAC AC"}} {:archive_center {:short_name "Larc"}}]}
            [] {:and [{:archive_center {:short_name "SEDAC AC"}} {:archive_center {:short_name "Larc"}}]}
-           [coll1 coll2 coll3 coll4 coll6 coll8 coll9] {:not {:archive_center {:short_name "SEDAC AC"}}}
+           [coll1 coll2 coll3 coll4 coll6 kms-coll1 kms-coll2 dif-coll1] {:not {:archive_center {:short_name "SEDAC AC"}}}
 
            ;; Wildcards
            [coll5 coll7] {:archive_center {:short_name "S*" :pattern true}}
@@ -93,24 +100,73 @@
            [] {:archive_center {:short_name "sedac ac" :ignore_case false}}
 
            ;; Test searching on KMS subfields
-           [coll8 coll9] {:archive_center {:level_0 "GOVERNMENT AGENCIES-U.S. FEDERAL AGENCIES"
+           [kms-coll1 kms-coll2] {:archive_center {:level_0 "GOVERNMENT AGENCIES-U.S. FEDERAL AGENCIES"
                                            :ignore_case false}}
            [] {:archive_center {:level_0 "government agencies-u.s. federal agencies" :ignore_case false}}
-           [coll8 coll9] {:archive_center {:level_0 "government agencies-u.s. federal agencies"
+           [kms-coll1 kms-coll2] {:archive_center {:level_0 "government agencies-u.s. federal agencies"
                                            :ignore_case true}}
-           [coll8] {:archive_center {:level_1 "NSF"}}
-           [coll9] {:archive_center {:level_2 "USGS"}}
-           [coll9] {:archive_center {:level_3 "Added level 3 value"}}
+           [kms-coll1] {:archive_center {:level_1 "NSF"}}
+           [kms-coll2] {:archive_center {:level_2 "USGS"}}
+           [kms-coll2] {:archive_center {:level_3 "Added level 3 value"}}
 
            ;; Short name uses KMS case rather than metadata case
            [] {:archive_center {:short_name "Ucar/ncar/eoL/ceoPdm" :ignore_case false}}
-           [coll8] {:archive_center {:short_name "Ucar/ncar/eoL/ceoPdm" :ignore_case true}}
-           [coll8] {:archive_center {:short_name "UCAR/NCAR/EOL/CEOPDM" :ignore_case false}}
+           [kms-coll1] {:archive_center {:short_name "Ucar/ncar/eoL/ceoPdm" :ignore_case true}}
+           [kms-coll1] {:archive_center {:short_name "UCAR/NCAR/EOL/CEOPDM" :ignore_case false}}
 
-           [coll8] {:archive_center {:long_name "ceop data M?nagement*" :pattern true}}
+           [kms-coll1] {:archive_center {:long_name "ceop data M?nagement*" :pattern true}}
            [] {:archive_center {:long_name "ceop data M?nagement*"}}
-           [coll8] {:archive_center {:url "http://www.eol.ucar.edu/projects/ceop/dm/"}}
-           [coll9] {:archive_center {:uuid "69db99c6-54d6-40b9-9f72-47eab9c34869"}}
-           [coll9] {:archive_center {:any "69db99c6*" :pattern true}}))))
+           [kms-coll1] {:archive_center {:url "http://www.eol.ucar.edu/projects/ceop/dm/"}}
+           [kms-coll2] {:archive_center {:uuid "69db99c6-54d6-40b9-9f72-47eab9c34869"}}
+           [kms-coll2] {:archive_center {:any "69db99c6*" :pattern true}}))
+
+    (testing "Search collections by data center using JSON Query."
+      (are [items search]
+           (d/refs-match? items (search/find-refs-with-json-query :collection {} search))
+
+           ;; Finds processing centers
+           [coll3 coll4 coll6] {:data_center {:short_name "Larc"}}
+           [coll5] {:data_center {:short_name "SEDAC PC"}}
+           [coll3 coll4 coll5 coll6 coll7] {:or [{:data_center {:short_name "SEDAC AC"}} {:data_center {:short_name "Larc"}}]}
+
+           ;; Finds distribution centers
+           [dif-coll1] {:data_center {:short_name "Dist center"}}
+
+           [coll5 coll7] {:data_center {:short_name "SEDAC AC"}}
+           [coll5 coll7] {:data_center {:short_name "Sedac AC"}}
+           [] {:data_center {:short_name "BLAH"}}
+           [] {:and [{:data_center {:short_name "SEDAC AC"}} {:data_center {:short_name "Larc"}}]}
+           [coll1 coll2 coll3 coll4 coll6 kms-coll1 kms-coll2 dif-coll1] {:not {:data_center {:short_name "SEDAC AC"}}}
+
+           ;; Wildcards
+           [coll5 coll7] {:data_center {:short_name "S*" :pattern true}}
+           [coll5 coll7] {:data_center {:short_name "SEDAC ?C" :pattern true}}
+           [coll5] {:data_center {:short_name "SEDAC ?C" :pattern true :ignore_case false}}
+           [] {:data_center {:short_name "*Q*" :pattern true}}
+
+           ;; Ignore case
+           [coll5 coll7] {:data_center {:short_name "sedac ac" :ignore_case true}}
+           [] {:data_center {:short_name "sedac ac" :ignore_case false}}
+
+           ;; Test searching on KMS subfields
+           [kms-coll1 kms-coll2] {:data_center {:level_0 "GOVERNMENT AGENCIES-U.S. FEDERAL AGENCIES"
+                                           :ignore_case false}}
+           [] {:data_center {:level_0 "government agencies-u.s. federal agencies" :ignore_case false}}
+           [kms-coll1 kms-coll2] {:data_center {:level_0 "government agencies-u.s. federal agencies"
+                                           :ignore_case true}}
+           [kms-coll1] {:data_center {:level_1 "NSF"}}
+           [kms-coll2] {:data_center {:level_2 "USGS"}}
+           [kms-coll2] {:data_center {:level_3 "Added level 3 value"}}
+
+           ;; Short name uses KMS case rather than metadata case
+           [] {:data_center {:short_name "Ucar/ncar/eoL/ceoPdm" :ignore_case false}}
+           [kms-coll1] {:data_center {:short_name "Ucar/ncar/eoL/ceoPdm" :ignore_case true}}
+           [kms-coll1] {:data_center {:short_name "UCAR/NCAR/EOL/CEOPDM" :ignore_case false}}
+
+           [kms-coll1] {:data_center {:long_name "ceop data M?nagement*" :pattern true}}
+           [] {:data_center {:long_name "ceop data M?nagement*"}}
+           [kms-coll1] {:data_center {:url "http://www.eol.ucar.edu/projects/ceop/dm/"}}
+           [kms-coll2] {:data_center {:uuid "69db99c6-54d6-40b9-9f72-47eab9c34869"}}
+           [kms-coll2] {:data_center {:any "69db99c6*" :pattern true}}))))
 
 
