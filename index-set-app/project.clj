b247717d859ea0731b211bca3ce0591f118053fe
Jason Gilman <jason@element84.com>
2015-10-09 06:35:12 -0400

    Fixing index set app

--- index-set-app/project.clj
+++ index-set-app/project.clj
@@ -6,6 +6,7 @@
                  [nasa-cmr/cmr-acl-lib "0.1.0-SNAPSHOT"]
                  [nasa-cmr/cmr-elastic-utils-lib "0.1.0-SNAPSHOT"]
                  [nasa-cmr/cmr-common-app-lib "0.1.0-SNAPSHOT"]
+                 [org.clojure/tools.nrepl "0.2.11"]
                  [compojure "1.4.0"]
                  [ring/ring-core "1.4.0" :exclusions [clj-time]]
                  [ring/ring-json "0.4.0"]]
