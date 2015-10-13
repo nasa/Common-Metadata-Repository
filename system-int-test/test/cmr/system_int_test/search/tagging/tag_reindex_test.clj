e42bab63ae7981c4a7d6633fc94df986effd26b3
Yonggang Liu <ygliuvt@gmail.com>
2015-10-13 09:07:02 -0400

    CMR-2088: Added re-index tags tests.

---
+++ system-int-test/test/cmr/system_int_test/search/tagging/tag_reindex_test.clj
@@ -0,0 +1,45 @@
+(ns cmr.system-int-test.search.tagging.tag-reindex-test
+  "This tests re-index tags."
+  (:require [clojure.test :refer :all]
+            [cmr.system-int-test.utils.ingest-util :as ingest]
+            [cmr.system-int-test.utils.index-util :as index]
+            [cmr.system-int-test.utils.tag-util :as tags]
+            [cmr.mock-echo.client.echo-util :as e]
+            [cmr.system-int-test.system :as s]))
+
+(use-fixtures :each (join-fixtures [(ingest/reset-fixture {})
+                                    tags/grant-all-tag-fixture]))
+
+
+(deftest reindex-tags-test
+  (let [user1-token (e/login (s/context) "user1")
+        user2-token (e/login (s/context) "user2")
+        tag1 (tags/save-tag user1-token (tags/make-tag
+                                          {:namespace "Namespace1"
+                                           :value "Value1"
+                                           :category "Category1"}))
+        tag2 (tags/save-tag user2-token (tags/make-tag
+                                          {:namespace "Namespace1"
+                                           :value "Value2"}))
+        tag3 (tags/save-tag user1-token (tags/make-tag
+                                          {:namespace "Namespace2"
+                                           :value "Value1"
+                                           :category "Category2"}))
+        all-tags [tag1 tag2 tag3]]
+    (index/wait-until-indexed)
+
+    ;; Now I should find all tags when searching
+    (tags/assert-tag-search all-tags (tags/search {}))
+
+    ;; Delete tags from elasticsearch index
+    (index/delete-all-tags-from-elastic)
+
+    ;; Now searching tags finds nothing
+    (tags/assert-tag-search [] (tags/search {}))
+
+    ;; Re-index all tags
+    (index/reindex-tags)
+    (index/wait-until-indexed)
+
+    ;; Now I should find all tags when searching
+    (tags/assert-tag-search all-tags (tags/search {}))))
