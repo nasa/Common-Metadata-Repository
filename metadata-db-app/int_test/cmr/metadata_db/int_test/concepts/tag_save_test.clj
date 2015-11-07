8bed559d1da86478f001b383b197cd48e2db95e9
James Norton <james@element84.com>
2015-11-06 21:31:06 -0500

    CMR-2146: Reverted inadvertent changed to tag_save_test

--- metadata-db-app/int_test/cmr/metadata_db/int_test/concepts/tag_save_test.clj
+++ metadata-db-app/int_test/cmr/metadata_db/int_test/concepts/tag_save_test.clj
@@ -34,128 +34,52 @@
           422
           ["Tag could not be associated with provider [REG_PROV]. Tags are system level entities."])))
 
-(deftest save-group-with-concept-id
-  (doseq [provider-id ["REG_PROV" "SMAL_PROV1"]]
-    (let [serv-concept-id (str "S10-" provider-id)
-          group (util/group-concept provider-id 1 {:concept-id serv-concept-id})
-          {:keys [status revision-id concept-id]} (util/save-concept group)]
+(deftest save-tag-with-concept-id
+  (testing "with concept-id"
+    ;; valid save with concept-id
+    (let [tag (util/tag-concept 1 {:concept-id "T10-CMR"})
+          {:keys [status revision-id concept-id]} (util/save-concept tag)]
       (is (= 201 status))
       (is (= revision-id 1))
-      (util/verify-concept-was-saved (assoc group :revision-id revision-id :concept-id concept-id))
+      (util/verify-concept-was-saved
+        (assoc tag :revision-id revision-id :provider-id "CMR"))
 
-      (testing "with incorrect native id"
-        (let [response (util/save-concept (assoc group :native-id "foo"))]
-          (is (= {:status 409,
-                  :errors [(msg/concept-exists-with-different-id
-                             serv-concept-id (:native-id group)
-                             serv-concept-id "foo"
-                             :group provider-id) ]}
-                 (select-keys response [:status :errors])))))
+      (testing "Get Tag Concept Id"
+        (is (= {:status 200 :concept-id concept-id :errors nil}
+               (util/get-concept-id :tag "CMR" (:native-id tag)))))
 
-      (testing "with incorrect concept id"
-        (let [other-serv-concept-id (str "S11-" provider-id)
-              response (util/save-concept (assoc group :concept-id other-serv-concept-id))]
-          (is (= {:status 409,
-                  :errors [(msg/concept-exists-with-different-id
-                             serv-concept-id (:native-id group)
-                             other-serv-concept-id (:native-id group)
-                             :group provider-id) ]}
-                 (select-keys response [:status :errors])))))
-
-      (testing "with incorrect concept id matching another concept"
-        (let [other-serv-concept-id (str "S11-" provider-id)
-              group2 (util/group-concept provider-id 2
-                                             {:concept-id other-serv-concept-id
-                                              :native-id "native2"})
-              _ (is (= 201 (:status (util/save-concept group2))))
-              response (util/save-concept (assoc group :concept-id other-serv-concept-id))]
-          (is (= {:status 409,
-                  :errors [(msg/concept-exists-with-different-id
-                             serv-concept-id (:native-id group)
-                             other-serv-concept-id (:native-id group)
-                             :group provider-id) ]}
-                 (select-keys response [:status :errors]))))))))
-
-(deftest save-group-with-revision-date-test
-  (doseq [provider-id ["REG_PROV" "SMAL_PROV1"]]
-    (let [concept (util/group-concept provider-id 1 {:revision-date (t/date-time 2001 1 1 12 12 14)})
-          {:keys [status revision-id concept-id]} (util/save-concept concept)]
-      (is (= 201 status))
-      (is (= revision-id 1))
-      (let [retrieved-concept (util/get-concept-by-id-and-revision concept-id revision-id)]
-        (is (= (:revision-date concept) (:revision-date (:concept retrieved-concept))))))))
-
-(deftest save-group-with-bad-revision-date-test
-  (doseq [provider-id ["REG_PROV" "SMAL_PROV1"]]
-    (let [concept (util/group-concept provider-id 1 {:revision-date "foo"})
-          {:keys [status errors]} (util/save-concept concept)]
-      (is (= 422 status))
-      (is (= ["[foo] is not a valid datetime"] errors)))))
-
-(deftest save-group-with-same-native-id-test
-  (testing "Save groups with the same native-id for two small providers is OK"
-    (let [serv1 (util/create-and-save-group "SMAL_PROV1" 1 1 {:native-id "foo"})
-          serv2 (util/create-and-save-group "SMAL_PROV2" 2 1 {:native-id "foo"})
-          [serv1-concept-id serv2-concept-id] (map :concept-id [serv1 serv2])]
-      (util/verify-concept-was-saved serv1)
-      (util/verify-concept-was-saved serv2)
-      (is (not= serv1-concept-id serv2-concept-id)))))
-
-(deftest save-group-test-with-proper-revision-id-test
-  (doseq [provider-id ["REG_PROV" "SMAL_PROV1"]]
-    (let [concept (util/group-concept provider-id 1)]
-      ;; save the concept once
-      (let [{:keys [revision-id concept-id]} (util/save-concept concept)
-            new-revision-id (inc revision-id)
-            revision-date-0 (get-in (util/get-concept-by-id-and-revision concept-id revision-id)
-                                    [:concept :revision-date])]
-        ;; save it again with a valid revision-id
-        (let [updated-concept (assoc concept :revision-id new-revision-id :concept-id concept-id)
-              {:keys [status revision-id]} (util/save-concept updated-concept)
-              revision-date-1 (get-in (util/get-concept-by-id-and-revision concept-id revision-id)
-                                      [:concept :revision-date])]
+      (testing "new revision"
+        (let [{:keys [status revision-id]} (util/save-concept (assoc tag :revision-id 2))]
           (is (= 201 status))
-          (is (= revision-id new-revision-id))
-          (is (t/after? revision-date-1 revision-date-0))
-          (util/verify-concept-was-saved updated-concept))))))
+          (is (= revision-id 2))
+          (util/verify-concept-was-saved
+            (assoc tag :revision-id revision-id :provider-id "CMR"))))
 
-(deftest save-group-with-skipped-revisions-test
-  (doseq [provider-id ["REG_PROV" "SMAL_PROV1"]]
-    (let [concept (util/group-concept provider-id 1)
-          {:keys [concept-id]} (util/save-concept concept)
-          concept-with-skipped-revisions (assoc concept :concept-id concept-id :revision-id 100)
-          {:keys [status revision-id]} (util/save-concept concept-with-skipped-revisions)
-          {retrieved-concept :concept} (util/get-concept-by-id concept-id)]
-      (is (= 201 status))
-      (is (= 100 revision-id (:revision-id retrieved-concept))))))
+      ;; error cases
+      (are2 [test-tag exp-status exp-errors]
+            (let [{:keys [status errors]} (util/save-concept test-tag)]
+              (is (= exp-status status))
+              (is (= (set exp-errors) (set errors))))
 
-(deftest auto-increment-of-revision-id-works-with-skipped-revisions-test
-  (doseq [provider-id ["REG_PROV" "SMAL_PROV1"]]
-    (let [concept (util/group-concept provider-id 1)
-          {:keys [concept-id]} (util/save-concept concept)
-          concept-with-concept-id (assoc concept :concept-id concept-id)
-          _ (util/save-concept (assoc concept-with-concept-id :revision-id 100))
-          {:keys [status revision-id]} (util/save-concept concept-with-concept-id)
-          {retrieved-concept :concept} (util/get-concept-by-id concept-id)]
-      (is (= 201 status))
-      (is (= 101 revision-id (:revision-id retrieved-concept))))))
+            "conflicting native-id"
+            (assoc tag :native-id "foo")
+            409
+            [(str "A concept with concept-id [T10-CMR] and native-id [namespace1" (char 29)
+                  "value1] already exists for concept-type [:tag] provider-id [CMR]. The given "
+                  "concept-id [T10-CMR] and native-id [foo] would conflict with that one.")]
 
-(deftest save-group-with-low-revision-test
-  (doseq [provider-id ["REG_PROV" "SMAL_PROV1"]]
-    (let [concept (util/group-concept provider-id 1)
-          {:keys [concept-id]} (util/save-concept concept)
-          concept-with-bad-revision (assoc concept :concept-id concept-id :revision-id 0)
-          {:keys [status revision-id]} (util/save-concept concept-with-bad-revision)]
-      (is (= 409 status))
-      (is (nil? revision-id)))))
 
-(deftest save-group-with-missing-required-field
-  (doseq [provider-id ["REG_PROV" "SMAL_PROV1"]]
-    (let [concept (util/group-concept provider-id 1)]
-      (are [field] (let [{:keys [status errors]} (util/save-concept (dissoc concept field))]
-                     (and (= 422 status)
-                          (re-find (re-pattern (name field)) (first errors))))
-           :concept-type
-           :provider-id
-           :native-id
-           :extra-fields))))
+            "conflicting concept-id"
+            (assoc tag :concept-id "T11-CMR")
+            409
+            [(str "A concept with concept-id [T10-CMR] and native-id [namespace1" (char 29)
+                  "value1] already exists for concept-type [:tag] provider-id [CMR]. The given "
+                  "concept-id [T11-CMR] and native-id [namespace1" (char 29) "value1] "
+                  "would conflict with that one.")]
+
+            "incorrect concept-id prefix"
+            (assoc (util/tag-concept 2) :concept-id "G10-CMR")
+            422
+            [(str "Concept-id [G10-CMR] for concept does not match provider-id [CMR] or "
+                  "concept-type [:tag].")]))))
+
