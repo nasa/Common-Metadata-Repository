2a53aea6bfb9bd22597e96167cd90d4851e91157
John Cromartie <john@element84.com>
2015-11-09 13:28:12 -0500

    CMR-2118 Adds code to support indexing of UMM-JSON metadata.
    Incoming UMM-JSON metadata is converted to ECHO 10 by the umm-spec lib
    and then parsed again via the old-style umm lib to make use of the
    existing indexing code.

--- indexer-app/src/cmr/indexer/data/concept_parser.clj
+++ indexer-app/src/cmr/indexer/data/concept_parser.clj
@@ -1,7 +1,9 @@
 (ns cmr.indexer.data.concept-parser
   "Contains helper functions to parse a concept for indexing."
   (:require [clojure.edn :as edn]
-            [cmr.umm.core :as umm]))
+            [cmr.common.mime-types :as mt]
+            [cmr.umm.core :as umm]
+            [cmr.umm-spec.core :as umm-spec]))
 
 (defmulti parse-concept
   "Parse the metadata from a concept map into a UMM model or map containing data needed for
@@ -15,4 +17,13 @@
 
 (defmethod parse-concept :default
   [concept]
-  (umm/parse-concept concept))
+  (if (= (:format concept) mt/umm-json)
+    ;; For UMM-JSON metadata, we will use the umm-spec lib to convert it to ECHO10, then parse it
+    ;; with the old umm lib for indexing. This will be updated when umm-spec is ready to handle all
+    ;; metadata ingest and indexing.
+    (let [model (umm-spec/parse-metadata (:concept-type concept) :umm-json (:metadata concept))
+          echo10-metadata (umm-spec/generate-metadata (:concept-type concept) :echo10 model)]
+      (umm/parse-concept (assoc concept
+                                :format mt/echo10
+                                :metadata echo10-metadata)))
+    (umm/parse-concept concept)))
