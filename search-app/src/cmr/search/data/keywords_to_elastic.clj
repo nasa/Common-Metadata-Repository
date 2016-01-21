d0a0bf68e15aee0bbeb7b30ade4e788bd7a54f39
James Norton <james@element84.com>
2016-01-21 10:54:37 -0500

    CMR-1335: Removed old defs for default boosts

--- search-app/src/cmr/search/data/keywords_to_elastic.clj
+++ search-app/src/cmr/search/data/keywords_to_elastic.clj
@@ -37,50 +37,16 @@
   for details on the function score query."
   (:require [clojure.string :as str]))
 
-;; TODO - Arey these really necessary now? Maybe just include them directly in the map.
-
-(def short-name-long-name-boost
-  "The boost to apply to the short-name/long-name component of the keyword matching"
-  1.4)
-
-(def project-boost
-  "The boost to apply to the campaign short name / long name fields"
-  1.3)
-
-(def platform-boost
-  "The boost to apply to the platform short name / long name"
-  1.3)
-
-(def instrument-boost
-  "The boost to apply to the instrument short name / long name"
-  1.2)
-
-(def sensor-boost
-  "The boost to apply to the sensor short name / long name"
-  1.2)
-
-(def spatial-keyword-boost
-  "The boost to apply to the spatial keyword"
-  1.1)
-
-(def science-keywords-boost
-  "The boost to apply to the science keyword field"
-  1.2)
-
-(def temporal-keyword-boost
-  "The boost to apply to the temporal keyword field"
-  1.1)
-
 (def default-boosts
   "Field boosts to use if not provided."
-  {:short-name short-name-long-name-boost
-   :project project-boost
-   :platform platform-boost
-   :instrument instrument-boost
-   :sensor sensor-boost
-   :science-keywords science-keywords-boost
-   :spatial-keyword spatial-keyword-boost
-   :temporal-keyword temporal-keyword-boost
+  {:short-name 1.4
+   :project 1.3
+   :platform 1.3
+   :instrument 1.2
+   :sensor 1.2
+   :science-keywords 1.2
+   :spatial-keyword 1.1
+   :temporal-keyword 1.1
    :version-id 1.0
    :entry-title 1.0
    :provider 1.0
