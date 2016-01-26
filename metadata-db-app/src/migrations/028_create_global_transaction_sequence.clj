74dd96e1829b8e22e75280a6d0e9635809121708
James Norton <james@element84.com>
2016-01-26 13:59:11 -0500

    CMR-2362: Removed unneeded code

--- metadata-db-app/src/migrations/028_create_global_transaction_sequence.clj
+++ metadata-db-app/src/migrations/028_create_global_transaction_sequence.clj
@@ -8,18 +8,17 @@
   []
   (println "migrations.028-create-global-transaction-sequence up...")
   (when-not (h/sequence-exists? "global_transaction_id_seq")
-    (let [table-name (h/get-all-concept-tablenames)
-          max-rev-id (reduce (fn [acc t]
-                              (let [resp (h/query (format "select max(revision_id) as max_rev_id from %s" t))
-                                    value (-> resp first :max_rev_id)]
-                                (if value
-                                  (max acc (long value))
-                                  acc)))
-                      0
-                      (h/get-all-concept-tablenames))]
-     (h/sql
-       (format "CREATE SEQUENCE METADATA_DB.global_transaction_id_seq START WITH %s INCREMENT BY 1 CACHE 400"
-               (+ 100000 max-rev-id))))))
+    (let [max-rev-id (reduce (fn [max-rev table]
+                               (let [resp (h/query (format "select max(revision_id) as max_rev_id from %s" table))
+                                     value (-> resp first :max_rev_id)]
+                                 (if value
+                                   (max max-rev (long value))
+                                   max-rev)))
+                             0
+                             (h/get-all-concept-tablenames))]
+      (h/sql
+        (format "CREATE SEQUENCE METADATA_DB.global_transaction_id_seq START WITH %s INCREMENT BY 1 CACHE 400"
+                (+ 100000 max-rev-id))))))
 
 (defn down
   "Migrates the database down from version 28."
