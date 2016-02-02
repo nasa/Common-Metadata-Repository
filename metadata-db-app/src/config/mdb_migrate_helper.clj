ece6316e441b986fd309441331f552785943d6c2
James Norton <james@element84.com>
2016-02-02 17:35:50 -0500

    CMR-2326: Fixed error caused by parinfer

--- metadata-db-app/src/config/mdb_migrate_helper.clj
+++ metadata-db-app/src/config/mdb_migrate_helper.clj
@@ -52,7 +52,7 @@
   ([]
    ;; use all concept types
    (apply get-concept-tablenames (keys s/num-revisions-to-keep-per-concept-type)))
-  ([& concept-types
+  ([& concept-types]
     (distinct
      (->
       (for [provider (p/get-providers (config/db))
@@ -64,7 +64,7 @@
       (into (when (contains? (set concept-types) :granule) ["small_prov_granules"]))
       (into (when (contains? (set concept-types) :service) ["small_prov_services"]))
       (into (when (contains? (set concept-types) :access-group) ["cmr_groups"]))
-      (into (when (contains? (set concept-types) :tag) ["cmr_tags"]))))]))
+      (into (when (contains? (set concept-types) :tag) ["cmr_tags"]))))))
 
 (defn get-collection-tablenames
   "Gets a list of all the collection tablenames. Primarily for enabling migrations of existing
