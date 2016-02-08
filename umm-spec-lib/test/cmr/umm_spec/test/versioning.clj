46a34e1726683858dad099d0e482291f5f5090ac
John Cromartie <john@element84.com>
2016-02-08 08:20:56 -0500

    CMR-2354 Add property-based test for UMM version migrations

--- umm-spec-lib/test/cmr/umm_spec/test/versioning.clj
+++ umm-spec-lib/test/cmr/umm_spec/test/versioning.clj
@@ -1,12 +1,25 @@
 (ns cmr.umm-spec.test.versioning
-  (:require [cmr.umm-spec.versioning :as v]
-            [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [cmr.common.mime-types :as mt]
+            [cmr.common.test.test-check-ext :as ext :refer [defspec]]
+            [cmr.umm-spec.versioning :as v]
+            [cmr.umm-spec.test.umm-generators :as umm-gen]
+            [clojure.test.check.generators :as gen]
+            [com.gfredericks.test.chuck.clojure-test :refer [for-all]]
+            [cmr.umm-spec.core :as core]))
 
 (deftest test-version-steps
   (with-redefs [cmr.umm-spec.versioning/versions ["1.0" "1.1" "1.2" "1.3"]]
     (is (= [] (v/version-steps "1.2" "1.2")))
     (is (= [["1.1" "1.2"] ["1.2" "1.3"]] (v/version-steps "1.1" "1.3")))
     (is (= [["1.2" "1.1"] ["1.1" "1.0"]] (v/version-steps "1.2" "1.0")))))
+
+(defspec all-migrations-produce-valid-umm-spec 100
+  (for-all [umm-record   (gen/no-shrink umm-gen/umm-c-generator)
+            dest-version (gen/elements v/versions)]
+    (let [dest-media-type (str mt/umm-json "; version=" dest-version)
+          metadata (core/generate-metadata umm-record dest-media-type)]
+      (empty? (core/validate-metadata :collection dest-media-type metadata)))))
 
 (deftest migrate-1_0-up-to-1_1
   (is (nil?
