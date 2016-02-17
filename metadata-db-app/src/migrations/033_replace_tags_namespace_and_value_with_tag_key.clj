8b9e6b4fd0434e28f8a44a570ac624a4b3f9e5f5
James Norton <james@element84.com>
2016-02-17 16:25:12 -0500

    CMR-2480: Added migration to drop category,namespace,value and add tag-key to cmr_tags

---
+++ metadata-db-app/src/migrations/033_replace_tags_namespace_and_value_with_tag_key.clj
@@ -0,0 +1,48 @@
+(ns migrations.033-replace-tags-namespace-and-value-with-tag-key
+  (:require [clojure.java.jdbc :as j]
+            [clojure.edn :as edn]
+            [config.migrate-config :as config]
+            [cmr.common.util :as util]
+            [config.mdb-migrate-helper :as h]))
+
+(defn up
+  "Migrates the database up to version 33."
+  []
+  (println "migrations.033-replace-tags-namespace-and-value-with-tag-key up...")
+  (doseq [result (h/query "SELECT * from cmr_tags")]
+    (let [{:keys [id metadata]} result
+          metadata (-> metadata
+                       (util/gzip-blob->string)
+                       (edn/read-string))
+          ;; Some rows may already have a tag-key instead of namespace and value.
+          {:keys [namespace value tag-key]} metadata
+          tag-key (or tag-key
+                      (str namespace "." value))
+          metadata (-> metadata
+                       (assoc :tag-key tag-key)
+                       (dissoc :namespace :value :category)
+                       (prn-str)
+                       (util/string->gzip-bytes))
+          result (assoc result :metadata metadata :native_id tag-key)]
+      (j/update! (config/db) "cmr_tags" result ["id = ?" id]))))
+
+(defn down
+  "Migrates the database down from version 33."
+  []
+  (println "migrations.033-replace-tags-namespace-and-value-with-tag-key down...")
+  (println "NOTE: category field and original native-id cannot be recoverd")
+  (doseq [result (h/query "SELECT * from cmr_tags")]
+    (let [{:keys [id metadata]} result
+          metadata (-> metadata
+                       (util/gzip-blob->string)
+                       (edn/read-string))
+          tag-key (:tag-key metadata)
+          [_ namespace value] (re-matches #"(.*)\.(.*$)" tag-key)
+          metadata (-> metadata
+                       (assoc :namespace namespace
+                              :value value)
+                       (dissoc :tag-key)
+                       (prn-str)
+                       (util/string->gzip-bytes))
+          result (assoc result :metadata metadata)]
+      (j/update! (config/db) "cmr_tags" result ["id = ?" id]))))
