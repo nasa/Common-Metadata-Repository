384563a2b82348aca73812d05219f13b6bf4faf9
Yonggang Liu <ygliuvt@gmail.com>
2016-02-25 11:06:08 -0500

    CMR-2482: Took out tag and tag-association from the cleanup old revisions job due to force delete issue.

--- metadata-db-app/src/cmr/metadata_db/services/jobs.clj
+++ metadata-db-app/src/cmr/metadata_db/services/jobs.clj
@@ -30,8 +30,6 @@
     (concept-service/delete-old-revisions context provider :collection)
     (concept-service/delete-old-revisions context provider :granule)
     (concept-service/delete-old-revisions context provider :service)
-    (concept-service/delete-old-revisions context provider :tag)
-    (concept-service/delete-old-revisions context provider :tag-association)
     (concept-service/delete-old-revisions context provider :access-group)))
 
 (def-stateful-job OldRevisionConceptCleanupJob
