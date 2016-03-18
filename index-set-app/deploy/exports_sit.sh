fdd3776e8b6ec5877f5ab58c60e248eff189e1b4
Jason Gilman <jason@element84.com>
2016-03-18 13:04:50 -0400

    CMR-2562: Removing deployment files that shouldn't be committed.

--- index-set-app/deploy/exports_sit.sh
+++
@@ -1,4 +0,0 @@
-export cap_application=index-set
-export cap_user=clj-index-set
-export cap_deploy_to=/data/clojure/index-set
-export cap_servers=cmr-sit-app1.dev.echo.nasa.gov,cmr-sit-app2.dev.echo.nasa.gov
--- index-set-app/deploy/exports_wl.sh
+++
@@ -1,4 +0,0 @@
-export cap_application=index-set
-export cap_user=clj-index-set
-export cap_deploy_to=/data/clojure/index-set
-export cap_servers=cmr-wl-app1.dev.echo.nasa.gov,cmr-wl-app2.dev.echo.nasa.gov,cmr-wl-app3.dev.echo.nasa.gov
