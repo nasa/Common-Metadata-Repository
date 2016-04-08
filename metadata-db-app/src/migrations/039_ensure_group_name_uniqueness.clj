e9a11e0c1045f3a749eb062cf8ee004a27310457
Catalino Cuadrado <catalino.cuadrado@nasa.gov>
2016-04-08 13:28:56 -0400

    CMR-2466 Update migration to avoid conflict

--- metadata-db-app/src/migrations/039_ensure_group_name_uniqueness.clj
+++ metadata-db-app/src/migrations/039_ensure_group_name_uniqueness.clj
@@ -1,14 +1,11 @@
-(ns migrations.038-ensure-group-name-uniqueness
+(ns migrations.039-ensure-group-name-uniqueness
   (:require [config.mdb-migrate-helper :as h]
             [cmr.metadata-db.services.concept-validations :as v]))
 
-;; NOTE: This migration is the same as 036, with the exception of the start value
-;; of the migration_transaction_id_seq sequence.
-
 (defn up
-  "Migrates the database up to version 36."
+  "Migrates the database up to version 39."
   []
-  (println "migrations.038-ensure-group-name-uniqueness up...")
+  (println "migrations.039-ensure-group-name-uniqueness up...")
   ;; Lowercase all native_id fields so that we can detect duplicates.
   ;; Will throw an exception if duplicate group names are detected, they must be cleaned up manually.
 
@@ -20,7 +17,7 @@
   (h/sql "UPDATE METADATA_DB.CMR_GROUPS A SET A.native_id = LOWER(A.native_id)"))
 
 (defn down
-  "Migrates the database down from version 36."
+  "Migrates the database down from version 39."
   []
-  (println "migrations.038-ensure-group-name-uniqueness down...")
+  (println "migrations.039-ensure-group-name-uniqueness down...")
   (println "nothing to do: previous transaction-id values cannot be restored"))
