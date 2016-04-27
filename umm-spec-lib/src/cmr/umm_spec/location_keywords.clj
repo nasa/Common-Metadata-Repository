a574a67dbc32a9559f9586bbca2edc8166dc7ea7
Catalino Cuadrado <catalino.cuadrado@nasa.gov>
2016-04-27 18:25:18 -0400

    CMR-2716 Refactor location keywords to facilitate testing

--- umm-spec-lib/src/cmr/umm_spec/location_keywords.clj
+++ umm-spec-lib/src/cmr/umm_spec/location_keywords.clj
@@ -1,6 +1,6 @@
 (ns cmr.umm-spec.location-keywords
-  (:require [cmr.umm-spec.models.collection :as c]
-            [cmr.common-app.services.kms-fetcher :as kf]))
+  (:require [cmr.common-app.services.kms-fetcher :as kf]
+            [clojure.string :as str]))
 
 (def duplicate-keywords
   "Temporary lookup table to account for any duplicate keywords. Key is :uuid which is a field in
@@ -8,32 +8,34 @@
   {"BLACK SEA" {:uuid "afbc0a01-742e-49da-939e-3eaa3cf431b0"}
    "SPACE" {:uuid "6f2c3b1f-acae-4af0-a759-f0d57ccfc83f"}})
 
-(def local-test-system-context
-  {:system (get-in user/system [:apps :indexer])})
-
-(defn gcmd-spatial-keywords-map
-  [context]
+(defn spatial-keywords-map
+  [full-map]
   "Returns a list of maps that correspond to the spatial keywords hierarchy of GCMD keywords.
   Keys are :category :type :subregion-1 :subregion-2 :subregion-3 :uuid. Values are single string-type.
   Not all keys are present in every map."
-  (vals (:spatial-keywords (kf/get-gcmd-keywords-map context))))
+  (vals (:spatial-keywords full-map)))
 
 (defn find-spatial-keyword-in-map
   "Finds a spatial keyword in the gcmd spatial-keywords map.
-  Takes a string keyword as a parameter, e.g. 'OCEAN' and returns a list of maps of hierarchies
+  Takes a string keyword as a parameter, e.g. 'OCEAN' and a list of spatial keyword maps; returns a list of maps of hierarchies
   which contain the keyword."
-  [context keyword]
-  (filter (fn [map] (some #{keyword} (vals map))) (gcmd-spatial-keywords-map context)))
+  [keyword-map-list keyword]
+  (filter (fn [map] (some #{keyword} (vals map))) keyword-map-list))
 
 (defn find-spatial-keyword
   "Finds spatial keywords in the heirarchy and pick the one with the fewest keys
-  (e.g. highest hierarchical depth.) Takes a string keyword as a parameter and returns the map of
-  hierarichies which contain the keyword. You can also look by entering the uuid of the entry
-  if you know it."
-  [context keyword]
-  (first (sort-by count (find-spatial-keyword-in-map context keyword))))
+  (e.g. highest hierarchical depth.) Takes a string keyword as a parameter (assumes no case) and
+  returns the map of hierarichies which contain the keyword. You can also look by entering the
+  uuid of the entry if you know it."
+  [keyword-map-list keyword]
+  (first (sort-by count (find-spatial-keyword-in-map keyword-map-list keyword))))
 
 ;; Example
 (comment
-  (find-spatial-keyword local-test-system-context "6f2c3b1f-acae-4af0-a759-f0d57ccfc83f")
- )
+ (def local-test-system-context
+   {:system (get-in user/system [:apps :indexer])})
+
+  (find-spatial-keyword  (spatial-keywords-map (kf/get-gcmd-keywords-map local-test-system-context)) "a028edce-a3d9-4a16-a8c7-d2cb12d3a318")
+  (find-spatial-keyword  (spatial-keywords-map (kf/get-gcmd-keywords-map local-test-system-context)) "CENTRAL AFRICA")
+  (find-spatial-keyword-in-map  (spatial-keywords-map (kf/get-gcmd-keywords-map local-test-system-context)) "CENTRAL AFRICA")
+  )
--- umm-spec-lib/test/cmr/umm_spec/test/location_keywords.clj
+++ umm-spec-lib/test/cmr/umm_spec/test/location_keywords.clj
@@ -1,9 +1,35 @@
 (ns cmr.umm-spec.test.location-keywords
-  (:require [clojure.umm-spec.location-keywords :as lk]))
+  (:require [clojure.test :refer :all]
+            [cmr.umm-spec.location-keywords :as lk]))
 
-(deftest generate-and-parse-umm-s-json
-  (testing "minimal umm-s record"
-    (let [json (uj/umm->json minimal-example-umm-s-record)
-          _ (is (empty? (js/validate-umm-json json :service)))
-          parsed (uj/json->umm :service json)]
-      (is (= minimal-example-umm-s-record parsed)))))
+(def sample-keyword-map (list {:category "CONTINENT", :uuid "0a672f19-dad5-4114-819a-2eb55bdbb56a"}
+                         {:category "CONTINENT", :type "AFRICA", :subregion-1 "CENTRAL AFRICA", :uuid "f2ffbe58-8792-413b-805b-3e1c8de1c6ff"}
+                         {:category "CONTINENT", :type "AFRICA", :subregion-1 "CENTRAL AFRICA", :subregion-2 "CHAD", :uuid "9b328d2c-07c9-4fd8-945d-f8d4d12e0bb3"}
+                         {:category "CONTINENT", :type "AFRICA", :subregion-1 "CENTRAL AFRICA", :subregion-2 "CAMEROON", :uuid "a028edce-a3d9-4a16-a8c7-d2cb12d3a318"}
+                         {:category "CONTINENT", :type "AFRICA", :subregion-1 "CENTRAL AFRICA", :subregion-2 "CENTRAL AFRICAN REPUBLIC", :uuid "065fc27f-5b54-4cfc-a616-8661a55e04b8"}
+                         {:category "CONTINENT", :type "AFRICA", :subregion-1 "CENTRAL AFRICA", :subregion-2 "CONGO", :uuid "0ebe4123-f42a-41fb-80ea-bb1b97b9db0b"}
+                         {:category "CONTINENT", :type "AFRICA", :subregion-1 "CENTRAL AFRICA", :subregion-2 "CONGO, DEMOCRATIC REPUBLIC", :uuid "3682524d-6ff3-4224-a635-9741726f025f"}
+                         {:category "CONTINENT", :type "AFRICA", :subregion-1 "CENTRAL AFRICA", :uuid "f2ffbe58-8792-413b-805b-3e1c8de1c6ff"}
+                         {:category "CONTINENT", :type "AFRICA", :subregion-1 "CENTRAL AFRICA", :subregion-2 "ZAIRE", :uuid "7848d9b7-a18d-4519-bec3-b4f5fe19a68f"}
+                         {:category "CONTINENT", :type "AFRICA", :subregion-1 "CENTRAL AFRICA", :subregion-2 "GABON", :uuid "864e3511-3326-4b8f-a534-1a8945fcc3eb"}
+                         {:category "CONTINENT", :type "AFRICA", :subregion-1 "CENTRAL AFRICA", :subregion-2 "EQUATORIAL GUINEA", :uuid "3b515cd8-bc42-4fab-9990-df3be2817938"}
+                         {:category "CONTINENT", :type "AFRICA", :subregion-1 "CENTRAL AFRICA", :subregion-2 "ANGOLA", :uuid "9b0a194d-d617-4fed-9625-df176319892d"}
+                         {:category "CONTINENT", :type "AFRICA", :subregion-1 "CENTRAL AFRICA", :subregion-2 "LAKE CHAD", :uuid "a1810ec4-2d03-4d98-b049-2cad380fb789"}))
+(comment
+ ;;Use for local system testing
+(def local-test-system-context
+  {:system (get-in user/system [:apps :indexer])})
+)
+
+(deftest test-location-keyword-lookup
+  (testing "Looking up a root keyword returns the top heirarchy result."
+  (let [keyword "CONTINENT"
+        expected {:category "CONTINENT", :uuid "0a672f19-dad5-4114-819a-2eb55bdbb56a"}
+        actual (lk/find-spatial-keyword sample-keyword-map keyword)]
+  (is (= expected actual))))
+
+  (testing "Looking up a uuid returns a valid result"
+    (let [uuid "a028edce-a3d9-4a16-a8c7-d2cb12d3a318"
+          expected {:category "CONTINENT", :type "AFRICA", :subregion-1 "CENTRAL AFRICA", :subregion-2 "CAMEROON", :uuid "a028edce-a3d9-4a16-a8c7-d2cb12d3a318"}
+          actual (lk/find-spatial-keyword sample-keyword-map uuid)]
+      (is (= expected actual)))))
