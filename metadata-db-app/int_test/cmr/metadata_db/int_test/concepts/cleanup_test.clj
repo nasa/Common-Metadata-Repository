ef2299f502cf709adcee129a7a21c4061b6bc8cf
James Norton <james@element84.com>
2016-06-02 09:47:33 -0400

    CMR-2201: Removed unused test fixture

--- metadata-db-app/int_test/cmr/metadata_db/int_test/concepts/cleanup_test.clj
+++ metadata-db-app/int_test/cmr/metadata_db/int_test/concepts/cleanup_test.clj
@@ -15,8 +15,7 @@
 (use-fixtures :each (join-fixtures
                       [(util/reset-database-fixture {:provider-id "REG_PROV" :small false}
                                                     {:provider-id "SMAL_PROV1" :small true}
-                                                    {:provider-id "SMAL_PROV2" :small true})
-                       tk/freeze-resume-time-fixture]))
+                                                    {:provider-id "SMAL_PROV2" :small true})]))
 
 (defn concept-revision-exists?
   "Returns true if the revision of the concept exists"
