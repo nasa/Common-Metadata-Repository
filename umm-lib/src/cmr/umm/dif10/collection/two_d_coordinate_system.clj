42cded9b9621d90a849c3974f02195a3413fb291
Yonggang Liu <ygliuvt@gmail.com>
2016-06-13 08:54:31 -0400

    CMR-3062: Added comments.

--- umm-lib/src/cmr/umm/dif10/collection/two_d_coordinate_system.clj
+++ umm-lib/src/cmr/umm/dif10/collection/two_d_coordinate_system.clj
@@ -1,10 +1,12 @@
 (ns cmr.umm.dif10.collection.two-d-coordinate-system
+  "Contains functions for convert two d coordinate systems to and parsing from DIF10 XML."
   (:require [clojure.data.xml :as x]
             [cmr.common.xml :as cx]
             [cmr.common.util :as util]
             [cmr.umm.collection :as c]))
 
 (defn- xml-elem->Coordinate
+  "Returns a UMM Coordinate from a parsed Coordinate XML structure"
   [coord-elem]
   (let [min-value (cx/double-at-path coord-elem [:Minimum_Value])
         max-value (cx/double-at-path coord-elem [:Maximum_Value])
@@ -14,6 +16,7 @@
       (c/map->Coordinate value-map))))
 
 (defn- xml-elem->TwoDCoordinateSystem
+  "Returns a UMM TwoDCoordinateSystem from a parsed TwoDCoordinateSystem XML structure"
   [two-d-elem]
   (let [name (cx/string-at-path two-d-elem [:TwoD_Coordinate_System_Name])
         coord-1-elem (cx/element-at-path two-d-elem [:Coordinate1])
@@ -23,6 +26,7 @@
                                   :coordinate-2 (xml-elem->Coordinate coord-2-elem)})))
 
 (defn xml-elem->TwoDCoordinateSystems
+  "Returns UMM TwoDCoordinateSystems from a parsed Collection XML structure"
   [collection-element]
   (seq (map xml-elem->TwoDCoordinateSystem
             (cx/elements-at-path collection-element
@@ -30,6 +34,7 @@
 
 
 (defn- generate-coordinate
+  "Returns coordinate xml structure for the given coordinate"
   [coord-key coord]
   (let [{:keys [min-value max-value]} coord]
     (x/element coord-key {}
@@ -37,6 +42,7 @@
                (when max-value (x/element :Maximum_Value {} max-value)))))
 
 (defn generate-two-ds
+  "Returns DIF10 Spatial_Info element for the given two d coordinate systems"
   [two-d-coordinate-systems]
   (when (seq two-d-coordinate-systems)
     (x/element
