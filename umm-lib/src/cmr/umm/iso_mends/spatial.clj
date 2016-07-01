31cf98312558c7b6b907800a63d2e73b7bfea6fe
Yonggang Liu <ygliuvt@gmail.com>
2016-07-01 10:04:32 -0400

    CMR-3179: Fixed umm-lib ISO 19115 granule spatial representation.

--- umm-lib/src/cmr/umm/iso_mends/spatial.clj
+++ umm-lib/src/cmr/umm/iso_mends/spatial.clj
@@ -2,7 +2,8 @@
   "Functions for extracting spatial extent information from an
   ISO-MENDS format XML document."
   (:require [clojure.data.xml :as x]
-            [clojure.string :as string]
+            [clojure.string :as str]
+            [camel-snake-kebab.core :as csk]
             [cmr.common.util :as util]
             [cmr.common.xml :as cx]
             [cmr.spatial.derived :as d]
@@ -14,7 +15,8 @@
             [cmr.spatial.relations :as r]
             [cmr.umm.collection :as c]
             [cmr.umm.iso-mends.core :as core]
-            [cmr.umm.spatial :as umm-s]))
+            [cmr.umm.spatial :as umm-s]
+            [cmr.umm.iso-mends.collection.helper :as h]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Parsing XML
@@ -42,19 +44,35 @@
   [xml]
   (umm-s/->coordinate-system (cx/string-at-path xml ref-sys-path)))
 
+(defn- xml-elem->granule-spatial-representation
+  "Returns the granule spatial representation from given ISO xml element. It is parsed from the
+  EX_Extent description string which looks something like:
+  \"SpatialCoverageType=Horizontal, SpatialGranuleSpatialRepresentation=CARTESIAN, Temporal Range Type=Continuous Range, Time Type=UTC\""
+  [xml]
+  (let [description (cx/string-at-path
+                      xml [:identificationInfo :MD_DataIdentification
+                           :extent :EX_Extent :description :CharacterString])]
+    (when description
+      (some-> (re-matches #".*SpatialGranuleSpatialRepresentation=(.*)$" description)
+              second
+              (str/split #"," 2)
+              first
+              csk/->kebab-case-keyword))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Public Functions
 
 (defn xml-elem->SpatialCoverage
-  "Returns a UMM SpatialCoverage record from the given ISO MENDS XML
-  document root element."
+  "Returns a UMM SpatialCoverage record from the given ISO MENDS XML element."
   [xml]
-  (when-let [geometries (seq (parse-geometries xml))]
-    (let [coord-sys  (parse-coordinate-system xml)]
+  (let [gran-spatial-rep (xml-elem->granule-spatial-representation xml)
+        geometries (parse-geometries xml)
+        coord-sys  (parse-coordinate-system xml)]
+    (when (or gran-spatial-rep (seq geometries))
       (c/map->SpatialCoverage
-       {:spatial-representation coord-sys
-        :granule-spatial-representation coord-sys
-        :geometries (map #(umm-s/set-coordinate-system coord-sys %) geometries)}))))
+        {:spatial-representation coord-sys
+         :granule-spatial-representation gran-spatial-rep
+         :geometries (seq (map #(umm-s/set-coordinate-system coord-sys %) geometries))}))))
 
 (defn spatial-coverage->coordinate-system-xml
   "Returns ISO MENDS coordinate system XML element from the given SpatialCoverage."
@@ -73,3 +91,15 @@
        ;; ISO MENDS interleaves MBRs and actual spatial areas
        (mapcat (juxt r/mbr identity))
        (map gmd/encode)))
+
+(defn spatial-coverage->extent-description-xml
+  "Returns the ISO MENDS EX_Extent descirption element from the given SpatialCoverage.
+  Granule spatial representation is parsed out of the description, here we only write out the
+  granule spatial representation. This field is lossy in a round trip from xml to xml."
+  [{:keys [granule-spatial-representation]}]
+  (when granule-spatial-representation
+    (h/iso-string-element
+      :gmd:description
+      (str "SpatialGranuleSpatialRepresentation="
+           (some-> granule-spatial-representation csk/->SCREAMING_SNAKE_CASE_STRING)))))
+
