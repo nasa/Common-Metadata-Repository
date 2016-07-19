ca187ac3d833b2c59b89a64b8a9a70d915194414
Jason Gilman <jason@element84.com>
2016-07-18 22:37:33 -0400

    Updating version of proto repl library.

--- access-control-app/project.clj
+++ access-control-app/project.clj
@@ -26,7 +26,7 @@
   :profiles
   {:dev {:dependencies ~(into '[[org.clojure/tools.namespace "0.2.11"]
                                 [pjstadig/humane-test-output "0.7.0"]
-                                [proto-repl "0.2.0"]]
+                                [proto-repl "0.3.0"]]
                           dev-cmr-deps)
          :source-paths ["src" "dev" "test" "int_test"]
          :test-paths ["test" "int_test"]
