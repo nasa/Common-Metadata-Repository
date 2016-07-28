8223b71d9b0e92bc8b1a6a14a04f5687e91e0453
Jason Gilman <jason@element84.com>
2016-07-28 13:37:44 -0400

    CMR-3228: Address review comments.

--- search-app/src/cmr/search/services/query_execution/granule_counts_results_feature.clj
+++ search-app/src/cmr/search/services/query_execution/granule_counts_results_feature.clj
@@ -56,17 +56,17 @@
     query
     ;; Matches spatial or temporal conditions
     (fn [condition-path condition]
-      (let [cond-type (type condition)]
-        (when (or (= cond-type SpatialCondition)
-                  (= cond-type TemporalCondition))
-          ;; Validate that the parent of the spatial or temporal condition is acceptable
-          (validate-path-to-condition query condition-path)
-          true)))))
+      (when (or (instance? SpatialCondition condition)
+                (instance? TemporalCondition condition))
+        ;; Validate that the parent of the spatial or temporal condition is acceptable
+        (validate-path-to-condition query condition-path)
+        true))))
+
 
 (defn- convert-spatial-or-temporal-condition
   "Applies any necessary changes to spatial or temporal condition to make them work with granules"
   [condition]
-  (if (= TemporalCondition (type condition))
+  (if (instance? TemporalCondition condition)
     ;; Turn off limit to granules so that the correct fields will be searched. limit to granules is a
     ;; collection applicable parameter.
     (assoc condition :limit-to-granules false)
