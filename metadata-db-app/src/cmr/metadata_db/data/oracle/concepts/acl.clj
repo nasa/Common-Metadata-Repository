5d9d9c862f450e8549b5f8a32496192117d51e34
John Cromartie <john@element84.com>
2016-07-29 09:44:30 -0400

    CMR-2894 Fixes acl save test

--- metadata-db-app/src/cmr/metadata_db/data/oracle/concepts/acl.clj
+++ metadata-db-app/src/cmr/metadata_db/data/oracle/concepts/acl.clj
@@ -1,7 +1,8 @@
 (ns cmr.metadata-db.data.oracle.concepts.acl
   "Implements multi-methods for ACL concepts."
   (:require [cmr.metadata-db.data.oracle.concepts :as c]
-            [cmr.metadata-db.data.oracle.concepts.group :as g]))
+            [cmr.metadata-db.data.oracle.concepts.group :as g]
+            [cmr.common.util :as u]))
 
 ;; NOTE: borrowing Access Group functions to implement these methods, because they are identical
 
@@ -10,7 +11,8 @@
   (some-> (c/db-result->concept-map :access-group db provider-id result)
           (assoc :concept-type :acl)
           (assoc-in [:extra-fields :acl-identity] (:acl_identity result))
-          (assoc-in [:extra-fields :target-provider-id] (:target_provider_id result))))
+          (assoc-in [:extra-fields :target-provider-id] (:target_provider_id result))
+          (update-in [:extra-fields] u/remove-nil-keys)))
 
 (defn- acl-insert-args
   [concept]
