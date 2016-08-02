3bc0d2b9c3d73800c5f7d3daa4f78da420479e75
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2016-08-02 08:35:50 -0400

    CMR-3233 PR fixes

--- umm-spec-lib/src/cmr/umm_spec/version_migration.clj
+++ umm-spec-lib/src/cmr/umm_spec/version_migration.clj
@@ -102,7 +102,7 @@
       (dissoc :DataCenters :ContactGroups :ContactPersons)
       (assoc :Organizations [not-provided-organization])))
 
-(defn update-attribute-description
+(defn- update-attribute-description
   "If description is nil, set to default of 'Not provided'"
   [attribute]
   (if (nil? (:Description attribute))
@@ -121,17 +121,17 @@
   ;; Don't need to migrate Additional Attribute description back since 'Not provided' is valid
   c)
 
-(defn first-contact-info
+(defn- first-contact-info
   "Update the array of contact infos to be a single instance using the first contact info in the list"
   [c]
   (update-in c [:ContactInformation] first))
 
-(defn update-data-center-contact-info
+(defn- update-data-center-contact-info
   "Update data center contact infos to be a single instance - this includes the contact info on the
    data center, the contact info inside each contact person and each contact group"
   [data-center]
   (-> data-center
-      (first-contact-info)
+      first-contact-info
       (update-in [:ContactPersons] #(mapv first-contact-info %))
       (update-in [:ContactGroups] #(mapv first-contact-info %))))
 
@@ -142,14 +142,14 @@
     (update-in [:ContactPersons] #(mapv first-contact-info %))
     (update-in [:ContactGroups] #(mapv first-contact-info %))))
 
-(defn contact-info-to-array
+(defn- contact-info-to-array
   "Update the contact info field to be an array. If contact info is nil, leave it as nil."
   [c]
   (if (some? (:ContactInformation c))
    (assoc c :ContactInformation [(:ContactInformation c)])
    c))
 
-(defn update-data-center-contact-info-to-array
+(defn- update-data-center-contact-info-to-array
   "Update data center contact infos to be arrays - this includes the contact info on the
    data center, the contact info inside each contact person and each contact group"
   [data-center]
