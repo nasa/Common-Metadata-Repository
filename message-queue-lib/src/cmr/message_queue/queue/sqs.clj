407a83b827ed172631ad06f3cf63aca8d08913af
James Norton <james@element84.com>
2016-08-05 11:45:21 -0400

    CMR-3044: Responsed to PR comments.

--- message-queue-lib/src/cmr/message_queue/queue/sqs.clj
+++ message-queue-lib/src/cmr/message_queue/queue/sqs.clj
@@ -44,6 +44,11 @@
  {:default 20
   :type Long})
 
+(def queue-arn-attribute
+  "String used by the AWS SQS API to identify the attribute containing a queue's
+  Amazon Resource Name"
+  "QueueArn")
+
 (defn- dead-letter-queue
   "Returns the dead-letter-queue name for a given queue name. The
   given queue name should already be normalized."
@@ -56,7 +61,9 @@
   (str/replace arn #".*:" ""))
 
 (defn- normalize-queue-name
-  "Replace dots with underscores"
+  "Replace dots with underscores. This is needed because SQS only allows
+  alpha-numeric chars plus dashes and underscores in queue names, while
+  CMR has dots (periods) in queue names."
   [queue-name]
   (str/replace queue-name "." "_"))
 
@@ -65,11 +72,12 @@
   [sns-client exchange-name]
   (let [exchange-name (normalize-queue-name exchange-name)
         topics (into [] (.getTopics (.listTopics sns-client)))]
-   (some (fn [topic] (let [topic-arn (.getTopicArn topic)
-                           topic-name (arn->name topic-arn)]
-                      (when (= exchange-name topic-name)
-                       topic)))
-         topics)))
+   (some (fn [topic]
+             (let [topic-arn (.getTopicArn topic)
+                   topic-name (arn->name topic-arn)]
+               (when (= exchange-name topic-name)
+                     topic)))
+     topics)))
 
 (def get-topic
  "Memoized function that returns the Topic with the given display name."
@@ -77,14 +85,15 @@
 
 (defn- get-queue-arn
   "Get the Amazon Resource Name (ARN) for the given queue.
-   Queue name must be normalized."
+  See http://docs.aws.amazon.com/general/latest/gr/aws-arns-and-namespaces.html.
+  Queue name must be normalized."
   [sqs-client queue-name]
   (let [q-url (.getQueueUrl (.getQueueUrl sqs-client queue-name))
-        q-attrs (->> (java.util.ArrayList. ["QueueArn"])
+        q-attrs (->> (ArrayList. [queue-arn-attribute])
                      (.getQueueAttributes sqs-client q-url)
                      .getAttributes
                      (into {}))]
-   (get q-attrs "QueueArn")))
+   (get q-attrs queue-arn-attribute)))
 
 (defn- create-async-handler
   "Creates a thread that will asynchronously pull messages off the queue, pass them to the handler,
@@ -94,26 +103,24 @@
   (let [queue-name (normalize-queue-name queue-name)
         sqs-client (get queue-broker :sqs-client)
         queue-url (.getQueueUrl (.getQueueUrl sqs-client queue-name))
-        rec-req (ReceiveMessageRequest. queue-url)
-        timeout (queue-polling-timeout)]
-    ;; Only take one message at a time form the queue.
+        rec-req (ReceiveMessageRequest. queue-url)]
+    ;; Only take one message at a time from the queue.
     (.setMaxNumberOfMessages rec-req (Integer. 1))
     ;; Tell SQS how long to wait before returning with no data (long polling).
     (.setWaitTimeSeconds rec-req (Integer. (queue-polling-timeout)))
     (a/thread
       (try
         (u/while-let [rec-result (.receiveMessage sqs-client rec-req)]
-          (let [messages (into [] (.getMessages rec-result))]
-            (when-let [msg (first messages)]
-              (let [msg-body (.getBody msg)
-                    msg-content (json/decode msg-body true)]
-                (try
-                  (handler msg-content)
-                  (.deleteMessage sqs-client queue-url (.getReceiptHandle msg))
-                  (catch Throwable e
-                    (error e "Message processing failed for message" (pr-str msg) "on queue" queue-name)))))))
-        (catch Exception e
-          (warn  "Async handler for queue" queue-name "completing."))))))
+          (when-let [msg (first (.getMessages rec-result))]
+            (let [msg-body (.getBody msg)
+                  msg-content (json/decode msg-body true)]
+              (try
+                (handler msg-content)
+                (.deleteMessage sqs-client queue-url (.getReceiptHandle msg))
+                (catch Throwable e
+                  (error e "Message processing failed for message" (pr-str msg) "on queue" queue-name))))))
+        (catch Throwable e
+          (error  e "Async handler for queue" queue-name "completing."))))))
 
 (defn- create-queue
  "Create a queue and its dead-letter-queue if they don't already exist and connect the two."
@@ -128,7 +135,7 @@
        redrive-policy (str "{\"maxReceiveCount\":\"5\", \"deadLetterTargetArn\": \"" dlq-arn "\"}")
        ;; create the primary queue
        queue-url (.getQueueUrl (.createQueue sqs-client q-name))
-       q-attrs (java.util.HashMap. {"RedrivePolicy" redrive-policy "VisibilityTimeout" "30"})
+       q-attrs (HashMap. {"RedrivePolicy" redrive-policy "VisibilityTimeout" "30"})
        set-queue-attrs-request (doto (SetQueueAttributesRequest.)
                                      (.setAttributes q-attrs)
                                      (.setQueueUrl queue-url))]
@@ -169,7 +176,7 @@
        q-url (.getQueueUrl (.getQueueUrl sqs-client q-name))
        ;; create an access policy to allow the topic to publish to the queue
        access-policy (sns-to-sqs-access-policy sns-client sqs-client queue-name exchange-names)
-       q-attrs (java.util.HashMap. {"Policy" access-policy})
+       q-attrs (HashMap. {"Policy" access-policy})
 
        ;; create and empty SetQueueAttributesRequest object and then set the attributes on it
        set-queue-attrs-request (doto (SetQueueAttributesRequest.)
@@ -187,7 +194,7 @@
 (defn- normalized-queue-name->original-queue-name
   "Convert a normalized queue name to the original queue name used to create it."
   [queue-broker queue-name]
-  (get (:normalized-queue-names queue-broker) queue-name queue-name))
+  (get-in queue-broker [:normalized-queue-names queue-name] queue-name))
 
 (defn- -get-queue-url
   "Returns the queue url for the given queue name."
@@ -199,7 +206,8 @@
   (memoize -get-queue-url))
 
 (defrecord SQSQueueBroker
- [
+  ;; A record containig fields related to accessing SNS/SQS exchanges and queues.
+  [
    ;; Connection to AWS SNS
    sns-client
 
@@ -210,32 +218,34 @@
    queues
 
    ;; map of normalized queue names to original queue names - needed for testing with queue broker wrapper
+   ;; See normalize-queue-name for an explanation of normalized queue names.
    normalized-queue-names
 
    ;; exchanges (topics) known to this broker
    exchanges
 
    ;; a map of queues to seqeunces of exchange names to which they should be bound
-   bindings]
+   queues-to-exchanges]
 
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- lifecycle/Lifecycle
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  lifecycle/Lifecycle
 
   (start
     [this system]
     (let [sqs-client (AmazonSQSClient.)
           sns-client (AmazonSNSClient.)
-          normalized-queue-names (reduce (fn [m queue-name] (let [nqn (normalize-queue-name queue-name)]
-                                                               (assoc m nqn queue-name)))
+          normalized-queue-names (reduce (fn [m queue-name]
+                                             (let [nqn (normalize-queue-name queue-name)]
+                                               (assoc m nqn queue-name)))
                                          {}
                                          queues)]
       (doseq [queue-name queues]
         (create-queue sqs-client queue-name))
       (doseq [exchange-name exchanges]
         (create-exchange sns-client exchange-name))
-      (doseq [queue (keys bindings)
-              :let [exchanges (get bindings queue)]]
-          (bind-queue-to-exchanges sns-client sqs-client exchanges queue))
+      (doseq [queue (keys queues-to-exchanges)
+              :let [exchanges (get queues-to-exchanges queue)]]
+        (bind-queue-to-exchanges sns-client sqs-client exchanges queue))
       (assoc this :sns-client sns-client :sqs-client sqs-client :normalized-queue-names normalized-queue-names)))
 
   (stop
@@ -243,7 +253,8 @@
     (.shutdown sns-client)
     (.shutdown sqs-client)
     this)
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   queue/Queue
 
   (publish-to-queue
@@ -259,77 +270,67 @@
           topic (get-topic sns-client exchange-name)
           topic-arn (.getTopicArn topic)
           subs (into [] (.getSubscriptions (.listSubscriptionsByTopic sns-client topic-arn)))]
-      (map (fn [sub] (->> sub
-                          .getEndpoint
-                          arn->name
-                          (normalized-queue-name->original-queue-name this)))
+      (map (fn [sub]
+               (->> sub
+                    .getEndpoint
+                    arn->name
+                    (normalized-queue-name->original-queue-name this)))
            subs)))
 
   (publish-to-exchange
-     [this exchange-name msg]
-     (let [msg (json/generate-string msg)
-           exchange-name (normalize-queue-name exchange-name)
-           topic (get-topic sns-client exchange-name)
-           topic-arn (.getTopicArn topic)]
+    [this exchange-name msg]
+    (let [msg (json/generate-string msg)
+          exchange-name (normalize-queue-name exchange-name)
+          topic (get-topic sns-client exchange-name)
+          topic-arn (.getTopicArn topic)]
       (.publish sns-client topic-arn msg)))
 
   (subscribe
-     [this queue-name handler]
-     (let [queue-name (normalize-queue-name queue-name)]
+    [this queue-name handler]
+    (let [queue-name (normalize-queue-name queue-name)]
       (create-async-handler this queue-name handler)))
 
   (reset
-     [this]
-     (let [sqs-client (:sqs-client this)]
-       (doseq [queue (:queues this)
-               :let [queue-name (normalize-queue-name queue)
-                     dlq-name (dead-letter-queue queue-name)
-                     queue-url (.getQueueUrl (.getQueueUrl sqs-client queue-name))
-                     dlq-url (.getQueueUrl (.getQueueUrl sqs-client dlq-name))
-                     q-purge-req (PurgeQueueRequest. queue-url)
-                     dlq-purge-req (PurgeQueueRequest. dlq-url)]]
-         (.purgeQueue sqs-client q-purge-req)
-         (.purgeQueue sqs-client dlq-purge-req))))
+    [this]
+    (let [sqs-client (:sqs-client this)]
+      (doseq [queue (:queues this)
+              :let [queue-name (normalize-queue-name queue)
+                    dlq-name (dead-letter-queue queue-name)
+                    queue-url (.getQueueUrl (.getQueueUrl sqs-client queue-name))
+                    dlq-url (.getQueueUrl (.getQueueUrl sqs-client dlq-name))
+                    q-purge-req (PurgeQueueRequest. queue-url)
+                    dlq-purge-req (PurgeQueueRequest. dlq-url)]]
+        (.purgeQueue sqs-client q-purge-req)
+        (.purgeQueue sqs-client dlq-purge-req))))
 
   (health
-     [this]
-     ;; try to get a list of queues and topic (exchanges) to test the connection to SNS/SQS
-     (try
-       (.listQueues sqs-client)
-       (.listTopics sns-client)
-       {:ok? true}
-       (catch Throwable e
-         {:ok? false :msg (.getMessage e)}))))
+    [this]
+    ;; try to get a list of queues and topics (exchanges) to test the connection to SNS/SQS
+    (try
+      (.listQueues sqs-client)
+      (.listTopics sns-client)
+      {:ok? true}
+      (catch Throwable e
+        {:ok? false :msg (.getMessage e)}))))
+
+(record-pretty-printer/enable-record-pretty-printing SQSQueueBroker)
 
 (defn create-queue-broker
   "Creates a broker that uses SNS/SQS"
   [{:keys [queues exchanges queues-to-exchanges]}]
   (->SQSQueueBroker nil nil queues nil exchanges queues-to-exchanges))
 
+;; Tests to make sure SNS/SQS is working
 (comment
-  (cmr.system-int-test.utils.ingest-util/create-provider {:provider-guid "provguid1" :provider-id "PROV1"})
-  (normalize-queue-name "cmr_access_control_index.queue")
-
+  ;; create a broker
   (def broker (lifecycle/start (create-queue-broker {}) nil))
+  ;; list the topics for the cmr-ingest_exchange exchange/topic
   (get-topic (:sns-client broker) "cmr_ingest_exchange")
-  (.getTopics (.listTopics (:sns-client broker)))
+  ;; list the queues for the cmr_ingest_exchange
   (queue/get-queues-bound-to-exchange broker "cmr_ingest_exchange")
-  (queue/subscribe broker "cmr-test-queue" (fn [msg] (println "Got Message: " msg)))
-  (queue/publish-to-queue broker "cmr-test-queue" "{\"body\": \"ABC\"}")
-
-  (create-queue broker "index.queue.test")
-
-  (def client (AmazonSQSClient.))
-  (def queue-url (.getQueueUrl (.getQueueUrl client "cmr-index-queue")))
-  (def msg-res (.sendMessage client queue-url "This is a test message"))
-  (str msg-res)
-
-  (def rec-msg (.receiveMessage client queue-url))
-  (str rec-msg)
-  (def messages (.getMessages rec-msg))
-  (def rhandle (.getReceiptHandle (.get messages 0)))
-  (.deleteMessage client queue-url rhandle)
-
-  (def topic-arn "arn:aws:sns:us-east-1:985962406024:cmr-indexer")
-  (def sns-client (AmazonSNSClient.))
-  (.publish sns-client topic-arn "This is a test message."))
+  ;; create a test queue
+  (create-queue (:sqs-client broker) "cmr-test.queue")
+  ;; subscribe to test queue with a simple handler that prints received messages
+  (queue/subscribe broker "cmr-test.queue" (fn [msg] (println "Got Message: " msg)))
+  ;; publish a message to the queue to verify our subscribe worked
+  (queue/publish-to-queue broker "cmr-test.queue" "{\"body\": \"ABC\"}"))
