5d70189951da36032ae2e7b7b52aa042b0d2e099
Yonggang Liu <ygliuvt@gmail.com>
2016-08-23 11:30:31 -0400

    CMR-3260: Added migration 45 for inserting the initial humanizer concept.

---
+++ metadata-db-app/src/migrations/045_insert_initial_humanizer_concept.clj
@@ -0,0 +1,38 @@
+(ns migrations.045-insert-initial-humanizer-concept
+  (:require [config.migrate-config :as config]
+            [config.mdb-migrate-helper :as h]
+            [cmr.metadata-db.data.oracle.concepts]
+            [cmr.metadata-db.data.concepts :as c]
+            [cheshire.core :as json]
+            [cmr.common-app.test.sample-humanizer :as sh]))
+
+(defn- insert-humanizer
+  []
+  (let [provider {:provider-id "CMR"
+                  :short-name "CMR"
+                  :system-level? true
+                  :cmr-only true
+                  :small false}
+        concept {:concept-type :humanizer
+                 :native-id "humanizer"
+                 :metadata (json/generate-string sh/sample-humanizers)
+                 :user-id "migration"
+                 :format "application/json"
+                 :provider-id "CMR"
+                 :concept-id "H12345-CMR"
+                 :revision-id 1
+                 :deleted false}]
+    (c/save-concept (config/db) provider concept)))
+
+(defn up
+  "Migrates the database up to version 45."
+  []
+  (println "migrations.045-insert-initial-humanizer-concept up...")
+  (h/sql "DELETE cmr_humanizers")
+  (insert-humanizer))
+
+(defn down
+  "Migrates the database down from version 45."
+  []
+  (println "migrations.045-insert-initial-humanizer-concept down...")
+  (h/sql "DELETE cmr_humanizers"))
