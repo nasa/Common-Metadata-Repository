116e352ac8059e6c931bcb5367a0668ee815a65e
Chris Durbin <christopher.d.durbin@nasa.gov>
2016-08-24 16:34:28 -0400

    CMR-3050: Remove commented out code.

--- metadata-db-app/src/migrations/046_add_primary_key_to_providers.clj
+++ metadata-db-app/src/migrations/046_add_primary_key_to_providers.clj
@@ -8,10 +8,6 @@
   (println "migrations.046-add-primary-key-to-providers up...")
   (h/sql "alter table providers drop constraint unique_provider_id")
   (h/sql "alter table providers add constraint providers_pk primary key (provider_id)"))
-  ; (h/sql (str "alter table sync_delete_work add constraint sync_delete_work_pk "
-  ;             "primary key (concept_id, revision_id)")))
-  ; (h/sql (str "alter table get_concepts_work_area add constraint get_concepts_work_area_pk "
-              ; "primary key (concept_id, revision_id)")))
 
 (defn down
   "Migrates the database down from version 46."
@@ -20,5 +16,3 @@
   (h/sql "alter table providers drop constraint providers_pk")
   (h/sql (str "alter table providers add constraint unique_provider_id unique(provider_id) "
               "using index (create unique index provider_id_index on providers(provider_id))")))
-  ; (h/sql "alter table sync_delete_work drop constraint sync_delete_work_pk")
-  ; (h/sql "alter table get_concepts_work_area drop constraint get_concepts_work_area_pk"))
