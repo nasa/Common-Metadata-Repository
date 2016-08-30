5cf41d47707af0a58eba6f579a87e875e87e7ba1
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2016-08-30 16:06:32 -0400

    CMR-3158 unit test for echo10 xml -> umm

---
+++ umm-spec-lib/test/cmr/umm_spec/test/xml_to_umm_mappings/echo10.clj
@@ -0,0 +1,32 @@
+(ns cmr.umm-spec.test.xml-to-umm-mappings.echo10
+  (:require
+   [clj-time.core :as t]
+   [clojure.test :refer :all]
+   [cmr.umm-spec.xml-to-umm-mappings.echo10.data-contact :as contact]
+   [cmr.common.xml.parse :refer :all]
+   [cmr.common.xml.simple-xpath :refer [select text]]))
+
+(deftest echo10-contact-role-test
+  (testing "ECHO10 Contact with invalid role and default applied"
+    ;; Test that when the contact role is invalid, the data center defaults to the correct role
+    (let [xml "<Collection>
+                <Contacts>
+                 <Contact>
+                  <Role>USER SERVICES</Role>
+                  <OrganizationName>LPDAAC</OrganizationName>
+                 </Contact>
+                </Contacts>
+               </Collection>"
+          data-centers (contact/parse-data-centers xml true)]
+      (is (= [contact/default-data-center-role] (:Roles (first data-centers)))))
+   (testing "ECHO10 Contact with invalid role and no default applied"
+     (let [xml "<Collection>
+                 <Contacts>
+                  <Contact>
+                   <Role>USER SERVICES</Role>
+                   <OrganizationName>LPDAAC</OrganizationName>
+                  </Contact>
+                 </Contacts>
+                </Collection>"
+           data-centers (contact/parse-data-centers xml false)]
+       (is (= [nil] (:Roles (first data-centers))))))))
