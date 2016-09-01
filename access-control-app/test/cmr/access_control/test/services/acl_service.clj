75009c0c6a422fc328376111b47577d00679fa91
Daniel Zamora <dpzamora@mbp-dpzamora.edn.ecs.nasa.gov>
2016-09-01 14:00:31 -0400

    CMR-3298 Adds logging to creating, updating, and deleeting ACLs

--- access-control-app/test/cmr/access_control/test/services/acl_service.clj
+++ access-control-app/test/cmr/access_control/test/services/acl_service.clj
@@ -1,5 +1,8 @@
 (ns cmr.access-control.test.services.acl-service
   (require [clojure.test :refer :all]
+           [cmr.access-control.test.util :as u]
+           [cmr.mock-echo.client.echo-util :as e]
+           [cmr.common.util :as util :refer [are3]]
            [cmr.access-control.services.acl-service :as as]))
 
 (deftest valid-permitted-group?
@@ -29,3 +32,26 @@
          "AG*-PROV1"
          "AG123?-PROV1")))
 
+(deftest acl-log-message
+  (let [token (e/login (u/conn-context) "admin")]
+    (testing "Create, update, and delete ACL log message function"
+      (are3 [new-acl existing-acl action expected-message]
+            (is (= expected-message (as/acl-log-message (merge (u/conn-context) {:token token}) new-acl existing-acl action)))
+
+            "Create ACL log message"
+            "new-acl"
+            nil
+            :create
+            "User: [admin] Created ACL [new-acl]"
+
+            "Update ACL log message"
+            "new-acl"
+            "existing-acl"
+            :update
+            "User: [admin] Updated ACL,\n before: [existing-acl]\n after: [new-acl]"
+
+            "Delete ACL log message"
+            "new-acl"
+            "existing-acl"
+            :delete
+            "User: [admin] Deleted ACL,\n before: [existing-acl]\n after: [new-acl]"))))
