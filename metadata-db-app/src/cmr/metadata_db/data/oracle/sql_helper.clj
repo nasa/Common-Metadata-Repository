c62d9b76a33c526aa403314d487dffc40e6140b3
James Norton <james@element84.com>
2016-09-13 17:09:47 -0400

    CMR-3058: Added documentation to docstring.

--- metadata-db-app/src/cmr/metadata_db/data/oracle/sql_helper.clj
+++ metadata-db-app/src/cmr/metadata_db/data/oracle/sql_helper.clj
@@ -10,7 +10,19 @@
   (:import cmr.oracle.connection.OracleStore))
 
 (defn find-params->sql-clause
-  "Converts a parameter map for finding concept types into a sql clause for inclusion in a query."
+  "Converts a parameter map for finding concept types into a sql clause for inclusion in a query. The type
+  of value determines the nature of the clause. If the value for a parameter is sequential then a clause
+  using 'in' is generated. If the value is a map then it must contain two keys, :comparator which specifies
+  what comparision operation to use, e.g. `> or `<, and :value which specifies the value for comparision.
+  Any other value type results in a simple 'equals' clause.
+
+  Examples:
+             {:provider-id \"PROV1\"}               =>   `(= :provider-id \"PROV1\")
+
+             {:provider-id [\"PROV1\", \"PROV2\"]}  =>   `(in :provider-id [\"PROV1\" \"PROV2\"])
+
+             {:revision-id {:comparator `>, :value \"2000-01-01T10:00:00Z\"}} =>
+                    `(> :revision-id \"2000-01-01T10:00:00Z\")"
   [params]
   ;; Validate parameter names as a sanity check to prevent sql injection
   (let [valid-param-name #"^[a-zA-Z][a-zA-Z0-9_\-]*$"]
