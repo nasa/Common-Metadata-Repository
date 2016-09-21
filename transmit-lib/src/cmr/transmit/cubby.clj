a448eff4b051ab06dfdea62f9dadaa47c3d8f171
James Norton <james@element84.com>
2016-09-21 15:39:40 -0400

    Changes to pass system token on calls to cubby

--- transmit-lib/src/cmr/transmit/cubby.clj
+++ transmit-lib/src/cmr/transmit/cubby.clj
@@ -23,14 +23,14 @@
   ([context]
    (get-keys context false))
   ([context raw]
-   (h/request context :cubby {:url-fn keys-url :method :get :raw? raw})))
+   (h/request context :cubby {:url-fn keys-url :method :get :raw? raw, :use-system-token? true})))
 
 (defn get-value
   "Gets the value associated with the given key."
   ([context key-name]
    (get-value context key-name false))
   ([context key-name raw]
-   (h/request context :cubby {:url-fn (partial key-url key-name), :method :get, :raw? raw})))
+   (h/request context :cubby {:url-fn (partial key-url key-name), :method :get, :raw? raw, :use-system-token? true})))
 
 (defn set-value
   "Associates a value with the given key."
@@ -40,6 +40,7 @@
    (h/request context :cubby {:url-fn (partial key-url key-name)
                               :method :put
                               :raw? raw
+                              :use-system-token? true
                               :http-options {:body value}})))
 
 (defn delete-value
@@ -47,14 +48,14 @@
   ([context key-name]
    (delete-value context key-name false))
   ([context key-name raw]
-   (h/request context :cubby {:url-fn (partial key-url key-name), :method :delete, :raw? raw})))
+   (h/request context :cubby {:url-fn (partial key-url key-name), :use-system-token? true, :method :delete, :raw? raw})))
 
 (defn delete-all-values
   "Deletes all values"
   ([context]
    (delete-all-values context false))
   ([context raw]
-   (h/request context :cubby {:url-fn keys-url, :method :delete, :raw? raw})))
+   (h/request context :cubby {:url-fn keys-url, :method :delete, :raw? raw, :use-system-token? true})))
 
 ;; Defines reset function
 (h/defresetter reset :cubby)
