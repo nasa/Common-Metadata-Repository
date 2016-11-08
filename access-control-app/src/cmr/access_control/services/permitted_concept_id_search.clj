da2766eee305f999683a8ac6c6a82c6486c735d2
Daniel Zamora <dpzamora@mbp-dpzamora.edn.ecs.nasa.gov>
2016-11-08 14:59:52 -0500

    CMR-3279 address pr comments

--- access-control-app/src/cmr/access_control/services/permitted_concept_id_search.clj
+++ access-control-app/src/cmr/access_control/services/permitted_concept_id_search.clj
@@ -5,22 +5,35 @@
     [cmr.common-app.services.search.query-model :as common-qm]
     [cmr.umm-spec.umm-spec-core :as umm-spec]))
 
+(defn- create-generic-collection-applicable-condition
+  "Constructs query condition for searching permitted_concept_ids by
+   collection-applicable acls without a collection identifier"
+  []
+  (gc/group-conds
+    :and
+    [(common-qm/boolean-condition :collection-identifier false)
+     (common-qm/boolean-condition :collection-applicable true)]))
+
 (defn- create-access-value-condition
   "Constructs query condition for searching permitted_concept_ids by access-value."
   [parsed-concept]
-  (if-let [access-value (:Value (:AccessConstraints parsed-concept))]
-    (common-qm/numeric-range-intersection-condition
-      :collection-access-value-min
-      :collection-access-value-max
-      access-value
-      access-value)
-    (common-qm/boolean-condition :collection-access-value-include-undefined-value true)))
+  (gc/group-conds
+    :and
+    [(if-let [access-value (:Value (:AccessConstraints parsed-concept))]
+       (common-qm/numeric-range-intersection-condition
+         :collection-access-value-min
+         :collection-access-value-max
+         access-value
+         access-value)
+       (common-qm/boolean-condition :collection-access-value-include-undefined-value true))
+     (common-qm/boolean-condition :collection-identifier true)
+     (common-qm/boolean-condition :collection-applicable true)]))
 
 (defn get-permitted-concept-id-conditions
   "Returns query to search for ACLs that could permit given concept"
   [context concept]
-  (let [parsed-concept (umm-spec/parse-metadata context concept)]
+  (let [parsed-concept (umm-spec/parse-metadata (merge {:ignore-kms-keywords true} context) concept)]
     (gc/group-conds
-      :or
-      [(common-qm/boolean-condition :collection-identifier false)
-       (create-access-value-condition parsed-concept)])))
+     :or
+     [(create-generic-collection-applicable-condition)
+      (create-access-value-condition parsed-concept)])))
