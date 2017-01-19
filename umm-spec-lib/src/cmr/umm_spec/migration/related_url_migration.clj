a736d2f35f0969ebb4664229fb431bd101305414
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2017-01-19 11:32:25 -0500

    CMR-3676 Add CollectionCitations to migrations

--- umm-spec-lib/src/cmr/umm_spec/migration/related_url_migration.clj
+++ umm-spec-lib/src/cmr/umm_spec/migration/related_url_migration.clj
@@ -3,26 +3,26 @@
   (:require
    [cmr.umm-spec.util :as util]))
 
-(defn migrate-publication-reference-to-online-resource
-  "Migrate the RelatedUrl in Publication Reference to Online Resource"
-  [pub-ref]
-  (if-let [related-url (:RelatedUrl pub-ref)]
-    (-> pub-ref
+(defn migrate-related-url-to-online-resource
+  "Migrate the RelatedUrl in the element to Online Resource"
+  [element]
+  (if-let [related-url (:RelatedUrl element)]
+    (-> element
         (assoc :OnlineResource {:Linkage (first (:URLs related-url))
                                 :Name (util/with-default (:Title related-url) true)
                                 :Description (util/with-default (:Description related-url) true)})
         (dissoc :RelatedUrl))
-    pub-ref))
+    element))
 
-(defn migrate-publication-reference-to-related-url
-  "Migrate the OnlineResource in Publication Reference to RelatedUrl"
-  [pub-ref]
-  (if-let [online-resource (:OnlineResource pub-ref)]
-    (-> pub-ref
+(defn migrate-online-resource-to-related-url
+  "Migrate the OnlineResource in the element to RelatedUrl"
+  [element]
+  (if-let [online-resource (:OnlineResource element)]
+    (-> element
         (assoc :RelatedUrl {:URLs [(:Linkage online-resource)]
                             :Title (:Name online-resource)
                             :Description (:Description online-resource)
                             :Relation ["VIEW RELATED INFORMATION" "Citation"]
                             :MimeType "text/html"})
         (dissoc :OnlineResource))
-    pub-ref))
+    element))
