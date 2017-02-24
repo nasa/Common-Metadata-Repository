d0535e11ec8b58b6d4b3250faf8e4d531e6b72bb
Daniel Zamora <dpzamora@mbp-dpzamora.edn.ecs.nasa.gov>
2017-02-24 10:42:59 -0500

    fixes function to return sequence of valid acls, without any empty values

--- access-control-app/src/cmr/access_control/services/auth_util.clj
+++ access-control-app/src/cmr/access_control/services/auth_util.clj
@@ -74,11 +74,12 @@
                          (= action-description "delete") [:delete]
                          :else [permission])]
    (when (or legacy-guid concept-id)
-     (doseq [permission permissions]
-       (filter #(or (= concept-id (-> % :single-instance-object-identity :target-guid))
-                    (= legacy-guid (-> % :single-instance-object-identity :target-guid)))
-               (acl/get-permitting-acls context
-                                        :single-instance-object "GROUP_MANAGEMENT" permission))))))
+     (remove empty?                  
+       (for [permission permissions]
+         (filter #(or (= concept-id (-> % :single-instance-object-identity :target-guid))
+                      (= legacy-guid (-> % :single-instance-object-identity :target-guid)))
+                 (acl/get-permitting-acls context
+                                          :single-instance-object "GROUP_MANAGEMENT" permission)))))))
 (defn- describe-group
   [group]
   (let [{:keys [provider-id]} group]
