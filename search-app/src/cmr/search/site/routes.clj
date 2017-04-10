976c76b8b1caca6fa177e7f92feb2580b6e2c491
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-04-10 09:58:42 -0500

    Separated api and site routes.
    Also provided over-arching web namespace that unites the two.

--- search-app/src/cmr/search/site/routes.clj
+++ search-app/src/cmr/search/site/routes.clj
@@ -11,23 +11,10 @@
    [cmr.common.api.context :as context]
    [cmr.common-app.api-docs :as api-docs]))
 
-(defn get-landing-pages
-  ""
-  [request]
-  {:status 200
-   :body "TBD"})
-
 (defn build-routes [system]
   (let [relative-root-url (get-in system [:public-conf :relative-root-url])]
     (routes
       (context relative-root-url []
-        ;; Landing pages - Note that the landing pages must come before the
-        ;; api-docs, since api-docs/docs-routes also use a context of "site"
-        ;; but have the last entry as a 404 renderer, and as such, would
-        ;; prevent any pages in the "site" context from rendering after that
-        ;; point.
-        (GET "/site/collections/landing-pages" req
-          (get-landing-pages req))
         ;; Add routes for API documentation
         (api-docs/docs-routes
           (get-in system [:public-conf :protocol])
