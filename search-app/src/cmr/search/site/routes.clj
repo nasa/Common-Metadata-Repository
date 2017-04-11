eaeda8d0c19342ad7ab3ba6ecfcaca2579d42aee
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-04-11 14:39:45 -0500

    Updated site routes to use templates.

--- search-app/src/cmr/search/site/routes.clj
+++ search-app/src/cmr/search/site/routes.clj
@@ -5,16 +5,47 @@
    ;; Third-party libs
    [compojure.core :refer :all]
    [ring.swagger.ui :as ring-swagger-ui]
+   [selmer.parser :as selmer]
 
    ;; CMR libs
    [cmr.collection-renderer.api.routes :as collection-renderer-routes]
    [cmr.common.api.context :as context]
-   [cmr.common-app.api-docs :as api-docs]))
+   [cmr.common-app.api-docs :as api-docs]
+   [cmr.search.site.data :as data]))
+
+(defn render-template-ok
+  ""
+  [template data]
+  {:status 200
+   :body (selmer/render-file template data)})
+
+(defn landing-links-page
+  ""
+  [request]
+  (render-template-ok
+    "templates/landing-links.html"
+    (data/get-landing-links request)))
+
+(defn eosdis-landing-links-page
+  ""
+  [request]
+  (render-template-ok
+    "templates/eosdis-landing-links.html"
+    (data/get-eosdis-landing-links request)))
 
 (defn build-routes [system]
   (let [relative-root-url (get-in system [:public-conf :relative-root-url])]
     (routes
       (context relative-root-url []
+        ;; Landing pages - Note that the landing pages must come before the
+        ;; api-docs, since api-docs/docs-routes also use a context of "site"
+        ;; but have the last entry as a 404 renderer, and as such, would
+        ;; prevent any pages in the "site" context from rendering after that
+        ;; point.
+        (GET "/site/collections/landing-pages" request
+          (landing-links-page request))
+        (GET "/site/collections/eosdis-landing-pages" request
+          (eosdis-landing-links-page request))
         ;; Add routes for API documentation
         (api-docs/docs-routes
           (get-in system [:public-conf :protocol])
