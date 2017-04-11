12eec3f4a7bb633f3bd5689a88c5de01690e4673
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-04-11 15:49:10 -0500

    Added link-formatters.

--- search-app/src/cmr/search/site/data.clj
+++ search-app/src/cmr/search/site/data.clj
@@ -3,17 +3,30 @@
   data structures to be consumed by site page templates.")
 
 (defn get-index
-  ""
+  "Return the data for the index page (none for now)."
   [request]
   {})
 
 (defn get-landing-links
-  ""
+  "Provide the list of links that will be rendered on the general landing
+  pages page."
   [request]
   {:links [{:href "/site/collections/eosdis-landing-pages"
             :text "Landing Pages for EOSDIS Collections"}]})
 
+(defn doi-link
+  "Given DOI umm data of the form `{:doi <STRING>}`, generate a landing page
+  link."
+  [doi-data]
+  (format "http://dx.doi.org/%s" (doi-data "DOI")))
+
+(defn cmr-link
+  "Given a CMR host and a concept ID, return the collection landing page for
+  the given id."
+  [cmr-host concept-id]
+  (format "https://%s/concepts/%s.html" cmr-host concept-id))
+
 (defn get-eosdis-landing-links
-  ""
+  "Generate the data necessary to render EOSDIS landing page links."
   [request]
   {})
--- search-app/test/cmr/search/test/site/data.clj
+++ search-app/test/cmr/search/test/site/data.clj
@@ -1,1 +1,16 @@
-(ns cmr.search.test.site.data)
+(ns cmr.search.test.site.data
+  (:require [clojure.test :refer :all]
+            [cmr.search.site.data :as data]))
+
+(deftest doi-link
+  (testing "generate a link from doi data"
+    (let [data {"DOI" "doi:10.7265/N5R78C49"}]
+      (is (= (data/doi-link data)
+             "http://dx.doi.org/doi:10.7265/N5R78C49")))))
+
+(deftest cmr-link
+  (testing "generate a cmr landing page from a host and a concept id"
+    (let [cmr-host "cmr.sit.earthdata.nasa.gov"
+          concept-id "C1200196931-SCIOPS"]
+      (is (= (data/cmr-link cmr-host concept-id)
+             "https://cmr.sit.earthdata.nasa.gov/concepts/C1200196931-SCIOPS.html")))))
