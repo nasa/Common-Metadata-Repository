aa0d62680b2222a555058bf18f9b4e69e8aaa6cc
Yannick Scherer <yannick.scherer@stylefruits.de>
2017-04-19 16:42:56 +0200

    Version 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject codox-theme-rdash "0.1.2-SNAPSHOT"
+(defproject codox-theme-rdash "0.1.2"
   :description "A modern and responsive Codox theme"
   :url "https://github.com/xsc/codox-theme-rdash"
   :license {:name "Eclipse Public License 1.0"
