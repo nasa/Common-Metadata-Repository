5dec35ab15b8390bbfe60b5096ee9d29c5861317
Yonggang Liu <ygliuvt@gmail.com>
2017-04-28 16:16:55 -0400

    CMR-4095: MMT updated preview gem again. The new commit is 6983be7.

--- collection-renderer-lib/project.clj
+++ collection-renderer-lib/project.clj
@@ -14,7 +14,7 @@
    the hardcoded commit id during dev integration with cmr_metadata_preview project.
    The hardcoded commit id should be updated when MMT releases a new version of the gem."
   (or (System/getenv "CMR_METADATA_PREVIEW_COMMIT")
-      "2f3acec"))
+      "6983be7"))
 
 (def gem-install-path
   "The directory within this library where Ruby gems are installed."
