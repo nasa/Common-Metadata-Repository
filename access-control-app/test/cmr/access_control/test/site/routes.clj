25c5e98067f31c01887d22a0a32b9ed74f8c4d83
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-05-02 13:54:06 -0500

    CMR-4093 - Added missing access-control route tests.

--- access-control-app/test/cmr/access_control/test/site/routes.clj
+++ access-control-app/test/cmr/access_control/test/site/routes.clj
@@ -1,24 +1,24 @@
-(ns cmr.search.test.site.routes
+(ns cmr.access-control.test.site.routes
   (:require [clojure.string :as string]
             [clojure.test :refer :all]
-            [cmr.search.site.routes :as r]
+            [cmr.access-control.site.routes :as r]
             [ring.mock.request :refer [request]]))
 
 (def ^:private scheme "https")
 (def ^:private host "cmr.example.com")
-(def ^:private base-path "/search")
+(def ^:private base-path "")
 (def ^:private base-url (format "%s://%s%s" scheme host base-path))
 (def ^:private system {:public-conf
                         {:protocol scheme
                          :host host
                          :relative-root-url base-path}})
-(def ^:private site (#'cmr.search.site.routes/build-routes system))
+(def ^:private site (#'cmr.access-control.site.routes/build-routes system))
 
-(deftest search-welcome-page
+(deftest access-control-welcome-page
   (testing "visited on a path without a trailing slash"
     (let [response (site (request :get base-url))]
       (testing "produces a HTTP 200 success response"
-        (is (= 200 (:status response))))
+        (is (= 200 (:status response) )))
       (testing "returns the welcome page HTML"
         (is (string/includes?
              (:body response)
@@ -32,20 +32,6 @@
              (:body response)
              "The Common Metadata Repository"))))))
 
-(deftest collections-directory-page
-  (testing "collections directory page returns content"
-    (let [url-path "/site/collections/directory"
-          response (site (request :get (str base-url url-path)))]
-      (is (= 200 (:status response)))
-      (testing "page title is correct"
-        (is (string/includes?
-             (:body response)
-              "Directory of Collections Landing Pages")))
-      (testing "page has a link to EOSDIS collections directory"
-        (is (string/includes?
-             (:body response)
-             "EOSDIS Collections"))))))
-
 (deftest test-404
   (testing "a 404 is returned for a missing document"
     (is (= 404
@@ -55,68 +41,54 @@
                 (site)
                 :status)))
     (is (= 404
-           (->> "/site/docs/search/NOT-A-PAGE.html"
+           (->> "/site/docs/access-control/NOT-A-PAGE.html"
                 (str base-url)
                 (request :get)
                 (site)
                 :status)))))
 
-(deftest search-api-documentation-page
+(deftest access-control-api-documentation-page
   (let [response (site
                    (request
-                     :get (str base-url "/site/docs/search/api.html")))]
+                     :get (str base-url "/site/docs/access-control/api.html")))]
     (testing "uses the incoming host and scheme for the docs endpoint"
       (is (= 200 (:status response)))
-      (is (string/includes?
-           (:body response)
-           "API Documentation")))))
+      (is (string/includes? (:body response) "API Documentation")))))
 
-(deftest search-site-documentation-page
+(deftest access-control-site-documentation-page
   (let [response (site
                    (request
-                     :get (str base-url "/site/docs/search/site.html")))]
+                     :get (str base-url "/site/docs/access-control/site.html")))]
     (testing "uses the incoming host and scheme for the docs endpoint"
       (is (= 200 (:status response)))
       (is (string/includes?
            (:body response)
            "Site Routes &amp; Web Resources Documentation")))))
 
-(deftest search-all-documentation-page
-  (let [response (site (request :get (str base-url "/site/docs/search")))]
+(deftest access-control-all-documentation-page
+  (let [response (site (request :get (str base-url "/site/docs/access-control")))]
     (testing "all docs links appear here"
       (is (= 200 (:status response)))
-      (is (string/includes?
-           (:body response)
-           "Documentation for CMR Search"))
-      (is (string/includes?
-           (:body response)
-           "site/docs/search/api"))
-      (is (string/includes?
-           (:body response)
-           "site/docs/search/site")))))
+      (is (string/includes? (:body response) "Documentation for CMR Access Control"))
+      (is (string/includes? (:body response) "site/docs/access-control/api"))
+      (is (string/includes? (:body response) "site/docs/access-control/site")))))
 
-(deftest search-url-reorg-redirects
-  (let [response (site (request :get (str base-url "/site/docs/search/api")))]
+(deftest access-control-url-reorg-redirects
+  (let [response (site (request :get (str base-url "/site/docs/access-control/api")))]
     (testing "clean docs URL for api docs performs redirect"
       (is (= 307 (:status response)))
       (is (string/includes?
-            (get-in response [:headers "Location"])
-            "site/docs/search/api.html"))))
-  (let [response (site (request :get (str base-url "/site/docs/search/site")))]
+           (get-in response [:headers "Location"])
+           "site/docs/access-control/api.html"))))
+  (let [response (site (request :get (str base-url "/site/docs/access-control/site")))]
     (testing "clean docs URL for site docs performs redirect"
       (is (= 307 (:status response)))
       (is (string/includes?
            (get-in response [:headers "Location"])
-           "site/docs/search/site.html"))))
-  (let [response (site (request :get (str base-url "/site/search_api_docs.html")))]
+           "site/docs/access-control/site.html"))))
+  (let [response (site (request :get (str base-url "/site/access_control_api_docs.html")))]
     (testing "clean docs URL for api docs performs redirect"
       (is (= 301 (:status response)))
       (is (string/includes?
-            (get-in response [:headers "Location"])
-            "site/docs/search/api.html"))))
-  (let [response (site (request :get (str base-url "/site/search_site_docs.html")))]
-    (testing "clean docs URL for site docs performs redirect"
-      (is (= 301 (:status response)))
-      (is (string/includes?
-            (get-in response [:headers "Location"])
-            "site/docs/search/site.html")))))
+           (get-in response [:headers "Location"])
+           "site/docs/access-control/api.html")))))
