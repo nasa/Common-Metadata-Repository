f12c3563ef011894cbee6703a75a6e5821980913
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-05-09 14:30:15 -0500

    CMR-4036 - Fixed kibit errors in dev-system.

--- dev-system/src/cmr/dev_system/tests.clj
+++ dev-system/src/cmr/dev_system/tests.clj
@@ -6,29 +6,31 @@
 
   { \"keys\": [\"alt+super+a\"], \"command\": \"run_command_in_repl\", \"args\": {\"command\": \"(def all-tests-future (future (cmr.dev-system.tests/run-all-tests {:fail-fast? true :speak? true} )))\", \"refresh_namespaces\": true}},
   "
-  (:require [cmr.common.test.test-runner :as t]
-            [cmr.system-int-test.utils.dev-system-util :as dev-sys-util]))
+  (:require
+   [cmr.common.test.test-runner :as t]
+   [cmr.system-int-test.utils.dev-system-util :as dev-sys-util]))
 
 (defn system-integration-test?
   [test-ns]
   (re-find #"system-int-test" test-ns))
 
 (defn integration-test-ns->compare-value
-  "The comparator function to use for sorting integration tests. We want system integration tests
-  to run last."
+  "The comparator function to use for sorting integration tests. We want system integration tests to run last."
   [test-ns]
   (if (system-integration-test? test-ns)
     10
     1))
 
 (defn run-all-tests
-  "Runs all the tests in the cmr. It takes the same options as cmr.common.test-runner/run-all-tests"
+  "Runs all the tests in the cmr. It takes the same options as
+  cmr.common.test-runner/run-all-tests."
   [options]
-  (let [integration-test-namespaces (->> (t/integration-test-namespaces)
-                                         (sort-by integration-test-ns->compare-value))]
-    (t/run-all-tests (assoc options
-                            :integration-test-namespaces integration-test-namespaces
-                            :reset-fn dev-sys-util/reset))))
+  (t/run-all-tests
+    (assoc options
+           :integration-test-namespaces (sort-by
+                                         integration-test-ns->compare-value
+                                         (t/integration-test-namespaces))
+           :reset-fn dev-sys-util/reset)))
 
 
 
