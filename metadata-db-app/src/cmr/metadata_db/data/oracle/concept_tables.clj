11b63abfa7dc966ff477fb3792e6497c503d19ee
Yonggang Liu <ygliuvt@gmail.com>
2017-05-10 08:35:16 -0400

    CMR-4013: Added Variable concept type.

--- metadata-db-app/src/cmr/metadata_db/data/oracle/concept_tables.clj
+++ metadata-db-app/src/cmr/metadata_db/data/oracle/concept_tables.clj
@@ -40,6 +40,10 @@
   [_ _]
   "cmr_humanizers")
 
+(defmethod get-table-name :variable
+  [_ _]
+  "cmr_variables")
+
 (defmethod get-table-name :default
   [provider concept-type]
   ;; Dont' remove the next line - needed to prevent SQL injection
