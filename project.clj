a17a4c2ab789d46588a84786636f847c47f267ad
Duncan McGreggor <oubiwann@gmail.com>
2017-05-12 19:48:36 -0500

    Updated description.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clojang/codox-theme "0.2.0-SNAPSHOT"
-  :description "A Codox Theme for Clojang Projects Documentation "
+  :description "A Codox Theme for Clojang Projects Documentation"
   :url "https://github.com/clojang/codox-theme"
   :license {:name "Eclipse Public License 1.0"
             :url "https://www.eclipse.org/legal/epl-v10.html"
