d3471636b946937620790d96ab025ba7b6d403e6
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-05-18 14:19:09 -0500

    CMR-4134 - Added variable validation.

---
+++ ingest-app/test/cmr/ingest/test/validation/business_rule_validation.clj
@@ -0,0 +1,17 @@
+(ns cmr.ingest.test.validation.business-rule-validation
+  (:require
+   [clojure.test :refer :all]
+   [cmr.ingest.validation.business-rule-validation :as bv]))
+
+(def granule-test-concept {:concept-type :granule})
+(def variable-test-concept {:concept-type :variable})
+
+(deftest business-rules-for-granules
+  (is (= []
+         (bv/business-rule-validations
+          (:concept-type granule-test-concept)))))
+
+(deftest business-rules-for-variables
+  (is (= []
+         (bv/business-rule-validations
+          (:concept-type variable-test-concept)))))
