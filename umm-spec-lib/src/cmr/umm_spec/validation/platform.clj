c6e38ab71ef8fb2a3ca70af0a6deaeb2bda6c296
Erich Reiter <erich.e.reiter@nasa.gov>
2017-06-12 08:48:46 -0400

    Fixing CMR-4203. Changing the platform validation so that it validates not just the shortname but the short name, and characteristics name and value together.

--- umm-spec-lib/src/cmr/umm_spec/validation/platform.clj
+++ umm-spec-lib/src/cmr/umm_spec/validation/platform.clj
@@ -19,7 +19,40 @@
                  (vu/unique-by-name-validator :ShortName)]
    :Characteristics (vu/unique-by-name-validator :Name)})
 
+(def ^:private unique-fields
+  "Defines the fields to use that combined create unique platform names.
+  GCMD's keyword management system (KMS) defines aircraft, boats, ballons, etc.
+  with the model number. So the short name is not specific enough to identify
+  specific platform instances. Therefore we are adding Characteristic names and values
+  so that specific aircraft tail numbers, or boat IDS, etc. can be specified with the
+  short name."
+  #{:ShortName :Characteristics :Name :Value})
+
+(defn- get-platform-identifiers
+  "Concatenates ShortName with Characteristic Name and Value fields.
+   GCMD's keyword management system (KMS) defines aircraft, boats, ballons, etc.
+   with the model number. So the short name is not specific enough to identify
+   specific platform instances. Therefore we are adding Characteristic names and values
+   so that specific aircraft tail numbers, or boat IDS, etc. can be specified with the
+   short name."
+  [values unique-fields]
+  (let [{:keys [ShortName Characteristics Name Value]} unique-fields]
+    (map (fn [value]
+           (apply str
+                 (ShortName value)
+                 (map #(str (Name %) (Value %)) (Characteristics value))))
+         values)))
+
+(defn- unique-platform-validator
+  "Validates a list of items is unique by a specified field. Takes the name field and returns a
+  new validator."
+  [unique-fields]
+  (fn valid [field-path values]
+    (let [freqs (frequencies (get-platform-identifiers values unique-fields))]
+      (when-let [duplicate-names (seq (for [[v freq] freqs :when (> freq 1)] v))]
+        {field-path [(format "%%s must be unique. This contains duplicates named [%s]."
+                             (clojure.string/join ", " duplicate-names))]}))))
+
 (def platforms-validation
-  "Defines the list of validation functions for validating collection platforms"
   [(v/every platform-validations)
-   (vu/unique-by-name-validator :ShortName)])
+   (unique-platform-validator unique-fields)])
