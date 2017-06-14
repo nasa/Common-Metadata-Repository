0b122567e57bdaf82c1974ede0d2b76e7f74466d
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-06-14 01:52:53 -0500

    IP SPRINT - Added more test runner utility functions.

--- common-lib/src/cmr/common/test/runners/util.clj
+++ common-lib/src/cmr/common/test/runners/util.clj
@@ -1,6 +1,7 @@
 (ns cmr.common.test.runners.util
   (:require
-   [clojure.set :as set]))
+   [clojure.set :as set]
+   [clojure.string :as string]))
 
 (defn integration-test-namespaces
   "The list of integration test namespaces. Anything that contains 'cmr.' and 'int-test' is
@@ -26,3 +27,38 @@
        (#(set/difference % (set (integration-test-namespaces))))
        vec
        sort))
+
+(defn union-namespaces
+  "Get a collection of combined unit and integration tests. This function is
+  useful for auditing CMR tests, in particular, comparing with the independent
+  results obtained from calling `(get-all-tests)`."
+  []
+  (set/union (integration-test-namespaces)
+             (unit-test-namespaces)))
+
+(defn- -all-cmr-namespaces
+  "Returns the collection of all CMR namespaces, each as a namespace object."
+  []
+  (filter (fn [x] (string/starts-with? (ns-name x) "cmr")) (all-ns)))
+
+(defn all-cmr-namespaces
+  "Returns the collection of all CMR namespaces, each as a namespace string.
+  This matches the return values of the `integration-test-namespaces` and
+  `unit-test-namespaces` functions."
+  []
+  (filter (fn [x] (string/starts-with? x "cmr")) (map ns-name (all-ns))))
+
+(defn get-all-tests
+  "This function is useful for auditing the CMR tests. Given a collection of
+  namespaces, it will search all of them for tests, returning all discovered
+  tests as vars. If no collection of namespaces is passed as an arg, a list
+  of all namepsace objects whose namepsace names beginn with 'cmr' will be
+  used instead."
+  ([]
+    (get-all-tests (-all-cmr-namespaces)))
+  ([nss]
+    (->> nss
+         (map #(vals (ns-interns %)))
+         (flatten)
+         (map #(-> % meta :test))
+         (remove nil?))))
