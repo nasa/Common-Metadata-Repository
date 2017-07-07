70628f70abcbac55ccea4014297c084cc934a267
Lauren Frederick <lauren@element84.com>
2017-07-07 14:55:57 -0400

    CMR-4323 update migration

--- metadata-db-app/src/migrations/057_regenerate_collection_identifiers.clj
+++ metadata-db-app/src/migrations/057_regenerate_collection_identifiers.clj
@@ -18,9 +18,8 @@
       (for [entry-title entry-titles
             :let [result (j/query (config/db) [(format "select distinct concept_id, entry_title from metadata_db.%s where entry_title = ?" t)
                                                entry-title])]
-            :when (not (nil? entry-title))]        
-        {:concept_id (:concept_id (first result))
-         :entry_title (:entry_title (first result))}))))
+            :when (not (nil? entry-title))]
+        (first result)))))
 
 (defn- get-entry-titles
   "For a given provider and concept-ids in collection-identifier, returns list of collection entry-titles"
@@ -30,11 +29,10 @@
                        providers/dbresult->provider)
           t (h/get-provider-collection-tablename provider)]
       (for [concept-id concept-ids
-            :let [result (j/query (config/db) [(format "select distinct concept_id entry_title from metadata_db.%s where concept_id = ?" t)
+            :let [result (j/query (config/db) [(format "select distinct concept_id, entry_title from metadata_db.%s where concept_id = ?" t)
                                                concept-id])]
             :when (not (nil? concept-id))]
-        {:concept_id (:concept_id (first result))
-         :entry_title (:entry_title (first result))}))))
+        (first result)))))
 
 (defn up
   "Migrates the database up to version 57."
@@ -48,19 +46,17 @@
                        (util/gzip-blob->string)
                        (edn/read-string))
           {{:keys [collection-identifier collection-applicable granule-applicable provider-id]} :catalog-item-identity} metadata
-          collections-by-entry-titles (if (or collection-applicable granule-applicable)
-                                        (get-concept-ids provider-id collection-identifier)
-                                        nil)
-          collections-by-concept-ids (if (or collection-applicable granule-applicable)
-                                       (get-entry-titles provider-id collection-identifier)
-                                       nil)
-          collections (distinct (concat collections-by-entry-titles collections-by-concept-ids))
+          collections-by-entry-titles (when (or collection-applicable granule-applicable)
+                                        (get-concept-ids provider-id collection-identifier))
+          collections-by-concept-ids (when (or collection-applicable granule-applicable)
+                                       (get-entry-titles provider-id collection-identifier))
+          collections (distinct (remove nil? (concat collections-by-entry-titles collections-by-concept-ids)))
           metadata (if deleted
                      metadata
                      (if (seq collections)
                        (-> metadata
                            (assoc-in [:catalog-item-identity :collection-identifier :concept-ids] (map :concept_id collections))
-                           (assoc-in [:catalog-item-identity :collection-identifier :entry-titles] (map :entry_titles collections)))
+                           (assoc-in [:catalog-item-identity :collection-identifier :entry-titles] (map :entry_title collections)))
                        metadata))
           metadata (-> metadata
                        pr-str
