bb7e7e6aaba8b8043d1f8453a25ea17d06a9fbbc
Yonggang Liu <ygliuvt@gmail.com>
2017-07-14 16:45:43 -0400

    CMR-4239 - Resolved merge conflicts.

--- system-int-test/src/cmr/system_int_test/data2/umm_spec_collection.clj
+++ system-int-test/src/cmr/system_int_test/data2/umm_spec_collection.clj
@@ -1,13 +1,11 @@
 (ns cmr.system-int-test.data2.umm-spec-collection
-  "Contains collection data generators for example-based testing in system
-  integration tests."
+  "Contains data generators for example based testing in system integration tests."
   (:require
     [clj-time.core :as t]
     [clj-time.format :as f]
     [cmr.common.date-time-parser :as p]
     [cmr.common.util :as util]
     [cmr.system-int-test.data2.core :as d]
-    [cmr.system-int-test.data2.umm-spec-common :as dc]
     [cmr.umm-spec.additional-attribute :as aa]
     [cmr.umm-spec.models.umm-collection-models :as umm-c]
     [cmr.umm-spec.models.umm-common-models :as umm-cmn]
@@ -19,10 +17,129 @@
   [attribs]
   (umm-c/map->LocationKeywordType attribs))
 
+(defn instrument
+  "Return an instrument based on instrument attribs"
+  [attribs]
+  (umm-cmn/map->InstrumentType (merge {:ShortName (d/unique-str "short-name")}
+                                      attribs)))
+
+(defn instruments
+  "Return a sequence of instruments with the given short names"
+  [& short-names]
+  (map #(umm-cmn/map->InstrumentType
+          {:ShortName %
+           :LongName (d/unique-str "long-name")})
+       short-names))
+
+(defn instrument-with-childinstruments
+  "Return an instrument, with a sequence of child instruments"
+  [short-name & child-instrument-short-names]
+  (let [childinstruments (apply instruments child-instrument-short-names)]
+    (umm-cmn/map->InstrumentType {:ShortName short-name
+                                  :LongName (d/unique-str "long-name")
+                                  :ComposedOf childinstruments})))
+
+(defn characteristic
+  "Returns a platform characteristic"
+  [attribs]
+  (umm-cmn/map->CharacteristicType (merge {:Name (d/unique-str "name")
+                                           :Description "dummy"
+                                           :DataType "dummy"
+                                           :Unit "dummy"
+                                           :Value "dummy"}
+                                     attribs)))
+(defn platform
+  "Return a platform based on platform attribs"
+  [attribs]
+  (umm-cmn/map->PlatformType (merge {:ShortName (d/unique-str "short-name")
+                                     :LongName (d/unique-str "long-name")
+                                     :Type (d/unique-str "Type")}
+                                    attribs)))
+(defn platforms
+  "Return a sequence of platforms with the given short names"
+  [& short-names]
+  (map #(umm-cmn/map->PlatformType
+          {:ShortName %
+           :LongName (d/unique-str "long-name")
+           :Type (d/unique-str "Type")})
+       short-names))
+
+(defn platform-with-instruments
+  "Return a platform with a list of instruments"
+  [short-name & instr-short-names]
+  (let [instruments (apply instruments instr-short-names)]
+    (umm-cmn/map->PlatformType {:ShortName short-name
+                                :LongName (d/unique-str "long-name")
+                                :Type (d/unique-str "Type")
+                                :Instruments instruments})))
+
+(defn platform-with-instrument-and-childinstruments
+  "Return a platform with an instrument and a list of child instruments"
+  [plat-short-name instr-short-name & child-instrument-short-names]
+  (let [instr-with-sensors (apply instrument-with-childinstruments instr-short-name child-instrument-short-names)]
+    (umm-cmn/map->PlatformType {:ShortName plat-short-name
+                                :LongName (d/unique-str "long-name")
+                                :Type (d/unique-str "Type")
+                                :Instruments [instr-with-sensors]})))
+
+(defn projects
+  "Return a sequence of projects with the given short names"
+  [& short-names]
+  (map #(umm-cmn/map->ProjectType
+          {:ShortName %
+           :LongName (d/unique-str "long-name")})
+       short-names))
+
+(defn project
+  "Return a project with the given short name and long name"
+  [project-sn long-name]
+  (umm-cmn/map->ProjectType
+    {:ShortName project-sn
+     :LongName (d/unique-str long-name)}))
+
+(defn data-center
+  "Returns data center"
+  [attribs]
+  (umm-cmn/map->DataCenterType attribs))
+
 (defn directory-name
   "Returns directory name"
   [attribs]
   (umm-c/map->DirectoryNameType attribs))
+
+(defn related-url
+  "Creates related url for online_only test"
+  ([]
+   (related-url nil))
+  ([attribs]
+   (umm-cmn/map->RelatedUrlType (merge {:URL (d/unique-str "http://example.com/file")
+                                        :Description (d/unique-str "description")}
+                                       attribs))))
+(defn spatial
+  [attributes]
+  (let [{:keys [sc hsd vsds gsr orbit]} attributes]
+    (umm-cmn/map->SpatialExtentType {:SpatialCoverageType sc
+                                     :HorizontalSpatialDomain
+                                       (when hsd (umm-cmn/map->HorizontalSpatialDomainType hsd))
+                                     :VerticalSpatialDomains
+                                       (when vsds (map umm-cmn/map->VerticalSpatialDomainType vsds))
+                                     :GranuleSpatialRepresentation gsr
+                                     :OrbitParameters (when orbit (umm-cmn/map->OrbitParametersType orbit))})))
+
+(defn contact-person
+  "Creates a Personnel record for the opendata tests."
+  ([first-name last-name email]
+   (contact-person first-name last-name email "dummy"))
+  ([first-name last-name email role]
+   (let [contact-info (when email
+                        (umm-cmn/map->ContactInformationType
+                          {:ContactMechanisms [(umm-cmn/map->ContactMechanismType
+                                                 {:Type "Email"
+                                                  :Value email})]}))]
+     (umm-cmn/map->ContactPersonType {:FirstName first-name
+                                      :LastName last-name
+                                      :ContactInformation contact-info
+                                      :Roles [role]}))))
 
 ;; Used for testing warnings when reqired properties are missing.
 (def umm-c-missing-properties
@@ -138,6 +255,6 @@
   ([attribs concept-format]
    (let [{:keys [provider-id native-id]} attribs]
      (-> attribs
-         (collection)
+         collection
          (assoc :provider-id provider-id :native-id native-id)
          (d/umm-c-collection->concept concept-format)))))
