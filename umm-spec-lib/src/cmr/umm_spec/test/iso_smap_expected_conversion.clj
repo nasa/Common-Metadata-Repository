261380baea9bba2254bfa3b467497d27b54f1075
Erich Reiter <erich.e.reiter@nasa.gov>
2017-07-13 23:45:55 -0400

    Fixing CMR-4314 adding UMM<=>SMAP mapping for CollectionDataType and fixing the ISO SMAP expected test dependency on ISO 19115-2

--- umm-spec-lib/src/cmr/umm_spec/test/iso_smap_expected_conversion.clj
+++ umm-spec-lib/src/cmr/umm_spec/test/iso_smap_expected_conversion.clj
@@ -13,30 +13,45 @@
    [cmr.umm-spec.related-url :as ru-gen]
    [cmr.umm-spec.test.expected-conversion-util :as conversion-util]
    [cmr.umm-spec.test.iso19115-expected-conversion :as iso]
+   [cmr.umm-spec.test.iso-shared :as iso-shared]
    [cmr.umm-spec.test.location-keywords-helper :as lkt]
    [cmr.umm-spec.umm-to-xml-mappings.iso19115-2.data-contact :as data-contact]
    [cmr.umm-spec.url :as url]
-   [cmr.umm-spec.util :as su]))
+   [cmr.umm-spec.util :as su])
+ (:use
+   [cmr.umm-spec.models.umm-collection-models]
+   [cmr.umm-spec.models.umm-common-models]))
+
+(defn- expected-related-url-get-service
+  "Returns related-url with the expected GetService"
+  [related-url]
+  (let [URI (if (empty? (get-in related-url [:GetService :URI]))
+              [(:URL related-url)]
+              (get-in related-url [:GetService :URI]))]
+      (if (and (= "DistributionURL" (:URLContentType related-url))
+               (= "GET SERVICE" (:Type related-url)))
+          (if (nil? (:GetService related-url))
+            (assoc related-url :GetService (map->GetServiceType
+                                              {:MimeType su/not-provided
+                                               :Protocol su/not-provided
+                                               :FullName su/not-provided
+                                               :DataID su/not-provided
+                                               :DataType su/not-provided
+                                               :URI URI}))
+            (assoc-in related-url [:GetService :URI] URI))
+          related-url)))
 
 (defn- expected-iso-smap-related-urls
-  "The expected RelatedUrl value when converting from umm-C to iso-smap
-   and back to umm-C"
-  [related-urls]
-  (seq (for [related-url related-urls]
-         (-> related-url
-             (update :URL #(url/format-url % true))))))
-
-(defn- expected-collection-related-urls
   "Update the collection top level RelatedUrls. Do processing not applicable
   for data center/data contact RelatedUrls. DataCenter and DataContact URL
   types are not applicable here, so remove."
   [related-urls]
-  (let [related-urls (expected-iso-smap-related-urls related-urls)]
-    (seq (for [related-url
-                (remove #(#{"DataCenterURL" "DataContactURL"} (:URLContentType %))
-                        related-urls)]
-           (-> related-url
-               (update :Description #(when % (str/trim %))))))))
+  (seq (for [related-url (remove #(#{"DataCenterURL" "DataContactURL"} (:URLContentType %)) related-urls)]
+         (-> related-url
+             (update :URL #(url/format-url % true))
+             (update :Description #(when % (str/trim %)))
+             (expected-related-url-get-service)
+             (assoc :GetData nil)))))
 
 (defn- normalize-smap-instruments
   "Collects all instruments across given platforms and returns a seq of platforms with all
@@ -232,17 +247,50 @@
          (map cmn/map->DataCenterType)
          distinct)))
 
+(defn- not-provided-begin-date
+  "Returns the default java epoch zero used for not provided date values.
+   This function returns the def record and not just a map so that
+   it can be compared to the actual round trip translation."
+  []
+  (for [x (range 0 1)]
+    (map->RangeDateTimeType {:BeginningDateTime du/parsed-default-date})))
+
+(defn- not-provided-temporal-extents
+  "Returns a default temporal extent type def record and not a map so that
+   it can be compared to the actual round trip translation."
+  []
+  (for [x (range 0 1)]
+    (map->TemporalExtentType {:TemporalRangeType nil
+                              :PrecisionOfSeconds nil
+                              :EndsAtPresentFlag nil
+                              :RangeDateTimes (not-provided-begin-date)
+                              :SingleDateTimes nil
+                              :PeriodicDateTimes nil})))
+
+(defn- expected-temporal
+  "Changes the temporal extent to the expected outcome of a ISO SMAP translation."
+  [temporal-extents]
+  (->> temporal-extents
+       (map #(assoc % :TemporalRangeType nil))
+       (map #(assoc % :PrecisionOfSeconds nil))
+       iso-shared/fixup-iso-ends-at-present
+       (iso-shared/split-temporals :RangeDateTimes)
+       (iso-shared/split-temporals :SingleDateTimes)
+       iso-shared/sort-by-date-type-iso
+       (#(or (seq %) (not-provided-temporal-extents)))))
+
 (defn umm-expected-conversion-iso-smap
-  [umm-coll original-brs]
+  "Change the UMM to what is expected when translating from ISO SMAP so that it can
+   be compared to the actual translation."
+  [umm-coll]
   (-> umm-coll
         (assoc :DirectoryNames nil)
         (update-in [:SpatialExtent] expected-smap-iso-spatial-extent)
         (update-in [:DataDates] expected-smap-data-dates)
-        ;; ISO SMAP does not support the PrecisionOfSeconds field.
-        (update-in-each [:TemporalExtents] assoc :PrecisionOfSeconds nil)
-        ;; Implement this as part of CMR-2057
-        (update-in-each [:TemporalExtents] assoc :TemporalRangeType nil)
+        (update :DataLanguage #(or % "eng"))
+        (update :TemporalExtents expected-temporal)
         (assoc :MetadataAssociations nil) ;; Not supported for ISO SMAP
+        (update :ISOTopicCategories iso-shared/expected-iso-topic-categories)
         (update :DataCenters expected-data-centers)
         (assoc :VersionDescription nil)
         (assoc :ContactGroups nil)
@@ -251,13 +299,12 @@
         (assoc :AccessConstraints nil)
         (assoc :SpatialKeywords nil)
         (assoc :TemporalKeywords nil)
-        (assoc :CollectionDataType nil)
         (assoc :AdditionalAttributes nil)
         (assoc :ProcessingLevel (umm-c/map->ProcessingLevelType {:Id su/not-provided}))
         (assoc :Distributions nil)
         (assoc :PublicationReferences nil)
         (assoc :AncillaryKeywords nil)
-        (update :RelatedUrls expected-collection-related-urls)
+        (update :RelatedUrls expected-iso-smap-related-urls)
         (update :ScienceKeywords expected-science-keywords)
         (assoc :PaleoTemporalCoverages nil)
         (assoc :MetadataDates nil)
