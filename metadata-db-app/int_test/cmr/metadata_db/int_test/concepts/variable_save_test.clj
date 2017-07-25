d48c0bb63001ea9784fe0e033c1d651ee3766294
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-07-25 10:09:41 -0500

    CMR-4350 - Added created-at test for variables (BONUS).

--- metadata-db-app/int_test/cmr/metadata_db/int_test/concepts/variable_save_test.clj
+++ metadata-db-app/int_test/cmr/metadata_db/int_test/concepts/variable_save_test.clj
@@ -7,10 +7,13 @@
    [cmr.metadata-db.int-test.concepts.concept-save-spec :as c-spec]
    [cmr.metadata-db.int-test.utility :as util]))
 
+(def provider-id "REG_PROV")
+
 ;;; fixtures
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-fixtures :each (util/reset-database-fixture {:provider-id "REG_PROV" :small false}))
+(use-fixtures :each (util/reset-database-fixture
+                     {:provider-id provider-id :small false}))
 
 (defmethod c-spec/gen-concept :variable
   [_ provider-id uniq-num attributes]
@@ -19,8 +22,43 @@
 ;; tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (deftest save-variable-test
-  (c-spec/general-save-concept-test :variable ["REG_PROV"]))
+  (c-spec/general-save-concept-test :variable [provider-id]))
 
 (deftest save-variable-with-missing-required-parameters-test
   (c-spec/save-test-with-missing-required-parameters
-    :variable ["REG_PROV"] [:concept-type :provider-id :native-id :extra-fields]))
+    :variable [provider-id] [:concept-type :provider-id :native-id :extra-fields]))
+
+(deftest save-variable-created-at-test
+  (testing "Save variable multiple times gets same created-at"
+    (let [initial-var (util/variable-concept provider-id 2)
+          ;; 1) Save a variable
+          ;; 2) Then wait for a small period of time before saving it again
+          ;; 3) Then wait again and save a tombstone.
+          ;; 4) Finally, wait a bit and save a new (non-tombstone) revision.
+          ;;
+          ;; All should have the same `created-at` value.
+          ;;
+          ;; Note - Originally planned to use the time-keeper functionality
+          ;; for this, but metdata-db tests don't have access to the control
+          ;; api that would allow this to work in CI.
+          {concept-id :concept-id
+           initial-revision-id :revision-id} (util/save-concept initial-var)
+          _ (Thread/sleep 100)
+          {second-revision-id :revision-id} (util/save-concept initial-var)
+          _ (Thread/sleep 100)
+          {tombstone-revision-id :revision-id} (util/delete-concept
+                                                concept-id)
+          _ (Thread/sleep 100)
+          {final-revision-id :revision-id} (util/save-concept initial-var)
+          [initial-revision
+           second-revision
+           tombstone
+           final-revision] (mapv #(:concept (util/get-concept-by-id-and-revision concept-id %))
+                                 [initial-revision-id
+                                  second-revision-id
+                                  tombstone-revision-id
+                                  final-revision-id])]
+      (is (util/created-at-same? initial-revision
+                                 second-revision
+                                 tombstone
+                                 final-revision)))))
