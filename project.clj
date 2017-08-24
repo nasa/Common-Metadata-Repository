43e7f19b842227f4cf23f449c91dc127d934ce14
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-08-24 11:16:56 -0500

    Added HTTP client library dependencies.

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "Apache License, Version 2.0"
             :url "https://opensource.org/licenses/Apache-2.0"}
   :dependencies [
+    [clj-http "3.7.0"]
+    [cljs-http "0.1.43"]
     [org.clojure/clojure "1.8.0"]
     [org.clojure/clojurescript "1.9.908"]]
   :profiles {
