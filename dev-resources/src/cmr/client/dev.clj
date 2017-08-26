c24ee20dda31a6046bf3fa7af71e1ba42892cd08
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-08-26 16:37:05 -0500

    Added common namespaces.

--- dev-resources/src/cmr/client/dev.clj
+++ dev-resources/src/cmr/client/dev.clj
@@ -8,6 +8,8 @@
    [clojure.string :as string]
    [clojure.tools.namespace.repl :as repl]
    [cmr.client.ac.core :as ac]
+   [cmr.client.common.const :as const]
+   [cmr.client.common.util :as util]
    [cmr.client.http.core :as http]
    [cmr.client.ingest.core :as ingest]
    [cmr.client.search.core :as search]))
