9585b470c0f8345336fbbe2b3c069dcc2edca017
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-08-27 03:03:42 -0500

    Added some Search API support.

--- src/clj/cmr/client/search/core.clj
+++ src/clj/cmr/client/search/core.clj
@@ -1,5 +1,43 @@
 (ns cmr.client.search.core
  (:require
-  [cmr.client.common.const :as const]
+  [cmr.client.base :as base]
   [cmr.client.common.util :as util]
-  [cmr.client.http.core :as http]))
+  [cmr.client.http.core :as http]
+  [cmr.client.search.impl :as impl])
+ (:import
+  (cmr.client.search.impl CMRSearchClientData)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Protocols &tc.   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defprotocol CMRSearchAPI
+  (get-collections [this] [this http-options] [this query-params http-options])
+  (get-concept [this concept-id http-options]
+               [this concept-id revision-id http-options])
+  (get-granules [this http-options] [this query-params http-options])
+  (get-humanizers [this] [this http-options])
+  (get-tag [this tag-id http-options] [this tag-id query-params http-options])
+  (get-tags [this http-options] [this query-params http-options])
+  (get-tiles [this http-options] [this query-params http-options])
+  (get-variables [this http-options] [this query-params http-options]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Implementation   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(extend CMRSearchClientData
+        CMRSearchAPI
+        impl/client-behaviour)
+
+(extend CMRSearchClientData
+        base/CMRClientAPI
+        base/client-behaviour)
+
+(def create-client
+  (util/create-service-client-constructor
+   :search
+   #'cmr.client.search.core/create-client
+   impl/->CMRSearchClientData
+   base/make-options
+   http/create-client))
---
+++ src/clj/cmr/client/search/impl.clj
@@ -0,0 +1,112 @@
+(ns cmr.client.search.impl
+ (:require
+  [cmr.client.base :as base]
+  [cmr.client.http.core :as http]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Records &tc.   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defrecord CMRSearchClientData [
+  endpoint
+  options
+  http-client])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Implementation   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn- get-collections
+  ([this]
+   (get-collections this {} {}))
+  ([this http-options]
+   (get-collections this {} http-options))
+  ([this query-params http-options]
+   (-> this
+       :http-client
+       (http/get (base/get-url this "/collections")
+                 (merge {:query-params query-params}
+                        http-options)))))
+
+(defn- get-concept
+  ([this concept-id http-options]
+   (-> this
+       :http-client
+       (http/get (base/get-url this (str "/concept/" concept-id))
+                 http-options)))
+  ([this concept-id revision-id http-options]
+   (-> this
+       :http-client
+       (http/get (base/get-url this (format "/concept/%s/%s"
+                                            concept-id
+                                            revision-id))
+                 http-options))))
+
+(defn- get-granules
+  ([this http-options]
+   (get-granules this {} http-options))
+  ([this query-params http-options]
+   (-> this
+       :http-client
+       (http/get (base/get-url this "/granules")
+                 (merge {:query-params query-params}
+                        http-options)))))
+
+(defn- get-humanizers
+  ([this]
+   (get-humanizers this {}))
+  ([this http-options]
+   (-> this
+       :http-client
+       (http/get (base/get-url this "/humanizers")
+                 http-options))))
+
+(defn- get-tag
+  ([this tag-id http-options]
+   (get-tag this tag-id {} http-options))
+  ([this tag-id query-params http-options]
+   (-> this
+       :http-client
+       (http/get (base/get-url this ("/tag/" tag-id))
+                 (merge {:query-params query-params}
+                        http-options)))))
+
+(defn- get-tags
+  ([this http-options]
+   (get-tags this {} http-options))
+  ([this query-params http-options]
+   (-> this
+       :http-client
+       (http/get (base/get-url this "/tags")
+                 (merge {:query-params query-params}
+                        http-options)))))
+
+(defn- get-tiles
+  ([this http-options]
+   (get-tiles this {} http-options))
+  ([this query-params http-options]
+   (-> this
+       :http-client
+       (http/get (base/get-url this "/tiles")
+                 (merge {:query-params query-params}
+                        http-options)))))
+
+(defn- get-variables
+  ([this http-options]
+   (get-variables this {} http-options))
+  ([this query-params http-options]
+   (-> this
+       :http-client
+       (http/get (base/get-url this "/variables")
+                 (merge {:query-params query-params}
+                        http-options)))))
+
+(def client-behaviour
+  {:get-collections get-collections
+   :get-concept get-concept
+   :get-granules get-granules
+   :get-humanizers get-humanizers
+   :get-tag get-tag
+   :get-tags get-tags
+   :get-tiles get-tiles
+   :get-variables get-variables})
