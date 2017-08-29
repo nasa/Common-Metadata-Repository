32b3351fa75caf466985e71b56fc7f5a9c7c67af
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-08-29 09:40:15 -0500

    CMR-4396 - Addressed review feedback.

--- search-app/src/cmr/search/site/static.clj
+++ search-app/src/cmr/search/site/static.clj
@@ -7,9 +7,14 @@
    [cmr.common-app.static :as static]
    [cmr.common.log :refer :all]
    [cmr.search.site.data :as data]
+   [cmr.search.site.static.directory :as directory]
+   [cmr.search.site.static.site :as site]
    [cmr.search.site.util :as util]
    [cmr.transmit.config :as transmit])
   (:gen-class))
+
+(def supported-directory-tags
+  ["gov.nasa.eosdis"])
 
 (defn generate-api-docs
   "Generate CMR Search API docs."
@@ -35,53 +40,17 @@
            :page-title "Site Routes & Web Resource Documentation"
            :page-content (static/md-file->html "docs/site.md")})))
 
-(defn generate-directory-resources
-  [context base]
-  ;; Generate top-level XML sitemap files
-  (static/generate (str base "/resources/public/sitemap.xml")
-                   "templates/search-sitemap-master.xml"
-                   (assoc (data/get-eosdis-directory-links context)
-                          :base-url "https://cmr.earthdata.nasa.gov/search/"))
-  (static/generate (str base "resources/public/site/sitemap.xml")
-                   "templates/search-sitemap-top-level.xml"
-                   (assoc (data/get-eosdis-directory-links context)
-                          :base-url "https://cmr.earthdata.nasa.gov/search/"))
-  ;; Generate top-level HTML fies
-  (static/generate (str base "resources/public/site/collections/directory/eosdis/index.html")
-                   "templates/search-eosdis-directory-links.html"
-                   (assoc (data/get-eosdis-directory-links context)
-                          :base-url (util/make-relative-parents 4)))
-  ;; Now work on the provider pages ...
-  (doseq [provider-id (map :provider-id (data/get-providers context))]
-    (debug "Generating static files for provider" provider-id)
-    (doseq [tag (:tags context)]
-      ;; Generate XML sitemap files per provider+tag combination
-      (static/generate (util/get-provider-sitemap base provider-id tag)
-                       "templates/search-sitemap-provider-tag.xml"
-                       (assoc (data/get-provider-tag-sitemap-landing-links
-                               context
-                               provider-id
-                               tag)
-                               :base-url "https://cmr.earthdata.nasa.gov/search"))
-      ;; Generate directory HTML files per provider+tag combination
-      (static/generate (util/get-provider-index base provider-id tag)
-                       "templates/search-provider-tag-landing-links.html"
-                       (assoc (data/get-provider-tag-landing-links
-                               context
-                               provider-id
-                               tag)
-                              :base-url (util/make-relative-parents 5))))))
-
 (defn generate-site-resources
   "Generate CMR Search site resources such as directory pages and XML sitemaps
   that are too expensive to generate dynamically."
   []
-  (let [supported-tags ["gov.nasa.eosdis"]
-        context {:cmr-application :search
+  (let [context {:cmr-application :search
                  :execution-context :cli
-                 :tags supported-tags}]
-    (info "Created context:" context)
-    (generate-directory-resources context (util/get-search-app-abs-path))))
+                 :tags supported-directory-tags}
+        app-base-path (util/get-search-app-abs-path)]
+    (debug "Created context for static generation:" context)
+    (site/generate-top-level-resources context app-base-path
+    (directory/generate-directory-resources context app-base-path))))
 
 (defn -main
   "The entrypoint for command-line static docs generation. Example usage:
