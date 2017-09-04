a0e823c29851f0389756c5d99cf7a3745bf88cef
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-09-04 01:50:35 -0500

    Updated deps.

--- project.clj
+++ project.clj
@@ -37,10 +37,10 @@
       :source-paths ^:replace ["src"]
       :test-paths ^:replace []
       :plugins [
-        [jonase/eastwood "0.2.3"]
+        [jonase/eastwood "0.2.4"]
         [lein-ancient "0.6.10"]
         [lein-bikeshed "0.4.1"]
-        [lein-kibit "0.1.2"]
+        [lein-kibit "0.1.5"]
         [venantius/yagni "0.1.4"]]}
     :cljs {
       :source-paths ^:replace ["src/cljs" "src/cljc"]}
