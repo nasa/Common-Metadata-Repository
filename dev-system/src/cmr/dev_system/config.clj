a8a5e5e0821025ad189b2e438bb4793c38157c6a
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-09-07 16:43:39 -0500

    SQS/SNS Hackfest - Refactored dev-system.

---
+++ dev-system/src/cmr/dev_system/config.clj
@@ -0,0 +1,67 @@
+(ns cmr.dev-system.config
+  (:require
+   [cmr.common.config :as config :refer [defconfig]]))
+
+(defn- base-parse-dev-system-component-type
+  "Parse the component type and validate it against the given set."
+  [value valid-types-set]
+  (when-not (valid-types-set value)
+    (throw (Exception. (str "Unexpected component type value:" value))))
+  (keyword value))
+
+(defn parse-dev-system-component-type
+  "Parse the component type and validate it is either in-memory or external."
+  [value]
+  (base-parse-dev-system-component-type value #{"in-memory" "external"}))
+
+(defn parse-dev-system-queue-type
+  "Parse the component type and validate it one of the valid queue types."
+  [value]
+  (base-parse-dev-system-component-type value #{"in-memory" "aws" "external"}))
+
+(defconfig external-echo-port
+  "Specifies the port on which to connect for external echo."
+  {:default 10000
+   :type Long})
+
+(defconfig use-web-compression?
+  "Indicates whether the servers will use gzip compression. Disable this to
+  make tcpmon usable"
+  {:default true
+   :type Boolean})
+
+(defconfig use-access-log
+  "Indicates whether the servers will use the access log."
+ {:default false
+  :type Boolean})
+
+(defconfig dev-system-echo-type
+  "Specifies whether dev system should run an in-memory mock ECHO or use an
+  external ECHO."
+  {:default :in-memory
+   :parser parse-dev-system-component-type})
+
+(defconfig dev-system-db-type
+  "Specifies whether dev system should run an in-memory database or use an
+  external database."
+  {:default :in-memory
+   :parser parse-dev-system-component-type})
+
+(defconfig dev-system-queue-type
+  "Specifies whether dev system should skip the use of a message queue or
+  use a Rabbit MQ or
+  AWS SNS/SQS message queue"
+  {:default :in-memory
+   :parser parse-dev-system-queue-type})
+
+(defconfig dev-system-elastic-type
+  "Specifies whether dev system should run an in-memory elasticsearch or
+  use an external instance."
+  {:default :in-memory
+   :parser parse-dev-system-component-type})
+
+(defconfig gorilla-repl-port
+  "Specifies the port gorilla repl should listen on. It will only be started
+  if non-zero."
+  {:default 0
+   :type Long})
