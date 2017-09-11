d4a271751d5344a010476bd58f2cee4ca5e3716b
laurenfrederick <lauren@element84.com>
2017-09-11 00:43:58 -0400

    CMR-4411 Functions to help analyze relevancy test failures (#273)

---
+++ search-relevancy-test/src/search_relevancy_test/anomaly_analyzer.clj
@@ -0,0 +1,46 @@
+(ns search-relevancy-test.anomaly-analyzer
+  "Functions to help analyze why a relevancy test is failing"
+  (:require
+   [cheshire.core :as json]
+   [clj-http.client :as client]
+   [clojure.string :as str]
+   [search-relevancy-test.core :as core]
+   [search-relevancy-test.relevancy-test :as relevancy-test]))
+
+(defn- get-test-data
+  "Get the test data from the CSV file - need the concept ids we want to look at
+  and the search params. If the anomaly has multiple tests, we get the data for
+  all of the tests."
+  [anomaly-number]
+  (let [csv-data (core/read-anomaly-test-csv)
+        tests (filter #(= (str anomaly-number) (:anomaly %))
+                      csv-data)
+        concept-ids (distinct
+                     (mapcat #(str/split (:concept-ids %) #",")
+                             tests))]
+    {:search (:search (first tests))
+     :concept-ids concept-ids}))
+
+(defn- filter-search-results
+  "Filter search results by the relevant concept ids and also the data that is
+  used for relevancy sorting: the score, short name, version, and processing level
+  id."
+  [search-results test]
+  (let [collections (filter
+                     #(contains? (set (:concept-ids test)) (:id %))
+                     (get-in search-results [:feed :entry]))
+        data (map #(select-keys % [:id :score :version_id :short_name :processing_level_id])
+                  collections)]
+
+    data))
+
+(defn analyze-test
+ "Get relevancy-related data for concepts in the test by performing the search
+ and filtering by concept"
+ [anomaly-number]
+ (let [test (get-test-data anomaly-number)
+       search-results (relevancy-test/perform-search test nil)]
+   (filter-search-results search-results test)))
+
+(comment
+ (analyze-test 39))
