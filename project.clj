7a2a2eb059e61bcd8e0c8c350e0fec3602ffdb58
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-09-27 09:18:38 -0500

    Re-orged code.

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Apache License 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [
+    [org.clojure/clojure "1.8.0"]
+    [org.clojure/java.jdbc "0.7.1"]])
