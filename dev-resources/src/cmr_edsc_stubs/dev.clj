3a1cecc1dded8b94b70620ada652f076bbdfd169
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-09-28 14:55:36 -0500

    Updated dev env.

--- dev-resources/src/cmr_edsc_stubs/dev.clj
+++ dev-resources/src/cmr_edsc_stubs/dev.clj
@@ -1,5 +1,7 @@
 (ns cmr-edsc-stubs.dev
   (:require
+   [clojure.java.io :as io]
+   [clojure.string :as string]
    [clojure.tools.namespace.repl :as repl]
    [cmr-edsc-stubs.core :as stubs]
    [cmr-edsc-stubs.data.core :as data]
