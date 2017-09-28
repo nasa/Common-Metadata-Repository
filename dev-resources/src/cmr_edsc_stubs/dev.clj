12acf971e2dae2b23770ab7790a445c359c53aac
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-09-27 19:47:33 -0500

    Added dev namespace.

---
+++ dev-resources/src/cmr_edsc_stubs/dev.clj
@@ -0,0 +1,12 @@
+(ns cmr-edsc-stubs.dev
+  (:require
+   [clojure.tools.namespace.repl :as repl]
+   [cmr-edsc-stubs.core :as stubs]
+   [cmr-edsc-stubs.data.core :as data]
+   [cmr-edsc-stubs.data.sources :as data-sources]
+   [cmr-edsc-stubs.util :as util]))
+
+;;; Aliases
+
+(def reload #'repl/refresh)
+(def reset #'repl/refresh)
