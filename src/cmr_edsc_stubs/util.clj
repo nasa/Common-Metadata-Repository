b5fe8374429be9abd6f0d2cbd522e22284151f69
Duncan McGreggor <oubiwann@gmail.com>
2017-10-03 11:50:11 -0500

    Removed util funcs that were moved to sample data repo.

--- src/cmr_edsc_stubs/util.clj
+++ src/cmr_edsc_stubs/util.clj
@@ -8,36 +8,6 @@
 (def local-token "mock-echo-system-token")
 (def local-token-header {"echo-token" local-token})
 
-(defn get-file
-  ([file-path]
-    (get-file file-path :data))
-  ([file-path handler]
-    (let [file-obj (io/resource file-path)]
-      (case handler
-        :obj file-obj
-        :data (slurp file-obj)
-        [:json :edn] (-> file-obj
-                         (io/reader)
-                         (json/parse-stream true))))))
-
-(defn get-dir
-  [dir-path]
-  (-> dir-path
-      (io/resource)
-      (.getFile)
-      (io/file)))
-
-(defn files?
-  [files-or-dirs]
-  (filter #(.isFile %) files-or-dirs))
-
-(defn get-files
-  [dir]
-  (-> dir
-      (get-dir)
-      (file-seq)
-      files?))
-
 (defn camel->
   [regex string-format string-data]
   (-> string-data
