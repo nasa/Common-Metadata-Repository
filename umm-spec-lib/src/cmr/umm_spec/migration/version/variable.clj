828da9a5541b144e0ae110b4340da648b6ccb810
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-10-12 00:11:40 -0400

    CMR-4521 - Address review feedback.

--- umm-spec-lib/src/cmr/umm_spec/migration/version/variable.clj
+++ umm-spec-lib/src/cmr/umm_spec/migration/version/variable.clj
@@ -4,16 +4,6 @@
    [cmr.umm-spec.migration.version.interface :as interface]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Utility Functions
-
-(defn- migrate-services-down
-  "Migrate :DOI from CollectionCitation level up to collection level."
-  [v]
-  (if-let [services-obj (some :Services v)]
-    (assoc v :Services services-obj)
-    v))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Variable Migration Implementations
 
 (defmethod interface/migrate-umm-version [:variable "1.0" "1.1"]
@@ -22,4 +12,4 @@
 
 (defmethod interface/migrate-umm-version [:variable "1.1" "1.0"]
   [context v & _]
-  (migrate-services-down v))
+  v)
