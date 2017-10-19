81b381c483f52852c5178a6fd66238e110f7601e
Daniel Zamora <daniel.p.zamora@nasa.gov>
2017-10-19 11:49:15 -0400

    CMR-4412 fixes indentation

--- ingest-app/src/cmr/ingest/services/bulk_update_service.clj
+++ ingest-app/src/cmr/ingest/services/bulk_update_service.clj
@@ -140,10 +140,10 @@
     (if-let [concept (mdb2/get-latest-concept context concept-id)]
       (let [updated-concept (update-collection-concept context concept bulk-update-params)
             warnings (validate-and-save-collection context updated-concept)]
-        (data-bulk-update/update-bulk-update-task-collection-status context task-id
-         concept-id complete-status (create-success-status-message warnings)))
-      (data-bulk-update/update-bulk-update-task-collection-status context task-id
-       concept-id failed-status (format "Concept-id [%s] is not valid." concept-id)))
+        (data-bulk-update/update-bulk-update-task-collection-status
+         context task-id concept-id complete-status (create-success-status-message warnings)))
+      (data-bulk-update/update-bulk-update-task-collection-status
+       context task-id concept-id failed-status (format "Concept-id [%s] is not valid." concept-id)))
     (catch clojure.lang.ExceptionInfo ex-info
       (if (= :conflict (:type (.getData ex-info)))
         ;; Concurrent update - re-queue concept update
