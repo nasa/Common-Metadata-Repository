bffffb25a7d6ef29a5ba2bbbb59c5099468627b7
Yonggang Liu <ygliuvt@gmail.com>
2017-10-26 16:42:08 -0400

    CMR-4479: Updated api doc; Added lots of tests.

--- system-int-test/test/cmr/system_int_test/search/service/concept_retrieval_test.clj
+++ system-int-test/test/cmr/system_int_test/search/service/concept_retrieval_test.clj
@@ -21,160 +21,104 @@
   [(ingest/reset-fixture {"provguid1" "PROV1"})
    service/grant-all-service-fixture]))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; Helper utility functions
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn- verify-retrieved-concept
+  "Verify the retrieved concept by checking against the expected service name,
+  which we have deliberately set to be different form each concept revision."
+  [concept-id revision-id accept-format expected-service-name]
+  (let [{:keys [status body]} (search/retrieve-concept
+                               concept-id revision-id {:accept accept-format})]
+    (is (= 200 status))
+    (is (=  expected-service-name
+            (:Name (json/parse-string body true))))))
 
-(defn- get-svc
-  "Ingest a single svc given a svc-name."
-  [svc-name]
-  (service/ingest-service-with-attrs {:Name svc-name}))
+(defmulti handle-retrieve-concept-error
+  "Exectue the retrieve concept call with the given parameters and returns the status and errors
+  based on the result format"
+  (fn [concept-id revision-id accept-format]
+    accept-format))
 
-(defn- get-updated-svc
-  "Update (re-ingest) an existing (old) service with new data."
-  [old-svc data]
-  (index/wait-until-indexed)
-  (service/ingest-service-with-attrs
-   (merge data
-          {:native-id (:native-id old-svc)})))
+(defmethod handle-retrieve-concept-error mt/umm-json
+  [concept-id revision-id accept-format]
+  (let [{:keys [status body]} (search/retrieve-concept concept-id revision-id
+                                                       {:accept accept-format})
+        errors (:errors (json/parse-string body true))]
+    {:status status :errors errors}))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; Retrieve by concept-id - general test
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defmethod handle-retrieve-concept-error :default
+  [concept-id revision-id accept-format]
+  (search/get-search-failure-xml-data
+   (search/retrieve-concept concept-id revision-id {:accept accept-format
+                                                    :throw-exceptions true})))
 
-(deftest retrieve-service-by-concept-id-any-accept-header
-  (let [svc1-name "service1"
-        svc1-name-new "service1-new"
-        del-svc-name "deleted-service"
-        svc1-v1 (get-svc svc1-name)
-        del-svc (get-svc del-svc-name)
-        _ (index/wait-until-indexed)
-        del-concept (mdb/get-concept (:concept-id del-svc))]
-    (ingest/delete-concept del-concept
-                           (service/token-opts (e/login (s/context) "user1")))
-    (index/wait-until-indexed)
-    (testing "retrieval of a deleted service results in a 404"
-      (let [{:keys [status errors]} (search/get-search-failure-xml-data
-                                     (search/retrieve-concept
-                                      (:concept-id del-svc)
-                                      nil
-                                      {:accept mt/any
-                                       :throw-exceptions true}))]
-        (is (= 404 status))
-        (is (= [(format "Concept with concept-id [%s] could not be found."
-                        (:concept-id del-svc))]
-               errors))))
-    (let [svc1-v2 (get-updated-svc svc1-v1 {:Name svc1-name-new})]
-      (testing (str "Sanity check that the test service got updated and its "
-                    "revision id was incremented.")
-        (is (= 2
-               (inc (:revision-id svc1-v1))
-               (:revision-id svc1-v2))))
-      (let [response (search/retrieve-concept
-                      (:concept-id svc1-v1)
-                      nil
-                      {:accept mt/any})
-            response-v1 (search/retrieve-concept
-                         (:concept-id svc1-v1)
-                         1
-                         {:accept mt/any})
-            response-v2 (search/retrieve-concept
-                         (:concept-id svc1-v1)
-                         2
-                         {:accept mt/any})]
-          (testing "retrieval by service concept-id returns the latest revision."
-            (is (= svc1-name-new
-                   (:Name (json/parse-string (:body response) true)))))
-          (testing (str "retrieval by service concept-id and revision-id returns "
-                    "the specified service")
-            (is (= svc1-name
-                   (:Name (json/parse-string (:body response-v1) true))))
-            (is (= svc1-name-new
-                   (:Name (json/parse-string (:body response-v2) true)))))))
-    (testing "retrieval by service concept-id and incorrect revision-id returns error"
-      (let [no-rev 10000
-            {:keys [status errors]} (search/get-search-failure-xml-data
-                                     (search/retrieve-concept
-                                      (:concept-id svc1-v1)
-                                      no-rev
-                                      {:accept mt/any
-                                       :throw-exceptions true}))]
-        (is (= 404 status))
-        (is (= [(format (str "Concept with concept-id [%s] and revision-id [%s] "
-                             "does not exist.")
-                        (:concept-id svc1-v1)
-                        no-rev)]
-               errors))))
-    (testing "retrieval by non-existent service and revision returns error"
-      (let [no-svc "S404404404-PROV1"
-            no-rev 10000
-            {:keys [status errors]} (search/get-search-failure-xml-data
-                                     (search/retrieve-concept
-                                      no-svc
-                                      no-rev
-                                      {:accept mt/any
-                                       :throw-exceptions true}))]
-        (is (= 404 status))
-        (is (= [(format (str "Concept with concept-id [%s] and revision-id [%s] "
-                             "does not exist.")
-                        no-svc
-                        no-rev)]
-               errors))))))
+(defn- verify-retrieved-concept-error
+  "Verify the expected error code and error message are returned when retrieving the given concept."
+  [concept-id revision-id accept-format err-code err-message]
+  (let [{:keys [status errors]} (handle-retrieve-concept-error
+                                 concept-id revision-id accept-format)]
+    (is (= err-code status))
+    (is (= [err-message] errors))))
 
-(deftest retrieve-service-by-concept-id-umm-json-accept-header
-  (let [svc1-name "service1"
-        svc1-name-new "service1-new"
-        svc1-v1 (get-svc svc1-name)
-        svc1-v2 (get-updated-svc svc1-v1 {:Name svc1-name-new})
-        ;; responses
-        response (search/retrieve-concept
-                  (:concept-id svc1-v1)
-                  nil
-                  {:accept mt/umm-json})
-        response-v1 (search/retrieve-concept
-                     (:concept-id svc1-v1)
-                     1
-                     {:accept mt/umm-json})
-        response-v2 (search/retrieve-concept
-                     (:concept-id svc1-v1)
-                     2
-                     {:accept mt/umm-json})]
-    (testing "retrieval by service concept-id returns the latest revision."
-      (is (= svc1-name-new
-             (:Name (json/parse-string (:body response) true)))))
-    (testing (str "retrieval by service concept-id and revision-id returns "
-              "the specified service")
-      (is (= svc1-name
-             (:Name (json/parse-string (:body response-v1) true))))
-      (is (= svc1-name-new
-             (:Name (json/parse-string (:body response-v2) true)))))
-    (testing "retrieval by service concept-id and incorrect revision-id returns error"
-      (let [no-rev 10000
-            {:keys [status body]} (search/retrieve-concept
-                                     (:concept-id svc1-v1)
-                                     no-rev
-                                     {:accept mt/umm-json})
-            errors (:errors (json/parse-string body true))]
-        (is (= 404 status))
-        (is (= [(format (str "Concept with concept-id [%s] and revision-id [%s] "
-                             "does not exist.")
-                        (:concept-id svc1-v1)
-                        no-rev)]
-               errors))))
-    (testing "unsupported content types return an error"
-      (let [unsupported-mt "unsupported/mime-type"
-            {:keys [status errors]} (search/get-search-failure-xml-data
-                                     (search/retrieve-concept
-                                      (:concept-id svc1-v1)
-                                      nil
-                                      {:accept unsupported-mt
-                                       :throw-exceptions true}))]
-        (is (= 400 status))
-        (is (= [(format (str "The mime types specified in the accept header "
-                             "[%s] are not supported.")
-                        unsupported-mt)]
-               errors))))))
+(deftest retrieve-service-by-concept-id
+  ;; We support UMM JSON format; No format and any format are also accepted.
+  (doseq [accept-format [mt/umm-json nil mt/any]]
+    (let [suffix (if accept-format
+                   (mt/mime-type->format accept-format)
+                   "nil")
+          ;; append result format to service name to make the service name unique
+          ;; for different formats so that the test can be run for multiple formats
+          svc1-r1-name (str "s1-r1" suffix)
+          svc1-r2-name (str "s1-r2" suffix)
+          native-id (str "service1" suffix)
+          svc1-r1 (service/ingest-service-with-attrs {:Name  svc1-r1-name
+                                                      :native-id native-id})
+          svc1-r2 (service/ingest-service-with-attrs {:Name svc1-r2-name
+                                                      :native-id native-id})
+          concept-id (:concept-id svc1-r1)
+          svc1-concept (mdb/get-concept concept-id)]
+      (index/wait-until-indexed)
+
+      (testing "Sanity check that the test service got updated and its revision id was incremented"
+        (is (= concept-id (:concept-id svc1-r2)))
+        (is (= 1 (:revision-id svc1-r1)))
+        (is (= 2 (:revision-id svc1-r2))))
+
+      (testing "retrieval by service concept-id and revision id returns the specified revision"
+        (verify-retrieved-concept concept-id 1 accept-format svc1-r1-name)
+        (verify-retrieved-concept concept-id 2 accept-format svc1-r2-name))
+
+      (testing "retrieval by only service concept-id returns the latest revision"
+        (verify-retrieved-concept concept-id nil accept-format svc1-r2-name))
+
+      (testing "retrieval by non-existent revision returns error"
+        (verify-retrieved-concept-error concept-id 3 accept-format 404
+          (format "Concept with concept-id [%s] and revision-id [3] does not exist." concept-id)))
+
+      (testing "retrieval by non-existent concept-id returns error"
+        (verify-retrieved-concept-error "S404404404-PROV1" nil accept-format 404
+          "Concept with concept-id [S404404404-PROV1] could not be found."))
+
+      (testing "retrieval by non-existent concept-id and revision-id returns error"
+        (verify-retrieved-concept-error "S404404404-PROV1" 1 accept-format 404
+          "Concept with concept-id [S404404404-PROV1] and revision-id [1] does not exist."))
+
+      (testing "retrieval of deleted concept"
+        ;; delete the service concept
+        (ingest/delete-concept svc1-concept (service/token-opts (e/login (s/context) "user1")))
+        (index/wait-until-indexed)
+
+        (testing "retrieval of deleted concept without revision id results in error"
+          (verify-retrieved-concept-error concept-id nil accept-format 404
+            (format "Concept with concept-id [%s] could not be found." concept-id)))
+
+        (testing "retrieval of deleted concept with revision id returns a 400 error"
+          (verify-retrieved-concept-error concept-id 3 accept-format 400
+            (format (str "The revision [3] of concept [%s] represents "
+                         "a deleted concept and does not contain metadata.")
+                    concept-id)))))))
+
+(deftest retrieve-service-with-invalid-format
+  (testing "unsupported accept header results in error"
+    (let [unsupported-mt "unsupported/mime-type"]
+      (verify-retrieved-concept-error "S1111-PROV1" nil unsupported-mt 400
+        (format "The mime types specified in the accept header [%s] are not supported."
+                unsupported-mt)))))
