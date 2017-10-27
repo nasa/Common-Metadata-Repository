a2d14c8800f40b55545a24997a171f035be1fbd0
Yonggang Liu <ygliuvt@gmail.com>
2017-10-27 09:17:47 -0400

    CMR-4479: Renamed function; Fixed typos.

--- system-int-test/test/cmr/system_int_test/search/service/concept_retrieval_test.clj
+++ system-int-test/test/cmr/system_int_test/search/service/concept_retrieval_test.clj
@@ -21,19 +21,19 @@
   [(ingest/reset-fixture {"provguid1" "PROV1"})
    service/grant-all-service-fixture]))
 
-(defn- verify-retrieved-concept
+(defn- assert-retrieved-concept
   "Verify the retrieved concept by checking against the expected service name,
-  which we have deliberately set to be different form each concept revision."
+  which we have deliberately set to be different for each concept revision."
   [concept-id revision-id accept-format expected-service-name]
   (let [{:keys [status body]} (search/retrieve-concept
                                concept-id revision-id {:accept accept-format})]
     (is (= 200 status))
-    (is (=  expected-service-name
-            (:Name (json/parse-string body true))))))
+    (is (= expected-service-name
+           (:Name (json/parse-string body true))))))
 
 (defmulti handle-retrieve-concept-error
-  "Exectue the retrieve concept call with the given parameters and returns the status and errors
-  based on the result format"
+  "Execute the retrieve concept call with the given parameters and returns the status and errors
+  based on the result format."
   (fn [concept-id revision-id accept-format]
     accept-format))
 
@@ -50,7 +50,7 @@
    (search/retrieve-concept concept-id revision-id {:accept accept-format
                                                     :throw-exceptions true})))
 
-(defn- verify-retrieved-concept-error
+(defn- assert-retrieved-concept-error
   "Verify the expected error code and error message are returned when retrieving the given concept."
   [concept-id revision-id accept-format err-code err-message]
   (let [{:keys [status errors]} (handle-retrieve-concept-error
@@ -83,22 +83,22 @@
         (is (= 2 (:revision-id svc1-r2))))
 
       (testing "retrieval by service concept-id and revision id returns the specified revision"
-        (verify-retrieved-concept concept-id 1 accept-format svc1-r1-name)
-        (verify-retrieved-concept concept-id 2 accept-format svc1-r2-name))
+        (assert-retrieved-concept concept-id 1 accept-format svc1-r1-name)
+        (assert-retrieved-concept concept-id 2 accept-format svc1-r2-name))
 
       (testing "retrieval by only service concept-id returns the latest revision"
-        (verify-retrieved-concept concept-id nil accept-format svc1-r2-name))
+        (assert-retrieved-concept concept-id nil accept-format svc1-r2-name))
 
       (testing "retrieval by non-existent revision returns error"
-        (verify-retrieved-concept-error concept-id 3 accept-format 404
+        (assert-retrieved-concept-error concept-id 3 accept-format 404
           (format "Concept with concept-id [%s] and revision-id [3] does not exist." concept-id)))
 
       (testing "retrieval by non-existent concept-id returns error"
-        (verify-retrieved-concept-error "S404404404-PROV1" nil accept-format 404
+        (assert-retrieved-concept-error "S404404404-PROV1" nil accept-format 404
           "Concept with concept-id [S404404404-PROV1] could not be found."))
 
       (testing "retrieval by non-existent concept-id and revision-id returns error"
-        (verify-retrieved-concept-error "S404404404-PROV1" 1 accept-format 404
+        (assert-retrieved-concept-error "S404404404-PROV1" 1 accept-format 404
           "Concept with concept-id [S404404404-PROV1] and revision-id [1] does not exist."))
 
       (testing "retrieval of deleted concept"
@@ -107,11 +107,11 @@
         (index/wait-until-indexed)
 
         (testing "retrieval of deleted concept without revision id results in error"
-          (verify-retrieved-concept-error concept-id nil accept-format 404
+          (assert-retrieved-concept-error concept-id nil accept-format 404
             (format "Concept with concept-id [%s] could not be found." concept-id)))
 
         (testing "retrieval of deleted concept with revision id returns a 400 error"
-          (verify-retrieved-concept-error concept-id 3 accept-format 400
+          (assert-retrieved-concept-error concept-id 3 accept-format 400
             (format (str "The revision [3] of concept [%s] represents "
                          "a deleted concept and does not contain metadata.")
                     concept-id)))))))
@@ -119,6 +119,6 @@
 (deftest retrieve-service-with-invalid-format
   (testing "unsupported accept header results in error"
     (let [unsupported-mt "unsupported/mime-type"]
-      (verify-retrieved-concept-error "S1111-PROV1" nil unsupported-mt 400
+      (assert-retrieved-concept-error "S1111-PROV1" nil unsupported-mt 400
         (format "The mime types specified in the accept header [%s] are not supported."
                 unsupported-mt)))))
