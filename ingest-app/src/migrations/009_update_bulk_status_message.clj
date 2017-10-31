2a454c65b129685029893d0355832114692715b1
Daniel Zamora <daniel.p.zamora@nasa.gov>
2017-10-31 15:20:43 -0400

    CMR-4553 addresses pr comments

--- ingest-app/src/migrations/009_update_bulk_status_message.clj
+++ ingest-app/src/migrations/009_update_bulk_status_message.clj
@@ -9,7 +9,7 @@
   (println "migrations.009-update-bulk-status-message up...")
   (j/db-do-commands (config/db)
                     "ALTER TABLE bulk_update_coll_status MODIFY
-                     (status_message VARCHAR(4000))"))
+                     (status_message VARCHAR2(4000))"))
 
 (defn down
   "Migrates the database down to version 8."
@@ -17,4 +17,4 @@
   (println "migrations.009-update-bulk-status-message down...")
   (j/db-do-commands (config/db) "TRUNCATE TABLE bulk_update_coll_status")
   (j/db-do-commands (config/db) "ALTER TABLE bulk_update_coll_status MODIFY
-                                 (status_message VARCHAR(255))"))
+                                 (status_message VARCHAR2(255))"))
