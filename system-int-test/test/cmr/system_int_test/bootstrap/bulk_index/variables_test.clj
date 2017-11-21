186da19be671f47f46cc5a7fcf31a7cbedbcb34e
Yonggang Liu <ygliuvt@gmail.com>
2017-11-21 13:38:51 -0500

    CMR-4398: Fixed bulk index of variables of a single provider.

--- system-int-test/test/cmr/system_int_test/bootstrap/bulk_index/variables_test.clj
+++ system-int-test/test/cmr/system_int_test/bootstrap/bulk_index/variables_test.clj
@@ -25,7 +25,10 @@
       ;; Disable message publishing so items are not indexed.
       (core/disable-automatic-indexing)
       ;; The following is saved, but not indexed due to the above call
-      (let [var1 (variable/ingest-variable-with-attrs {:provider-id "PROV1"} {} 1)]
+      (let [var1 (variable/ingest-variable-with-attrs {:provider-id "PROV1"} {} 1)
+            ;; create a variable on a different provider PROV2
+            ;; and this variable won't be indexed as a result of indexing variables of PROV1
+            var2 (variable/ingest-variable-with-attrs {:provider-id "PROV2"} {} 1)]
         (is (= 0 (:hits (variable/search {}))))
         (bootstrap/bulk-index-variables "PROV1")
         (index/wait-until-indexed)
