09d4fa7bb47f1cc52a30c2f5eb5d6d685f6c3e90
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-11-29 09:01:58 -0600

    Set version for 0.0.1 tag.

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
   (println (slurp "dev-resources/text/banner.txt"))
   (println (slurp "dev-resources/text/loading.txt")))
 
-(defproject gov.nasa.earthdata/cmr-dev-env-manager "0.1.0-SNAPSHOT"
+(defproject gov.nasa.earthdata/cmr-dev-env-manager "0.0.1"
   :description "An Alternate Development Environment Manager for the CMR"
   :url "https://github.com/cmr-exchange/dev-env-manager"
   :license {
