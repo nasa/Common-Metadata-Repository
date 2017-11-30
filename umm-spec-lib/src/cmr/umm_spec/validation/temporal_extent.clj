8e53db7a4eff59beeb8b1b754143e30cd8c0eea3
Lauren Frederick <lauren@element84.com>
2017-11-30 14:25:51 -0500

    CMR-4320 Move temporal extent validations to their own file

---
+++ umm-spec-lib/src/cmr/umm_spec/validation/temporal_extent.clj
@@ -0,0 +1,30 @@
+(ns cmr.umm-spec.validation.temporal-extent
+  "Defines validations for UMM temporal extents."
+  (:require
+   [clj-time.core :as time]
+   [cmr.common.validations.core :as validations-core]
+   [cmr.umm-spec.date-util :as date]
+   [cmr.umm-spec.validation.umm-spec-validation-utils :as util]))
+
+(defn- range-date-time-validation
+  "Defines range-date-time validation"
+  [field-path value]
+  (let [{:keys [BeginningDateTime EndingDateTime]} value]
+    (when (and BeginningDateTime EndingDateTime (time/after? BeginningDateTime EndingDateTime))
+      {field-path [(format "BeginningDateTime [%s] must be no later than EndingDateTime [%s]"
+                           (str BeginningDateTime) (str EndingDateTime))]})))
+
+(defn- temporal-end-date-in-past-validator
+  "Validate that the end date in TemporalExtents is in the past"
+  [field-path value]
+  (when (and value (not (date/is-in-past? value)))
+    {field-path [(str "Ending date should be in the past. Either set ending date to a date "
+                      "in the past or remove end date and set the ends at present flag to true.")]}))
+
+(def temporal-extent-warning-validation
+  {:RangeDateTimes (validations-core/every {:BeginningDateTime util/date-in-past-validator
+                                            :EndingDateTime temporal-end-date-in-past-validator})
+   :SingleDateTimes (validations-core/every util/date-in-past-validator)})
+
+(def temporal-extent-validation
+  {:RangeDateTimes (validations-core/every range-date-time-validation)})
