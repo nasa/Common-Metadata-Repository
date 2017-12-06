767cff438fe506ebeeef49c68545b5defab77a52
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-12-06 08:42:56 -0600

    Added messenger-oriented batch-subscribe function.

--- src/cmr/dev/env/manager/messaging/core.clj
+++ src/cmr/dev/env/manager/messaging/core.clj
@@ -18,3 +18,10 @@
   [msgr-type]
   (case msgr-type
     :pubsub (pubsub/new-messenger)))
+
+(defn batch-subscribe
+  "The passed argument `subscribers` is a list of maps with each map having
+  `:topic` and `:content` keys with corresponding values."
+  [messenger subscribers]
+  (for [subscriber subscribers]
+    (subscribe messenger (:topic subscriber) (:content subscriber))))
