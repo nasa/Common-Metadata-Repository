e4cbf71860c69c8740713aefac203769c249afbe
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-12-06 18:34:05 -0600

    Removed unused ns.

--- src/cmr/dev/env/manager/components/cmr/cubby.clj
+++
@@ -1,5 +0,0 @@
-(ns cmr.dev.env.manager.components.cmr.cubby
-  (:require
-    [cmr.dev.env.manager.config :as config]
-    [com.stuartsierra.component :as component]
-    [taoensso.timbre :as log]))
