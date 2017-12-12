489f65fe5d16521ca12fba147c8b83ba726af0b3
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-12-12 09:48:06 -0600

    Added health API support for process runners.

--- src/cmr/dev/env/manager/components/checks/health.clj
+++ src/cmr/dev/env/manager/components/checks/health.clj
@@ -1,8 +1,10 @@
 (ns cmr.dev.env.manager.components.checks.health
   (:require
-    [cmr.dev.env.manager.components.docker :as docker])
+    [cmr.dev.env.manager.components.docker :as docker]
+    [cmr.dev.env.manager.components.process :as process])
   (:import
-    (cmr.dev.env.manager.components.docker DockerRunner)))
+    (cmr.dev.env.manager.components.docker DockerRunner)
+    (cmr.dev.env.manager.components.process ProcessRunner)))
 
 (defprotocol Healthful
   (get-summary [this]
@@ -13,3 +15,7 @@
 (extend DockerRunner
         Healthful
         docker/healthful-behaviour)
+
+(extend ProcessRunner
+        Healthful
+        process/healthful-behaviour)
