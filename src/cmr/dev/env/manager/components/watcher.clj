b98a8587af183fb5eeab10eb1a0c42a621991028
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-12-12 11:46:21 -0600

    Filled in stubs for watcher component.

--- src/cmr/dev/env/manager/components/watcher.clj
+++ src/cmr/dev/env/manager/components/watcher.clj
@@ -1,3 +1,47 @@
 (ns cmr.dev.env.manager.components.watcher
   (:require
-    [cmr.dev.env.manager.watcher.core :as watcher])
+    [cmr.dev.env.manager.watcher.core :as watcher]
+    [taoensso.timbre :as log]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Watcher Component API   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; XXX TBD
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Lifecycle Implementation   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defrecord Watcher [])
+
+(defn start
+  [this]
+  (log/info "Starting watcher component ...")
+  (log/debug "Started watcher component.")
+  this)
+
+(defn stop
+  [this]
+  (log/info "Stopping watcher component ...")
+  (log/debug "Stopped watcher component.")
+  this)
+
+(def lifecycle-behaviour
+  {:start start
+   :stop stop})
+
+(extend Watcher
+  component/Lifecycle
+  lifecycle-behaviour)
+
+(defn create-component
+  ""
+  []
+  (->Watcher))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Health-check Implementation   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; XXX TBD
