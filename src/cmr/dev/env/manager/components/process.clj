d42a54251b153aeda356981610e94d87c67c08cf
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-12-11 19:43:41 -0600

    Updated process runner lifecycle to use new approach.

--- src/cmr/dev/env/manager/components/process.clj
+++ src/cmr/dev/env/manager/components/process.clj
@@ -9,6 +9,10 @@
     [me.raynes.conch.low-level :as shell]
     [taoensso.timbre :as log]))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Process Component API   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defn get-process
   [system service-key]
   (get-in system [service-key :process-data]))
@@ -46,47 +50,60 @@
     (shell/destroy process-data)
     @exit))
 
-(defrecord ProcessRunner [
-  builder
-  process-keyword
-  process-data]
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Lifecycle Implementation   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defrecord ProcessRunner
+  [builder
+   process-keyword
+   process-data])
+
+(defn start
+  [this]
+  (let [cfg ((:builder this) (:process-keyword this))
+        this (assoc this :config cfg)
+        process-key (:process-keyword this)
+        process-name (name process-key)]
+    (log/infof "Starting %s component ..." process-name)
+    (log/trace "Component keys:" (keys this))
+    (log/trace "Built configuration:" cfg)
+    (log/trace "Config:" (:config this))
+    (if (config/service-enabled? this process-key)
+      (let [process-data (spawn! this "lein" process-name)]
+        (log/debugf "Started %s component." process-name)
+        (assoc this :process-data process-data
+                    :enabled true))
+      (do
+        (log/debugf "Service %s not enabled; skipping component start-up."
+                    process-key)
+        (assoc this :enabled false)))))
+
+(defn stop
+  [this]
+  (let [process-key (:process-keyword this)
+        process-name (name process-key)]
+    (log/debug "Process name:" process-name)
+    (log/infof "Stopping %s component ..." process-name)
+    (if (config/service-enabled? this process-key)
+      (let [process-data (:process-data this)]
+        (log/trace "process-data:" process-data)
+        (log/trace "process:" (:process process-data))
+        (terminate! this (:process-data this))
+        (log/debugf "Stopped %s component." process-name)
+        (assoc this :process-data nil))
+      (do
+        (log/debugf "Service %s not enabled; skipping component shut-down."
+                    process-key)
+        this))))
+
+(def lifecycle-behaviour
+  {:start start
+   :stop stop})
+
+(extend ProcessRunner
   component/Lifecycle
-
-  (start [component]
-    (let [cfg (builder (:process-keyword component))
-          component (assoc component :config cfg)
-          process-key (:process-keyword component)
-          process-name (name process-key)]
-      (log/infof "Starting %s component ..." process-name)
-      (log/trace "Component keys:" (keys component))
-      (log/trace "Built configuration:" cfg)
-      (log/trace "Config:" (:config component))
-      (if (config/service-enabled? component process-key)
-        (let [process-data (spawn! component "lein" process-name)]
-          (log/debugf "Started %s component." process-name)
-          (assoc component :process-data process-data
-                           :enabled true))
-        (do
-          (log/debugf "Service %s not enabled; skipping component start-up."
-                      process-key)
-          (assoc component :enabled false)))))
-
-  (stop [component]
-    (let [process-key (:process-keyword component)
-          process-name (name process-key)]
-      (log/debug "Process name:" process-name)
-      (log/infof "Stopping %s component ..." process-name)
-      (if (config/service-enabled? component process-key)
-        (let [process-data (:process-data component)]
-          (log/trace "process-data:" process-data)
-          (log/trace "process:" (:process process-data))
-          (terminate! component (:process-data component))
-          (log/debugf "Stopped %s component." process-name)
-          (assoc component :process-data nil))
-        (do
-          (log/debugf "Service %s not enabled; skipping component shut-down."
-                      process-key)
-          component)))))
+  lifecycle-behaviour)
 
 (defn create-component
   ""
