91d98e4845d09b472ee358435198d23e6a1ad9c7
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-12-12 11:38:25 -0600

    Added more stubs.

--- src/cmr/dev/env/manager/watcher/core.clj
+++ src/cmr/dev/env/manager/watcher/core.clj
@@ -1,4 +1,13 @@
 (ns cmr.dev.env.manager.watcher.core
   (:require
     [cmr.dev.env.manager.watcher.impl.darwin :as darwin]
-    [cmr.dev.env.manager.watcher.impl.linux :as linux]))
+    [cmr.dev.env.manager.watcher.impl.hawk :as hawk]
+    [cmr.dev.env.manager.watcher.impl.linux :as linux]
+    [cmr.dev.env.manager.watcher.impl.nio :as nio]))
+
+(defprotocol Watcher
+  (add [this type coll])
+  (add-dir [this dir])
+  (add-dirs [this dirs])
+  (add-file [this file])
+  (add-files [this files]))
