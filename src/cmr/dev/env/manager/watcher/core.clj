0d55566218c0005f1551e678ff55dcf0c31e14c9
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-12-13 19:47:40 -0600

    Updated definition of protocol.
    Also pulled in implementation for use as API.

--- src/cmr/dev/env/manager/watcher/core.clj
+++ src/cmr/dev/env/manager/watcher/core.clj
@@ -3,11 +3,24 @@
     [cmr.dev.env.manager.watcher.impl.darwin :as darwin]
     [cmr.dev.env.manager.watcher.impl.hawk :as hawk]
     [cmr.dev.env.manager.watcher.impl.linux :as linux]
-    [cmr.dev.env.manager.watcher.impl.nio :as nio]))
+    [cmr.dev.env.manager.watcher.impl.nio :as nio])
+  (:import
+    (cmr.dev.env.manager.watcher.impl.hawk HawkWatcher)))
+
+(def default-watcher-type :hawk)
 
 (defprotocol Watcher
-  (add [this type coll])
-  (add-dir [this dir])
-  (add-dirs [this dirs])
-  (add-file [this file])
-  (add-files [this files]))
+  (add-path [this path])
+  (add-paths [this paths])
+  (handle-event [this event]))
+
+(extend HawkWatcher
+        Watcher
+        hawk/behaviour)
+
+(defn create-watcher
+  ([service-key messenger]
+    (create-watcher default-watcher-type service-key messenger))
+  ([type service-key messenger]
+    (case type
+      :hawk (hawk/create-watcher service-key messenger))))
