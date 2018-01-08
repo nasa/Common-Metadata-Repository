d2938a41b6921744252d009433d6078bf6c39915
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2018-01-08 13:47:08 -0600

    CMR-4537: Resolved jar file conflicts for search app.

--- search-app/project.clj
+++ search-app/project.clj
@@ -1,58 +1,70 @@
 (defproject nasa-cmr/cmr-search-app "0.1.0-SNAPSHOT"
   :description "Provides a public search API for concepts in the CMR."
   :url "https://github.com/nasa/Common-Metadata-Repository/tree/master/search-app"
-  :dependencies [[com.github.fge/json-schema-validator "2.2.6"]
-                 ;; XXX REMOVE the following deps when the stubbed
-                 ;;     responses are replaced with the real ones
-                 ;;     See https://bugs.earthdata.nasa.gov/browse/CMR-4583
-                 [gov.nasa.earthdata/cmr-client "0.2.0-SNAPSHOT"
-                  :exclusions [cljs-http
-                               clj-http
-                               com.google.code.findbugs/jsr305
-                               gov.nasa.earthdata/cmr-client
-                               instaparse
-                               org.clojure/clojurescript
-                               org.clojure/core.async
-                               org.clojure/data.xml
-                               org.clojure/tools.analyzer.jvm
-                               org.clojure/tools.reader
-                               org.clojure/java.jdbc
-                               ring/ring-codec]]
-                 [gov.nasa.earthdata/cmr-edsc-stubs "0.2.0-SNAPSHOT"
-                  :exclusions [cljs-http
-                               clj-http
-                               com.google.code.findbugs/jsr305
-                               gov.nasa.earthdata/cmr-client
-                               instaparse
-                               org.clojure/clojurescript
-                               org.clojure/core.async
-                               org.clojure/data.xml
-                               org.clojure/tools.analyzer.jvm
-                               org.clojure/tools.reader
-                               org.clojure/java.jdbc
-                               ring/ring-codec]]
-                 [nasa-cmr/cmr-collection-renderer-lib "0.1.0-SNAPSHOT"]
-                 [nasa-cmr/cmr-common-app-lib "0.1.0-SNAPSHOT"]
-                 [nasa-cmr/cmr-common-lib "0.1.1-SNAPSHOT"]
-                 [nasa-cmr/cmr-elastic-utils-lib "0.1.0-SNAPSHOT"]
-                 [nasa-cmr/cmr-metadata-db-app "0.1.0-SNAPSHOT"]
-                 [nasa-cmr/cmr-orbits-lib "0.1.0-SNAPSHOT"]
-                 [nasa-cmr/cmr-spatial-lib "0.1.0-SNAPSHOT"]
-                 [nasa-cmr/cmr-umm-lib "0.1.0-SNAPSHOT"]
-                 [nasa-cmr/cmr-umm-spec-lib "0.1.0-SNAPSHOT"]
-                 [net.sf.saxon/Saxon-HE "9.7.0-7"]
-                 [org.clojure/clojure "1.8.0"]
-                 [org.clojure/data.csv "0.1.3"]
-                 [org.clojure/tools.nrepl "0.2.12"]
-                 [ring/ring-core "1.5.0"]
-                 [ring/ring-json "0.4.0"]
-                 [selmer "1.10.7"]
-
-                 ;; Temporary inclusion of libraries needed for swagger UI until the dev portal is
-                 ;; done.
-                 [metosin/ring-swagger-ui "2.1.4-0"]
-                 [metosin/ring-swagger "0.22.9"]
-                 [prismatic/schema "1.1.3"]]
+  :exclusions [
+    [cheshire]
+    [clj-time]
+    [org.clojure/clojure]
+    [org.clojure/tools.reader]]
+  :dependencies [
+    [cheshire "5.8.0"]
+    [clj-time "0.14.2"]
+    [com.github.fge/json-schema-validator "2.2.6"]
+    ;; XXX REMOVE the following deps when the stubbed
+    ;;     responses are replaced with the real ones
+    ;;     See https://bugs.earthdata.nasa.gov/browse/CMR-4583
+    [clojusc/ltest "0.3.0-SNAPSHOT"]
+    [gov.nasa.earthdata/cmr-client "0.2.0-SNAPSHOT"
+     :exclusions [cljs-http
+                  clj-http
+                  clojusc/ltest
+                  com.google.code.findbugs/jsr305
+                  gov.nasa.earthdata/cmr-client
+                  instaparse
+                  org.clojure/clojurescript
+                  org.clojure/core.async
+                  org.clojure/data.xml
+                  org.clojure/tools.analyzer.jvm
+                  org.clojure/tools.reader
+                  org.clojure/java.jdbc
+                  ring/ring-codec]]
+    [gov.nasa.earthdata/cmr-edsc-stubs "0.2.0-SNAPSHOT"
+     :exclusions [cljs-http
+                  clj-http
+                  clojusc/ltest
+                  com.google.code.findbugs/jsr305
+                  gov.nasa.earthdata/cmr-client
+                  instaparse
+                  org.clojure/clojurescript
+                  org.clojure/core.async
+                  org.clojure/data.xml
+                  org.clojure/tools.analyzer.jvm
+                  org.clojure/tools.reader
+                  org.clojure/java.jdbc
+                  ring/ring-codec]]
+    ;; XXX end REMOVE
+    [nasa-cmr/cmr-collection-renderer-lib "0.1.0-SNAPSHOT"]
+    [nasa-cmr/cmr-common-app-lib "0.1.0-SNAPSHOT"]
+    [nasa-cmr/cmr-common-lib "0.1.1-SNAPSHOT"]
+    [nasa-cmr/cmr-elastic-utils-lib "0.1.0-SNAPSHOT"]
+    [nasa-cmr/cmr-metadata-db-app "0.1.0-SNAPSHOT"]
+    [nasa-cmr/cmr-orbits-lib "0.1.0-SNAPSHOT"]
+    [nasa-cmr/cmr-spatial-lib "0.1.0-SNAPSHOT"]
+    [nasa-cmr/cmr-umm-lib "0.1.0-SNAPSHOT"]
+    [nasa-cmr/cmr-umm-spec-lib "0.1.0-SNAPSHOT"]
+    [net.sf.saxon/Saxon-HE "9.7.0-7"]
+    [org.clojure/clojure "1.8.0"]
+    [org.clojure/data.csv "0.1.3"]
+    [org.clojure/tools.nrepl "0.2.12"]
+    [org.clojure/tools.reader "1.1.1"]
+    [ring/ring-core "1.5.0"]
+    [ring/ring-json "0.4.0"]
+    [selmer "1.10.7"]
+    ;; Temporary inclusion of libraries needed for swagger UI until the dev portal is
+    ;; done.
+    [metosin/ring-swagger-ui "2.1.4-0"]
+    [metosin/ring-swagger "0.22.9"]
+    [prismatic/schema "1.1.3"]]
 
   :plugins [[test2junit "1.2.1"]
             [lein-exec "0.3.4"]]
