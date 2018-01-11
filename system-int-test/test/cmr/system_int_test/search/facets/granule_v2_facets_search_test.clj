5b994dde2a1882ad60418b45333840df10fbd2e3
Chris Durbin <christopher.d.durbin@nasa.gov>
2018-01-11 10:04:47 -0500

    CMR-4681: Add in validation that only a single collection concept ID is targeted by a granule v2 facets query

--- system-int-test/test/cmr/system_int_test/search/facets/granule_v2_facets_search_test.clj
+++ system-int-test/test/cmr/system_int_test/search/facets/granule_v2_facets_search_test.clj
@@ -1,4 +1,4 @@
-(ns cmr.system-int-test.search.facets.granule-facets-v2-search-test
+(ns cmr.system-int-test.search.facets.granule-v2-facets-search-test
   "This tests retrieving v2 facets when searching for granules."
   (:require
    [clojure.test :refer :all]
@@ -9,16 +9,14 @@
 
 (defn- search-and-return-v2-facets
   "Returns only the facets from a v2 granule facets search request."
-  ([]
-   (search-and-return-v2-facets {}))
-  ([search-params]
-   (index/wait-until-indexed)
-   (let [query-params (merge search-params {:page-size 0 :include-facets "v2"})]
-     (get-in (search/find-concepts-json :granule query-params) [:results :facets]))))
+  [search-params]
+  (index/wait-until-indexed)
+  (let [query-params (merge search-params {:page-size 0 :include-facets "v2"})]
+    (get-in (search/find-concepts-json :granule query-params) [:results :facets])))
 
 (deftest granule-facet-tests
   (testing "Granule facets are returned when requesting V2 facets in JSON format."
-    (let [facets (search-and-return-v2-facets)]
+    (let [facets (search-and-return-v2-facets {:collection-concept-id "C1-PROV1"})]
       (is (= "Browse Granules" (:title facets)))))
 
   (testing "Only the json format supports V2 granule facets."
@@ -40,7 +38,8 @@
 
   (testing "The value for include_facets must be v2 case insensitive."
     (testing "Case insensitive"
-      (let [facets (get-in (search/find-concepts-json :granule {:include-facets "V2"})
+      (let [facets (get-in (search/find-concepts-json :granule {:include-facets "V2"
+                                                                :collection-concept-id "C1-PROV1"})
                            [:results :facets])]
         (is (= "Browse Granules" (:title facets)))))
     (testing "Invalid values"
