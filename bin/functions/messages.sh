1640ebb7fecb8538a044200bf99bfebd4cbefbc6
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2018-01-24 18:43:52 -0600

    CMR-4718: Created CMR CLI script.

---
+++ bin/functions/messages.sh
@@ -0,0 +1,7 @@
+function cmd_not_found {
+    echo "Command not defined; for help message use 'cmr help'"
+}
+
+function subcmd_not_found {
+    echo "Subcommand not defined; for help message use 'cmr $@ help'"
+}
