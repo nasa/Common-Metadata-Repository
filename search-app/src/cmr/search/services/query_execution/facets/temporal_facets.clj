a4c2b9acbdf7a3289946a8986bc97f28c4326b5d
Chris Durbin <christopher.d.durbin@nasa.gov>
2018-02-13 14:53:39 -0500

    CMR-4686: Add months to granule temporal facets.

--- search-app/src/cmr/search/services/query_execution/facets/temporal_facets.clj
+++ search-app/src/cmr/search/services/query_execution/facets/temporal_facets.clj
@@ -23,10 +23,36 @@
   [datetime interval]
   (second (re-find #"^\d{4}-\d{2}-\d{2}T(\d{2}):\d{2}:\d{2}\+\d+$" datetime)))
 
+(defn- time-intervals->next-interval
+  "Returns the next interval based on the values in the map."
+  [time-intervals]
+  ; (println "My time intervals are" time-intervals)
+  (if-not (contains? (set time-intervals) :year)
+    :year
+    :month))
+
+(defn- get-subfields
+  "Returns the set of subfields within all of the provided temporal_facet params."
+  [temporal-facet-params]
+  ; (println "temporal-facet-params" temporal-facet-params)
+  (let [field-regex (re-pattern "temporal_facet\\[\\d+\\]\\[(.*)\\]")]
+    (keep #(second (re-matches field-regex %)) temporal-facet-params)))
+
+(defn query-params->time-interval
+  "Returns the time interval to request in temporal facets based on the query parameters."
+  [query-params]
+  (let [field-reg-ex (re-pattern "temporal_facet.*")
+        temporal-facet-params (keep (fn [[k v]]
+                                      (when (re-matches field-reg-ex k) k))
+                                    query-params)]
+    ; (println "Subfields are:" (get-subfields temporal-facet-params))
+    (time-intervals->next-interval (map keyword (get-subfields temporal-facet-params)))))
+
 (defn temporal-facet
   "Creates a temporal facet for the provided field."
-  [interval-granularity]
-  (let [interval-granularity :year]
+  [query-params]
+  (let [interval-granularity (query-params->time-interval query-params)]
+    ; (println "Interval granularity is:" interval-granularity)
     {:date_histogram
      {:field :start-date-doc-values
       :interval interval-granularity}}))
