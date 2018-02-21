570e29f5b1f2bea39f4e54bb4cdca22fd366d17d
Duncan McGreggor <oubiwann@gmail.com>
2018-02-21 13:58:21 -0600

    CMR-4657: Use CMR formatting standard for namespaces.

--- indexer-app/test/cmr/indexer/test/data/concepts/collection/science_keyword.clj
+++ indexer-app/test/cmr/indexer/test/data/concepts/collection/science_keyword.clj
@@ -1,7 +1,8 @@
 (ns cmr.indexer.test.data.concepts.collection.science-keyword
   "Tests for science keywords"
-  (:require [clojure.test :refer :all]
-            [cmr.indexer.data.concepts.collection.science-keyword :as sk]))
+  (:require
+   [clojure.test :refer :all]
+   [cmr.indexer.data.concepts.collection.science-keyword :as sk]))
 
 (deftest flatten-science-keywords-test
   (let [science-keyword1 {:Category "category"
@@ -18,4 +19,4 @@
         actual-keywords (sk/flatten-science-keywords collection)
         expected-keywords #{"category" "topic" "term" "another-term" "another-topic"}]
     (is (= (count expected-keywords) (count actual-keywords)))
-    (is (=  expected-keywords (set actual-keywords)))))
+    (is (= expected-keywords (set actual-keywords)))))
