bb9f6b2d161dd947acde40920e10d704b2ae7261
Duncan McGreggor <oubiwann@gmail.com>
2018-02-22 13:56:32 -0600

    CMR-4657: Added missing int tests for variables.

--- indexer-app/int-test/cmr/indexer/test/services/index_fields/collections.clj
+++ indexer-app/int-test/cmr/indexer/test/services/index_fields/collections.clj
@@ -30,28 +30,29 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn- save-collection
-  ([context es-doc concept-id revision-id]
-   (save-collection context es-doc concept-id revision-id {}))
-  ([context es-doc concept-id revision-id options]
+  "Save a collection Elasticsearch document."
+  ([es-doc concept-id revision-id]
+   (save-collection es-doc concept-id revision-id {}))
+  ([es-doc concept-id revision-id options]
    (save-document-in-elastic
-    context "collection" es-doc concept-id revision-id options)))
+    "collection" es-doc concept-id revision-id options)))
 
 (defn- delete-collection
-  ([context concept-id revision-id]
-   (delete-collection context concept-id revision-id {}))
-  ([context concept-id revision-id options]
-   (delete-document-in-elastic
-    context "collection" concept-id revision-id options)))
+  "Delete a collection Elasticsearch document."
+  ([concept-id revision-id]
+   (delete-collection concept-id revision-id {}))
+  ([concept-id revision-id options]
+   (delete-document-in-elastic "collection" concept-id revision-id options)))
 
 (defn- assert-version-coll
   "Assert the retrieved document for the given id is of the given version"
-  [id version]
-  (assert-version "collection" id version))
+  [concept-id version]
+  (assert-same "collection" concept-id :_version version))
 
 (defn- assert-delete-coll
-  "Assert the document with the given id is deleted"
-  [id]
-  (assert-delete "collection" id))
+  "Assert the document with the given concept-id is deleted"
+  [concept-id]
+  (assert-delete "collection" concept-id))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;   Tests   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -59,61 +60,61 @@
 
 (deftest save-collection-with-increment-versions-test
   (testing "Save with increment versions"
-    (save-collection @context (es-doc-coll) "C1234-PROV1" 1)
+    (save-collection (es-doc-coll) "C1234-PROV1" 1)
     (assert-version-coll "C1234-PROV1" 1)
-    (save-collection @context (es-doc-coll) "C1234-PROV1" 2)
+    (save-collection (es-doc-coll) "C1234-PROV1" 2)
     (assert-version-coll "C1234-PROV1" 2)
-    (save-collection @context (es-doc-coll) "C1234-PROV1" 10)
+    (save-collection (es-doc-coll) "C1234-PROV1" 10)
     (assert-version-coll "C1234-PROV1" 10)))
 
 (deftest save-collection-with-equal-versions-test
   (testing "Save with equal versions"
-    (save-collection @context (es-doc-coll) "C1234-PROV1" 1)
+    (save-collection (es-doc-coll) "C1234-PROV1" 1)
     (assert-version-coll "C1234-PROV1" 1)
-    (save-collection @context (es-doc-coll) "C1234-PROV1" 1)
+    (save-collection (es-doc-coll) "C1234-PROV1" 1)
     (assert-version-coll "C1234-PROV1" 1)))
 
 (deftest save-collection-with-earlier-versions-test
   (testing "Save with earlier versions with ignore-conflict false"
-    (save-collection @context (es-doc-coll) "C1234-PROV1" 3)
+    (save-collection (es-doc-coll) "C1234-PROV1" 3)
     (assert-version-coll "C1234-PROV1" 3)
     (try
-      (save-collection @context (es-doc-coll) "C1234-PROV1" 2)
+      (save-collection (es-doc-coll) "C1234-PROV1" 2)
       (catch clojure.lang.ExceptionInfo e
         (let [type (:type (ex-data e))
               err-msg (first (:errors (ex-data e)))]
           (is (= :conflict type))
           (is (re-find #"version conflict, current \[3\], provided \[2\]" err-msg))))))
   (testing "Save with earlier versions with ignore-conflict true"
-    (save-collection @context (es-doc-coll) "C1234-PROV1" 3
+    (save-collection (es-doc-coll) "C1234-PROV1" 3
                               {:ignore-conflict? true})
     (assert-version-coll "C1234-PROV1" 3)
-    (save-collection @context (es-doc-coll) "C1234-PROV1" 2
+    (save-collection (es-doc-coll) "C1234-PROV1" 2
                               {:ignore-conflict? true})
     (assert-version-coll "C1234-PROV1" 3)))
 
 (deftest delete-collection-with-increment-versions-test
   (testing "Delete with increment versions"
-    (save-collection @context (es-doc-coll) "C1234-PROV1" 1)
-    (delete-collection @context "C1234-PROV1" "2")
+    (save-collection (es-doc-coll) "C1234-PROV1" 1)
+    (delete-collection "C1234-PROV1" "2")
     (assert-delete-coll "C1234-PROV1")
-    (delete-collection @context "C1234-PROV1" "8")
+    (delete-collection "C1234-PROV1" "8")
     (assert-delete-coll "C1234-PROV1")))
 
 (deftest delete-collection-with-equal-versions-test
   (testing "Delete with equal versions"
-    (save-collection @context (es-doc-coll) "C1234-PROV1" 1)
-    (delete-collection @context "C1234-PROV1" "1")
+    (save-collection (es-doc-coll) "C1234-PROV1" 1)
+    (delete-collection "C1234-PROV1" "1")
     (assert-delete-coll "C1234-PROV1")))
 
 (deftest delete-collection-with-earlier-versions-test
   (testing "Delete with earlier versions ignore-conflict false"
-    (save-collection @context (es-doc-coll) "C1234-PROV1" 2)
+    (save-collection (es-doc-coll) "C1234-PROV1" 2)
     (try
-      (delete-collection @context "C1234-PROV1" "1")
+      (delete-collection "C1234-PROV1" "1")
       (catch java.lang.Exception e
         (is (re-find #"version conflict, current \[2\], provided \[1\]" (.getMessage e))))))
   (testing "Delete with earlier versions ignore-conflict true"
-    (save-collection @context (es-doc-coll) "C1234-PROV1" 2 {:ignore-conflict? true})
-    (delete-collection @context "C1234-PROV1" "1" {:ignore-conflict? true})
+    (save-collection (es-doc-coll) "C1234-PROV1" 2 {:ignore-conflict? true})
+    (delete-collection "C1234-PROV1" "1" {:ignore-conflict? true})
     (assert-version-coll "C1234-PROV1" 2)))
