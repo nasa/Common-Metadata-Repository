fb1352feaddcfd1ba5cab87beda915443131cfd8
Duncan McGreggor <oubiwann@gmail.com>
2018-02-26 15:56:35 -0600

    CMR-4657: Cleaned up unrelated tests.

--- collection-renderer-lib/test/cmr/collection_renderer/test/services/collection_renderer.clj
+++ collection-renderer-lib/test/cmr/collection_renderer/test/services/collection_renderer.clj
@@ -1,19 +1,21 @@
 (ns cmr.collection-renderer.test.services.collection-renderer
-  (require [clojure.test :refer :all]
-           [cmr.common.test.test-check-ext :as ext :refer [defspec]]
-           [clojure.test.check.generators :as gen]
-           [com.gfredericks.test.chuck.clojure-test :refer [for-all]]
-           [clojure.string :as str]
-           [cmr.common.lifecycle :as l]
-           [cmr.collection-renderer.services.collection-renderer :as cr]
-           [cmr.umm-spec.test.expected-conversion :as expected-conversion]
-           [cmr.umm-spec.test.umm-generators :as umm-gen]))
+  (require
+   [clojure.string :as string]
+   [clojure.test :refer :all]
+   [clojure.test.check.generators :as gen]
+   [cmr.collection-renderer.services.collection-renderer :as cr]
+   [cmr.common.lifecycle :as l]
+   [cmr.common.test.test-check-ext :as ext :refer [defspec]]
+   [cmr.umm-spec.test.expected-conversion :as expected-conversion]
+   [cmr.umm-spec.test.umm-generators :as umm-gen]
+   [com.gfredericks.test.chuck.clojure-test :refer [for-all]]))
 
 (defn create-renderer-context
   "Returns a context to use with testing collection"
   []
   {:system (l/start {cr/system-key (cr/create-collection-renderer)
-                     :public-conf {:relative-root-url "/search" :edsc-url "https://search.earthdata.nasa.gov"}}
+                     :public-conf {:relative-root-url "/search"
+                                   :edsc-url "https://search.earthdata.nasa.gov"}}
                     nil)})
 
 (def renderer-context
@@ -21,28 +23,29 @@
   (memoize create-renderer-context))
 
 (deftest render-default-collection
-  (let [coll expected-conversion/example-collection-record
-        ^String html (cr/render-collection (renderer-context) coll "C1234-PROV1")]
-    (is html "Expected some HTML to be returned")
-    (is (.contains html (:EntryTitle coll)))))
+  (testing "Get returned HTML"
+    (let [coll expected-conversion/example-collection-record
+          ^String html (cr/render-collection (renderer-context) coll "C1234-PROV1")]
+      (is (string/includes? html (:EntryTitle coll))))))
 
 ;; This checks that we can render any UMM collection without getting an exception.
 ;; A small number of tries is done to avoid making tests take a long time.
 (defspec render-any-umm-collection 10
-  (for-all [umm-record (gen/no-shrink umm-gen/umm-c-generator)]
-    (let [^String html (cr/render-collection (renderer-context) umm-record "C1234-PROV1")]
-      (and html (.contains html (:EntryTitle umm-record))))))
+  (testing "Render without exception"
+    (for-all [umm-record (gen/no-shrink umm-gen/umm-c-generator)]
+      (let [^String html (cr/render-collection (renderer-context) umm-record "C1234-PROV1")]
+        (and html (string/includes? html (:EntryTitle umm-record)))))))
 
-;; Verify that the title of the html is the entry title
 (deftest render-title-as-entry-title
-  (let [coll expected-conversion/example-collection-record
-        ^String html (cr/render-collection (renderer-context) coll "C1234-PROV1")]
-    (is html "Expected a title containing the entry title")
-    (is (.contains html (str "<title>" (:EntryTitle coll) "</title>")))))
+  (testing "Verify that the title of the html is the entry title"
+    (let [coll expected-conversion/example-collection-record
+          ^String html (cr/render-collection (renderer-context) coll "C1234-PROV1")]
+      (is (string/includes? html (str "<title>" (:EntryTitle coll) "</title>"))))))
 
-;; Verify that the correct EDSC url is rendered
 (deftest render-edsc-link
-  (let [coll expected-conversion/example-collection-record
-        ^String html (cr/render-collection (renderer-context) coll "C1234-PROV1")]
-    (is html "Expected the correct edsc url to be rendered")
-    (is (.contains html (str "https://search.earthdata.nasa.gov/search/granules?p=C1234-PROV1")))))
+  (testing "Verify that the correct EDSC url is rendered"
+    (let [coll expected-conversion/example-collection-record
+          ^String html (cr/render-collection (renderer-context) coll "C1234-PROV1")]
+      (is (string/includes?
+           html
+           "https://search.earthdata.nasa.gov/search/granules?p=C1234-PROV1")))))
