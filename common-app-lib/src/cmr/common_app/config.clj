df109cc50a004bd37beabdbedb100122e08338f3
Daniel Zamora <daniel.p.zamora@nasa.gov>
2018-03-01 10:34:40 -0500

    CMR-4756 pr comments

--- common-app-lib/src/cmr/common_app/config.clj
+++ common-app-lib/src/cmr/common_app/config.clj
@@ -2,7 +2,7 @@
   "A namespace that allows for global configuration. Configuration can be provided at runtime or
   through an environment variable. Configuration items should be added using the defconfig macro."
   (:require
-    [cmr.common.config :refer [defconfig]]))
+   [cmr.common.config :refer [defconfig]]))
 
 (defconfig cwic-tag
   "has-granules-or-cwic should also return any collection with configured cwic-tag"
