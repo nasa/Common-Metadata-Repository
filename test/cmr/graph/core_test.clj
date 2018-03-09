da1bd895680e394fffc963033646b4b111012b19
Duncan McGreggor <oubiwann@gmail.com>
2018-03-09 16:01:31 -0600

    Fixed old ns.

--- test/cmr/graph/core_test.clj
+++ test/cmr/graph/core_test.clj
@@ -1,6 +1,7 @@
 (ns cmr.graph.core-test
-  (:require [clojure.test :refer :all]
-            [cmr-graph.core :refer :all]))
+  (:require
+   [clojure.test :refer :all]
+   [cmr.graph.core :as core]))
 
 (deftest a-test
-  (is (= 1 1))))
+  (is (= 1 1)))
