73aae1bb55de775fd6347871ddec315098893b25
Daniel Zamora <daniel.p.zamora@nasa.gov>
2018-03-12 14:10:32 -0400

    CMR-4653 changes ingest default umm version back

--- ingest-app/src/cmr/ingest/config.clj
+++ ingest-app/src/cmr/ingest/config.clj
@@ -18,7 +18,7 @@
 (defconfig collection-umm-version
   "Defines the latest collection umm version accepted by ingest - it's the latest official version.
    This environment variable needs to be manually set when newer UMM version becomes official"
-  {:default "1.10"})
+  {:default "1.9"})
 
 (defconfig variable-umm-version
   "Defines the latest variable umm version accepted by ingest - it's the latest official version.
