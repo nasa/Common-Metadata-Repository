4b8c135676bc3684cefe6e367177ff4ed55097a9
Chris Durbin <christopher.d.durbin@nasa.gov>
2018-03-21 12:06:43 -0400

    Remove code and CSV files for types that weren't actually needed.

--- README.md
+++ README.md
@@ -99,7 +99,6 @@ At this point, your instance of Neo4j should have the demo movie data.
 2. `CREATE CONSTRAINT ON (urlType:UrlType) ASSERT urlType.name IS UNIQUE`
 3. `CREATE CONSTRAINT ON (coll:Collection) ASSERT coll.md5Leo IS UNIQUE`
 4. `CREATE CONSTRAINT ON (dataCenter:DataCenter) ASSERT dataCenter.name IS UNIQUE`
-
 5. ```cypher
 LOAD CSV WITH HEADERS FROM "https://raw.githubusercontent.com/cmr-exchange/cmr-graph/master/resources/data/collections.csv" AS csvLine
 MERGE (format:MetadataFormat {name: csvLine.MetadataFormat})
@@ -110,7 +109,6 @@ CREATE (coll)-[:OWNED_BY]->(provider)
 CREATE (coll)-[:FORMATTED_IN]->(format)
 CREATE (coll)-[:VERSION_IS]->(version)
 ```
-
 6. ```cypher
 USING PERIODIC COMMIT 500
 LOAD CSV WITH HEADERS FROM "https://raw.githubusercontent.com/cmr-exchange/cmr-graph/master/resources/data/collection_and_urls.csv" AS csvLine
@@ -120,7 +118,6 @@ MERGE (urlType:UrlType { name: csvLine.URLType})
 CREATE (coll)-[:LINKS_TO]->(url)
 CREATE (url)-[:HAS_TYPE]->(urlType)
 ```
-
 7. ```cypher
 USING PERIODIC COMMIT 500
 LOAD CSV WITH HEADERS FROM "https://raw.githubusercontent.com/cmr-exchange/cmr-graph/master/resources/data/collection_and_data_centers.csv" AS csvLine
--- src/cmr/graph/data/import.clj
+++ src/cmr/graph/data/import.clj
@@ -16,24 +16,6 @@
 (def collection-csv-file
   "data/collections.csv")
 
-(def url-csv-file
-  "data/urls.csv")
-
-(def url-types-csv-file
-  "data/url_types.csv")
-
-(def providers-csv-file
-  "data/providers.csv")
-
-(def formats-csv-file
-  "data/formats.csv")
-
-(def data-centers-csv-file
-  "data/data_centers.csv")
-
-(def version-ids-csv-file
-  "data/version_ids.csv")
-
 (def collection-url-csv-file
   "data/collection_and_urls.csv")
 
@@ -92,70 +74,6 @@
     (csv/write-csv csv-file [collection-columns])
     (csv/write-csv csv-file (mapv collection->row collections))))
 
-(defn write-url-csv
-  "Creates the URL csv file"
-  [collections output-filename]
-  (let [unique-urls (->> (mapcat :related-urls collections)
-                         (keep :url)
-                         set)]
-    (with-open [csv-file (io/writer output-filename)]
-      (csv/write-csv csv-file [["MD5Leo" "Url"]])
-      (csv/write-csv csv-file (mapv (fn [url]
-                                      [(md5-leo url) url])
-                                    unique-urls)))))
-
-(defn write-url-type-csv
-  "Creates the URL types csv file"
-  [collections output-filename]
-  (let [unique-url-types (->> (mapcat :related-urls collections)
-                              (keep :type)
-                              set)]
-    (with-open [csv-file (io/writer output-filename)]
-      (csv/write-csv csv-file [["MD5Leo" "UrlType"]])
-      (csv/write-csv csv-file (mapv (fn [url-type]
-                                      [(md5-leo url-type) url-type])
-                                    unique-url-types)))))
-
-(defn write-providers-csv
-  "Creates the providers csv file"
-  [collections output-filename]
-  (let [providers (set (mapcat :provider-id collections))]
-    (with-open [csv-file (io/writer output-filename)]
-      (csv/write-csv csv-file [["MD5Leo" "ProviderId"]])
-      (csv/write-csv csv-file (mapv (fn [provider]
-                                      [(md5-leo provider) provider])
-                                    providers)))))
-
-(defn write-formats-csv
-  "Creates the metadata formats csv file"
-  [collections output-filename]
-  (let [metadata-formats (set (mapcat :metadata-format collections))]
-    (with-open [csv-file (io/writer output-filename)]
-      (csv/write-csv csv-file [["MD5Leo" "MetadataFormat"]])
-      (csv/write-csv csv-file (mapv (fn [metadata-format]
-                                      [(md5-leo metadata-format) metadata-format])
-                                    metadata-formats)))))
-
-(defn write-data-centers-csv
-  "Creates the Data Centers csv file"
-  [collections output-filename]
-  (let [data-centers (set (mapcat :data-center collections))]
-    (with-open [csv-file (io/writer output-filename)]
-      (csv/write-csv csv-file [["MD5Leo" "DataCenter"]])
-      (csv/write-csv csv-file (mapv (fn [data-center]
-                                      [(md5-leo data-center) data-center])
-                                    data-centers)))))
-
-(defn write-version-ids-csv
-  "Creates the version IDs csv file"
-  [collections output-filename]
-  (let [version-ids (set (mapcat :version-id collections))]
-    (with-open [csv-file (io/writer output-filename)]
-      (csv/write-csv csv-file [["MD5Leo" "VersionId"]])
-      (csv/write-csv csv-file (mapv (fn [version-id]
-                                      [(md5-leo version-id) version-id])
-                                    version-ids)))))
-
 (defn construct-collection-url-row
   "Creates a collection URL row for a relationship CSV file."
   [collection url]
@@ -200,22 +118,6 @@
 
  (write-collection-csv (mapv prepare-collection-for-import (:hits (:hits (read-json-file json-collections-filename))))
                        (str "resources/" collection-csv-file))
-
- (write-url-csv (mapv prepare-collection-for-import (:hits (:hits (read-json-file json-collections-filename))))
-                (str "resources/" url-csv-file))
-
- (write-url-type-csv (mapv prepare-collection-for-import (:hits (:hits (read-json-file json-collections-filename))))
-                     (str "resources/" url-types-csv-file))
- (write-providers-csv (mapv prepare-collection-for-import (:hits (:hits (read-json-file json-collections-filename))))
-                      (str "resources/" providers-csv-file))
-
- (write-formats-csv (mapv prepare-collection-for-import (:hits (:hits (read-json-file json-collections-filename))))
-                    (str "resources/" formats-csv-file))
- (write-data-centers-csv (mapv prepare-collection-for-import (:hits (:hits (read-json-file json-collections-filename))))
-                         (str "resources/" data-centers-csv-file))
-
- (write-version-ids-csv (mapv prepare-collection-for-import (:hits (:hits (read-json-file json-collections-filename))))
-                        (str "resources/" version-ids-csv-file))
 
  (write-collection-url-relationship-csv (mapv prepare-collection-for-import (:hits (:hits (read-json-file json-collections-filename))))
                                         (str "resources/" collection-url-csv-file))
