3e976e4119d4d32692024d71d741738f5b0b7374
Chris Durbin <christopher.d.durbin@nasa.gov>
2018-03-21 16:02:38 -0400

    Add a route to import all of the collection data.

--- src/cmr/graph/data/import.clj
+++ src/cmr/graph/data/import.clj
@@ -2,8 +2,9 @@
   "Functions for importing data into neo4j."
   (:require
    [cheshire.core :as json]
-   [clojure.java.io :as io]
    [clojure.data.csv :as csv]
+   [clojure.java.io :as io]
+   [clojurewerkz.neocons.rest.cypher :as cypher]
    [cmr.graph.data.statement :as statement]
    [cmr.graph.data.tags :as tags]
    [digest :as digest]))
@@ -138,6 +139,47 @@
     (with-open [csv-file (io/writer output-filename)]
       (csv/write-csv csv-file [["CollectionMD5Leo" "TagKey"]])
       (csv/write-csv csv-file rows))))
+
+(def import-statements
+  "All of the import statements to run to populate a completely empty database. Make sure to
+  delete everything before running."
+  ["CREATE CONSTRAINT ON (url:Url) ASSERT url.name IS UNIQUE"
+   "CREATE CONSTRAINT ON (urlType:UrlType) ASSERT urlType.name IS UNIQUE"
+   "CREATE CONSTRAINT ON (coll:Collection) ASSERT coll.md5Leo IS UNIQUE"
+   "CREATE CONSTRAINT ON (dataCenter:DataCenter) ASSERT dataCenter.name IS UNIQUE"
+   "CREATE CONSTRAINT ON (tag:Tag) ASSERT tag.name IS UNIQUE"
+   "LOAD CSV WITH HEADERS FROM \"https://raw.githubusercontent.com/cmr-exchange/cmr-graph/master/resources/data/collections.csv\" AS csvLine
+      MERGE (format:MetadataFormat {name: csvLine.MetadataFormat})
+      MERGE (version:Version {name: csvLine.VersionId})
+      MERGE (provider:Provider {name: csvLine.ProviderId})
+      CREATE (coll:Collection {md5Leo: csvLine.MD5Leo, conceptId: csvLine.ConceptId})
+      CREATE (coll)-[:OWNED_BY]->(provider)
+      CREATE (coll)-[:FORMATTED_IN]->(format)
+      CREATE (coll)-[:VERSION_IS]->(version)"
+   "USING PERIODIC COMMIT 500
+      LOAD CSV WITH HEADERS FROM \"https://raw.githubusercontent.com/cmr-exchange/cmr-graph/master/resources/data/collection_and_urls.csv\" AS csvLine
+      MATCH (coll:Collection { md5Leo: csvLine.CollectionMD5Leo})
+      MERGE (url:Url { name: csvLine.URL})
+      MERGE (urlType:UrlType { name: csvLine.URLType})
+      CREATE (coll)-[:LINKS_TO]->(url)
+      CREATE (url)-[:HAS_TYPE]->(urlType)"
+   "USING PERIODIC COMMIT 500
+      LOAD CSV WITH HEADERS FROM \"https://raw.githubusercontent.com/cmr-exchange/cmr-graph/master/resources/data/collection_and_data_centers.csv\" AS csvLine
+      MATCH (coll:Collection { md5Leo: csvLine.CollectionMD5Leo})
+      MERGE (dataCenter:DataCenter { name: csvLine.DataCenter})
+      CREATE (coll)-[:AFFILIATED_WITH]->(dataCenter)"
+   "USING PERIODIC COMMIT 500
+      LOAD CSV WITH HEADERS FROM \"https://raw.githubusercontent.com/cmr-exchange/cmr-graph/master/resources/data/collection_and_tags.csv\" AS csvLine
+      MATCH (coll:Collection { md5Leo: csvLine.CollectionMD5Leo})
+      MERGE (tag:Tag { name: csvLine.TagKey})
+      CREATE (coll)-[:TAGGED_WITH]->(tag)"])
+
+(defn import-all-data
+  "Imports all of the collection data."
+  [conn]
+  (doseq [statement import-statements]
+    (cypher/tquery conn statement)))
+
 
 (comment
  (prepare-collection-for-import (first (:hits (:hits (read-json-file json-collections-filename)))))
