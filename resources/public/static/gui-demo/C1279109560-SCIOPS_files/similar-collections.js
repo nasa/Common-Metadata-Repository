b7a146a468dd5df7c70bd9bef6a98f42c62f676b
mschmele <mschmele@gmail.com>
2018-03-22 14:13:17 -0400

    BONUS: no more yucky setTimeouts

--- resources/public/static/gui-demo/C1279109560-SCIOPS_files/similar-collections.js
+++ resources/public/static/gui-demo/C1279109560-SCIOPS_files/similar-collections.js
@@ -3,41 +3,38 @@ similar_collections = {
     collectionId: window.location.pathname.split('/').pop().split('.')[0],
     relatedCollections: [],
 
-    getConcepts: async function (concept) {
+    getConcepts: function (concept) {
         var graph_client = cmr.client.graph.create_client({"return-body?": true})
 
         var graph_channel = cmr.client.graph.get_collection_url_relation(
             graph_client, similar_collections.collectionId)
         cmr.client.common.util.with_callback(graph_channel, async function(data) {
-            var conceptMetadata = await data.map(async function(conceptId) {
-                $.get({url: similar_collections.CMR + conceptId,
-                    beforeSend: function(xhr){xhr.setRequestHeader('Accept', 'application/vnd.nasa.cmr.umm_results+json');}},
-                    function(data) {
-                        var meta = data.items[0].meta
-                        var umm = data.items[0].umm
+            var conceptMetadata = data.map(async function(conceptId) {
+                var data = fetch(similar_collections.CMR + conceptId, {headers: {'Accept': 'application/vnd.nasa.cmr.umm_results+json'}})
+                                .then((response) => response.json())
 
-                        similar_collections.relatedCollections.push({
-                            collectionId: meta["concept-id"],
-                            provider: meta["provider-id"],
-                            shortName: umm["ShortName"],
-                            collectionUrl: "https://cmr.earthdata.nasa.gov/search/concepts/" + conceptId + "/" + meta["revision-id"]
-                        })
-                    })
+                var meta = data.items[0].meta
+                var umm = data.items[0].umm
+                return {
+                    collectionId: meta["concept-id"],
+                    provider: meta["provider-id"],
+                    shortName: umm["ShortName"],
+                    collectionUrl: "https://cmr.earthdata.nasa.gov/search/concepts/" + conceptId + "/" + meta["revision-id"]
+                }
             })
+
+            similar_collections.renderAndAppend($('.related-collections-cards'), Promise.all(conceptMetadata).then((values) => values))
         })
     },
+    renderAndAppend: function(target, data) {
+        var template = fetch('C1279109560-SCIOPS_files/related-collection-template.html').then((response) => response.text())
 
-    renderAndAppend: function(target) {
-        $.get('C1279109560-SCIOPS_files/related-collection-template.html', function(template) {
-            Mustache.parse(template)
-            var render = Mustache.render(template, {collection: similar_collections.relatedCollections}) || "Not Provided"
-            target.html(render)
-        })
+        Mustache.parse(template)
+        var render = Mustache.render(template, {collection: data}) || "Not Provided"
+        target.html(render)
     }
 }
 
 $(document).ready(function() {
     similar_collections.getConcepts(similar_collections.collection_id)
-
-    setTimeout(function(){ similar_collections.renderAndAppend($('.related-collections-cards'))}, 500)
 })
