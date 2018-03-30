92bb4720c13ee4e377ba05bf62e6071d02cd1e3c
Duncan McGreggor <oubiwann@gmail.com>
2018-03-30 11:25:25 -0500

    Initial files.

---
+++ dev-resources/src/user.clj
@@ -0,0 +1,33 @@
+(ns user
+  "A dev namespace that supports Proto-REPL.
+
+  It seems that Proto-REPL doesn't support the flexible approach that lein
+  uses: any configurable ns can be the starting ns for a REPL. As such, this
+  minimal ns was created for Proto-REPL users, so they too can have an env
+  that supports startup and shutdown."
+  (:require
+   [cheshire.core :as json]
+   [clj-http.client :as httpc]
+   [clojure.java.io :as io]
+   [clojure.pprint :refer [pprint]]
+   [clojure.tools.namespace.repl :as repl]
+   [clojurewerkz.neocons.rest.cypher :as ncy]
+   [clojurewerkz.neocons.rest.nodes :as nn]
+   [clojurewerkz.neocons.rest.paths :as np]
+   [clojurewerkz.neocons.rest.relationships :as nrl]
+   [cmr.opendap.dev :as dev]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   State Management   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def startup #'dev/startup)
+(def shutdown #'dev/shutdown)
+(def system #'dev/system)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Reloading Management   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def reset #'dev/reset)
+(def refresh #'repl/refresh)
