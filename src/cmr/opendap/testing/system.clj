68b6a91dd7a0138c7c9bee9737213aaf59572c61
Duncan McGreggor <oubiwann@gmail.com>
2018-04-04 13:42:28 -0500

    Added a test fixture for brining up a system in integration tests.

---
+++ src/cmr/opendap/testing/system.clj
@@ -0,0 +1,34 @@
+(ns cmr.opendap.testing.system
+  (:require
+    [clojusc.dev.system.core :as system-api]
+    [clojusc.twig :as logger]
+    [cmr.opendap.components.testing]))
+
+;; Hide logging as much as possible before the system starts up, which shuld
+;; disable logging entirely for tests.
+(logger/set-level! '[] :fatal)
+
+(def ^:dynamic *mgr* (atom nil))
+
+(defn startup
+  []
+  (alter-var-root #'*mgr* (constantly (atom (system-api/create-state-manager))))
+  (system-api/set-system-ns (:state @*mgr*) "cmr.opendap.components.testing")
+  (system-api/startup @*mgr*))
+
+(defn shutdown
+  []
+  (when *mgr*
+    (let [result (system-api/shutdown @*mgr*)]
+      (alter-var-root #'*mgr* (constantly (atom nil)))
+      result)))
+
+(defn system
+  []
+  (system-api/get-system (:state @*mgr*)))
+
+(defn with-test-system
+  [test-fn]
+  (startup)
+  (test-fn)
+  (shutdown))
