eb1e5a7f152e85df6d1bdb3b9b1256460e82c264
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2018-04-16 14:55:01 -0500

    Used environ project to provide nested data from system properties and env.

--- src/cmr/opendap/config.clj
+++ src/cmr/opendap/config.clj
@@ -1,13 +1,42 @@
 (ns cmr.opendap.config
   (:require
    [clojure.edn :as edn]
-   [clojure.java.io :as io]))
+   [clojure.java.io :as io]
+   [clojure.string :as string]
+   [environ.core :as environ]))
 
 (def config-file "config/cmr-opendap/config.edn")
 
-(defn data
+(defn cfg-data
   ([]
-    (data config-file))
+    (cfg-data config-file))
   ([filename]
     (with-open [rdr (io/reader (io/resource filename))]
       (edn/read (new java.io.PushbackReader rdr)))))
+
+(defn cmr-only
+  [[k v]]
+  (let [key-name (name k)]
+    (when (string/starts-with? key-name "cmr-")
+      [(mapv keyword (string/split key-name #"-"))
+       (try
+        (Integer/parseInt v)
+        (catch Exception _e
+          v))])))
+
+(defn nest-vars
+  [acc [ks v]]
+  (assoc-in acc ks v))
+
+(defn env-props-data
+  []
+  (->> (#'environ/read-system-props)
+       (merge (#'environ/read-system-env))
+       (map cmr-only)
+       (remove nil?)
+       (reduce nest-vars {})))
+
+(defn data
+  []
+  (merge (cfg-data)
+         (env-props-data)))
