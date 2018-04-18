53c051e496299e898a95339c038c33fad873b37d
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2018-04-18 14:18:36 -0500

    Added deep-merge.

---
+++ src/cmr/opendap/util.clj
@@ -0,0 +1,8 @@
+(ns cmr.opendap.util)
+
+(defn deep-merge
+  "Merge maps recursively."
+  [& maps]
+  (if (every? #(or (map? %) (nil? %)) maps)
+    (apply merge-with deep-merge maps)
+    (last maps)))
