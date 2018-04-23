a2342e140ccb6c86b7c111ca604b3d9a4f7da523
Duncan McGreggor <oubiwann@gmail.com>
2018-04-23 14:07:39 -0500

    CMR-4891: Updated project metadata for Clojars.

--- site-templates/project.clj
+++ site-templates/project.clj
@@ -1,6 +1,6 @@
 (defproject gov.nasa.earthdata/cmr-site-templates "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "https://github.com/cmr-exchange/cmr-site-templates"
+  :description "Selmer templates for CMR documentation, directory pages, and various static web content"
+  :url "https://github.com/nasa/Common-Metadata-Repository/site-templates"
   :license {
     :name "Apache License, Version 2.0"
     :url "http://www.apache.org/licenses/LICENSE-2.0"}
