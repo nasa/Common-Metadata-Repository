e9c509bd7f18ef72af6d422b8e149450963547b1
Duncan McGreggor <oubiwann@gmail.com>
2018-04-24 10:05:12 -0500

    CMR-4891: Added missing aliases used in CMR build.

--- site-templates/project.clj
+++ site-templates/project.clj
@@ -5,4 +5,13 @@
     :name "Apache License, Version 2.0"
     :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [
-    [org.clojure/clojure "1.8.0"]])
+    [org.clojure/clojure "1.8.0"]]
+  :profiles {
+    :test {
+      :plugins [
+        [lein-shell "0.5.0"]
+        [test2junit "1.4.0"]]}}
+  :aliases {
+    ;; The following aliases are needed for the CMR build process.
+    "generate-static" ["with-profile" "+test" "shell" "echo" "NO OP"]
+    "test-out" ["with-profile" "+test" "test2junit"]})
