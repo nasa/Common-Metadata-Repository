d0b12d81a225432dd5a20e73b13cb28134ddffea
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2018-05-12 12:20:37 -0500

    Moved around code comments to better locations.

--- src/cmr/opendap/ous/core.clj
+++ src/cmr/opendap/ous/core.clj
@@ -12,50 +12,6 @@
    [taoensso.timbre :as log]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;   Notes   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; General caveat: the logic for this code was translated -- without domain
-;; knowledge -- from Node.js code that was established as faulty and buggy.
-;; ALL OF THIS needs to be REASSESSED with an intelligent, knowledgable eye.
-
-;; Notes on representing spatial extents.
-;;
-;; EDSC uses URL-encoded long/lat numbers representing a bounding box
-;; Note that the ordering is the same as that used by CMR (see below).
-;;  `-9.984375%2C56.109375%2C19.828125%2C67.640625`
-;; which URL-decodes to:
-;;  `-9.984375,56.109375,19.828125,67.640625`
-;;
-;; OPeNDAP download URLs have something I haven't figured out yet; given that
-;; one of the numbers if over 180, it can't be degrees ... it might be what
-;; WCS uses for `x` and `y`?
-;;  `Latitude[22:34],Longitude[169:200]`
-;;
-;; The OUS Prototype uses the WCS standard for lat/long:
-;;  `SUBSET=axis[,crs](low,high)`
-;; For lat/long this takes the following form:
-;;  `subset=lat(56.109375,67.640625)&subset=lon(-9.984375,19.828125)`
-;;
-;; CMR supports bounding spatial extents by describing a rectangle using four
-;; comma-separated values:
-;;  1. lower left longitude
-;;  2. lower left latitude
-;;  3. upper right longitude
-;;  4. upper right latitude
-;; For example:
-;;  `bounding_box==-9.984375,56.109375,19.828125,67.640625`
-;;
-;; Google's APIs use lower left, upper right, but the specify lat first, then
-;; long:
-;;  `southWest = LatLng(56.109375,-9.984375);`
-;;  `northEast = LatLng(67.640625,19.828125);`
-
-;;; We're going to codify parameters with records to keep things well
-;;; documented. Additionally, this will make converting between parameter
-;;; schemes an explicit operation on explicit data.
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;   Utility/Support Functions   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
