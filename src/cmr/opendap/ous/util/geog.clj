eafc73f404356f1edbf6a3efce0f58da959fd708
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2018-06-07 10:22:33 -0500

    Generalized phase-shift code and reduced code redundancy.

--- src/cmr/opendap/ous/util/geog.clj
+++ src/cmr/opendap/ous/util/geog.clj
@@ -22,7 +22,8 @@
   resolution is stored at indices from 0 to 359 and similar latitudinal data is
   stored at indices from 0 to 179. The max values for lat and lon are stored in
   the UMM-Var records as part of the dimensions. Sometimes those values are
-  pre-decremented for use in OPeNDAP, sometimes not. This function attempts to
+  pre-decremented for use in OPeNDAP, sometimes not (e.g., sometimes max
+  longitude is given as 359, sometimes as 360). This function attempts to
   ensure a consistent use of decremented max values for indices."
   ([max default-max]
    (offset-index max default-max const/default-lat-lon-resolution))
@@ -31,53 +32,65 @@
      max
      (dec max))))
 
+(defn phase-shift
+  "Longitude goes from -180 to 180 and latitude from -90 to 90. However, when
+  referencing data in OPeNDAP arrays, 0-based indices are needed. Thus in order
+  to get indices that match up with degrees, our longitude needs to be
+  phase-shifted by 180 degrees, latitude by 90 degrees."
+  [degrees-max default-abs-degrees-max default-degrees-max degrees adjust-fn round-fn]
+  (let [res (Math/ceil (/ degrees-max default-abs-degrees-max))]
+    (log/trace "Got degrees-max:" degrees-max)
+    (log/trace "Got degrees:" degrees)
+    (log/trace "Got resolution:" res)
+    (-> (/ (* (offset-index degrees-max default-abs-degrees-max res)
+              (adjust-fn degrees res))
+           (adjust-fn default-degrees-max res))
+        round-fn
+        int)))
+
 (defn lon-lo-phase-shift
   [lon-max lon-lo]
-  (let [res (Math/ceil (/ lon-max const/default-lon-abs-hi))]
-    (log/debug "Got lon-max:" lon-max)
-    (log/debug "Got resolution:" res)
-    (-> (/ (* (offset-index lon-max const/default-lon-abs-hi res)
-              (adjusted-lon lon-lo res))
-           (adjusted-lon const/default-lon-hi res))
-        Math/floor
-        int)))
+  (phase-shift
+   lon-max
+   const/default-lon-abs-hi
+   const/default-lon-hi
+   lon-lo
+   adjusted-lon
+   #(Math/floor %)))
 
 (defn lon-hi-phase-shift
   [lon-max lon-hi]
-  (let [res (Math/ceil (/ lon-max const/default-lon-abs-hi))]
-    (log/debug "Got lon-max:" lon-max)
-    (log/debug "Got resolution:" res)
-    (-> (/ (* (offset-index lon-max const/default-lon-abs-hi res)
-              (adjusted-lon lon-hi res))
-           (adjusted-lon const/default-lon-hi res))
-        Math/ceil
-        int)))
+  (phase-shift
+   lon-max
+   const/default-lon-abs-hi
+   const/default-lon-hi
+   lon-hi
+   adjusted-lon
+   #(Math/ceil %)))
 
 (defn lat-lo-phase-shift
   "This is used for reading values from OPeNDAP where -90N is stored at the
   zero (first) index in the array."
   [lat-max lat-lo]
-  (let [res (Math/ceil (/ lat-max const/default-lat-abs-hi))]
-    (log/debug "Got lat-max:" lat-max)
-    (log/debug "Got resolution:" res)
-    (-> (/ (* (offset-index lat-max const/default-lat-abs-hi res)
-              (adjusted-lat lat-lo res))
-           (adjusted-lat const/default-lat-hi res))
-        Math/floor
-        int)))
+  (phase-shift
+   lat-max
+   const/default-lat-abs-hi
+   const/default-lat-hi
+   lat-lo
+   adjusted-lat
+   #(Math/floor %)))
 
 (defn lat-hi-phase-shift
   "This is used for reading values from OPeNDAP where -90N is stored at the
   zero (first) index in the array."
   [lat-max lat-hi]
-  (let [res (Math/ceil (/ lat-max const/default-lat-abs-hi))]
-    (log/debug "Got lat-max:" lat-max)
-    (log/debug "Got resolution:" res)
-    (-> (/ (* (offset-index lat-max const/default-lat-abs-hi res)
-              (adjusted-lat lat-hi res))
-           (adjusted-lat const/default-lat-hi res))
-        Math/ceil
-        int)))
+  (phase-shift
+   lat-max
+   const/default-lat-abs-hi
+   const/default-lat-hi
+   lat-hi
+   adjusted-lat
+   #(Math/ceil %)))
 
 (defn lat-lo-phase-shift-reversed
   "This is used for reading values from OPeNDAP where 90N is stored at the
