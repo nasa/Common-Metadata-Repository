82d0c27ec10e556000ee69c36ef38ba569919a89
Duncan McGreggor <oubiwann@gmail.com>
2018-06-16 22:31:07 -0500

    Moved caching component from cmr-opendap to here.

--- src/cmr/authz/components/config.clj
+++ src/cmr/authz/components/config.clj
@@ -32,6 +32,29 @@
 (def get-access-control-url #(get-service-url % :access-control))
 (def get-echo-rest-url #(get-service-url % :echo-rest))
 
+(defn cache-dumpfile
+  [system]
+  (get-in (get-cfg system) [:auth-caching :dumpfile]))
+
+(defn cache-init
+  [system]
+  (get-in (get-cfg system) [:auth-caching :init]))
+
+(defn cache-lru-threshold
+  [system]
+  (get-in (get-cfg system) [:auth-caching :lru :threshold]))
+
+(defn cache-ttl-ms
+  [system]
+  (* (get-in (get-cfg system) [:auth-caching :ttl :minutes]) ; minutes
+     60 ; seconds
+     1000 ; milliseconds
+     ))
+
+(defn cache-type
+  [system]
+  (get-in (get-cfg system) [:auth-caching :type]))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;   Component Lifecycle Implementation   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
