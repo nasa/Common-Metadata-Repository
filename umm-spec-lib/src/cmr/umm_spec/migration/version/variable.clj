7073c7f90320fe63bbe2a2f82f6c28025d6f3f07
Duncan McGreggor <oubiwann@gmail.com>
2018-07-20 11:33:44 -0500

    CMR-4997: Addressed review feedback.

--- umm-spec-lib/src/cmr/umm_spec/migration/version/variable.clj
+++ umm-spec-lib/src/cmr/umm_spec/migration/version/variable.clj
@@ -3,22 +3,22 @@
   (:require
    [cmr.umm-spec.migration.version.interface :as interface]))
 
-(defn sample-11->12
+(defn sample-1-1->1-2
   [x]
   {:MeasurementConditions (:MeasurementConditions x)
    :ReportingConditions (:ReportingConditions x)})
 
-(defn measurement-11->12
+(defn measurement-1-1->1-2
   [x]
   {:MeasurementSource (:MeasurementSource x)
    :MeasurementName {:MeasurementObject (:MeasurementName x)}})
 
-(defn measurement-12->11
+(defn measurement-1-2->1-1
   [x]
   {:MeasurementSource (:MeasurementSource x)
    :MeasurementName (get-in x [:MeasurementName :MeasurementObject])})
 
-(defn dimensions-12->11
+(defn dimensions-1-2->1-1
   [dims]
   (mapv #(dissoc % :Type) dims))
 
@@ -41,8 +41,8 @@
   (-> v
       (assoc :Characteristics (mapv #(select-keys % [:GroupPath])
                                     (:Characteristics v))
-             :MeasurementIdentifiers (mapv measurement-11->12 (:Measurements v))
-             :SamplingIdentifiers (mapv sample-11->12 (:Characteristics v)))
+             :MeasurementIdentifiers (mapv measurement-1-1->1-2 (:Measurements v))
+             :SamplingIdentifiers (mapv sample-1-1->1-2 (:Characteristics v)))
       (dissoc :Measurements)))
 
 (defmethod interface/migrate-umm-version [:variable "1.2" "1.1"]
@@ -55,8 +55,8 @@
              ;; because there's no way to definitely correlate indices of separate
              ;; arrays when trying to construct a 1.1 Characteristics array by
              ;; combining 1.2 Characteristics and 1.2 SamplingIdentifier arrays.
-             :Measurements (mapv measurement-12->11 (:MeasurementIdentifiers v)))
-      (update :Dimensions dimensions-12->11)
+             :Measurements (mapv measurement-1-2->1-1 (:MeasurementIdentifiers v)))
+      (update :Dimensions dimensions-1-2->1-1)
       (dissoc :MeasurementIdentifiers
               :SamplingIdentifiers
               :VariableSubType)))
