c2247535c1bcc00fcb89d4d0b7199829f4fa3ca8
Duncan McGreggor <oubiwann@gmail.com>
2018-09-18 12:41:43 -0500

    Added utility function from cmr-jar-plugin project.

--- src/cmr/exchange/common/util.clj
+++ src/cmr/exchange/common/util.clj
@@ -1,4 +1,16 @@
 (ns cmr.exchange.common.util
   (:require
-   [taoensso.timbre :as log]))
+   [clojure.string :as string]
+   [taoensso.timbre :as log])
+  (:import
+   (clojure.lang Symbol)))
 
+(defn resolve-fully-qualified-fn
+  [^Symbol fqfn]
+  (when fqfn
+    (try
+      (let [[name-sp fun] (mapv symbol (string/split (str fqfn) #"/"))]
+        (require name-sp)
+        (var-get (ns-resolve name-sp fun)))
+      (catch  Exception _
+        (log/warn "Couldn't resolve one or more of" fqfn)))))
