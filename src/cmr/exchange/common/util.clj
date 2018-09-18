68e3bfe18dbb60de6b716203fb2b819cb9d3870f
Duncan McGreggor <oubiwann@gmail.com>
2018-09-18 13:20:48 -0500

    Moved useful functions over from cmr-process-manager.

--- src/cmr/exchange/common/util.clj
+++ src/cmr/exchange/common/util.clj
@@ -1,9 +1,42 @@
 (ns cmr.exchange.common.util
   (:require
+   [clojure.java.io :as io]
    [clojure.string :as string]
    [taoensso.timbre :as log])
   (:import
    (clojure.lang Symbol)))
+
+(defn newline?
+  ""
+  [byte]
+  (= (char byte) \newline))
+
+(defn bytes->ascii
+  ""
+  [bytes]
+  (.trim (new String bytes "US-ASCII")))
+
+(defn bytes->utf8
+  ""
+  [bytes]
+  (.trim (new String bytes "UTF-8")))
+
+(defn bytes->int
+  ""
+  [bytes]
+  (-> bytes
+      (bytes->ascii)
+      (Integer/parseInt)))
+
+(defn str->bytes
+  ""
+  [str]
+  (.getBytes str))
+
+(defn str->stream
+  ""
+  [str]
+  (io/input-stream (str->bytes str)))
 
 (defn resolve-fully-qualified-fn
   [^Symbol fqfn]
