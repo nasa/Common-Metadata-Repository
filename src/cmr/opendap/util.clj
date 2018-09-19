fe409b79f2f613cac306c783380b6919ba6657bc
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2018-09-19 16:36:46 -0500

    CMR-5145: Moved utility functions into cmr.exchange.common.

--- src/cmr/opendap/util.clj
+++ src/cmr/opendap/util.clj
@@ -1,45 +1,15 @@
-(ns cmr.opendap.util)
+(ns cmr.opendap.util
+  (:require
+   [cmr.exchange.common.util :as util]))
 
-(defn bool
-  [arg]
-  (if (contains? #{true :true "true" "TRUE" "t" "T" 1} arg)
-    true
-    false))
-
-(defn remove-empty
-  [coll]
-  (remove #(or (nil? %) (empty? %)) coll))
-
-(defn deep-merge
-  "Merge maps recursively."
-  [& maps]
-  (if (every? #(or (map? %) (nil? %)) maps)
-    (apply merge-with deep-merge maps)
-    (last maps)))
-
-(defn now
-  []
-  (/ (System/currentTimeMillis) 1000))
-
-(defn timed
-  [start]
-  (float (- (now) start)))
-
-(defn most-frequent
-  "This identifies the most frequently occuring data in a collection
-  and returns it."
-  [data]
-  (->> data
-       frequencies
-       ;; the 'frequencies' function puts data first; let's swap the order
-       (map (fn [[k v]] [v k]))
-       ;; sort in reverse order to get the highest counts first
-       (sort (comp - compare))
-       ;; just get the highest
-       first
-       ;; the first element is the count, the second is the bounding data
-       second))
-
-(defn promise?
-  [p]
-  (isa? (class p) clojure.lang.IPending))
+;; XXX Once the ns's that call this functions have been updated to reference
+;; `cmr.exchange.common.util` directly, we can delete this (and this whole
+;; file).
+(def bool util/bool?)
+(def bool? util/bool?)
+(def remove-empty util/remove-empty)
+(def deep-merge util/deep-merge)
+(def now util/now)
+(def timed util/timed)
+(def most-frequent util/most-frequent)
+(def promise? util/promise?)
