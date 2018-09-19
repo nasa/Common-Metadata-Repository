cdacc63a19e6851e4860d14d926a595e4130a39d
Duncan McGreggor <oubiwann@gmail.com>
2018-09-19 16:34:50 -0500

    Moved utility functions over from cmr.opendap.

--- src/cmr/exchange/common/util.clj
+++ src/cmr/exchange/common/util.clj
@@ -11,6 +11,52 @@
   [byte]
   (= (char byte) \newline))
 
+(defn bool?
+  [arg]
+  (if (contains? #{true :true "true" "TRUE" "t" "T" 1} arg)
+    true
+    false))
+
+(def bool bool?)
+
+(defn remove-empty
+  [coll]
+  (remove #(or (nil? %) (empty? %)) coll))
+
+(defn deep-merge
+  "Merge maps recursively."
+  [& maps]
+  (if (every? #(or (map? %) (nil? %)) maps)
+    (apply merge-with deep-merge maps)
+    (last maps)))
+
+(defn now
+  []
+  (/ (System/currentTimeMillis) 1000))
+
+(defn timed
+  [start]
+  (float (- (now) start)))
+
+(defn most-frequent
+  "This identifies the most frequently occuring data in a collection
+  and returns it."
+  [data]
+  (->> data
+       frequencies
+       ;; the 'frequencies' function puts data first; let's swap the order
+       (map (fn [[k v]] [v k]))
+       ;; sort in reverse order to get the highest counts first
+       (sort (comp - compare))
+       ;; just get the highest
+       first
+       ;; the first element is the count, the second is the bounding data
+       second))
+
+(defn promise?
+  [p]
+  (isa? (class p) clojure.lang.IPending))
+
 (defn bytes->ascii
   ""
   [bytes]
