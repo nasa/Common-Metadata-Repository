dc321bbf6bd0670e08ac1d761367b5df107cec4e
sxu123 <wind.cloud.sky@gmail.com>
2018-09-20 15:38:17 -0400

    CMR-5080: Log metadata-size-bytes with provider info, after successful ingest (#572)
    * CMR-5080: Log metadata-size-bytes with provider info, after successful ingest.
    
    * CMR=5080: Addressed the PR comments.

--- ingest-app/src/cmr/ingest/api/collections.clj
+++ ingest-app/src/cmr/ingest/api/collections.clj
@@ -44,13 +44,17 @@
     (common-enabled/validate-write-enabled request-context "ingest")
     (let [concept (api-core/body->concept! :collection provider-id native-id body content-type headers)
           validation-options (get-validation-options headers)
+          ;; Log the ingest attempt
+          _ (info (format "Ingesting collection %s from client %s"
+                          (api-core/concept->loggable-string concept)
+                          (:client-id request-context)))
           save-collection-result (ingest/save-collection
                                   request-context
                                   (api-core/set-user-id concept request-context headers)
                                   validation-options)]
-      (info (format "Ingesting collection %s from client %s"
-              (api-core/concept->loggable-string (assoc concept :entry-title (:entry-title save-collection-result)))
-              (:client-id request-context)))
+      ;; Log the successful ingest, with the metadata size in bytes.
+      (api-core/log-concept-with-metadata-size 
+        (assoc concept :entry-title (:entry-title save-collection-result)) request-context)
       (api-core/generate-ingest-response headers
                                          (api-core/contextualize-warnings
                                           ;; entry-title is added just for the logging above.
