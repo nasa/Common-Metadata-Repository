43fd69e66a44379c994f78efab19db6f5afa0b71
Duncan McGreggor <oubiwann@gmail.com>
2018-09-24 11:33:06 -0500

    Added nvd/security checks to build.

--- project.clj
+++ project.clj
@@ -21,13 +21,24 @@
     :name "Apache License, Version 2.0"
     :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [
-    [clojusc/trifl "0.2.0"]
-    [clojusc/twig "0.3.2"]
+    [clojusc/trifl "0.3.0"]
+    [clojusc/twig "0.3.3"]
     [org.clojure/clojure "1.9.0"]]
+  :aot [clojure.tools.logging.impl]
   :profiles {
     :ubercompile {
       :aot :all
       :source-paths ["test"]}
+    :security {
+      :plugins [
+        [lein-nvd "0.5.4"]]
+      :source-paths ^:replace ["src"]
+      :nvd {
+        :suppression-file "resources/security/false-positives.xml"}
+      :exclusions [
+        ;; The following are excluded due to their being flagged as a CVE
+        [com.google.protobuf/protobuf-java]
+        [com.google.javascript/closure-compiler-unshaded]]}
     :dev {
       :dependencies [
         [clojusc/system-manager "0.3.0-SNAPSHOT"]
@@ -45,7 +56,7 @@
       :source-paths ^:replace ["src"]
       :test-paths ^:replace []
       :plugins [
-        [jonase/eastwood "0.2.6"]
+        [jonase/eastwood "0.2.9"]
         [lein-ancient "0.6.15"]
         [lein-kibit "0.1.6"]]}
     :test {
@@ -63,7 +74,7 @@
     "repl" ["do"
       ["clean"]
       ["repl"]]
-    "ubercompile" ["with-profile" "+ubercompile" "compile"]
+    "ubercompile" ["with-profile" "+ubercompile,+security" "compile"]
     "check-vers" ["with-profile" "+lint" "ancient" "check" ":all"]
     "check-jars" ["with-profile" "+lint" "do"
       ["deps" ":tree"]
@@ -71,19 +82,27 @@
     "check-deps" ["do"
       ["check-jars"]
       ["check-vers"]]
+    "ltest" ["with-profile" "+test,+system,+security" "ltest"]
+    ;; Linting
     "kibit" ["with-profile" "+lint" "kibit"]
     "eastwood" ["with-profile" "+lint" "eastwood" "{:namespaces [:source-paths]}"]
     "lint" ["do"
       ["kibit"]
       ;["eastwood"]
       ]
-    "ltest" ["with-profile" "+test" "ltest"]
-    ;; Documentation and static content
+    ;; Security
+    "check-sec" ["with-profile" "+security" "do"
+      ["clean"]
+      ["nvd" "check"]]
     ;; Build tasks
+    "build-jar" ["with-profile" "+security" "jar"]
+    "build-uberjar" ["with-profile" "+security" "uberjar"]
     "build-lite" ["do"
       ["ltest" ":unit"]]
     "build" ["do"
       ["clean"]
+      ["check-vers"]
+      ["check-sec"]
       ["ltest" ":unit"]
       ["ubercompile"]
-      ["uberjar"]]})
+      ["build-uberjar"]]})
