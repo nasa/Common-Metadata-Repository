74dc7c6e1a77f8b6992bd64e3e9e655205361390
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2018-09-26 13:43:38 -0500

    CMR-5145: Use the latest version of query that switched back to keywords.

--- project.clj
+++ project.clj
@@ -29,7 +29,7 @@
     [environ "1.1.0"]
     [gov.nasa.earthdata/cmr-authz "0.1.1-SNAPSHOT"]
     [gov.nasa.earthdata/cmr-exchange-common "0.2.0-SNAPSHOT"]
-    [gov.nasa.earthdata/cmr-exchange-query "0.1.0-SNAPSHOT"]
+    [gov.nasa.earthdata/cmr-exchange-query "0.2.0-SNAPSHOT"]
     [gov.nasa.earthdata/cmr-http-kit "0.1.2-SNAPSHOT"]
     [gov.nasa.earthdata/cmr-jar-plugin "0.1.0-SNAPSHOT"]
     [gov.nasa.earthdata/cmr-mission-control "0.1.0-SNAPSHOT"]
