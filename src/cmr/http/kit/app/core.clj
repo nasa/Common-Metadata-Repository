4e1440b1c2369421808df7e9a1601d5330591775
Duncan McGreggor <oubiwann@gmail.com>
2018-10-03 11:37:40 -0500

    Updates to how routes are now collected based on jar-plugin changes.

--- src/cmr/http/kit/app/core.clj
+++ src/cmr/http/kit/app/core.clj
@@ -20,17 +20,18 @@
   For more information about JAR-file-based plugins, see:
   * https://github.com/cmr-exchange/cmr-jar-plugin"
   [httpd-component]
-  (let [{plugins-site-routes :site
-         plugins-api-routes :api} (registry/assembled-routes httpd-component)
-         main-site-routes-fn (config/site-routes httpd-component)]
-    (log/trace "plugins-site-routes:" (vec plugins-site-routes))
-    (log/trace "plugins-api-routes:" (vec plugins-api-routes))
+  (let [{plugins-site-routes-fns :site
+         plugins-api-routes-fns :api} (registry/resolved-routes httpd-component)
+         main-site-routes-fn (config/site-routes httpd-component)
+         main-api-routes-fn (config/api-routes httpd-component)]
+    (log/trace "plugins-site-routes-fns:" (vec plugins-site-routes-fns))
+    (log/trace "plugins-api-routes-fns:" (vec plugins-api-routes-fns))
     (log/trace "main-site-routes-fn:" main-site-routes-fn)
     ;; Note that the following calls don't call the routes, rather they call
     ;; the configuration function which extract the routes from the config
     ;; data. The route functions provided in the configuration data will be
     ;; called by a middleware wrapper.
-    {:site-routes (concat plugins-site-routes
+    {:site-routes (concat (mapcat #(% httpd-component) plugins-site-routes-fns)
                           (main-site-routes-fn httpd-component))
-     :plugins-api-routes plugins-api-routes
-     :main-api-routes-fn (config/api-routes httpd-component)}))
+     :plugins-api-routes-fns plugins-api-routes-fns
+     :main-api-routes-fn main-api-routes-fn}))
