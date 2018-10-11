764d28bc13a386934e64e2dfca14accd50a2ad93
Duncan McGreggor <oubiwann@gmail.com>
2018-10-11 18:03:12 -0500

    CMR-5145: Updated to use new NGAP/EECS service-bridge setup.

--- src/cmr/sizing/app/routes/rest.clj
+++ src/cmr/sizing/app/routes/rest.clj
@@ -11,12 +11,12 @@
 
 (defn size-estimate-api
   [httpd-component]
-  [["/opendap/size-estimate/collection/:concept-id" {
+  [["/service-bridge/size-estimate/collection/:concept-id" {
     :get {:handler (handler/estimate-size httpd-component)
           :permissions #{:read}}
     :post {:handler (handler/estimate-size httpd-component)
            :permissions #{:read}}}]
-   ["/opendap/size-estimate/streaming-collection/:concept-id" {
+   ["/service-bridge/size-estimate/streaming-collection/:concept-id" {
     :get {:handler (handler/stream-estimate-size httpd-component)
           :permissions #{:read}}}]])
 
