cdacc169d945f65e424d6624efb7355310b5d24f
mschmele <mschmele@gmail.com>
2018-10-19 14:52:32 -0400

    CMR-5192: Add docstring to core application startup

--- src/cmr/opendap/app/core.clj
+++ src/cmr/opendap/app/core.clj
@@ -6,6 +6,19 @@
    [taoensso.timbre :as log]))
 
 (defn main
+  "CMR Service Bridge is a framework that supports the addition of new features
+  via plugins. As a framework, it offers a limited number of routes and handlers,
+  but it is the individual plugins that provide the functionality.
+
+  As you can see below, site-routes are extracted from the route-data; the reason
+  we do not extract api-routes (i.e. REST resources), is because api-routes are
+  versioned. As such, a different version of the route may be required at
+  request time. To satisfy that condition, we build the routes dynamically
+  inside the middleware that handles the API versioning.
+
+  Lastly, a whole series of middleware (both community-provided and CMR-created)
+  are applied after we perform versioning dispatch. These may be viewed in the
+  cmr-http-kit project."
   [httpd-component]
   (log/trace "httpd-component keys:" (keys httpd-component))
   (let [{site-routes :site-routes :as route-data}
