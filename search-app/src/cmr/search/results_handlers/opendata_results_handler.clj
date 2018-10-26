87d7f68a85c7e4593d3ef3e039fc216a9183653e
Cayvon Hamidizadeh <cayvon.hamidizadeh@nasa.gov>
2018-10-26 09:44:25 -0400

    CMR-5236: Change url-type for readability.

--- search-app/src/cmr/search/results_handlers/opendata_results_handler.clj
+++ search-app/src/cmr/search/results_handlers/opendata_results_handler.clj
@@ -219,8 +219,9 @@
   (let [{:keys [description url get-data-mime-type]} related-url
         get-data-mime-type (or (util/nil-if-value umm-spec-util/not-provided get-data-mime-type)
                                (ru/infer-url-mime-type url))
-        downloadable? (and (ru/downloadable-url? related-url) get-data-mime-type)]
-    (util/remove-nil-keys {(if downloadable? :downloadURL :accessURL) (ru/related-url->encoded-url url)
+        downloadable? (and (ru/downloadable-url? related-url) get-data-mime-type)
+        url-type (if downloadable? :downloadURL :accessURL)]
+    (util/remove-nil-keys {url-type (ru/related-url->encoded-url url)
                            :mediaType (when downloadable? get-data-mime-type)
                            :description description})))
 
