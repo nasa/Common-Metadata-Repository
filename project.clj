01243b8b37dac35972af3ce652023af4d1feb140
Duncan McGreggor <oubiwann@gmail.com>
2018-10-30 16:17:17 -0500

    CMR-5192: Updated deps.

--- project.clj
+++ project.clj
@@ -66,7 +66,7 @@
       :source-paths ^:replace ["src"]
       :test-paths ^:replace []
       :plugins [
-        [jonase/eastwood "0.3.1"]
+        [jonase/eastwood "0.3.3"]
         [lein-ancient "0.6.15"]
         [lein-bikeshed "0.5.1"]
         [lein-kibit "0.1.6"]
