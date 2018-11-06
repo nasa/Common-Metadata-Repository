eddc1ba79efb53a742c1a7564dc0fbcf744d6de4
Yonggang Liu <ygliuvt@gmail.com>
2018-11-06 09:25:12 -0500

    CMR-5248: As a UMM-VAR creator I should be able to ingest multiple UMM-VAR with the same name in the same provider.

--- metadata-db-app/int-test/cmr/metadata_db/int_test/concepts/variable_save_test.clj
+++ metadata-db-app/int-test/cmr/metadata_db/int_test/concepts/variable_save_test.clj
@@ -35,10 +35,30 @@
   (let [concept (concepts/create-concept :variable "PROV1" 2)]
     (util/concept-created-at-assertions "variable" concept)))
 
-(deftest save-variable-with-conflicting-native-id
-  (let [concept (concepts/create-concept :variable "PROV1" 1)]
-    (util/concept-with-conflicting-native-id-assertions
-     "variable"
-     :variable-name
-     concept
-     "var-native-different")))
+(deftest save-variable-with-same-variable-name
+  (let [concept1 (concepts/create-concept :variable "PROV1" 1)
+        {status :status
+         revision-id :revision-id
+         concept1-concept-id :concept-id} (util/save-concept concept1)]
+    ;; verify variable is saved and the revision id is 1
+    (is (= status 201))
+    (is (= 1 revision-id))
+    (testing "save variable with all the same data, i.e. update the variable"
+      (let [{:keys [status concept-id revision-id]} (util/save-concept concept1)]
+        (is (= 201 status))
+        (is (= concept1-concept-id concept-id))
+        (is (= 2 revision-id))))
+    (testing "save variable with the same data, but a different native id creates a new variable"
+      (let [concept2 (assoc concept1 :native-id "different-native-id")
+            {:keys [status concept-id revision-id]} (util/save-concept concept2)]
+        ;; We allow variables with the same variable name
+        ;; to be saved within the same provider using a different native id
+        (is (= 201 status))
+        (is (not= concept1-concept-id concept-id))
+        (is (= 1 revision-id))))
+    (testing "save variable with same data but different provider"
+      (let [concept3 (assoc concept1 :provider-id "PROV2")
+            {:keys [status concept-id revision-id]} (util/save-concept concept3)]
+        (is (= status 201))
+        (is (not= concept1-concept-id concept-id))
+        (is (= 1 revision-id))))))
