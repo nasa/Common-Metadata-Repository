4e083cdce9b1abe81d08848b30d4aa373b681ae3
Cayvon Hamidizadeh <cayvon.hamidizadeh@nasa.gov>
2018-11-13 12:56:05 -0500

    CMR-5212: Add FilePackageType record.
    * Add FilePackageType record.
    * Change ArchiveAndDistributionInformationType to be anyOf FilePackageType or FileType.

--- umm-spec-lib/src/cmr/umm_spec/models/umm_granule_models.clj
+++ umm-spec-lib/src/cmr/umm_spec/models/umm_granule_models.clj
@@ -669,6 +669,33 @@
   ])
 (record-pretty-printer/enable-record-pretty-printing ArchiveAndDistributionInformationType)
 
+;; This set of elements describes a file package or a file that contains other files.
+;; Normally this is either a tar or a zip file.
+(defrecord FilePackageType
+  [
+   ;; This field describes the name of the actual file.
+   Name
+
+   ;; The size of the file.
+   Size
+
+   ;; The unit of the file size.
+   SizeUnit
+
+   ;; This element defines a single format for a distributable artifact.
+   Format
+
+   ;; The mime type of the resource.
+   MimeType
+
+   ;; Allows the provider to provide the checksum value for the file.
+   Checksum
+
+   ;; Allows the provider to add the list of the files that are included in this one.
+   Files
+   ])
+(record-pretty-printer/enable-record-pretty-printing FilePackageType)
+
 ;; A reference to a collection metadata record's short name and version, or entry title to which
 ;; this granule metadata record belongs.
 (defrecord CollectionReferenceType
