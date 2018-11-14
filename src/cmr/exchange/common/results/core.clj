f84b5d64d57208619e332cdd65669cdbd3861796
Daniel Zamora <daniel.p.zamora@nasa.gov>
2018-11-14 13:48:36 -0500

    CMR-5199 Adds request-id to result map.

--- src/cmr/exchange/common/results/core.clj
+++ src/cmr/exchange/common/results/core.clj
@@ -9,16 +9,19 @@
    took
    ;; The actual items in the result set
    items
+   ;; The randomly generated request-id string
+   request-id
    ;; Any non-error messages that need to be returned
    warnings])
 
 (defn create
-  [results & {:keys [elapsed warnings]}]
+  [results & {:keys [request-id elapsed warnings]}]
   (map->CollectionResults
     (merge {;; Our 'hits' is simplistic for now; will change when we support
             ;; paging, etc.
             :hits (count results)
             :took elapsed
+            :request-id request-id 
             :items results}
            warnings)))
 
