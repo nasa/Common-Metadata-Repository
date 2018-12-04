0f85da760109e99ff0fd90b6cc67b9dd506249b4
Daniel Zamora <daniel.p.zamora@nasa.gov>
2018-12-04 09:19:11 -0500

    CMR-5343 micro version bump

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
        ns
        "\u001B[35m]\u001B[33m λ\u001B[m=> "))
 
-(defproject gov.nasa.earthdata/cmr-service-bridge "1.6.0-SNAPSHOT"
+(defproject gov.nasa.earthdata/cmr-service-bridge "1.6.1-SNAPSHOT"
   :description "A CMR connector service that provides an inter-service API"
   :url "https://github.com/cmr-exchange/cmr-service-bridge"
   :license {
@@ -36,7 +36,7 @@
     [gov.nasa.earthdata/cmr-mission-control "0.1.0"]
     [gov.nasa.earthdata/cmr-ous-plugin "0.3.0-SNAPSHOT"]
     [gov.nasa.earthdata/cmr-site-templates "0.1.0"]
-    [gov.nasa.earthdata/cmr-sizing-plugin "0.2.2-SNAPSHOT"]
+    [gov.nasa.earthdata/cmr-sizing-plugin "0.2.3-SNAPSHOT"]
     [http-kit "2.3.0"]
     [markdown-clj "1.0.5"]
     [metosin/reitit-core "0.2.9"]
