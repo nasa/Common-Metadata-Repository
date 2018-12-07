c9512dc3b0c3f248d3e65cd7f6d565eac5c76792
Yonggang Liu <ygliuvt@gmail.com>
2018-12-07 09:53:21 -0500

    CMR-5346: Fixed UMM-Var migration from v1.1 to v1.2 to generate valid record.

--- umm-spec-lib/src/cmr/umm_spec/migration/version/variable.clj
+++ umm-spec-lib/src/cmr/umm_spec/migration/version/variable.clj
@@ -1,27 +1,60 @@
 (ns cmr.umm-spec.migration.version.variable
   "Contains functions for migrating between versions of the UMM Variable schema."
   (:require
+   [clojure.string :as string]
    [cmr.common.util :as util]
    [cmr.umm-spec.migration.version.interface :as interface]))
 
-(defn sample-1-1->1-2
+(defn- sample-1-1->1-2
   [x]
-  {:MeasurementConditions (:MeasurementConditions x)
-   :ReportingConditions (:ReportingConditions x)})
+  (let [{:keys [MeasurementConditions ReportingConditions]} x]
+    (when (and MeasurementConditions ReportingConditions)
+      [{:MeasurementConditions MeasurementConditions
+       :ReportingConditions ReportingConditions}])))
 
-(defn measurement-1-1->1-2
+(def ^:private measurement-sources
+ #{"CSDMS", "CF", "BODC", "OTHER"})
+
+(def ^:private fill-value-types
+  #{"SCIENCE_FILLVALUE", "QUALITY_FILLVALUE", "ANCILLARY_FILLVALUE", "OTHER"})
+
+(defn- enum-value
+  "Returns the proper enum value for the allowed enum hash, the initial value and default"
+  [enum-hash value default-value]
+  (if value
+    (if-let [new-value (enum-hash (string/upper-case value))]
+      new-value
+      default-value)
+    default-value))
+
+(defn- measurement-1-1->1-2
   [x]
-  {:MeasurementSource (:MeasurementSource x)
-   :MeasurementName {:MeasurementObject (:MeasurementName x)}})
+  (let [{:keys [MeasurementSource MeasurementName]} x
+        source (enum-value measurement-sources MeasurementSource "OTHER")]
+    (when (and source MeasurementName)
+      {:MeasurementSource source
+       :MeasurementName {:MeasurementObject MeasurementName}})))
 
-(defn measurement-1-2->1-1
+(defn- measurement-1-2->1-1
   [x]
-  {:MeasurementSource (:MeasurementSource x)
-   :MeasurementName (get-in x [:MeasurementName :MeasurementObject])})
+  (when x
+    {:MeasurementSource (:MeasurementSource x)
+     :MeasurementName (get-in x [:MeasurementName :MeasurementObject])}))
 
-(defn dimensions-1-2->1-1
+(defn- fill-value-1-1->1-2
+  [value]
+  (if-let [fill-value-type (:Type value)]
+    (assoc value :Type (enum-value fill-value-types fill-value-type "OTHER"))
+    value))
+
+(defn- fill-values-1-1->1-2
+  [values]
+  (when (seq values)
+    (map fill-value-1-1->1-2 values)))
+
+(defn- dimensions-1-2->1-1
   [dims]
-  (mapv #(dissoc % :Type) dims))
+  (map #(dissoc % :Type) dims))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Variable Migration Implementations
@@ -40,15 +73,21 @@
   ;; Migrate up to 1.2
   [context v & _]
   (-> v
-      (assoc :MeasurementIdentifiers (mapv measurement-1-1->1-2 (:Measurements v))
-             :SamplingIdentifiers [(sample-1-1->1-2 (:Characteristics v))])
-      (dissoc :Measurements :Characteristics)))
+      (assoc :MeasurementIdentifiers (->> (:Measurements v)
+                                          (map measurement-1-1->1-2)
+                                          (remove nil?)
+                                          seq)
+             :SamplingIdentifiers (sample-1-1->1-2 (:Characteristics v)))
+      (dissoc :Measurements :Characteristics)
+      (util/update-in-each [:Dimensions] assoc :Type "OTHER")
+      (update :FillValues fill-values-1-1->1-2)
+      util/remove-nil-keys))
 
 (defmethod interface/migrate-umm-version [:variable "1.2" "1.1"]
   ;; Migrate down to 1.1
   [context v & _]
   (-> v
-      (assoc :Measurements (mapv measurement-1-2->1-1 (:MeasurementIdentifiers v)))
+      (assoc :Measurements (map measurement-1-2->1-1 (:MeasurementIdentifiers v)))
       (update :Dimensions dimensions-1-2->1-1)
       (dissoc :MeasurementIdentifiers
               ;; Note that there is no characteristics/*conditions x-form
