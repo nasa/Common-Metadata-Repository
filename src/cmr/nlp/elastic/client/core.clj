1ef366ecf0438a1367231f529941d2af674fdf27
Duncan McGreggor <oubiwann@gmail.com>
2018-12-14 11:41:21 -0600

    CMR-5351: Added support for ES document API methods.

--- src/cmr/nlp/elastic/client/core.clj
+++ src/cmr/nlp/elastic/client/core.clj
@@ -1,6 +1,7 @@
 (ns cmr.nlp.elastic.client.core
   (:require
    [cheshire.core :as json]
+   [cmr.nlp.elastic.client.document :as document]
    [cmr.nlp.elastic.client.index :as index]
    [cmr.nlp.elastic.client.pipeline :as pipeline]
    [taoensso.timbre :as log])
@@ -18,6 +19,12 @@
 (defprotocol ElasticsearchAPI
   ;; Base API
   (close [this])
+  ;; Document API
+  (bulk [this requests-data])
+  (index [this index-name doc-type doc-id source])
+  (delete-document [this index-name doc-type doc-id])
+  (update-document [this index-name doc-type doc-id source])
+  (upsert-document [this index-name doc-type doc-id source])
   ;; Cluster API
   (cluster [this])
   (health [this])
@@ -34,6 +41,8 @@
 ;;;   Implementation   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Bulk API
+
 ;; Cluster API
 
 (def -cluster #(.cluster %))
@@ -45,6 +54,39 @@
     (-> resp
         str
         (json/parse-string true))))
+
+;; Document API
+
+(defn -index
+  [this index-name doc-type doc-id source]
+  (let [req (document/index index-name doc-type doc-id source)
+        resp (.index this req RequestOptions/DEFAULT)]
+    (.getIndex resp)))
+
+(defn -bulk
+  [this reqs-data]
+  (let [req (document/bulk reqs-data)
+        resp (.bulk this req RequestOptions/DEFAULT)]
+    (log/info "Bulk document operation took:" (str (.getTook resp)))
+    (.getItems resp)))
+
+(defn -delete-document
+  [this index-name doc-type doc-id]
+  (let [req (document/delete index-name doc-type doc-id)
+        resp (.delete this req RequestOptions/DEFAULT)]
+    (.getIndex resp)))
+
+(defn -update-document
+  [this index-name doc-type doc-id source]
+  (let [req (document/update index-name doc-type doc-id source)
+        resp (.update this req RequestOptions/DEFAULT)]
+    (.getIndex resp)))
+
+(defn -upsert-document
+  [this index-name doc-type doc-id source]
+  (let [req (document/upsert index-name doc-type doc-id source)
+        resp (.update this req RequestOptions/DEFAULT)]
+    (.getIndex resp)))
 
 ;; Indices API
 
@@ -93,16 +135,21 @@
       (.isAcknowledged resp))))
 
 (def behaviour
-  {:create-index -create-index
+  {:bulk -bulk
+   :create-index -create-index
    :close #(.close %)
    :cluster -cluster
+   :delete-document -delete-document
    :delete-index -delete-index
    :delete-pipeline -delete-pipeline
    :get-pipeline -get-pipeline
    :health -health
    :indices -indices
+   :index -index
    :ingest -ingest
-   :put-pipeline -put-pipeline})
+   :put-pipeline -put-pipeline
+   :update-document -update-document
+   :upsert-document -upsert-document})
 
 (extend RestHighLevelClient
         ElasticsearchAPI
---
+++ src/cmr/nlp/elastic/client/document.clj
@@ -0,0 +1,49 @@
+(ns cmr.nlp.elastic.client.document
+  (:require
+   [cheshire.core :as json]
+   [taoensso.timbre :as log])
+  (:import
+   (clojure.lang Keyword)
+   (java.lang String)
+   (org.elasticsearch.action.bulk BulkRequest)
+   (org.elasticsearch.action.delete DeleteRequest)
+   (org.elasticsearch.action.index IndexRequest)
+   (org.elasticsearch.action.update UpdateRequest)
+   (org.elasticsearch.common.xcontent XContentType))
+  (:refer-clojure :exclude [index update]))
+
+(defn index
+  [^String index-name ^String doc-type ^String doc-id ^String source]
+  (let [req (new IndexRequest index-name doc-type doc-id)]
+    (.source req source XContentType/JSON)
+    req))
+
+(defn delete
+  [^String index-name ^String doc-type ^String doc-id]
+  (new DeleteRequest index-name doc-type doc-id))
+
+(defn update
+  [^String index-name ^String doc-type ^String doc-id ^String source]
+  (let [req (new UpdateRequest index-name doc-type doc-id)]
+    (.doc req source XContentType/JSON)
+    req))
+
+(defn upsert
+  [^String index-name ^String doc-type ^String doc-id ^String source]
+  (let [req (new UpdateRequest index-name doc-type doc-id)]
+    (.upsert req source XContentType/JSON)
+    req))
+
+(defn- request
+  [^Keyword request-type & args]
+  (case request-type
+    :index (apply index args)
+    :delete (apply delete args)
+    :update (apply update args)
+    :upsert (apply upsert args)))
+
+(defn bulk
+  [reqs-data]
+  (let [req (new BulkRequest)]
+    (mapv #(.add req %) reqs-data)
+    req))
