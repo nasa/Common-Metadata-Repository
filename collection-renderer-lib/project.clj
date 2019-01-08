178ffd91eebe0ab17cb28bc8a9906e814b6f70e3
Daniel Zamora <daniel.p.zamora@nasa.gov>
2019-01-08 10:40:04 -0500

    CMR-4784 update cmr_metadata_preview commit

--- collection-renderer-lib/project.clj
+++ collection-renderer-lib/project.clj
@@ -14,7 +14,7 @@
    the hardcoded commit id during dev integration with cmr_metadata_preview project.
    The hardcoded commit id should be updated when MMT releases a new version of the gem."
   (or (System/getenv "CMR_METADATA_PREVIEW_COMMIT")
-      "72fe8b4a178"))
+      "991048ffbcb"))
 
 (def gem-install-path
   "The directory within this library where Ruby gems are installed."
