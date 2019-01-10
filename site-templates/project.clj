991b70833b903b2163dd982a35a432cad835e8d0
Chris Durbin <christopher.d.durbin@nasa.gov>
2019-01-10 06:07:09 -0500

    CMR-5389: Update site templates to Clojure 1.10.

--- site-templates/project.clj
+++ site-templates/project.clj
@@ -5,7 +5,7 @@
     :name "Apache License, Version 2.0"
     :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [
-    [org.clojure/clojure "1.8.0"]]
+    [org.clojure/clojure "1.10.0"]]
   :profiles {
     :test {
       :plugins [
