b9325c5cda56e9494bfd79b029a19e2b3bed9acf
Mark Schmele <MSchmele@gmail.com>
2019-01-11 15:58:30 -0500

    CMR-5366: Fix error being caused by ISO MENDS collection RelatedUrls (#647)
    * CMR-5366: Fix error being caused by ISO MENDS collection RelatedUrls
    
    * CMR-5366: PR feedback

--- umm-spec-lib/test/cmr/umm_spec/test/xml_to_umm_mappings/iso_shared/distributions_related_url.clj
+++ umm-spec-lib/test/cmr/umm_spec/test/xml_to_umm_mappings/iso_shared/distributions_related_url.clj
@@ -2,6 +2,7 @@
   (:require
    [clojure.java.io :as io]
    [clojure.test :refer :all]
+   [cmr.common.util :refer [are3]]
    [cmr.umm-spec.xml-to-umm-mappings.iso-shared.distributions-related-url :as sru]
    [cmr.umm-spec.xml-to-umm-mappings.iso-smap.distributions-related-url :as smap-ru]
    [cmr.umm-spec.xml-to-umm-mappings.iso19115-2.distributions-related-url :as mends-ru]))
@@ -74,6 +75,72 @@
         :Checksum nil,
         :MimeType nil}}))
 
+(def expected-distribution-related-url-record-CMR-5366
+  '({:URL "https://nsidc.org/daac/subscriptions.html",
+     :URLContentType "DistributionURL",
+     :Type "GET DATA",
+     :Subtype "Subscribe",
+     :Description
+     "Subscribe to have new data automatically sent when the data become available.",
+     :GetData
+     {:Format "Not provided",
+      :Size 0.0,
+      :Unit "KB",
+      :Fees nil,
+      :Checksum nil,
+      :MimeType nil}}
+    {:URL "https://n5eil01u.ecs.nsidc.org/MOST/MOD10A1.061/",
+     :URLContentType "DistributionURL",
+     :Type "GET DATA",
+     :Subtype "DIRECT DOWNLOAD",
+     :Description "Direct download via HTTPS protocol.",
+     :GetData
+     {:Format "Not provided",
+      :Size 0.0,
+      :Unit "KB",
+      :Fees nil,
+      :Checksum nil,
+      :MimeType nil}}
+    {:URL "https://search.earthdata.nasa.gov/search?q=MOD10A1",
+     :URLContentType "DistributionURL",
+     :Type "GET DATA",
+     :Subtype "Earthdata Search",
+     :Description
+     "NASA's newest search and order tool for subsetting, reprojecting, and reformatting data.",
+     :GetData
+     {:Format "Not provided",
+      :Size 0.0,
+      :Unit "KB",
+      :Fees nil,
+      :Checksum nil,
+      :MimeType nil}}
+    {:URL "https://search.earthdata.nasa.gov/search?q=MOD10A1",
+     :URLContentType "DistributionURL",
+     :Type "GET DATA",
+     :Subtype "Earthdata Search",
+     :Description
+     "NASA's newest search and order tool for subsetting, reprojecting, and reformatting data 2.",
+     :GetData
+     {:Format "Not provided",
+      :Size 0.0,
+      :Unit "KB",
+      :Fees nil,
+      :Checksum "A12345F",
+      :MimeType nil}}
+    {:URL "https://search.earthdata.nasa.gov/search?q=MOD10A1",
+     :URLContentType "DistributionURL",
+     :Type "GET DATA",
+     :Subtype nil,
+     :Description
+     "NASA's newest search and order tool for subsetting, reprojecting, and reformatting data 3.",
+     :GetData
+     {:Format "Not provided",
+      :Size 0.0,
+      :Unit "KB",
+      :Fees nil,
+      :Checksum nil,
+      :MimeType nil}}))
+
 (defn- distribution-related-url-iso-mends-record
   "Returns an example ISO19115 metadata record that includes multiple related urls
    for multiple transfterOptions and multiple online resources."
@@ -85,6 +152,12 @@
    for multiple transfterOptions and multiple online resources."
   []
   (slurp (io/resource "example-data/iso-smap/SMAPExample.xml")))
+
+(defn- distribution-related-url-iso-mends-arc-error-record
+  "Returns an example iso mends record that caused internal errors while parsing.
+   It contains multiple related urls."
+  []
+  (slurp (io/resource "example-data/error-cases/CMR-5366.xml")))
 
 (deftest iso-mends-multiple-distributed-related-url-test
   (testing "The the software that checks multiple related urls for multiple distributors,
@@ -101,3 +174,31 @@
           doc (distribution-related-url-iso-smap-record)]
       (is (= expected-distribution-related-url-record
             (sru/parse-online-urls doc sanitize? smap-ru/service-url-path smap-ru/distributor-xpaths-map))))))
+
+(deftest iso-mends-test-CMR-5366
+  (testing "Check error case when parsing RelatedUrls that causes negative index error."
+    (let [sanitize? true
+          doc (distribution-related-url-iso-mends-arc-error-record)]
+      (is (= expected-distribution-related-url-record-CMR-5366
+             (sru/parse-online-urls doc sanitize? mends-ru/service-url-path mends-ru/distributor-xpaths-map))))))
+
+(deftest description-string-parsing
+  (testing "Parsing given string and converting it to a map"
+    (are3 [string regex expected]
+      (is (= expected (sru/convert-iso-description-string-to-map string regex)))
+
+      "ISO MENDS Collection Description string"
+      "URLContentType: DistributionURL Description: NASA's newest search and order tool for subsetting, reprojecting, and reformatting data. Type: GET DATA Subtype: Earthdata Search"
+      (re-pattern "URLContentType:|Description:|Type:|Subtype:|Checksum:")
+      {"Type" "GET DATA",
+       "URLContentType" "DistributionURL",
+       "Description" "NASA's newest search and order tool for subsetting, reprojecting, and reformatting data.",
+       "Subtype" "Earthdata Search"}
+
+      "String with odd and nil values"
+      ":: URLContentType:nil Checksum: \"nil\" Description: NASA's newest lawnmower ascii art: __\\.-.,,,, Type: SELF PROPELLED Subtype: Earthdata Search"
+      (re-pattern "URLContentType:|Description:|Type:|Subtype:|Checksum:")
+      {"Checksum" "\"nil\"",
+       "Description" "NASA's newest lawnmower ascii art: __\\.-.,,,,",
+       "Type" "SELF PROPELLED",
+       "Subtype" "Earthdata Search"})))
