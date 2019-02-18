72822fd45ca3a7b31fd0f287f0ff19b4b03d9480
Cayvon Hamidizadeh <cayvon.hamidizadeh@nasa.gov>
2019-02-18 13:41:35 -0500

    CMR-5493: Split out schema validation lib.

---
+++ schema-validation-lib/dev/user.clj
@@ -0,0 +1,14 @@
+(ns user
+  (:require [clojure.pprint :refer (pprint pp)]
+            [clojure.tools.namespace.repl :refer (refresh refresh-all)]
+            ;; Make Proto REPL lib properties available.
+            [proto-repl.saved-values])
+  (:use [clojure.test :only [run-all-tests]]
+        [clojure.repl]
+        ;; Needed to make debug-repl available
+        [alex-and-georges.debug-repl]))
+
+(defn reset []
+  (refresh))
+
+(println "Custom user.clj loaded.")
