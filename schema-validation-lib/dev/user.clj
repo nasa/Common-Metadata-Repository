49a519b36ac83c09095f2327334d1f100eb334e5
Cayvon Hamidizadeh <cayvon.hamidizadeh@nasa.gov>
2019-02-19 09:25:37 -0500

    CMR-5493: Cleanup user namespace.

--- schema-validation-lib/dev/user.clj
+++ schema-validation-lib/dev/user.clj
@@ -1,12 +1,13 @@
 (ns user
-  (:require [clojure.pprint :refer (pprint pp)]
-            [clojure.tools.namespace.repl :refer (refresh refresh-all)]
-            ;; Make Proto REPL lib properties available.
-            [proto-repl.saved-values])
-  (:use [clojure.test :only [run-all-tests]]
-        [clojure.repl]
-        ;; Needed to make debug-repl available
-        [alex-and-georges.debug-repl]))
+  (:require
+   ;; Needed to make debug-repl available
+   [alex-and-georges.debug-repl]
+   [clojure.pprint :refer [pprint pp]]
+   [clojure.repl]
+   [clojure.test :only [run-all-tests]]
+   [clojure.tools.namespace.repl :refer [refresh refresh-all]]
+   ;; Make Proto REPL lib properties available.
+   [proto-repl.saved-values]))
 
 (defn reset []
   (refresh))
