3140b9e6f1a902928d5565d4b83fce4a4774bceb
Cayvon Hamidizadeh <cayvon.hamidizadeh@nasa.gov>
2019-03-26 17:30:37 -0400

    CMR-5320: metadata-db vulnerability updates.
    * Use only needed aws-java-sdk packages in message-queue-lib.
    * Update quartz in metadata-db.
    * Suppress es, mintToken, clansi, and oracle vulnerabilities.

--- metadata-db-app/project.clj
+++ metadata-db-app/project.clj
@@ -25,7 +25,7 @@
                  [org.clojure/clojure "1.10.0"]
                  [org.clojure/tools.nrepl "0.2.13"]
                  [org.clojure/tools.reader "1.3.2"]
-                 [org.quartz-scheduler/quartz "2.3.0"]
+                 [org.quartz-scheduler/quartz "2.3.1"]
                  [org.slf4j/slf4j-api "1.7.25"]
                  [ring/ring-core "1.7.1"]
                  [ring/ring-json "0.4.0"]]
