24edbf1de26f9e0f2fc5f9346ff8396dd518264d
Aimee Barciauskas <aimee@developmentseed.org>
2019-04-08 11:27:47 -0400

    Fix test and add comment

--- transmit-lib/test/cmr/transmit/test/echo/rest.clj
+++ transmit-lib/test/cmr/transmit/test/echo/rest.clj
@@ -5,6 +5,10 @@
 
 (deftest test-error-masked
   (let [error-message "Unexpected error message: Token 123 does not exist."
-       status 500]
-    (is (= (format "Unexpected status %d from response. body: %s" status "Token does not exist")
-           (rest/unexpected-status-error! status error-message)))))
+        status 500]
+    (is (thrown-with-msg?
+        java.lang.Exception
+        ;; Fixme - (format "%d %s" status message) was getting the error:
+        ;; java.lang.ClassCastException: java.lang.String cannot be cast to java.util.regex.Pattern
+        #"Unexpected status 500 from response. body: Token does not exist"
+        (rest/unexpected-status-error! status error-message)))))
