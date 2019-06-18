799dfd23a3190d84b4be61295ac57dca21f5b6c4
mschmele <mschmele@gmail.com>
2019-06-18 14:55:14 -0400

    CMR-5745: add browse-scaler lambda function

---
+++ browse-scaler/src/__test__/resize.test.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const sharp = require('sharp');
+const { resizeImage, notFound } = require('../resize');
+
+describe('Image resizing', () => {
+  test('Images are resized as expected', async () => {
+    const image = fs.readFileSync('stars.jpg');
+
+    const imageResponse = await resizeImage(image, 200, 200);
+    fs.writeFileSync('stars-resize.png', imageResponse, { encoding: 'base64' });
+
+    const imgDimensions = await sharp('stars-resize.png').metadata();
+    expect(imgDimensions.width).toBe(200);
+    expect(imgDimensions.height).toBe(200);
+
+    fs.unlinkSync('stars-resize.png');
+  });
+
+  test('Images aspect ratios are maintained when image sizes do not match', async () => {
+    const image = fs.readFileSync('desk_flip.jpg');
+
+    const imageResponse = await resizeImage(image, 458, 358);
+    fs.writeFileSync('desk-flip-resize.png', imageResponse, { encoding: 'base64' });
+
+    const dimensions = await sharp('desk-flip-resize.png').metadata();
+    expect(dimensions.width).toBe(358);
+    expect(dimensions.height).toBe(358);
+
+    fs.unlinkSync('desk-flip-resize.png');
+  });
+
+  test('Resize image to be smaller than original, then larger than original', async () => {
+    const image = fs.readFileSync('desk_flip.jpg');
+
+    /* Make Image Smaller */
+    const smallerImage = await resizeImage(image, 75, 90);
+    fs.writeFileSync('desk-flip-smaller.png', smallerImage, { encoding: 'base64' });
+
+    const smallerDimensions = await sharp('desk-flip-smaller.png').metadata();
+    expect(smallerDimensions.width).toBe(75);
+    expect(smallerDimensions.height).toBe(75);
+
+    fs.unlinkSync('desk-flip-smaller.png');
+
+    /* Make Image Larger */
+    const largerImage = await resizeImage(image, 600, 600);
+    fs.writeFileSync('desk-flip-larger.png', largerImage, { encoding: 'base64' });
+
+    const largerDimensions = await sharp('desk-flip-larger.png').metadata();
+    expect(largerDimensions.width).toBe(600);
+    expect(largerDimensions.height).toBe(600);
+
+    fs.unlinkSync('desk-flip-larger.png');
+  });
+});
---
+++ browse-scaler/src/resize.js
@@ -0,0 +1,45 @@
+const sharp = require('sharp');
+const fetch = require('isomorphic-unfetch');
+
+exports.slurpImageIntoBuffer = async imageUrl => {
+  const thumbnail = await fetch(imageUrl)
+    .then(response => {
+      if (response.ok) {
+        return response;
+      }
+      return Promise.reject(
+        new Error(`Failed to fetch ${response.url}: ${response.status} ${response.statusText}`)
+      );
+    })
+    .then(response => response.buffer());
+
+  return thumbnail;
+};
+
+exports.resizeImage = async (image, height, width) => {
+  // If an image needs to be resized, it is because it was not available
+  // in cache, so we will always want to cache that image
+  try {
+    const thumbnail = await sharp(image)
+      .resize({
+        width,
+        height,
+        fit: 'inside'
+      })
+      .toFormat('png')
+      .toBuffer();
+
+    return thumbnail.toString('base64');
+  } catch (err) {
+    console.log(`Could not resize image: ${err}`);
+    return null;
+  }
+};
+
+exports.notFound = async (height, width) => {
+  const notFoundSvg = sharp('image-unavailable.svg')
+    .resize(width, height)
+    .toBuffer();
+
+  return notFoundSvg.toString('base64');
+};
