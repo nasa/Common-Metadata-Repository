a269a695f064b05476e6634e6fdea2e5083b493b
mschmele <mschmele@gmail.com>
2019-06-25 16:23:19 -0400

    CMR-5774: update routes and parameter handling

--- browse-scaler/src/index.js
+++ browse-scaler/src/index.js
@@ -1,24 +1,31 @@
-const { slurpImageIntoBuffer, resizeImage, notFound } = require('./resize');
-const { getCollectionLevelBrowseImage, getGranuleLevelBrowseImage } = require('./cmr');
-const { cacheImage, getImageFromCache } = require('./cache');
+const { slurpImageIntoBuffer, resizeImage, notFound } = require("./resize");
+const {
+  getCollectionLevelBrowseImage,
+  getGranuleLevelBrowseImage
+} = require("./cmr");
+const { cacheImage, getImageFromCache } = require("./cache");
 
 const buildResponse = (image, format) => {
   return {
     statusCode: 200,
     headers: {
-      'Content-Type': format
+      "Content-Type": format
     },
     body: image,
     isBase64Encoded: true
   };
 };
 
-const getImageUrlFromConcept = async conceptId => {
-  if (conceptId.startsWith('G')) {
+const getImageUrlFromConcept = async (conceptId, conceptType) => {
+  if (conceptId === null || conceptId === undefined) {
+    return null;
+  }
+
+  if (conceptType === "granules") {
     return getGranuleLevelBrowseImage(conceptId);
   }
 
-  if (conceptId.startsWith('C')) {
+  if (conceptType === "datasets") {
     return getCollectionLevelBrowseImage(conceptId);
   }
 
@@ -26,41 +33,74 @@ const getImageUrlFromConcept = async conceptId => {
   return null;
 };
 
-const resizeImageFromConceptId = async (conceptId, height, width) => {
+const resizeImageFromConceptId = async (
+  conceptType,
+  conceptId,
+  height,
+  width
+) => {
   // If given an image url, fetch the image and resize. If no image
   // exists, return the not found response
-  const imageUrl = await getImageUrlFromConcept;
+  const imageUrl = await getImageUrlFromConcept(conceptId, conceptType);
 
   if (imageUrl === null) {
-    const cachedSvg = await getImageFromCache('NOT-FOUND');
+    const cachedSvg = await getImageFromCache("NOT-FOUND");
     if (cachedSvg) {
-      return buildResponse(cachedSvg, 'image/svg');
+      return buildResponse(cachedSvg, "image/svg");
     }
 
     const imgNotFound = notFound(height, width);
-    cacheImage('NOT-FOUND', imgNotFound);
+    cacheImage("NOT-FOUND", imgNotFound);
 
-    return buildResponse(imgNotFound, 'image/svg');
+    return buildResponse(imgNotFound, "image/svg");
   }
 
   const cacheKey = `${conceptId}-${height}-${width}`;
   const imageFromCache = await getImageFromCache(cacheKey);
   if (imageFromCache) {
-    return buildResponse(imageFromCache, 'image/png');
+    return buildResponse(imageFromCache, "image/png");
   }
 
   const imageBuffer = await slurpImageIntoBuffer(imageUrl);
   const thumbnail = resizeImage(imageBuffer, height, width);
   cacheImage(cacheKey, thumbnail);
 
-  return buildResponse(thumbnail, 'image/png');
+  return buildResponse(thumbnail, "image/png");
 };
 
-exports.handler = async event => {
-  const conceptId = event.queryStringParameters.concept_id;
-  const { height } = event.queryStringParameters;
-  const { width } = event.queryStringParameters;
+const parseArguments = async event => {
+  const pathParams = event.path
+    .split("/")
+    .filter(param => param !== "browse-scaler" && param !== "browse_images");
 
-  console.log(`Attempting to resize browse image for concept: ${conceptId}`);
-  return resizeImageFromConceptId(conceptId, height, width);
+  const arguments = {
+    conceptType: pathParams[1],
+    conceptId: pathParams[2],
+    h: event.queryStringParameters.h,
+    w: event.queryStringParameters.w
+  };
+
+  if (arguments.conceptId === null) {
+    throw new Error("Please supply a concept id");
+  }
+
+  if (arguments.h === null && arguments.w === null) {
+    throw new Error("Please supply at least a height or a width");
+  }
+
+  return arguments;
+};
+
+exports.handler = async (event, context) => {
+  const arguments = parseArguments(event);
+
+  console.log(
+    `Attempting to resize browse image for concept: ${arguments.conceptId}`
+  );
+  return resizeImageFromConceptId(
+    arguments.conceptType,
+    arguments.conceptId,
+    arguments.h,
+    arguments.w
+  );
 };
