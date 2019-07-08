4f673a3e14262c7c29504be71b301616b93cac14
mschmele <mschmele@gmail.com>
2019-07-08 12:36:15 -0400

    CMR-5774: fix ESLint and Prettier configuration, lint files

--- browse-scaler/src/index.js
+++ browse-scaler/src/index.js
@@ -1,9 +1,6 @@
-const { slurpImageIntoBuffer, resizeImage, notFound } = require("./resize");
-const {
-  getCollectionLevelBrowseImage,
-  getGranuleLevelBrowseImage
-} = require("./cmr");
-const { cacheImage, getImageFromCache } = require("./cache");
+const { slurpImageIntoBuffer, resizeImage, notFound } = require('./resize');
+const { getCollectionLevelBrowseImage, getGranuleLevelBrowseImage } = require('./cmr');
+const { cacheImage, getImageFromCache } = require('./cache');
 
 const buildResponse = (image, format) => {
   console.log(`Image for response: ${image}`);
@@ -11,7 +8,7 @@ const buildResponse = (image, format) => {
   return {
     statusCode: 200,
     headers: {
-      "Content-Type": format
+      'Content-Type': format
     },
     body: image,
     isBase64Encoded: true
@@ -24,11 +21,11 @@ const getImageUrlFromConcept = async (conceptId, conceptType) => {
     return null;
   }
 
-  if (conceptType === "granules") {
+  if (conceptType === 'granules') {
     return getGranuleLevelBrowseImage(conceptId);
   }
 
-  if (conceptType === "datasets") {
+  if (conceptType === 'datasets') {
     return getCollectionLevelBrowseImage(conceptId);
   }
 
@@ -36,12 +33,7 @@ const getImageUrlFromConcept = async (conceptId, conceptType) => {
   return null;
 };
 
-const resizeImageFromConceptId = async (
-  conceptType,
-  conceptId,
-  height,
-  width
-) => {
+const resizeImageFromConceptId = async (conceptType, conceptId, height, width) => {
   // If given an image url, fetch the image and resize. If no image
   // exists, return the not found response
   const imageUrl = await getImageUrlFromConcept(conceptId, conceptType);
@@ -55,7 +47,7 @@ const resizeImageFromConceptId = async (
     const imgNotFound = await notFound(height, width);
     // cacheImage("NOT-FOUND", imgNotFound);
 
-    return buildResponse(imgNotFound, "image/svg");
+    return buildResponse(imgNotFound, 'image/svg');
   }
 
   // const cacheKey = `${conceptId}-${height}-${width}`;
@@ -68,16 +60,13 @@ const resizeImageFromConceptId = async (
   const thumbnail = await resizeImage(imageBuffer, height, width);
   // cacheImage(cacheKey, thumbnail);
 
-  return buildResponse(thumbnail, "image/png");
+  return buildResponse(thumbnail, 'image/png');
 };
 
 const parseArguments = event => {
   const pathParams = event.path
-    .split("/")
-    .filter(
-      param =>
-        param !== "browse-scaler" && param !== "browse_images" && param !== ""
-    );
+    .split('/')
+    .filter(param => param !== 'browse-scaler' && param !== 'browse_images' && param !== '');
 
   const args = {
     conceptType: pathParams[0],
@@ -87,11 +76,11 @@ const parseArguments = event => {
   };
 
   if (args.conceptId === null) {
-    throw new Error("Please supply a concept id");
+    throw new Error('Please supply a concept id');
   }
 
   if (args.h === null && args.w === null) {
-    throw new Error("Please supply at least a height or a width");
+    throw new Error('Please supply at least a height or a width');
   }
 
   return args;
@@ -99,14 +88,7 @@ const parseArguments = event => {
 
 exports.handler = async (event, context) => {
   const args = parseArguments(event);
-  console.log(
-    `Attempting to resize browse image for concept: ${JSON.stringify(args)}`
-  );
+  console.log(`Attempting to resize browse image for concept: ${JSON.stringify(args)}`);
 
-  return resizeImageFromConceptId(
-    args.conceptType,
-    args.conceptId,
-    args.h,
-    args.w
-  );
+  return resizeImageFromConceptId(args.conceptType, args.conceptId, args.h, args.w);
 };
