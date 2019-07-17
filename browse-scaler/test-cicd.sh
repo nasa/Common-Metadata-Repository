c7f4fef31cf5d819fcac5eafa81b28fe9fd00b3d
Cayvon Hamidizadeh <cayvon.hamidizadeh@nasa.gov>
2019-07-17 11:29:38 -0400

    CMR-5683: Rename file. Cleanup image after test.

---
+++ browse-scaler/test-cicd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Runs tests in docker and saves output as junit.xml
+
+echo "Running tests..."
+(cd src && docker-compose -f docker-compose.test.yml run --rm browse-scaler-cicd)
+
+# Cleanup image
+echo "Cleaning up image..."
+docker rmi -f \
+  $(docker inspect browse-scaler:test-cicd \
+  | jq -r .[].Id \
+  | cut -f2 -d: \
+  )
