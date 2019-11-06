59a33a67ee421c0e6dffb30a5b578b674cb68952
Ryan Miller <ryan.miller-1@nasa.gov>
2019-11-06 11:26:47 -0500

    MMT-2039  Updating the dependencies in the preview gem.

--- collection-renderer-lib/project.clj
+++ collection-renderer-lib/project.clj
@@ -14,7 +14,7 @@
    the hardcoded commit id during dev integration with cmr_metadata_preview project.
    The hardcoded commit id should be updated when MMT releases a new version of the gem."
   (or (System/getenv "CMR_METADATA_PREVIEW_COMMIT")
-      "03072e2c727"))
+      "5eaeaafefa8"))
 
 (def gem-install-path
   "The directory within this library where Ruby gems are installed."
