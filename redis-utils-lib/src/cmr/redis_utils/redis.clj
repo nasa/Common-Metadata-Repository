b2778b1e7f6b99c5e87d4cfff779ea6243c93690
Cayvon Hamidizadeh <cayvon.hamidizadeh@nasa.gov>
2019-11-12 17:13:59 -0500

    CMR-6023: Persist KMS and humanizers.
    * Make redis calls safe.

--- redis-utils-lib/src/cmr/redis_utils/redis.clj
+++ redis-utils-lib/src/cmr/redis_utils/redis.clj
@@ -1,14 +1,18 @@
 (ns cmr.redis-utils.redis
   "Used to implement functions for calling Redis."
   (:require
+   [cmr.common.log :refer [error]]
    [cmr.redis-utils.config :as config]
    [taoensso.carmine :as carmine :refer [wcar]]))
 
 (defmacro wcar*
-  "Call redis with conn opts. For available commands refer to Redis docuemntation or:
-  https://github.com/ptaoussanis/carmine/blob/master/src/taoensso/carmine/commands.edn"
+  "Safe call redis with conn opts. For available commands refer to Redis docuemntation or:
+  https://github.com/ptaoussanis/carmine/blob/master/src/taoensso/carmine/commands.edn."
   [& body]
-  `(carmine/wcar (config/redis-conn-opts) ~@body))
+  `(try
+     (carmine/wcar (config/redis-conn-opts) ~@body)
+     (catch Exception e#
+       (error "Redis failed with exception " e#))))
 
 (defn healthy?
   "Returns true if able to reach Redis."
