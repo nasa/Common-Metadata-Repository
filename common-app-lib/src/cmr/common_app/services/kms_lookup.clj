554c6818552c7b82288aa099e72fce187a95aaed
Mark Schmele <mark.h.schmele@nasa.gov>
2019-11-26 08:15:13 -0500

    Revert "CMR-5920: add granuledataformat, fix some tests"
    This reverts commit 76b73374b19222d2217d4c339b257c5d500729dc.

--- common-app-lib/src/cmr/common_app/services/kms_lookup.clj
+++ common-app-lib/src/cmr/common_app/services/kms_lookup.clj
@@ -34,8 +34,7 @@
    :providers [:short-name]
    :spatial-keywords [:category :type :subregion-1 :subregion-2 :subregion-3]
    :concepts [:short-name]
-   :iso-topic-categories [:iso-topic-category]
-   :granule-data-format [:archive-and-distribution-information :file-distribution-information :format]})
+   :iso-topic-categories [:iso-topic-category]})
 
 (defn- normalize-for-lookup
   "Takes a map (either a UMM-C keyword or a KMS keyword) or string m,
