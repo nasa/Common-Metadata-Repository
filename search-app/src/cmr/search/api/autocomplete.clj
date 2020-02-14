2f487d845ab89ba9091254785dc605c0a6784785
Jay Barra <jay.barra@nasa.gov>
2020-02-14 10:35:53 -0500

    CMR-6161 autocomplete api calls

--- search-app/src/cmr/search/api/autocomplete.clj
+++ search-app/src/cmr/search/api/autocomplete.clj
@@ -1,7 +1,10 @@
 (ns cmr.search.api.autocomplete
   "Defines the API for autocomplete-suggest in the CMR."
   (:require
+    [cmr.common.log :refer (debug info warn error)]
+    [cmr.common.services.errors :as svc-errors]
     [cmr.search.services.autocomplete-service :as ac]
+    [cmr.search.services.query-service :as qs]
     [cmr.common-app.api.routes :as common-routes]
     [cmr.common.mime-types :as mt]
     [compojure.core :refer :all]))
@@ -11,22 +14,24 @@
 
 (defn- get-autocomplete-suggestions
   "Invokes Elasticsearch for autocomplete query result"
-  [query types]
-  (if query
-    {:status  200
-     :headers {common-routes/CONTENT_TYPE_HEADER (str (mt/format->mime-type :json) "; charset=utf-8")}
-     :body    {:query   query
-               :results (ac/autocomplete query)}}
-    {:status  400
-     :headers {common-routes/CONTENT_TYPE_HEADER (str (mt/format->mime-type :json) "; charset=utf-8")}
-     :body    {:message "No query term provided"
-               :usage   "/autocomplete?q=<value>"}}))
+  [ctx params]
+  (let [term (:q params)]
+    (if term
+      (let [results (ac/autocomplete ctx term)]
+        {:status  200
+         :headers {common-routes/CONTENT_TYPE_HEADER (str (mt/format->mime-type :json) "; charset=utf-8")
+                   common-routes/CORS_ORIGIN_HEADER  "*"}
+         :body    {:query   term
+                   :results results}})
+      (svc-errors/throw-service-errors
+       :bad-request
+       ["Missing param [q]" "Usage [/autocomplete?q=<term>]"]))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Route Definitions
 
 (def autocomplete-api-routes
   (context "/autocomplete" []
-     (GET "/"
-        [q types]
-        (get-autocomplete-suggestions q types))))
+           (GET "/"
+                {params :params ctx :request-context}
+                (get-autocomplete-suggestions ctx params))))
