627e766e70d577b26a56fea63e3148aa142c9b85
Jay Barra <jay.barra@nasa.gov>
2020-04-01 08:06:59 -0400

    CMR-6217 updated tests

--- access-control-app/int-test/cmr/access_control/int_test/access_control_group_crud_test.clj
+++ access-control-app/int-test/cmr/access_control/int_test/access_control_group_crud_test.clj
@@ -286,8 +286,8 @@
         {:keys [concept_id revision_id]} (test-util/create-group token group1)
         group2-concept-id (:concept_id (test-util/create-group token group2))]
     (testing "Delete without token"
-      (is (= :status 401
-             :errors ["Valid user token required."]
+      (is (= {:status 401
+              :errors ["Valid user token required."]}
              (test-util/delete-group nil concept_id))))
 
     (testing "Delete success"
@@ -383,33 +383,19 @@
       (is (empty? (set (:items (test-util/search-for-acls token {:permitted_group group-1-concept-id}))))))))
 
 (deftest update-group-test
-  (testing "successful update"
-    (let [group (test-util/make-group {:members ["user1" "user2"]})
-          token (echo-util/login (test-util/conn-context) "user1")
-          {:keys [concept_id]} (test-util/create-group token group)]
+  (let [group (test-util/make-group {:members ["user1" "user2"]})
+        token (echo-util/login (test-util/conn-context) "user1")
+        {:keys [concept_id]} (test-util/create-group token group)]
 
-      ;; Do not specify members in the update
-      (let [updated-group {:name "Updated Group Name"
-                           :description "Updated group description"}
-            token2 (echo-util/login (test-util/conn-context) "user2")
-            response (test-util/update-group token2 concept_id updated-group)]
-        (is (= {:status 200 :concept_id concept_id :revision_id 2}
-               response))
-        ;; Members should be intact
-        (test-util/assert-group-saved (assoc updated-group :members ["user1" "user2"]) "user2" concept_id 2))))
-  
-  (testing "name collision updates should fail"
-    (let [group1 (test-util/make-group {:name "Group1" :members ["user1"]})
-          group2 (test-util/make-group {:name "Group2" :members ["user1"]})
-          updated-group2 {:name "Group1"
-                          :description "name collision expected"
-                          :members ["user1"]}
-          token (echo-util/login (test-util/conn-context) "user1")
-          {id1 :concept_id} (test-util/create-group token group1)
-          {id2 :concept_id} (test-util/create-group token group2)
-          _ (println token " " id1 " " id2)
-          response (test-util/update-group token id2 updated-group2)]
-      (is (= 400 (:status response))))))
+    ;; Do not specify members in the update
+    (let [updated-group {:name "Updated Group Name"
+                         :description "Updated group description"}
+          token2 (echo-util/login (test-util/conn-context) "user2")
+          response (test-util/update-group token2 concept_id updated-group)]
+      (is (= {:status 200 :concept_id concept_id :revision_id 2}
+             response))
+      ;; Members should be intact
+      (test-util/assert-group-saved (assoc updated-group :members ["user1" "user2"]) "user2" concept_id 2))))
 
 (deftest update-group-with-members-test
   (let [group (test-util/make-group {:members ["user1" "user2"]})
@@ -426,8 +412,10 @@
 
 (deftest update-group-failure-test
   (let [group (test-util/make-group)
+        group2 (test-util/make-group {:name "Group2" :members ["user1"]})        
         token (echo-util/login (test-util/conn-context) "user1")
-        {:keys [concept_id revision_id]} (test-util/create-group token group)]
+        {:keys [concept_id revision_id]} (test-util/create-group token group)
+        {concept_id2 :concept_id} (test-util/create-group token group2)]
 
     (testing "Update group with invalid content type"
       (is (= {:status 400,
@@ -465,7 +453,15 @@
       (test-util/delete-group token concept_id)
       (is (= {:status 404
               :errors [(format "Group with concept id [%s] was deleted." concept_id)]}
-             (test-util/update-group token concept_id group))))))
+             (test-util/update-group token concept_id group))))
+
+    (testing "Update a group with conflicting name"
+      (test-util/wait-until-indexed)
+      (is (= {:status 409
+              :errors [(format "A system group with name [%s] already exists with concept id [%s]."
+                               (:name group)
+                               concept_id)]}
+             (test-util/update-group token concept_id2 (assoc group2 :name (:name group))))))))
 
 (deftest update-group-legacy-guid-test
   (let [group1 (test-util/make-group {:legacy_guid "legacy_guid_1" :name "group1"})
