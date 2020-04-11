2dc17b29f720e12a2d6e79daddaa8925992538a6
Yonggang Liu <ygliuvt@gmail.com>
2020-04-11 00:28:33 -0400

    CMR-6138: Fixed cache api test.

--- system-int-test/test/cmr/system_int_test/admin/cache_api_test.clj
+++ system-int-test/test/cmr/system_int_test/admin/cache_api_test.clj
@@ -262,9 +262,9 @@
         (url/indexer-read-caches-url)
         "indexer-index-set-cache"
         "concept-mapping-types"
-        {:collection "collection"
-         :granule "granule"
-         :tag "tag"
-         :variable "variable"
-         :service "service"
-         :subscription "subscription"}))))
+        {:collection "properties"
+         :granule "properties"
+         :tag "properties"
+         :variable "properties"
+         :service "properties"
+         :subscription "properties"}))))
