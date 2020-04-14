59796a74083e0267513cbc2365887be61318f0f8
Jay Barra <jay.barra@nasa.gov>
2020-04-14 15:11:38 -0400

    CMR-6318 adding a multi-match type for autocomplete

--- common-app-lib/src/cmr/common_app/services/search/query_to_elastic.clj
+++ common-app-lib/src/cmr/common_app/services/search/query_to_elastic.clj
@@ -330,6 +330,14 @@
   (condition->elastic
    [{:keys [field value]} _]
    {:match {field value}})
+  
+  cmr.common_app.services.search.query_model.MultiMatchCondition
+  (condition->elastic
+    [{:keys [fields value opts]}]
+    (let [opts (dissoc opts :fields :query)]
+      {:multi_match (assoc {:fields fields
+                            :query value}
+                           opts)}))
 
   cmr.common_app.services.search.query_model.MatchFilterCondition
   (condition->elastic
