79567e1744cd31a773b92ca1ef7606dc144272ce
siwei xu <Siwei_xu@raytheon.com>
2020-06-12 20:23:53 -0400

    CMR-6415: Added debug to investigate failure in Bamboo

--- search-app/src/cmr/search/services/acls/subscription_acls.clj
+++ search-app/src/cmr/search/services/acls/subscription_acls.clj
@@ -4,6 +4,7 @@
    [cmr.common-app.services.search.group-query-conditions :as gc]
    [cmr.common-app.services.search.query-execution :as qe]
    [cmr.common-app.services.search.query-model :as qm]
+   [cmr.common.util :as util]
    [cmr.search.services.acls.acl-helper :as acl-helper]
    [cmr.transmit.config :as tc]))
 
@@ -16,7 +17,10 @@
   (if (tc/echo-system-token? context)
     query
     (let [;; esm-acls that grant read permission to the current user
+          _ (println "sids are: " (util/lazy-get context :sids))
           esm-acls (acl-helper/get-esm-acls-applicable-to-token context)
+          _ (println "esm-acls with read permission to the current user: " esm-acls)
           provider-ids (map #(get-in % [:provider-identity :provider-id]) esm-acls)
+          _ (println "provider-ids are: " provider-ids)
           acl-cond (qm/string-conditions :provider-id provider-ids true)]
       (update-in query [:condition] #(gc/and-conds [acl-cond %])))))
