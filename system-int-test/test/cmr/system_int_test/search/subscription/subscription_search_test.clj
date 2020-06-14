59abc9de0c7817932b6c20c8d533d4f616886e2f
siwei xu <Siwei_xu@raytheon.com>
2020-06-14 11:17:05 -0400

    CMR-6415: testing bamboo.

--- system-int-test/test/cmr/system_int_test/search/subscription/subscription_search_test.clj
+++ system-int-test/test/cmr/system_int_test/search/subscription/subscription_search_test.clj
@@ -18,14 +18,9 @@
                                        "provguid2" "PROV2" 
                                        "provguid3" "PROV3"
                                        "provguid4" "PROV4"})
+                ;; No read permission granted for any user for PROV3
                 (subscriptions/grant-all-subscription-fixture
-                  {"provguid1" "PROV1" "provguid2" "PROV2"} [:read :update] [:read :update])
-                ;; No read permission granted for guest for PROV3
-                (subscriptions/grant-all-subscription-fixture
-                  {"provguid3" "PROV3"} [:update] [:update])
-                ;; No read permission granted for any user_types for PROV4.
-                (subscriptions/grant-all-subscription-fixture
-                  {"provguid4" "PROV4"} [:update] [:update])]))
+                  {"provguid3" "PROV3"} [:update] [:update])]))
 
 (deftest search-for-subscriptions-user-read-permission-test
   ;; EMAIL_SUBSCRIPTION_MANAGEMENT ACL grants only update permission for guest on PROV3,
@@ -46,36 +41,7 @@
           (subscriptions/assert-subscription-search expected-subscriptions (subscriptions/search-json query))))
 
       "Find all returns nothing for guest"
-      [] {}
+      [] {:token guest-token}
 
-      "Find all returns all for registered user"
-      [subscription3] {:token user1-token})))
-
-(deftest search-for-subscriptions-group-read-permission-test
-  (let [subscription4 (subscriptions/ingest-subscription-with-attrs {:native-id "Sub4"
-                                                                     :Name "Subscription4"
-                                                                     :SubscriberId "SubId4"
-                                                                     :CollectionConceptId "C1-PROV4"
-                                                                     :provider-id "PROV4"})
-        ;; create two groups
-        group1-concept-id (echo-util/get-or-create-group (system/context) "group1")
-        group2-concept-id (echo-util/get-or-create-group (system/context) "group2")
-        ;; create two user tokens, associated with two groups
-        user1g-token (echo-util/login (system/context) "user1g" [group1-concept-id])
-        user2g-token (echo-util/login (system/context) "user2g" [group2-concept-id])]
-    (index/wait-until-indexed)
-
-    ;; grant one group read permission
-    (echo-util/grant-all-subscription-group-esm (system/context) "PROV4" group1-concept-id [:read])
-    
-    (is (= 201 (:status subscription4)))
-    (are3 [expected-subscriptions query]
-      (do
-        (testing "JSON format"
-          (subscriptions/assert-subscription-search expected-subscriptions (subscriptions/search-json query))))
-
-      "Find all returns nothing for user2g"
-      [] {:token user2g-token}
-
-      "Find all returns all for user1g"
-      [subscription4] {:token user1g-token})))
+      "Find all returns nothing for registered user"
+      [] {:token user1-token})))
