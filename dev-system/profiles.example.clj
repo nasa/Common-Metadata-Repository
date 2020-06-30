8a322b592489b0f10cde16d71fc725fb0084c941
Jay Barra <jay.barra@nasa.gov>
2020-06-30 15:10:01 -0400

    fixed typo in documentation (#997)

--- dev-system/profiles.example.clj
+++ dev-system/profiles.example.clj
@@ -1,4 +1,5 @@
 {:dev-config {:env {:cmr-metadata-db-password "CHANGE_ME"
                     :cmr-sys-dba-password "CHANGE_ME"
                     :cmr-bootstrap-password "CHANGE_ME"
-                    :cmr-ingest-password "CHANGE_ME"}}}
+                    :cmr-ingest-password "CHANGE_ME"
+                    :cmr-urs-password "CHANGE_ME"}}}
