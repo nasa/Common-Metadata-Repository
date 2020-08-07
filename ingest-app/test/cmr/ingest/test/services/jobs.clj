ee373c23727987547d12ad929e92f171e66284e5
Thomas Cherry <thomas.a.cherry@nasa.gov>
2020-08-07 11:47:12 -0400

    CMR-6612 - moving to let instead of def

--- ingest-app/test/cmr/ingest/test/services/jobs.clj
+++ ingest-app/test/cmr/ingest/test/services/jobs.clj
@@ -12,31 +12,32 @@
 
 (deftest email-granule-url-list-test
  "This tests the utility function that unpacts a list of urls and turns it into markdown"
- (def actual (jobs/email-granule-url-list
-   '("https://cmr.link/g1" "https://cmr.link/g2" "https://cmr.link/g3")))
- (def expected (str "* [https://cmr.link/g1](https://cmr.link/g1)\n"
-  "* [https://cmr.link/g2](https://cmr.link/g2)\n"
-  "* [https://cmr.link/g3](https://cmr.link/g3)"))
- (is (= actual expected)))
+ (let [actual (jobs/email-granule-url-list '("https://cmr.link/g1"
+        "https://cmr.link/g2"
+        "https://cmr.link/g3"))
+       expected (str "* [https://cmr.link/g1](https://cmr.link/g1)\n"
+        "* [https://cmr.link/g2](https://cmr.link/g2)\n"
+        "* [https://cmr.link/g3](https://cmr.link/g3)")]
+  (is (= actual expected))))
 
 (deftest create-email-test
  "This tests the HTML output of the email generation"
- (def actual (jobs/create-email-content
-  "cmr-support@earthdata.nasa.gov"
-  "someone@gmail.com"
-  '("https://cmr.link/g1" "https://cmr.link/g2" "https://cmr.link/g3")
-  {:metadata {:SubscriberId "tcherry"}}))
- (are [exp act] (= exp act)
-  (:to actual) "someone@gmail.com"
-  (:subject actual) "Email Subscription Notification"
-  (:type (first (:body actual))) "text/html"
-  (:content (first (:body actual))) (str "<p>You have subscribed to receive "
-   "notifications when the following query is updated:</p>"
-   "<p>Since this query was last run at, the following granules have been "
-   "added or updated:</p>"
-   "<ul><li><a href='https://cmr.link/g1'>https://cmr.link/g1</a></li><li>"
-   "<a href='https://cmr.link/g2'>https://cmr.link/g2</a></li><li>"
-   "<a href='https://cmr.link/g3'>https://cmr.link/g3</a></li></ul>"
-   "<p>To unsubscribe from these notifications, or if you have any questions, "
-   "please contact us at <a href='mailto:cmr-support@earthdata.nasa.gov'>"
-   "cmr-support@earthdata.nasa.gov</a>.</p>")))
+ (let [actual (jobs/create-email-content
+   "cmr-support@earthdata.nasa.gov"
+   "someone@gmail.com"
+   '("https://cmr.link/g1" "https://cmr.link/g2" "https://cmr.link/g3")
+   {:metadata {:SubscriberId "tcherry"}})]
+  (are [exp act] (= exp act)
+   (:to actual) "someone@gmail.com"
+   (:subject actual) "Email Subscription Notification"
+   (:type (first (:body actual))) "text/html"
+   (:content (first (:body actual))) (str "<p>You have subscribed to receive "
+    "notifications when the following query is updated:</p>"
+    "<p>Since this query was last run at, the following granules have been "
+    "added or updated:</p>"
+    "<ul><li><a href='https://cmr.link/g1'>https://cmr.link/g1</a></li><li>"
+    "<a href='https://cmr.link/g2'>https://cmr.link/g2</a></li><li>"
+    "<a href='https://cmr.link/g3'>https://cmr.link/g3</a></li></ul>"
+    "<p>To unsubscribe from these notifications, or if you have any questions, "
+    "please contact us at <a href='mailto:cmr-support@earthdata.nasa.gov'>"
+    "cmr-support@earthdata.nasa.gov</a>.</p>"))))
