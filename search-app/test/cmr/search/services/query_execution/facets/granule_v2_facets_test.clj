4d137b6e88a5aee2b6d6508deb5c70f7b3a083c9
Jay Barra <jay.barra@nasa.gov>
2020-08-20 09:17:54 -0400

    uncommented tests

--- search-app/test/cmr/search/services/query_execution/facets/granule_v2_facets_test.clj
+++ search-app/test/cmr/search/services/query_execution/facets/granule_v2_facets_test.clj
@@ -31,86 +31,100 @@
                     "include_facets" "v2"
                     "collection_concept_id" "C1-PROV1"
                     "cycle[]" "42"}
-                   {:start-date-doc-values {:buckets []},
+                   {:start-date-doc-values {:buckets []}
                     :cycle
-                    {:doc_count 3,
+                    {:doc_count 3
                      :pass
                      {:buckets
-                      [{:key 1.0, :doc_count 1}
-                       {:key 3.0, :doc_count 1}
-                       {:key 4.0, :doc_count 1}]}}})
+                      [{:key 1.0 :doc_count 1}
+                       {:key 3.0 :doc_count 1}
+                       {:key 4.0 :doc_count 1}]}}})
           children (:children result)]
-      (clojure.pprint/pprint result)
-      (is (= "42" (:title (first children))))
-      (is (= {:remove "http://localhost:3003/granules.json?page_size=10&include_facets=v2&collection_concept_id=C1-PROV1&cycle%5B%5D=42"}
-             (:links (second children)))))))
+      (is (= "42" (:title children)))
+      (is (= {:remove          
+              "http://localhost:3003/granules.json?page_size=10&include_facets=v2&collection_concept_id=C1-PROV1"}
+             (:links children))))))
 
 (deftest create-v2-facets-by-concept-type-test
-  #_(testing "temporal"
-      (let [result (v2-facets/create-v2-facets-by-concept-type 
-                     :granule
-                     "http://localhost:3003/granules.json"
-                     {"page_size" "10"
-                      "include_facets" "v2"
-                      "collection_concept_id" "C1-PROV1"}
-                     {:start-date-doc-values
-                      {:buckets
-                       [{:key_as_string "1999-01-01T00:00:00.000Z"
-                         :key 915148800000
-                         :doc_count 1}
-                        {:key_as_string "2010-01-01T00:00:00.000Z"
-                         :key 1262304000000
-                         :doc_count 3}
-                        {:key_as_string "2011-01-01T00:00:00.000Z"
-                         :key 1293840000000
-                         :doc_count 1}
-                        {:key_as_string "2012-01-01T00:00:00.000Z"
-                         :key 1325376000000
-                         :doc_count 1}]}}
-                     nil)]
-        (is (= [{:title "Temporal"          
+  (testing "temporal"
+    (let [result (v2-facets/create-v2-facets-by-concept-type 
+                   :granule
+                   "http://localhost:3003/granules.json"
+                   {"page_size" "10"
+                    "include_facets" "v2"
+                    "collection_concept_id" "C1-PROV1"}
+                   {:start-date-doc-values
+                    {:buckets
+                     [{:key_as_string "1999-01-01T00:00:00.000Z"
+                       :key 915148800000
+                       :doc_count 1}
+                      {:key_as_string "2010-01-01T00:00:00.000Z"
+                       :key 1262304000000
+                       :doc_count 3}
+                      {:key_as_string "2011-01-01T00:00:00.000Z"
+                       :key 1293840000000
+                       :doc_count 1}
+                      {:key_as_string "2012-01-01T00:00:00.000Z"
+                       :key 1325376000000
+                       :doc_count 1}]}}
+                   nil)]
+      (is (= [{:title "Temporal"          
+               :type :group
+               :applied false
+               :has_children true
+               :children
+               [{:title "Year"
                  :type :group
                  :applied false
                  :has_children true
                  :children
-                 [{:title "Year"
-                   :type :group
-                   :applied false
-                   :has_children true
-                   :children
-                   '({:title "1999"
-                      :type :filter
-                      :applied false
-                      :count 1
-                      :links
-                      {:apply
-                       "http://localhost:3003/granules.json?page_size=10&include_facets=v2&collection_concept_id=C1-PROV1&temporal_facet%5B0%5D%5Byear%5D=1999"}
-                      :has_children true}
-                     {:title "2010"
-                      :type :filter
-                      :applied false
-                      :count 3
-                      :links
-                      {:apply
-                       "http://localhost:3003/granules.json?page_size=10&include_facets=v2&collection_concept_id=C1-PROV1&temporal_facet%5B0%5D%5Byear%5D=2010"}
-                      :has_children true}
-                     {:title "2011"
-                      :type :filter
-                      :applied false
-                      :count 1
-                      :links
-                      {:apply
-                       "http://localhost:3003/granules.json?page_size=10&include_facets=v2&collection_concept_id=C1-PROV1&temporal_facet%5B0%5D%5Byear%5D=2011"}
-                      :has_children true}
-                     {:title "2012"
-                      :type :filter
-                      :applied false
-                      :count 1
-                      :links
-                      {:apply
-                       "http://localhost:3003/granules.json?page_size=10&include_facets=v2&collection_concept_id=C1-PROV1&temporal_facet%5B0%5D%5Byear%5D=2012"}
-                      :has_children true})}]}]
-               result))))
+                 '({:title "1999"
+                    :type :filter
+                    :applied false
+                    :count 1
+                    :links
+                    {:apply
+                     "http://localhost:3003/granules.json?page_size=10&include_facets=v2&collection_concept_id=C1-PROV1&temporal_facet%5B0%5D%5Byear%5D=1999"}
+                    :has_children true}
+                   {:title "2010"
+                    :type :filter
+                    :applied false
+                    :count 3
+                    :links
+                    {:apply
+                     "http://localhost:3003/granules.json?page_size=10&include_facets=v2&collection_concept_id=C1-PROV1&temporal_facet%5B0%5D%5Byear%5D=2010"}
+                    :has_children true}
+                   {:title "2011"
+                    :type :filter
+                    :applied false
+                    :count 1
+                    :links
+                    {:apply
+                     "http://localhost:3003/granules.json?page_size=10&include_facets=v2&collection_concept_id=C1-PROV1&temporal_facet%5B0%5D%5Byear%5D=2011"}
+                    :has_children true}
+                   {:title "2012"
+                    :type :filter
+                    :applied false
+                    :count 1
+                    :links
+                    {:apply
+                     "http://localhost:3003/granules.json?page_size=10&include_facets=v2&collection_concept_id=C1-PROV1&temporal_facet%5B0%5D%5Byear%5D=2012"}
+                    :has_children true})}]}
+              {:title "Cycle"
+               :type :group
+               :applied false
+               :has_children true
+               :children
+               {:title nil
+                :type :filter
+                :applied false
+                :count 0
+                :links
+                {:apply
+                 "http://localhost:3003/granules.json?page_size=10&include_facets=v2&collection_concept_id=C1-PROV1"}
+                :has_children false
+                :children nil}}]
+             result))))
 
   (testing "cycle"
     (let [result (v2-facets/create-v2-facets-by-concept-type
@@ -121,67 +135,59 @@
                     "collection_concept_id" "C1-PROV1"}
                    {:cycle {:buckets [{:key 3.0 :doc_count 6}
                                       {:key 42.0 :doc_count 2}]}}
-                   nil)
-          cycle-result (first result)]
-      (println "==== the result =====")
-      (clojure.pprint/pprint cycle-result)
-      (is (= "Cycle" (:title cycle-result)))
-      (is (= 2 (count (:children result))))))
-  #_(testing "cycle and pass"
-      (let [result (v2-facets/create-v2-facets-by-concept-type 
-                     :granule
-                     "http://localhost:3003/granules.json"
-                     {"page_size" "10"
-                      "include_facets" "v2"
-                      "collection_concept_id" "C1-PROV1"}
-                     {:start-date-doc-values {:buckets []}
-                      :cycle
-                      {:buckets
-                       [{:key 3.0
-                         :doc_count 6
-                         :passes
-                         {:doc_count 11
-                          :pass
-                          {:buckets
-                           [{:key 1.0 :doc_count 2}
-                            {:key 2.0 :doc_count 1}
-                            {:key 3.0 :doc_count 4}
-                            {:key 4.0 :doc_count 4}]}}}
-                        {:key 42.0
-                         :doc_count 2
-                         :passes
-                         {:doc_count 3
-                          :pass
-                          {:buckets
-                           [{:key 1.0 :doc_count 1}
-                            {:key 3.0 :doc_count 1}
-                            {:key 4.0 :doc_count 1}]}}}]}}
+                   nil)]
+      (is (= "Cycle" (:title (first result))))
+      (is (= 2 (count (:children (first result)))))))
+  (testing "cycle and pass"
+    (let [result (v2-facets/create-v2-facets-by-concept-type 
+                   :granule
+                   "http://localhost:3003/granules.json"
+                   {"page_size" "10"
+                    "include_facets" "v2"
+                    "collection_concept_id" "C1-PROV1"}
+                   {:start-date-doc-values {:buckets []}
+                    :cycle
+                    {:buckets
+                     [{:key 3.0
+                       :doc_count 6
+                       :passes
+                       {:doc_count 11
+                        :pass
+                        {:buckets
+                         [{:key 1.0 :doc_count 2}
+                          {:key 2.0 :doc_count 1}
+                          {:key 3.0 :doc_count 4}
+                          {:key 4.0 :doc_count 4}]}}}
+                      {:key 42.0
+                       :doc_count 2
+                       :passes
+                       {:doc_count 3
+                        :pass
+                        {:buckets
+                         [{:key 1.0 :doc_count 1}
+                          {:key 3.0 :doc_count 1}
+                          {:key 4.0 :doc_count 1}]}}}]}}
 
-                     nil)]
-        (is (= [{:title "Cycle"          
-                 :type :group
-                 :applied false
-                 :has_children true
-                 :children
-                 [{:title "Cycle"
-                   :type :group
-                   :applied false
-                   :has_children true
-                   :children
-                   '({:title "3"
-                      :type :filter
-                      :applied false
-                      :count 8
-                      :links
-                      {:apply
-                       "http://localhost:3003/granules.json?page_size=10&include_facets=v2&collection_concept_id=C1-PROV1&null%5B0%5D%5Bnull%5D=3"}
-                      :has_children true}
-                     {:title "42"
-                      :type :filter
-                      :applied false
-                      :count 6
-                      :links
-                      {:apply
-                       "http://localhost:3003/granules.json?page_size=10&include_facets=v2&collection_concept_id=C1-PROV1&null%5B0%5D%5Bnull%5D=42"}
-                      :has_children true})}]}]
-               result)))))
+                   nil)]
+      (is (= [{:title "Cycle"          
+               :type :group
+               :applied false
+               :has_children true
+               :children
+               '({:title "3"
+                  :type :filter
+                  :applied false
+                  :count 6
+                  :links
+                  {:apply
+                   "http://localhost:3003/granules.json?page_size=10&include_facets=v2&collection_concept_id=C1-PROV1&cycle%5B%5D=3"}
+                  :has_children false}
+                 {:title "42"
+                  :type :filter
+                  :applied false
+                  :count 2
+                  :links
+                  {:apply
+                   "http://localhost:3003/granules.json?page_size=10&include_facets=v2&collection_concept_id=C1-PROV1&cycle%5B%5D=42"}
+                  :has_children false})}]
+             result)))))
