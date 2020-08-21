14d531e5f8ee45aaefa4a16d28f98e0470af96f7
Thomas Cherry <thomas.a.cherry@nasa.gov>
2020-08-21 10:42:39 -0400

    CMR-6612 - revert to see if bamboo builds

--- ingest-app/src/cmr/ingest/services/jobs.clj
+++ ingest-app/src/cmr/ingest/services/jobs.clj
@@ -262,7 +262,7 @@
                params2 (merge {:revision-date time-constraint
                                :collection-concept-id coll-id}
                               query-params)]]
-      (info "Processing subscription: " sub-name " with " (str subscription) "."
+      (info "Processing subscription: " sub-name)
       (try
         (let [gran-ref1 (search/find-granule-references context params1)
               gran-ref2 (search/find-granule-references context params2)
