50cf71dc508d8825bfab48a75906718acf06e779
Erich Reiter <erich.e.reiter@nasa.gov>
2020-08-26 21:38:21 -0400

    CMR-6614 adding TilingInformationSystemName a.k.a two-d-coordinate-system to the v2 facets.

--- search-app/src/cmr/search/services/query_execution/facets/collection_v2_facets.clj
+++ search-app/src/cmr/search/services/query_execution/facets/collection_v2_facets.clj
@@ -27,7 +27,7 @@
    :processing-level-id :processing-level-id-humanized
    :variables :variables
    :granule-data-format :granule-data-format-humanized
-   :tiling-system :two-d-coordinate-system-name})
+   :two-d-coordinate-system-name :two-d-coord-name})
 
 (defmethod v2-facets/facets-v2-params->elastic-fields :collection
   [_]
@@ -62,7 +62,7 @@
 (defmethod v2-facets/v2-facets-result-field-in-order :collection
   [_]
   ["Keywords" "Platforms" "Instruments" "Organizations" "Projects" "Processing levels"
-   "Measurements" "Output File Formats" "Reprojections"])
+   "Measurements" "Output File Formats" "Reprojections" "Tiling System"])
 
 (defmethod v2-facets/v2-facets-root :collection
   [_]
@@ -73,6 +73,25 @@
   "Controls whether or not to display variable facets. Feature toggle needed while prototyping
   with EDSC in certain environments."
   {:type Boolean :default false})
+
+(defn create-two-d-v2-facets
+  "Parses the elastic aggregations and generates the v2 facets for the
+   two-d-coordinate-system-name field."
+  [elastic-aggregations base-url query-params facet-field]
+  (let [search-terms-from-query (lh/get-values-for-field query-params facet-field)
+        value-counts (v2-facets/add-terms-with-zero-matching-collections
+                      (frf/buckets->value-count-pairs (get elastic-aggregations facet-field))
+                      search-terms-from-query)
+        snake-case-field (csk/->snake_case_string facet-field)
+        applied? (some? (or (get query-params snake-case-field)
+                            (get query-params (str snake-case-field "[]"))))
+        query-field (keyword (str/replace (name facet-field) #"-h" ""))
+        children (map (v2h/generate-filter-node base-url query-params query-field applied?)
+                      (sort-by first util/compare-natural-strings value-counts))]
+    (when (seq children)
+      (vector
+       (v2h/generate-group-node (facet-field v2h/fields->human-readable-label) applied?
+                                children)))))
 
 (defmethod v2-facets/create-v2-facets-by-concept-type :collection
   [concept-type base-url query-params aggs facet-fields]
@@ -85,8 +104,12 @@
         variables-facets (when (and (facet-fields-set :variables) (include-variable-facets))
                            (hv2/create-hierarchical-v2-facets
                             aggs base-url query-params :variables-h))
+        two-d-facets (when (facet-fields-set :two-d-coordinate-system-name)
+                       (create-two-d-v2-facets
+                         aggs base-url query-params :two-d-coordinate-system-name-h))
         v2-facets (concat science-keywords-facets
                           (v2-facets/create-prioritized-v2-facets
                            :collection aggs flat-facet-fields base-url query-params)
-                          variables-facets)]
+                          variables-facets
+                          two-d-facets)]
     v2-facets))
