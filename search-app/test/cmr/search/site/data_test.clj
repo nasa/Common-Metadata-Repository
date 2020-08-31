f757eba25500d3a266a768c35be8e6a71083e97b
Jay Barra <jay.barra@nasa.gov>
2020-08-31 10:13:47 -0400

    CMR-6591 testing all environments

--- search-app/test/cmr/search/site/data_test.clj
+++ search-app/test/cmr/search/site/data_test.clj
@@ -4,10 +4,31 @@
    [cmr.search.site.data :as data]))
 
 (deftest app-url->virtual-directory-url-test
-  (testing "with trailing slash"
-    (is (= "https://cmr.earthdata.nasa.gov/virtual-directory/"
-           (data/app-url->virtual-directory-url "https://cmr.earthdata.nasa.gov/search/"))))
-  (testing "without trailing slash"
-    (is (= "https://cmr.earthdata.nasa.gov/virtual-directory/"
-           (data/app-url->virtual-directory-url "https://cmr.earthdata.nasa.gov/search")))))
+  (are [input expected]
+      (= expected
+         (data/app-url->virtual-directory-url input))
 
+    ;; PROD
+    "https://cmr.earthdata.nasa.gov/search/"
+    "https://cmr.earthdata.nasa.gov/virtual-directory/"
+
+    "https://cmr.earthdata.nasa.gov/search"
+    "https://cmr.earthdata.nasa.gov/virtual-directory/"
+
+    ;; SIT
+    "https://cmr.sit.earthdata.nasa.gov/search"
+    "https://cmr.sit.earthdata.nasa.gov/virtual-directory/"
+
+    "https://cmr.sit.earthdata.nasa.gov/search"
+    "https://cmr.sit.earthdata.nasa.gov/virtual-directory/"
+
+    ;; UAT
+    "https://cmr.uat.earthdata.nasa.gov/search"
+    "https://cmr.uat.earthdata.nasa.gov/virtual-directory/"
+
+    "https://cmr.uat.earthdata.nasa.gov/search"
+    "https://cmr.uat.earthdata.nasa.gov/virtual-directory/"
+
+    ;; localhost doesn't matter
+    "http://localhost:3003"
+    "http://localhost:3003"))
