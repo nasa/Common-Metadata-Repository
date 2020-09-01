523b01a010927b497eb939d33f24193f152d6391
Jay Barra <jay.barra@nasa.gov>
2020-09-01 08:11:48 -0400

    CMR-6591 Updated Virtual Directory Link Path (#1036)

---
+++ search-app/test/cmr/search/site/data_test.clj
@@ -0,0 +1,34 @@
+(ns cmr.search.site.data-test
+  (:require
+   [clojure.test :refer :all]
+   [cmr.search.site.data :as data]))
+
+(deftest app-url->virtual-directory-url-test
+  (are [input expected]
+      (= expected
+         (data/app-url->virtual-directory-url input))
+
+    ;; PROD
+    "https://cmr.earthdata.nasa.gov/search"
+    "https://cmr.earthdata.nasa.gov/virtual-directory/"
+
+    "https://cmr.earthdata.nasa.gov/search/"
+    "https://cmr.earthdata.nasa.gov/virtual-directory/"
+
+    ;; SIT
+    "https://cmr.sit.earthdata.nasa.gov/search"
+    "https://cmr.sit.earthdata.nasa.gov/virtual-directory/"
+
+    "https://cmr.sit.earthdata.nasa.gov/search/"
+    "https://cmr.sit.earthdata.nasa.gov/virtual-directory/"
+
+    ;; UAT
+    "https://cmr.uat.earthdata.nasa.gov/search"
+    "https://cmr.uat.earthdata.nasa.gov/virtual-directory/"
+
+    "https://cmr.uat.earthdata.nasa.gov/search/"
+    "https://cmr.uat.earthdata.nasa.gov/virtual-directory/"
+
+    ;; localhost - no change expected
+    "http://localhost:3003"
+    "http://localhost:3003"))
