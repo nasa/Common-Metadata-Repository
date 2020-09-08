735a6b184f6f9183e9bd275f1ce0b221f7f5be1f
Thomas Cherry <thomas.a.cherry@nasa.gov>
2020-09-08 13:50:56 -0400

    CMR-6631 - adding primary key sequence and a uniq constraint

--- metadata-db-app/src/cmr/metadata_db/migrations/072_add_cmr_sub_notifications_table.clj
+++ metadata-db-app/src/cmr/metadata_db/migrations/072_add_cmr_sub_notifications_table.clj
@@ -9,17 +9,31 @@
  last_notified_at TIMESTAMP WITH TIME ZONE DEFAULT SYSTIMESTAMP NOT NULL")
 
  (def ^:private sub-notifications-constraint-sql
-   (str "CONSTRAINT sub_notifications_pk PRIMARY KEY (id)"))
+   (str "CONSTRAINT sub_notifications_pk PRIMARY KEY (id),"
+   "CONSTRAINT subscriptions_nid_rid UNIQUE (subscription_concept_id)"))
+
+(defn- create-notification-table
+ "Create the sub-notification table"
+ []
+ (h/sql (format "CREATE TABLE METADATA_DB.cmr_sub_notifications (%s, %s)"
+  sub-notifications-column-sql
+  sub-notifications-constraint-sql)))
+
+(defn- create-notification-sequence
+ "Create Sequences used by this table"
+  []
+  (h/sql "CREATE SEQUENCE cmr_sub_notifications_seq"))
 
 (defn up
   "Migrates the database up to version 72."
   []
   (println "cmr.metadata-db.migrations.072-add-cmr-sub-notifications-table up...")
-  (h/sql (format "CREATE TABLE METADATA_DB.cmr_sub_notifications (%s, %s)"
-   sub-notifications-column-sql sub-notifications-constraint-sql)))
+  (create-notification-table)
+  (create-notification-sequence))
 
 (defn down
   "Migrates the database down from version 72."
   []
   (println "cmr.metadata-db.migrations.072-add-cmr-sub-notifications-table down.")
+  (h/sql "DROP SEQUENCE METADATA_DB.cmr_sub_notifications_seq")
   (h/sql "DROP TABLE cmr_sub_notifications"))
