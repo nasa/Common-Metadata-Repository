f1cf2dba7030f52b5abe14190ce14694035f71a5
Jay Barra <jay.barra@nasa.gov>
2020-09-09 12:37:06 -0400

    CMR-6649 updated tests to work with new parameter

--- system-int-test/test/cmr/system_int_test/search/granule_search_format_test.clj
+++ system-int-test/test/cmr/system_int_test/search/granule_search_format_test.clj
@@ -564,15 +564,16 @@
                  (-> response :results :entries first :size))))))
 
     (testing "json"
-      (let [gran-json (dj/granules->expected-json [gran1] [coll1] "granules.json?granule_ur=Granule1")
-            response (search/find-concepts-json :granule {:granule-ur "Granule1"})]
+      (let [gran-json (dj/granules->expected-json [gran1] [coll1] "granules.json?granule_ur=Granule1&include_polygons=true")
+            response (search/find-concepts-json :granule {:granule-ur "Granule1"
+                                                          :include-polygons true})]
         (is (= 200 (:status response)))
         (is (= gran-json
                (:results response))))
 
       (let [gran-json (dj/granules->expected-json
-                        [gran1 gran2 gran3 gran4 gran5] [coll1 coll2 coll3 coll1 coll3] "granules.json")
-            response (search/find-concepts-json :granule {})]
+                        [gran1 gran2 gran3 gran4 gran5] [coll1 coll2 coll3 coll1 coll3] "granules.json?include_polygons=true")
+            response (search/find-concepts-json :granule {:include-polygons true})]
         (is (= 200 (:status response)))
         (is (= gran-json
                (:results response))))
