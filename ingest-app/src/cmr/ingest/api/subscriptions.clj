d7081ec6c749ef50db225814c116e9137e71acca
Thomas Cherry <thomas.a.cherry@nasa.gov>
2020-11-12 10:09:04 -0500

    CMR-6623 - bypassing ACLs if the requesting user is the same as the one the subscription is for

--- ingest-app/src/cmr/ingest/api/subscriptions.clj
+++ ingest-app/src/cmr/ingest/api/subscriptions.clj
@@ -1,6 +1,7 @@
 (ns cmr.ingest.api.subscriptions
   "Subscription ingest functions in support of the ingest API."
   (:require
+   [cheshire.core :as json]
    [cmr.acl.core :as acl]
    [cmr.common-app.api.enabled :as common-enabled]
    [cmr.common-app.api.launchpad-token-validation :as lt-validation]
@@ -18,6 +19,13 @@
     (v/validate-concept-metadata concept)
     concept))
 
+(defn subscription-user-matches-token?
+  "Check to see if the user for the supplied token matches the user in the subscription request"
+  [concept request-context headers]
+  (let [token-user (api-core/get-user-id request-context headers)
+        subscriber (get (json/decode (:metadata concept)) "SubscriberId")]
+        (= subscriber token-user)))
+
 (defn ingest-subscription
   "Processes a request to create or update a subscription."
   [provider-id native-id request]
@@ -26,10 +34,18 @@
                  :subscription provider-id native-id body content-type headers)
         _ (lt-validation/validate-launchpad-token request-context)
         _ (api-core/verify-provider-exists request-context provider-id)
-        _ (acl/verify-ingest-management-permission
-            request-context :update :provider-object provider-id)
-        _ (acl/verify-subscription-management-permission
-            request-context :update :provider-object provider-id)
+        subscription-user (get (json/decode (:metadata concept)) "SubscriberId")
+        token-user (api-core/get-user-id request-context headers)
+        bypass-acls (subscription-user-matches-token? concept request-context headers)
+        _ (when-not bypass-acls
+                    (info "ACLs were checked because the token user"
+                          token-user
+                          "is not the same as the subscription user"
+                          subscription-user)
+                    (acl/verify-ingest-management-permission
+                      request-context :update :provider-object provider-id)
+                    (acl/verify-subscription-management-permission
+                      request-context :update :provider-object provider-id))
         _ (common-enabled/validate-write-enabled request-context "ingest")
         concept (validate-and-prepare-subscription-concept concept)
         concept-with-user-id (api-core/set-user-id concept request-context headers)
@@ -39,6 +55,13 @@
                         (:client-id request-context)))
         save-subscription-result (ingest/save-subscription request-context concept-with-user-id)]
     ;; Log the successful ingest, with the metadata size in bytes.
+
+    ; there is a worry that subscriptions could be created neferiusly, log the
+    ; action. When no longer needed (log) also remove the variables from the let.
+    (when bypass-acls
+      (warn "ACLs were bypassed because the requesting account" token-user
+        "matched the subscription user" subscription-user ""))
+
     (api-core/log-concept-with-metadata-size concept-with-user-id request-context)
     (api-core/generate-ingest-response headers save-subscription-result)))
 
