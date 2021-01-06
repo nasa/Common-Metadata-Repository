04b72ef44e13deb9255937565a968e9098e985c2
John Teague <johnwteague@gmail.com>
2021-01-06 09:44:21 -0700

    Revert "Cmr 6901 cmr 6966 validate get data and file archive info formats in kms (#1129)"
    This reverts commit 0fa2b71cf56a9c5c31829bf0b584ae068941a04d.
    CMR-6901 was failed due to exposed error when -H "Validate-Keywords=true" is set

--- system-int-test/src/cmr/system_int_test/data2/umm_spec_collection.clj
+++ system-int-test/src/cmr/system_int_test/data2/umm_spec_collection.clj
@@ -21,11 +21,6 @@
   [attribs]
   (umm-c/map->FileDistributionInformationType attribs))
 
-(defn file-archive-information
-  "Returns a FileArchiveInformation based on given attributes"
-  [attribs]
-  (umm-c/map->FileArchiveInformationType attribs))
-
 (defn location-keyword
   "Return a location keyword based on the given attributes."
   [attribs]
