248c77d685475647dfaaff7482a9c3867a107184
Yiduo Ke <yk467@cornell.edu>
2021-01-07 13:30:22 -0500

    formatting (installed a Clojure extension for my editor)

--- search-app/test/cmr/search/test/api/tags.clj
+++ search-app/test/cmr/search/test/api/tags.clj
@@ -4,35 +4,17 @@
    [clojure.test :refer :all]
    [cmr.search.api.tags :as tags]))
 
-(def dictionary-with-error [
-    {
-        "errors" [
-        "Collection [C1200000006-PROV1] does not exist or is not visible."
-        ]
+(def dictionary-with-error [{"errors" ["Collection [C1200000006-PROV1] does not exist or is not visible."]
 
-        "tagged_item" {
-        "concept_id" "C1200000006-PROV1"
-        }
-    }
-])
+                             "tagged_item" {"concept_id" "C1200000006-PROV1"}}])
 
-(def dictionary-with-no-error [
-    {
-        "tag_association" {
-            "concept_id" "TA1200000009-CMR"
-            "revision_id" 1
-        }
-        "tagged_item" {
-            "concept_id" "C1200000005-PROV1"
-        }
-    }
-])
+(def dictionary-with-no-error [{"tag_association" {"concept_id" "TA1200000009-CMR"
+                                                   "revision_id" 1}
+                                "tagged_item" {"concept_id" "C1200000005-PROV1"}}])
 
 (deftest tag-api-response-status-code-test
   (testing "Making sure `tag-api-response` returns status code 400 when there's errors"
-      (do
-            (is
-                  (= 400 (:status (tags/tag-api-response dictionary-with-error))))
-            (is
-                  (= 200 (:status (tags/tag-api-response dictionary-with-no-error)))))))
-      
+    (is
+     (= 400 (:status (tags/tag-api-response dictionary-with-error))))
+    (is
+     (= 200 (:status (tags/tag-api-response dictionary-with-no-error))))))
