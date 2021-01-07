cfd5c9f6fc8956d4883605dd78df4d7b99245a20
Yiduo Ke <yk467@cornell.edu>
2021-01-07 13:40:07 -0500

    formatting

--- search-app/test/cmr/search/test/api/tags.clj
+++ search-app/test/cmr/search/test/api/tags.clj
@@ -5,7 +5,6 @@
    [cmr.search.api.tags :as tags]))
 
 (def dictionary-with-error [{"errors" ["Collection [C1200000006-PROV1] does not exist or is not visible."]
-
                              "tagged_item" {"concept_id" "C1200000006-PROV1"}}])
 
 (def dictionary-with-no-error [{"tag_association" {"concept_id" "TA1200000009-CMR"
@@ -14,7 +13,5 @@
 
 (deftest tag-api-response-status-code-test
   (testing "Making sure `tag-api-response` returns status code 400 when there's errors"
-    (is
-     (= 400 (:status (tags/tag-api-response dictionary-with-error))))
-    (is
-     (= 200 (:status (tags/tag-api-response dictionary-with-no-error))))))
+    (is (= 400 (:status (tags/tag-api-response dictionary-with-error))))
+    (is (= 200 (:status (tags/tag-api-response dictionary-with-no-error))))))
