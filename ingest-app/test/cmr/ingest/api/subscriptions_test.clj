856ceca77c3357e352aad5d8307bf3f7af8770b4
Jay Barra <jay.barra@nasa.gov>
2021-01-27 09:27:54 -0500

    CMR-6930 changed test mock method for clarity

--- ingest-app/test/cmr/ingest/api/subscriptions_test.clj
+++ ingest-app/test/cmr/ingest/api/subscriptions_test.clj
@@ -6,19 +6,19 @@
 
 (deftest ingest-subscription-test
   (testing "with native-id provided"
-    (with-redefs [subscriptions/perform-subscription-ingest (constantly nil)
-                  subscriptions/common-ingest-checks (constantly nil)
-                  api-core/body->concept! (fn [_concept-type _provider-id native-id _body _content-type _headers]
-                                            (is (= "given-native-id" native-id)))
-                  subscriptions/check-subscription-ingest-permission (constantly nil)]
-      (subscriptions/ingest-subscription "test-provider" "given-native-id" nil)))
+    (with-redefs-fn {#'subscriptions/perform-subscription-ingest (constantly nil)
+                     #'subscriptions/common-ingest-checks (constantly nil)
+                     #'api-core/body->concept! (fn [_concept-type _provider-id native-id _body _content-type _headers]
+                                                 (is (= "given-native-id" native-id)))
+                     #'subscriptions/check-subscription-ingest-permission (constantly nil)}
+      #(subscriptions/ingest-subscription "test-provider" "given-native-id" nil)))
 
   (testing "with native-id not provided"
-    (with-redefs [subscriptions/perform-subscription-ingest (constantly nil)
-                  subscriptions/common-ingest-checks (constantly nil)
-                  api-core/body->concept! (fn [_concept-type _provider-id native-id _body _content-type _headers]
-                                            (printf "generated native-id [%s]%n" native-id)
-                                            (is (string? native-id))
-                                            (is (not (nil? native-id))))
-                  subscriptions/check-subscription-ingest-permission (constantly nil)]
-      (subscriptions/ingest-subscription "test-provider" nil nil))))
+    (with-redefs-fn {#'subscriptions/perform-subscription-ingest (constantly nil)
+                     #'subscriptions/common-ingest-checks (constantly nil)
+                     #'api-core/body->concept! (fn [_concept-type _provider-id native-id _body _content-type _headers]
+                                                 (printf "generated native-id [%s]%n" native-id)
+                                                 (is (string? native-id))
+                                                 (is (not (nil? native-id))))
+                     #'subscriptions/check-subscription-ingest-permission (constantly nil)}
+      #(subscriptions/ingest-subscription "test-provider" nil nil))))
