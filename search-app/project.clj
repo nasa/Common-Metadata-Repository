867ca7f95ead800774dd543eb0cf5b6504a90e1d
Jay Barra <jay.barra@nasa.gov>
2021-02-04 08:24:34 -0500

    CMR-6390 restored original ring-mock

--- search-app/project.clj
+++ search-app/project.clj
@@ -70,7 +70,7 @@
                                   [org.clojure/tools.namespace "0.2.11"]
                                   [org.clojure/tools.nrepl "0.2.13"]
                                   [pjstadig/humane-test-output "0.9.0"]
-                                  [ring/ring-mock "0.4.0"]]
+                                  [ring-mock "0.1.5"]]
                    :jvm-opts ^:replace ["-server"]
                    :resource-paths ["resources" "test/resources"]
                    :source-paths ["src" "dev" "test"]
