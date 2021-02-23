d3a6a6f996e0e299f3228646b047f422a81d45c9
Jay Barra <jay.barra@nasa.gov>
2021-02-23 11:56:24 -0500

    CMR-7078 removed unused imports

--- system-int-test/test/cmr/system_int_test/ingest/collection_direct_distribution_fields_test.clj
+++ system-int-test/test/cmr/system_int_test/ingest/collection_direct_distribution_fields_test.clj
@@ -1,33 +1,12 @@
 (ns cmr.system-int-test.ingest.collection-direct-distribution-fields-test
   (:require
-   [clj-http.client :as client]
-   [clojure.java.io :as io]
    [clojure.string :as string]
    [clojure.test :refer :all]
-   [cmr.access-control.test.util :as test-util]
-   [cmr.common-app.config :as common-config]
-   [cmr.common-app.test.side-api :as side]
-   [cmr.common.date-time-parser :as date-time-parser]
-   [cmr.common.log :as log :refer (debug info warn error)]
-   [cmr.common.mime-types :as mime-types]
-   [cmr.common.util :as util]
-   [cmr.mock-echo.client.echo-util :as echo-util]
-   [cmr.system-int-test.data2.core :as data-core]
-   [cmr.system-int-test.data2.granule :as granule]
    [cmr.system-int-test.data2.umm-spec-collection :as data-umm-c]
-   [cmr.system-int-test.data2.umm-spec-common :as data-umm-cmn]
-   [cmr.system-int-test.system :as system]
-   [cmr.system-int-test.utils.dev-system-util :as dev-sys-util]
    [cmr.system-int-test.utils.index-util :as index]
    [cmr.system-int-test.utils.ingest-util :as ingest]
-   [cmr.system-int-test.utils.metadata-db-util :as mdb]
    [cmr.system-int-test.utils.search-util :as search]
-   [cmr.transmit.config :as transmit-config]
-   [cmr.umm-spec.models.umm-collection-models :as umm-coll]
-   [cmr.umm-spec.models.umm-common-models :as umm-cmn]
-   [cmr.umm-spec.test.expected-conversion :as expected-conversion]
-   [cmr.umm-spec.test.location-keywords-helper :as location-keywords-helper]
-   [cmr.umm-spec.umm-spec-core :as umm-spec]))
+   [cmr.umm-spec.models.umm-collection-models :as umm-coll]))
 
 (use-fixtures :each (ingest/reset-fixture {"provguid1" "PROV1"
                                            "provguid2" "PROV2"}))
