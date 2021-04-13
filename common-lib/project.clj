6c0d04232645e479fa25c950b9980c916fc8195f
mitchstartzel <mstartzel16@gmail.com>
2021-04-13 14:28:34 -0400

    CMR-7318: Bumped jetty version

--- common-lib/project.clj
+++ common-lib/project.clj
@@ -36,10 +36,10 @@
                  [org.clojure/test.check "0.9.0"]
                  [org.clojure/tools.nrepl "0.2.13"]
                  [org.clojure/tools.reader "1.3.2"]
-                 [org.eclipse.jetty/jetty-http "9.4.14.v20181114"]
-                 [org.eclipse.jetty/jetty-io "9.4.14.v20181114"]
-                 [org.eclipse.jetty/jetty-servlets "9.4.14.v20181114"]
-                 [org.eclipse.jetty/jetty-util "9.4.14.v20181114"]
+                 [org.eclipse.jetty/jetty-http "9.4.39.v20210325"]
+                 [org.eclipse.jetty/jetty-io "9.4.39.v20210325"]
+                 [org.eclipse.jetty/jetty-servlets "9.4.39.v20210325"]
+                 [org.eclipse.jetty/jetty-util "9.4.39.v20210325"]
                  ;; load jts core lib first to make sure it is available for shapefile integration,
                  ;; otherwise ES referenced 1.15.0 version will be mistakenly picked for shapefile
                  [org.locationtech.jts/jts-core "1.16.1"]
