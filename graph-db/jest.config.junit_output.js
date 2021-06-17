77b7af73425e7e29c4418e3817fbe92e78113b89
Yonggang Liu <ygliuvt@gmail.com>
2021-06-17 16:13:12 -0400

    CMR-7443: Fixed ci-test.

--- graph-db/jest.config.junit_output.js
+++
@@ -1,9 +0,0 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
-
-module.exports = {
-  clearMocks: true,
-  coverageDirectory: 'coverage',
-  reporters: ['default', ['jest-junit', { suiteName: 'jest tests' }]],
-  testEnvironment: 'node'
-}
