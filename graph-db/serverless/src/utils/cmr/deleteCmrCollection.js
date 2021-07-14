ef7688959342492c7d481d328d1f9dd6a075d4ba
Yonggang Liu <ygliuvt@gmail.com>
2021-07-14 11:46:22 -0400

    CMR-7528: Create Campaign vertices and relate them to Collections.

--- graph-db/serverless/src/utils/cmr/deleteCmrCollection.js
+++ graph-db/serverless/src/utils/cmr/deleteCmrCollection.js
@@ -8,22 +8,45 @@ const { P: { lte } } = gremlin.process
  * Delete the collection with the given concept id from graph db
  * @param {String} conceptId Collection concept id from CMR
  * @param {Gremlin Traversal Object} gremlinConnection connection to gremlin server
+ * @param {String} vertexLabel Label of the linked vertex
+ * @param {String} edgeName Name of the edge between the dataset vertex and its linked vertex
  * @returns
  */
-export const deleteCmrCollection = async (conceptId, gremlinConnection) => {
-  // drop all the documentation vertices that are connected to and only connected to the dataset vertex
+const deleteLinkedVertices = async (conceptId, gremlinConnection, vertexLabel, edgeName) => {
   try {
     await gremlinConnection
       .V()
       .has('dataset', 'concept-id', conceptId)
-      .outE('documentedBy')
+      .outE(edgeName)
       .inV()
-      .where(gremlinStatistics.inE('documentedBy').count().is(lte(1)))
+      .where(gremlinStatistics.inE(edgeName).count().is(lte(1)))
       .drop()
       .next()
   } catch (error) {
-    console.error(`Error deleting documentation vertices only linked to collection [${conceptId}]: ${error.message}`)
+    console.error(`Error deleting ${vertexLabel} vertices only linked to collection [${conceptId}]: ${error.message}`)
 
+    return false
+  }
+
+  return true
+}
+/**
+ * Delete the collection with the given concept id from graph db
+ * @param {String} conceptId Collection concept id from CMR
+ * @param {Gremlin Traversal Object} gremlinConnection connection to gremlin server
+ * @returns
+ */
+export const deleteCmrCollection = async (conceptId, gremlinConnection) => {
+  // drop all the campaign vertices that are connected to and only connected to the dataset vertex
+  let success
+  success = await deleteLinkedVertices(conceptId, gremlinConnection, 'campaign', 'includedIn')
+  if (success === false) {
+    return false
+  }
+
+  // drop all the documentation vertices that are connected to and only connected to the dataset vertex
+  success = await deleteLinkedVertices(conceptId, gremlinConnection, 'documentation', 'documentedBy')
+  if (success === false) {
     return false
   }
 
