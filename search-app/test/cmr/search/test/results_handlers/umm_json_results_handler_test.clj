9f3615c1e5d5ab28d646cc93ed6070755d790d39
Jay Barra <jay.f.barra@gmail.com>
2021-07-20 14:50:26 -0400

    removed unused test file

--- search-app/test/cmr/search/test/results_handlers/umm_json_results_handler_test.clj
+++
@@ -1,5 +0,0 @@
-(ns cmr.search.test.results-handlers.umm-json-results-handler-test
-  (:require
-   [clojure.test :refer :all]
-   [cmr.common.util :as util :refer [are3]]
-   [cmr.search.results-handlers.umm-json-results-handler :as umm-results]))
