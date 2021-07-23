bda250a35c4ba104c99b7d8c7f495d40105f0a34
Yonggang Liu <ygliuvt@gmail.com>
2021-07-23 15:09:25 -0400

    CMR-7529: Refactored code.

---
+++ graph-db/serverless/src/testUtil/verifyDocumentation.js
@@ -0,0 +1,50 @@
+import gremlin from 'gremlin'
+
+const gremlinStatistics = gremlin.process.statics
+
+export const verifyDocumentationExistInGraphDb = async (datasetTitle, docName) => {
+  // verify the dataset vertex with the given title exists
+  const dataset = await global.testGremlinConnection
+    .V()
+    .has('dataset', 'title', datasetTitle)
+    .next()
+  const { value: { id: datasetId } } = dataset
+  expect(datasetId).not.toBe(null)
+
+  // verify the documentation vertex with the given name exists
+  const doc = await global.testGremlinConnection
+    .V()
+    .has('documentation', 'name', docName)
+    .next()
+  const { value: { id: docId } } = doc
+  expect(docId).not.toBe(null)
+
+  // verify the edge exists between the two vertices
+  const record = await global.testGremlinConnection
+    .V()
+    .has('dataset', 'title', datasetTitle)
+    .outE('documentedBy')
+    .filter(gremlinStatistics.inV()
+      .has('documentation', 'name', docName))
+    .next()
+  const { value: { id: edgeId } } = record
+  expect(edgeId).not.toBe(null)
+}
+
+export const verifyDocumentationNotExistInGraphDb = async (datasetTitle, docName) => {
+  // verify the dataset vertex with the given title does not exist
+  const dataset = await global.testGremlinConnection
+    .V()
+    .has('dataset', 'title', datasetTitle)
+    .next()
+  const { value: datasetValue } = dataset
+  expect(datasetValue).toBe(null)
+
+  // verify the documentation vertex with the given name does not exist
+  const doc = await global.testGremlinConnection
+    .V()
+    .has('documentation', 'name', docName)
+    .next()
+  const { value: docValue } = doc
+  expect(docValue).toBe(null)
+}
---
+++ graph-db/serverless/src/utils/cmr/indexInstrument.js
@@ -0,0 +1,41 @@
+import gremlin from 'gremlin'
+
+import { createAcquiredByEdge } from './createAcquiredByEdge'
+
+const gremlinStatistics = gremlin.process.statics
+
+/**
+ * Given an Instrument object, Gremlin connection, and associated platform and collection, index instrument and build relationships between platform/instrument and collection
+ * @param {JSON} instrument the instrument
+ * @param {Connection} gremlinConnection a connection to the gremlin server
+ * @param {String} platformName the name of the platform
+ * @param {Graph Node} dataset the parent collection vertex in the gremlin server
+ * @returns null
+ */
+export const indexInstrument = async (instrument, gremlinConnection, platformName, dataset) => {
+  const {
+    ShortName: instrumentName
+  } = instrument
+
+  let piVertex
+  try {
+    piVertex = await gremlinConnection
+      .V()
+      .has('platformInstrument', 'platform', platformName)
+      .has('instrument', instrumentName)
+      .fold()
+      .coalesce(
+        gremlinStatistics.unfold(),
+        gremlinConnection.addV('platformInstrument').property('platform', platformName).property('instrument', instrumentName)
+      )
+      .next()
+  } catch (error) {
+    console.error(`ERROR indexing instrument: ${error}`)
+  }
+
+  const { value: vertexValue = {} } = piVertex
+  const { id: piId } = vertexValue
+  console.log(`PlatformInstrument vertex [${piId}] indexed for collection [${dataset}]`)
+
+  await createAcquiredByEdge(piId, gremlinConnection, dataset)
+}
