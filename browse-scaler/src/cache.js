772410df5377904a50623f920d7e19ddf4e985a2
Jay Barra <jay.f.barra@gmail.com>
2021-08-11 17:45:50 -0400

    CMR-7342 whitespace fix

--- browse-scaler/src/cache.js
+++ browse-scaler/src/cache.js
@@ -2,34 +2,34 @@
 const redis = require('redis');
 const config = require ('./config');
 const { REDIS_URL,
-        REDIS_PORT,
-        REDIS_KEY_EXPIRE_SECONDS } = require ('./config');
+    REDIS_PORT,
+    REDIS_KEY_EXPIRE_SECONDS } = require ('./config');
 
 const { promisify } = require('util');
 
 const Cache = (function (cfg) {
-    // redis singleton
-    let instance;
+  // redis singleton
+  let instance;
 
-    /**
-     * getInstance: Retrieve the Redis client. Start new client if client is null or not connected. //
-     * @returns {RedisClient} RedisClient object containing the connection to Redis. //
-     */
-    function getInstance() {
-        if (!instance) {
-            instance = redis.createClient({
-                return_buffers: true,
-                host: cfg.REDIS_URL,
-                port: cfg.REDIS_PORT
-            }).on("error", (err) => {
-                console.error(`Failed to connect to Redis with error: ${err}.`);
-                instance.quit();
-            });
-        }
-
-        return instance;
+  /**
+   * getInstance: Retrieve the Redis client. Start new client if client is null or not connected. //
+   * @returns {RedisClient} RedisClient object containing the connection to Redis. //
+   */
+  function getInstance() {
+    if (!instance) {
+      instance = redis.createClient({
+        return_buffers: true,
+        host: cfg.REDIS_URL,
+        port: cfg.REDIS_PORT
+      }).on("error", (err) => {
+        console.error(`Failed to connect to Redis with error: ${err}.`);
+        instance.quit();
+      });
     }
-    return { getInstance };
+
+    return instance;
+  }
+  return { getInstance };
 }) ({REDIS_PORT, REDIS_URL});
 
 exports.Cache = Cache;
@@ -40,11 +40,11 @@ exports.Cache = Cache;
  * @param {Buffer<Image>} image This is what you want the key to get
  */
 exports.cacheImage = (key, image, expiration = REDIS_KEY_EXPIRE_SECONDS) => {
-    Cache.getInstance().set(key, image, 'EX', expiration, err => {
-        if (err) {
-            console.error(`Unable to cache image ${key}: ${err}`);
-        }
-    });
+  Cache.getInstance().set(key, image, 'EX', expiration, err => {
+    if (err) {
+      console.error(`Unable to cache image ${key}: ${err}`);
+    }
+  });
 };
 
 /**
@@ -53,19 +53,19 @@ exports.cacheImage = (key, image, expiration = REDIS_KEY_EXPIRE_SECONDS) => {
  * @returns {Buffer<Image>} the image associated with given cache key or null if none is found
  */
 exports.getImageFromCache = async key => {
-    let client = Cache.getInstance();
-    return promisify(client.get).bind(client)(key)
-        .catch(err => {
-            console.error(`Unable to retrieve image ${key} from Redis.`);
-            return null;
-        })
-        .then(image => {
-            // workaround for bad cache entries that have the string "null" for the value
-            if (image && image !== 'null') {
-                console.log(`got image ${key} from cache`);
-                return image;
-            }
-            console.log(`image ${key} is not in cache`);
-            return null;
-        });
+  let client = Cache.getInstance();
+  return promisify(client.get).bind(client)(key)
+    .catch(err => {
+      console.error(`Unable to retrieve image ${key} from Redis.`);
+      return null;
+    })
+    .then(image => {
+      // workaround for bad cache entries that have the string "null" for the value
+      if (image && image !== 'null') {
+        console.log(`got image ${key} from cache`);
+        return image;
+      }
+      console.log(`image ${key} is not in cache`);
+      return null;
+    });
 };
