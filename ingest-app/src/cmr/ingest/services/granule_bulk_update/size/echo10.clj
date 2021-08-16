c978ea82b369b7697d9062977cb8cdedd4e1fc46
mitchstartzel <mstartzel16@gmail.com>
2021-08-16 13:10:26 -0400

    CMR-7504: Full Size Echo10 BGU functionality

--- ingest-app/src/cmr/ingest/services/granule_bulk_update/size/echo10.clj
+++ ingest-app/src/cmr/ingest/services/granule_bulk_update/size/echo10.clj
@@ -23,47 +23,58 @@
   "Throws an error indicating the size parent element (DataGranule) is missing"
   []
   (errors/throw-service-errors :invalid-data
-   ["Can't update <Checksum>: no parent <DataGranule> element"]))
+   ["Can't update Size: no parent <DataGranule> element"]))
 
-(defn- update-size-element
-  "Takes in a zipper location, call the given insert function to build a new Size element.
-   This new size element will either be added to the DataGranule or replace an existing size element."
-  [loc insert-fn size])
-  ;;if SizeMB, might need to add to the left
+(defn- get-sizes
+  "gets sizes"
+  [size]
+  (let [sizes (map string/trim (string/split size #","))
+        mb-vals (remove nil? (map #(re-find #"^[0-9]+\.[0-9]+$" %) sizes))
+        byte-vals (remove nil? (map #(re-find #"^[0-9]+$" %) sizes))
+        size-map (merge (when (seq mb-vals) {:SizeMBDataGranule (first mb-vals)})
+                        (when (seq byte-vals) {:DataGranuleSizeInBytes (first byte-vals)}))]
+    (when (or (< 2 (count sizes))
+              (< 1 (count mb-vals))
+              (< 1 (count byte-vals))
+              (not (or (seq mb-vals) (seq byte-vals))))
+      (errors/throw-service-errors :invalid-data
+       [(str "Can't update Size: invalid data specified. Please include at most one value for "
+             "DataGranuleSizeInBytes, and one value for SizeMBDataGranule, seperated by a comma.")]))
+    size-map))
 
 (defn- update-size-in-data-granule
-  "Takes the zipper loc of a DataGranule element in the xml, and updates the size element with
+  "Takes the zipper loc of a DataGranule element's leftmost child in the xml, and updates the size element with
    the provided values before returning the root"
-  [data-granule-loc size]
-  (let [start-loc (zip/down data-granule-loc)]
-    (loop [loc start-loc done false]
-      (if done
-        (zip/root loc)
-        (cond
-          ;; at a Size element, replace the node with updated value
-          (= :DataGranuleSizeInBytes (-> loc zip/node :tag))
-          (recur (update-size-element loc zip/replace size) true)
+  [loc sizes element]
+  (let [{:keys [DataGranuleSizeInBytes SizeMBDataGranule]} sizes]
 
-          (= :SizeMBDataGranule (-> loc zip/node :tag))
-          (recur (update-size-element loc zip/replace size) true)
+    (cond
+      ;; at a Size element, replace the node with updated value
+      (= :DataGranuleSizeInBytes element)
+      (if DataGranuleSizeInBytes
+        (let [xml-elem (xml/element :DataGranuleSizeInBytes {} DataGranuleSizeInBytes)]
+          (if (= :DataGranuleSizeInBytes (-> loc zip/node :tag))
+            (update-size-in-data-granule
+             (zip/right (zip/replace loc xml-elem))
+             sizes :SizeMBDataGranule)
+            (update-size-in-data-granule
+             (zip/insert-left loc xml-elem)
+             sizes :SizeMBDataGranule)))
+        (update-size-in-data-granule loc sizes :SizeMBDataGranule))
 
-          ;; at an element after size, add to the left
-          (some tags-after-size [(-> loc zip/node :tag)])
-          (recur (update-size-element loc zip/insert-left size) true)
+      (and (= :SizeMBDataGranule element) SizeMBDataGranule)
+      (let [xml-elem (xml/element :SizeMBDataGranule {} SizeMBDataGranule)]
+        (zip/root (if (= :SizeMBDataGranule (-> loc zip/node :tag))
+                    (zip/replace loc xml-elem)
+                    (zip/insert-left loc xml-elem))))
 
-          ;have reached the end of the DataGranule without encountering any of the tags-after-size.
-          ;this condition should be impossible to actually trigger, as there are required fields after size.
-          (nil? loc)
-          (errors/throw-service-errors :invalid-data
-            ["Cannot update size: invalid DataGranule element detected."])
-
-          ;; no action needs to be taken, move to the next node
-          :else
-          (recur (zip/right loc) false))))))
+      ;; just return the root
+      :else
+      (zip/root loc))))
 
 (defn- update-data-granule-element
   "Take a parsed granule xml, update size"
-  [parsed size]
+  [parsed sizes]
   (let [zipper (zip/xml-zip parsed)
         start-loc (-> zipper zip/down)]
     (loop [loc start-loc]
@@ -71,7 +82,7 @@
         (cond
           ;; at a DataGranule element, attempt to update/add size inside this node
           (= :DataGranule (-> right-loc zip/node :tag))
-          (update-size-in-data-granule right-loc size)
+          (update-size-in-data-granule (zip/down right-loc) sizes :DataGranuleSizeInBytes)
 
           ;; at an element after DataGranule, cannot update granule
           (or (nil? right-loc) (some tags-after-data-granule [(-> right-loc zip/node :tag)]))
@@ -84,12 +95,12 @@
 (defn- update-size-metadata
   "Takes the granule ECHO10 xml"
   [gran-xml size]
-  (let [parsed (xml/parse-str gran-xml)]
-    (xml/indent-str (update-data-granule-element parsed size))))
+  (let [size-map (get-sizes size)
+        parsed (xml/parse-str gran-xml)]
+    (xml/indent-str (update-data-granule-element parsed size-map))))
 
 (defn update-size
   "Update the ECHO10 granule metadata"
   [concept size]
-  (let [size-vec (string/split size "z")
-        updated-metadata (update-size-metadata (:metadata concept) size-vec)]
+  (let [updated-metadata (update-size-metadata (:metadata concept) size)]
     (assoc concept :metadata updated-metadata)))
