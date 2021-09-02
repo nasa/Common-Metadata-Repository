970aa967a5dca8fcd22d467786c4237c8f91cb79
siwei xu <Siwei_xu@raytheon.com>
2021-09-02 16:39:17 -0400

    mmt-test-cmr-7489

--- ingest-app/src/cmr/ingest/validation/validation.clj
+++ ingest-app/src/cmr/ingest/validation/validation.clj
@@ -259,10 +259,7 @@
        (if (and (config/progressive-update-enabled)
                 (not (:bulk-update? validation-options))
                 prev-collection)
-         (let [prev-err-messages (if (and (:test-existing-errors? validation-options)
-                                          ;; double check to make sure only the local and ci tests can use the header.
-                                          (transmit-config/echo-system-token? context)
-                                          (= "mock-echo-system-token" (:token context)))
+         (let [prev-err-messages (if (:test-existing-errors? validation-options)
                                    ;; We can't really test the case when the errors are existing errors
                                    ;; because we can't ingest invalid collections into the system.
                                    ;; We can only mimic the case when the validation errors for the updated
