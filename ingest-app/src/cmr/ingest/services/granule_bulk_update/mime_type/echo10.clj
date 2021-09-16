1c2052d6da782d734dc931a17a59c5157f27575a
Jay Barra <jay.f.barra@gmail.com>
2021-09-16 09:58:44 -0400

    CMR-7536 expanded tests and improved nil handling

--- ingest-app/src/cmr/ingest/services/granule_bulk_update/mime_type/echo10.clj
+++ ingest-app/src/cmr/ingest/services/granule_bulk_update/mime_type/echo10.clj
@@ -2,7 +2,6 @@
   "Contains functions to update ECHO10 granule xml for OnlineResource MimeType bulk update."
   (:require
    [clojure.data.xml :as xml]
-   [clojure.set :as set]
    [clojure.zip :as zip]
    [cmr.common.services.errors :as errors]
    [cmr.common.xml :as cx]))
@@ -20,8 +19,7 @@
      :mime-type mime-type}))
 
 (defn- update-resources
-  "Take the parsed online resources in the original metadata and the desired OPeNDAP urls,
-   return the updated online resources xml element that can be used by zipper to update the xml."
+  "Constructs the new OnlineResources node in zipper representation"
   [online-resources url-map]
   (let [edn-resources (map xml-elem->online-resource online-resources)
         resources (map #(merge %
@@ -38,37 +36,87 @@
                       (xml/element :Type {} type)
                       (when mime-type (xml/element :MimeType {} mime-type))))))))
 
-(defn- update-xml
-  "Take a parsed granule xml, add an OnlineResources node with the given parsed OPeNDAP url.
+(defn- xml-elem->online-access-url
+  "Parses and returns XML element for OnlineAccessURL"
+  [elem]
+  (let [url (cx/string-at-path elem [:URL])
+        description (cx/string-at-path elem [:URLDescription])
+        mime-type (cx/string-at-path elem [:MimeType])]
+    {:url url
+     :url-description description
+     :mime-type mime-type}))
+
+(defn- update-access
+  "Constructs the new OnlineAccessURLs node in zipper representation"
+  [online-access-urls url-map]
+  (let [edn-access-urls (map xml-elem->online-access-url online-access-urls)
+        access-urls (map #(merge %
+                                 (when-let [mime-type (get url-map (:url %))]
+                                   {:mime-type mime-type}))
+                         edn-access-urls)]
+    (xml/element
+     :OnlineAccessURLs {}
+     (for [r access-urls]
+       (let [{:keys [url url-description mime-type]} r]
+         (xml/element :OnlineAccessURL {}
+                      (xml/element :URL {} url)
+                      (when url-description (xml/element :URLDescription {} url-description))
+                      (when mime-type (xml/element :MimeType {} mime-type))))))))
+
+(defn- replace-in-tree
+  "Take a parsed granule xml, replace the given node with the provided replacement
   Returns the zipper representation of the updated xml."
-  [replacement parsed]
-  (let [zipper (zip/xml-zip parsed)
+  [tree element-tag replacement]
+  (let [zipper (zip/xml-zip tree)
         start-loc (zip/down zipper)]
     (loop [loc start-loc done false]
       (if done
         (zip/root loc)
-        (let [right-loc (zip/right loc)]
+        (if-let [right-loc (zip/right loc)]
           (cond
             ;; at an OnlineResources element, replace the node with updated value
-            (= :OnlineResources (-> right-loc zip/node :tag))
+            (= element-tag (-> right-loc zip/node :tag))
             (recur (zip/replace right-loc replacement) true)
 
             ;; no action needs to be taken, move to the next node
             :else
-            (recur right-loc false)))))))
+            (recur right-loc false))
+          (recur loc true))))))
+
+(defn- update-online-resources
+  "Return an OnlineResources node where MimeType is updated where the URL has a matching entry in the url-map."
+  [tree url-map]
+  (let [online-resources (cx/elements-at-path
+                          tree
+                          [:OnlineResources :OnlineResource])]
+    (when (seq online-resources)
+      (update-resources online-resources url-map))))
+
+(defn- update-online-access-urls
+  "Return an OnlineAccessURLs node where MimeType is updated where the URL has a matching entry in the url-map."
+  [tree url-map]
+  (let [online-access-urls (cx/elements-at-path
+                            tree
+                            [:OnlineAccessURLs :OnlineAccessURL])]
+    (when (seq online-access-urls)
+      (update-access online-access-urls url-map))))
 
 (defn update-mime-type
+  "Replace the MimeType for elements within OnlineResources and OnlineAccess."
   [concept links]
+
   (let [parsed (xml/parse-str (:metadata concept))
-        online-resources (cx/elements-at-path
-                          parsed
-                          [:OnlineResources :OnlineResource])
+
         url-map (apply merge (map #(hash-map (:URL %) (:MimeType %)) links))
         _ (when-not (= (count (set (keys url-map))) (count links))
             (errors/throw-service-errors :invalid-data
                                          ["Update failed - duplicate URLs provided for granule update"]))
-        updated-metadata (-> online-resources
-                             (update-resources url-map)
-                             (update-xml parsed)
+
+        online-resources (update-online-resources parsed url-map)
+        access-urls (update-online-access-urls parsed url-map)
+
+        updated-metadata (-> parsed
+                             (replace-in-tree :OnlineResources online-resources)
+                             (replace-in-tree :OnlineAccessURLs access-urls)
                              xml/indent-str)]
     (assoc concept :metadata updated-metadata)))
--- ingest-app/test/cmr/ingest/services/granule_bulk_update/mime_type/echo10_test.clj
+++ ingest-app/test/cmr/ingest/services/granule_bulk_update/mime_type/echo10_test.clj
@@ -1,7 +1,6 @@
 (ns cmr.ingest.services.granule-bulk-update.mime-type.echo10-test
   (:require
    [clojure.data.xml :as xml]
-   [clojure.string :as string]
    [clojure.test :refer [deftest testing is]]
    [cmr.common.util :as util :refer [are3]]
    [cmr.common.xml :as cx]
@@ -14,6 +13,11 @@
         <OnlineAccessURL>
             <URL>https://podaac-tools.jpl.nasa.gov/drive/files/allData/ghrsst/data/GDS2/L4/GLOB/JPL/MUR/v4.1/2002/153/20020602090000-JPL-L4_GHRSST-SSTfnd-MUR-GLOB-v02.0-fv04.1.nc</URL>
             <URLDescription>The HTTP location for the granule.</URLDescription>
+        </OnlineAccessURL>
+        <OnlineAccessURL>
+            <URL>https://link-3.com</URL>
+            <URLDescription>The HTTP location for the granule.</URLDescription>
+            <MimeType>text/html</MimeType>
         </OnlineAccessURL>
     </OnlineAccessURLs>
     <OnlineResources>
@@ -33,19 +37,115 @@
     <DataFormat>NETCDF</DataFormat>
 </Granule>")
 
+(def echo10-metadata-no-access "
+<Granule>
+    <GranuleUR>20020602090000-JPL-L4_GHRSST-SSTfnd-MUR-GLOB-v02.0-fv04.1.nc</GranuleUR>
+    <OnlineResources>
+        <OnlineResource>
+            <URL>https://link-1.com</URL>
+            <Description>The link to be updated</Description>
+            <Type>USE SERVICE API : OPENDAP DATA</Type>
+            <MimeType>application/error+json</MimeType>
+        </OnlineResource>
+        <OnlineResource>
+            <URL>https://link-2.com</URL>
+            <Description>The link to be ignored</Description>
+            <Type>documentation</Type>
+            <MimeType>text/html</MimeType>
+        </OnlineResource>
+    </OnlineResources>
+    <DataFormat>NETCDF</DataFormat>
+</Granule>")
+
+(def echo10-metadata-no-resources "
+<Granule>
+    <GranuleUR>20020602090000-JPL-L4_GHRSST-SSTfnd-MUR-GLOB-v02.0-fv04.1.nc</GranuleUR>
+    <OnlineAccessURLs>
+        <OnlineAccessURL>
+            <URL>https://podaac-tools.jpl.nasa.gov/drive/files/allData/ghrsst/data/GDS2/L4/GLOB/JPL/MUR/v4.1/2002/153/20020602090000-JPL-L4_GHRSST-SSTfnd-MUR-GLOB-v02.0-fv04.1.nc</URL>
+            <URLDescription>The HTTP location for the granule.</URLDescription>
+        </OnlineAccessURL>
+        <OnlineAccessURL>
+            <URL>https://link-3</URL>
+            <URLDescription>The HTTP location for the granule.</URLDescription>
+            <MimeType>text/html</MimeType>
+        </OnlineAccessURL>
+    </OnlineAccessURLs>
+    <DataFormat>NETCDF</DataFormat>
+</Granule>")
+
+(def echo10-metadata-no-resources-no-access "
+<Granule>
+    <GranuleUR>20020602090000-JPL-L4_GHRSST-SSTfnd-MUR-GLOB-v02.0-fv04.1.nc</GranuleUR>
+    <DataFormat>NETCDF</DataFormat>
+</Granule>")
+
 (deftest update-mime-type-test
-  (let [granule {:metadata echo10-metadata}
+  (testing "single link provided to update"
+    (let [granule {:metadata echo10-metadata}
 
-        updated-gran(echo10/update-mime-type granule {"https://link-1.com" "application/echo10+xml"})
+          updated-gran (echo10/update-mime-type granule
+                                                [{:URL "https://link-1.com"
+                                                  :MimeType "application/echo10+xml"}])
 
-        metadata (:metadata updated-gran)
-        online-resources (cx/elements-at-path
-                          (xml/parse-str metadata)
-                          [:OnlineResources :OnlineResource])]
-    (is (not-empty updated-gran))
-    (is (string? metadata))
-    (is (= 2 (count online-resources)))
+          metadata (:metadata updated-gran)
+          online-resources (cx/elements-at-path
+                            (xml/parse-str metadata)
+                            [:OnlineResources :OnlineResource])
+          access-urls (cx/elements-at-path
+                       (xml/parse-str metadata)
+                       [:OnlineAccessURLs :OnlineAccessURL])]
 
-    (testing "only the specified link is updated"
-      (is (= "application/echo10+xml" (cx/string-at-path (first online-resources) [:MimeType])))
-      (is (= "text/html" (cx/string-at-path (second online-resources) [:MimeType]))))))
+      (is (not-empty updated-gran))
+      (is (string? metadata))
+      (is (= 2 (count online-resources)))
+      (is (= 2 (count access-urls)))
+
+      (testing "only the specified link is updated"
+        (is (= "application/echo10+xml" (cx/string-at-path (first online-resources) [:MimeType])))
+        (is (= "text/html" (cx/string-at-path (second online-resources) [:MimeType]))))))
+
+  (testing "multiple links provided"
+    (let [granule {:metadata echo10-metadata}
+
+          updated-gran (echo10/update-mime-type granule
+                                                [{:URL "https://link-1.com"
+                                                  :MimeType "application/echo10+xml"}
+                                                 {:URL "https://link-3.com"
+                                                  :MimeType "application/gzip"}])
+
+          metadata (:metadata updated-gran)
+          online-resources (cx/elements-at-path
+                            (xml/parse-str metadata)
+                            [:OnlineResources :OnlineResource])
+          access-urls (cx/elements-at-path
+                       (xml/parse-str metadata)
+                       [:OnlineAccessURLs :OnlineAccessURL])]
+
+      (testing "the specified links are updated"
+        (is (= "application/echo10+xml" (cx/string-at-path (first online-resources) [:MimeType])))
+        (is (= "application/gzip" (cx/string-at-path (second access-urls) [:MimeType]))))))
+
+  (testing "throws exceptions when duplicate URLs are provided"
+    (let [granule {:metadata echo10-metadata}]
+      (is (thrown-with-msg? clojure.lang.ExceptionInfo
+                            #"Update failed - duplicate URLs provided.*"
+                            (echo10/update-mime-type
+                             granule
+                             [{:URL "https://link-1.com"
+                               :MimeType "application/echo10+xml"}
+                              {:URL "https://link-1.com"
+                               :MimeType "application/gzip"}])))))
+
+  (testing "Handles xml regardless of which elements are present where"
+    (are3 [metadata]
+          (let [granule {:metadata metadata}
+                output (echo10/update-mime-type
+                        granule
+                        [{:URL "https://foo.com" :MimeType "application/baz+xml"}])]
+            (is (string? (:metadata output))))
+
+          "Both OnlineResources and OnlineAccessURLs present" echo10-metadata
+          "Only OnlineAccessURLs" echo10-metadata-no-resources
+          "Only OnlineResoruces" echo10-metadata-no-access
+          "No Online elements" echo10-metadata-no-resources-no-access)))
