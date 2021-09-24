39ff5a4c922082f317181d0763c7a18bc722fd47
sxu123 <wind.cloud.sky@gmail.com>
2021-09-24 13:06:44 -0400

    CMR-7498: Added kms keywords search for mime-type. (#1349)
    * CMR-7498: Added kms keywords search for mime-type.
    
    * CMR-7498: Fixed a test.
    
    * CMR-7498: Added another test.
    
    Co-authored-by: siwei xu <Siwei_xu@raytheon.com>

--- system-int-test/test/cmr/system_int_test/search/keyword_endpoint_test.clj
+++ system-int-test/test/cmr/system_int_test/search/keyword_endpoint_test.clj
@@ -570,7 +570,152 @@
                           {"value" "JPEG",
                            "uuid"
                            [{"value" "7443bb2d-1dbb-44d1-bd29-0241d30fbc57",
-                             "uuid" "7443bb2d-1dbb-44d1-bd29-0241d30fbc57"}]}]}})
+                             "uuid" "7443bb2d-1dbb-44d1-bd29-0241d30fbc57"}]}]}
+    :mime-type {"mime_type"
+                [{"value" "text/css"
+                  "uuid"
+                  [{"value" "3195dfce-51db-4b40-aadb-808b43573743"
+                    "uuid" "3195dfce-51db-4b40-aadb-808b43573743"}]}
+                  {"value" "application/x-vnd.iso.19139-2+xml"
+                   "uuid"
+                   [{"value" "c1a8dbb7-312d-4481-998e-58d126b32080"
+                     "uuid" "c1a8dbb7-312d-4481-998e-58d126b32080"}]}
+                  {"value" "application/xml"
+                   "uuid"
+                   [{"value" "dd6c5cea-4100-4973-9ba9-659fdd7fd608"
+                     "uuid" "dd6c5cea-4100-4973-9ba9-659fdd7fd608"}]}
+                  {"value" "image/jpeg"
+                   "uuid"
+                   [{"value" "3f697f52-6a1c-4e2c-bd4b-13aaaf45f2e6"
+                     "uuid" "3f697f52-6a1c-4e2c-bd4b-13aaaf45f2e6"}]}
+                  {"value" "application/x-netcdf"
+                   "uuid"
+                   [{"value" "2b192915-32a8-4b68-a720-8ca8a84f04ca"
+                     "uuid" "2b192915-32a8-4b68-a720-8ca8a84f04ca"}]}
+                  {"value" "application/zip"
+                   "uuid"
+                   [{"value" "4e5db77b-bc1d-4f7c-9f13-e3e54e0b2e3b"
+                     "uuid" "4e5db77b-bc1d-4f7c-9f13-e3e54e0b2e3b"}]}
+                  {"value" "application/tar"
+                   "uuid"
+                   [{"value" "84ef762f-e348-42a6-981c-563822a47806"
+                     "uuid" "84ef762f-e348-42a6-981c-563822a47806"}]}
+                  {"value" "application/vnd.ms-excel"
+                   "uuid"
+                   [{"value" "7c99ff72-5239-424d-a0bf-9712c33ea76d"
+                     "uuid" "7c99ff72-5239-424d-a0bf-9712c33ea76d"}]}
+                  {"value" "application/msword"
+                   "uuid"
+                   [{"value" "c79a0e11-2774-4cf3-a194-45b9e58a93fd"
+                     "uuid" "c79a0e11-2774-4cf3-a194-45b9e58a93fd"}]}
+                  {"value" "text/javascript"
+                   "uuid"
+                   [{"value" "40cb0bdd-67f4-43c8-aa57-2bdc260e6950"
+                     "uuid" "40cb0bdd-67f4-43c8-aa57-2bdc260e6950"}]}
+                  {"value" "application/tar+zip"
+                   "uuid"
+                   [{"value" "17e82b7c-498d-4d69-993c-fd691aa25ce8"
+                     "uuid" "17e82b7c-498d-4d69-993c-fd691aa25ce8"}]}
+                  {"value" "application/x-hdf5"
+                   "uuid"
+                   [{"value" "4e80047b-c50b-4805-ac68-789dbc38803f"
+                     "uuid" "4e80047b-c50b-4805-ac68-789dbc38803f"}]}
+                  {"value" "application/gzip"
+                   "uuid"
+                   [{"value" "a8ee535a-8bc8-46fd-8b97-917bd7ea7666"
+                     "uuid" "a8ee535a-8bc8-46fd-8b97-917bd7ea7666"}]}
+                  {"value" "application/octet-stream"
+                   "uuid"
+                   [{"value" "b77e64ef-ce80-4dab-b552-c6062990a6e0"
+                     "uuid" "b77e64ef-ce80-4dab-b552-c6062990a6e0"}]}
+                  {"value" "text/markdown"
+                   "uuid"
+                   [{"value" "b403039f-a107-4a84-88a1-29e4d1b30b0b"
+                     "uuid" "b403039f-a107-4a84-88a1-29e4d1b30b0b"}]}
+                  {"value" "text/csv"
+                   "uuid"
+                   [{"value" "2065aabb-9beb-4c84-8ad7-0e16cfed17cf"
+                     "uuid" "2065aabb-9beb-4c84-8ad7-0e16cfed17cf"}]}
+                  {"value" "application/vnd.google-earth.kml+xml"
+                   "uuid"
+                   [{"value" "80045dcb-18ee-463a-8baf-ffcabed510ea"
+                     "uuid" "80045dcb-18ee-463a-8baf-ffcabed510ea"}]}
+                  {"value" "image/bmp"
+                   "uuid"
+                   [{"value" "b7687b8f-7a24-4150-bd9d-28e0d53f7554"
+                     "uuid" "b7687b8f-7a24-4150-bd9d-28e0d53f7554"}]}
+                  {"value" "application/x-bufr"
+                   "uuid"
+                   [{"value" "e384b8a8-8cec-4230-9ebe-4db76bbef706"
+                     "uuid" "e384b8a8-8cec-4230-9ebe-4db76bbef706"}]}
+                  {"value" "application/tar+gzip"
+                   "uuid"
+                   [{"value" "43ca8ee0-04a5-4020-b0ec-998ec0e0f30e"
+                     "uuid" "43ca8ee0-04a5-4020-b0ec-998ec0e0f30e"}]}
+                  {"value" "application/pdf"
+                   "uuid"
+                   [{"value" "627269ae-ba93-492e-8c31-cc4de1d69810"
+                     "uuid" "627269ae-ba93-492e-8c31-cc4de1d69810"}]}
+                  {"value" "application/opensearchdescription+xml"
+                   "uuid"
+                   [{"value" "07bcc60e-1551-44d9-b87e-7c260d230ecb"
+                     "uuid" "07bcc60e-1551-44d9-b87e-7c260d230ecb"}]}
+                  {"value" "application/x-hdfeos"
+                   "uuid"
+                   [{"value" "b1eac265-2b00-4c39-a429-797c13a2c640"
+                     "uuid" "b1eac265-2b00-4c39-a429-797c13a2c640"}]}
+                  {"value" "text/html"
+                   "uuid"
+                   [{"value" "415a10b5-7286-4195-a88e-00c7b995b7d0"
+                     "uuid" "415a10b5-7286-4195-a88e-00c7b995b7d0"}]}
+                  {"value" "application/vnd.opendap.dap4.dmrpp+xml"
+                   "uuid"
+                   [{"value" "b26761fa-8d8e-4bd8-a8ba-db6575554ad7"
+                     "uuid" "b26761fa-8d8e-4bd8-a8ba-db6575554ad7"}]}
+                  {"value" "application/gml+xml"
+                   "uuid"
+                   [{"value" "40bdf6e5-780c-43e2-ab8e-e5dfae4bd779"
+                     "uuid" "40bdf6e5-780c-43e2-ab8e-e5dfae4bd779"}]}
+                  {"value" "image/png"
+                   "uuid"
+                   [{"value" "edb9e800-ec31-4d5c-848d-c548fd151db2"
+                     "uuid" "edb9e800-ec31-4d5c-848d-c548fd151db2"}]}
+                  {"value" "application/x-hdf"
+                   "uuid"
+                   [{"value" "b0a3e733-4d1b-486f-b56c-c405a5e4367b"
+                     "uuid" "b0a3e733-4d1b-486f-b56c-c405a5e4367b"}]}
+                  {"value" "text/plain"
+                   "uuid"
+                   [{"value" "fea4e0a7-d794-481d-9915-52f1be226714"
+                     "uuid" "fea4e0a7-d794-481d-9915-52f1be226714"}]}
+                  {"value" "application/json"
+                   "uuid"
+                   [{"value" "8542dd4a-a11b-475d-8d46-cad785a7f510"
+                     "uuid" "8542dd4a-a11b-475d-8d46-cad785a7f510"}]}
+                  {"value" "image/vnd.collada+xml"
+                   "uuid"
+                   [{"value" "d3ef6fe7-b6cd-45b4-9a27-d42fa3289116"
+                     "uuid" "d3ef6fe7-b6cd-45b4-9a27-d42fa3289116"}]}
+                  {"value" "image/tiff"
+                   "uuid"
+                   [{"value" "3e048f9e-8f93-4f0c-9f0b-20bafb909c68"
+                     "uuid" "3e048f9e-8f93-4f0c-9f0b-20bafb909c68"}]}
+                  {"value" "application/vnd.google-earth.kmz"
+                   "uuid"
+                   [{"value" "f7328bf5-8ef2-4f95-a4e0-6fb16d122237"
+                     "uuid" "f7328bf5-8ef2-4f95-a4e0-6fb16d122237"}]}
+                  {"value" "text/xml"
+                   "uuid"
+                   [{"value" "091b6afc-ab75-4790-8e71-3b32ae8bd0a4"
+                     "uuid" "091b6afc-ab75-4790-8e71-3b32ae8bd0a4"}]}
+                  {"value" "application/x-tar-gz"
+                   "uuid"
+                   [{"value" "5e70beda-396e-4cc8-bdd5-70dfc8a1142e"
+                     "uuid" "5e70beda-396e-4cc8-bdd5-70dfc8a1142e"}]}
+                  {"value" "image/gif"
+                   "uuid"
+                   [{"value" "ad61b259-8131-4e0e-aac8-a800a0a51ca6"
+                     "uuid" "ad61b259-8131-4e0e-aac8-a800a0a51ca6"}]}]}})
 
 
 (deftest get-keywords-test
@@ -606,6 +751,9 @@
     "Testing correct keyword hierarchy returned for spatial keywords."
     :spatial-keywords (:spatial-keywords expected-hierarchy)
 
+    "Testing correct keyword hierarchy returned for mimetype"
+    :mime-type (:mime-type expected-hierarchy)
+
     "Testing correct keyword heirarchy returned for granule data format"
     :granule-data-format (:granule-data-format expected-hierarchy)))
 
@@ -620,7 +768,7 @@
     (is (= {:status 400
             :errors [(str "The keyword scheme [foo] is not supported. Valid schemes are:"
                           " providers, measurement_name, spatial_keywords, granule_data_format,"
-                          " related_urls, iso_topic_categories,"
+                          " mime_type, related_urls, iso_topic_categories,"
                           " instruments, science_keywords, concepts, temporal_keywords, platforms,"
                           " archive_centers, data_centers, location_keywords, and projects.")]}
            (search/get-keywords-by-keyword-scheme :foo)))))
