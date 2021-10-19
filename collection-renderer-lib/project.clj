7414cfa1161d8d6be7cdabb1d438515ee2785097
mitchstartzel <mstartzel16@gmail.com>
2021-10-19 16:53:12 -0400

    CMR-7811: Locked racc in preview gem branch

--- collection-renderer-lib/project.clj
+++ collection-renderer-lib/project.clj
@@ -12,7 +12,7 @@
   {:repo "https://git.earthdata.nasa.gov/scm/cmr/cmr_metadata_preview.git"
    :version "cmr_metadata_preview-0.2.9"
    :commit-id (or (System/getenv "CMR_METADATA_PREVIEW_COMMIT")
-                  "4bdb1669787")})
+                  "f980f9ec0f2")})
 
 (def gem-install-path
   "The directory within this library where Ruby gems are installed."
