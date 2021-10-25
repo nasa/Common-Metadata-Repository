12e63c78f4d1ad024c476037f370e639b156ba0c
Mark Schmele <mark.h.schmele@nasa.gov>
2021-10-25 13:18:56 -0600

    CMR-7722: add test for relatedurls

--- graph-db/serverless/src/utils/cmr/__test__/indexRelatedUrl.test.js
+++ graph-db/serverless/src/utils/cmr/__test__/indexRelatedUrl.test.js
@@ -1,5 +1,6 @@
 import { updateCollection } from '../../../testUtil/indexCollection'
 import { verifyRelatedUrlExistInGraphDb } from '../../../testUtil/verifyRelatedUrl'
+import { indexRelatedUrl } from '../indexRelatedUrl'
 
 describe('indexRelatedUrl', () => {
   describe('when the provided relatedUrl is correct', () => {
@@ -40,6 +41,27 @@ describe('indexRelatedUrl', () => {
       )
 
       verifyRelatedUrlExistInGraphDb('Vulputate Mollis Commodo', 'https://example.com/test.json')
+    })
+  })
+
+  describe('when an exceptionn is thrown', () => {
+    test('it is caught and a new one is thrown', async () => {
+      let errorMessage
+      const consoleError = jest.spyOn(console, 'error')
+      const relatedUrl = {
+        Type: 'VIEW RELATED INFORMATION',
+        Description: 'Nulla vitae elit libero, a pharetra augue.',
+        URL: 'https://example.com/test.json'
+      }
+
+      try {
+        await indexRelatedUrl(relatedUrl, null, {}, 'C1000000-CMR')
+      } catch (error) {
+        errorMessage = error.message
+      }
+
+      expect(consoleError).toBeCalledTimes(1)
+      expect(errorMessage).toEqual("Cannot read property 'addV' of null")
     })
   })
 })
