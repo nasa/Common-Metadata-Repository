c17aab79fc6edc212d27827b9069b64d8cc659b3
Brian Alexander <brianalexander28@gmail.com>
2022-01-27 12:27:44 -0800

    CMR-7854: Refactor are3 tests allowing for error messages

--- system-int-test/test/cmr/system_int_test/ingest/subscription_time_frame_job_test.clj
+++ system-int-test/test/cmr/system_int_test/ingest/subscription_time_frame_job_test.clj
@@ -3,54 +3,48 @@
   (:require
    [cheshire.core :as json]
    [clj-http.client :as client]
-   [clojure.test :refer [deftest is join-fixtures testing use-fixtures]]
+   [clojure.test :refer [deftest is  testing]]
    [cmr.common.util :as util :refer [are3]]
-   [cmr.mock-echo.client.echo-util :as echo-util]
    [cmr.system-int-test.system :as system]
-   [cmr.system-int-test.utils.dev-system-util :as dev-system]
-   [cmr.system-int-test.utils.ingest-util :as ingest]
    [cmr.system-int-test.utils.url-helper :as url]))
 
-(use-fixtures :each
-  (join-fixtures
-   [(ingest/reset-fixture {"provguid1" "PROV1" "provguid2" "PROV2"}
-                          {:grant-all-search? false
-                           :grant-all-ingest? false})
-    (dev-system/freeze-resume-time-fixture)]))
-
 (defn- revision-date-range-query-with-response-status
-  [query-params expected-status]
-  (let [response (client/post (url/email-subscription-processing)
-                              {:accept "application/json"
-                               :query-params query-params
-                               :connection-manager (system/conn-mgr)
-                               :throw-exceptions false})]
-    (is (= expected-status
-           (:status response)))))
+  ([query-params expected-status]
+   (revision-date-range-query-with-response-status query-params expected-status nil))
+  ([query-params expected-status expected-msg]
+   (let [response (client/post (url/email-subscription-processing)
+                               {:accept "application/json"
+                                :query-params (merge {:token "mock-echo-system-token"} query-params)
+                                :connection-manager (system/conn-mgr)
+                                :throw-exceptions false})
+         errors (:errors (json/decode (:body response) true))]
+     (is (= expected-status
+            (:status response)))
+     (when expected-msg
+       (is (= [expected-msg] errors))))))
 
 (deftest subscriptions-job-endpoint-test
-  (let [admin-update-group (echo-util/get-or-create-group (system/context) "admin-update-group")
-        admin-update-token (echo-util/login (system/context) "admin1" [admin-update-group])
-        _ (echo-util/grant-group-admin (system/context) admin-update-group :update)
-        non-admin-token (echo-util/login (system/context) "user1")]
-    (testing "subscription-job-endpoint-test"
-      (are3 [query-params expected-status]
-        (revision-date-range-query-with-response-status query-params expected-status)
+  (testing "subscription-job-endpoint-test"
+    (are3 [query-params expected-status expected-msg]
+      (revision-date-range-query-with-response-status query-params expected-status expected-msg)
 
-        "returns 200 with valid time range format"
-        {:token admin-update-token :revision-date-range "2000-01-01T10:00:00Z,2010-03-10T12:00:00Z"} 200
+      "returns 400 when the start time is after the end time"
+      {:revision-date-range "2010-01-01T10:00:00Z,2000-03-10T12:00:00Z"} 400 "The start date should occur before the end date."
 
-        "returns 422 with invalid time range format"
-        {:token admin-update-token :revision-date-range "2000-01-01T10:00:Z,2010-03-10T12:00:00Z"} 422
+      "returns 400 when the start time equals the end time"
+      {:revision-date-range "2000-03-10T12:00:00Z,2000-03-10T12:00:00Z"} 400 "The start date should occur before the end date."
 
-        "returns 400 when the start time is after the end time"
-        {:token admin-update-token :revision-date-range "2010-01-01T10:00:00Z,2000-03-10T12:00:00Z"} 400
+      "returns 400 when only a start-date is provided"
+      {:revision-date-range "2000-01-01T10:00:00Z,"} 400 "Missing end date in revision-date-range."
 
-        "returns 400 when the start time equals the end time"
-        {:token admin-update-token :revision-date-range "2000-03-10T12:00:00Z,2000-03-10T12:00:00Z"} 400
+      "returns 400 when only an end-date is provided"
+      {:revision-date-range ",2010-03-10T12:00:00Z"} 400 "Missing start date in revision-date-range.")
 
-        "returns 400 when only a start-date is provided"
-        {:token admin-update-token :revision-date-range "2000-01-01T10:00:00Z,"} 400
+    (are3 [query-params expected-status]
+      (revision-date-range-query-with-response-status query-params expected-status)
 
-        "returns 400 when only an end-date is provided"
-        {:token admin-update-token :revision-date-range ",2010-03-10T12:00:00Z"} 400))))
+      "returns 200 with valid time range format"
+      {:revision-date-range "2000-01-01T10:00:00Z,2010-03-10T12:00:00Z"} 200
+
+      "returns 422 with invalid time range format"
+      {:revision-date-range "2000-01-01T10:00:Z,2010-03-10T12:00:00Z"} 422)))
