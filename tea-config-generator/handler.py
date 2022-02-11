01da545f64fc8c52adb142a8e86998dc99fc4568
Thomas Cherry <thomas.a.cherry@nasa.gov>
2022-02-11 13:32:39 -0500

    MMT-2798 - handler cleanup

--- tea-config-generator/handler.py
+++ tea-config-generator/handler.py
@@ -5,6 +5,7 @@ AWS lambda functions for generating a TEA configuration.
 import logging
 import os
 import json
+import datetime
 import boto3
 
 import CreateTeaConfig as tea
@@ -14,9 +15,32 @@ import CreateTeaConfig as tea
 # ******************************************************************************
 #mark - Utility functions
 
-def aws_return_message(status, body):
+def finish_timer(start):
+    """ Calculate and format the number of seconds from start to 'now'. """
+    delta = datetime.datetime.now() - start
+    sec = delta.total_seconds()
+    return f"{sec:.6f}"
+
+def aws_return_message(event, status, body, start=None):
     """ build a dictionary which AWS Lambda will accept as a return value """
-    return {"statusCode": status, "body": json.dumps(body, indent=1)}
+
+    # look for pretty in both the header and query parameters
+    indent = None # none means do not pretty print, a number is the tab count
+    pretty = 'false'
+    query_params = event.get('queryStringParameters')
+    if query_params:
+        pretty = query_params.get('pretty', "false")
+    if pretty=='false' and 'Cmr-Pretty' in event['headers']:
+        pretty = event['headers']['Cmr-Pretty']
+    if pretty.lower()=="true":
+        indent=1
+
+    ret = {"statusCode": status, "body": json.dumps(body, indent=indent)}
+    if start is not None:
+        if 'headers' not in ret:
+            ret['headers'] = {}
+        ret['headers']['cmr-took'] = finish_timer(start)
+    return ret
 
 def parameter_read(pname, default_value=''):
     """
@@ -44,41 +68,43 @@ def capability(event, endpoint, name, description):
 
 def capabilities(event, context):
     """ Return a static output stating the calls supported by this package """
+    start = datetime.datetime.now()
     body = {
         'urls': [
-            capability(event, '/', 'capabilities', 'what can be done here'),
-            capability(event, '/capabilities', 'capabilities', 'what can be done here'),
-            capability(event, '/status', 'Simple status', 'status'),
-            capability(event, '/<provider>', 'Generate', 'Generate a TEA config for provider'),
+            capability(event, '/', 'Root', 'Alias for capabilities'),
+            capability(event, '/capabilities', 'Capabilities',
+                'Show which endpoints exist on this service'),
+            capability(event, '/status', 'Status', 'Returns service status'),
+            capability(event, '/provider/<provider>', 'Generate',
+                'Generate a TEA config for provider'),
         ]
     }
-    return aws_return_message(200, body)
+    return aws_return_message(event, 200, body, start)
 
 def debug(event, context):
     """ Return debug information about AWS in general """
+    start = datetime.datetime.now()
     body = {
-        'arn': f"Lambda function ARN:{context.invoked_function_arn}",
-        'log_stream' : f"CloudWatch log stream name: {context.log_stream_name}",
-        'group' : f"CloudWatch log group name:{context.log_group_name}",
-        'request-id': f"Lambda Request ID:{context.aws_request_id}",
-        'memory-limit': f"Lambda function memory limits in MB:{context.memory_limit_in_mb}",
-        'remaining-time': f"Lambda time remaining in MS:{context.get_remaining_time_in_millis()}",
+        'context': context.get_remaining_time_in_millis(),
         'event': event,
         'tea_config_cmr': parameter_read('aws_tea_config_cmr',
             default_value='https://cmr.earthdata.nasa.gov'),
+        'tea_config_log_level': parameter_read('aws_tea_config_log_level',
+            default_value='INFO'),
     }
 
     for env, value in os.environ.items():
         if env.startswith('AWS_'):
             body[env] = value
-    return aws_return_message(200, body)
+    return aws_return_message(event, 200, body, start)
 
-def teapot(event, context):
+def health(event, context):
     """
-    Provide an enpoint for testing service availability and for complicance with
+    Provide an endpoint for testing service availability and for complicance with
     RFC 7168
     """
-    return aws_return_message(418, "I'm a teapot")
+    return aws_return_message(event, 418, "I'm a teapot", datetime.datetime.now())
+
 
 def generate_tea_config(event, context):
     """
@@ -88,55 +114,83 @@ def generate_tea_config(event, context):
     * Path Parameter named 'id' with CMR provider name
     * HTTP Header named 'Cmr-Token' with a CMR compatible token
     """
+    start = datetime.datetime.now()
+
+    provider = event['pathParameters'].get('id')
+    if provider is None or len(provider)<1:
+        return aws_return_message(event, 400, "Provider is required", start)
+    token = event['headers'].get('Cmr-Token')
+    if token is None or len(token)<1:
+        return aws_return_message(event, 400, "Token is required", start)
+
     cmr_config = parameter_read('aws_tea_config_cmr',
       default_value='https://cmr.earthdata.nasa.gov')
+    log_level = parameter_read('aws_tea_config_log_level',
+      default_value='info')
 
-    provider = event['pathParameters']['id']
-    token = event['headers']['Cmr-Token']
-
-    env = {'cmr-url':cmr_config, 'logging-level': 'info'}
+    env = {'cmr-url':cmr_config, 'logging-level': log_level}
 
     processor = tea.CreateTeaConfig()
     result = processor.create_tea_config(env, provider, token)
+    result['headers'] = {'cmr-took': finish_timer(start)}
 
-    #result = {'env': env, 'provider': provider, 'token': token}
-
-    return aws_return_message(200, result)
+    return result
 
 # ******************************************************************************
 #mark - command line functions
 
 def main():
-    """ Used to test functions locally """
+    """ Unit testing, collect variables needed to run generate_tea_config() """
+    #pylint: disable=C0415 # library is only needed for local execution, not AWS
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Test handler')
+
+    aws_group = parser.add_argument_group()
+    aws_group.add_argument('-l', '--log-level', default='debug',
+        help="Set the python log level")
+    aws_group.add_argument('-p', '--provider', default='POCLOUD',
+        help="CMR Provider name such as POCLOUD")
+    aws_group.add_argument('-e', '--env', default='uat', help="sit, uat, ops")
+
+    token_ops = parser.add_mutually_exclusive_group()
+    token_ops.add_argument('-t', '--token', default='', help="EDL Token")
+    token_ops.add_argument('-tf', '--token-file', default='',
+        help="Path to a file holding an EDL Token")
+
+    args = parser.parse_args()
+
+    log_level = args.log_level.upper()
+    provider = args.provider
+    which_cmr = args.env
+
+    if args.token_file:
+        with open(args.token_file, encoding='utf8') as file_obj:
+            token = file_obj.readline().strip()
+    elif args.token:
+        token = args.token
+    else:
+        token = None
+
+    # Used to test functions locally
     if len(logging.getLogger().handlers)>0:
-        logging.getLogger().setLevel(logging.INFO)
+        logging.getLogger().setLevel(log_level)
     else:
         logging.basicConfig(filename='script.log',
-          format='%(asctime)s %(message)s',
-          #encoding='utf-8',
-          level=logging.DEBUG)
-
-    provider = input('Enter provider: (POCLOUD)')
-    if provider is None:
-        provider = 'POCLOUD'
-    
-    which_cmr = input('Enter env such as sit, uat or prod (uat): ')
-    if which_cmr is None:
-        which_cmr = 'uat'
-    
-    token = input('Enter EDL token: ')
+            format='%(asctime)s %(message)s',
+            level=log_level)
 
     cmrs = {'sit':'https://cmr.sit.earthdata.nasa.gov',
         'uat':'https://cmr.uat.earthdata.nasa.gov',
+        'ops':'https://cmr.earthdata.nasa.gov',
         'prod':'https://cmr.earthdata.nasa.gov'}
 
-    env = {'cmr-url': cmrs[which_cmr],
-        'logging_level': 'INFO'
-    }
+    os.environ['AWS_TEA_CONFIG_LOG_LEVEL'] = log_level
+    os.environ['AWS_TEA_CONFIG_LOG_CMR'] = cmrs[which_cmr]
+    event = {'headers': {'Cmr-Token': token}, 'pathParameters': {'id': provider}}
+    context = {}
 
-    processor = tea.CreateTeaConfig()
-    result = processor.create_tea_config(env, provider, token)
-    print (result)
+    print(generate_tea_config(event, context))
 
 if __name__ == "__main__":
     main()
