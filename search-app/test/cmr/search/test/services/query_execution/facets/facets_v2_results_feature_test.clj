37b5dcf63ca2b174406d39b93f91e0f3d6e489d7
siwei xu <Siwei_xu@raytheon.com>
2022-02-14 10:00:13 -0500

    CMR-8025: Fixed a test.

--- search-app/test/cmr/search/test/services/query_execution/facets/facets_v2_results_feature_test.clj
+++ search-app/test/cmr/search/test/services/query_execution/facets/facets_v2_results_feature_test.clj
@@ -75,6 +75,7 @@
                   {:priority
                    {:avg
                     {:field :processing-level-id-humanized.priority}}}}}},
+              :latency-h {:terms {:field :latency, :size 50}},
               :granule-data-format-h
                {:nested {:path :granule-data-format-humanized},
                 :aggs
