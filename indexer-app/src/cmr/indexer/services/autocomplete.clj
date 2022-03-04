bf1bb88d2890c96f52b336f64217f56df9bc3588
Erich Reiter <erich.e.reiter@nasa.gov>
2022-03-04 09:34:44 -0500

    CMR-8073 reducing reindexing batch size to 1 since the exception is still thrown at 10.

--- indexer-app/src/cmr/indexer/services/autocomplete.clj
+++ indexer-app/src/cmr/indexer/services/autocomplete.clj
@@ -203,7 +203,7 @@
          flatten
          (remove anti-value-suggestion?))))
 
-(def REINDEX_BATCH_SIZE 10)
+(def REINDEX_BATCH_SIZE 1)
 
 (defn-timed reindex-autocomplete-suggestions-for-provider
   "Reindex autocomplete suggestion for a given provider"
