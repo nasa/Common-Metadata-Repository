e4ff561b172f94ae6c46c6b11bea7b150a619d0e
eereiter <erich.e.reiter@nasa.gov>
2022-03-07 11:42:26 -0500

    Cmr 8073 - Changing the autocomplete and reindexing batch size to be operationally configurable  (#1484)
    * CMR-8073 the out of memory error is still occuring.
    
    * CMR-8073 reducing reindexing batch size to 1 since the exception is still thrown at 10.
    
    * CMR-8073 using defconfigs to allow operational changes to the reindex default values also changing the reindexing batch size per provider.
    
    * CMR-8073 Fixing test
    
    * CMR-8073 putting the batch size back to 10 since I deployed the wrong code
    
    * CMR-8073 adding NOAA_NCEI and upping the batch to 20
    
    * CMR-8073 changing the default reindex batch size back to 10

--- indexer-app/test/cmr/indexer/test/services/index_service.clj
+++ indexer-app/test/cmr/indexer/test/services/index_service.clj
@@ -2,6 +2,7 @@
   "Tests for index service"
   (:require
    [clojure.test :refer :all]
+   [cmr.common.util :refer [are3]]
    [cmr.indexer.services.index-service :as index-svc]))
 
 (deftest index-concept-invalid-input-test
@@ -15,3 +16,14 @@
          "C123-PROV1" nil
          nil 1
          nil nil)))
+
+(deftest determine-reindex-batch-size-test
+  (testing "determining the reindexing batch size based on the given provider."
+    (are3 [expected-size provider]
+      (is (= expected-size (index-svc/determine-reindex-batch-size provider)))
+
+      "Testing a provider that has normal sized collections."
+      (index-svc/collection-reindex-batch-size) "NSIDC"
+
+      "Testing a provider that has large sized collections."
+      (index-svc/collection-large-file-providers-reindex-batch-size) "GHRSSTCWIC")))
