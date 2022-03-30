1cf64ef3689e7e21851933ed60547afd29b8e4ab
Yonggang Liu <ygliuvt@gmail.com>
2022-03-30 09:42:20 -0400

    CMR-8114: Update browse-scaler to use Node.js v14.

--- browse-scaler/local_test.sh
+++ browse-scaler/local_test.sh
@@ -4,7 +4,8 @@ CMR_ENVIRONMENT="${CMR_ENVIRONMENT:-prod}"
 
 if [[ -f $1 ]]; then
     cd src
-    docker run --network=host --rm -e REDIS_URL=docker.for.mac.host.internal -e CMR_ROOT=$CMR_ROOT -e CMR_ENVIRONMENT=$CMR_ENVIRONMENT -v "$PWD":/var/task lambci/lambda:nodejs12.x index.handler "$( < ../$1 )"
+    echo "$PWD"
+    docker run --network=host --rm -e REDIS_URL=docker.for.mac.host.internal -e CMR_ROOT=$CMR_ROOT -e CMR_ENVIRONMENT=$CMR_ENVIRONMENT -v "$PWD":/var/task amazon/aws-lambda-nodejs:14 index.handler "$( < ../$1 )"
 else
     echo "Usage: ./local_test.sh <json event file>"
 fi
