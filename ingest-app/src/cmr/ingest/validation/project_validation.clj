60d54bdf050e87ee8d9d80434a94446709f96624
Erich Reiter <erich.e.reiter@nasa.gov>
2022-03-31 11:39:27 -0400

    CMR-8134 need to capitalize the project

--- ingest-app/src/cmr/ingest/validation/project_validation.clj
+++ ingest-app/src/cmr/ingest/validation/project_validation.clj
@@ -2,20 +2,9 @@
   "Provides functions to validate the projects during collection update"
   (:require
    [clojure.set :as s]
+   [clojure.string :as string]
    [cmr.common.util :as util]
    [cmr.ingest.services.humanizer-alias-cache :as humanizer-alias-cache]))
-
-(comment
-  (println concept-id)
-  (deleted-project-searches context concept-id concept prev-concept)
-  platform-alias-map (get (humanizer-alias-cache/get-humanizer-alias-map context) "platform")
-        current-platforms (map :ShortName (:Platforms concept))
-        previous-platforms (map :ShortName (:Platforms prev-concept))
-        platform-aliases (mapcat #(get platform-alias-map %) (map str/upper-case current-platforms))
-        ;; Only the deleted ones that are not part of the platform-aliases need to be validated.
-        deleted-platform-names (s/difference
-                                (set (map util/safe-lowercase previous-platforms))
-                                (set (map util/safe-lowercase (concat current-platforms platform-aliases)))))
 
 (defn deleted-project-searches
   "Returns granule searches for deleted projects. We should not delete projects in a collection
@@ -25,7 +14,7 @@
   (let [project-alias-map (get (humanizer-alias-cache/get-humanizer-alias-map context) "project")
         current-projects (map :ShortName (:Projects concept))
         previous-projects (map :ShortName (:Projects prev-concept))
-        project-aliases (mapcat #(get project-alias-map %) current-projects)
+        project-aliases (mapcat #(get project-alias-map %) (map string/upper-case current-projects))
         deleted-project-names (s/difference
                                 (set (map util/safe-lowercase previous-projects))
                                 (set (map util/safe-lowercase (concat current-projects project-aliases))))]
