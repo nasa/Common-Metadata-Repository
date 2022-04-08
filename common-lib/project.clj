80c1851f404ce9fec4853313bf02e1a666201e47
Yonggang Liu <ygliuvt@gmail.com>
2022-04-08 15:21:50 -0400

    CMR-8143: Removed jackson-databind dependency from common libs.

--- common-lib/project.clj
+++ common-lib/project.clj
@@ -19,7 +19,8 @@
                  [clojusc/ltest "0.3.0"]
                  [com.dadrox/quiet-slf4j "0.1"]
                  [com.fasterxml.jackson.core/jackson-core "2.13.2"]
-                 [com.fasterxml.jackson.dataformat/jackson-dataformat-cbor "2.13.2"]
+                 [com.fasterxml.jackson.dataformat/jackson-dataformat-cbor "2.13.2"
+                  :exclusions [com.fasterxml.jackson.core/jackson-databind]]
                  [com.gfredericks/test.chuck "0.2.9"]
                  [com.taoensso/timbre "5.1.0"]
                  [commons-codec/commons-codec "1.11"]
