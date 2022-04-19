b22e3471cfd59620801b3550513529a50898529e
Thomas Cherry <thomas.a.cherry@nasa.gov>
2022-04-19 14:53:37 -0400

    CMR-8181 - getting all but one case to work

--- metadata-db-app/src/cmr/metadata_db/data/oracle/generic_documents.clj
+++ metadata-db-app/src/cmr/metadata_db/data/oracle/generic_documents.clj
@@ -1,7 +1,7 @@
 (ns cmr.metadata-db.data.oracle.generic-documents
   "Functions for saving, retrieving, deleting generic documents."
   (:require
-   [clojure.java.jdbc :as j]
+   [clojure.java.jdbc :as jdbc]
    [clojure.pprint :refer [pprint pp]]
    [cmr.common.log :refer [debug info warn error]]
    [cmr.common.util :as cutil]
@@ -22,12 +22,6 @@
 ;; Utilities -- Prototype
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; for inserting into BLOB field
-(defn file->bytes [file]
-  (with-open [xin (io/input-stream file)
-              xout (java.io.ByteArrayOutputStream.)]
-    (io/copy xin xout)
-    (.toByteArray xout)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -46,10 +40,6 @@
                           :mime_type mime_type
                           :metadata (when metadata (cutil/gzip-blob->string metadata))
                           :revision_id (int revision_id)
-                          ;; these cause this error:
-                          ;; ; Error printing return value at cmr.common.services.errors/internal-error! (errors.clj:61).
-                          ;; ; Called db->oracle-conn with connection that was not within a db transaction. It must be called from within call j/with-db-transaction
-                          ;; however, if you try to wrap them with j/with-db-transaction the repl BLOWS UP -- if you try it, wait for it after first error
                           :revision_date (oracle/oracle-timestamp->str-time db revision_date)
                           :created_at (when created_at
                                         (oracle/oracle-timestamp->str-time db created_at))
@@ -65,17 +55,19 @@
   ;; probably going to need to pull from concepts method
   )
 
-;; not working -- see function above for reason why
+;; not working -- connection is closed
 (defn get-documents
-  [db]
+  [db format]
   (map #(dbresult->genericdoc % db)
-       (j/query db ["SELECT * FROM cmr_generic_documents"])))
+       (jdbc/query db ["SELECT * FROM cmr_generic_documents WHERE format = ?" format])))
+
+;(jdbc/with-db-transaction [conn db] (get-documents conn "myformat"))
 
 ;; not working -- see function above for reason why
 (defn get-document
   [db id]
   (first (map #(dbresult->genericdoc % db)
-              (j/query db
+              (jdbc/query db
                        [(str "SELECT *"
                              " FROM cmr_generic_documents"
                              " WHERE id = ?")
@@ -86,22 +78,22 @@
   [db {:keys [id concept_id native_id provider_id document_name schema format
               mime_type metadata revision_id revision_date created_at deleted
               user_id transaction_id]}]
-  (j/update! db
+  (jdbc/update! db
              :cmr_generic_documents
              {:id id
               :concept_id concept_id
-              :native_id native_id
+              :native_id ""
               :provider-id provider_id
               :document_name document_name
               :schema schema
               :format format ;; concepts convert this to mimetype in the get, but we already have mimetype
               :mime_type mime_type
-              :metadata (when metadata (cutil/gzip-blob->string metadata))
+              :metadata (when metadata (cutil/string->gzip-bytes metadata))
               :revision_id (int revision_id)
                           ;; these cause this error:
                           ;; ; Error printing return value at cmr.common.services.errors/internal-error! (errors.clj:61).
-                          ;; ; Called db->oracle-conn with connection that was not within a db transaction. It must be called from within call j/with-db-transaction
-                          ;; however, if you try to wrap them with j/with-db-transaction the repl BLOWS UP -- if you try it, wait for it after first error
+                          ;; ; Called db->oracle-conn with connection that was not within a db transaction. It must be called from within call jdbc/with-db-transaction
+                          ;; however, if you try to wrap them with jdbc/with-db-transaction the repl BLOWS UP -- if you try it, wait for it after first error
               :revision_date (oracle/oracle-timestamp->str-time db revision_date)
               :created_at (when created_at
                             (oracle/oracle-timestamp->str-time db created_at))
@@ -135,27 +127,34 @@
   (def db (get-in user/system [:apps :metadata-db :db]))
 
   ;; io starts looking from the dev-system/ directory bc that's where the REPL starts
-  (def my-file (io/file "../metadata-db-app/resources" "sample_tool.json"))
-  (def bytes-blob (file->bytes my-file))
+  (def test-file (slurp (clojure.java.io/resource "sample_tool.json")))
+  (def gzip-blob (cutil/string->gzip-bytes test-file))
 
   ;; save-document
-  (j/insert! db
-             :cmr_generic_documents
-             ["id" "concept_id" "native_id" "provider_id" "document_name" "schema" "format"
-              "mime_type" "metadata" "revision_id" "revision_date" "created_at" "deleted"
-              "user_id" "transaction_id"]
-             [1 "myconceptid" "mynativeid" "PROV1" "mydocname" "myschema" "myformat"
-              "application/json" bytes-blob 1 (cr/to-sql-time (p/parse-datetime "2020"))
-              (cr/to-sql-time (p/parse-datetime "2020")) 1 "myuserid" 1])
+  (jdbc/insert! db
+                :cmr_generic_documents
+                ["id" "concept_id" "native_id" "provider_id" "document_name" "schema" "format"
+                 "mime_type" "metadata" "revision_id" "revision_date" "created_at" "deleted"
+                 "user_id" "transaction_id"]
+                [1 "myconceptid" "mynativeid" "PROV1" "mydocname" "myschema" "myformat"
+                 "application/json" gzip-blob 1 (cr/to-sql-time (p/parse-datetime "2020"))
+                 (cr/to-sql-time (p/parse-datetime "2020")) 1 "myuserid" 1])
+
+  (jdbc/with-db-transaction
+    [conn db]
+    (doall (get-document conn 1)))
 
   ;; get documents
-  (def get-all-result (j/query db ["SELECT * FROM cmr_generic_documents"]))
-  ;; as you can see, this doesn't work -- we can't just use this default -- but we might be able to extend our own method to this multi
-  (concepts/db-result->concept-map "generic" db nil get-all-result)
-  ;; here is the solution used in code above -- still needs work (see code above)
-  (get-documents db)
+  (jdbc/with-db-transaction
+   [conn db]
+   (jdbc/query db ["SELECT * FROM cmr_generic_documents"]))
+
+  ;(concepts/db-result->concept-map "generic" db nil get-all-result)
+  (jdbc/with-db-transaction [conn db] (get-documents conn "myformat"))
 
   ;; get document -- still needs work (see code above)
-  (get-document db 1)
-  
+  (jdbc/with-db-transaction [conn db] (get-document conn 1))
+
+  (jdbc/delete! db "cmr_generic_documents" ["id=?" 1])
+
   )
