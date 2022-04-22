fd9558a51b35d946d075ee18172355df6b7e4ace
Tyler Heald <tyler.heald@nasa.gov>
2022-04-22 12:13:16 -0500

    CMR-8207: changed geotools version to 24.6

--- common-lib/project.clj
+++ common-lib/project.clj
@@ -44,7 +44,7 @@
                  [org.eclipse.jetty/jetty-util "9.4.39.v20210325"]
                  ;; load jts core lib first to make sure it is available for shapefile integration,
                  ;; otherwise ES referenced 1.15.0 version will be mistakenly picked for shapefile
-                 [org.locationtech.jts/jts-core "1.16.1"]
+                 [org.locationtech.jts/jts-core "1.18.2"]
                  [org.ow2.asm/asm "7.0"]
                  [potemkin "0.4.5"]
                  [ring/ring-core "1.9.2"]
