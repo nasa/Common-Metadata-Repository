d7c346ff1feb3fad384d9c67d5f542721eda320d
Brian Alexander <brian.t.alexander@nasa.gov>
2022-05-04 07:38:27 -0700

    fixes

--- system-int-test/src/cmr/system_int_test/data2/umm_spec_subscription.clj
+++ system-int-test/src/cmr/system_int_test/data2/umm_spec_subscription.clj
@@ -39,9 +39,9 @@
    (subscription attribs latest-umm-sub-verison))
   ([attribs version]
    (let [sample-subscription (merge (sample-umm-subscription version) attribs)
-         subscription (if (and (contains? attribs :Type) (= (:Type attribs) "collection"))
-             (dissoc sample-subscription :CollectionConceptId)
-             sample-subscription)]
+         subscription (if (= (:Type attribs) "collection")
+                        (dissoc sample-subscription :CollectionConceptId)
+                        sample-subscription)]
      (umm-sub/map->UMM-Sub subscription))))
 
 (defn- umm-sub->concept
