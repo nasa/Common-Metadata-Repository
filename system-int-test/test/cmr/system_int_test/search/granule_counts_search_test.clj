e6f73cebf4889e941fe81025598bcfebd7759ccd
Tyler Heald <tyler.heald@nasa.gov>
2022-05-18 09:02:14 -0500

    CMR-7856: Testing with find-granule-hits

--- system-int-test/test/cmr/system_int_test/search/granule_counts_search_test.clj
+++ system-int-test/test/cmr/system_int_test/search/granule_counts_search_test.clj
@@ -18,6 +18,7 @@
    [cmr.system-int-test.utils.index-util :as index]
    [cmr.system-int-test.utils.ingest-util :as ingest]
    [cmr.system-int-test.utils.search-util :as search]
+   [cmr.transmit.search :as transmit-search]
    [cmr.umm.umm-spatial :as umm-spatial]))
 
 (use-fixtures :each (ingest/reset-fixture
@@ -758,35 +759,33 @@
   (let [coll1 (make-coll 1 m/whole-world nil)]
     (loop [count 1]
       (make-gran coll1 (p/point 90 0) nil)
-      (when (= 0 (mod count 1000000))
-        (print count))
       (when (< (+ 10 Integer/MAX_VALUE) count)
         (recur (+ count 1))))
     (index/wait-until-indexed)
-    ;; (testing "testing hits over integer max_value"
-    ;;   (is (> (transmit-search/find-granule-hits s/context {:collection-concept-id coll1}) Integer/MAX_VALUE)))
-    (testing "test that the granule-count field returns over Integer/MAX_VALUE successfully"
-        (are3 [result-format results]
-              (let [expected-granule-count (util/map-keys :concept-id {coll1 (+ 10 (Long/parseLong Integer/MAX_VALUE))})
-                    actual-granule-count (gran-counts/results->actual-granule-count result-format results)]
-                (is (= expected-granule-count actual-granule-count)))
-              "granule count in xml format"
-              :xml (search/find-refs :collection {:include-granule-counts true})
+    (testing "testing hits over integer max_value"
+      (is (> (transmit-search/find-granule-hits s/context {:collection-concept-id coll1}) Integer/MAX_VALUE)))))
+    ;; (testing "test that the granule-count field returns over Integer/MAX_VALUE successfully"
+    ;;     (are3 [result-format results]
+    ;;           (let [expected-granule-count (util/map-keys :concept-id {coll1 (+ 10 (Long/parseLong Integer/MAX_VALUE))})
+    ;;                 actual-granule-count (gran-counts/results->actual-granule-count result-format results)]
+    ;;             (is (= expected-granule-count actual-granule-count)))
+    ;;           "granule count in xml format"
+    ;;           :xml (search/find-refs :collection {:include-granule-counts true})
 
-              "granule count in echo10 format"
-              :echo10 (search/find-metadata :collection :echo10 {:include-granule-counts true})
+    ;;           "granule count in echo10 format"
+    ;;           :echo10 (search/find-metadata :collection :echo10 {:include-granule-counts true})
 
-              "granule count in iso format"
-              :iso19115 (search/find-metadata :collection :iso19115 {:include-granule-counts true})
+    ;;           "granule count in iso format"
+    ;;           :iso19115 (search/find-metadata :collection :iso19115 {:include-granule-counts true})
 
-              "granule count in umm_json format"
-              :umm_json (search/find-concepts-umm-json :collection {:include-granule-counts true})
+    ;;           "granule count in umm_json format"
+    ;;           :umm_json (search/find-concepts-umm-json :collection {:include-granule-counts true})
 
-              "granule count in legacy-umm-json format"
-              :legacy-umm-json (search/find-concepts-legacy-umm-json :collection {:include-granule-counts true})
+    ;;           "granule count in legacy-umm-json format"
+    ;;           :legacy-umm-json (search/find-concepts-legacy-umm-json :collection {:include-granule-counts true})
 
-              "granule count in atom format"
-              :atom (search/find-concepts-atom :collection {::include-granule-counts true})
+    ;;           "granule count in atom format"
+    ;;           :atom (search/find-concepts-atom :collection {::include-granule-counts true})
 
-              "granule count in json format"
-              :atom (search/find-concepts-json :collection {:include-granule-counts true})))))
+    ;;           "granule count in json format"
+    ;;           :atom (search/find-concepts-json :collection {:include-granule-counts true})))))
