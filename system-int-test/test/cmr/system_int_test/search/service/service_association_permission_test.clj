942593a648f55661a3029529fb4ba5295098e813
siwei xu <Siwei_xu@raytheon.com>
2022-05-28 01:51:16 -0400

    CMR-8260: Added dissociation tests.

--- system-int-test/test/cmr/system_int_test/search/service/service_association_permission_test.clj
+++ system-int-test/test/cmr/system_int_test/search/service/service_association_permission_test.clj
@@ -24,8 +24,8 @@
   ;; PROV1 has :grant-all-ingest? set to false, meaning no one has update permission on INGEST_MANAGEMENT_ACL.
   ;; PROV2 has :grant-all-ingest? set to true, meaning everyone has update permission on INGEST_MANAGEMENT_ACL.
 
-  ;; Even though guest has update permission on INGEST_MANAGEMENT_ACL for PROV2, we still need to
-  ;; Make sure all the collections on PROV2 are visible to guest.
+  ;; Make sure all the collections on PROV1 and PROV2 are visible to guest.
+  (echo-util/grant-guest (system/context) (echo-util/coll-catalog-item-id "PROV1"))
   (echo-util/grant-guest (system/context) (echo-util/coll-catalog-item-id "PROV2"))
   (let [coll1 (data-core/ingest-umm-spec-collection "PROV1"
                (data-umm-c/collection
@@ -74,3 +74,72 @@
 
     (is (= {:status 400, :body [{:errors [(str "User doesn't have update permission on INGEST_MANAGEMENT_ACL for provider of collection [" coll1-concept-id "] to make the association.")], :associated-item {:concept-id coll1-concept-id}} {:service-association {:concept-id sa2-concept-id, :revision-id 1}, :associated-item {:concept-id coll2-concept-id, :revision-id coll2-revision-id}}]}
            assoc-response2))))
+
+(deftest service-dissociation-permission-test
+  ;; Create two collections and services on PROV1 and PROV2
+  ;; PROV1 has :grant-all-ingest? set to false, meaning no one has update permission on INGEST_MANAGEMENT_ACL.
+  ;; PROV2 has :grant-all-ingest? set to true, meaning everyone has update permission on INGEST_MANAGEMENT_ACL.
+
+  ;; Make sure all the collections on PROV1 and PROV2 are visible to guest.
+  (echo-util/grant-guest (system/context) (echo-util/coll-catalog-item-id "PROV1"))
+  (echo-util/grant-guest (system/context) (echo-util/coll-catalog-item-id "PROV2"))
+  (let [coll1 (data-core/ingest-umm-spec-collection "PROV1"
+               (data-umm-c/collection
+                {:ShortName "coll1"
+                 :EntryTitle "entry-title1"})
+               {:token "mock-echo-system-token"})
+        coll2 (data-core/ingest-umm-spec-collection "PROV2"
+               (data-umm-c/collection
+                {:ShortName "coll2"
+                 :EntryTitle "entry-title2"}))
+        coll1-concept-id (:concept-id coll1)
+        coll2-concept-id (:concept-id coll2)
+        coll2-revision-id (:revision-id coll2)
+        svc1-concept (service-util/make-service-concept {:native-id "svc1"
+                                                         :Name "Service1"
+                                                         :provider-id "PROV1"})
+        svc2-concept (service-util/make-service-concept {:native-id "svc2"
+                                                         :Name "Service2"
+                                                         :provider-id "PROV2"})
+        serv1 (service-util/ingest-service svc1-concept {:token "mock-echo-system-token"})
+        serv2 (service-util/ingest-service svc2-concept)
+        _ (index/wait-until-indexed)
+
+        ;; make all 4 associations using mock-echo-system-token
+        assoc-response1 (association-util/associate-by-concept-ids "mock-echo-system-token" 
+                                                                   (:concept-id serv1)
+                                                                   [{:concept-id coll1-concept-id}
+                                                                    {:concept-id coll2-concept-id
+                                                                     :revision-id coll2-revision-id}])
+        assoc-response2 (association-util/associate-by-concept-ids "mock-echo-system-token" 
+                                                                   (:concept-id serv2)
+                                                                   [{:concept-id coll1-concept-id}
+                                                                    {:concept-id coll2-concept-id
+                                                                     :revision-id coll2-revision-id}])
+        sa1-concept-ids (map #(when (:service-association %) (get-in % [:service-association :concept-id]))
+                             (:body assoc-response1))
+        sa2-concept-ids (map #(when (:service-association %) (get-in % [:service-association :concept-id]))
+                             (:body assoc-response2))
+        ;; create a guest token to be used for dissociation.
+        token (echo-util/login-guest (system/context))
+        
+        ;; dissoc 4 associations using guest token
+        ;; token has no permission to dissoc serv1 from coll1
+        ;; token has permission to dissoc serv1 from coll2 
+        dissoc-response1 (association-util/dissociate-by-concept-ids token 
+                                                                     (:concept-id serv1)
+                                                                     [{:concept-id coll1-concept-id}
+                                                                      {:concept-id coll2-concept-id
+                                                                       :revision-id coll2-revision-id}])
+        ;; token has permission to dissoc serv2 from coll1
+        ;; token has permission to dissoc serv2 from coll2
+        dissoc-response2 (association-util/dissociate-by-concept-ids token
+                                                                     (:concept-id serv2)
+                                                                     [{:concept-id coll1-concept-id}
+                                                                      {:concept-id coll2-concept-id
+                                                                       :revision-id coll2-revision-id}])]
+   (is (= {:status 200, :body [{:service-association {:concept-id (first sa1-concept-ids), :revision-id 2}, :associated-item {:concept-id coll1-concept-id}} {:service-association {:concept-id (last sa1-concept-ids), :revision-id 2}, :associated-item {:concept-id coll2-concept-id, :revision-id 1}}]}
+          dissoc-response1))
+
+   (is (= {:status 200, :body [{:service-association {:concept-id (first sa2-concept-ids), :revision-id 2}, :associated-item {:concept-id coll1-concept-id}} {:service-association {:concept-id (last sa2-concept-ids), :revision-id 2}, :associated-item {:concept-id coll2-concept-id, :revision-id 1}}]}
+          dissoc-response2))))
