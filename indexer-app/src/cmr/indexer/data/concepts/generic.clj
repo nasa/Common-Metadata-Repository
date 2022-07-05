bff8d952d9d3380d753a2edf2aa6dd0dfa538762
Thomas Cherry <thomas.a.cherry@nasa.gov>
2022-07-05 11:41:29 -0400

    CMR-8185 - removed unused requires, moving all lowercase to the cmr version, renamed short :as names

--- indexer-app/src/cmr/indexer/data/concepts/generic.clj
+++ indexer-app/src/cmr/indexer/data/concepts/generic.clj
@@ -4,16 +4,14 @@
    that can be indexed in lucine."
   (:require
    [cheshire.core :as json]
-   [clojure.string :as string]
    [cmr.common.concepts :as concepts]
    [cmr.common.log :refer (debug info warn error)]
-   [cmr.common.mime-types :as mt]
+   [cmr.common.mime-types :as mtype]
    [cmr.common.util :as util]
    [cmr.indexer.data.concept-parser :as concept-parser]
    [cmr.indexer.data.concepts.generic-util :as gen-util]
    [cmr.indexer.data.concepts.keyword-util :as keyword-util]
-   [cmr.indexer.data.elasticsearch :as es]
-   [cmr.transmit.metadata-db :as mdb]))
+   [cmr.indexer.data.elasticsearch :as esearch]))
   
 (defmulti field->index
   "Functions which convert a part of metadata to a name-value which can be added
@@ -27,7 +25,7 @@
              :Field '.l1'
              :Indexer 'complex-field'
              :Configuration {:sub-fields [:s2] :format '%s == %s'}}
-            {:l1 {:s1 'one' :s2 'two'}}))"
+            {:l1 {:s1 'one' :s2 'two'}})"
   (fn [settings _] (or (:Indexer settings) :default)))
   
 (defmethod field->index "complex-field"
@@ -39,7 +37,7 @@
         config (get settings :Configuration {})
         sub-fields (get config :sub-fields {})
         layout (get config :format "%s=%s")
-        field-name (string/lower-case (:Name settings))
+        field-name (util/safe-lowercase (:Name settings))
         field-name-lower (str field-name"-lowercase")
         field-value (reduce (fn [data-str, key-name]
                               (str
@@ -48,7 +46,7 @@
                                (format layout key-name (get field-data (keyword key-name)))))
                             ""
                             sub-fields)
-        field-value-lower (string/lower-case field-value)]
+        field-value-lower (util/safe-lowercase field-value)]
     {(keyword field-name) field-value
      (keyword field-name-lower) field-value-lower}))
 
@@ -56,10 +54,10 @@
   ;; The default indexer which will map one metadata field to two indexes. One is
   ;; with the literal case, another is all lower case"
   [settings data]
-  (let [field-name (string/lower-case (:Name settings))
+  (let [field-name (util/safe-lowercase (:Name settings))
         field-name-lower (str field-name "-lowercase")
         value (get-in data (gen-util/jq->list (:Field settings) keyword))
-        value-lower (string/lower-case value)]
+        value-lower (util/safe-lowercase value)]
     {(keyword field-name) value
      (keyword field-name-lower) value-lower}))
 
@@ -69,7 +67,7 @@
   (let [{:keys [concept-id revision-id deleted provider-id user-id
                 revision-date format-key extra-fields]} concept
         long-name (:LongName parsed-concept) ; should this exist as a required field
-        gen-name (string/lower-case (get-in parsed-concept [:MetadataSpecification :Name]))
+        gen-name (util/safe-lowercase (get-in parsed-concept [:MetadataSpecification :Name]))
         gen-ver (get-in parsed-concept [:MetadataSpecification :Version])
         approved (cmr.ingest.api.generic-documents/approved-generic? (keyword gen-name) gen-ver)
         ]
@@ -88,11 +86,11 @@
          :revision-id revision-id
          :deleted deleted
          :gen-name gen-name
-         :gen-name-lowercase (string/lower-case gen-name)
+         :gen-name-lowercase (util/safe-lowercase gen-name)
          :gen-version gen-ver
          :generic-type (str gen-name " " gen-ver)
          :provider-id provider-id
-         :provider-id-lowercase (string/lower-case provider-id)
+         :provider-id-lowercase (util/safe-lowercase provider-id)
          :keyword keyword-values
          :user-id user-id
          :revision-date revision-date}
@@ -103,13 +101,13 @@
              common-doc
              configs)]
              (if deleted
-               (assoc common-doc :metadata-format (name (mt/format-key format-key))
-                      :gen-type-lowercase (string/lower-case gen-name)
+               (assoc common-doc :metadata-format (name (mtype/format-key format-key))
+                      :gen-type-lowercase (util/safe-lowercase gen-name)
                       :long-name long-name
-                      :long-name-lowercase (string/lower-case long-name))
+                      :long-name-lowercase (util/safe-lowercase long-name))
                doc)))))
 
-(defmethod es/parsed-concept->elastic-doc :generic
+(defmethod esearch/parsed-concept->elastic-doc :generic
   ;; Public function called by the indexer framework when a document is needed.
   [context concept parsed-concept]
   ;; context is not needed for this work, so call a local function without it
