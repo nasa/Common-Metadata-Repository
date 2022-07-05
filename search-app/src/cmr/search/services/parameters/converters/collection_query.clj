c452d4bb22466ee8dace7e975b3dcf725a7c1b9d
Yonggang Liu <ygliuvt@gmail.com>
2022-07-05 18:14:18 -0400

    CMR-8464: Changed granule concept-id parameter searches to collection search condition.

--- search-app/src/cmr/search/services/parameters/converters/collection_query.clj
+++ search-app/src/cmr/search/services/parameters/converters/collection_query.clj
@@ -1,10 +1,14 @@
 (ns cmr.search.services.parameters.converters.collection-query
   "Contains functions for converting query parameters to collection query condition"
-  (:require [clojure.set :as set]
-            [cmr.search.models.query :as qm]
-            [cmr.common-app.services.search.params :as p]))
+  (:require
+   [cmr.search.models.query :as qm]
+   [cmr.common-app.services.search.params :as p]))
 
 ;; Converts parameter and values into collection query condition
 (defmethod p/parameter->condition :collection-query
   [context concept-type param value options]
-  (qm/->CollectionQueryCondition (p/parameter->condition context :collection param value options)))
+  (let [param-name (if (= :collection-concept-id param)
+                     :concept-id
+                     param)]
+    (qm/->CollectionQueryCondition
+     (p/parameter->condition context :collection param-name value options))))
