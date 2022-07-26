bc73ecc96d2474fd03e3480c9c0b7630e89c13f5
K. Lucia Z <7947215+zimzoom@users.noreply.github.com>
2022-07-26 10:54:36 -0500

    CMR-8376: Add feature toggle from new generic document pipeline

---
+++ common-lib/src/cmr/common/generics.clj
@@ -0,0 +1,9 @@
+(ns cmr.common.generics
+  "Defines utilities for new generic document pipeline"
+  (:require 
+   [cmr.common.config :as cfg]))
+
+(defn approved-generic?
+  "Check to see if a requested generic is on the approved list"
+  [schema version]
+  (some #(= version %) (schema (cfg/approved-pipeline-documents))))
