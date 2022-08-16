137b6cb62ffc61745f2eb369473fbcf20d766e46
eriknelson11 <106171241+eriknelson11@users.noreply.github.com>
2022-08-16 11:11:31 -0400

    Added copied schemas to .gitignore

--- dev-system/dev/user.clj
+++ dev-system/dev/user.clj
@@ -132,11 +132,11 @@
                          (clojure.string/split #"\n"))]
     (doseq [app apps]
       (doseq [src-name source-names]
-        (if-not (clojure.string/includes? src-name ".md")
+        (if-not (clojure.string/ends-with? src-name ".md")
           (let [dest-path (clojure.string/replace path #"dev-system" app)
                 dest-name (clojure.string/lower-case src-name)]
             (shell/sh "mkdir" "-p" (format "%s/schemas" dest-path))
-            (shell/sh "cp" "-r" (format "%s/%s" source src-name)  (format "%s/schemas/%s" dest-path dest-name))))))))
+            (shell/sh "cp" "-r" (format "%s/%s" source src-name) (format "%s/schemas/%s" dest-path dest-name))))))))
 
 (defn set-legacy
   "Passing `true` to this function will cause legacy configuration to be used
