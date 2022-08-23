bcd10119ecf9b14d9e8b8083be55554c75a2ff98
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2022-08-23 12:38:14 -0400

    CMR-8578: Removing 'bearer' prefix string; removing extra debug console log
    When indexing collections using the bootstrap function, the system token was not being parsed due to the bearer prefix.

--- graph-db/serverless/src/utils/cmr/fetchPageFromCMR.js
+++ graph-db/serverless/src/utils/cmr/fetchPageFromCMR.js
@@ -31,7 +31,7 @@ export const fetchPageFromCMR = async ({
   console.log(`Fetch collections from CMR, searchAfter #${searchAfterNum}`)
 
   if (token) {
-    requestHeaders.Authorization = `Bearer ${token}`
+    requestHeaders.Authorization = token
   }
 
   if (searchAfter) {
@@ -39,7 +39,6 @@ export const fetchPageFromCMR = async ({
   }
 
   let fetchUrl = `${process.env.CMR_ROOT}/search/collections.json?page_size=${process.env.PAGE_SIZE}`
-  console.log('CMR_ROOT', process.env.CMR_ROOT)
 
   if (providerId !== null) {
     fetchUrl += `&provider=${providerId}`
