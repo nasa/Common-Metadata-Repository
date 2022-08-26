61f19c7d4c7da791ac2b1845786e1136ee217026
Erich Reiter <erich.e.reiter@nasa.gov>
2022-08-26 11:15:51 -0400

    CMR-8590 fixing broken unit tests.

--- indexer-app/test/cmr/indexer/test/data/elasticsearch.clj
+++ indexer-app/test/cmr/indexer/test/data/elasticsearch.clj
@@ -2,65 +2,69 @@
   (:require
    [clojure.test :refer :all]
    [cmr.indexer.data.elasticsearch :as es]
-   [cmr.indexer.data.index-set :as i]))
+   [cmr.indexer.data.index-set :as i]
+   [cmr.indexer.data.index-set-generics :as index-set-gen]))
 
 (def test-index-set
   "A real copy of an index set from UAT with the mappings replaced to be smaller and reduce churn"
   {:index-set
-   {:name "cmr-base-index-set",
-    :id 1,
-    :create-reason "indexer app requires this index set",
-    :collection {:indexes [{:name "collections-v2",
-                            :settings i/collection-setting-v2}
-                           {:name "all-collection-revisions",
-                            :settings i/collection-setting-v1},]
-                 :mapping i/collection-mapping},
-    :deleted-granule {:indexes [{:name "deleted_granules",
-                                 :settings i/deleted-granule-setting}]
-                      :mapping i/deleted-granule-mapping}
-    :granule {:indexes
-              [{:name "small_collections",
-                :settings i/granule-settings-for-small-collections-index}
+   (merge 
+    {:name "cmr-base-index-set",
+     :id 1,
+     :create-reason "indexer app requires this index set",
+     :collection {:indexes [{:name "collections-v2",
+                             :settings i/collection-setting-v2}
+                            {:name "all-collection-revisions",
+                             :settings i/collection-setting-v1}]
+                  :mapping i/collection-mapping},
+     :deleted-granule {:indexes [{:name "deleted_granules",
+                                  :settings i/deleted-granule-setting}]
+                       :mapping i/deleted-granule-mapping}
+     :granule {:indexes
+               [{:name "small_collections",
+                 :settings i/granule-settings-for-small-collections-index}
 
-               {:name "C274209-USGS_EROS",
-                :settings i/granule-settings-for-individual-indexes}
-               {:name "C274211-USGS_EROS",
-                :settings i/granule-settings-for-individual-indexes}],
-              :individual-index-settings i/granule-settings-for-individual-indexes
-              :mapping i/granule-mapping}
-    :tag {:indexes [{:name "tags",
-                     :settings i/tag-setting}],
-          :mapping i/tag-mapping},
-    :variable {:indexes [{:name "variables",
-                          :settings i/variable-setting}
-                         {:name "all-variable-revisions",
-                          :settings i/variable-setting}],
-               :mapping i/variable-mapping},
-    :service {:indexes [{:name "services",
-                         :settings i/service-setting}
-                        {:name "all-service-revisions",
+                {:name "C274209-USGS_EROS",
+                 :settings i/granule-settings-for-individual-indexes}
+                {:name "C274211-USGS_EROS",
+                 :settings i/granule-settings-for-individual-indexes}],
+               :individual-index-settings i/granule-settings-for-individual-indexes
+               :mapping i/granule-mapping}
+     :tag {:indexes [{:name "tags",
+                      :settings i/tag-setting}],
+           :mapping i/tag-mapping},
+     :variable {:indexes [{:name "variables",
+                           :settings i/variable-setting}
+                          {:name "all-variable-revisions",
+                           :settings i/variable-setting}],
+                :mapping i/variable-mapping},
+     :service {:indexes [{:name "services",
+                          :settings i/service-setting}
+                         {:name "all-service-revisions",
                           :settings i/service-setting}],
-              :mapping i/service-mapping},
-    :tool {:indexes [{:name "tools",
-                      :settings i/tool-setting}
-                     {:name "all-tool-revisions",
-                      :settings i/tool-setting}],
-           :mapping i/tool-mapping},
-    :subscription {:indexes [{:name "subscriptions",
-                              :settings i/subscription-setting}
-                             {:name "all-subscription-revisions",
-                              :settings i/subscription-setting}],
-              :mapping i/subscription-mapping},
-    :concepts {:collection {:collections "1_collections_v2",
-                            :all-collection-revisions "1_all_collection_revisions"},
-               :granule {:small_collections "1_small_collections",
-                         :C274209-USGS_EROS "1_c274209_usgs_eros",
-                         :C274211-USGS_EROS "1_c274211_usgs_eros"},
-               :tag {:tags "1_tags"}
-               :variable {:variables "1_variables"}}
-    :autocomplete {:indexes [{:name "autocomplete"
-                              :settings i/autocomplete-settings}]
-                   :mapping i/autocomplete-mapping}}})
+               :mapping i/service-mapping},
+     :tool {:indexes [{:name "tools",
+                       :settings i/tool-setting}
+                      {:name "all-tool-revisions",
+                       :settings i/tool-setting}],
+            :mapping i/tool-mapping},
+     :subscription {:indexes [{:name "subscriptions",
+                               :settings i/subscription-setting}
+                              {:name "all-subscription-revisions",
+                               :settings i/subscription-setting}],
+                    :mapping i/subscription-mapping},
+     :concepts {:collection {:collections "1_collections_v2",
+                             :all-collection-revisions "1_all_collection_revisions"},
+                :granule {:small_collections "1_small_collections",
+                          :C274209-USGS_EROS "1_c274209_usgs_eros",
+                          :C274211-USGS_EROS "1_c274211_usgs_eros"},
+                :tag {:tags "1_tags"}
+                :variable {:variables "1_variables"}}
+     :autocomplete {:indexes [{:name "autocomplete"
+                               :settings i/autocomplete-settings}]
+                    :mapping i/autocomplete-mapping}}
+    ;; merge into the set of indexes all the configured generic documents
+    (index-set-gen/generic-mappings-generator))})
 
 (def valid-item-resp
   "Response for an item that is successfullly indexed during bulk indexing."
