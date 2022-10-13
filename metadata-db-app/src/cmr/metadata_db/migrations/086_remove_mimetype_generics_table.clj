b0a4361e8fd1f4b74e76b3afa34d5fd415c9a972
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2022-10-13 12:00:11 -0400

    CMR-8614: fix comment, remove unused module requires

--- metadata-db-app/src/cmr/metadata_db/migrations/086_remove_mimetype_generics_table.clj
+++ metadata-db-app/src/cmr/metadata_db/migrations/086_remove_mimetype_generics_table.clj
@@ -1,8 +1,5 @@
 (ns cmr.metadata-db.migrations.086-remove-mimetype-generics-table
    (:require
-   [cheshire.core :as json]
-   [cmr.common.util :as util]
-   [cmr.common-app.services.ingest.subscription-common :as sub-common]
    [config.mdb-migrate-helper :as h]))
 
 (defn up
@@ -10,7 +7,7 @@
   []
   (println "cmr.metadata-db.migrations.086-remove-mimetype-generics-table up...")
   (h/sql "ALTER TABLE CMR_GENERIC_DOCUMENTS ADD MIME_TYPE VARCHAR(255)")
-  ;;Retrieve the version # from the format column and prepdend that to the restored MIME_TYPE column value for each row
+  ;;Retrieve the version # from the format column and append that to the restored MIME_TYPE column value for each row
   (h/sql "UPDATE CMR_GENERIC_DOCUMENTS SET MIME_TYPE = CONCAT('application/vnd.nasa.cmr.umm+json;', SUBSTR(FORMAT, -5))"))
 
 (defn down
