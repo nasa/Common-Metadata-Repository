6fc1a9a99c1697972dc792304ed1b0daae953a1c
Thomas Cherry <thomas.a.cherry@nasa.gov>
2022-10-27 08:35:59 -0400

    CMR-8694 - rolling back spaces to make code look exactly like original

--- indexer-app/src/cmr/indexer/data/collection_granule_aggregation_cache.clj
+++ indexer-app/src/cmr/indexer/data/collection_granule_aggregation_cache.clj
@@ -220,15 +220,15 @@
                       (coll-gran-aggregates->cachable-value merged-map))
 
          (when (seq updated-collections)
-           (info "Reindexing collections found with updated temporal values since last ingest:"
-                 (pr-str updated-collections))
+          (info "Reindexing collections found with updated temporal values since last ingest:"
+                (pr-str updated-collections))
 
           ;; Reindex the collections that were modified
-           (->> updated-collections
-                (meta-db/get-latest-concepts context)
+          (->> updated-collections
+               (meta-db/get-latest-concepts context)
                ;; wrap it in a vector to make a batch to bulk index
-                vector
-                (index-service/bulk-index context)))))
+               vector
+               (index-service/bulk-index context)))))
 
       ;; There's no existing value so a full refresh is required.
       (full-cache-refresh context))))
