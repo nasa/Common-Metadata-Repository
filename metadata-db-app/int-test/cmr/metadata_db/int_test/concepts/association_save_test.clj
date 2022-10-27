03daf3d7608753271cd339e9304794e76d9245be
Erich Reiter <erich.e.reiter@nasa.gov>
2022-10-27 12:36:12 -0400

    CMR-8675 adding test for generic association payloads

---
+++ metadata-db-app/int-test/cmr/metadata_db/int_test/concepts/association_save_test.clj
@@ -0,0 +1,21 @@
+(ns cmr.metadata-db.int-test.concepts.association-save-test
+  "Contains integration tests for saving tool associations. Tests saves with various
+   configurations including checking for proper error handling."
+  (:require
+   [clojure.edn :as edn]
+   [clojure.test :refer :all]
+   [cmr.metadata-db.int-test.concepts.utils.interface :as concepts]
+   [cmr.metadata-db.int-test.utility :as util]))
+
+(use-fixtures :each (util/reset-database-fixture {:provider-id "REG_PROV" :small false}))
+
+(deftest save-generic-assoc-data-test
+  (testing "Ensure the generic association saved worked with a data in the payload."
+    (let [coll-concept (concepts/create-and-save-concept :collection "REG_PROV" 1)
+          gen-concept (concepts/create-and-save-concept :dataqualitysummary "REG_PROV" 1)
+          assoc-concept (concepts/create-concept :generic-association coll-concept gen-concept 1 {:data {:XYZ "ZYX"}})
+          saved-assoc (util/save-concept assoc-concept)
+          stored-assoc (:concept (util/get-concept-by-id-and-revision (:concept-id saved-assoc)
+                                                                      (:revision-id saved-assoc)))
+          assoc-metadata (edn/read-string (:metadata stored-assoc))]
+      (is (= {:XYZ "ZYX"} (:data assoc-metadata))))))
---
+++ metadata-db-app/int-test/cmr/metadata_db/int_test/concepts/utils/generic.clj
@@ -0,0 +1,30 @@
+(ns cmr.metadata-db.int-test.concepts.utils.generic
+  "Defines implementations for all of the multi-methods for generics in the metadata-db
+  integration tests."
+  (:require
+   [cheshire.core :as json]
+   [cmr.metadata-db.int-test.concepts.utils.interface :as concepts]))
+
+(defmethod concepts/get-sample-metadata :dataqualitysummary
+  [_concept-type]
+  (json/generate-string
+   {:Id "8EA5CA1F-E339-8065-26D7-53B64074D7CC",
+    :Name "CER-BDS_Terra",
+    :Summary "Summary",
+    :MetadataSpecification {:Name "DataQualitySummary",
+                            :Version "1.0.0",
+                            :URL "https://cdn.earthdata.nasa.gov/generics/dataqualitysummary/v1.0.0"}}))
+
+(defmethod concepts/create-concept :dataqualitysummary
+  [concept-type & args]
+  (let [[provider-id uniq-num attributes] (concepts/parse-create-concept-args concept-type args)
+        native-id (str "dqs-native" uniq-num)
+        extra-fields (merge {:document-name "CER-BDS_Terra"
+                             :schema "dataqualitysummary"}
+                            (:extra-fields attributes))
+        attributes (merge {:user-id (str "user" uniq-num)
+                           :format "application/json"
+                           :native-id native-id
+                           :extra-fields extra-fields}
+                          (dissoc attributes :extra-fields))]
+    (concepts/create-any-concept provider-id concept-type uniq-num attributes)))
