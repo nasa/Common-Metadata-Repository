61c9f3c653cde2d101aa7065034789a448b85ab9
Thomas Cherry <thomas.a.cherry@nasa.gov>
2022-10-31 13:22:04 -0400

    CMR-8774 - removing a test that no longer has a matching function

--- ingest-app/test/cmr/ingest/test/api/generic_documents.clj
+++ ingest-app/test/cmr/ingest/test/api/generic_documents.clj
@@ -7,33 +7,35 @@
 
 (deftest required-query-parameters-test
   "Tests the required query parameters functionality works."
-  (are3 [expected request required-query-parameters]
-        (try
-          (let [actual (gendoc/validate-any-required-query-parameters request required-query-parameters)]
-            (is (= expected (first actual))))
-          (catch Exception e
-            (is (= expected (str (.getMessage e))))))
 
-        "Test that the required query parameter exists"
-        nil
-        {:params {:provider "PROV2"}}
-        {:provider cmr.ingest.services.messages/provider-does-not-exist}
+  (comment testing "Tests the required query parameter"
+    (are3 [expected request required-query-parameters]
+          (try
+            (let [actual (gendoc/validate-any-required-query-parameters request required-query-parameters)]
+              (is (= expected (first actual))))
+            (catch Exception e
+              (is (= expected (str (.getMessage e))))))
 
-        "Test that the required query parameter doesn't exist"
-        (cmr.ingest.services.messages/provider-does-not-exist)
-        {:params {:provider1 "PROV2"}}
-        {:provider cmr.ingest.services.messages/provider-does-not-exist}
+          "Test that the required query parameter exists"
+          nil
+          {:params {:provider "PROV2"}}
+          {:provider cmr.ingest.services.messages/provider-does-not-exist}
 
-        "Test that more than 1 required query parameter works."
-        nil
-        {:params {:provider "PROV2"
-                  :provider2 "PROV3"}}
-        {:provider cmr.ingest.services.messages/provider-does-not-exist
-         :provider2 cmr.ingest.services.messages/provider-does-not-exist}
+          "Test that the required query parameter doesn't exist"
+          (cmr.ingest.services.messages/provider-does-not-exist)
+          {:params {:provider1 "PROV2"}}
+          {:provider cmr.ingest.services.messages/provider-does-not-exist}
 
-        "Test when more than 1 required query parameter exists and 1 fails."
-        (cmr.ingest.services.messages/provider-does-not-exist)
-        {:params {:provider "PROV2"
-                  :provider3 "PROV3"}}
-        {:provider cmr.ingest.services.messages/provider-does-not-exist
-         :provider2 cmr.ingest.services.messages/provider-does-not-exist}))
+          "Test that more than 1 required query parameter works."
+          nil
+          {:params {:provider "PROV2"
+                    :provider2 "PROV3"}}
+          {:provider cmr.ingest.services.messages/provider-does-not-exist
+           :provider2 cmr.ingest.services.messages/provider-does-not-exist}
+
+          "Test when more than 1 required query parameter exists and 1 fails."
+          (cmr.ingest.services.messages/provider-does-not-exist)
+          {:params {:provider "PROV2"
+                    :provider3 "PROV3"}}
+          {:provider cmr.ingest.services.messages/provider-does-not-exist
+           :provider2 cmr.ingest.services.messages/provider-does-not-exist})))
