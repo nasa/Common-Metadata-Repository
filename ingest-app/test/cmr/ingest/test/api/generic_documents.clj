23d0a6753cb668aaef81e980af63492b51d34313
Thomas Cherry <thomas.a.cherry@nasa.gov>
2022-11-01 13:36:10 -0400

    CMR-8774 - removing unused file after merge from master branch

--- ingest-app/test/cmr/ingest/test/api/generic_documents.clj
+++
@@ -1,6 +0,0 @@
-(ns cmr.ingest.test.api.generic-documents
-  "This tests functions in generic documents."
-  (:require
-   [clojure.test :refer :all]
-   [cmr.common.util :as u :refer [are3]]
-   [cmr.ingest.api.generic-documents :as gendoc]))
