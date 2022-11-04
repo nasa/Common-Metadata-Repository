923fc87fcd1e009ad24dc8d5e3d607242fdc0eec
Thomas Cherry <thomas.a.cherry@nasa.gov>
2022-11-04 14:06:30 -0400

    CMR-8694 - fixed a test

--- ingest-app/test/cmr/ingest/api/core_test.clj
+++ ingest-app/test/cmr/ingest/api/core_test.clj
@@ -15,34 +15,34 @@
 
 (deftest read-body-test
   (testing "Test the read-body! function to make sure it can handle all the original and new cases"
-    (are3 [data expected]
-          (is (= expected (core/read-multiple-body! (string->stream data))))
+    (are3 [expected given]
+          (is (= expected (core/read-multiple-body! (string->stream given)))
+              (str "Failed on [" given "]."))
 
           "Empty strings"
-          ""
           [""]
+          ""
 
           "Some simple text, passes thru"
-          "{}"
           ["{}"]
+          "{}"
 
           "Looks like JSON, passed thru"
-          "{\"name\": \"value\"}"
           ["{\"name\": \"value\"}"]
+          "{\"name\": \"value\"}"
 
           "JSON with a URL in it, passes thru"
-          "{\"url\": \"http://fake.gov/path?content=value&data=value\"}"
           ["{\"url\": \"http://fake.gov/path?content=value&data=value\"}"]
+          "{\"url\": \"http://fake.gov/path?content=value&data=value\"}"
 
           "A payload example"
-          "{\"content\": {\"a\":true}, \"data\": {\"b\": false}}"
           ["{\"a\":true}" "{\"b\":false}"]
+          "{\"content\": {\"a\":true}, \"data\": {\"b\": false}}"
 
           "expected payload example for JSON only, split up"
+          ["{\"url\":\"http://fake.gov/path?content=value&data=value\"}" "{\"XYZ\":\"zyx\"}"]
           "{\"content\":{\"url\":\"http://fake.gov/path?content=value&data=value\"},\"data\":{\"XYZ\":\"zyx\"}}"
-          ["{\"url\":\"http://fake.gov/path?content=value&data=value\"}" "{\"XYZ\":\"zyx\"}"]))
 
-       (testing "try an xml input which should fail"
-         (is (thrown? com.fasterxml.jackson.core.JsonParseException
-                      (core/read-multiple-body!
-                       (string->stream "<example></example>"))))))
+          "Make sure that XML is passed thru without change"
+          ["<example>data</example>"]
+          "<example>data</example>")))
