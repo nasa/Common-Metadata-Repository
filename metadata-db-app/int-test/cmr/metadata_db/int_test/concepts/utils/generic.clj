c5d5e5f1adc6f133378335eb28023eb6a71e68e6
K. Lucia Zimmermann <k.lucia.zimmermann@loqi.dev>
2022-11-08 16:14:08 -0600

    CMR-8755: fix a test

--- metadata-db-app/int-test/cmr/metadata_db/int_test/concepts/utils/generic.clj
+++ metadata-db-app/int-test/cmr/metadata_db/int_test/concepts/utils/generic.clj
@@ -5,22 +5,22 @@
    [cheshire.core :as json]
    [cmr.metadata-db.int-test.concepts.utils.interface :as concepts]))
 
-(defmethod concepts/get-sample-metadata :dataqualitysummary
+(defmethod concepts/get-sample-metadata :data-quality-summary
   [_concept-type]
   (json/generate-string
    {:Id "8EA5CA1F-E339-8065-26D7-53B64074D7CC",
     :Name "CER-BDS_Terra",
     :Summary "Summary",
-    :MetadataSpecification {:Name "DataQualitySummary",
+    :MetadataSpecification {:Name "Data Quality Summary",
                             :Version "1.0.0",
-                            :URL "https://cdn.earthdata.nasa.gov/generics/dataqualitysummary/v1.0.0"}}))
+                            :URL "https://cdn.earthdata.nasa.gov/generics/data-quality-summary/v1.0.0"}}))
 
-(defmethod concepts/create-concept :dataqualitysummary
+(defmethod concepts/create-concept :data-quality-summary
   [concept-type & args]
   (let [[provider-id uniq-num attributes] (concepts/parse-create-concept-args concept-type args)
         native-id (str "dqs-native" uniq-num)
         extra-fields (merge {:document-name "CER-BDS_Terra"
-                             :schema "dataqualitysummary"}
+                             :schema "data-quality-summary"}
                             (:extra-fields attributes))
         attributes (merge {:user-id (str "user" uniq-num)
                            :format "application/json"
