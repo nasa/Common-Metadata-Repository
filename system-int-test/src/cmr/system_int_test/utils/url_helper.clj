14457d137515104466a0eeef5337b3e765a91805
drewpesall <andrew.t.pesall@nasa.gov>
2022-11-08 13:41:38 -0500

    CMR-8679 Moved permissions POST test. Removed redundant code

--- system-int-test/src/cmr/system_int_test/utils/url_helper.clj
+++ system-int-test/src/cmr/system_int_test/utils/url_helper.clj
@@ -691,11 +691,6 @@
   []
   (format "http://localhost:%s/acls" (transmit-config/access-control-port)))
 
-(defn access-control-permissions-url
-  "URL to search permissions"
-  []
-  (format "http://localhost:%s/permissions" (transmit-config/access-control-port)))
-
 (defn access-control-s3-buckets-url
   "URL to search permitted s3-buckets"
   []
