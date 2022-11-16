a1ab09afdb7092ece56509b7b1747393dce3e079
Yonggang Liu <ygliuvt@gmail.com>
2022-11-16 09:22:17 -0500

    CMR-8735: As a CMR developer, I want to migrate ServiceOptionDefinition in Legacy Services to CMR core in SIT.

--- legacy-migration/migrate.py
+++ legacy-migration/migrate.py
@@ -24,11 +24,13 @@ providers_map = {}
 data_quality_summaries = {}
 # option definitions with guid -> concept_id
 option_definitions = {}
+# service option definitions with guid -> concept_id
+service_option_definitions = {}
 
 success_count: int = 0
 failure_count: int = 0
 
-## retrieve providers map with provider_guid_to_name_map
+# retrieve providers map with provider_guid_to_name_map
 def provider_guid_to_name_map():
     global providers_map
     with oracledb.connect(user=user, password=pwd, dsn="localhost:1521/orcl") as connection:
@@ -58,11 +60,12 @@ def migrate_dqs_row(id, name, summary, provider_guid):
                         headers=header)
     if resp.status_code >= 300:
         failure_count += 1
-        print(f"Failed to migrate [{id}] with status code: [{resp.status_code}], error: {resp.text}")
+        print(f"Failed to migrate DQS [{id}] with status code: [{resp.status_code}], error: {resp.text}")
     else:
         success_count += 1
         data_quality_summaries[id] = get_concept_id(resp.text)
         print(f"Successfully migrated DQS [{id}]")
+
 
 def migrate_od_row(id, name, description, form, scope, sort_key, deprecated, provider_guid):
     global success_count, failure_count
@@ -83,11 +86,35 @@ def migrate_od_row(id, name, description, form, scope, sort_key, deprecated, pro
                         headers=header)
     if resp.status_code >= 300:
         failure_count += 1
-        print(f"Failed to migrate [{id}] with status code: [{resp.status_code}], error: {resp.text}")
+        print(f"Failed to migrate OD [{id}] with status code: [{resp.status_code}], error: {resp.text}")
     else:
         success_count += 1
         option_definitions[id] = get_concept_id(resp.text)
         print(f"Successfully migrated OD [{id}]")
+
+
+def migrate_sod_row(id, provider_guid, name, description, form):
+    global success_count, failure_count
+    umm = {"Id": id,
+           "Name": name,
+           "Description": description,
+           "Form": form,
+           "MetadataSpecification": {
+               "Name": "Service Option",
+               "Version": "1.0.0",
+               "URL": "https://cdn.earthdata.nasa.gov/generics/service-option/v1.0.0"}}
+
+    resp = requests.put(f"{url_root}/ingest/providers/{providers_map[provider_guid]}/service-options/{id}",
+                        data=json.dumps(umm),
+                        headers=header)
+    if resp.status_code >= 300:
+        failure_count += 1
+        print(f"Failed to migrate SOD [{id}] with status code: [{resp.status_code}], error: {resp.text}")
+    else:
+        success_count += 1
+        service_option_definitions[id] = get_concept_id(resp.text)
+        print(f"Successfully migrated SOD [{id}]")
+
 
 # Migrate Data Quality Summary Assignments for a single DataQualitySummary concept
 def migrate_dqsas(dqs_id, coll_concept_ids):
@@ -104,6 +131,7 @@ def migrate_dqsas(dqs_id, coll_concept_ids):
         success_count += 1
         print(f"Successfully migrated associations for [{dqs_id}], concept_id: [{data_quality_summaries[dqs_id]}]")
 
+
 # Migrate Option Definition Assignments for a single OptionDefinition concept
 def migrate_odas(od_id, coll_concept_ids):
     global success_count, failure_count
@@ -118,6 +146,7 @@ def migrate_odas(od_id, coll_concept_ids):
     else:
         success_count += 1
         print(f"Successfully migrated associations for [{od_id}], concept_id: [{option_definitions[od_id]}]")
+
 
 def migrate_data_quality_summary():
     global success_count, failure_count
@@ -135,9 +164,10 @@ def migrate_data_quality_summary():
     end = time.time()
     print(f"Total DataQualitySummary data migration time spent: {end - start} seconds, Succeeded: {success_count}, Failed: {failure_count}")
 
+
 def migrate_option_definition():
     global success_count, failure_count
-    success_count= 0
+    success_count = 0
     failure_count = 0
     start = time.time()
     print("Starting OptionDefinition data migration...")
@@ -150,6 +180,24 @@ def migrate_option_definition():
 
     end = time.time()
     print(f"Total OptionDefinition data migration time spent: {end - start} seconds, Succeeded: {success_count}, Failed: {failure_count}")
+
+
+def migrate_service_option_definition():
+    global success_count, failure_count
+    success_count = 0
+    failure_count = 0
+    start = time.time()
+    print("Starting ServiceOptionDefinition data migration...")
+
+    with oracledb.connect(user=user, password=pwd, dsn="localhost:1521/orcl") as connection:
+        with connection.cursor() as cursor:
+            sql = """select GUID, PROVIDER_GUID, NAME, DESCRIPTION, FORM from I_10_0_TESTBED_BUSINESS.SERVICE_OPTION_DEFINITION"""
+            for r in cursor.execute(sql):
+                migrate_sod_row(r[0], r[1], r[2], r[3], r[4])
+
+    end = time.time()
+    print(f"Total ServiceOptionDefinition data migration time spent: {end - start} seconds, Succeeded: {success_count}, Failed: {failure_count}")
+
 
 def migrate_data_quality_summary_assignment():
     global success_count, failure_count
@@ -166,10 +214,11 @@ def migrate_data_quality_summary_assignment():
                 assignments[r[0]].append(r[1])
 
     for k, v in assignments.items():
-       migrate_dqsas(k, v)
+        migrate_dqsas(k, v)
 
     end = time.time()
     print(f"Total DATA_QUAL_SUMMARY_ASSIGN data migration time spent: {end - start} seconds, Succeeded: {success_count}, Failed: {failure_count}")
+
 
 def migrate_option_definition_assignment():
     global success_count, failure_count
@@ -186,7 +235,7 @@ def migrate_option_definition_assignment():
                 assignments[r[0]].append(r[1])
 
     for k, v in assignments.items():
-       migrate_odas(k, v)
+        migrate_odas(k, v)
 
     end = time.time()
     print(f"Total EJB_CAT_OPTN_ASSGN data migration time spent: {end - start} seconds, Succeeded: {success_count}, Failed: {failure_count}")
@@ -198,3 +247,4 @@ migrate_data_quality_summary()
 migrate_data_quality_summary_assignment()
 migrate_option_definition()
 migrate_option_definition_assignment()
+migrate_service_option_definition()
