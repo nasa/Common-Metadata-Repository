270cc508fd198f1c2348c683dc2e3c785c55d8c1
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2022-12-29 17:44:03 -0500

    CMR-8853: Adding association details to the variable .json response
    * Adds associations and association-details to the .json variable response in the appropriate snake-case

--- search-app/src/cmr/search/results_handlers/variables_json_results_handler.clj
+++ search-app/src/cmr/search/results_handlers/variables_json_results_handler.clj
@@ -6,11 +6,12 @@
    [cmr.common-app.services.search :as qs]
    [cmr.common-app.services.search.elastic-results-to-query-results :as elastic-results]
    [cmr.common-app.services.search.elastic-search-index :as elastic-search-index]
-   [cmr.common.util :as util]))
+   [cmr.common.util :as util]
+   [cmr.search.results-handlers.results-handler-util :as rs-util]))
 
 (defmethod elastic-search-index/concept-type+result-format->fields [:variable :json]
   [concept-type query]
-  ["concept-id" "revision-id" "deleted" "provider-id" "native-id" "variable-name" "measurement"])
+  ["concept-id" "revision-id" "deleted" "provider-id" "native-id" "variable-name" "measurement" "associations-gzip-b64"])
 
 (defmethod elastic-results/elastic-result->query-result-item [:variable :json]
   [context query elastic-result]
@@ -19,7 +20,11 @@
           deleted :deleted
           provider-id :provider-id
           native-id :native-id
-          concept-id :concept-id} :_source} elastic-result
+          concept-id :concept-id
+          associations-gzip-b64 :associations-gzip-b64} :_source} elastic-result
+          associations (some-> associations-gzip-b64
+                               util/gzip-base64->string
+                               edn/read-string)
         revision-id (elastic-results/get-revision-id-from-elastic-result :variable elastic-result)
         result-item (util/remove-nil-keys
                      {:concept_id concept-id
@@ -27,7 +32,9 @@
                       :provider_id provider-id
                       :native_id native-id
                       :name variable-name
-                      :long_name measurement})]
+                      :long_name measurement
+                      :associations (rs-util/build-association-concept-id-list associations :variable)
+                      :association_details (rs-util/build-association-details (rs-util/replace-snake-keys associations) :variable)})]
     (if deleted
       (assoc result-item :deleted deleted)
       result-item)))
