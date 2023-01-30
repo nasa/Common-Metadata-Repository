e1e94817372740f4d7ab7f9a73df94f6b79421c7
Erich Reiter <erich.e.reiter@nasa.gov>
2023-01-30 12:21:38 -0500

    CMR-8506 Adding Bulk granule update for ECHO10 OnlineAccessURLs

--- ingest-app/src/cmr/ingest/services/granule_bulk_update/mime_type/echo10.clj
+++ ingest-app/src/cmr/ingest/services/granule_bulk_update/mime_type/echo10.clj
@@ -5,66 +5,9 @@
    [clojure.data.xml :as xml]
    [clojure.set :as set]
    [clojure.string :as string]
-   [clojure.zip :as zip]
    [cmr.common.services.errors :as errors]
    [cmr.common.xml :as cx]
    [cmr.ingest.services.granule-bulk-update.utils.echo10 :as echo10-utils]))
-
-(defn- xml-elem->online-access-url
-  "Parses and returns XML element for OnlineAccessURL"
-  [elem]
-  (let [url (cx/string-at-path elem [:URL])
-        description (cx/string-at-path elem [:URLDescription])
-        mime-type (cx/string-at-path elem [:MimeType])]
-    {:url url
-     :url-description description
-     :mime-type mime-type}))
-
-(defn- update-accesses
-  "Constructs the new OnlineAccessURLs node in zipper representation"
-  [online-access-urls url-map]
-  (let [edn-access-urls (map xml-elem->online-access-url online-access-urls)
-        access-urls (map #(merge %
-                                 (when-let [mime-type (get url-map (:url %))]
-                                   {:mime-type mime-type}))
-                         edn-access-urls)]
-    (xml/element
-     :OnlineAccessURLs {}
-     (for [r access-urls]
-       (let [{:keys [url url-description mime-type]} r]
-         (xml/element :OnlineAccessURL {}
-                      (xml/element :URL {} url)
-                      (when url-description (xml/element :URLDescription {} url-description))
-                      (when mime-type (xml/element :MimeType {} mime-type))))))))
-
-(defn- replace-in-tree
-  "Take a parsed granule xml, replace the given node with the provided replacement
-  Returns the zipper representation of the updated xml."
-  [tree element-tag replacement]
-  (let [zipper (zip/xml-zip tree)
-        start-loc (zip/down zipper)]
-    (loop [loc start-loc done false]
-      (if done
-        (zip/root loc)
-        (if-let [right-loc (zip/right loc)]
-          (cond
-            ;; at an OnlineResources element, replace the node with updated value
-            (= element-tag (-> right-loc zip/node :tag))
-            (recur (zip/replace right-loc replacement) true)
-
-            ;; no action needs to be taken, move to the next node
-            :else
-            (recur right-loc false))
-          (recur loc true))))))
-
-(defn- update-online-access-urls
-  "Return an OnlineAccessURLs node where MimeType is updated where the URL has a matching entry in the url-map."
-  [tree url-map]
-  (let [online-access-urls (cx/elements-at-path
-                            tree
-                            [:OnlineAccessURLs :OnlineAccessURL])]
-    (when (seq online-access-urls)
-      (update-accesses online-access-urls url-map))))
 
 (defn update-mime-type
   "Update the the MimeType for elements within OnlineResources and OnlineAccess in echo10
@@ -93,10 +36,10 @@
                    "]")]))
 
         online-resources (echo10-utils/update-online-resources parsed :url :mime-type url-map)
-        access-urls (update-online-access-urls parsed url-map)
+        access-urls (echo10-utils/update-online-access-urls parsed :url :mime-type url-map)
 
         updated-metadata (-> parsed
-                             (replace-in-tree :OnlineResources online-resources)
-                             (replace-in-tree :OnlineAccessURLs access-urls)
+                             (echo10-utils/replace-in-tree :OnlineResources online-resources)
+                             (echo10-utils/replace-in-tree :OnlineAccessURLs access-urls)
                              xml/indent-str)]
     (assoc concept :metadata updated-metadata)))
