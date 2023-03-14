7d46e057d80eb46c19479789739b6eefd8ae4f15
Tyler Heald <tyler.heald@nasa.gov>
2023-03-14 09:54:02 -0400

    Moved after-save call out of when block and shifted paren

--- metadata-db-app/src/cmr/metadata_db/data/oracle/concepts.clj
+++ metadata-db-app/src/cmr/metadata_db/data/oracle/concepts.clj
@@ -444,11 +444,11 @@
           (when (not (= "efs-only" (efs-config/efs-toggle)))
             (info "Creating Oracle record with efs-toggle value " (efs-config/efs-toggle))
             (info "Time taken for Oracle insertion: " (first (util/time-execution
-                                                              (j/db-do-prepared db stmt values)
-                                                              (after-save conn provider concept)))) " ms")
+                                                              (j/db-do-prepared db stmt values))) " ms"))
           (when (not (= "efs-off" (efs-config/efs-toggle)))
             (info "Creating EFS record with efs-toggle value " (efs-config/efs-toggle))
             (info "Time taken for EFS insertion: " (first (util/time-execution (efs/save-concept provider concept-type (zipmap (map keyword cols) values)))) " ms"))
+          (after-save conn provider concept)
           nil)))
     (catch Exception e
       (let [error-message (.getMessage e)
