4b8966c4cbbd036510a603a815ceedbe1478175e
Tyler Heald <tyler.heald@nasa.gov>
2023-03-20 11:48:28 -0400

    Testing helper function

--- efs-lib/src/cmr/efs/connection.clj
+++ efs-lib/src/cmr/efs/connection.clj
@@ -60,6 +60,7 @@
    (get-concept provider concept-type concept-id (get-latest-revision provider concept-type concept-id)))
   ([provider concept-type concept-id revision-id]
    (info "Get-concept " provider " " concept-type " " concept-id " " revision-id)
+   (info "concept-revision-exists " (concept-revision-exists provider concept-type concept-id revision-id))
    (if (and revision-id
             (concept-revision-exists provider concept-type concept-id revision-id))
      (let [concept-path (format "%s/%s/%s/%s/%s.r%d.zip" (efs-config/efs-directory) (:provider-id provider) (name concept-type) concept-id concept-id revision-id)]
