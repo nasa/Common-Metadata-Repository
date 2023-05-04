e8ecab7880988c59a865650ad62de5d915f2fdc4
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2023-05-04 17:28:30 -0400

    Update jest.confg for ci-test to use ES6 style exports

--- browse-scaler/src/jest.config.junit_output.js
+++ browse-scaler/src/jest.config.junit_output.js
@@ -1,7 +1,7 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 
-module.exports = {
+export default {
   clearMocks: true,
   coverageDirectory: 'coverage',
   reporters: ['default', ['jest-junit', { suiteName: 'jest tests' }]],
