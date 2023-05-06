daf62a9d025f121bd0493e0ddfacdd6c0eb0a9de
drewpesall <andrew.t.pesall@nasa.gov>
2023-05-06 01:03:41 -0400

    CMR-9145 spelling

--- common-lib/project.clj
+++ common-lib/project.clj
@@ -40,7 +40,7 @@
                  [org.clojure/test.check "0.9.0"]
                  [org.clojure/tools.nrepl "0.2.13"]
                  [org.clojure/tools.reader "1.3.2"]
-                 ;; These dependencies should be updated in tandem with the ring dependiences below.
+                 ;; These dependencies should be updated in tandem with the ring dependencies below.
                  ;; To find the corresponding versions, see: https://clojars.org/ring/ring-core/versions/1.9.6
                  [org.eclipse.jetty/jetty-http "9.4.48.v20220622"]
                  [org.eclipse.jetty/jetty-io "9.4.48.v20220622"]
