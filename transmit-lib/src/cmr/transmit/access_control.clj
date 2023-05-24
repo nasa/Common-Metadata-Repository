80bd0d8ca3b9d7fd69d37a6859690c06a3bf0a10
Thomas Cherry <thomas.a.cherry@nasa.gov>
2023-05-24 13:25:14 -0400

    CMR-9154 - change client id

--- transmit-lib/src/cmr/transmit/access_control.clj
+++ transmit-lib/src/cmr/transmit/access_control.clj
@@ -134,7 +134,7 @@
   (let [token (:token context)
         headers (-> token
                     (when {config/token-header token})
-                    (as-> item (merge {:client-id "cmr-access-control"} item)))
+                    (as-> item (merge {:client-id "cmr-internal"} item)))
         body (json/generate-string {:user-token user-token})]
     (h/request context :access-control
                {:url-fn current-sids-url
