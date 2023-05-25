9aad25ca5268c20679e2c05be8a55a206768fcd2
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2023-05-25 17:12:40 -0400

    CMR-8964: removing inline diff and single letter import

--- search-app/src/cmr/search/services/provider_service.clj
+++ search-app/src/cmr/search/services/provider_service.clj
@@ -3,17 +3,15 @@
   the underlying Metadata DB API clj-http response which can be used
   as a Ring response."
   (:require
-   [cmr.common.util :as u]
+   [cmr.common.util :as util]
    [cmr.transmit.metadata-db :as mdb]))
 
 (defn read-provider
   "Read a provider."
   [context provider-id]
-  (def my-context context)
-  (u/time-execution (mdb/read-provider context provider-id)))
+  (util/time-execution (mdb/read-provider context provider-id)))
 
 (defn get-providers-raw
   "Get a list of provider ids in raw http response."
   [context]
-  (def my-context context)
-  (u/time-execution (mdb/get-all-providers context)))
+  (util/time-execution (mdb/get-all-providers context)))
