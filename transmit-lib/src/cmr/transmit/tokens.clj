575bc6b354c9d9bbb5c058a76f4f0595c61141d7
Tyler Heald <tyler.heald@nasa.gov>
2023-08-09 11:40:32 -0400

    Adding logging dependency

--- transmit-lib/src/cmr/transmit/tokens.clj
+++ transmit-lib/src/cmr/transmit/tokens.clj
@@ -8,6 +8,7 @@
    [clj-http.client :as client]
    [clojure.string :as string]
    [cmr.common.date-time-parser :as date-time-parser]
+   [cmr.common.log :as log :refer (debug info warn error)]
    [cmr.common.services.errors :as errors]
    [cmr.common.time-keeper :as tk]
    [cmr.common.util :as common-util]
