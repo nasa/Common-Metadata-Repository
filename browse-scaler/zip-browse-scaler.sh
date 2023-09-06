f1c9879222d748b8d72b9e31393616bc06cdc6be
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2023-09-06 10:37:35 -0400

    Fixing docker compose issue since rhel8

--- browse-scaler/zip-browse-scaler.sh
+++ browse-scaler/zip-browse-scaler.sh
@@ -3,6 +3,6 @@
 # for the browse-scaler to be read by the browse_scaler.tf file.
 # see the `source_code_hash` field
 
-(cd src && CURRENT_UID=$(id -u):$(id -u) docker-compose up --build)
+(cd src && CURRENT_UID=$(id -u):$(id -u) docker compose up --build)
 
 (cd src && zip -r scaler.zip index.js cache.js cmr.js resize.js util.js config.js in-memory-cache.js package.json package-lock.json image-unavailable.svg node_modules)
