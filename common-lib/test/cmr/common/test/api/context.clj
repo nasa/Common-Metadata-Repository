c3daad78d90a1b3ecbb45217d9874329e91628cd
TylerHeald1 <97175638+TylerHeald1@users.noreply.github.com>
2023-10-05 15:06:24 -0400

    CMR-9424: Improve code coverage for common-lib to 65% (#1974)
    * Added unit tests to common-lib
    
    * fix tests
    
    * Added newline
    
    * removed added are2 test, added new test
    
    * Added back new are3 test, actually removed added are2 test
    
    * Added some info on tests in api.context

---
+++ common-lib/test/cmr/common/test/api/context.clj
@@ -0,0 +1,28 @@
+(ns cmr.common.test.api.context
+  "This tests capabilities of the API context utilities."
+  (:require
+   [clojure.test :refer :all]
+   [cmr.common.util :as util]
+   [cmr.common.api.context :as context]))
+
+(deftest request-context-test
+  (testing "Normal usage with supplied request-id"
+    (let [system {:foo "bar"}
+          request-id "request-test"]
+      (is (= {:system {:foo "bar"} :request {:request-id "request-test"}} (context/request-context system request-id))))))
+
+(deftest context->request-id-test
+  (testing "Normal usage, pulling request-id from the context"
+    (let [context {:request {:request-id "request-test"}}]
+      (is (= "request-test" (context/context->request-id context))))))
+
+(deftest context->http-headers-test
+  (testing "Normal usage, creating HTTP header map"
+    (let [context {:request {:request-id "request-test"}}]
+      (is (= {"cmr-request-id" "request-test"} (context/context->http-headers context))))))
+
+(deftest context->user-id-test
+  (testing "Normal usage, pulling user-id from the context"
+    (let [context-temp {:request {:request-id "request-test"} :token "test-token"}
+          context (util/lazy-assoc context-temp :user-id "test-user")]
+      (is (= "test-user" (context/context->user-id context))))))
