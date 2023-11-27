a4e4b4f459a823c4b0cb13db14643f4fcabab25d
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2023-11-27 17:56:13 -0500

    CMR-9548: Fixing snyk issues and overrides

--- graph-db/serverless/src/utils/cmr/__test__/fetchCmrCollection.test.js
+++ graph-db/serverless/src/utils/cmr/__test__/fetchCmrCollection.test.js
@@ -51,6 +51,6 @@ describe('fetchCmrCollection', () => {
     expect(result).toBe(null)
 
     expect(consoleMock).toHaveBeenCalledTimes(1)
-    expect(consoleMock).toBeCalledWith('Could not fetch collection C1216257563-LPDAAC_TS1 due to error: Error: Request failed with status code 500')
+    expect(consoleMock).toBeCalledWith('Could not fetch collection C1216257563-LPDAAC_TS1 due to error: AxiosError: Request failed with status code 500')
   })
 })
