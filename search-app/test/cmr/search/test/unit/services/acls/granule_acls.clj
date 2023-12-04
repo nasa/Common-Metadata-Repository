4ce9246c5e51d5d19ea35e0d11977bda584561bb
Thomas Cherry <thomas.a.cherry@nasa.gov>
2023-12-04 16:57:14 -0500

    CMR-9539 - adding back code for test of test

--- search-app/test/cmr/search/test/unit/services/acls/granule_acls.clj
+++ search-app/test/cmr/search/test/unit/services/acls/granule_acls.clj
@@ -1,7 +1,7 @@
 (ns cmr.search.test.unit.services.acls.granule-acls
   (:require [clojure.test :refer :all]
             [cmr.common.cache.in-memory-cache :as mem-cache]
-            ;;[cmr.common.hash-cache :as hash-cache]
+            [cmr.common.hash-cache :as hash-cache]
             [cmr.common.util :as util :refer [are3]]
             [cmr.redis-utils.test.test-util :as test-util]
             [cmr.search.services.acls.collections-cache :as coll-cache]
