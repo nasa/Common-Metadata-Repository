bc4bff6366a75fd225963bdf1d6810b192da1625
siwei xu <Siwei_xu@raytheon.com>
2023-12-05 01:03:11 -0500

    CMR-9520: Set the launchpad token enforced default value back to false

--- common-app-lib/src/cmr/common_app/config.clj
+++ common-app-lib/src/cmr/common_app/config.clj
@@ -12,7 +12,7 @@
 
 (defconfig launchpad-token-enforced
   "Flag for whether or not launchpad token is enforced."
-  {:default true 
+  {:default false 
    :type Boolean})
 
 (defconfig release-version
