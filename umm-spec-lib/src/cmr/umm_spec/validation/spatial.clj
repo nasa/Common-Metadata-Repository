b0259278624388dcc5292bd6833201cc47d7af1e
Erich Reiter <erich.e.reiter@nasa.gov>
2023-12-07 05:45:39 -0500

    CMR-9557 putting back validations, since converting from XML specifications we don't check schema validations.

--- umm-spec-lib/src/cmr/umm_spec/validation/spatial.clj
+++ umm-spec-lib/src/cmr/umm_spec/validation/spatial.clj
@@ -2,9 +2,25 @@
   "Defines validations for UMM spatial elements."
   (:require
    [camel-snake-kebab.core :as csk]
+   [clojure.string :as string]
+   [cmr.common.util :as util]
    [cmr.common.validations.core :as v]
    [cmr.spatial.validation :as sv]
    [cmr.umm-spec.spatial-conversion :as sc]))
+
+(def valid-coord-systems
+  "Coordinate systems that are valid for umm-spec geometries."
+  #{"GEODETIC" "CARTESIAN"})
+
+(def valid-granule-spatial-representations
+  "Granule spatial representations that are valid for umm-spec spatial extents."
+  #{"GEODETIC" "CARTESIAN" "ORBIT" "NO_SPATIAL"})
+
+(defn- string->enum-keyword
+  "Returns s as keyword if s belongs to enums."
+  [s enums]
+  (when (and s (get enums s))
+    (csk/->kebab-case-keyword s)))
 
 (defn polygon-validation
   "Validates a polygon for umm-spec spatial geometry."
@@ -21,14 +37,52 @@
     (update-in spatial-extent path (fn [geometry] (map #(assoc % :coord-system coord-sys) geometry)))
     spatial-extent))
 
+(defn- validate-enum
+  "Validate if value exists in enums if value is not nil."
+  [field-path value enums]
+  (when value
+    (when-not (string->enum-keyword value enums)
+      {field-path
+       [(format "Value [%s] not found in enum (possible values: [%s])"
+                value
+                (string/join "," (sort (map #(format "\"%s\"" %) enums))))]}))) ; Sorting to make order predictable for testing.
+
+(defn- validate-spatial-representation
+  "Validate that the granule spatial representation is valid. A granule spatial representation is invalid when:
+  Geometry exists and granule spatial representation is nil
+  Granule Spatial Representation is not nil and is not one of the specified enums."
+ [field-path spatial-extent]
+ (let [granule-spatial-representation (:GranuleSpatialRepresentation spatial-extent)
+       geometry (util/remove-nil-keys
+                 (dissoc (get-in spatial-extent [:HorizontalSpatialDomain :Geometry]) :CoordinateSystem))]
+  (if (and (not (empty? geometry))
+           (nil? granule-spatial-representation))
+    {field-path ["Granule Spatial Representation must be supplied."]}
+    (validate-enum field-path granule-spatial-representation valid-granule-spatial-representations))))
+
 (defn- set-geometries-spatial-representation
   "Attach the coordinate system to each geometry to make it available during validation."
   [spatial-extent]
   (when-let [coord-sys (-> spatial-extent :HorizontalSpatialDomain :Geometry :CoordinateSystem)]
-    (let [coord-sys (csk/->kebab-case-keyword coord-sys)]
+    (let [coord-sys (string->enum-keyword coord-sys valid-coord-systems)]
       (-> spatial-extent
           (set-spatial-representation [:HorizontalSpatialDomain :Geometry :GPolygons] coord-sys)
           (set-spatial-representation [:HorizontalSpatialDomain :Geometry :Lines] coord-sys)))))
+
+(defn orbit-collection-has-orbit-parameters
+  "Validates the existence of orbit parameters when the granule spatial representation is orbit"
+  [field-path spatial-extent]
+  (let [{:keys [GranuleSpatialRepresentation OrbitParameters]} spatial-extent]
+    (when (and (= GranuleSpatialRepresentation "ORBIT") (nil? OrbitParameters))
+      {field-path
+       [(str "Orbit Parameters must be defined for a collection "
+             "whose granule spatial representation is ORBIT.")]})))
+
+(defn point-validation
+  "Validates point for umm-spec spatial geometry."
+  [field-path point]
+  (when-let [errors (seq (sv/validate (sc/umm-spec-point->point point)))]
+    {field-path  (map #(str "Spatial validation error: " %) errors)}))
 
 (defn line-validation
   "Validates line for umm-spec spatial geometry."
@@ -45,9 +99,13 @@
 
 (def spatial-extent-validation
   "Validation for the SpatialExtent of a umm-spec collection."
-  [(v/pre-validation set-geometries-spatial-representation
+  [validate-spatial-representation
+   orbit-collection-has-orbit-parameters
+   (v/pre-validation set-geometries-spatial-representation
     {:HorizontalSpatialDomain
      {:Geometry
-      {:GPolygons (v/every polygon-validation)
+      {:CoordinateSystem #(validate-enum %1 %2 valid-coord-systems)
+       :GPolygons (v/every polygon-validation)
+       :Points (v/every point-validation)
        :Lines (v/every line-validation)
        :BoundingRectangles (v/every bounding-rectangle-validation)}}})])
--- umm-spec-lib/test/cmr/umm_spec/test/validation/spatial.clj
+++ umm-spec-lib/test/cmr/umm_spec/test/validation/spatial.clj
@@ -1,9 +1,20 @@
 (ns cmr.umm-spec.test.validation.spatial
   "This has tests for UMM validations."
   (:require
+   [clojure.string :as string]
    [clojure.test :refer :all]
+   [cmr.common.services.errors :as e]
+   [cmr.common.util :as util :refer [are3]]
+   [cmr.spatial.mbr :as m]
+   [cmr.spatial.point :as p]
    [cmr.umm-spec.models.umm-collection-models :as coll]
-   [cmr.umm-spec.test.validation.umm-spec-validation-test-helpers :as helpers]))
+   [cmr.umm-spec.test.validation.umm-spec-validation-test-helpers :as helpers]
+   [cmr.umm-spec.validation.umm-spec-validation-core :as v]))
+
+(defn- umm-spec-point
+  "Returns a point for a umm-spec model."
+  [lon lat]
+  {:Longitude lon :Latitude lat})
 
 (defn- umm-spec-mbr
   "Returns an mbr for a umm-spec model."
@@ -22,10 +33,64 @@
 ;; This is built on top of the existing spatial validation. It just ensures that the spatial
 ;; validation is being called.
 (deftest collection-spatial-coverage
-  (let [invalid-mbr (umm-spec-mbr -180 45 180 46)]
+  (let [valid-point {:Longitude 0 :Latitude 90} ;; north pole
+        valid-mbr (umm-spec-mbr 0 0 0 0)
+        invalid-point (umm-spec-point -181 0)
+        invalid-mbr (umm-spec-mbr -180 45 180 46)]
+    (testing "Valid spatial areas"
+      (helpers/assert-valid (coll-with-geometry {:CoordinateSystem "GEODETIC" :Points [valid-point]}
+                                                "GEODETIC"))
+      (helpers/assert-valid (coll-with-geometry {:CoordinateSystem "GEODETIC" :Points [valid-point] :BoundingRectangles [valid-mbr]}
+                                                "GEODETIC")))
+    (testing "Invalid single geometry"
+      (helpers/assert-invalid
+        (coll-with-geometry {:CoordinateSystem "GEODETIC" :Points [invalid-point]}
+                            "GEODETIC")
+        [:SpatialExtent :HorizontalSpatialDomain :Geometry :Points 0]
+        ["Spatial validation error: Point longitude [-181] must be within -180.0 and 180.0"]))
     (testing "Invalid multiple geometry"
-      (let [expected-errors [{:path [:SpatialExtent :HorizontalSpatialDomain :Geometry :BoundingRectangles 0]
+      (let [expected-errors [{:path [:SpatialExtent :HorizontalSpatialDomain :Geometry :Points 1]
+                              :errors ["Spatial validation error: Point longitude [-181] must be within -180.0 and 180.0"]}
+                             {:path [:SpatialExtent :HorizontalSpatialDomain :Geometry :BoundingRectangles 0]
                               :errors ["Spatial validation error: The bounding rectangle north value [45] was less than the south value [46]"]}]]
-        (helpers/assert-multiple-invalid (coll-with-geometry {:CoordinateSystem "GEODETIC" :BoundingRectangles [invalid-mbr]}
+        (helpers/assert-multiple-invalid (coll-with-geometry {:CoordinateSystem "GEODETIC" :Points [valid-point invalid-point] :BoundingRectangles [invalid-mbr]}
                                                              "GEODETIC")
-                                         expected-errors)))))
+                                     expected-errors)))))
+
+(deftest collection-orbit-parameters
+ (testing "Missing orbit params"
+  (helpers/assert-invalid
+   (coll/map->UMM-C {:SpatialExtent {:GranuleSpatialRepresentation "ORBIT"}})
+   [:SpatialExtent]
+   [(str "Orbit Parameters must be defined for a collection "
+         "whose granule spatial representation is ORBIT.")])))
+
+(deftest coordinate-systems
+  (testing "Valid coordinate systems"
+    (doseq [coordinate-system ["GEODETIC" "CARTESIAN"]]
+      (helpers/assert-valid (coll-with-geometry {:CoordinateSystem coordinate-system} "NO_SPATIAL"))))
+  (testing "Invalid coordinate system"
+    (helpers/assert-invalid
+     (coll-with-geometry {:CoordinateSystem "INVALID_COORDINATE_SYSTEM"} "NO_SPATIAL")
+     [:SpatialExtent :HorizontalSpatialDomain :Geometry :CoordinateSystem]
+     ["Value [INVALID_COORDINATE_SYSTEM] not found in enum (possible values: [\"CARTESIAN\",\"GEODETIC\"])"])))
+
+(deftest granule-spatial-representations
+  (testing "Geometry exists and granule spatial representation is nil"
+    (helpers/assert-invalid
+     (coll-with-geometry {:HorizontalSpatialDomain {:Geometry {:CoordinateSystem "GEODETIC"}}} nil)
+     [:SpatialExtent]
+     ["Granule Spatial Representation must be supplied."]))
+  (testing "Not provided geometry and nil granule spatial representation"
+    (helpers/assert-valid (coll/map->UMM-C {:SpatialExtent {:GranuleSpatialRepresentation nil}})))
+  (testing "Valid granule spatial representations"
+    (doseq [granule-spatial-representation ["GEODETIC" "CARTESIAN" "ORBIT" "NO_SPATIAL"]]
+      (helpers/assert-valid (coll/map->UMM-C {:SpatialExtent
+                                              {:GranuleSpatialRepresentation granule-spatial-representation
+                                               :OrbitParameters {}
+                                               :HorizontalSpatialDomain {:Geometry {:CoordinateSystem "GEODETIC"}}}}))))
+  (testing "Invalid granule spatial representation"
+    (helpers/assert-invalid
+     (coll-with-geometry {:CoordinateSystem "CARTESIAN"} "INVALID_GRANULE_SPATIAL_REPRESENTATION")
+     [:SpatialExtent]
+     ["Value [INVALID_GRANULE_SPATIAL_REPRESENTATION] not found in enum (possible values: [\"CARTESIAN\",\"GEODETIC\",\"NO_SPATIAL\",\"ORBIT\"])"])))
