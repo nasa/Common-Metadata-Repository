66c4087a46bfb1e26c70c35a0dc261a2c9909195
Tyler Heald <tyler.heald@nasa.gov>
2024-04-29 11:49:06 -0400

    removed accidentally added whitespace

--- dev-system/dev/user.clj
+++ dev-system/dev/user.clj
@@ -209,7 +209,7 @@
   ;; a means of self-documentation.
   [& {:keys [_elastic _echo _db _messaging _redis] :as new-modes}]
 
-  (config/reset-config-values) 
+  (config/reset-config-values)
 
   (jobs/set-default-job-start-delay! (* 3 3600))
 
