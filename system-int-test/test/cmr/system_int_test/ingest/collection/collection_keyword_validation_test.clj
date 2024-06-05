f409abee959b705364f2fde6f57e1ff071771175
siwei xu <Siwei_xu@raytheon.com>
2024-06-05 10:37:54 -0400

    CMR-9429: Validate Processing Level Id

--- system-int-test/test/cmr/system_int_test/ingest/collection/collection_keyword_validation_test.clj
+++ system-int-test/test/cmr/system_int_test/ingest/collection/collection_keyword_validation_test.clj
@@ -7,7 +7,8 @@
     [cmr.system-int-test.data2.core :as d]
     [cmr.system-int-test.data2.umm-spec-collection :as data-umm-c]
     [cmr.system-int-test.data2.umm-spec-common :as data-umm-cmn]
-    [cmr.system-int-test.utils.ingest-util :as ingest]))
+    [cmr.system-int-test.utils.ingest-util :as ingest]
+    [cmr.umm-spec.models.umm-collection-models :as umm-c]))
 
 (defn assert-invalid
   ([coll-attributes field-path errors]
@@ -54,13 +55,16 @@
                                                             :LongName "Airbus A340-600"
                                                             :Type "Jet"})]
                         :DataCenters [(data-umm-cmn/data-center {:Roles ["ARCHIVER"]
-                                                                 :ShortName "SomeCenter"})]})
+                                                                 :ShortName "SomeCenter"})]
+                        :ProcessingLevel (umm-c/map->ProcessingLevelType {:Id "wrong"})})
 
           response (ingest/validate-concept concept {:validate-keywords true})]
       (is (= {:status 422
               :errors [{:path ["Platforms" 0]
                         :errors [(str "Platform short name [foo], long name [Airbus A340-600], "
                                       "and type [Jet] was not a valid keyword combination.")]}
+                       {:path ["ProcessingLevel" "Id"]
+                        :errors ["ProcessingLevel Id [wrong] was not a valid keyword."]}
                        {:path ["DataCenters" 0]
                         :errors [(str "Data center short name [SomeCenter] was not a valid "
                                       "keyword.")]}]}
