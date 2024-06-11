8f2b5821944344e2232ee431f33b0bbfbfb63221
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2024-06-11 11:02:44 -0500

    CMR-9950: Archieve browse-scaler source code
    - remove GH actions task
    - remove browse-scaler task out of cmr-checks.yml
    - remove browse-scaler references from .gitignore

--- browse-scaler/src/jest.config.junit_output.js
+++
@@ -1,9 +0,0 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
-
-export default {
-  clearMocks: true,
-  coverageDirectory: 'coverage',
-  reporters: ['default', ['jest-junit', { suiteName: 'jest tests' }]],
-  testEnvironment: 'node'
-};
--- browse-scaler/zip-browse-scaler.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# builds node_modules, then zip up the source code
-# for the browse-scaler to be read by the browse_scaler.tf file.
-# see the `source_code_hash` field
-
-(cd src && CURRENT_UID=$(id -u):$(id -u) docker compose up --build)
-
-(cd src && zip -r scaler.zip index.js cache.js cmr.js resize.js util.js config.js in-memory-cache.js package.json package-lock.json image-unavailable.svg node_modules)
