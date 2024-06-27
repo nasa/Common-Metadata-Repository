b56345c7dfc5bb524050b476885d22f1efad8f0f
jmaeng72 <44305062+jmaeng72@users.noreply.github.com>
2024-06-27 09:21:34 -0400

    make table prefix all caps

--- metadata-db-app/src/cmr/metadata_db/migrations/090_add_granule_ur_index_to_bulk_update_gran_status.clj
+++ metadata-db-app/src/cmr/metadata_db/migrations/090_add_granule_ur_index_to_bulk_update_gran_status.clj
@@ -6,7 +6,7 @@
   "Migrates the database up to version 90."
   []
   (println "cmr.metadata-db.migrations.090-add-granule-ur-index-to-bulk-update-gran-status up...")
-  (mh/sql "create index gran_ur_idx ON cmr_ingest.bulk_update_gran_status (granule_ur)"))
+  (mh/sql "create index gran_ur_idx ON CMR_INGEST.bulk_update_gran_status (granule_ur)"))
 
 (defn down
   "Migrates the database down from version 90."
