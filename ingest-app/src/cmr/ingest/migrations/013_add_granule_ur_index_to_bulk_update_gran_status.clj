2bab2c208144ca285bbcf0f07cb916faf7f2e752
jmaeng72 <44305062+jmaeng72@users.noreply.github.com>
2024-06-27 13:47:00 -0400

    update number

--- ingest-app/src/cmr/ingest/migrations/013_add_granule_ur_index_to_bulk_update_gran_status.clj
+++ ingest-app/src/cmr/ingest/migrations/013_add_granule_ur_index_to_bulk_update_gran_status.clj
@@ -5,11 +5,11 @@
 (defn up
   "Migrates the database up to version 13."
   []
-  (println "cmr.ingest.migrations.090-add-granule-ur-index-to-bulk-update-gran-status up...")
+  (println "cmr.ingest.migrations.013-add-granule-ur-index-to-bulk-update-gran-status up...")
   (h/sql "create index bugs_gran_ur_idx ON CMR_INGEST.bulk_update_gran_status(GRANULE_UR)"))
 
 (defn down
   "Migrates the database down from version 13."
   []
-  (println "cmr.ingest.migrations.090-add-granule-ur-index-to-bulk-update-gran-status down...")
+  (println "cmr.ingest.migrations.013-add-granule-ur-index-to-bulk-update-gran-status down...")
   (h/sql "DROP INDEX bugs_gran_ur_idx"))
