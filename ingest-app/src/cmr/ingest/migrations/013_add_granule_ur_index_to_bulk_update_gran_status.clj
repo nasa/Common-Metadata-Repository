d194098b0594dc8bc260c7eb435a26f5749c070e
jmaeng72 <44305062+jmaeng72@users.noreply.github.com>
2024-06-27 13:30:18 -0400

    moved migration sql to ingest

---
+++ ingest-app/src/cmr/ingest/migrations/013_add_granule_ur_index_to_bulk_update_gran_status.clj
@@ -0,0 +1,15 @@
+(ns cmr.ingest.migrations.013-add-granule-ur-index-to-bulk-update-gran-status
+  (:require
+    [config.ingest-migrate-helper :as h]))
+
+(defn up
+  "Migrates the database up to version 13."
+  []
+  (println "cmr.ingest.migrations.090-add-granule-ur-index-to-bulk-update-gran-status up...")
+  (h/sql "create index bugs_gran_ur_idx ON CMR_INGEST.bulk_update_gran_status(GRANULE_UR)"))
+
+(defn down
+  "Migrates the database down from version 13."
+  []
+  (println "cmr.ingest.migrations.090-add-granule-ur-index-to-bulk-update-gran-status down...")
+  (h/sql "DROP INDEX bugs_gran_ur_idx"))
