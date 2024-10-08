aaab34c705cc8863b21f5b1f7827acd67f9e3b99
daniel-zamora <daniel.p.zamora@nasa.gov>
2024-10-08 11:41:18 -0400

    CMR-10069 fixes periodic temporal condition to adjust for leap years

--- search-app/src/cmr/search/data/complex_to_simple_converters/temporal.clj
+++ search-app/src/cmr/search/data/complex_to_simple_converters/temporal.clj
@@ -1,15 +1,15 @@
 (ns cmr.search.data.complex-to-simple-converters.temporal
   "Defines functions that implement the reduce-query-condition method of the ComplexQueryToSimple
   protocol for Temporal conditions."
-
-  (:require [clj-time.core :as t]
-            [cmr.common.time-keeper :as tk]
-            [cmr.common.services.errors :as errors]
-            [cmr.search.models.query :as qm]
-            [cmr.common.services.search.query-model :as cqm]
-            [cmr.elastic-utils.search.es-group-query-conditions :as gc]
-            [cmr.elastic-utils.datetime-helper :as h]
-            [cmr.elastic-utils.search.query-transform :as c2s]))
+  (:require
+   [clj-time.core :as t]
+   [cmr.common.time-keeper :as tk]
+   [cmr.common.services.errors :as errors]
+   [cmr.search.models.query :as qm]
+   [cmr.common.services.search.query-model :as cqm]
+   [cmr.elastic-utils.search.es-group-query-conditions :as gc]
+   [cmr.elastic-utils.datetime-helper :as h]
+   [cmr.elastic-utils.search.query-transform :as c2s]))
 
 (defn- intersect-temporal->simple-conditions
   "Convert a temporal condition with INTERSECT mask into a combination of simpler conditions
@@ -43,43 +43,86 @@
          (cqm/nested-condition :temporals and-conditions))
        and-conditions)))
 
+(defn- is-leap-year?
+  "Checks if year is a leap year."
+  [year]
+  (cond
+    (not (zero? (mod year 4)))   
+    false
+
+    (not (zero? (mod year 100))) 
+    true
+
+    (zero? (mod year 400)) 
+    true
+
+    :else 
+    false))
+
+(defn- adjust-day 
+  "Adjusts a given julian day based on whether the current year is a leap year."
+  [current-year day]
+  (let [adjusted-day (if (is-leap-year? current-year)
+                       day 
+                       (dec day))]
+    adjusted-day))
+
 (defn current-end-date
   "Returns the current end datetime for a given year and attributes of a periodic temporal condition"
   [current-year end-date start-day end-day end-year]
-  (cond
-    (nil? end-day)
-    (if (and (= current-year end-year) end-date)
-      end-date
-      (t/minus (t/date-time (inc current-year)) (t/seconds 1)))
+  (let [adjusted-end-day (adjust-day current-year end-day)
+        adjusted-date-time (t/plus (t/date-time current-year) (t/days adjusted-end-day))
+        current-end (t/date-time current-year
+                                 (t/month adjusted-date-time)
+                                 (t/day adjusted-date-time)
+                                 (t/hour end-date)
+                                 (t/minute end-date)
+                                 (t/second end-date)
+                                 (t/milli end-date))]
+    (cond
+      (nil? end-day)
+      (if (and (= current-year end-year) end-date)
+        end-date
+        (t/minus (t/date-time (inc current-year)) (t/seconds 1)))
 
-    ;; recurring range does not cross over a year boundary - for example January 25 - May 6
-    (>= end-day start-day)
-    (let [current-end (t/plus (t/date-time current-year) (t/days (dec end-day)))]
+      ;; recurring range does not cross over a year boundary - for example January 25 - May 6
+      (>= end-day start-day)
       (if (and end-date (t/after? current-end end-date))
         end-date
-        current-end))
+        current-end)
 
-    ;; recurring range does cross over a year boundary - for example October 1 - March 10
-    :else
-    (let [current-end (t/plus (t/date-time (inc current-year)) (t/days (dec end-day)))]
-      (if (and end-date (t/after? current-end end-date))
-        end-date
-        current-end))))
+      ;; recurring range does cross over a year boundary - for example October 1 - March 10
+      :else
+      (let [current-end (t/plus current-end (t/years 1))]
+        (if (and end-date (t/after? current-end end-date))
+          end-date
+          current-end)))))
 
 (defn- simple-conditions-for-year
   "Returns simple-conditions constructed for a given year with the periodic temporal condition"
   [current-year start-date end-date start-day end-day end-year limit-to-granules concept-type]
-  (let [current-start (t/plus (t/date-time current-year) (t/days (dec start-day)))
-        current-start (if (t/before? current-start start-date) start-date current-start)
+  (let [adjusted-start-day (adjust-day current-year start-day)
+        adjusted-date-time (t/plus (t/date-time current-year) (t/days adjusted-start-day))
+        current-start (t/date-time current-year
+                                   (t/month adjusted-date-time)
+                                   (t/day adjusted-date-time)
+                                   (t/hour start-date)
+                                   (t/minute start-date)
+                                   (t/second start-date)
+                                   (t/milli start-date))
+        current-start (if (t/before? current-start start-date)
+                        start-date
+                        current-start)
         current-end (current-end-date current-year end-date start-day end-day end-year)]
+
     (when-not (t/before? current-end current-start)
       (intersect-temporal->simple-conditions
-        (assoc
-         (qm/map->TemporalCondition {:start-date current-start
-                                     :end-date current-end
-                                     :limit-to-granules limit-to-granules})
-         :concept-type
-         concept-type)))))
+       (assoc
+        (qm/map->TemporalCondition {:start-date current-start
+                                    :end-date current-end
+                                    :limit-to-granules limit-to-granules})
+        :concept-type
+        concept-type)))))
 
 (defn- periodic-temporal->simple-conditions
   "Convert a periodic temporal condition into a combination of simpler conditions
