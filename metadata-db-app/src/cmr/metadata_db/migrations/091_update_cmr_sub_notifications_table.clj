c3f52dca238d5c4f0a86dde182496d2226542b3c
Erich Reiter <erich.e.reiter@nasa.gov>
2024-11-01 06:25:48 -0400

    CMR-10140: Making the cmr_sub_notification last_notified_at nullable

--- metadata-db-app/src/cmr/metadata_db/migrations/091_update_cmr_sub_notifications_table.clj
+++ metadata-db-app/src/cmr/metadata_db/migrations/091_update_cmr_sub_notifications_table.clj
@@ -7,10 +7,12 @@
   "Migrates the database up to version 91."
   []
   (println "cmr.metadata-db.migrations.091-update-cmr-sub-notifications-table up...")
-  (h/sql "ALTER TABLE METADATA_DB.CMR_SUB_NOTIFICATIONS ADD AWS_ARN VARCHAR(2048) NULL"))
+  (h/sql "ALTER TABLE METADATA_DB.CMR_SUB_NOTIFICATIONS ADD AWS_ARN VARCHAR(2048) NULL")
+  (h/sql "ALTER TABLE METADATA_DB.CMR_SUB_NOTIFICATIONS MODIFY LAST_NOTIFIED_AT TIMESTAMP WITH TIME ZONE NULL"))
 
 (defn down
   "Migrates the database down from version 91."
   []
   (println "cmr.metadata-db.migrations.091-update-cmr-sub-notifications-table down.")
   (h/sql "ALTER TABLE METADATA_DB.CMR_SUB_NOTIFICATIONS DROP COLUMN AWS_ARN"))
+  ;; When the database has null values, we cannot make them null so the column will not change back.
