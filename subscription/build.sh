7ce697f9b32e5b38add8b13a2fba4e00012432a4
Erich Reiter <erich.e.reiter@nasa.gov>
2024-11-13 21:58:07 -0500

    CMR-10164: bamboo trial

--- subscription/build.sh
+++ subscription/build.sh
@@ -1,9 +1,1 @@
-mkdir package
-
-pip3 install --target ./package boto3 Flask
-cd package
-
-zip -r ../deployment_package.zip .
-cd ..
-
-zip deployment_package.zip subscription_worker.py
+zip deployment_package.zip subscription_worker.py part_docker
