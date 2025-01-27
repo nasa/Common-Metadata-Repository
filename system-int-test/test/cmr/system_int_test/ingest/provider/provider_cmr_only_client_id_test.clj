8b71d4c1f5295fa7ec2aeb04533b368069cd336f
JM <44305062+jmaeng72@users.noreply.github.com>
2025-01-27 10:52:36 -0500

    CMR-10323: Int test periodically failing (#2208)
    * fix test
    
    * wait for caches to finish clearing

--- system-int-test/test/cmr/system_int_test/ingest/provider/provider_cmr_only_client_id_test.clj
+++ system-int-test/test/cmr/system_int_test/ingest/provider/provider_cmr_only_client_id_test.clj
@@ -24,51 +24,53 @@
 
 (deftest collection-cmr-only-client-id-test
   (testing "ingest operations on CMR-ONLY provider can be submitted by client Echo"
-    (let [coll1 (d/ingest-umm-spec-collection "PROV1" (data-umm-c/collection {:EntryTitle "ET1"}) {:validate-keywords false})
+    (let [coll1 (d/ingest-umm-spec-collection "PROV1" (data-umm-c/collection {:EntryTitle "ET1"}))
           concept (dissoc (d/item->concept coll1) :revision-id)]
       (doseq [func ingest-functions-to-test]
         (assert-ingest-success func concept "ECHO"))))
   (testing "ingest operations on CMR-ONLY provider can be submitted by client other than Echo"
-    (let [coll1 (d/ingest-umm-spec-collection "PROV1" (data-umm-c/collection {:EntryTitle "ET1"}) {:validate-keywords false})
+    (let [coll1 (d/ingest-umm-spec-collection "PROV1" (data-umm-c/collection {:EntryTitle "ET1"}))
           concept (dissoc (d/item->concept coll1) :revision-id)]
       (doseq [func ingest-functions-to-test]
         (assert-ingest-success func concept "any"))))
   (testing "ingest operations on non CMR-ONLY provider can be submitted by client Echo"
-    (let [coll1 (d/ingest-umm-spec-collection "PROV1" (data-umm-c/collection {:EntryTitle "ET1"}) {:validate-keywords false})
+    (let [coll1 (d/ingest-umm-spec-collection "PROV1" (data-umm-c/collection {:EntryTitle "ET1"}))
           concept (dissoc (d/item->concept coll1) :revision-id)]
       (ingest/update-ingest-provider {:provider-id "PROV1"
                                       :short-name "PROV1"
                                       :cmr-only false
                                       :small false})
       (ingest/clear-caches)
+      (Thread/sleep 3000)
       (doseq [func ingest-functions-to-test]
         (assert-ingest-success func concept "ECHO"))))
   (testing "ingest operations on non CMR-ONLY provider can be submitted by client other than Echo"
-    (let [coll1 (d/ingest-umm-spec-collection "PROV1" (data-umm-c/collection {:EntryTitle "ET1"}) {:validate-keywords false})
+    (let [coll1 (d/ingest-umm-spec-collection "PROV1" (data-umm-c/collection {:EntryTitle "ET1"}))
           concept (dissoc (d/item->concept coll1) :revision-id)]
       (ingest/update-ingest-provider {:provider-id "PROV1"
                                       :short-name "PROV1"
                                       :cmr-only false
                                       :small false})
       (ingest/clear-caches)
+      (Thread/sleep 3000)
       (doseq [func ingest-functions-to-test]
         (assert-ingest-success func concept "any")))))
 
 (deftest granule-cmr-only-client-id-test
   (testing "ingest operations on CMR-ONLY provider can be submitted by client Echo"
-    (let [collection (d/ingest-umm-spec-collection "PROV1" (data-umm-c/collection {:EntryTitle "ET1"}) {:validate-keywords false})
+    (let [collection (d/ingest-umm-spec-collection "PROV1" (data-umm-c/collection {:EntryTitle "ET1"}))
           concept (d/item->concept (dg/granule-with-umm-spec-collection collection "C1-PROV1"))]
       (ingest/ingest-concept concept)
       (doseq [func ingest-functions-to-test]
         (assert-ingest-success func concept "ECHO"))))
   (testing "ingest operations on CMR-ONLY provider can be submitted by client other than Echo"
-    (let [collection (d/ingest-umm-spec-collection "PROV1" (data-umm-c/collection {:EntryTitle "ET1"}) {:validate-keywords false})
+    (let [collection (d/ingest-umm-spec-collection "PROV1" (data-umm-c/collection {:EntryTitle "ET1"}))
           concept (d/item->concept (dg/granule-with-umm-spec-collection collection "C1-PROV1"))]
       (ingest/ingest-concept concept)
       (doseq [func ingest-functions-to-test]
         (assert-ingest-success func concept "any"))))
   (testing "ingest operations on non CMR-ONLY provider can be submitted by client Echo"
-    (let [collection (d/ingest-umm-spec-collection "PROV1" (data-umm-c/collection {:EntryTitle "ET1"}) {:validate-keywords false})
+    (let [collection (d/ingest-umm-spec-collection "PROV1" (data-umm-c/collection {:EntryTitle "ET1"}))
           concept (d/item->concept (dg/granule-with-umm-spec-collection collection "C1-PROV1"))]
       (ingest/ingest-concept concept)
       (ingest/update-ingest-provider {:provider-id "PROV1"
@@ -76,10 +78,11 @@
                                       :cmr-only false
                                       :small false})
       (ingest/clear-caches)
+      (Thread/sleep 3000)
       (doseq [func ingest-functions-to-test]
         (assert-ingest-success func concept "ECHO"))))
   (testing "ingest operations on non CMR-ONLY provider can be submitted by client other than Echo"
-    (let [collection (d/ingest-umm-spec-collection "PROV1" (data-umm-c/collection {:EntryTitle "ET1"}) {:validate-keywords false})
+    (let [collection (d/ingest-umm-spec-collection "PROV1" (data-umm-c/collection {:EntryTitle "ET1"}))
           concept (d/item->concept (dg/granule-with-umm-spec-collection collection "C1-PROV1"))]
       (ingest/ingest-concept concept)
       (ingest/update-ingest-provider {:provider-id "PROV1"
@@ -87,12 +90,13 @@
                                       :cmr-only false
                                       :small false})
       (ingest/clear-caches)
+      (Thread/sleep 3000)
       (doseq [func ingest-functions-to-test]
         (assert-ingest-success func concept "any")))))
 
 (deftest granule-virtual-product-service-ingest-test
   (testing "ingest with Virtual-Product-Service as client-id should succeed for cmr-only provider"
-    (let [collection (d/ingest-umm-spec-collection "PROV1" (data-umm-c/collection {:EntryTitle "ET1"}) {:validate-keywords false})
+    (let [collection (d/ingest-umm-spec-collection "PROV1" (data-umm-c/collection {:EntryTitle "ET1"}))
           granule (dg/granule-with-umm-spec-collection collection "C1-PROV1")
           concept (d/item->concept granule)]
       (assert-ingest-success #'ingest/validate-concept concept "Virtual-Product-Service")
@@ -105,7 +109,7 @@
       (is (= 0 (:hits (search/find-refs :granule {:granule-ur (:granule-ur granule)
                                                   :page-size 50}))))))
   (testing "ingest with Virtual-Product-Service as client-id should succeed for non cmr-only provider"
-    (let [collection (d/ingest-umm-spec-collection "PROV1" (data-umm-c/collection {:EntryTitle "ET1"}) {:validate-keywords false})
+    (let [collection (d/ingest-umm-spec-collection "PROV1" (data-umm-c/collection {:EntryTitle "ET1"}))
           granule (dg/granule-with-umm-spec-collection collection "C1-PROV1")
           concept (d/item->concept granule)]
       (ingest/update-ingest-provider {:provider-id "PROV1"
@@ -113,6 +117,7 @@
                                       :cmr-only false
                                       :small false})
       (ingest/clear-caches)
+      (Thread/sleep 3000)
       (assert-ingest-success #'ingest/validate-concept concept "Virtual-Product-Service")
       (assert-ingest-success #'ingest/ingest-concept concept "Virtual-Product-Service")
       (index/wait-until-indexed)
