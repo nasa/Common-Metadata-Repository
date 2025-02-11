3c10988dc5b92a416a8fc936418fc6de78d24db6
Erich Reiter <erich.e.reiter@nasa.gov>
2025-02-11 10:53:29 -0500

    CMR-10311: Adding a lambda that can send notification messages to scubscription URLs.

---
+++ subscription/notify_lambda/build.sh
@@ -0,0 +1,1 @@
+docker buildx build --platform linux/amd64 --provenance=false -t notification-lambda:latest .
---
+++ subscription/notify_lambda/notification-lambda.py
@@ -0,0 +1,41 @@
+import json
+import requests
+from logger import logger
+
+def handler(event, context):
+    logger.debug(f""Received event: {json.dumps(event, indent=2)}"
+    for record in event['Records']:
+        process_message(record)
+
+def process_message(record):
+    try:
+        message = record['Sns']['Message']
+        message_attributes = record['Sns']['MessageAttributes']
+        logger.info(f"Processing message {message}")
+        send_message(message_attributes['endpoint'], message)
+        
+    except Exception as e:
+        logger.error(f"An error occurred {e}")
+        raise e
+
+def send_message(url, message):
+    # Prepare the data to be sent
+
+    try:
+        # Send a POST request to the URL with the message data
+        headers = {'Content-Type': 'application/json'}
+        logger.debug("URL: ", url)
+        logger.debug("Message: ", message)
+        response = requests.post(url, headers=headers, json=message)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            logger.debug("Message sent successfully!")
+            logger.debug(f"Response: {response.text}")
+        else:
+            logger.error(f"Failed to send message. Status code: {response.status_code}")
+            logger.debug(f"Response: {response.text}")
+
+    except requests.exceptions.RequestException as e:
+        logger.error(f"An error occurred while sending the message: {e}")
+
