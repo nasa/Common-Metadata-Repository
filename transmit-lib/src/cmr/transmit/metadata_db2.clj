fa71a7293b6919a5239340f466d6afde9fddd91f
JM <44305062+jmaeng72@users.noreply.github.com>
2025-02-21 09:30:28 -0500

    Revert "CMR-10393: Allow only one sqs client queue to subscribe to one collec…" (#2221)
    This reverts commit f17c748d55e73fa90ac31151f19f850db550fe81.

--- transmit-lib/src/cmr/transmit/metadata_db2.clj
+++ transmit-lib/src/cmr/transmit/metadata_db2.clj
@@ -2,14 +2,10 @@
   "This contains functions for interacting with the metadata db API. It uses the newer transmit namespace
   style that concepts, and access control use"
   (:require
-    [cheshire.core :as json]
-    [clj-http.client :as client]
-    [cmr.common.api.context :as ch]
-    [cmr.common.services.errors :as errors]
-    [cmr.transmit.config :as config]
-    [cmr.transmit.connection :as conn]
-    [cmr.transmit.http-helper :as http-helper]
-    [ring.util.codec :as codec]))
+   [cmr.transmit.config :as config]
+   [cmr.transmit.connection :as conn]
+   [cmr.transmit.http-helper :as h]
+   [ring.util.codec :as codec]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; URL functions
@@ -56,14 +52,14 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Request functions
 (declare reset)
-(http-helper/defresetter reset :metadata-db)
+(h/defresetter reset :metadata-db)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Provider functions
 (declare create-provider update-provider delete-provider)
-(http-helper/defcreator create-provider :metadata-db providers-url {:use-system-token? true})
-(http-helper/defupdater update-provider :metadata-db provider-url {:use-system-token? true})
-(http-helper/defdestroyer delete-provider :metadata-db provider-url {:use-system-token? true})
+(h/defcreator create-provider :metadata-db providers-url {:use-system-token? true})
+(h/defupdater update-provider :metadata-db provider-url {:use-system-token? true})
+(h/defdestroyer delete-provider :metadata-db provider-url {:use-system-token? true})
 
 (defn get-providers
   "Returns the list of providers configured in the metadata db. Valid options are
@@ -77,30 +73,30 @@
   ([context {:keys [raw? http-options token]}]
    (let [token (or token (:token context))
          headers (when token {config/token-header token})]
-     (http-helper/request context :metadata-db
-                          {:url-fn providers-url
+     (h/request context :metadata-db
+                {:url-fn providers-url
                  :method :get
                  :raw? raw?
-                 :http-options (http-helper/include-request-id context (merge {:accept :json
-                                                                               :headers headers}
-                                                                              http-options))}))))
+                 :http-options (h/include-request-id context (merge {:accept :json
+                                                                   :headers headers}
+                                                                  http-options))}))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Concept functions
 (declare save-concept)
-(http-helper/defcreator save-concept :metadata-db concepts-url {:use-system-token? true})
+(h/defcreator save-concept :metadata-db concepts-url {:use-system-token? true})
 
 (defn get-concept-id
   "Returns a concept id for the given concept type, provider, and native id"
   ([context concept-type provider-id native-id]
    (get-concept-id context concept-type provider-id native-id nil))
   ([context concept-type provider-id native-id {:keys [raw? http-options]}]
-   (let [response (http-helper/request context :metadata-db
-                                       {:url-fn #(concept-id-url % concept-type provider-id native-id)
-                                        :method :get
-                                        :raw? raw?
-                                        :use-system-token? true
-                                        :http-options (http-helper/include-request-id context (merge {:accept :json} http-options))})]
+   (let [response (h/request context :metadata-db
+                             {:url-fn #(concept-id-url % concept-type provider-id native-id)
+                              :method :get
+                              :raw? raw?
+                              :use-system-token? true
+                              :http-options (h/include-request-id context (merge {:accept :json} http-options))})]
      (if raw?
        response
        (:concept-id response)))))
@@ -115,12 +111,12 @@
   (find-concepts context params concept-type nil))
  ([context params concept-type {:keys [raw? http-options]}]
   (-> context
-      (http-helper/request :metadata-db
-                           {:url-fn #(concept-search-url % concept-type)
-                            :method :get
-                            :raw? raw?
-                            :use-system-token? true
-                            :http-options (http-helper/include-request-id context (merge {:accept :json} http-options params))})
+      (h/request :metadata-db
+                 {:url-fn #(concept-search-url % concept-type)
+                  :method :get
+                  :raw? raw?
+                  :use-system-token? true
+                  :http-options (h/include-request-id context (merge {:accept :json} http-options params))})
       finish-parse-concept)))
 
 (defn get-concept
@@ -132,12 +128,12 @@
    (get-concept context concept-id revision-id nil))
   ([context concept-id revision-id {:keys [raw? http-options]}]
    (-> context
-       (http-helper/request :metadata-db
-                            {:url-fn #(concept-revision-url % concept-id revision-id)
+       (h/request :metadata-db
+                  {:url-fn #(concept-revision-url % concept-id revision-id)
                    :method :get
                    :raw? raw?
                    :use-system-token? true
-                   :http-options (http-helper/include-request-id context (merge {:accept :json} http-options))})
+                   :http-options (h/include-request-id context (merge {:accept :json} http-options))})
        finish-parse-concept)))
 
 (defn get-latest-concept
@@ -149,40 +145,14 @@
    (get-latest-concept context concept-id nil))
   ([context concept-id {:keys [raw? http-options]}]
    (-> context
-       (http-helper/request :metadata-db
-                            {:url-fn #(latest-concept-url % concept-id)
+       (h/request :metadata-db
+                  {:url-fn #(latest-concept-url % concept-id)
                    :method :get
                    :raw? raw?
                    :use-system-token? true
-                   :http-options (http-helper/include-request-id context (merge {:accept :json} http-options))})
+                   :http-options (h/include-request-id context (merge {:accept :json} http-options))})
        finish-parse-concept)))
 
 ;; Defines health check function
 (declare get-metadata-db-health)
-(http-helper/defhealther get-metadata-db-health :metadata-db {:timeout-secs 2})
-
-
-(defn get-subscription-cache-content
-  "Retrieves the cache contents of the ingest subscription cache."
-  ([context coll-concept-id]
-   (get-subscription-cache-content context coll-concept-id nil))
-  ([context coll-concept-id {:keys [raw? http-options]}]
-   (let [conn (config/context->app-connection context :metadata-db)
-        request-url (str (conn/root-url conn) "/subscription/cache-content")
-        params (merge
-                 (config/conn-params conn)
-                 {:accept :json
-                  :query-params {:collection-concept-id coll-concept-id}
-                  :headers (merge
-                             (ch/context->http-headers context)
-                             {:client-id config/cmr-client-id})
-                  :throw-exceptions false
-                  :http-options (http-helper/include-request-id context {})})
-        response (client/get request-url params)
-         {:keys [status body]} response
-         status (int status)]
-     (case status
-       200 (json/decode body true)
-       ;; default
-       (errors/internal-error!
-          (format "Get subscription cache content failed for collection %s. status: %s body: %s" coll-concept-id status body))))))
+(h/defhealther get-metadata-db-health :metadata-db {:timeout-secs 2})
