25cebdd934f8e712e0c4b06cab395e2b4d6a6e08
Tyler Heald <tyler.heald@nasa.gov>
2025-03-06 12:55:50 -0500

    refactoring

--- job-utilities/job_router/lambda_function.py
+++ job-utilities/job_router/lambda_function.py
@@ -22,67 +22,73 @@ def handler(event, _):
     """
     environment = os.getenv('CMR_ENVIRONMENT')
     cmr_lb_name = os.getenv('CMR_LB_NAME')
-    timeout = int(os.getenv('ROUTER_TIMEOUT', '300'))
-    service = event.get('service', 'bootstrap')
-    endpoint = event.get('endpoint')
-    single_target = event.get('single-target', True)
-    request_type = event.get('request-type', "GET")
 
     if environment is None:
         print("ERROR: CMR_ENVIRONMENT variable not set!")
     if cmr_lb_name is None:
         print("ERROR: CMR_LB_NAME variable not set!")
-    #An extra check here so that if both variables are not set,
-    #it can at least be reported at one time
     if environment is None or cmr_lb_name is None:
         sys.exit(1)
 
     if environment == 'local':
-        json_file = open('service-ports.json', encoding="UTF-8")
+        route_local(event=event)
+    else:
+        route(host=cmr_lb_name, environment=environment, event=event)
+
+def send_request(request_type, token, url):
+    timeout = int(os.getenv('ROUTER_TIMEOUT', '300'))
+
+    pool_manager = urllib3.PoolManager(headers={"Authorization": token, \
+                                                "client-id": "cmr-job-router"}, \
+                                    timeout=urllib3.Timeout(timeout))
+
+    response = pool_manager.request(request_type, url)
+    if response.status != 200:
+        print(f"Error received sending {request_type} to {url}: " \
+                + f"{str(response.status)} reason: {response.reason}")
+        sys.exit(-1)
+
+def route_local(event):
+    service = event.get('service', 'bootstrap')
+    endpoint = event.get('endpoint')
+    request_type = event.get('request-type', "GET")
+
+    with open('service-ports.json', encoding="UTF-8") as json_file:
         service_ports = json.load(json_file)
 
         token = 'mock-echo-system-token'
-        pool_manager = urllib3.PoolManager(num_pools=1, \
-                                           headers={"Authorization": token, \
-                                                    "client-id": "cmr-job-router"}, \
-                                           timeout=urllib3.Timeout(timeout))
 
         print(f"Sending to: host.docker.internal:{service_ports[service]}/{endpoint}")
         try:
-            response = pool_manager.request(request_type, \
-                                    f"host.docker.internal:{service_ports[service]}/{endpoint}")
-            if response.status != 200:
-                print(f"Error received sending {request_type} to " \
-                      + f"host.docker.internal:{service_ports[service]}/{endpoint}: " \
-                      + f"{str(response.status)} reason: {response.reason}")
-                sys.exit(-1)
+            send_request(request_type=request_type,
+                            token=token,
+                            url=f"host.docker.internal:{service_ports[service]}/{endpoint}")
         except Exception as e:
             print("Ran into an error!")
             print(e)
             sys.exit(-1)
-        return
+
+def route(host, environment, event):
+    service = event.get('service', 'bootstrap')
+    endpoint = event.get('endpoint')
+    single_target = event.get('single-target', True)
+    request_type = event.get('request-type', "GET")
 
     client = boto3.client('ecs')
     ssm_client = boto3.client('ssm')
     elb_client = boto3.client('elbv2')
 
-    cmr_url = elb_client.describe_load_balancers(Names=[cmr_lb_name])["LoadBalancers"][0]["DNSName"]
+    cmr_url = elb_client.describe_load_balancers(Names=[host])["LoadBalancers"][0]["DNSName"]
 
     token = ssm_client.get_parameter(Name=f"/{environment}/{service}/CMR_ECHO_SYSTEM_TOKEN", \
-                                     WithDecryption=True)['Parameter']['Value']
-
-    pool_manager = urllib3.PoolManager(headers={"Authorization": token, \
-                                                "client-id": "cmr-job-router"}, \
-                                       timeout=urllib3.Timeout(timeout))
+                                    WithDecryption=True)['Parameter']['Value']
 
     if single_target:
         print(f"Running {request_type} on URL: {cmr_url}/{service}/{endpoint}")
 
-        response = pool_manager.request(request_type, f"{cmr_url}/{service}/{endpoint}")
-        if response.status != 200:
-            print(f"Error received sending request to {cmr_url}/{service}/{endpoint}: " \
-                  + str(response.status) + f" reason: {response.reason}")
-            sys.exit(-1)
+        send_request(request_type=request_type,
+                    token=token,
+                    url=f"{cmr_url}/{service}/{endpoint}")
     else:
         #Multi-target functionality is not fully implemented.
         #CMR-9688 has been made to finish this part out
@@ -102,8 +108,6 @@ def handler(event, _):
         for task in task_ips:
             print(f"Running POST on URL: {task}/{service}/{endpoint}")
 
-            response = pool_manager.request(request_type, f"{task}/{service}/{endpoint}")
-            if response.status != 200:
-                print(f"Error received sending {request_type} to {task}/{service}/{endpoint}: " \
-                      + f"{str(response.status)} reason: {response.reason}")
-                sys.exit(-1)
+            send_request(request_type=request_type,
+                        token=token,
+                        url=f"{task}/{service}/{endpoint}")
