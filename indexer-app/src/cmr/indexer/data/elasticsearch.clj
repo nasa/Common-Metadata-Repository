73a215296ed83b7a9547604ed8e1e6898f81124c
Tyler Heald <tyler.heald@nasa.gov>
2025-03-24 12:00:50 -0400

    fix parens

--- indexer-app/src/cmr/indexer/data/elasticsearch.clj
+++ indexer-app/src/cmr/indexer/data/elasticsearch.clj
@@ -167,7 +167,7 @@
   (let [existing-index-set (index-set-es/get-index-set context idx-set/index-set-id)
         expected-extra-granule-indexes (idx-set/index-set->reduced-extra-granule-indexes existing-index-set index)
         expected-index-set (idx-set/index-set expected-extra-granule-indexes)
-        ])
+        ]
   (if (requires-update? existing-index-set expected-index-set)
     (do
       (info "Removing index to produce index set " (pr-str expected-index-set))
@@ -177,7 +177,7 @@
     (do
       (info "Ignoring delete index request because index set is unchanged.")
       (info "Existing index set: " (pr-str existing-index-set))
-      (info "Requested index deletion: " (pr-str index)))))
+      (info "Requested index deletion: " (pr-str index))))))
 
 (defn reset-es-store
   "Delete elasticsearch indexes and re-create them via index-set app. A nuclear option just for the development team."
