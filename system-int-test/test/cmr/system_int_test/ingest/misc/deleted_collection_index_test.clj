2ef43cefea938273f24136ade9b0d84a1757a4e3
Tyler Heald <tyler.heald@nasa.gov>
2025-04-21 11:42:06 -0400

    Fixing index-set cache issue

--- system-int-test/test/cmr/system_int_test/ingest/misc/deleted_collection_index_test.clj
+++ system-int-test/test/cmr/system_int_test/ingest/misc/deleted_collection_index_test.clj
@@ -12,7 +12,7 @@
 (use-fixtures :once (ingest/reset-fixture {"provguid1" "PROV1"}))
 
 (deftest deleted-collection-test-index
-  (testing "Ingest granule, rebalance collection, delete collection"
+  (testing "Ingest collection, rebalance collection, delete collection"
     (let [collection (data-core/ingest-umm-spec-collection "PROV1"
                                                            (data-umm-c/collection {})
                                                            {:validate-keywords false})]
@@ -25,4 +25,27 @@
             _ (index/wait-until-indexed)
             index-exists-after-delete-response (index/check-index-exists collection)]
         (is (= 200 (:status index-exists-before-delete-response)))
-        (is (= 404 (:status index-exists-after-delete-response)))))))
+        (is (= 404 (:status index-exists-after-delete-response))))))
+  (testing "Ingest collection, rebalance collection, delete collection, ingest collection, ingest granule, check index doesn't exist"
+    (let [collection (data-core/ingest-umm-spec-collection "PROV1"
+                                                           (data-umm-c/collection {})
+                                                           {:validate-keywords false})]
+      (bootstrap/start-rebalance-collection (:concept-id collection))
+      (index/wait-until-indexed)
+      (bootstrap/finalize-rebalance-collection (:concept-id collection))
+      (index/wait-until-indexed)
+      (ingest/delete-concept (data-core/umm-c-collection->concept collection :echo10) {})
+      (let [index-not-exists-before-reingest-response (index/check-index-exists collection)
+            new-collection (data-core/ingest-umm-spec-collection "PROV1"
+                                                                 (data-umm-c/collection {})
+                                                                 {:validate-keywords false})
+            gran1 (data-core/ingest "PROV1" (update-in (granule/granule-with-umm-spec-collection new-collection (:concept-id new-collection))
+                                                       [:collection-ref]
+                                                       dissoc :ShortName :Version))
+            _ (index/wait-until-indexed)
+
+            index-not-exists-after-reingest-response (index/check-index-exists collection)]
+        (is (= 200 (:status index-exists-before-delete-response)))
+        (is (= 404 (:status index-exists-after-delete-response)))
+        (is (= 404 (:status index-not-exists-before-reingest-response)))
+        (is (= 404 (:status index-not-exists-after-reingest-response)))))))
