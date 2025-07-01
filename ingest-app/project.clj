84e3dfc341dbc9e5a3c37e826896757110e15492
Tyler Heald <tyler.heald@nasa.gov>
2025-07-01 09:20:47 -0400

    adding ingest app exclusion

--- ingest-app/project.clj
+++ ingest-app/project.clj
@@ -19,7 +19,8 @@
                  [nasa-cmr/cmr-message-queue-lib "0.1.0-SNAPSHOT"]
                  [nasa-cmr/cmr-metadata-db-app "0.1.0-SNAPSHOT"]
                  [nasa-cmr/cmr-oracle-lib "0.1.0-SNAPSHOT"]
-                 [nasa-cmr/cmr-redis-utils-lib "0.1.0-SNAPSHOT"]
+                 [nasa-cmr/cmr-redis-utils-lib "0.1.0-SNAPSHOT" :exclusions
+                  [com.taoensso/encore]]
                  [nasa-cmr/cmr-schemas "0.0.1-SNAPSHOT"]
                  [nasa-cmr/cmr-transmit-lib "0.1.0-SNAPSHOT"]
                  [nasa-cmr/cmr-umm-lib "0.1.0-SNAPSHOT"]
