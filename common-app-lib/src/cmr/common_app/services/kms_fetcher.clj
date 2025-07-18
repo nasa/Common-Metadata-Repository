f59ceae5d93ab2bec4d4e569a3bcd846ec2720be
Thomas Cherry <thomas.a.cherry@nasa.gov>
2025-07-18 15:34:40 -0400

    CMR-10682: Adding KMS cache token to some more logs

--- common-app-lib/src/cmr/common_app/services/kms_fetcher.clj
+++ common-app-lib/src/cmr/common_app/services/kms_fetcher.clj
@@ -78,11 +78,11 @@
                        [keyword-scheme (kms/get-keywords-for-keyword-scheme context keyword-scheme)]))
           data (cmr.common.util/remove-nil-keys data)]
       (when-not (empty? data)
-        (info "get-kms-index: have parsed keyword data, about to create cache.")
+        (info "refresh-kms-cache: get-kms-index: have parsed keyword data, about to create cache.")
         (kms-lookup/create-kms-index context data)))
     (catch Exception e
       (if (string/includes? (ex-message e) "Carmine connection error")
-        (error "fetch-gcmd-keywords-map found redis carmine exception. Will return nil result." e)
+        (error "refresh-kms-cache: fetch-gcmd-keywords-map found redis carmine exception. Will return nil result." e)
         (throw e)))))
 
 ;; This is only called by cmr.search.api.keywords (and tests). May be private in the future
@@ -100,7 +100,7 @@
         kms-index))
     (catch Exception e
       (if (clojure.string/includes? (ex-message e) "Carmine connection error")
-        (error "get-kms-index found redis carmine exception. Will return nil result." e)
+        (error "refresh-kms-cache: get-kms-index found redis carmine exception. Will return nil result." e)
         (throw e)))))
 
 ;; This is only called by bootstrap and should not be used out side of that app
