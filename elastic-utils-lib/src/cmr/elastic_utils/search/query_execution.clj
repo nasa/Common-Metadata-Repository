3f9c6d925c2e1ca4d2a2e080714fb12e0cc2f668
Yonggang Liu <ygliuvt@gmail.com>
2025-09-19 17:18:25 -0400

    CMR-10853: Skip granule query when no collections are found using the collection identifiers in granule search params.

--- elastic-utils-lib/src/cmr/elastic_utils/search/query_execution.clj
+++ elastic-utils-lib/src/cmr/elastic_utils/search/query_execution.clj
@@ -2,6 +2,7 @@
   "Defines a bunch of defmulti functions and their defaults to handle different stats of the
    query-execution. These will be extended by other files."
   (:require
+   [cmr.common.log :as log :refer [debug]]
    [cmr.elastic-utils.search.es-index :as idx]
    [cmr.elastic-utils.search.es-results-to-query-results :as rc]
    [cmr.elastic-utils.search.query-transform :as c2s]
@@ -93,16 +94,38 @@
   (fn [_context query]
     (query->execution-strategy query)))
 
+(def empty-es-results
+  "An empty result returned by Elasticsearch when no match is found."
+  {:took 0, :timed_out false, :_shards {:total 0, :successful 0, :skipped 0, :failed 0}, :hits {:total {:value 0}, :max_score nil, :hits ()}})
+
+(defn- granule-no-coll-match?
+  "Returns true if this is a granule query with a collection identifier
+   but no matching collection IDs, meaning we should skip querying ES."
+  [{:keys [query-concept-type has-coll-identifier query-collection-ids]}]
+  (and (= query-concept-type :granule)
+       has-coll-identifier
+       (empty? query-collection-ids)))
+
+(defn- run-elasticsearch-query
+  "Runs the Elasticsearch query pipeline: preprocess, apply ACLs (if needed),
+   reduce, and execute."
+  [context processed-query]
+  (let [processed-query (pre-process-query-result-features context processed-query)]
+    (->> processed-query
+         (#(if (or (tc/echo-system-token? context) (:skip-acls? %))
+             %
+             (add-acl-conditions-to-query context %)))
+         (c2s/reduce-query context)
+         (idx/execute-query context))))
+
+
 (defmethod execute-query :elasticsearch
   [context query]
-  (let [[context processed-query] (concept-type-specific-query-processing
-                                   context query)
-        processed-query (pre-process-query-result-features context processed-query)
-        elastic-results (->> processed-query
-                             (#(if (or (tc/echo-system-token? context) (:skip-acls? %))
-                                 %
-                                 (add-acl-conditions-to-query context %)))
-                             (c2s/reduce-query context)
-                             (idx/execute-query context))
+  (let [[context processed-query] (concept-type-specific-query-processing context query)
+        elastic-results (if (granule-no-coll-match? context)
+                          (do
+                            (debug "No collection matched by collection identifying parameters in granule search, skip querying ES.")
+                            empty-es-results)
+                          (run-elasticsearch-query context processed-query))
         query-results (rc/elastic-results->query-results context processed-query elastic-results)]
     (post-process-query-result-features context query elastic-results query-results)))
