abcdb62ee299cc56e95dd34f2f0d88c5e099526f
DuJuan Rowell <dujuan.rowell@nasa.gov>
2025-10-03 12:23:08 -0400

    CMR-10826: Removed dupe

--- search-app/src/cmr/search/data/granule_counts_cache.clj
+++ search-app/src/cmr/search/data/granule_counts_cache.clj
@@ -60,46 +60,6 @@
                                             :concept-type :collection})
                 num-granules]))))
 
-(defn get-collection-granule-counts
-  "Returns the collection granule count by searching elasticsearch by aggregation"
-  [context provider-ids]
-  (let [condition (if (seq provider-ids)
-                    (qm/string-conditions :provider-id provider-ids true)
-                    qm/match-all)
-        query (qm/query {:concept-type :granule
-                         :condition condition
-                         :page-size 0
-                         :aggregations {:by-provider
-                                        {:terms {:field (q2e/query-field->elastic-field
-                                                         :provider-id :granule)
-                                                 :size 10000}
-                                         :aggs {:by-collection-id
-                                                {:terms {:field (q2e/query-field->elastic-field
-                                                                 :collection-concept-seq-id-long
-                                                                 :granule)
-                                                         :size 10000}}}}}})
-        results (common-esi/execute-query context query)
-        extra-provider-count (get-in results [:aggregations :by-provider :sum_other_doc_count])]
-    (when (> extra-provider-count 0)
-      (e/internal-error! (str "There were [" extra-provider-count "] more providers with granules than we ever expected to see.")))
-
-    (into {} (for [provider-bucket (get-in results [:aggregations :by-provider :buckets])
-                   :let [provider-id (:key provider-bucket)
-                         extra-collection-count (get-in provider-bucket [:by-collection-id :sum_other_doc_count])
-                         _ (when (> extra-collection-count 0)
-                             (e/internal-error!
-                              (format "Provider %s has more collections ([%s]) with granules than we support"
-                                      provider-id
-                                      extra-collection-count)))
-                         coll-buckets (get-in provider-bucket [:by-collection-id :buckets])]]
-               (for [coll-bucket coll-buckets
-                     :let [coll-seq-id (:key coll-bucket)
-                           num-granules (:doc_count coll-bucket)]]
-                 [(concepts/build-concept-id {:sequence-number coll-seq-id
-                                              :provider-id provider-id
-                                              :concept-type :collection})
-                  num-granules])))))
-
 (defn refresh-granule-counts-cache
   "This is called from lambda that is triggered by an event bridge schedule refreshes the granule counts cache with the latest data."
   ([context]
