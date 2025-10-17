489970718bc8d0bb7af001b94d30797cef402ccc
Chris Durbin <christopher.d.durbin@nasa.gov>
2025-10-17 13:01:13 -0400

    CMR-10939: Update the run-kondo script to run against all of the projects that have had linter errors and warnings fixed. Fail on warnings now.

--- dev-system/support/run-kondo.sh
+++ dev-system/support/run-kondo.sh
@@ -9,35 +9,67 @@
 #     run: ./dev-system/support/run-kondo.sh >> report.log
 
 # error or warning
-fail_level=error
+fail_level=warning
 
 # set to the string true to always pass, anything else will be considered a false
 always_pass=false
 
-# A list of directories to run kondo on
-projects_to_check='access-control-app/src \
-            acl-lib/src \
-            common-app-lib/src \
-            common-lib/src \
-            elastic-utils-lib/src \
-            es-spatial-plugin/src \
-            indexer-app/src \
-            ingest-app/src \
-            message-queue-lib/src \
-            umm-lib/src'
+# Explicitly excluded projects
+exclude_list=(
+  "indexer-app"
+  "ingest-app"
+  "metadata-db-app"
+  "mock-echo-app"
+  "search-app"
+)
 
-# Call kondo from inside a docker image and then check the exit code for success
+# Dynamically find all *-lib and *-app directories in the root
+projects_to_check=""
+for dir in */; do
+  dir_name="${dir%/}"  # strip trailing slash
+  if [[ "$dir_name" == *-lib || "$dir_name" == *-app ]]; then
+    skip=false
+    for exclude in "${exclude_list[@]}"; do
+      if [[ "$dir_name" == "$exclude" ]]; then
+        skip=true
+        break
+      fi
+    done
+    if [ "$skip" = false ]; then
+      projects_to_check+="$dir_name "
+    fi
+  fi
+done
+
+# Show which projects will be checked
+echo "Running clj-kondo on the following projects:"
+for project in $projects_to_check; do
+  echo "  - $project"
+done
+echo ""
+
 work() {
-  docker run \
-    --volume $PWD:/src \
-    --rm cljkondo/clj-kondo \
-    sh -c "cd /src && clj-kondo --parallel --fail-level ${fail_level} --lint ${projects_to_check}"
+  overall_result=0
+
+  # Call kondo for each project individually to stream output
+  for project in $projects_to_check; do
+    echo "=== Linting project: $project ==="
+    docker run \
+      --volume "$PWD:/src" \
+      --rm cljkondo/clj-kondo \
+      sh -c "cd /src && clj-kondo --parallel --fail-level ${fail_level} --lint $project"
     result=$?
 
-    if [ "${always_pass}" == "true" ]; then
-      exit 0
+    if [ $result -ne 0 ]; then
+      overall_result=$result
     fi
-    exit $result
+    echo ""  # blank line between projects
+  done
+
+  if [ "$always_pass" == "true" ]; then
+    exit 0
+  fi
+  exit $overall_result
 }
 
 # Explain all the flags the script uses
@@ -46,13 +78,13 @@ usage() {
   format="%4s %-4s : %s\n"
   printf "$format" Flag Arg Description
   printf "$format" ---- --- -----------
-  printf "$format" -p list 'A space deliminated list of projects to check'
+  printf "$format" -p list 'A space delimited list of projects to check'
   printf "$format" -w '' 'Fail on warnings'
   printf "$format" -a '' 'Always pass, just report'
 }
 
 # Process the command line arguments then do the "work" of the script
-while getopts "p:wW" opt
+while getopts "p:wa" opt
 do
     case ${opt} in
         p) projects_to_check=$OPTARG ;;
