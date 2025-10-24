49c23cc335069ec06319141b2604a07f302ec4c9
jmaeng72 <44305062+jmaeng72@users.noreply.github.com>
2025-10-23 23:33:29 -0400

    add ticket num to comment

--- indexer-app/src/cmr/indexer/data/index_set_elasticsearch.clj
+++ indexer-app/src/cmr/indexer/data/index_set_elasticsearch.clj
@@ -118,7 +118,7 @@
 (defn get-old-index-set
   "Fetch index-sets that existing in the pre-split cluster archtecture.
   This func should only be called once during the first time we transition to the split cluster.
-  It may be deleted after the transition is determined to be successful and permanent."
+  It may be deleted after the transition is determined to be successful and permanent with the ticket CMR-10949."
   [context es-cluster-name index-set-id]
   (let [es-cluster-name-keyword (es-config/es-cluster-name-str->keyword es-cluster-name)
         {:keys [mapping]} (config/idx-cfg-for-index-sets es-cluster-name)
