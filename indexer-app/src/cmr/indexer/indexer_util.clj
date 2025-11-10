73bd39fa2ceae9ab161d4b33301305e5522dd616
JM <44305062+jmaeng72@users.noreply.github.com>
2025-11-10 09:10:24 -0500

    CMR-11011: Fix split cluster to work with new reshard index names (#2341)
    * update index getter
    * update readme

--- indexer-app/src/cmr/indexer/indexer_util.clj
+++ indexer-app/src/cmr/indexer/indexer_util.clj
@@ -15,16 +15,18 @@
   [context es-cluster-name]
   (get-in context [:system (es-util-config/es-cluster-name-str->keyword es-cluster-name) :conn]))
 
+;; IMPORTANT: If the rules of index naming ever changes, this func will need to change too
 (defn get-es-cluster-name-by-index-or-alias
   [es-index-or-alias]
   ;; if es-index-or-alias is granule type then get granule connection, else get the non-gran cluster connection
+  ;; determining granule indexes by 'startswith' because when resharding, index names will be changed and use the name of index as a prefix
   (if
     (and (not (= es-index-or-alias (idx-set/collections-index)))
          (not (= es-index-or-alias (idx-set/collections-index-alias)))
          (or (string/starts-with? es-index-or-alias idx-set/granule-index-name-prefix)
-             (= es-index-or-alias idx-set/small-collections-index-name)
-             (= es-index-or-alias idx-set/small-collections-index-alias)
-             (= es-index-or-alias idx-set/deleted-granule-index-name)
-             (= es-index-or-alias idx-set/deleted-granules-index-alias)))
+             (string/starts-with? es-index-or-alias idx-set/small-collections-index-name)
+             (string/starts-with? es-index-or-alias idx-set/small-collections-index-alias)
+             (string/starts-with? es-index-or-alias idx-set/deleted-granule-index-name)
+             (string/starts-with? es-index-or-alias idx-set/deleted-granules-index-alias)))
     es-util-config/gran-elastic-name
     es-util-config/elastic-name))
