d3fbc7b4fc1e02218f564d073411d397db5830c7
TylerHeald1 <97175638+TylerHeald1@users.noreply.github.com>
2025-11-28 09:04:17 -0500

    change testcontainers version (#2351)
    Merging this as the build is green and I see 2 other people then I have approved it.

--- project.clj
+++ project.clj
@@ -22,7 +22,7 @@
                           [clj-time "0.15.1"] ;; latest is 0.15.2
                           [org.clojure/clojure "1.11.2"] ;; lattest is 1.11.4 or 1.12.2
                           [org.apache.commons/commons-compress "1.28.0"] ;; see testcontainers
-                          [org.testcontainers/testcontainers "1.21.3" ;; latest
+                          [org.testcontainers/testcontainers "2.0.2" ;; latest
                            :exclusions [[org.apache.commons/commons-compress]]]]
   :profiles {:uberjar {:modules {:dirs ["access-control-app"
                                         "bootstrap-app"
