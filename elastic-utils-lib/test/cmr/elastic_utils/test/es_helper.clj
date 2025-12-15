7a9403917b1b86718d99ebf8b3f88f6460194d72
jmaeng72 <44305062+jmaeng72@users.noreply.github.com>
2025-12-15 13:40:58 -0500

    fix formatting for reindex resp example

--- elastic-utils-lib/test/cmr/elastic_utils/test/es_helper.clj
+++ elastic-utils-lib/test/cmr/elastic_utils/test/es_helper.clj
@@ -8,18 +8,18 @@
 (def reindex-resp-example
   {:nodes
    {:node-name {
-                :tasks {
-                        :node-name:102917 {
-                                           :description "reindex from [1_c1111111111_prov] to [1_c1111111111_prov_10_shards][_doc]",
-                                           :action "indices:data/write/reindex"}}},
+     :tasks {
+       :node-name:102917 {
+         :description "reindex from [1_c1111111111_prov] to [1_c1111111111_prov_10_shards][_doc]",
+         :action "indices:data/write/reindex"}}},
     :node-name-2 {
-                  :tasks {
-                          :node-name-2:00000 {
-                                              :description "reindex from [1_c000000_prov] to [1_c00000_prov_20_shards][_doc]",
-                                              :action "indices:data/write/reindex"}
-                          :node-name-2:111111 {
-                                               :description "reindex from [1_c2222_prov] to [1_c2222_prov_5_shards][_doc]",
-                                               :action "indices:data/write/reindex"}}}}})
+     :tasks {
+      :node-name-2:00000 {
+       :description "reindex from [1_c000000_prov] to [1_c00000_prov_20_shards][_doc]",
+       :action "indices:data/write/reindex"}
+      :node-name-2:111111 {
+       :description "reindex from [1_c2222_prov] to [1_c2222_prov_5_shards][_doc]",
+       :action "indices:data/write/reindex"}}}}})
 
 (deftest test-extract-descriptions-from-reindex-resp
   (let [extract-fn #'cmr.elastic-utils.es-helper/extract-descriptions-from-reindex-resp
