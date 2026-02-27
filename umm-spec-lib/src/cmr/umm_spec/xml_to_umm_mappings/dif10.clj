fead9e9379ba905fbfa122526420005059fe9491
daniel-zamora <daniel.p.zamora@nasa.gov>
2026-02-27 10:30:19 -0500

    CMR-11104: updates some calls to use clojure.edn

--- umm-spec-lib/src/cmr/umm_spec/xml_to_umm_mappings/dif10.clj
+++ umm-spec-lib/src/cmr/umm_spec/xml_to_umm_mappings/dif10.clj
@@ -2,6 +2,7 @@
  "Defines mappings from DIF10 XML into UMM records"
  (:require
    [camel-snake-kebab.core :as csk]
+   [clojure.edn :as edn]
    [clojure.string :as string]
    [cmr.common.date-time-parser :as dtp]
    [cmr.common.util :as util]
@@ -166,7 +167,7 @@
                                                      :PeriodCycleDurationValue (value-of pdt "Period_Cycle_Duration_Value")})
                                :TemporalResolution
                                 (if-let [value (value-of temporal "Temporal_Info/Temporal_Resolution/Value")]
-                                  {:Value (read-string value)
+                                  {:Value (edn/read-string value)
                                    :Unit (value-of temporal "Temporal_Info/Temporal_Resolution/Unit")}
                                   (when-let [unit (value-of temporal "Temporal_Info/Temporal_Resolution/Unit")]
                                     {:Unit unit}))})]
