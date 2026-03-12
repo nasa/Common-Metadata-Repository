97324f94996afbdb85115474af7138255d2e4b4f
JM <44305062+jmaeng72@users.noreply.github.com>
2026-03-12 13:18:11 -0400

    CMR-10949: Remove get-old-index-set and CMR-10600 feature toggle and logs (#2399)

--- indexer-app/src/cmr/indexer/data/index_set_elasticsearch.clj
+++ indexer-app/src/cmr/indexer/data/index_set_elasticsearch.clj
@@ -115,22 +115,6 @@
           (get-in [:_source :index-set-request])
           decode-field))))
 
-(defn get-old-index-set
-  "Fetch index-sets that existing in the pre-split cluster architecture.
-  This func should only be called once during the first time we transition to the split cluster.
-  It may be deleted after the transition is determined to be successful and permanent with the ticket CMR-10949."
-  [context es-cluster-name index-set-id]
-  (when (es-config/split-cluster-log-toggle)
-    (info "CMR-10600 Getting old index set to copy over to new split cluster index set indexes."))
-  (let [es-cluster-name-keyword (es-config/elastic-name-str->keyword es-cluster-name)
-        {:keys [mapping]} (config/idx-cfg-for-index-sets es-cluster-name)
-        idx-mapping-type (first (keys mapping))]
-    (when-let [result (index-set-exists?
-                        (get-in context [:system es-cluster-name-keyword]) "index-sets" idx-mapping-type index-set-id)]
-      (-> result
-          (get-in [:_source :index-set-request])
-          decode-field))))
-
 (defn get-index-set-ids
   "Fetch ids of all index-sets in specific elastic cluster."
   [{:keys [conn]} index-name idx-mapping-type]
