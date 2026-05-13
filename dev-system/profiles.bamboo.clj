0f666dd8a5eec6d1127881e37d715fbe8f12df99
Thomas Cherry <thomas.a.cherry@nasa.gov>
2026-05-13 17:13:18 -0400

    CMR-11289: adding a bunch of repositories under the belive that we can control the order

--- dev-system/profiles.bamboo.clj
+++ dev-system/profiles.bamboo.clj
@@ -1,3 +1,7 @@
 {:user
  {:repositories
-  {"earthdata-nexus" {:url "https://maven.earthdata.nasa.gov/repository/maven-public"}}}}
+  {"earthdata-nexus"   {:url "https://maven.earthdata.nasa.gov/repository/maven-public"}
+   "central"           {:url "https://repo1.maven.org/maven2/"}
+   "clojars"           {:url "https://repo.clojars.org/"}
+   "sonatype-releases" {:url "https://oss.sonatype.org/content/repositories/releases/"}
+   "apache-releases"   {:url "https://repository.apache.org/content/repositories/releases/"}}}}
