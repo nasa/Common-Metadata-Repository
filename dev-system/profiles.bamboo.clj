08c205fb425c075b19a34f7d1e29744528536408
Thomas Cherry <thomas.a.cherry@nasa.gov>
2026-05-14 15:15:41 -0400

    CMR-11289: missing a trailing slash

--- dev-system/profiles.bamboo.clj
+++ dev-system/profiles.bamboo.clj
@@ -1,6 +1,6 @@
 {:user
  {:repositories
-  {"earthdata-nexus"   {:url "https://maven.earthdata.nasa.gov/repository/maven-public"}
+  {"earthdata-nexus"   {:url "https://maven.earthdata.nasa.gov/repository/maven-public/"}
    "central"           {:url "https://repo1.maven.org/maven2/"}
    "clojars"           {:url "https://repo.clojars.org/"}
    "sonatype-releases" {:url "https://oss.sonatype.org/content/repositories/releases/"}
