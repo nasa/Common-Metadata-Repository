973b8962ffde819134bee01dc6e8821778247be4
eereiter <erich.e.reiter@nasa.gov>
2026-05-14 17:11:39 -0400

    Update profiles.bamboo.clj - Fixing production issue - multiple developers are reviewing the changes. (#2432)

--- dev-system/profiles.bamboo.clj
+++ dev-system/profiles.bamboo.clj
@@ -1,7 +1,16 @@
 {:user
- {:repositories
-  {"earthdata-nexus"   {:url "https://maven.earthdata.nasa.gov/repository/maven-public/"}
-   "central"           {:url "https://repo1.maven.org/maven2/"}
-   "clojars"           {:url "https://repo.clojars.org/"}
-   "sonatype-releases" {:url "https://oss.sonatype.org/content/repositories/releases/"}
-   "apache-releases"   {:url "https://repository.apache.org/content/repositories/releases/"}}}}
+ {;; By defining repositories as a vector, you control the lookup order.
+  ;; Leiningen will search for dependencies from top to bottom.
+  :repositories
+  [["earthdata-nexus" {:url "https://maven.earthdata.nasa.gov/repository/maven-public/"}]
+
+   ;; It's crucial to include central and clojars, as this list
+   ;; completely replaces the Leiningen defaults.
+   ["central" {:url "https://repo1.maven.org/maven2/"
+               ;; Good practice: Central doesn't host snapshots.
+               :snapshots false}]
+   ["clojars" {:url "https://repo.clojars.org/"}]
+
+   ;; The other repositories you had defined
+   ["sonatype-releases" {:url "https://oss.sonatype.org/content/repositories/releases/"}]
+   ["apache-releases" {:url "https://repository.apache.org/content/repositories/releases/"}]]}}
