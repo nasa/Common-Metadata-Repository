20e4cdff75a780e5d7b3ba16d84e89253aa23fb3
Anthony Ortega <aortega@appddictionstudio.com>
2026-05-27 15:04:31 -0500

    initializing additional caches to prevent hash-cache error

--- umm-spec-lib/src/cmr/umm_spec/test/location_keywords_helper.clj
+++ umm-spec-lib/src/cmr/umm_spec/test/location_keywords_helper.clj
@@ -28,7 +28,20 @@
                      kms-lookup/kms-short-name-cache-key (kms-lookup/create-kms-short-name-cache)
                      kms-lookup/kms-umm-c-cache-key (kms-lookup/create-kms-umm-c-cache)
                      kms-lookup/kms-location-cache-key (kms-lookup/create-kms-location-cache)
-                     kms-lookup/kms-measurement-cache-key (kms-lookup/create-kms-measurement-cache)}}})
+                     kms-lookup/kms-measurement-cache-key (kms-lookup/create-kms-measurement-cache)
+                     kms-lookup/kms-processing-level-cache-key (kms-lookup/create-kms-processing-level-uuid-cache)
+                     kms-lookup/kms-science-keywords-cache-key (kms-lookup/create-kms-science-keywords-uuid-cache)
+                     kms-lookup/kms-platforms-cache-key (kms-lookup/create-kms-platforms-uuid-cache)
+                     kms-lookup/kms-instruments-cache-key (kms-lookup/create-kms-instruments-uuid-cache)
+                     kms-lookup/kms-providers-cache-key (kms-lookup/create-kms-providers-uuid-cache)
+                     kms-lookup/kms-spatial-keywords-cache-key (kms-lookup/create-kms-spatial-keywords-uuid-cache)
+                     kms-lookup/kms-concepts-cache-key (kms-lookup/create-kms-concepts-uuid-cache)
+                     kms-lookup/kms-iso-topic-categories-cache-key (kms-lookup/create-kms-iso-topic-categories-uuid-cache)
+                     kms-lookup/kms-granule-data-format-cache-key (kms-lookup/create-kms-granule-data-format-uuid-cache)
+                     kms-lookup/kms-mime-type-cache-key (kms-lookup/create-kms-mime-type-uuid-cache)
+                     kms-lookup/kms-related-urls-cache-key (kms-lookup/create-kms-related-urls-uuid-cache)
+                     kms-lookup/kms-temporal-keywords-cache-key (kms-lookup/create-kms-temporal-keywords-uuid-cache)
+                     kms-lookup/kms-projects-cache-key (kms-lookup/create-kms-project-uuid-cache)}}})
 
 (defn redis-cache-fixture
   "This fixture wraps any tests that use it with a set of values
