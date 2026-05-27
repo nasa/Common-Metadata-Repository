f132d50215ab27f220a2d826daec56c95d0855a9
Anthony Ortega <aortega@appddictionstudio.com>
2026-05-27 15:31:40 -0500

    added additional KMS caches to kms_lookup.cls and kms_lookup_errors_test.clj to address CmrHashCache error

--- common-app-lib/test/cmr/common_app/test/services/kms_lookup_errors_test.clj
+++ common-app-lib/test/cmr/common_app/test/services/kms_lookup_errors_test.clj
@@ -1,4 +1,4 @@
-(ns cmr.common-app.test.services.kms-lookup-errors-test
+`(ns cmr.common-app.test.services.kms-lookup-errors-test
   "Unit tests for specific connection errors coming from kms-lookup"
   (:require
     [clojure.test :refer [deftest is testing]]
@@ -10,7 +10,19 @@
                      kms-lookup/kms-projects-cache-key (kms-lookup/create-kms-project-uuid-cache)
                      kms-lookup/kms-umm-c-cache-key (kms-lookup/create-kms-umm-c-cache)
                      kms-lookup/kms-location-cache-key (kms-lookup/create-kms-location-cache)
-                     kms-lookup/kms-measurement-cache-key (kms-lookup/create-kms-measurement-cache)}}})
+                     kms-lookup/kms-measurement-cache-key (kms-lookup/create-kms-measurement-cache)
+                     kms-lookup/kms-processing-level-cache-key (kms-lookup/create-kms-processing-level-uuid-cache)
+                     kms-lookup/kms-science-keywords-cache-key (kms-lookup/create-kms-science-keywords-uuid-cache)
+                     kms-lookup/kms-platforms-cache-key (kms-lookup/create-kms-platforms-uuid-cache)
+                     kms-lookup/kms-instruments-cache-key (kms-lookup/create-kms-instruments-uuid-cache)
+                     kms-lookup/kms-providers-cache-key (kms-lookup/create-kms-providers-uuid-cache)
+                     kms-lookup/kms-spatial-keywords-cache-key (kms-lookup/create-kms-spatial-keywords-uuid-cache)
+                     kms-lookup/kms-concepts-cache-key (kms-lookup/create-kms-concepts-uuid-cache)
+                     kms-lookup/kms-iso-topic-categories-cache-key (kms-lookup/create-kms-iso-topic-categories-uuid-cache)
+                     kms-lookup/kms-granule-data-format-cache-key (kms-lookup/create-kms-granule-data-format-uuid-cache)
+                     kms-lookup/kms-mime-type-cache-key (kms-lookup/create-kms-mime-type-uuid-cache)
+                     kms-lookup/kms-related-urls-cache-key (kms-lookup/create-kms-related-urls-uuid-cache)
+                     kms-lookup/kms-temporal-keywords-cache-key (kms-lookup/create-kms-temporal-keywords-uuid-cache)}}})
 
 (def create-context-broken
   "Creates a testing concept with the KMS caches."
@@ -18,7 +30,19 @@
                          kms-lookup/kms-projects-cache-key (kms-lookup/create-kms-project-uuid-cache)
                          kms-lookup/kms-umm-c-cache-key (kms-lookup/create-kms-umm-c-cache)
                          kms-lookup/kms-location-cache-key (kms-lookup/create-kms-location-cache)
-                         kms-lookup/kms-measurement-cache-key (kms-lookup/create-kms-measurement-cache)}}}
+                         kms-lookup/kms-measurement-cache-key (kms-lookup/create-kms-measurement-cache)
+                         kms-lookup/kms-processing-level-cache-key (kms-lookup/create-kms-processing-level-uuid-cache)
+                         kms-lookup/kms-science-keywords-cache-key (kms-lookup/create-kms-science-keywords-uuid-cache)
+                         kms-lookup/kms-platforms-cache-key (kms-lookup/create-kms-platforms-uuid-cache)
+                         kms-lookup/kms-instruments-cache-key (kms-lookup/create-kms-instruments-uuid-cache)
+                         kms-lookup/kms-providers-cache-key (kms-lookup/create-kms-providers-uuid-cache)
+                         kms-lookup/kms-spatial-keywords-cache-key (kms-lookup/create-kms-spatial-keywords-uuid-cache)
+                         kms-lookup/kms-concepts-cache-key (kms-lookup/create-kms-concepts-uuid-cache)
+                         kms-lookup/kms-iso-topic-categories-cache-key (kms-lookup/create-kms-iso-topic-categories-uuid-cache)
+                         kms-lookup/kms-granule-data-format-cache-key (kms-lookup/create-kms-granule-data-format-uuid-cache)
+                         kms-lookup/kms-mime-type-cache-key (kms-lookup/create-kms-mime-type-uuid-cache)
+                         kms-lookup/kms-related-urls-cache-key (kms-lookup/create-kms-related-urls-uuid-cache)
+                         kms-lookup/kms-temporal-keywords-cache-key (kms-lookup/create-kms-temporal-keywords-uuid-cache)}}}
       (update-in
        [:system :caches :kms-measurement-index :read-connection :spec :host]
        (constantly "example.gov"))))
