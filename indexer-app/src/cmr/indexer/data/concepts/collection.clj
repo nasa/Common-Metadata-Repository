5e6d46b04781983eaff1589f76af7c447a025230
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2026-06-01 12:19:42 -0400

    CMR-11300: Fix missing uuid lookups for the concept scheme providers and fix using the new cache lookup for spatial-keywords

--- indexer-app/src/cmr/indexer/data/concepts/collection.clj
+++ indexer-app/src/cmr/indexer/data/concepts/collection.clj
@@ -472,7 +472,8 @@
                                                 :kms-uuids (let [concept-uuids (keep :uuid (map #(kms-util/concept->elastic-doc context %) (:DirectoryNames collection)))
                                                                  granule-data-format-uuids (keep :uuid (map #(kms-util/granule-data-format->elastic-doc context %) granule-data-format))
                                                                  iso-topic-uuids (keep :uuid (map #(kms-util/iso-topic-category->elastic-doc context %) (:ISOTopicCategories collection)))
-                                                                 location-uuids (keep :uuid (map #(clk/location-keyword->elastic-doc context %) (:LocationKeywords collection)))
+                                                                 ;; Location and spatial-keywords are used somewhat interchangably in the system
+                                                                 location-uuids (keep :uuid (map #(kms-util/spatial-keyword-by-map->elastic-doc context %) (:LocationKeywords collection)))
                                                                  ;; The mimetypes being validated are from the `GETDATA` field
                                                                  mime-type-uuids (keep :uuid (map #(kms-util/mime-type->elastic-doc context %) (keep :MimeType (keep :GetData related-urls))))
                                                                  processing-level-uuid (:uuid (kms-util/processing-level-id->elastic-doc context processing-level-id))
@@ -481,8 +482,9 @@
                                                                  temporal-uuids (keep :uuid (map #(kms-util/temporal-keyword->elastic-doc context %) temporal-keywords))
                                                                  science-keyword-uuids (keep :uuid (map #(kms-util/science-keyword->elastic-doc context %) (:ScienceKeywords collection)))
                                                                  platform-uuids (keep :uuid (map #(kms-util/platform->elastic-doc context %) platform-short-names))
+                                                                 provider-uuids (keep :uuid (map #(kms-util/provider->elastic-doc context %) data-center-names))
                                                                  instrument-uuids (keep :uuid (map #(kms-util/instrument->elastic-doc context %) instrument-short-names))
-                                                                 uuids (distinct (concat concept-uuids granule-data-format-uuids iso-topic-uuids location-uuids mime-type-uuids project-uuids related-url-uuids temporal-uuids science-keyword-uuids platform-uuids instrument-uuids))]
+                                                                 uuids (distinct (concat concept-uuids granule-data-format-uuids iso-topic-uuids location-uuids mime-type-uuids project-uuids related-url-uuids temporal-uuids science-keyword-uuids platform-uuids instrument-uuids provider-uuids))]
                                                              ;; Uniquely this can only be a single value
                                                              (if processing-level-uuid
                                                                (conj uuids processing-level-uuid)
