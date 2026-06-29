573b4f57c282de88b68823d0f010e41d14925d0f
jmaeng72 <44305062+jmaeng72@users.noreply.github.com>
2026-06-29 14:48:45 -0400

    revert migration changes for 092

--- metadata-db-app/src/cmr/metadata_db/migrations/092_update_cmr_subscriptions_table.clj
+++ metadata-db-app/src/cmr/metadata_db/migrations/092_update_cmr_subscriptions_table.clj
@@ -7,16 +7,12 @@
   "Migrates the database up to version 92."
   []
   (println "cmr.metadata-db.migrations.092-update-cmr-sub-notifications-table up...")
-  (when (h/column-exists? "CMR_SUB_NOTIFICATIONS" "AWS_ARN")
-    (h/sql "ALTER TABLE METADATA_DB.CMR_SUB_NOTIFICATIONS DROP COLUMN AWS_ARN"))
-  (when-not (h/column-exists? "CMR_SUBSCRIPTIONS" "AWS_ARN")
-    (h/sql "ALTER TABLE METADATA_DB.CMR_SUBSCRIPTIONS ADD AWS_ARN VARCHAR(2048) NULL")))
+  (h/sql "ALTER TABLE METADATA_DB.CMR_SUB_NOTIFICATIONS DROP COLUMN AWS_ARN")
+  (h/sql "ALTER TABLE METADATA_DB.CMR_SUBSCRIPTIONS ADD AWS_ARN VARCHAR(2048) NULL"))
 
 (defn down
   "Migrates the database down from version 92."
   []
   (println "cmr.metadata-db.migrations.092-update-cmr-sub-notifications-table down.")
-  (when-not (h/column-exists? "CMR_SUB_NOTIFICATIONS" "AWS_ARN")
-    (h/sql "ALTER TABLE METADATA_DB.CMR_SUB_NOTIFICATIONS ADD AWS_ARN VARCHAR(2048) NULL"))
-  (when (h/column-exists? "CMR_SUBSCRIPTIONS" "AWS_ARN")
-    (h/sql "ALTER TABLE METADATA_DB.CMR_SUBSCRIPTIONS DROP COLUMN AWS_ARN")))
+  (h/sql "ALTER TABLE METADATA_DB.CMR_SUB_NOTIFICATIONS ADD AWS_ARN VARCHAR(2048) NULL")
+  (h/sql "ALTER TABLE METADATA_DB.CMR_SUBSCRIPTIONS DROP COLUMN AWS_ARN"))
