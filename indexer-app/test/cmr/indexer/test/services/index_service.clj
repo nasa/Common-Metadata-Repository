260cf0411880d1d376356a676ab8d450c7340fba
Lucia Zimmermann <7947215+zimzoom@users.noreply.github.com>
2026-07-07 22:43:23 -0400

    CMR-11271: fix collection cascade delete

--- indexer-app/test/cmr/indexer/test/services/index_service.clj
+++ indexer-app/test/cmr/indexer/test/services/index_service.clj
@@ -4,6 +4,12 @@
    [cheshire.core :as json]
    [clojure.test :refer :all]
    [cmr.common.util :refer [are3]]
+   [cmr.elastic-utils.config :as es-config]
+   [cmr.elastic-utils.es-helper :as es-helper]
+   [cmr.indexer.data.elasticsearch :as es]
+   [cmr.indexer.data.index-set :as idx-set]
+   [cmr.indexer.indexer-util :as indexer-util]
+   [cmr.indexer.services.index-set-service :as index-set-svc]
    [cmr.indexer.services.index-service :as index-svc]))
 
 (deftest index-concept-invalid-input-test
@@ -55,3 +61,82 @@
       (is (some? data) "time-to-visibility-json parsing test")
       (is (= "ACL" (:ct data)) "Checking concept type")
       (is (= "index-vis" (:mg data)) "Checking message id"))))
+
+(deftest cascade-collection-delete-test
+  (let [context {}
+        concept-mapping-types {:granule "granule"}
+        concept-id "C1234-PROV1"
+        revision-id 3
+        small-collections-index "1_small_collections"
+        individual-index "1_c1234_prov1"]
+    (testing "removes index-set metadata after successfully deleting an individual granule index"
+      (let [cleanup-calls (atom [])]
+        (with-redefs [idx-set/get-concept-type-index-names
+                      (fn [_context]
+                        {:index-names {:granule {:small_collections small-collections-index}}})
+                      idx-set/get-granule-index-names-for-collection
+                      (fn [_context coll-concept-id]
+                        (is (= concept-id coll-concept-id))
+                        [individual-index])
+                      es/delete-granule-index
+                      (fn [_context index]
+                        (is (= individual-index index))
+                        {:status 200})
+                      index-set-svc/remove-collection-granule-index-if-exists
+                      (fn [_context coll-concept-id]
+                        (swap! cleanup-calls conj coll-concept-id))
+                      index-svc/reindex-associated-variables
+                      (fn [_context _concept-id _revision-id])]
+          (#'index-svc/cascade-collection-delete context concept-mapping-types concept-id revision-id)
+          (is (= [concept-id] @cleanup-calls)))))
+
+    (testing "does not remove index-set metadata when granules are in small_collections"
+      (let [delete-by-query-calls (atom [])
+            cleanup-calls (atom [])]
+        (with-redefs [idx-set/get-concept-type-index-names
+                      (fn [_context]
+                        {:index-names {:granule {:small_collections small-collections-index}}})
+                      idx-set/get-granule-index-names-for-collection
+                      (fn [_context coll-concept-id]
+                        (is (= concept-id coll-concept-id))
+                        [small-collections-index])
+                      indexer-util/context->conn
+                      (fn [_context elastic-name]
+                        (is (= es-config/gran-elastic-name elastic-name))
+                        nil)
+                      es-helper/delete-by-query
+                      (fn [_conn index mapping query]
+                        (swap! delete-by-query-calls conj [index mapping query])
+                        {:status 200})
+                      es/delete-granule-index
+                      (fn [& _args]
+                        (is false "delete-granule-index should not be called for small_collections"))
+                      index-set-svc/remove-collection-granule-index-if-exists
+                      (fn [& _args]
+                        (swap! cleanup-calls conj :called))
+                      index-svc/reindex-associated-variables
+                      (fn [_context _concept-id _revision-id])]
+          (#'index-svc/cascade-collection-delete context concept-mapping-types concept-id revision-id)
+          (is (= 1 (count @delete-by-query-calls)))
+          (is (empty? @cleanup-calls)))))
+
+    (testing "does not remove index-set metadata when deleting an individual granule index fails"
+      (let [cleanup-calls (atom [])]
+        (with-redefs [idx-set/get-concept-type-index-names
+                      (fn [_context]
+                        {:index-names {:granule {:small_collections small-collections-index}}})
+                      idx-set/get-granule-index-names-for-collection
+                      (fn [_context coll-concept-id]
+                        (is (= concept-id coll-concept-id))
+                        [individual-index])
+                      es/delete-granule-index
+                      (fn [_context index]
+                        (is (= individual-index index))
+                        {:status 500})
+                      index-set-svc/remove-collection-granule-index-if-exists
+                      (fn [& _args]
+                        (swap! cleanup-calls conj :called))
+                      index-svc/reindex-associated-variables
+                      (fn [_context _concept-id _revision-id])]
+          (#'index-svc/cascade-collection-delete context concept-mapping-types concept-id revision-id)
+          (is (empty? @cleanup-calls)))))))
