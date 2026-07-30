590b1c73897d410af42dfec6480f3694f76bea21
Anthony Ortega <aortega@appddictionstudio.com>
2026-07-30 15:32:56 -0500

    CMR-11271 - Added integration test for cascade-collection-delete

--- system-int-test/test/cmr/system_int_test/ingest/misc/deleted_granule_index_test.clj
+++ system-int-test/test/cmr/system_int_test/ingest/misc/deleted_granule_index_test.clj
@@ -12,6 +12,37 @@
 
 (use-fixtures :once (ingest/reset-fixture {"provguid1" "PROV1"}))
 
+(deftest cascade-collection-delete-removes-separate-granule-index-test
+  (let [collection (data-core/ingest-umm-spec-collection
+                    "PROV1"
+                    (data-umm-c/collection {})
+                    {:validate-keywords false})
+        concept-id (:concept-id collection)
+        concept-key (keyword concept-id)]
+    (bootstrap/start-rebalance-collection concept-id)
+    (index/wait-until-indexed)
+    (bootstrap/finalize-rebalance-collection concept-id)
+    (index/wait-until-indexed)
+
+    (testing "the separate index exists before collection deletion"
+      (let [index-set (index/get-index-set-by-id 1)]
+        (is (= 200 (:status index-set)))
+        (is (some? (get-in index-set [:index-set :concepts :granule concept-key])))
+        (is (some #(= concept-id (:name %))
+                  (get-in index-set [:index-set :granule :indexes])))
+        (is (= 200 (:status (index/gran-elastic-index-exists? collection))))))
+
+    (ingest/delete-concept (data-core/umm-c-collection->concept collection :echo10) {})
+    (index/wait-until-indexed)
+
+    (testing "cascade deletion removes the separate index and its index-set entries"
+      (let [index-set (index/get-index-set-by-id 1)]
+        (is (= 200 (:status index-set)))
+        (is (nil? (get-in index-set [:index-set :concepts :granule concept-key])))
+        (is (not-any? #(= concept-id (:name %))
+                      (get-in index-set [:index-set :granule :indexes])))
+        (is (= 404 (:status (index/gran-elastic-index-exists? collection))))))))
+
 (deftest deleted-granule-test-index
   (testing "Ingest collection, rebalance collection, delete collection"
     (let [collection (data-core/ingest-umm-spec-collection "PROV1"
