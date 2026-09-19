785aa4d543c3c8af1404dd6bb5330c7d16877343
Erich Reiter <erich.e.reiter@nasa.gov>
2026-09-19 08:55:57 -0400

    CMR-11541: Fixing sys int test.

--- system-int-test/test/cmr/system_int_test/transmit/error_handling_test.clj
+++ system-int-test/test/cmr/system_int_test/transmit/error_handling_test.clj
@@ -1,9 +1,7 @@
 (ns cmr.system-int-test.transmit.error-handling-test
   (:require
    [cheshire.core :as json]
-   [clojure.test :refer :all]
-   [cmr.common.util :as util :refer [are3]]
-   [cmr.mock-echo.client.echo-util :as echo-util]
+   [clojure.test :refer [deftest is testing use-fixtures]]
    [cmr.system-int-test.data2.umm-spec-collection :as data-umm-c]
    [cmr.system-int-test.utils.ingest-util :as ingest]
    [cmr.system-int-test.utils.search-util :as search]))
@@ -13,9 +11,9 @@
 (deftest gateway-timeout-test
   (testing "Gateway Timeouts are handled"
     (testing "during search"
-      (is (= {:status 504
+      (is (= {:status 528
               :errors ["A gateway timeout occurred, please try your request again later."]}
-             ;; gateway-timeout is a special-token that triggers a 504 in mock-urs
+             ;; gateway-timeout is a special-token that triggers a 528 in mock-urs
              (search/find-refs :collection {:token "gateway-timeout"}))))
 
     (testing "during ingest"
@@ -25,6 +23,6 @@
                                      :StandardProduct true}
                                     :umm-json)
                                    {:token "gateway-timeout"})]
-        (is (= 504 status))
+        (is (= 528 status))
         (is (= {:errors ["A gateway timeout occurred, please try your request again later."]}
                (json/parse-string body true)))))))
